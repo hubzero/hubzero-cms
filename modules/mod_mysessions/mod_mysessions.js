04ace1537b6b699a67111d0bed45a87ffbc6ea48
Shawn Rice <zooley@purdue.edu>
2009-12-10 10:24:34 -0500

    partition my/admin session list

--- modules/mod_mysessions/mod_mysessions.js
+++ modules/mod_mysessions/mod_mysessions.js
@@ -27,18 +27,76 @@ if (!HUB) {
 // My sessions module
 //-------------------------------------------------------------
 
+var MySessionsTabs = new Class({
+	
+	initialize: function(element, options) {
+		this.options = Object.extend({
+			changeTransition:	Fx.Transitions.Bounce.easeOut,
+			duration:			1000,
+			mouseOverClass:		'active',
+			activateOnLoad:		'first'
+		}, options || {});
+		
+		this.el = $(element);
+		this.elid = element;
+		
+		this.titles = $$('#' + this.elid + ' ul.session_tab_titles li');
+		this.panels = $$('#' + this.elid + ' .session_tab_panel');
+		
+		this.titles.each(function(item) {
+			item.addEvent('click', function(){
+					item.removeClass(this.options.mouseOverClass);
+					this.activate(item);
+				}.bind(this)
+			);
+			
+			item.addEvent('mouseover', function() {
+				if (item != this.activeTitle) {
+					item.addClass(this.options.mouseOverClass);
+				}
+			}.bind(this));
+			
+			item.addEvent('mouseout', function() {
+				if (item != this.activeTitle) {
+					item.removeClass(this.options.mouseOverClass);
+				}
+			}.bind(this));
+		}.bind(this));
+	},
+	
+	activate: function(tab) {
+		if ($type(tab) == 'string') {
+			myTab = $$('#' + this.elid + ' ul li').filterByAttribute('title', '=', tab)[0];
+			tab = myTab;
+		}
+		
+		if ($type(tab) == 'element') {
+			var newTab = tab.getProperty('title');
+			this.panels.removeClass('active');
+			this.activePanel = this.panels.filterById(newTab)[0];
+			this.activePanel.addClass('active');
+			this.titles.removeClass('active');
+			
+			tab.addClass('active');
+			
+			this.activeTitle = tab;
+		}
+	}
+});
+
 HUB.Mod_MySessions = {
 	initialize: function() {
 		HUB.Mod_MySessions.diskMonitor();
+		MTT = new MySessionsTabs('mySessionsTabs', {changeTransition: 'none', mouseOverClass: 'over'});
 	},
 
 	diskMonitor: function() {
 		if ($('diskusage')) {
-			function fetch(){			
+			function fetch() {			
 				new Ajax('index.php?option=com_mw&task=diskusage&no_html=1&msgs=1',{
-						 'method' : 'get',
-						 'update' : $('diskusage')
-						 }).request();
+					'method' : 'get',
+					'update' : $('diskusage')
+				}).request();
 			}
 
 			fetch.periodical(60000);
