0f3bb2bc8de392e83b952ce4406015d78ae20a32
Shawn Rice <zooley@purdue.edu>
2010-02-09 10:35:32 -0500

    add misc common functions to Hubzero_View_Helper_Html

--- plugins/xhub/xlibraries/Hubzero_View_Helper_Html.php
+++ plugins/xhub/xlibraries/Hubzero_View_Helper_Html.php
@@ -163,4 +163,82 @@ class Hubzero_View_Helper_Html
 
 		return $text;
 	}
+	
+	//-----------
+
+	public function mkt($stime)
+	{
+		if ($stime && ereg("([0-9]{4})-([0-9]{2})-([0-9]{2})[ ]([0-9]{2}):([0-9]{2}):([0-9]{2})", $stime, $regs )) {
+			$stime = mktime( $regs[4], $regs[5], $regs[6], $regs[2], $regs[3], $regs[1] );
+		}
+		return $stime;
+	}
+
+	//-----------
+
+	public function timeAgoo($timestamp)
+	{
+		// Store the current time
+		$current_time = time();
+
+		// Determine the difference, between the time now and the timestamp
+		$difference = $current_time - $timestamp;
+
+		// Set the periods of time
+		$periods = array('second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade');
+
+		// Set the number of seconds per period
+		$lengths = array(1, 60, 3600, 86400, 604800, 2630880, 31570560, 315705600);
+
+		// Determine which period we should use, based on the number of seconds lapsed.
+		// If the difference divided by the seconds is more than 1, we use that. Eg 1 year / 1 decade = 0.1, so we move on
+		// Go from decades backwards to seconds
+		for ($val = sizeof($lengths) - 1; ($val >= 0) && (($number = $difference / $lengths[$val]) <= 1); $val--);
+
+		// Ensure the script has found a match
+		if ($val < 0) $val = 0;
+
+		// Determine the minor value, to recurse through
+		$new_time = $current_time - ($difference % $lengths[$val]);
+
+		// Set the current value to be floored
+		$number = floor($number);
+
+		// If required create a plural
+		if ($number != 1) $periods[$val].= 's';
+
+		// Return text
+		$text = sprintf("%d %s ", $number, $periods[$val]);
+
+		// Ensure there is still something to recurse through, and we have not found 1 minute and 0 seconds.
+		if (($val >= 1) && (($current_time - $new_time) > 0)) {
+			$text .= Hubzero_View_Helper_Html::timeAgoo($new_time);
+		}
+
+		return $text;
+	}
+
+	//-----------
+
+	public function timeAgo($timestamp) 
+	{
+		$text = Hubzero_View_Helper_Html::timeAgoo($timestamp);
+
+		$parts = explode(' ',$text);
+
+		$text  = $parts[0].' '.$parts[1];
+		$text .= ($parts[2]) ? ' '.$parts[2].' '.$parts[3] : '';
+		return $text;
+	}
+	
+	//-----------
+	
+	public function niceidformat($someid) 
+	{
+		while (strlen($someid) < 5) 
+		{
+			$someid = 0 . "$someid";
+		}
+		return $someid;
+	}
 }
