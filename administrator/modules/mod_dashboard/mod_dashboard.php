d0bef5054fa734750e78f6dc14139cc56b769453
Shawn Rice <zooley@purdue.edu>
2010-03-25 18:15:36 -0400

    don't use index2 in dashboard links

--- administrator/modules/mod_dashboard/mod_dashboard.php
+++ administrator/modules/mod_dashboard/mod_dashboard.php
@@ -108,27 +108,27 @@ defined('_JEXEC') or die( 'Restricted access' );
 
 ?>
                 <div id="dashboard">
-                 <h3 class="hed-abuse"><a href="index2.php?option=com_reportabuse">Abuse reports</a></h3>
-                   <p class="<?php if ($reports) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($reports==1) ? 'is': 'are'; ?> <?php if($reports) { echo '<a href="index2.php?option=com_support&amp;task=abusereports">'; } ?><?php echo $reports ?>  new abuse report<?php echo ($reports==1) ? '': 's'; ?><?php if($reports)  { echo '</a>';} ?>.</p>
+                 <h3 class="hed-abuse"><a href="index.php?option=com_support&amp;task=abusereports">Abuse reports</a></h3>
+                   <p class="<?php if ($reports) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($reports==1) ? 'is': 'are'; ?> <?php if($reports) { echo '<a href="index.php?option=com_support&amp;task=abusereports">'; } ?><?php echo $reports ?>  new abuse report<?php echo ($reports==1) ? '': 's'; ?><?php if($reports)  { echo '</a>';} ?>.</p>
                    
-                 <h3 class="hed-support"><a href="index2.php?option=com_support">Support tickets</a></h3>
-                   <p class="<?php if ($newtickets) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($newtickets==1) ? 'is': 'are'; ?> <?php if($newtickets) { echo '<a href="index2.php?option=com_support">'; } ?><?php echo $newtickets ?>  unassigned ticket<?php echo ($newtickets==1) ? '': 's'; ?><?php if($newtickets)  { echo '</a>';} ?>.</p>
+                 <h3 class="hed-support"><a href="index.php?option=com_support">Support tickets</a></h3>
+                   <p class="<?php if ($newtickets) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($newtickets==1) ? 'is': 'are'; ?> <?php if($newtickets) { echo '<a href="index.php?option=com_support">'; } ?><?php echo $newtickets ?>  unassigned ticket<?php echo ($newtickets==1) ? '': 's'; ?><?php if($newtickets)  { echo '</a>';} ?>.</p>
                    
-                   <p class="<?php if ($oldtickets) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($oldtickets==1) ? 'is': 'are'; ?> <?php if($oldtickets) { echo '<a href="index2.php?option=com_support">'; } ?><?php echo $oldtickets ?>  ticket<?php echo ($newtickets==1) ? '': 's'; ?><?php if($oldtickets)  { echo '</a>';} ?> open for more than three months.</p>
+                   <p class="<?php if ($oldtickets) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($oldtickets==1) ? 'is': 'are'; ?> <?php if($oldtickets) { echo '<a href="index.php?option=com_support">'; } ?><?php echo $oldtickets ?>  ticket<?php echo ($newtickets==1) ? '': 's'; ?><?php if($oldtickets)  { echo '</a>';} ?> open for more than three months.</p>
                    
-                 <h3 class="hed-resources"><a href="index2.php?option=com_resources&amp;task=pending&amp;status=3">Pending resources</a></h3>
-                   <p class="<?php if ($pending) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($pending==1) ? 'is': 'are'; ?> <?php if($pending) { echo '<a href="index2.php?option=com_resources&amp;task=pending&amp;status=3">'; } ?><?php echo $pending ?>  pending resource<?php echo ($pending==1) ? '': 's'; ?><?php if($pending)  { echo '</a>';} ?>.</p>
+                 <h3 class="hed-resources"><a href="index.php?option=com_resources&amp;task=pending&amp;status=3">Pending resources</a></h3>
+                   <p class="<?php if ($pending) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($pending==1) ? 'is': 'are'; ?> <?php if($pending) { echo '<a href="index.php?option=com_resources&amp;task=pending&amp;status=3">'; } ?><?php echo $pending ?>  pending resource<?php echo ($pending==1) ? '': 's'; ?><?php if($pending)  { echo '</a>';} ?>.</p>
                    
                  <h3 class="hed-contribtool"><a href="../index.php?option=com_contribtool">Tool contributions</a></h3>
                    <p class="<?php if ($contribtool) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($contribtool==1) ? 'is': 'are'; ?> <?php if($contribtool) { echo '<a href="../index.php?option=com_contribtool">'; } ?><?php echo $contribtool ?>  tool contribution<?php echo ($contribtool==1) ? '': 's'; ?><?php if($contribtool)  { echo '</a>';} ?> requiring administrator action.</p>
                    
     <?php if($banking) {?>
-                 <h3 class="hed-store"><a href="index2.php?option=com_store">Store orders</a></h3>
-                   <p class="<?php if ($orders) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($orders==1) ? 'is': 'are'; ?> <?php if($orders) { echo '<a href="index2.php?option=com_store&amp;task=orders&amp;filterby=new">'; } ?><?php echo $orders ?> new order<?php echo ($orders==1) ? '': 's'; ?><?php if($orders)  { echo '</a>';} ?> in the store.</p>
+                 <h3 class="hed-store"><a href="index.php?option=com_store">Store orders</a></h3>
+                   <p class="<?php if ($orders) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($orders==1) ? 'is': 'are'; ?> <?php if($orders) { echo '<a href="index.php?option=com_store&amp;task=orders&amp;filterby=new">'; } ?><?php echo $orders ?> new order<?php echo ($orders==1) ? '': 's'; ?><?php if($orders)  { echo '</a>';} ?> in the store.</p>
                    
     <?php } ?>
-                 <h3 class="hed-quote"><a href="index2.php?option=com_feedback">Success stories</a></h3>
-                   <p class="<?php if ($quotes) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($quotes==1) ? 'is': 'are'; ?> <?php if($quotes) { echo '<a href="index2.php?option=com_feedback">'; } ?><?php echo $quotes ?> new success stor<?php echo ($quotes==1) ? 'y': 'ies'; ?><?php if($quotes)  { echo '</a>';} ?> received in the past month.</p>
+                 <h3 class="hed-quote"><a href="index.php?option=com_feedback">Success stories</a></h3>
+                   <p class="<?php if ($quotes) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($quotes==1) ? 'is': 'are'; ?> <?php if($quotes) { echo '<a href="index.php?option=com_feedback">'; } ?><?php echo $quotes ?> new success stor<?php echo ($quotes==1) ? 'y': 'ies'; ?><?php if($quotes)  { echo '</a>';} ?> received in the past month.</p>
                    
                  <h3 class="hed-wish">Wishes</h3>
                    <p class="<?php if ($wishes) { echo 'p-attention';} else { echo'p-zeroitems' ;} ?>">There <?php echo ($wishes==1) ? 'is': 'are'; ?> <?php if($wishes) { echo '<a href="../index.php?option=com_wishlist">'; } ?><?php echo $wishes ?> open wish<?php echo ($wishes==1) ? '': 'es'; ?><?php if($wishes)  { echo '</a>';} ?> on the main <?php echo $hubname; ?> Wish List.</p>
