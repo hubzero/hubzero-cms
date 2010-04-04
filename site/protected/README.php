06fa1576c604d02748bec5b23acf2402128566b5
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2010-04-04 14:32:14 -0400

    add site and site/protected directories to default install

---
+++ site/protected/README.php
@@ -0,0 +1,8 @@
+<?php 
+die();
+?>
+
+This protected directory is only protected from access if the webserver
+has been configured to deny access to this directory. For increased 
+security you may want to move the protected directory outside of the document
+root entirely.
