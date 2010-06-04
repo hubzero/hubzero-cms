8a47b05fccee8171cf32f1231b5e680740fe290c
Shawn Rice <zooley@purdue.edu>
2010-06-04 13:17:48 -0400

    add helper for mod_randomquote

--- modules/mod_randomquote/mod_randomquote.php
+++ modules/mod_randomquote/mod_randomquote.php
@@ -1,101 +1,36 @@
 <?php
-// No direct access
-defined('_JEXEC') or die('Restricted access');
+/**
+ * @package		HUBzero CMS
+ * @author		Alissa Nedossekina <alisa@purdue.edu>
+ * @copyright	Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GPLv2
+ *
+ * Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
-//-------------------------------------------------------------
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
 
-	class modRandomQuote
-	{
-		private $attributes = array();
-
-		//-----------
-	
-		public function __construct( $params ) 
-		{
-			$this->params = $params;
-		}
-	
-		//-----------
-	
-		public function __set($property, $value)
-		{
-			$this->attributes[$property] = $value;
-		}
-		
-		//-----------
-		
-		public function __get($property)
-		{
-			if (isset($this->attributes[$property])) {
-				return $this->attributes[$property];
-			}
-		}
-		
-		//-----------
-
-		public function display() 
-		{
-			require_once( JPATH_ROOT.DS.'administrator'.DS.'components'.DS.'com_feedback'.DS.'selectedquotes.class.php' );
-			ximport('Hubzero_View_Helper_Html');
-		
-			$database =& JFactory::getDBO();
-			
-			$params =& $this->params;
-			
-			//Get the admin configured settings
-			$filters = array();
-			$filters['limit'] = 1;
-			$charlimit = $params->get( 'charlimit', 150 );
-			$showauthor = $params->get( 'show_author', 1 );
-			$showall = $params->get( 'show_all_link', 1 );
-			$quotesrc = $params->get( 'quotesrc', 'miniquote' );
-		
-			$pool = trim($params->get( 'quotepool'));
-			$filters['notable_quotes'] = $pool == 'notable_quotes' ?  1 : 0;
-			$filters['flash_rotation'] = $pool == 'flash_rotation' ?  1 : 0;
-			$filters['miniquote'] = $quotesrc == 'miniquote' ?  1 : 0;
-			$filters['sortby'] = 'RAND()';
-	
-			$this->filters = $filters;
-	
-			// Get quotes
-			$sq = new SelectedQuotes( $database );
-			$quotes = $sq->getResults( $filters );
-			$quote = $quotes ? $quotes[0] : '';
-			
-			// Push some CSS to the template
-			ximport('xdocument');
-			XDocument::addModuleStylesheet('mod_randomquote');
-			
-			// Start output
-			$html = '';
-			$quote_to_show = $quotesrc == 'miniquote' ? stripslashes($quote->miniquote) : stripslashes($quote->short_quote);
-			/*
-			if($quote) {
-				$html  .= '<p class="fquote">'."\n";
-				$html  .= ' <a href="/about/quotes/?quoteid='.$quote->id.'" title="'.JText::_('View the full quote by').' '.stripslashes($quote->fullname).'. '.stripslashes($quote->org).'" class="showfullquote">'.Hubzero_View_Helper_Html::shortenText(stripslashes($quote_to_show), $charlimit, 0).'</a> '."\n" ;
-				$html  .= ' <span> - '.$quote->fullname.', '.JText::_('in').'&nbsp;<a href="/about/quotes">'.JText::_('Notable Quotes').'</a></span></p>'."\n";
-			}*/
-			
-			if($quote) {
-				$html  .= '<h3 class="notable_quote">'.JText::_('Notable Quote').'</h3>'."\n";
-				$html  .= '<div class="frontquote">'."\n";
-				$html  .= ' <blockquote cite="'.$quote->fullname.'"><p>'."\n";
-				$html  .= Hubzero_View_Helper_Html::shortenText(stripslashes($quote_to_show), $charlimit, 0)."\n" ;
-				$html  .= strlen($quote->quote) > $charlimit 
-				? '<a href="/about/quotes/?quoteid='.$quote->id.'" title="'.JText::_('View the full quote by').' '.$quote->fullname.'" class="showfullquote">...&raquo;</a>'."\n" 
-				: '' ;
-				$html  .= ' </p></blockquote>'."\n";
-				$html  .= '<p class="cite"><cite>'.$quote->fullname.'</cite>, '.$quote->org.' <span>-</span> <span>'.JText::_('in').'&nbsp;<a href="/about/quotes">'.JText::_('Notable&nbsp;Quotes').'</a></span></p>'."\n";
-				//$html  .= ' <p class="cite"><cite>'.$quote->fullname.'</cite> - '.JText::_('in').'&nbsp;<a href="/about/quotes">'.JText::_('Quotes').'</a></p>'."\n";
-				$html  .= '</div>'."\n";
-			}			
-			echo $html;
-		}		
-	}
+// Include the logic only once
+require_once (dirname(__FILE__).DS.'helper.php');
 
 //-------------------------------------------------------------
 
 $modrandomquote = new modRandomQuote( $params );
+$modrandomquote->display();
+
 require( JModuleHelper::getLayoutPath('mod_randomquote') );
-?>
