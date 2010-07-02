bb58ca6b26c8bcd11b6af5a36b1c354eb39a4b68
Hub Admin User <hubadmin@stage.neeshub.org>
2010-07-02 14:17:14 -0400

    [NEESHUB] extendedform groups

---
+++ plugins/groups/extendedform.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package		HUBzero CMS
+ * @author		Shawn Rice <zooley@purdue.edu>
+ * @copyright	Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GPLv2
+ *
+ * Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+//-----------
+
+jimport( 'joomla.plugin.plugin' );
+JPlugin::loadLanguage( 'plg_groups_extendedform' );
+
+//-----------
+
+class plgGroupsExtendedform extends JPlugin
+{
+	public function plgGroupsExtendedform(&$subject, $config)
+	{
+		parent::__construct($subject, $config);
+
+		// Load plugin parameters
+		$this->_plugin = JPluginHelper::getPlugin( 'groups', 'extendedform' );
+		$this->_params = new JParameter( $this->_plugin->params );
+	}
+
+	//-----------
+
+	public function onGroupDelete( $group ) 
+	{
+		// Start the log text
+		$log = JText::_('Example delete log').': ';
+		
+		// Do whatever you need for group deletion processing
+		
+		// Return the log
+		return $log;
+	}
+
+	//-----------
+	
+	public function onGroupNew( $group ) 
+	{
+		// Start the log text
+		$log = JText::_('Example new log').': ';
+		
+		// Do whatever you need for post group creation processing
+		$createShare = JRequest::getVar('shareddir', 0, 'post');
+		
+		// Return the log
+		return $log;
+	}
+}
