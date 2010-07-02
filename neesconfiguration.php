45e00707659855e631008e429b876018d5c4b65b
Hub Admin User <hubadmin@stage.neeshub.org>
2010-07-02 14:19:10 -0400

    [NEESHUB] add neesconfiguration.php

---
+++ neesconfiguration.php
@@ -0,0 +1,30 @@
+<?php
+
+  class NeesConfig{
+  	
+  	#DEV
+  	const ORACLE_SERVER = "devdb.neeshub.org/orcldev.neeshub.org";
+  	const ORACLE_USERNAME = "gemezm";
+  	const ORACLE_PASSWORD = "gemezm";
+  	const ORACLE_SCHEMA = "gemezm";
+  	
+  	#STAGE
+  	#const ORACLE_SERVER = "db-stg.neeshub.org:1521/orclstg.neeshub.org";
+  	#const ORACLE_USERNAME = "central";
+  	#const ORACLE_PASSWORD = "bees4nees";
+  	#const ORACLE_SCHEMA = "oracle";
+  	
+  	#PROD
+  	#const ORACLE_SERVER = "db.neeshub.org/orcl";
+  	#const ORACLE_USERNAME = "central";
+  	#const ORACLE_PASSWORD = "TomTurkey";
+  	#const ORACLE_SCHEMA = "oracle";
+  	
+  	#HPC
+  	#const ORACLE_SERVER = "128.210.135.51/orcl";
+  	#const ORACLE_USERNAME = "central";
+  	#const ORACLE_PASSWORD = "bees4nees";
+  	#const ORACLE_SCHEMA = "oracle";
+  }
+
+?>
