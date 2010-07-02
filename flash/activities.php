b7143a6669aa92bfeae9c9e99f38d2fe15e2d6b9
Hub Admin User <hubadmin@stage.neeshub.org>
2010-07-02 14:16:47 -0400

    [NEESHUB] nees flash

---
+++ flash/activities.php
@@ -0,0 +1,10 @@
+<?php ?>
+
+
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="https://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="322" height="462" id="map" align="middle">
+<param name="allowScriptAccess" value="sameDomain" />
+<param name="movie" value="/flash/map.swf" />
+<param name="quality" value="best" />
+<param name="bgcolor" value="#b3b3b3" />
+<embed src="/flash/map.swf" quality="best" bgcolor="#b3b3b3" width="462" height="322" name="map" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="https://www.macromedia.com/go/getflashplayer" />
+</object>
