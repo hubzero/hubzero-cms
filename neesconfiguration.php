e2a6315bef264099c31a2c688170c144e714f7de
Hub Admin User <hubadmin@neeshub.org>
2010-07-30 14:02:16 -0400

    [NEESHUB] point oracle to needb, set LAUNCH_INDEED variable

--- neesconfiguration.php
+++ neesconfiguration.php
@@ -2,29 +2,12 @@
 
   class NeesConfig{
   	
-  	#DEV
-  	const ORACLE_SERVER = "devdb.neeshub.org/orcldev.neeshub.org";
-  	const ORACLE_USERNAME = "gemezm";
-  	const ORACLE_PASSWORD = "gemezm";
-  	const ORACLE_SCHEMA = "gemezm";
+  	const ORACLE_SERVER = "neesdb.neeshub.org/neesprod";
+  	const ORACLE_USERNAME = "central";
+  	const ORACLE_PASSWORD = "bees4nees";
+  	const ORACLE_SCHEMA = "oracle";
   	
-  	#STAGE
-  	#const ORACLE_SERVER = "db-stg.neeshub.org:1521/orclstg.neeshub.org";
-  	#const ORACLE_USERNAME = "central";
-  	#const ORACLE_PASSWORD = "bees4nees";
-  	#const ORACLE_SCHEMA = "oracle";
-  	
-  	#PROD
-  	#const ORACLE_SERVER = "db.neeshub.org/orcl";
-  	#const ORACLE_USERNAME = "central";
-  	#const ORACLE_PASSWORD = "TomTurkey";
-  	#const ORACLE_SCHEMA = "oracle";
-  	
-  	#HPC
-  	#const ORACLE_SERVER = "128.210.135.51/orcl";
-  	#const ORACLE_USERNAME = "central";
-  	#const ORACLE_PASSWORD = "bees4nees";
-  	#const ORACLE_SCHEMA = "oracle";
+  	const LAUNCH_INDEED = "/tools/indeed/invoke?list";
   }
 
 ?>
