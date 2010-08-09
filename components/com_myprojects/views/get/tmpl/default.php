5f0e5f4c5a82df6eb785a50243e1a36257dc45a6
Hub Admin User <hubadmin@neeshub.org>
2010-08-09 12:55:45 -0400

    [NEESHUB] missing end tag angle bracket

--- components/com_myprojects/views/get/tmpl/default.php
+++ components/com_myprojects/views/get/tmpl/default.php
@@ -62,7 +62,7 @@ defined('_JEXEC') or die( 'Restricted access' );
                   
           ?>
             <div id="Project" style="width:100%;">
-              <div id="ProjectInfo" style="float:left;width:85%;"
+              <div id="ProjectInfo" style="float:left;width:85%;">
                 <a href="/warehouse/project/<?php echo $iProjectId; ?>" style="font-size: 15px;"><?php echo $strTitle; ?></a>
                 <p style="color: #666666"><?php echo $strStartDate; ?></p>
                 <p><?php echo $oDescriptionClob; ?></p>
