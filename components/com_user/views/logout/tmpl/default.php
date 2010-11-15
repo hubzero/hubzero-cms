08c094c29ff298860e46d61565e4658ff683b7e7
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2010-11-15 17:22:36 -0500

    add logout view to com_user, currently mostly a dupe of login view

---
+++ components/com_user/views/logout/tmpl/default.php
@@ -0,0 +1,8 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php if ($this->params->get( 'show_page_title', 1)) : ?>
+<div class="componentheading<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
+	<?php echo $this->escape($this->params->get('page_title')); ?>
+</div>
+<?php endif; ?>
+<?php echo $this->loadTemplate($this->type); ?>
+
---
+++ components/com_user/views/logout/view.html.php
@@ -0,0 +1,158 @@
+<?php
+/**
+* @version		$Id: view.html.php 14401 2010-01-26 14:10:00Z louis $
+* @package		Joomla
+* @subpackage	Logout
+* @copyright	Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.view');
+
+/**
+ * User component logout view class
+ *
+ * @package		Joomla
+ * @subpackage	Users
+ * @since	1.0
+ */
+class UserViewLogout extends JView
+{
+	function display($tpl = null)
+	{
+		global $mainframe, $option;
+
+		// Initialize variables
+		$document	=& JFactory::getDocument();
+		$user		=& JFactory::getUser();
+		$pathway	=& $mainframe->getPathway();
+		$image		= '';
+
+		$menu   =& JSite::getMenu();
+		$item   = $menu->getActive();
+		if($item)
+			$params	=& $menu->getParams($item->id);
+		else
+			$params	=& $menu->getParams(null);
+
+		$type = 'logout'; // (!$user->get('guest')) ? 'logout' : 'login';
+
+		// Set some default page parameters if not set
+		$params->def( 'show_page_title', 				1 );
+		if (!$params->get( 'page_title')) {
+				$params->set('page_title',	JText::_( 'Login' ));
+			}
+		if(!$item)
+		{
+			$params->def( 'header_login', 			'' );
+			$params->def( 'header_logout', 			'' );
+		}
+
+		$params->def( 'pageclass_sfx', 			'' );
+		$params->def( 'login', 					'index.php' );
+		$params->def( 'logout', 				'index.php' );
+		$params->def( 'description_login', 		1 );
+		$params->def( 'description_logout', 		1 );
+		$params->def( 'description_login_text', 	JText::_( 'LOGIN_DESCRIPTION' ) );
+		$params->def( 'description_logout_text',	JText::_( 'LOGOUT_DESCRIPTION' ) );
+		$params->def( 'image_login', 				'key.jpg' );
+		$params->def( 'image_logout', 				'key.jpg' );
+		$params->def( 'image_login_align', 			'right' );
+		$params->def( 'image_logout_align', 		'right' );
+		$usersConfig = &JComponentHelper::getParams( 'com_users' );
+		$params->def( 'registration', 				$usersConfig->get( 'allowUserRegistration' ) );
+
+		if ( !$user->get('guest') )
+		{
+			$title = JText::_( 'Logout');
+
+			// pathway item
+			$pathway->addItem($title, '' );
+			// Set page title
+			$document->setTitle( $title );
+		}
+		else
+		{
+			$title = JText::_( 'Login');
+
+			// pathway item
+			//$pathway->addItem($title, '' ); /* HUBZERO: remove extra pathway which goes to empty site url */
+			// Set page title
+			$document->setTitle( $title );
+		}
+
+		// Build login image if enabled
+		if ( $params->get( 'image_'.$type ) != -1 ) {
+			$image = 'images/stories/'.$params->get( 'image_'.$type );
+			$image = '<img src="'. $image  .'" align="'. $params->get( 'image_'.$type.'_align' ) .'" hspace="10" alt="" />';
+		}
+
+		// Get the return URL
+		if (!$url = JRequest::getVar('return', '', 'method', 'base64')) {
+			$url = base64_encode($params->get($type));
+		}
+
+		$uri = JURI::getInstance();
+		$furl = base64_encode($uri->toString());
+		$this->assign('freturn', $furl);
+
+		$errors =& JError::getErrors();
+
+		$this->assign('image' , $image);
+		$this->assign('type'  , $type);
+		$this->assign('return', $url);
+
+		$this->assignRef('params', $params);
+
+		// if authenticator is specified call plugin display method, otherwise (or if method does not exist) use default
+		
+		$authenticator = JRequest::getVar('authenticator', '', 'method');
+
+		if (!empty($authenticator)) {
+			
+			JPluginHelper::importPlugin('authentication');
+
+			$plugins = JPluginHelper::getPlugin('authentication');
+
+			foreach ($plugins as $plugin)
+			{	
+				$className = 'plg'.$plugin->type.$plugin->name;
+
+				if ($plugin->name != $authenticator)
+					continue;
+
+				if (class_exists($className))
+				{
+					$myplugin = new $className($this,(array)$plugin);
+
+					if (method_exists($className,'status'))
+					{
+						$status[$plugin->name] = $myplugin->status();
+
+						$this->assign('status', $status);
+					}
+					
+					if (method_exists($className,'display'))
+					{
+						$result = $myplugin->display($this, $tpl);
+
+						return $result;
+					}
+				}
+			}
+		}
+
+		parent::display($tpl);
+	}
+	
+	function attach() {}
+}
+
