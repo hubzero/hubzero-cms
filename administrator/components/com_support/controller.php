5162f90efe8ce9f2d119c9156f348838ef1a4317
Shawn Rice <zooley@purdue.edu>
2011-02-04 17:13:28 -0500

    [NEESHUB][#1806] Fix to display groups on tickets stats page

--- administrator/components/com_support/controller.php
+++ administrator/components/com_support/controller.php
@@ -331,6 +331,9 @@ class SupportController extends Hubzero_Controller
 		$view->type = ($type == 'automatic') ? 1 : 0;
 		
 		$view->group = JRequest::getVar('group', '');
+		if (!$view->group && trim($this->config->get('group'))) {
+			$view->group = trim($this->config->get('group'));
+		}
 		
 		// Set up some dates
 		$jconfig =& JFactory::getConfig();
@@ -381,13 +384,21 @@ class SupportController extends Hubzero_Controller
 		$view->closed['week'] = $st->getCountOfTicketsClosed($view->type, $year, $month, $week, null, $view->group);
 		
 		// Users
-		$query = "SELECT a.username, a.name, a.id"
-			. "\n FROM #__users AS a"
-			. "\n INNER JOIN #__core_acl_aro AS aro ON aro.value = a.id"	// map user to aro
-			. "\n INNER JOIN #__core_acl_groups_aro_map AS gm ON gm.aro_id = aro.id"	// map aro to group
-			. "\n INNER JOIN #__core_acl_aro_groups AS g ON g.id = gm.group_id"
-			. "\n WHERE a.block = '0' AND g.id=25"
-			. "\n ORDER BY a.name";
+		if ($view->group) {
+			$query = "SELECT a.username, a.name, a.id"
+				. "\n FROM #__users AS a, #__xgroups AS g, #__xgroups_members AS gm"
+				. "\n WHERE g.cn='".$view->group."' AND g.gidNumber=gm.gidNumber AND gm.uidNumber=a.id"
+				. "\n ORDER BY a.name";
+		} else {
+			$query = "SELECT a.username, a.name, a.id"
+				. "\n FROM #__users AS a"
+				. "\n INNER JOIN #__core_acl_aro AS aro ON aro.value = a.id"	// map user to aro
+				. "\n INNER JOIN #__core_acl_groups_aro_map AS gm ON gm.aro_id = aro.id"	// map aro to group
+				. "\n INNER JOIN #__core_acl_aro_groups AS g ON g.id = gm.group_id"
+				. "\n WHERE a.block = '0' AND g.id=25"
+				. "\n ORDER BY a.name";
+		}
+		
 		$this->database->setQuery( $query );
 		$view->users = $this->database->loadObjectList();
 		if ($view->users) {
