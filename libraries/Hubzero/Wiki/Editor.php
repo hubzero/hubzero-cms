7ffdd8f949a12a7c7f612a47c3fdf6c25ac9cec4
Shawn Rice <zooley@purdue.edu>
2011-02-14 11:30:30 -0500

    new wiki parser and editor classes

---
+++ libraries/Hubzero/Wiki/Editor.php
@@ -0,0 +1,283 @@
+<?php
+/**
+ * @package		HUBzero CMS
+ * @author		Shawn Rice <zooley@purdue.edu>
+ * @copyright	Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GPLv2
+ *
+ * Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// Check to ensure this file is within the rest of the framework
+defined('JPATH_BASE') or die();
+
+jimport('joomla.event.dispatcher');
+
+
+class Hubzero_Wiki_Editor extends JObservable
+{
+	/**
+	 * Editor Plugin object
+	 *
+	 * @var	object
+	 */
+	private $_editor = null;
+
+	/**
+	 * Editor Plugin name
+	 *
+	 * @var string
+	 */
+	private $_name = null;
+
+	/**
+	 * constructor
+	 *
+	 * @access	protected
+	 * @param	string	The editor name
+	 */
+	public function __construct($editor = '')
+	{
+		if (!$editor) {
+			$database =& JFactory::getDBO();
+			$database->setQuery( "SELECT element FROM #__plugins WHERE folder='hubzero' AND published=1 AND element LIKE 'wikieditor%' ORDER BY published DESC LIMIT 1" );
+			$editor = $database->loadResult();
+		}
+		$this->_name = $editor;
+	}
+
+	/**
+	 * Returns a reference to a global Editor object, only creating it
+	 * if it doesn't already exist.
+	 *
+	 * This method must be invoked as:
+	 * 		<pre>  $editor =& Hubzer_Wiki_Editor::getInstance($editor);</pre>
+	 *
+	 * @access	public
+	 * @param	string	$editor  The editor to use.
+	 * @return	JEditor	The Editor object.
+	 */
+	public function &getInstance($editor = '')
+	{
+		static $instances;
+
+		if (!isset($instances)) {
+			$instances = array();
+		}
+
+		$signature = serialize($editor);
+
+		if (empty($instances[$signature])) {
+			$instances[$signature] = new Hubzero_Wiki_Editor($editor);
+		}
+
+		return $instances[$signature];
+	}
+
+	/**
+	 * Initialize the editor
+	 */
+	public function initialise()
+	{
+		// Check if editor is already loaded
+		if (is_null(($this->_editor))) {
+			return;
+		}
+
+		$args['event'] = 'onInitEditor';
+
+		$return = '';
+		$results[] = $this->_editor->update($args);
+		foreach ($results as $result) 
+		{
+			if (trim($result)) {
+				$return = $result;
+			}
+		}
+
+		if ($return) {
+			$document =& JFactory::getDocument();
+			$document->addCustomTag($return);
+		}
+	}
+
+	/**
+	 * Present a text area
+	 *
+	 * @param	string	The control name
+	 * @param	string	The control id
+	 * @param	string	The contents of the text area
+	 * @param	string	The width of the text area (px or %)
+	 * @param	string	The height of the text area (px or %)
+	 * @param	int		The number of columns for the textarea
+	 * @param	int		The number of rows for the textarea
+	 * @param	array	Associative array of editor parameters
+	 */
+	public function display($name, $id, $html, $cls, $col, $row, $params = array())
+	{
+		// Return a standard textarea if no editor is found
+		if (!$this->_name) {
+			return '<textarea id="'.$name.'" name="'.$id.'" cols="'.$col.'" rows="'.$row.'" class="'.$cls.'">'.$html.'</textarea>'."\n";
+		}
+		
+		$this->_loadEditor($params);
+
+		// Check if editor is already loaded
+		if (is_null(($this->_editor))) {
+			return;
+		}
+
+		// Initialize variables
+		$return = null;
+
+		$args['name']    = $name;
+		$args['id']      = $id;
+		$args['content'] = $html;
+		$args['cls']     = $cls;
+		$args['col']     = $col;
+		$args['row']     = $row;
+		$args['event']   = 'onDisplayEditor';
+
+		$results[] = $this->_editor->update($args);
+
+		foreach ($results as $result)
+		{
+			if (trim($result)) {
+				$return .= $result;
+			}
+		}
+		return $return;
+	}
+
+	/**
+	 * Save the editor content
+	 *
+	 * @param	string	The name of the editor control
+	 */
+	public function save( $editor )
+	{
+		$this->_loadEditor();
+
+		// Check if editor is already loaded
+		if (is_null(($this->_editor))) {
+			return;
+		}
+
+		$args[] = $editor;
+		$args['event'] = 'onSaveEditorContent';
+
+		$return = '';
+		$results[] = $this->_editor->update($args);
+		foreach ($results as $result) 
+		{
+			if (trim($result)) {
+				$return .= $result;
+			}
+		}
+		return $return;
+	}
+
+	/**
+	 * Get the editor contents
+	 *
+	 * @param	string	The name of the editor control
+	 */
+	public function getContent( $editor )
+	{
+		$this->_loadEditor();
+
+		$args['name'] = $editor;
+		$args['event'] = 'onGetEditorContent';
+
+		$return = '';
+		$results[] = $this->_editor->update($args);
+		foreach ($results as $result) 
+		{
+			if (trim($result)) {
+				$return .= $result;
+			}
+		}
+		return $return;
+	}
+
+	/**
+	 * Set the editor contents
+	 *
+	 * @param	string	The name of the editor control
+	 * @param	string	The contents of the text area
+	 */
+	public function setContent( $editor, $html )
+	{
+		$this->_loadEditor();
+
+		$args['name'] = $editor;
+		$args['html'] = $html;
+		$args['event'] = 'onSetEditorContent';
+
+		$return = '';
+		$results[] = $this->_editor->update($args);
+		foreach ($results as $result) 
+		{
+			if (trim($result)) {
+				$return .= $result;
+			}
+		}
+		return $return;
+	}
+
+	/**
+	 * Load the editor
+	 *
+	 * @access	private
+	 * @param	array	Associative array of editor config paramaters
+	 * @since	1.5
+	 */
+	private function _loadEditor($config = array())
+	{
+		// Check if editor is already loaded
+		if (!is_null(($this->_editor))) {
+			return;
+		}
+
+		jimport('joomla.filesystem.file');
+		
+		// Build the path to the needed editor plugin
+		$name = JFilterInput::clean($this->_name, 'cmd');
+		$path = JPATH_SITE.DS.'plugins'.DS.'hubzero'.DS.$name.'.php';
+
+		if (!JFile::exists($path)) {
+			JError::raiseWarning( 500, JText::_('Cannot load the editor') );
+			return false;
+		}
+
+		// Require plugin file
+		require_once $path;
+
+		// Get the plugin
+		$plugin =& JPluginHelper::getPlugin('hubzero', $this->_name);
+		$params = new JParameter($plugin->params);
+		$params->loadArray($config);
+		$plugin->params = $params;
+
+		// Build editor plugin classname
+		$name = 'plgHubzero'.$this->_name;
+		if ($this->_editor = new $name($this, (array)$plugin)) {
+			// Load plugin parameters
+			$this->initialise();
+		}
+	}
+}
---
+++ libraries/Hubzero/Wiki/Parser.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * @package		HUBzero CMS
+ * @author		Shawn Rice <zooley@purdue.edu>
+ * @copyright	Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GPLv2
+ *
+ * Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// Check to ensure this file is within the rest of the framework
+defined('JPATH_BASE') or die();
+
+jimport('joomla.event.dispatcher');
+
+
+class Hubzero_Wiki_Parser extends JObservable
+{
+	/**
+	 * Parser Plugin object
+	 *
+	 * @var	object
+	 */
+	public $_parser = null;
+
+	/**
+	 * Parser Plugin name
+	 *
+	 * @var string
+	 */
+	public $_name = null;
+
+	/**
+	 * constructor
+	 *
+	 * @access	protected
+	 * @param	string	The parser name
+	 */
+	public function __construct($parser = '')
+	{
+		if (!$parser) {
+			$database =& JFactory::getDBO();
+			$database->setQuery( "SELECT element FROM #__plugins WHERE folder='hubzero' AND published=1 AND element LIKE 'wikiparser%' ORDER BY published DESC LIMIT 1" );
+			$parser = $database->loadResult();
+		}
+		$this->_name = $parser;
+	}
+
+	/**
+	 * Returns a reference to a global Parser object, only creating it
+	 * if it doesn't already exist.
+	 *
+	 * This method must be invoked as:
+	 * 		<pre>  $parser =& Hubzero_Wiki_Parser::getInstance($parser_name);</pre>
+	 *
+	 * @access	public
+	 * @param	string	$parser  The name of the parser to use.
+	 * @return	object  Hubzero_Wiki_Parser  The Parser object.
+	 */
+	public function &getInstance($parser = '')
+	{
+		static $instances;
+
+		if (!isset($instances)) {
+			$instances = array();
+		}
+
+		$signature = serialize($parser);
+
+		if (empty($instances[$signature])) {
+			$instances[$signature] = new Hubzero_Wiki_Parser($parser);
+		}
+
+		return $instances[$signature];
+	}
+
+	/**
+	 * Initialize the parser
+	 */
+	public function initialise($config=array(), $getnew=false)
+	{
+		// Check if parser is already loaded
+		if (is_null(($this->_parser))) {
+			return;
+		}
+
+		$args['config'] = $config;
+		$args['getnew'] = $getnew;
+		$args['event'] = 'onGetWikiParser';
+
+		$return = '';
+		$results[] = $this->_parser->update($args);
+		foreach ($results as $result) 
+		{
+			if (is_object($result)) {
+				$return = $result;
+			}
+		}
+	}
+
+	/**
+	 * Parse the text
+	 *
+	 * @param	string	The content to be parsed
+	 * @param	array	Params for the parser
+	 * @param	bool	Do a full parse or not
+	 * @param	bool	Use the existing parser or get new
+	 * @param	array	Params for the plugin
+	 */
+	public function parse($text, $config, $fullparse=true, $getnew=false, $params=array())
+	{
+		if (!$this->_name) {
+			return nl2br($text);
+		}
+		
+		$this->_loadParser($params, $config, $getnew);
+
+		// Check if parser is already loaded
+		if (is_null(($this->_parser))) {
+			return nl2br($text);
+		}
+
+		// Initialize variables
+		$return = null;
+
+		$args['text']      = $text;
+		$args['config']    = $config;
+		$args['fullparse'] = $fullparse;
+		$args['getnew']    = $getnew;
+		$args['event']     = 'onWikiParseText';
+
+		$results[] = $this->_parser->update($args);
+
+		foreach ($results as $result)
+		{
+			if (trim($result)) {
+				$return .= $result;
+			}
+		}
+		return $return;
+	}
+
+	/**
+	 * Load the parser
+	 *
+	 * @access	private
+	 * @param	array	Associative array of parser plugin config paramaters
+	 * @param	array	Associative array of parser config paramaters
+	 * @param	bool	Tells initialise() to create new parser or not
+	 * @since	1.5
+	 */
+	private function _loadParser($config=array(), $pconfig=array(), $getnew=false)
+	{
+		// Check if editor is already loaded
+		if (!is_null(($this->_parser))) {
+			return;
+		}
+
+		jimport('joomla.filesystem.file');
+		
+		// Build the path to the needed parser plugin
+		$name = JFilterInput::clean($this->_name, 'cmd');
+		$path = JPATH_SITE.DS.'plugins'.DS.'hubzero'.DS.$name.'.php';
+
+		if (!JFile::exists($path)) {
+			JError::raiseWarning( 500, JText::_('Cannot load the parser') );
+			return false;
+		}
+
+		// Require plugin file
+		require_once $path;
+
+		// Get the plugin
+		$plugin =& JPluginHelper::getPlugin('hubzero', $this->_name);
+		$params = new JParameter($plugin->params);
+		$params->loadArray($config);
+		$plugin->params = $params;
+
+		// Build parser plugin classname
+		$name = 'plgHubzero'.$this->_name;
+		if ($this->_parser = new $name($this, (array)$plugin)) {
+			// Load plugin parameters
+			$this->initialise($pconfig, $getnew);
+		}
+	}
+}
