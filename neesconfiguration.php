16021f335ab0c1c40a02d61f066ddca274d7ac01
Gemez Marshall <gemezm@purdue.edu>
2011-02-16 15:22:54 -0500

    make neesconfiguration.php a link into /etc/nees

--- neesconfiguration.php
+++ neesconfiguration.php
@@ -1,13 +1,1 @@
-<?php
-
-  class NeesConfig{
-  	
-  	const ORACLE_SERVER = "neesdb.neeshub.org/neesprod";
-  	const ORACLE_USERNAME = "central";
-  	const ORACLE_PASSWORD = "bees4nees";
-  	const ORACLE_SCHEMA = "oracle";
-  	
-  	const LAUNCH_INDEED = "/tools/indeed/invoke?list";
-  }
-
-?>
+/etc/nees/neesconfiguration.php
