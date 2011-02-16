aa4642cd9d07de0c9f6eb8e3f9548cd532ea72a2
Jason Lambert <jblamber@purdue.edu>
2011-02-16 13:48:39 -0500

    [NEESHUB][R2][#1903] description toolbar for sup docs

---
+++ site/ckeditor/config.js
@@ -0,0 +1,37 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+		
+	config.toolbar = 'NEESLearningObject';
+
+    config.toolbar_NEESLearningObject =
+    [
+        ['Preview','Maximize'],
+        ['Find','Replace','-','SelectAll','RemoveFormat'],
+        '/',
+        ['Image'],['Table','HorizontalRule','SpecialChar'],
+        ['Styles','Format'],
+        ['Link','Unlink'],        
+         '/',
+         ['Cut','Copy','Paste','PasteText'],
+         ['Bold','Italic','Strike'],
+         ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],		
+        
+    ];		config.toolbar = 'NEESLearningObjectPOWER';    config.toolbar_NEESLearningObjectPOWER =    [        ['Preview','Maximize'],        ['Find','Replace','-','SelectAll','RemoveFormat'],        '/',        ['Image'],['Table','HorizontalRule','SpecialChar'],        ['Styles','Format'],        ['Link','Unlink'],                 '/',         ['Cut','Copy','Paste','PasteText'],         ['Bold','Italic','Strike'],         ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],		            ];
+	
+	config.toolbar = 'NEESAbstract';
+
+    config.toolbar_NEESAbstract =
+    [		['Image'],		
+        ['Cut','Copy','Paste','PasteText'],
+        ['Bold','Italic','Strike'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+    ];		config.toolbar = 'NEESBasicDescription';    config.toolbar_NEESBasicDescription =    [		        ['Cut','Copy','Paste','PasteText'],    ];
+};
