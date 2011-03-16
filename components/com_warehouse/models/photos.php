72fe4abc3417b5e8d218fc119247293bdfcc0fcd
Gemez Marshall <gemezm@purdue.edu>
2011-03-16 10:13:09 -0400

    [NEESHUB][#2082] Changed experiment additional photos query to explicitly exclude specific photo types.  Previously, looked for photos that were not categorized.

--- components/com_warehouse/models/photos.php
+++ components/com_warehouse/models/photos.php
@@ -4,9 +4,10 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
  
 jimport( 'joomla.application.component.model' );
 
-require_once('base.php');
+//require_once('base.php');
+require_once('experiment.php');
 
-class WarehouseModelPhotos extends WarehouseModelBase{
+class WarehouseModelPhotos extends WarehouseModelExperiment{
 	
   /**
    * Constructor
@@ -17,9 +18,11 @@ class WarehouseModelPhotos extends WarehouseModelBase{
 	parent::__construct();
   }
   
+  /*
   public function findDataFileByMimeType($p_iProjectId, $p_iExperimentId, $p_iTrialId=0, $p_iRepetitionId=0, $p_iLowerLimit=0, $p_iUpperLimit = 24){
     return DataFilePeer::findDataFileByMimeType($p_iProjectId, $p_iExperimentId, $p_iTrialId, $p_iRepetitionId, $p_iLowerLimit, $p_iUpperLimit);	
   }
+  */
   
   /*
   public function findDataFileByMimeTypeCount($p_iProjectId, $p_iExperimentId, $p_iTrialId=0, $p_iRepetitionId=0){
