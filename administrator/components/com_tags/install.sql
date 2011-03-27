05a543f46ce85bbc08755fb408d1f67f9a6e8cb8
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2011-03-27 19:04:43 -0400

    update copyrights and license text in remaining hubzero files that had old license

--- administrator/components/com_tags/install.sql
+++ administrator/components/com_tags/install.sql
@@ -1,19 +1,29 @@
 /**
- * Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906.
+ * @package      hubzero-cms
+ * @file         administrator/components/com_tags/install.sql
+ * @copyright    Copyright (c) 2005-2010 Purdue University. All rights reserved.
+ * @license      http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ *
+ * Copyright (c) 2005-2010 Purdue University
  * All rights reserved.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License,
- * version 2 as published by the Free Software Foundation.
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
  *
- * This program is distributed in the hope that it will be useful,
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
  */
 
 CREATE TABLE IF NOT EXISTS `#__tags` (
