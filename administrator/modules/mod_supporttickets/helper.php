6f3671ad49834b99fa7ed9b9b21b6fa42d1fa708
Shawn Rice <zooley@purdue.edu>
2011-11-01 08:59:22 -0400

    New admin module for support tickets

---
+++ administrator/modules/mod_supporttickets/helper.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * @package     hubzero-cms
+ * @author      Shawn Rice <zooley@purdue.edu>
+ * @copyright   Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+class modSupportTickets
+{
+	private $_attributes = array();
+
+	//-----------
+
+	public function __construct($params, $module) 
+	{
+		$this->params = $params;
+		$this->module = $module;
+	}
+
+	//-----------
+
+	public function __set($property, $value)
+	{
+		$this->_attributes[$property] = $value;
+	}
+	
+	//-----------
+	
+	public function __get($property)
+	{
+		if (isset($this->_attributes[$property])) 
+		{
+			return $this->_attributes[$property];
+		}
+	}
+	
+	//-----------
+	
+	public function __isset($property)
+	{
+		return isset($this->_attributes[$property]);
+	}
+
+	//-----------
+
+	public function display()
+	{
+		include_once(JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_support' . DS . 'tables' . DS . 'ticket.php');
+		
+		$juser =& JFactory::getUser();
+		
+		$this->database = JFactory::getDBO();
+		
+		$jconfig =& JFactory::getConfig();
+		$this->offset = $jconfig->getValue('config.offset');
+		
+		$type = JRequest::getVar('type', 'submitted');
+		$this->type  = ($type == 'automatic') ? 1 : 0;
+		
+		$this->group = JRequest::getVar('group', '');
+		
+		$this->year  = JRequest::getInt('year', strftime("%Y", time()+($this->offset*60*60)));
+		
+		$st = new SupportTicket($this->database);
+		
+		$opened = array();
+		$my = array();
+		
+		// Currently open tickets
+		$opened['open'] = $st->getCountOfOpenTickets($this->type, false, $this->group);
+		
+		// Currently unassigned tickets
+		$opened['unassigned'] = $st->getCountOfOpenTickets($this->type, true, $this->group);
+		
+		$filters = array();
+		$filters['search'] = '';
+		$filters['status'] = 'new';
+		$filters['type'] = 0;
+		$filters['owner'] = '';
+		$filters['reportedby'] = '';
+		$filters['severity'] = '';
+		$filters['sort'] = 'created';
+		$filters['sortdir'] = 'DESC';
+		
+		$opened['new'] = $st->getTicketsCount($filters, true);
+		
+		$filters['status'] = 'open';
+		$filters['reportedby'] = $juser->get('username');
+		$my['open'] = $st->getTicketsCount($filters, true);
+		
+		$filters['reportedby'] = '';
+		$filters['status'] = 'open';
+		$filters['owner'] = $juser->get('username');
+		$my['assigned'] = $st->getTicketsCount($filters, true);
+		
+		$this->opened = $opened;
+		$this->my = $my;
+		
+		// Get avgerage lifetime
+		$this->lifetime = $st->getAverageLifeOfTicket($this->type, $this->year, $this->group);
+		
+		//ximport('Hubzero_Document');
+		//Hubzero_Document::addModuleStyleSheet($this->module->module);
+		
+		$document =& JFactory::getDocument();
+		$document->addStyleSheet('/administrator/modules/' . $this->module->module . '/' . substr($this->module->module, 4). '.css');
+		
+		// Get the view
+		require(JModuleHelper::getLayoutPath($this->module->module));
+	}
+}
---
+++ administrator/modules/mod_supporttickets/tmpl/default.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @package     hubzero-cms
+ * @author      Shawn Rice <zooley@purdue.edu>
+ * @copyright   Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+?>
+<table class="support-stats-overview open-tickets" summary="<?php echo JText::_('Overview of open support tickets'); ?>">
+	<thead>
+		<tr>
+			<th scope="col"><?php echo JText::_('Open'); ?></th>
+			<th scope="col"><?php echo JText::_('Unassigned'); ?></th>
+			<th scope="col"><?php echo JText::_('New'); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="major"><a href="index.php?option=com_support&amp;c=tickets&amp;show=status:open" title="<?php echo JText::_('View open tickets'); ?>"><?php echo $this->opened['open']; ?></a></td>
+			<td class="critical"><a href="index.php?option=com_support&amp;c=tickets&amp;show=owner:none" title="<?php echo JText::_('View unassigned tickets'); ?>"><?php echo $this->opened['unassigned']; ?></a></td>
+			<td class="newt"><a href="index.php?option=com_support&amp;c=tickets&amp;show=status:new" title="<?php echo JText::_('View new tickets'); ?>"><?php echo $this->opened['new']; ?></a></td>
+		</tr>
+	</tbody>
+</table>
+
+<table class="support-stats-overview closed-tickets" summary="<?php echo JText::_('Overview of closed support tickets'); ?>">
+	<thead>
+		<tr>
+			<th scope="col" class="block"><?php echo JText::_('Average lifetime'); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="block">
+				<?php echo (isset($this->lifetime[0])) ? $this->lifetime[0] : 0; ?> <span><?php echo JText::_('days'); ?></span> 
+				<?php echo (isset($this->lifetime[1])) ? $this->lifetime[1] : 0; ?> <span><?php echo JText::_('hours'); ?></span> 
+				<?php echo (isset($this->lifetime[2])) ? $this->lifetime[2] : 0; ?> <span><?php echo JText::_('minutes'); ?></span>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<table class="support-stats-overview my-tickets" summary="<?php echo JText::_('Overview of my support tickets'); ?>">
+	<thead>
+		<tr>
+			<th scope="col"><?php echo JText::_('My Tickets (reported)'); ?></th>
+			<th scope="col"><?php echo JText::_('My Tickets (assigned)'); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><a href="index.php?option=com_support&amp;c=tickets&amp;show=reportedby:me" title="<?php echo JText::_('View my reported tickets'); ?>"><?php echo $this->my['open']; ?></a></td>
+			<td><a href="index.php?option=com_support&amp;c=tickets&amp;show=status:open%20owner:me" title="<?php echo JText::_('View my assigned tickets'); ?>"><?php echo $this->my['assigned']; ?></a></td>
+		</tr>
+	</tbody>
+</table>
