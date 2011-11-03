83419c409111c83d5abb81e88c2a77a4efb81e7e
Steve Snyder <snyder13@purdue.edu>
2011-11-03 14:43:53 -0400

    fix handling of quotes in variable

--- dbshell.php
+++ dbshell.php
@@ -7,7 +7,7 @@ conf="`dirname $0`/configuration.php"
 get_var () { grep "var\ \$$1 " $conf | awk '{ print $4 }' | sed "s/^'\|[';]\+$//g"; }
 
 mkfifo -m 600 $HOME/.my.cred
-(echo "[client]"; echo -n "password="; get_var "password") > $HOME/.my.cred &
+(echo "[client]"; echo -n "password=\""; get_var "password" | sed "s/$/\"/"; ) > $HOME/.my.cred &
 mysql --defaults-extra-file="$HOME/.my.cred" -u `get_var 'user'` `get_var 'db'`
 rm $HOME/.my.cred
 
