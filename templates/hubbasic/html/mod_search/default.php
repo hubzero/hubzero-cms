5128872f847fd85b7c63b00586d8d97bcdaf8457
Sam Wilson <samwilson@purdue.edu>
2011-12-13 12:04:01 -0500

    Add submit button to hubbasic template mod_search override

--- templates/hubbasic/html/mod_search/default.php
+++ templates/hubbasic/html/mod_search/default.php
@@ -5,5 +5,6 @@ defined('_JEXEC') or die('Restricted access');?>
 		<legend><?php echo $text; ?></legend>
 		<label for="searchword" id="searchword-label"><?php echo $text; ?></label>
 		<input type="text" name="terms" id="searchword" size="<?php echo $width; ?>" value="<?php echo $text; ?>" />
+		<?php if ($params->get('button')) { echo '<input type="submit" name="submitquery" id="submitquery" value="' . $params->get('button_text') . '" />'; } ?>
 	</fieldset>
 </form>
