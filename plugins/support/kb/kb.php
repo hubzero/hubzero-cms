a7bb0d0b6bf24aae4b1ca0e4b9e0bf1633f2599b
Shawn Rice <zooley@purdue.edu>
2012-02-07 10:27:14 -0500

    Added KB plugin to support for report abuse functionality

---
+++ plugins/support/kb/kb.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Alissa Nedossekina <alisa@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+jimport( 'joomla.plugin.plugin' );
+
+/**
+ * Plugin for abuse reports on KB comments
+ */
+class plgSupportKb extends JPlugin
+{
+
+	/**
+	 * Constructor
+	 * 
+	 * @param      unknown &$subject Parameter description (if any) ...
+	 * @param      unknown $config Parameter description (if any) ...
+	 * @return     void
+	 */
+	public function __construct(&$subject, $config)
+	{
+		parent::__construct($subject, $config);
+
+		// load plugin parameters
+		$this->_plugin = JPluginHelper::getPlugin( 'support', 'kb' );
+		if (version_compare(JVERSION, '1.6', 'lt'))
+		{
+			$this->params = new JParameter($this->_plugin->params);
+		}
+	}
+
+	/**
+	 * Short description for 'getReportedItem'
+	 * 
+	 * Long description (if any) ...
+	 * 
+	 * @param      string $refid Parameter description (if any) ...
+	 * @param      string $category Parameter description (if any) ...
+	 * @param      string $parent Parameter description (if any) ...
+	 * @return     array Return description (if any) ...
+	 */
+	public function getReportedItem($refid, $category, $parent)
+	{
+		if ($category != 'kb') 
+		{
+			return null;
+		}
+
+		$query  = "SELECT rc.id, rc.content as text, rc.created_by as author, NULL as subject, rc.anonymous as anon, 'kb' AS parent_category, 
+					s.alias AS section, c.alias AS category, f.alias AS article 
+					FROM #__faq_comments AS rc
+					LEFT JOIN #__faq AS f 
+						ON f.id = rc.entry_id
+					LEFT JOIN #__faq_categories AS s 
+						ON s.id = f.section
+					LEFT JOIN #__faq_categories AS c
+						ON c.id = f.category
+					WHERE rc.id=".$refid;
+
+		$database =& JFactory::getDBO();
+		$database->setQuery($query);
+		$rows = $database->loadObjectList();
+		if ($rows) 
+		{
+			foreach ($rows as $key => $row)
+			{
+				$rows[$key]->href = JRoute::_('index.php?option=com_kb&section='.$row->section.'&category='.$row->category.'&alias='.$row->article);
+			}
+		}
+		return $rows;
+	}
+}
