87034c112ef40b4e1acc6dc29f2c8fb466688e33
Shawn Rice <zooley@purdue.edu>
2012-03-26 08:54:56 -0400

    Fixed issue where confirming delete wouldn't follow thorugh

--- plugins/groups/forum/forum.js
+++ plugins/groups/forum/forum.js
@@ -22,7 +22,7 @@ HUB.Plugins.GroupsForum = {
 	initialize: function() {
 		$$('a.delete').each(function(el) {
 			el.addEvent('click', function(e) {
-				new Event(e).stop();
+				//new Event(e).stop();
 
 				return confirm('Are you sure you wish to delete this item?');
 			});
