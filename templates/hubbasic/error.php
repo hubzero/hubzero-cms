b610fb1f10c78abf9b4a67b9cfb5006174bec3a2
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2012-04-13 12:02:00 -0400

    Changes for jquery and HTML5

--- templates/hubbasic/error.php
+++ templates/hubbasic/error.php
@@ -1,11 +1,39 @@
 <?php
-defined( '_JEXEC' ) or die( 'Restricted access' );
- 
-ximport('Hubzero_Module_Helper');
- 
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Alissa Nedossekina <alisa@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
 $config =& JFactory::getConfig();
 $juser =& JFactory::getUser();
- 
+
 $this->setTitle($config->getValue('config.sitename') . ' - ' . $this->getTitle());
 ?>
 <!DOCTYPE html>
@@ -14,59 +42,58 @@ $this->setTitle($config->getValue('config.sitename') . ' - ' . $this->getTitle()
 <!--[if IE 8 ]>    <html dir="<?php echo  $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="ie8"> <![endif]-->
 <!--[if IE 9 ]>    <html dir="<?php echo  $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html dir="<?php echo $this->direction; ?>" lang="<?php echo  $this->language; ?>"> <!--<![endif]-->
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
-	<link rel="stylesheet" href="/templates/hubbasic/css/main.css" type="text/css" />
-	<link rel="stylesheet" href="/templates/hubbasic/css/error.css" type="text/css" />
-	<link rel="stylesheet" href="/templates/hubbasic/html/mod_reportproblems/mod_reportproblems.css" type="text/css" />
-	<link rel="stylesheet" type="text/css" media="print" href="<?php echo $this->baseurl; ?>/templates/hubbasic/css/print.css" />
-	<?php if (JPluginHelper::isEnabled('system', 'jquery')) { ?>
-	<script type="text/javascript" src="/media/system/js/jquery.js"></script>
-	<script type="text/javascript" src="/media/system/js/jquery.ui.js"></script>
-	<script type="text/javascript" src="/media/system/js/jquery.fancybox.js"></script>
-	<script type="text/javascript" src="/media/system/js/jquery.tools.js"></script>
-	<script type="text/javascript" src="/templates/hubbasic/js/hub.jquery.js"></script>
-	<script type="text/javascript" src="/modules/mod_reportproblems/mod_reportproblems.jquery.js"></script>
-	<?php } else { ?>
-	<script type="text/javascript" src="/media/system/js/mootools.js"></script>
-	<script type="text/javascript" src="/templates/hubbasic/js/hub.js"></script>
-	<script type="text/javascript" src="/modules/mod_reportproblems/mod_reportproblems.js"></script>
-	<?php } ?>
-
-	<!--[if IE 8]>
-	        <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl; ?>/templates/hubbasic/css/ie8win.css" />
-	<![endif]-->
-	<!--[if IE 7]>
-	        <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl; ?>/templates/hubbasic/css/ie7win.css" />
-	<![endif]-->
-</head>
-<body>
-<?php Hubzero_Module_Helper::displayModules('notices'); ?>
-	<div id="top">
-		<a name="top"></a>
-		<p class="skip" id="to-content"><a href="#content">Skip to content</a></p>
-		<p id="tab">
-			<a href="/support/" title="Need help? Send a trouble report to our support team.">
-				<span>Need Help?</span>
-			</a>
-		</p>
-		<div class="clear"></div>
-	</div><!-- / #top -->
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	
-	<?php Hubzero_Module_Helper::displayModules('helppane'); ?>
-	
-	<div id="header">
-		<div id="header-wrap">
-			<a name="header"></a>
-			<h1>
-				<a href="." title="<?php echo $config->getValue('config.sitename'); ?>">
-					<?php echo $config->getValue('config.sitename'); ?> 
-					<span id="tagline">A HUBzero site</span>
+		<link rel="stylesheet" href="/templates/hubbasic/css/main.css" type="text/css" />
+		<link rel="stylesheet" href="/templates/hubbasic/css/error.css" type="text/css" />
+		<link rel="stylesheet" href="/templates/hubbasic/html/mod_reportproblems/mod_reportproblems.css" type="text/css" />
+		<link rel="stylesheet" type="text/css" media="print" href="<?php echo $this->baseurl ?>/templates/hubbasic/css/print.css" />
+<?php if (JPluginHelper::isEnabled('system', 'jquery')) { ?>
+		<script type="text/javascript" src="/media/system/js/jquery.js"></script>
+		<script type="text/javascript" src="/media/system/js/jquery.ui.js"></script>
+		<script type="text/javascript" src="/media/system/js/jquery.fancybox.js"></script>
+		<script type="text/javascript" src="/media/system/js/jquery.tools.js"></script>
+		<script type="text/javascript" src="/templates/hubbasic/js/hub.jquery.js"></script>
+		<script type="text/javascript" src="/modules/mod_reportproblems/mod_reportproblems.jquery.js"></script>
+<?php } else { ?>
+		<script type="text/javascript" src="/media/system/js/mootools.js"></script>
+		<script type="text/javascript" src="/templates/hubbasic/js/hub.js"></script>
+		<script type="text/javascript" src="/modules/mod_reportproblems/mod_reportproblems.js"></script>
+<?php } ?>
+		<!--[if IE 8]>
+			<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl ?>/templates/hubbasic/css/ie8.css" />
+		<![endif]-->
+		<!--[if lte IE 7]>
+			<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl ?>/templates/hubbasic/css/ie7.css" />
+		<![endif]-->
+	</head>
+	<body>
+		<?php Hubzero_Module_Helper::displayModules('notices'); ?>
+		<div id="top">
+			<a name="top"></a>
+			<p class="skip" id="to-content"><a href="#content">Skip to content</a></p>
+			<p id="tab">
+				<a href="/support/" title="Need help? Send a trouble report to our support team.">
+					<span>Need Help?</span>
 				</a>
-			</h1>
+			</p>
+			<div class="clear"></div>
+		</div><!-- / #top -->
+	
+		<?php Hubzero_Module_Helper::displayModules('helppane'); ?>
+	
+		<div id="header">
+			<div id="header-wrap">
+				<a name="header"></a>
+				<h1>
+					<a href="." title="<?php echo $config->getValue('config.sitename'); ?>">
+						<?php echo $config->getValue('config.sitename'); ?> 
+						<span id="tagline">A HUBzero site</span>
+					</a>
+				</h1>
 		
-			<ul id="toolbar" class="<?php if (!$juser->get('guest')) { echo 'loggedin'; } else { echo 'loggedout'; } ?>">
+				<ul id="toolbar" class="<?php if (!$juser->get('guest')) { echo 'loggedin'; } else { echo 'loggedout'; } ?>">
 <?php
 if (!$juser->get('guest')) {
 	// Find the user's most recent support tickets
@@ -84,21 +111,21 @@ if (!$juser->get('guest')) {
 	echo "\t\t\t\t".'<li id="register"><a href="/register" title="Sign up for a free account">Register</a></li>'."\n";
 }
 ?>
-			</ul>
+				</ul>
 		
-			<?php Hubzero_Module_Helper::displayModules('search'); ?>
-		</div><!-- / #header-wrap -->
-	</div><!-- / #header -->
+				<?php Hubzero_Module_Helper::displayModules('search'); ?>
+			</div><!-- / #header-wrap -->
+		</div><!-- / #header -->
 	
-	<div id="nav">
-		<a name="nav"></a>
-		<h2>Navigation</h2>
-		<?php Hubzero_Module_Helper::displayModules('user3'); ?>
-		<div class="clear"></div>
-	</div><!-- / #nav -->
+		<div id="nav">
+			<a name="nav"></a>
+			<h2>Navigation</h2>
+			<?php Hubzero_Module_Helper::displayModules('user3'); ?>
+			<div class="clear"></div>
+		</div><!-- / #nav -->
 
-	<div id="trail">
-		You are here: <?php
+		<div id="trail">
+			You are here: <?php
 	$app =& JFactory::getApplication();
 	$pathway =& $app->getPathway();
 
@@ -118,48 +145,48 @@ if (!$juser->get('guest')) {
 		$l[] = '<a href="'.$url.'">'.$text.'</a>';
 	}
 	echo implode(' &rsaquo; ',$l);
-	?>
-	</div><!-- / #trail -->
+?>
+		</div><!-- / #trail -->
 
-	<div id="wrap">
-		<div id="content" class="<?php echo $option; ?>">
-			<div id="content-wrap">
-			<a name="content"></a>
+		<div id="wrap">
+			<div id="content" class="<?php echo $option; ?>">
+				<div id="content-wrap">
+					<a name="content"></a>
 
-			<div id="outline">
-				<div id="errorbox" class="code-<?php echo $this->error->code ?>">
-					<h2><?php echo $this->error->message ?></h2>
+					<div id="outline">
+						<div id="errorbox" class="code-<?php echo $this->error->code ?>">
+							<h2><?php echo $this->error->message ?></h2>
 
-					<p><?php echo JText::_('You may not be able to visit this page because of:'); ?></p>
+							<p><?php echo JText::_('You may not be able to visit this page because of:'); ?></p>
 
-					<ol>
+							<ol>
 <?php if ($this->error->code != 403) { ?>
-						<li><?php echo JText::_('An out-of-date bookmark/favourite.'); ?></li>
-						<li><?php echo JText::_('A search engine that has an out-of-date listing for this site.'); ?></li>
-						<li><?php echo JText::_('A mis-typed address.'); ?></li>
-						<li><?php echo JText::_('The requested resource was not found.'); ?></li>
+								<li><?php echo JText::_('An out-of-date bookmark/favourite.'); ?></li>
+								<li><?php echo JText::_('A search engine that has an out-of-date listing for this site.'); ?></li>
+								<li><?php echo JText::_('A mis-typed address.'); ?></li>
+								<li><?php echo JText::_('The requested resource was not found.'); ?></li>
 <?php } ?>
-						<li><?php echo JText::_('This page may belong to a group with restricted access.  Only members of the group can view the contents.'); ?></li>
-						<li><?php echo JText::_('An error has occurred while processing your request.'); ?></li>
-					</ol>
+								<li><?php echo JText::_('This page may belong to a group with restricted access.  Only members of the group can view the contents.'); ?></li>
+								<li><?php echo JText::_('An error has occurred while processing your request.'); ?></li>
+							</ol>
 <?php if ($this->error->code != 403) { ?>
-					<p><?php echo JText::_('If difficulties persist, please contact the system administrator of this site.'); ?></p>
+							<p><?php echo JText::_('If difficulties persist, please contact the system administrator of this site.'); ?></p>
 <?php } else { ?>
-					<p><?php echo JText::_('If difficulties persist and you feel that you should have access to the page, please file a trouble report by clicking on the Help! option on the menu above.'); ?></p>
+							<p><?php echo JText::_('If difficulties persist and you feel that you should have access to the page, please file a trouble report by clicking on the Help! option on the menu above.'); ?></p>
 <?php } ?>
-				</div>
+						</div><!-- / #errorbox -->
 
-				<form method="get" action="/search">
-					<fieldset>
-						<?php echo JText::_('Please try the'); ?> <a href="/index.php" title="<?php echo JText::_('Go to the home page'); ?>"><?php echo JText::_('Home Page'); ?></a> <span><?php echo JText::_('or'); ?></span> 
-						<label>
-							<?php echo JText::_('Search:'); ?> 
-							<input type="text" name="searchword" value="" />
-						</label>
-						<input type="submit" value="<?php echo JText::_('Go'); ?>" />
-					</fieldset>
-				</form>
-			</div>
+						<form method="get" action="/search">
+							<fieldset>
+								<?php echo JText::_('Please try the'); ?> <a href="/index.php" title="<?php echo JText::_('Go to the home page'); ?>"><?php echo JText::_('Home Page'); ?></a> <span><?php echo JText::_('or'); ?></span> 
+								<label>
+									<?php echo JText::_('Search:'); ?> 
+									<input type="text" name="searchword" value="" />
+								</label>
+								<input type="submit" value="<?php echo JText::_('Go'); ?>" />
+							</fieldset>
+						</form>
+					</div><!-- / #outline -->
 <?php 
 			if ($this->debug || $juser->get('username') == 'nkissebe' || $juser->get('username') == 'zooley') :
 				echo "\t\t".'<div id="techinfo">'."\n";
@@ -168,19 +195,15 @@ if (!$juser->get('guest')) {
 			endif;
 ?>
 
-			</div><!-- / #content-wrap -->
-		</div><!-- / #content -->
-	</div><!-- / #wrap -->
+				</div><!-- / #content-wrap -->
+			</div><!-- / #content -->
+		</div><!-- / #wrap -->
 	
-	<div id="footer">
-		<a name="footer"></a>
-		<!-- Start footer modules output -->
-		<?php Hubzero_Module_Helper::displayModules('footer'); ?>
-		<!-- End footer modules output -->
-	</div><!-- / #footer -->
- </body>
+		<div id="footer">
+			<a name="footer"></a>
+			<!-- Start footer modules output -->
+			<?php Hubzero_Module_Helper::displayModules('footer'); ?>
+			<!-- End footer modules output -->
+		</div><!-- / #footer -->
+	</body>
 </html>
-<?php
-$title = $this->getTitle();
-$this->setTitle( $config->getValue('config.sitename').' - '.$title );
-?>
--- templates/hubbasic/index.php
+++ templates/hubbasic/index.php
@@ -1,19 +1,49 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
- 
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
 $config =& JFactory::getConfig();
 $juser =& JFactory::getUser();
- 
+
 //do we want to include jQuery
-if (JPluginHelper::isEnabled('system', 'jquery'))
+if (JPluginHelper::isEnabled('system', 'jquery')) 
 {
-        $this->addScript($this->baseurl . '/templates/' . $this->template . '/js/hub.jquery.js');
-}
-else
+	$this->addScript($this->baseurl . '/templates/' . $this->template . '/js/hub.jquery.js');
+} 
+else 
 {
-        $this->addScript($this->baseurl . '/templates/' . $this->template . '/js/hub.js');
+	$this->addScript($this->baseurl . '/templates/' . $this->template . '/js/hub.js');
 }
- 
+
 $this->setTitle($config->getValue('config.sitename') . ' - ' . $this->getTitle());
 ?>
 <!DOCTYPE html>
@@ -22,48 +52,47 @@ $this->setTitle($config->getValue('config.sitename') . ' - ' . $this->getTitle()
 <!--[if IE 8 ]>    <html dir="<?php echo  $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="ie8"> <![endif]-->
 <!--[if IE 9 ]>    <html dir="<?php echo  $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html dir="<?php echo $this->direction; ?>" lang="<?php echo  $this->language; ?>"> <!--<![endif]-->
-<head>
-	<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" media="print" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/print.css" />
-
-	<jdoc:include type="head" />
-
-	<!--[if IE 8]>
-	        <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/ie8win.css" />
-	<![endif]-->
-	<!--[if IE 7]>
-	        <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/ie7win.css" />
-	<![endif]-->
-</head>
- 
-<body <?php if ($this->countModules( 'banner or welcome' )) { echo "class=\"frontpage\""; } ?>>
-<jdoc:include type="modules" name="notices" />
-	<div id="top">
-		<a name="top"></a>
-		<p class="skip" id="to-content"><a href="#content">Skip to content</a></p>
-<?php if ($this->countModules( 'helppane' )) : ?>
-		<p id="tab">
-			<a href="/support/" title="Need help? Send a trouble report to our support team.">
-				<span>Need Help?</span>
-			</a>
-		</p>
-<?php endif; ?>
-		<div class="clear"></div>
-	</div><!-- / #top -->
-	
-	<jdoc:include type="modules" name="helppane" />
-	
-	<div id="header">
-		<div id="header-wrap">
-			<a name="header"></a>
-			<h1>
-				<a href="<?php echo $this->baseurl ?>" title="<?php echo $config->getValue('config.sitename'); ?>">
-					<?php echo $config->getValue('config.sitename'); ?> 
-					<span id="tagline">powered by HUBzero&reg;</span>
-				</a>
-			</h1>
+	<head>
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/main.css" />
+		<link rel="stylesheet" type="text/css" media="print" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/print.css" />
 		
-			<ul id="toolbar" class="<?php if (!$juser->get('guest')) { echo 'loggedin'; } else { echo 'loggedout'; } ?>">
+		<jdoc:include type="head" />
+		
+		<!--[if IE 8]>
+			<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/ie8win.css" />
+		<![endif]-->
+		<!--[if IE 7]>
+			<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/ie7win.css" />
+		<![endif]-->
+	</head>
+	<body <?php if ($this->countModules( 'banner or welcome' )) { echo "class=\"frontpage\""; } ?>>
+		<jdoc:include type="modules" name="notices" />
+		<div id="top">
+			<a name="top"></a>
+			<p class="skip" id="to-content"><a href="#content">Skip to content</a></p>
+<?php if ($this->countModules( 'helppane' )) : ?>
+			<p id="tab">
+				<a href="/support/" title="Need help? Send a trouble report to our support team.">
+					<span>Need Help?</span>
+				</a>
+			</p>
+<?php endif; ?>
+			<div class="clear"></div>
+		</div><!-- / #top -->
+	
+		<jdoc:include type="modules" name="helppane" />
+	
+		<div id="header">
+			<div id="header-wrap">
+				<a name="header"></a>
+				<h1>
+					<a href="<?php echo $this->baseurl ?>" title="<?php echo $config->getValue('config.sitename'); ?>">
+						<?php echo $config->getValue('config.sitename'); ?> 
+						<span id="tagline">powered by HUBzero&reg;</span>
+					</a>
+				</h1>
+		
+				<ul id="toolbar" class="<?php if (!$juser->get('guest')) { echo 'loggedin'; } else { echo 'loggedout'; } ?>">
 <?php
 if (!$juser->get('guest')) {
 	// Find the user's most recent support tickets
@@ -81,38 +110,38 @@ if (!$juser->get('guest')) {
 	echo "\t\t\t\t".'<li id="register"><a href="/register" title="Sign up for a free account">Register</a></li>'."\n";
 }
 ?>
-			</ul>
+				</ul>
 		
-			<jdoc:include type="modules" name="search" />
-		</div><!-- / #header-wrap -->
-	</div><!-- / #header -->
+				<jdoc:include type="modules" name="search" />
+			</div><!-- / #header-wrap -->
+		</div><!-- / #header -->
 	
-	<div id="nav">
-		<a name="nav"></a>
-		<h2>Navigation</h2>
-		<jdoc:include type="modules" name="user3" />
-		<div class="clear"></div>
-	</div><!-- / #nav -->
+		<div id="nav">
+			<a name="nav"></a>
+			<h2>Navigation</h2>
+			<jdoc:include type="modules" name="user3" />
+			<div class="clear"></div>
+		</div><!-- / #nav -->
 
 <?php if ($this->countModules( 'banner or welcome' ) && $option == 'com_content') : ?>
-	<div id="home-splash">
-		<div id="features">
+		<div id="home-splash">
+			<div id="features">
 <?php if ($this->countModules( 'banner' )) : ?>
-			<jdoc:include type="modules" name="banner" />
+				<jdoc:include type="modules" name="banner" />
 <?php else : ?>
 <?php endif; ?>
-		</div><!-- / #features -->
+			</div><!-- / #features -->
 <?php if ($this->countModules( 'welcome' )) : ?>
-		<div id="welcome">
-			<jdoc:include type="modules" name="welcome" />
-		</div><!-- / #welcome -->
+			<div id="welcome">
+				<jdoc:include type="modules" name="welcome" />
+			</div><!-- / #welcome -->
 <?php endif; ?>
-	</div><!-- / #home-splash -->
+		</div><!-- / #home-splash -->
 <?php endif; ?>
 
 <?php if (!$this->countModules( 'banner or welcome' )) : ?>
-	<div id="trail">
-		You are here: <?php
+		<div id="trail">
+			You are here: <?php
 	$app =& JFactory::getApplication();
 	$pathway =& $app->getPathway();
 
@@ -132,52 +161,48 @@ if (!$juser->get('guest')) {
 		$l[] = '<a href="'.$url.'">'.$text.'</a>';
 	}
 	echo implode(' &rsaquo; ',$l);
-	?>
-	</div><!-- / #trail -->
+?>
+		</div><!-- / #trail -->
 <?php endif; ?>
 
-	<div id="wrap">
-		<div id="content" class="<?php echo $option; ?>">
-			<div id="content-wrap">
-				<?php if ($this->getBuffer('message')) : ?>
-					<jdoc:include type="message" />
-				<?php endif; ?>
-			<a name="content"></a>
+		<div id="wrap">
+			<div id="content" class="<?php echo $option; ?>">
+				<div id="content-wrap">
+					<?php if ($this->getBuffer('message')) : ?>
+						<jdoc:include type="message" />
+					<?php endif; ?>
+					<a name="content"></a>
 <?php if ($this->countModules( 'left' )) : ?>
-				<div class="main section withleft">
-					<div class="aside">
-						<jdoc:include type="modules" name="left" />
-					</div><!-- / .aside -->
-					<div class="subject">
+					<div class="main section withleft">
+						<div class="aside">
+							<jdoc:include type="modules" name="left" />
+						</div><!-- / .aside -->
+						<div class="subject">
 <?php endif; ?>
 <?php if ($this->countModules('right')) : ?>
-				<div class="main section">
-					<div class="aside">
-						<jdoc:include type="modules" name="right" />
-					</div><!-- / .aside -->
-					<div class="subject">
+					<div class="main section">
+						<div class="aside">
+							<jdoc:include type="modules" name="right" />
+						</div><!-- / .aside -->
+						<div class="subject">
 <?php endif; ?>
-				<!-- Start component output -->
-				<jdoc:include type="component" />
-				<!-- End component output -->
+					<!-- Start component output -->
+					<jdoc:include type="component" />
+					<!-- End component output -->
 <?php if ($this->countModules('left or right')) : ?>
-					</div><!-- / .subject -->
-					<div class="clear"></div>
-				</div><!-- / .main section -->
+						</div><!-- / .subject -->
+						<div class="clear"></div>
+					</div><!-- / .main section -->
 <?php endif; ?>
-			</div><!-- / #content-wrap -->
-		</div><!-- / #content -->
-	</div><!-- / #wrap -->
+				</div><!-- / #content-wrap -->
+			</div><!-- / #content -->
+		</div><!-- / #wrap -->
 	
-	<div id="footer">
-		<a name="footer"></a>
-		<!-- Start footer modules output -->
-		<jdoc:include type="modules" name="footer" />
-		<!-- End footer modules output -->
-	</div><!-- / #footer -->
- </body>
+		<div id="footer">
+			<a name="footer"></a>
+			<!-- Start footer modules output -->
+			<jdoc:include type="modules" name="footer" />
+			<!-- End footer modules output -->
+		</div><!-- / #footer -->
+	</body>
 </html>
-<?php
-$title = $this->getTitle();
-$this->setTitle( $config->getValue('config.sitename').' - '.$title );
-?>
