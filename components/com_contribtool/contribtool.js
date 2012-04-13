0343acdf106054725222909075c14e5a2a62ec6b
Shawn Rice <zooley@purdue.edu>
2012-04-13 10:32:32 -0400

    Fixed issue with trying to use HUB namespace before creation

--- components/com_contribtool/contribtool.js
+++ components/com_contribtool/contribtool.js
@@ -4,6 +4,12 @@
  * @copyright   Copyright 2005-2011 Purdue University. All rights reserved.
  * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
+//-----------------------------------------------------------
+//  Ensure we have our namespace
+//-----------------------------------------------------------
+if (!HUB) {
+	var HUB = {};
+}
 
 //----------------------------------------------------------
 // Contribtool admin actions form
