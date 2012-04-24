b3012206872d52247692981841809c338e7656ca
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2012-04-24 11:41:21 -0400

    missed close php section in earlier patch merge

--- components/com_contribute/views/steps/tmpl/authorslist.php
+++ components/com_contribute/views/steps/tmpl/authorslist.php
@@ -30,7 +30,7 @@
 
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die( 'Restricted access' );
-
+?>
 <div id="small-page">
 <?php if ($this->getError()) { ?>
 		<p class="error"><?php echo $this->getError(); ?></p>
