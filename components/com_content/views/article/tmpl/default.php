1e479239e5ae7ae3238c38b7346b90b8495564cf
Shawn Rice <zooley@purdue.edu>
2012-05-02 14:55:03 -0400

    Removed tables and disabled edit link

--- components/com_content/views/article/tmpl/default.php
+++ components/com_content/views/article/tmpl/default.php
@@ -1,72 +1,76 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-$canEdit	= ($this->user->authorize('com_content', 'edit', 'content', 'all') || $this->user->authorize('com_content', 'edit', 'content', 'own'));
+$canEdit = ($this->user->authorize('com_content', 'edit', 'content', 'all') || $this->user->authorize('com_content', 'edit', 'content', 'own'));
+
+$app =& JFactory::getApplication();
+$pathway =& $app->getPathway();
+if (count($pathway->getPathWay()) <= 0) {
+	//$pathway->addItem($this->escape($this->article->title),$this->article->readmore_link);
+	$pathway->addItem($this->escape($this->article->title),$_SERVER['REQUEST_URI']);
+}
 ?>
-<?php if ($this->params->get('show_page_title', 1) && $this->params->get('page_title') != $this->article->title) : ?>
-	<div class="componentheading<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-		<?php echo $this->escape($this->params->get('page_title')); ?>
-	</div>
-<?php endif; ?>
 <?php if ($canEdit || $this->params->get('show_title') || $this->params->get('show_pdf_icon') || $this->params->get('show_print_icon') || $this->params->get('show_email_icon')) : ?>
-<table class="contentpaneopen<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-<tr>
+<div class="contentpaneopen<?php echo $this->params->get( 'pageclass_sfx' ); ?> heading">
 	<?php if ($this->params->get('show_title')) : ?>
-	<td class="contentheading<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>" width="100%">
+	<div class="content-header">
+	<h2 class="contentheading<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
 		<?php if ($this->params->get('link_titles') && $this->article->readmore_link != '') : ?>
-		<a href="<?php echo $this->article->readmore_link; ?>" class="contentpagetitle<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-			<?php echo $this->escape($this->article->title); ?></a>
+		<a href="<?php echo $this->article->readmore_link; ?>" class="contentpagetitle<?php echo $this->params->get( 'pageclass_sfx' ); ?>"><?php echo $this->escape($this->article->title); ?></a>
 		<?php else : ?>
 			<?php echo $this->escape($this->article->title); ?>
 		<?php endif; ?>
-	</td>
+	</h2>
+	</div>
 	<?php endif; ?>
 	<?php if (!$this->print) : ?>
+<?php if ($this->params->get('show_pdf_icon') || $this->params->get( 'show_print_icon' ) || $this->params->get('show_email_icon')) : ?>
+		<p class="buttonheadings">
 		<?php if ($this->params->get('show_pdf_icon')) : ?>
-		<td align="right" width="100%" class="buttonheading">
+		<span class="buttonheading">
 		<?php echo JHTML::_('icon.pdf',  $this->article, $this->params, $this->access); ?>
-		</td>
+		</span>
 		<?php endif; ?>
 
 		<?php if ( $this->params->get( 'show_print_icon' )) : ?>
-		<td align="right" width="100%" class="buttonheading">
+		<span class="buttonheading">
 		<?php echo JHTML::_('icon.print_popup',  $this->article, $this->params, $this->access); ?>
-		</td>
+		</span>
 		<?php endif; ?>
 
 		<?php if ($this->params->get('show_email_icon')) : ?>
-		<td align="right" width="100%" class="buttonheading">
+		<span class="buttonheading">
 		<?php echo JHTML::_('icon.email',  $this->article, $this->params, $this->access); ?>
-		</td>
+		</span>
 		<?php endif; ?>
-		<?php if ($canEdit) : ?>
-		<td align="right" width="100%" class="buttonheading">
+		<?php /*if ($canEdit) : ?>
+		<span class="buttonheading">
 			<?php echo JHTML::_('icon.edit', $this->article, $this->params, $this->access); ?>
-		</td>
-		<?php endif; ?>
+		</span>
+		<?php endif;*/ ?>
+		</p>
+<?php endif; ?>
 	<?php else : ?>
-		<td align="right" width="100%" class="buttonheading">
+		<p class="buttonheadings">
 		<?php echo JHTML::_('icon.print_screen',  $this->article, $this->params, $this->access); ?>
-		</td>
+		</p>
 	<?php endif; ?>
-</tr>
-</table>
+</div>
 <?php endif; ?>
 
 <?php  if (!$this->params->get('show_intro')) :
 	echo $this->article->event->afterDisplayTitle;
 endif; ?>
 <?php echo $this->article->event->beforeDisplayContent; ?>
-<table class="contentpaneopen<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
+<div class="contentpaneopen<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
 <?php if (($this->params->get('show_section') && $this->article->sectionid) || ($this->params->get('show_category') && $this->article->catid)) : ?>
-<tr>
-	<td>
+	<p>
 		<?php if ($this->params->get('show_section') && $this->article->sectionid && isset($this->article->section)) : ?>
 		<span>
 			<?php if ($this->params->get('link_section')) : ?>
 				<?php echo '<a href="'.JRoute::_(ContentHelperRoute::getSectionRoute($this->article->sectionid)).'">'; ?>
 			<?php endif; ?>
-			<?php echo $this->escape($this->article->section); ?>
+			<?php echo $this->article->section; ?>
 			<?php if ($this->params->get('link_section')) : ?>
 				<?php echo '</a>'; ?>
 			<?php endif; ?>
@@ -80,59 +84,44 @@ endif; ?>
 			<?php if ($this->params->get('link_category')) : ?>
 				<?php echo '<a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($this->article->catslug, $this->article->sectionid)).'">'; ?>
 			<?php endif; ?>
-			<?php echo $this->escape($this->article->category); ?>
+			<?php echo $this->article->category; ?>
 			<?php if ($this->params->get('link_category')) : ?>
 				<?php echo '</a>'; ?>
 			<?php endif; ?>
 		</span>
 		<?php endif; ?>
-	</td>
-</tr>
+	</p>
 <?php endif; ?>
 <?php if (($this->params->get('show_author')) && ($this->article->author != "")) : ?>
-<tr>
-	<td valign="top">
+	<p class="aticleauthor">
 		<span class="small">
-			<?php JText::printf( 'Written by', ($this->escape($this->article->created_by_alias) ? $this->escape($this->article->created_by_alias) : $this->escape($this->article->author)) ); ?>
+			<?php JText::printf( 'Written by', ($this->article->created_by_alias ? $this->article->created_by_alias : $this->article->author) ); ?>
 		</span>
-		&nbsp;&nbsp;
-	</td>
-</tr>
+	</p>
 <?php endif; ?>
 
 <?php if ($this->params->get('show_create_date')) : ?>
-<tr>
-	<td valign="top" class="createdate">
+	<p class="createdate">
 		<?php echo JHTML::_('date', $this->article->created, JText::_('DATE_FORMAT_LC2')) ?>
-	</td>
-</tr>
+	</p>
 <?php endif; ?>
 
 <?php if ($this->params->get('show_url') && $this->article->urls) : ?>
-<tr>
-	<td valign="top">
-		<a href="http://<?php echo $this->article->urls ; ?>" target="_blank">
-			<?php echo $this->escape($this->article->urls); ?></a>
-	</td>
-</tr>
+	<p>
+		<a href="http://<?php echo $this->article->urls ; ?>" rel="external"><?php echo $this->article->urls; ?></a>
+	</p>
 <?php endif; ?>
 
-<tr>
-<td valign="top">
 <?php if (isset ($this->article->toc)) : ?>
 	<?php echo $this->article->toc; ?>
 <?php endif; ?>
 <?php echo $this->article->text; ?>
-</td>
-</tr>
 
 <?php if ( intval($this->article->modified) !=0 && $this->params->get('show_modify_date')) : ?>
-<tr>
-	<td class="modifydate">
-		<?php echo JText::sprintf('LAST_UPDATED2', JHTML::_('date', $this->article->modified, JText::_('DATE_FORMAT_LC2'))); ?>
-	</td>
-</tr>
+	<p class="modifydate">
+		<?php echo JText::_( 'Last Updated' ); ?> ( <?php echo JHTML::_('date', $this->article->modified, JText::_('DATE_FORMAT_LC2')); ?> )
+	</p>
 <?php endif; ?>
-</table>
-<span class="article_separator">&nbsp;</span>
+</div>
+<p class="article_separator">&nbsp;</p>
 <?php echo $this->article->event->afterDisplayContent; ?>
