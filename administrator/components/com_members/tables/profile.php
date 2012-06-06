b404531bb0aad2b94b159ef335304c56d6f283c0
Shawn Rice <zooley@purdue.edu>
2012-06-06 16:52:20 -0400

    Added registerDate to content pulled for admin list

--- administrator/components/com_members/tables/profile.php
+++ administrator/components/com_members/tables/profile.php
@@ -633,7 +633,7 @@ class MembersProfile extends JTable
 		}
 
 		$query  = "SELECT m.uidNumber, m.username, m.name, m.givenName, m.middleName, m.surname, 
-						m.organization, m.email, m.emailConfirmed, u.lastvisitDate, 
+						m.organization, m.email, m.emailConfirmed, u.lastvisitDate, m.registerDate,
 					CASE WHEN m.givenName IS NOT NULL AND m.givenName != '' AND m.givenName != '&nbsp;' THEN 
 						m.givenName 
 					ELSE 
