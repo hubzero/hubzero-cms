c141dc83561802d84d792c66b7f8da4cf0c80849
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2012-06-20 14:14:10 -0400

    restructure hzldap plugin file location for J17, remove deprecated breeze functions from nanohub

--- plugins/xauthentication/hzldap/hzldap.php
+++ plugins/xauthentication/hzldap/hzldap.php
@@ -2,7 +2,7 @@
 /**
  * HUBzero CMS
  *
- * Copyright 2005-2011 Purdue University. All rights reserved.
+ * Copyright 2005-2012 Purdue University. All rights reserved.
  *
  * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
  *
@@ -24,125 +24,37 @@
  *
  * @package   hubzero-cms
  * @author    Nicholas J. Kisseberth <nkissebe@purdue.edu>
- * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @copyright Copyright 2005-2012 Purdue University. All rights reserved.
  * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die( 'Restricted access' );
 
-jimport('joomla.plugin.plugin');
+jimport( 'joomla.plugin.plugin' );
 
 /**
- * Authentication plugin for HUBzero LDAP
+ * Short description for 'plgXAuthenticationHzldap'
+ * 
+ * Long description (if any) ...
  */
 class plgXAuthenticationHzldap extends JPlugin
 {
 	/**
 	 * Constructor
-	 * 
-	 * @param      object &$subject Event observer
-	 * @param      array  $config   Optional config values
-	 * @return     void
+	 *
+	 * For php4 compatability we must not use the __constructor as a constructor for plugins
+	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+	 * This causes problems with cross-referencing necessary for the observer design pattern.
+	 *
+	 * @param 	object $subject The object to observe
+	 * @param 	array  $config  An array that holds the plugin configuration
 	 */
-	public function __construct(&$subject, $config)
+	function plgXAuthenticationHzldap(& $subject, $config)
 	{
 		parent::__construct($subject, $config);
 	}
 
-	/**
-	 * Short description for 'logintobreeze'
-	 * 
-	 * Long description (if any) ...
-	 * 
-	 * @param      unknown $login Parameter description (if any) ...
-	 * @param      unknown $passwd Parameter description (if any) ...
-	 * @return     void
-	 */
-	public function logintobreeze($login, $passwd)
-	{
-		$buffer = '';
-		//Get login and password from a different file and do the stuff here
-
-		//Logout any user first
-		$this->logoutofbreeze();
-
-		$login = strtolower($login);
-		// Login Current user
-		$handle = fopen("http://breeze.nanohub.org/api/xml?action=login&login=$login&password=$passwd", "rb");
-
-		// First check if the login was successful
-		// else login failed
-
-		//$file = "newfile.xml";
-		//if (!$file_handle = fopen($file,"a")) { echo "Cannot open file"; }
-
-		$header = $http_response_header;
-		if ($handle) 
-		{
-			while (!feof($handle))
-			{
-				$buffer .= fgets($handle, 4096);
-				//echo $buffer.'@';
-				$bt = substr($buffer, 1, 7);
-				if ($bt == 'results')
-				{
-					//echo $buffer[1];
-					for ($i=0; $i<100; $i++)
-					{
-						//echo $buffer[$i];
-						//echo $bt[$i];
-					}
-				}
-				//echo $buffer;
-				//if (!fwrite($file_handle, $buffer)) { echo "Cannot write to file"; }
-				//if (!fwrite($file_handle, '#')) { echo "Cannot write to file"; }
-			}
-		}
-		//echo $buffer;
-		//$temp = explode(' ',$buffer);
-		//echo $temp[0];
-		//echo "You have successfully written data to $file";
-		//fclose($file_handle);
-
-		// Reading Breezesession cookie here
-		$header = $http_response_header;
-		$header_size = sizeof($http_response_header);
-
-		//echo $temp1;
-		for ($i=0;$i<$header_size;$i++)
-		{
-			$sub1 = substr($header[$i], 0, 10);
-			if ($sub1 == 'Set-Cookie') 
-			{
-				$string = $header[$i];
-				$needle = ";";
-				$clean = substr("$string", 0, strpos($string, $needle));
-				$cookie = ltrim(strrchr($clean, '='), '=');
-			}
-		}
-		//echo $string;
-		//echo '<br/>';
-		//echo $cookie;
-		//cleaning up
-		fclose($handle);
-
-		// setting cookie
-		setcookie("breezecookie",$cookie, time()+57600);
-
-		//header('location:cookie.php');
-	}
-
-	/**
-	 * Removes Breeze cookie, effectively logging out of Breeze
-	 * 
-	 * @return     void
-	 */
-	public function logoutofbreeze()
-	{
-		setcookie("breezecookie", "", time()-60000);
-		$handle1 = fopen("http://breeze.nanohub.org/api/xml?action=logout", "rb");
-	}
 
 	/**
 	 * This method should handle any authentication and report back to the subject
@@ -153,7 +65,7 @@ class plgXAuthenticationHzldap extends JPlugin
 	 * @param	object	$response	Authentication response object
 	 * @return	object	boolean
 	 */
-	public function onAuthenticate($credentials, $options, &$response)
+	function onAuthenticate( $credentials, $options, &$response )
 	{
 		// Initialize variables
 		$userdetails = null;
@@ -170,40 +82,36 @@ class plgXAuthenticationHzldap extends JPlugin
 			return false;
 		}
 
-		$xhub =& Hubzero_Factory::getHub();
+        $xhub =& Hubzero_Factory::getHub();
 
-		$hubShortName = $xhub->getCfg('hubShortName', '');
+		$hubShortName = $xhub->getCfg('hubShortName','');
 		$auth_method   = 'bind';
 		$port          = '389';
-		$base_dn       = $xhub->getCfg('hubLDAPBaseDN', 'ou=users,dc=localhost');
-		$search_string = 'uid=[search],ou=users,' . $base_dn;
+        $base_dn       = $xhub->getCfg('hubLDAPBaseDN','ou=users,dc=localhost');
+        $search_string = 'uid=[search],ou=users,' . $base_dn;
 		$users_dn      = 'uid=[username],ou=users,' . $base_dn;
 		$use_ldapV3    = 1;
 		$no_referrals  = 1;
-		$negotiate_tls = $xhub->getCfg('hubLDAPNegotiateTLS', '0');
-		$username      = $xhub->getCfg('hubLDAPSearchUserDN', 'uid=search,dc=localhost');
-		$password      = $xhub->getCfg('hubLDAPSearchUserPW', '');
-		$host          = $xhub->getCfg('hubLDAPMasterHost', 'localhost');
+        $negotiate_tls = $xhub->getCfg('hubLDAPNegotiateTLS','0');
+        $username      = $xhub->getCfg('hubLDAPSearchUserDN','uid=search,dc=localhost');
+		$password      = $xhub->getCfg('hubLDAPSearchUserPW','');
+        $host          = $xhub->getCfg('hubLDAPMasterHost','localhost');
 
 		if (!$port)
-		{
 			$port = '389';
-		}
 
-		$pattern = "/^\s*(ldap[s]{0,1}:\/\/|)([^:]*)(\:(\d+)|)\s*$/";
+    	$pattern = "/^\s*(ldap[s]{0,1}:\/\/|)([^:]*)(\:(\d+)|)\s*$/";
 
-		if (preg_match($pattern, $host, $matches))
+    	if (preg_match($pattern, $host, $matches))
 		{
 			$host = $matches[2];
 
-			if ($matches[1] == 'ldaps://')
-			{
-				$negotiate_tls = false;
+    		if ($matches[1] == 'ldaps://') {
+        		$negotiate_tls = false;
 			}
 
-			if (isset($matches[4]) && is_numeric($matches[4]))
-			{
-				$port = $matches[4];
+    		if (isset($matches[4]) && is_numeric($matches[4])) {
+        		$port = $matches[4];
 			}
 		}
 
@@ -216,51 +124,49 @@ class plgXAuthenticationHzldap extends JPlugin
 			return;
 		}
 
-		if ($use_ldapV3 && !@ldap_set_option($_ldc, LDAP_OPT_PROTOCOL_VERSION, 3))
+		if ( $use_ldapV3 && !@ldap_set_option($_ldc, LDAP_OPT_PROTOCOL_VERSION, 3) )
 		{
 			$response->status = JAUTHENTICATE_STATUS_FAILURE;
 			$response->error_message = 'HUBzero LDAP server refuses protocol version 3.';
 			return;
 		}
 
-		if ($use_ldapV3 && !@ldap_set_option($_ldc, LDAP_OPT_REFERRALS, $no_referrals))
+		if ( $use_ldapV3 && !@ldap_set_option($_ldc, LDAP_OPT_REFERRALS, $no_referrals) )
 		{
 			$response->status = JAUTHENTICATE_STATUS_FAILURE;
 			$response->error_message = 'HUBzero LDAP server refuses to set referrals options.';
 			return;
 		}
 
-		if ($use_ldapV3 && $negotiate_tls && !@ldap_start_tls($_ldc))
+		if ( $use_ldapV3 && $negotiate_tls && !@ldap_start_tls($_ldc))
 		{
 			$response->status = JAUTHENTICATE_STATUS_FAILURE;
 			$response->error_message = 'HUBzero LDAP server refuses to start TLS mode.';
 			return;
 		}
 
-		$attributes = array('mail', 'cn', 'uid');
+		$attributes = array('mail','cn', 'uid');
 
 		if ($auth_method == 'search')
 		{
-			if (strlen($username))
-			{
+			if (strlen($username)) {
 				$bindtest = @ldap_bind($username, $password);
 			}
-			else
-			{
+			else {
 				$bindtest = @ldap_bind($_ldc);
 			}
 
-			if ($bindtest)
+			if($bindtest)
 			{
 				// Search for users DN
 				$dn = str_replace("[search]", $credentials['username'], $search_string);
 
-				$search_result = @ldap_search($_ldc, $dn, '(objectClass=hubAccount)', array('mail', 'cn', 'uid'));
+       			$search_result = @ldap_search($_ldc, $dn, '(objectClass=hubAccount)', array('mail','cn','uid'));
 
 				$userdetails = @ldap_get_entries($_ldc, $search_result);
 
 				// Verify Users Credentials
-				$success = @ldap_bind($userdetails[0]['dn'][0], $credentials['password']);
+				$success = @ldap_bind($userdetails[0]['dn'][0],$credentials['password']);
 			}
 			else
 			{
@@ -274,12 +180,12 @@ class plgXAuthenticationHzldap extends JPlugin
 
 			$success = @ldap_bind($_ldc, $dn, $credentials['password']);
 
-			$search_result = @ldap_search($_ldc, $dn, '(objectClass=hubAccount)', array('mail', 'cn', 'uid'));
+			$search_result = @ldap_search($_ldc, $dn, '(objectClass=hubAccount)', array('mail','cn','uid'));
 
 			$userdetails = @ldap_get_entries($_ldc, $search_result);
 		}
 
-		if (!$success)
+		if(!$success)
 		{
 			$response->status = JAUTHENTICATE_STATUS_FAILURE;
 			$response->error_message = 'Incorrect username/password';
@@ -287,22 +193,17 @@ class plgXAuthenticationHzldap extends JPlugin
 		else
 		{
 			// Grab some details from LDAP and return them
-			if (isset($userdetails[0]['uid'][0])) 
-			{
+			if (isset($userdetails[0]['uid'][0])) {
 				$response->username = $userdetails[0]['uid'][0];
 			}
 
-			if (isset($userdetails[0]['mail'][0])) 
-			{
+			if (isset($userdetails[0]['mail'][0])) {
 				$response->email = $userdetails[0]['mail'][0];
 			}
 
-			if (isset($userdetails[0]['cn'][0])) 
-			{
+			if(isset($userdetails[0]['cn'][0])) {
 				$response->fullname = $userdetails[0]['cn'][0];
-			} 
-			else 
-			{
+			} else {
 				$response->fullname = $credentials['username'];
 			}
 
@@ -311,10 +212,6 @@ class plgXAuthenticationHzldap extends JPlugin
 			// Were good - So say so.
 			$response->status        = JAUTHENTICATE_STATUS_SUCCESS;
 			$response->error_message = '';
-			if ($hubShortName == 'nanoHUB.org')
-			{
-				$this->logintobreeze($credentials['username'], $credentials['password']);
-			}
 		}
 
 		@ldap_close($_ldc);
