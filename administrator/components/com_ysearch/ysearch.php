339ff6b0af8b53f9fc2c5a6388e488dee39bc304
Shawn Rice <zooley@purdue.edu>
2012-06-21 08:54:21 -0400

    Re-organized, updated for J17, docblock filled in

--- administrator/components/com_ysearch/ysearch.php
+++ administrator/components/com_ysearch/ysearch.php
@@ -28,30 +28,62 @@
  */
 
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
-ini_set('display_errors', 1);
-require_once JPATH_ROOT.'/components/com_ysearch/include.php';
+defined('_JEXEC') or die('Restricted access');
+
+error_reporting(E_ALL);
+@ini_set('display_errors', '1');
+
+// Authorization check
+if (version_compare(JVERSION, '1.6', 'lt'))
+{
+	$jacl = JFactory::getACL();
+	$jacl->addACL($option, 'manage', 'users', 'super administrator');
+	$jacl->addACL($option, 'manage', 'users', 'administrator');
+	$jacl->addACL($option, 'manage', 'users', 'manager');
+
+	$user = JFactory::getUser();
+	if (!$user->authorize($option, 'manage'))
+	{
+		$app = JFactory::getApplication();
+		$app->redirect( 'index.php', JText::_('ALERTNOTAUTH') );
+	}
+}
+else 
+{
+	if (!JFactory::getUser()->authorise('core.manage', $option)) 
+	{
+		return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+	}
+}
+
+require_once JPATH_ROOT . DS . 'components' . DS . $option . DS . 'include.php';
 
 JPluginHelper::importPlugin('ysearch');
 
 $context = array();
 if (array_key_exists('ysearch-task', $_POST))
-	foreach (JApplication::triggerEvent('onYSearchTask'.$_POST['ysearch-task']) as $resp)
+{
+	foreach (JApplication::triggerEvent('onYSearchTask' . $_POST['ysearch-task']) as $resp)
 	{
 		list($name, $html, $ctx) = $resp;
 		echo $html;
 		if (array_key_exists($name, $context))
+		{
 			$context[$name] = array_merge($context[$name], $ctx);
+		}
 		else
+		{
 			$context[$name] = $ctx;
+		}
 	}
+}
 
 JToolBarHelper::title(JText::_('Search'), 'search.png');
 
 foreach (JApplication::triggerEvent('onYSearchAdministrate', array($context)) as $plugin)
 {
 	list($name, $html) = $plugin;
-	echo '<h3>'.$name.'</h3>';
+	echo '<h3>' . $name . '</h3>';
 	echo $html;
 }
 
