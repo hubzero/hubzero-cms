d481f1c4eb6bad6bc8f5066900e60e184ab505fa
Shawn Rice <zooley@purdue.edu>
2012-07-24 10:50:09 -0400

    Changed author to HUBzero.

--- modules/mod_googleanalytics/tmpl/default.php
+++ modules/mod_googleanalytics/tmpl/default.php
@@ -28,8 +28,10 @@ defined('_JEXEC') or die('Restricted access');
 <script type="text/javascript">
 	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	try {
+</script>
+<script type="text/javascript">
+	try { 
 		var pageTracker = _gat._getTracker("<?php echo $this->params->get('key'); ?>");
 		pageTracker._trackPageview();
-	} catch (err) {}
+	} catch(err) {} 
 </script>
