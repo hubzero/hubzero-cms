1635d0a7d32ab9aad3f46469ce4bef1852017eae
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2012-08-03 16:52:21 -0400

    add ldap user plugin for future use. do not activate yet.

---
+++ plugins/user/ldap/ldap.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2012 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Nicholas J. Kisseberth <nkissebe@purdue.edu>
+ * @copyright Copyright 2012 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.event.plugin');
+
+/**
+ * User plugin for hub users
+ */
+class plgUserLdap extends JPlugin
+{
+	/**
+	 * Constructor
+	 * 
+	 * @param      object &$subject Event observer
+	 * @param      array  $config   Optional config values
+	 * @return     void
+	 */
+	public function __construct(&$subject, $config)
+	{
+		parent::__construct($subject, $config);
+	}
+
+	/**
+	 * Method is called after user data is stored in the database
+	 *
+	 * @param array holds the new user data
+	 * @param boolean true if a new user is stored
+	 * @param boolean true if user was succesfully stored in the database
+	 * @param string message
+	 */
+	public function onAfterStoreUser($user, $isnew, $succes, $msg)
+	{
+		Hubzero_Ldap::syncUser($user->id);
+	}
+
+	/**
+	 * Method is called after user data is deleted from the database
+	 *
+	 * @param array holds the user data
+	 * @param boolean true if user was succesfully stored in the database
+	 * @param string message
+	 */
+	public function onAfterDeleteUser($user, $succes, $msg)
+	{
+		Hubzero_Ldap::syncUser($user->id);
+	}
+	
+	/**
+	 * Method is called after group data is stored in the database
+	 *
+	 * @param array holds the new user data
+	 * @param boolean true if a new user is stored
+	 * @param boolean true if user was succesfully stored in the database
+	 * @param string message
+	 */
+	public function onAfterStoreUser($group)
+	{
+		Hubzero_Ldap::syncGroup($group->cn);
+	}
+	
+	/**
+	 * Method is called after user data is deleted from the database
+	 *
+	 * @param array holds the user data
+	 * @param boolean true if user was succesfully stored in the database
+	 * @param string message
+	 */
+	public function onAfterDeleteUser($group)
+	{
+		Hubzero_Ldap::syncUser($group->cn);
+	}	
+}
