65c31a64edb3ae61898863c93af50999c1520f7e
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2012-08-16 13:56:02 -0400

    restore installation application, fix cms to not object to its existence

--- includes/framework.php
+++ includes/framework.php
@@ -23,7 +23,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 /*
  * Installation check, and check on removal of the install directory.
  */
-if (!file_exists( JPATH_CONFIGURATION . DS . 'configuration.php' ) || (filesize( JPATH_CONFIGURATION . DS . 'configuration.php' ) < 10) || is_writable( JPATH_INSTALLATION . DS . 'index.php' )) {
+if (!file_exists( JPATH_CONFIGURATION . DS . 'configuration.php' ) ) {
 	if( file_exists( JPATH_INSTALLATION . DS . 'index.php' ) ) {
 		header( 'Location: installation/index.php' );
 		exit();
@@ -55,6 +55,20 @@ if (@$CONFIG->error_reporting === 0) {
 }
 
 define( 'JDEBUG', $CONFIG->debug );
+
+
+/* if configuration just has an install key and no other properties then redirect into the installer */
+
+if ((@$CONFIG->install_key != '') && count(get_object_vars($CONFIG) == 1))
+{
+	if( file_exists( JPATH_INSTALLATION . DS . 'index.php' ) ) {
+		header( 'Location: installation/index.php' );
+		exit();
+	} else {
+		echo 'Installation requested by configuration but no installation code available. Exiting...';
+		exit();
+	}
+}
 
 unset( $CONFIG );
 
