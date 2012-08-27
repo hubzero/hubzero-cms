cdc12ab4708aeedc8d9f6aaa261fb2dc582938a4
Shawn Rice <zooley@purdue.edu>
2012-08-27 11:37:15 -0400

    Added class for consisten styling and tweaked view descriptions

--- components/com_events/views/browse/tmpl/month.php
+++ components/com_events/views/browse/tmpl/month.php
@@ -39,7 +39,7 @@ defined('_JEXEC') or die( 'Restricted access' );
 <?php if ($this->authorized) { ?>
 <div id="content-header-extra">
 	<ul id="useroptions">
-		<li class="last"><a href="<?php echo JRoute::_('index.php?option='.$this->option.'&task=add'); ?>"><?php echo JText::_('EVENTS_ADD_EVENT'); ?></a></li>
+		<li class="last"><a class="add" href="<?php echo JRoute::_('index.php?option='.$this->option.'&task=add'); ?>"><?php echo JText::_('EVENTS_ADD_EVENT'); ?></a></li>
 	</ul>
 </div><!-- / #content-header-extra -->
 <?php } ?>
