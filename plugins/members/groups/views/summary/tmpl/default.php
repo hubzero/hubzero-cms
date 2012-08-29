0a83fe49b91de09067de0bdbb63c0bd29d93ba8f
Shawn Rice <zooley@purdue.edu>
2012-08-29 17:32:58 -0400

    Reworked interface to gain space and be a little cleaner

--- plugins/members/groups/views/summary/tmpl/default.php
+++ plugins/members/groups/views/summary/tmpl/default.php
@@ -31,56 +31,68 @@
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die( 'Restricted access' );
 ?>
-<h3 class="section-header"><a name="groups"></a><?php echo JText::_('PLG_MEMBERS_GROUPS'); ?></h3>
-<div class="aside">
-	<div class="container">
-		<h4><?php echo JText::_('Your Groups'); ?></h4>
-		<p class="starter"><span class="starter-point"></span>These are the groups you have membership in or have been invited to.</p>
-	</div><!-- / .container -->
-	<div class="container">
-		<h4><?php echo JText::_('PLG_MEMBERS_GROUPS_WHAT_ARE_GROUPS'); ?></h4>
-		<p class="starter"><span class="starter-point"></span><?php echo JText::_('PLG_MEMBERS_GROUPS_EXPLANATION'); ?></p>
-		<p>Go to the <a href="<?php echo JRoute::_('index.php?option=com_groups'); ?>"><?php echo JText::_('Groups page'); ?></a>.</p>
-	</div><!-- / .container -->
-	<p class="add"><a href="<?php echo JRoute::_('index.php?option=com_groups&task=new'); ?>"><?php echo JText::_('PLG_MEMBERS_GROUPS_CREATE'); ?></a></p>
-</div><!-- / .aside -->
-<div class="subject">
+<h3 class="section-header">
+	<a name="groups"></a>
+	<?php echo JText::_('PLG_MEMBERS_GROUPS'); ?>
+</h3>
+
+<?php if ($this->groups) { ?>
 	<div class="container">
 		<table class="groups entries" summary="<?php echo JText::_('PLG_MEMBERS_GROUPS_TBL_SUMMARY'); ?>">
 			<caption>
 				<?php echo JText::_('Your Groups'); ?>
 				<span>(<?php echo count($this->groups); ?>)</span>
 			</caption>
+			<tfoot>
+				<tr>
+					<td colspan="5">
+						<a class="add" href="<?php echo JRoute::_('index.php?option=com_groups&task=new'); ?>"><?php echo JText::_('PLG_MEMBERS_GROUPS_CREATE'); ?></a>
+					</td>
+				</tr>
+			</tfoot>
 			<tbody>
 <?php
-if ($this->groups) {
 	foreach ($this->groups as $group)
 	{
 		$status = '';
-		if ($group->manager && $group->published) {
+		if ($group->manager && $group->published) 
+		{
 			$status = 'manager';
 
-			$opt  = '<a href="'.JRoute::_('index.php?option='.$this->option.'&gid='.$group->cn.'&active=members') .'">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_MANAGE').'</a>';
-			$opt .= ' <a href="'.JRoute::_('index.php?option='.$this->option.'&gid='.$group->cn.'&task=edit') .'">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_EDIT').'</a>';
-			$opt .= ' <a href="'.JRoute::_('index.php?option='.$this->option.'&gid='.$group->cn.'&task=delete') .'">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_DELETE').'</a>';
-		} else {
-			if (!$group->published) {
+			$opt  = '<a class="manage tooltips" href="' . JRoute::_('index.php?option=' . $this->option . '&gid=' . $group->cn . '&active=members') .'" title="Manager Options :: Manage group membership">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_MANAGE').'</a>';
+			$opt .= ' <a class="customize tooltips" href="' . JRoute::_('index.php?option=' . $this->option . '&gid=' . $group->cn . '&task=edit') .'" title="Manager Options :: Edit this group">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_EDIT').'</a>';
+			$opt .= ' <a class="delete tooltips" href="' . JRoute::_('index.php?option=' . $this->option . '&gid=' . $group->cn . '&task=delete') .'" title="Manager Options :: Delete this group">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_DELETE').'</a>';
+		} 
+		else 
+		{
+			if (!$group->published) 
+			{
 				$status = 'new';
-			} else {
-				if ($group->registered) {
-					if ($group->regconfirmed) {
+			} 
+			else 
+			{
+				if ($group->registered) 
+				{
+					if ($group->regconfirmed) 
+					{
 						$status = 'member';
-						$opt = '<a href="'.JRoute::_('index.php?option='.$this->option.'&gid='.$group->cn.'&task=cancel') .'">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_CANCEL').'</a>';
-					} else {
+					} 
+					else 
+					{
 						$status = 'pending';
-						$opt = '<a href="'.JRoute::_('index.php?option='.$this->option.'&gid='.$group->cn.'&task=cancel') .'">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_CANCEL').'</a>';
 					}
-				} else {
-					if ($group->regconfirmed) {
+					$opt = '<a class="cancel tooltips" href="' . JRoute::_('index.php?option=' . $this->option . '&gid=' . $group->cn . '&task=cancel') .'" title="Membership Options :: Cancel membership to this group">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_CANCEL').'</a>';
+				} 
+				else 
+				{
+					if ($group->regconfirmed) 
+					{
 						$status = 'invitee';
-						$opt  = '<a href="'.JRoute::_('index.php?option='.$this->option.'&gid='.$group->cn.'&task=accept') .'">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_ACCEPT').'</a>';
-						$opt .= ' <a href="'.JRoute::_('index.php?option='.$this->option.'&gid='.$group->cn.'&task=cancel') .'">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_CANCEL').'</a>';
-					} else {
+						$opt  = '<a class="accept tooltips" href="' . JRoute::_('index.php?option=' . $this->option . '&gid=' . $group->cn . '&task=accept') .'" title="Membership Options :: Accept membership to this group">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_ACCEPT').'</a>';
+						$opt .= ' <a class="cancel tooltips" href="' . JRoute::_('index.php?option=' . $this->option . '&gid=' . $group->cn . '&task=cancel') .'" title="Membership Options :: Cancel membership to this group">'.JText::_('PLG_MEMBERS_GROUPS_ACTION_CANCEL').'</a>';
+					} 
+					else 
+					{
 						$opt = '';
 					}
 				}
@@ -92,52 +104,59 @@ if ($this->groups) {
 						<span class="entry-id"><?php echo $group->gidNumber; ?></span>
 					</th>
 					<td>
-						<a class="entry-title" rel="<?php echo $group->gidNumber; ?>" href="<?php echo JRoute::_('index.php?option='.$this->option.'&gid='. $group->cn); ?>"><?php echo stripslashes($group->description); ?></a><br />
+						<a class="entry-title" rel="<?php echo $group->gidNumber; ?>" href="<?php echo JRoute::_('index.php?option=' . $this->option . '&gid='. $group->cn); ?>">
+							<?php echo $this->escape(stripslashes($group->description)); ?>
+						</a><br />
 						<span class="entry-details">
-							<span class="entry-alias"><?php echo $group->cn; ?></span>
+							<span class="entry-alias"><?php echo $this->escape($group->cn); ?></span>
 						</span>
 					</td>
 					<td>
 						<?php
 						switch ($group->join_policy)
 						{
-							case 3: echo '<span class="closed join-policy">'.JText::_('Closed').'</span>'."\n"; break;
-							case 2: echo '<span class="inviteonly join-policy">'.JText::_('Invite Only').'</span>'."\n"; break;
-							case 1: echo '<span class="restricted join-policy">'.JText::_('Restricted').'</span>'."\n";  break;
+							case 3: echo '<span class="closed join-policy">' . JText::_('Closed') . '</span>'."\n"; break;
+							case 2: echo '<span class="inviteonly join-policy">' . JText::_('Invite Only') . '</span>'."\n"; break;
+							case 1: echo '<span class="restricted join-policy">' . JText::_('Restricted') . '</span>'."\n";  break;
 							case 0:
-							default: echo '<span class="open join-policy">'.JText::_('Open').'</span>'."\n"; break;
+							default: echo '<span class="open join-policy">' . JText::_('Open') . '</span>'."\n"; break;
 						}
-?>
+						?>
 					</td>
 					<td>
 						<span class="<?php echo $status; ?> status"><?php
 							switch ($status)
 							{
-								case 'manager': echo JText::_('PLG_MEMBERS_GROUPS_STATUS_MANAGER'); break;
-								case 'new': echo JText::_('PLG_MEMBERS_GROUPS_STATUS_NEW_GROUP'); break;
-								case 'member': echo JText::_('PLG_MEMBERS_GROUPS_STATUS_APPROVED'); break;
-								case 'pending': echo JText::_('PLG_MEMBERS_GROUPS_STATUS_PENDING'); break;
-								case 'invitee': echo JText::_('PLG_MEMBERS_GROUPS_STATUS_INVITED'); break;
+								case 'manager': echo JText::_('PLG_MEMBERS_GROUPS_STATUS_MANAGER');   break;
+								case 'new':     echo JText::_('PLG_MEMBERS_GROUPS_STATUS_NEW_GROUP'); break;
+								case 'member':  echo JText::_('PLG_MEMBERS_GROUPS_STATUS_APPROVED');  break;
+								case 'pending': echo JText::_('PLG_MEMBERS_GROUPS_STATUS_PENDING');   break;
+								case 'invitee': echo JText::_('PLG_MEMBERS_GROUPS_STATUS_INVITED');   break;
 								default: break;
 							}
 						?></span>
 					</td>
-					<td>
+					<td class="user-options">
 						<?php echo $opt; ?>
 					</td>
 				</tr>
 <?php
 	}
-} else {
-?>
-				<tr>
-					<td colspan="5"><?php echo JText::_('PLG_MEMBERS_GROUPS_NO_MEMBERSHIPS'); ?></td>
-				</tr>
-<?php
-}
 ?>
 			</tbody>
 		</table>
+<?php } else { ?>
+		<div class="two columns first">
+			<h4><?php echo JText::_('Your Groups'); ?></h4>
+			<p><?php echo JText::_('Here you will find the groups you created, have membership in, or have been invited to. You may cancel membership in a group at any time.'); ?></p>
+			<p><a class="add" href="<?php echo JRoute::_('index.php?option=com_groups&task=new'); ?>"><?php echo JText::_('PLG_MEMBERS_GROUPS_CREATE'); ?></a></p>
+		</div><!-- / .container -->
+		<div class="two columns second">
+			<h4><?php echo JText::_('PLG_MEMBERS_GROUPS_WHAT_ARE_GROUPS'); ?></h4>
+			<p><?php echo JText::_('PLG_MEMBERS_GROUPS_EXPLANATION'); ?></p>
+			<p>Go to the <a href="<?php echo JRoute::_('index.php?option=com_groups'); ?>"><?php echo JText::_('Groups page'); ?></a>.</p>
+		</div><!-- / .container -->
+		<div class="clear"></div>
+<?php } ?>
 	</div><!-- / .container -->
-</div><!-- / .subject -->
-<div class="clear"></div>
+
