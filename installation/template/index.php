50d7f2508dd0e38400321cec742f8fd07306709f
Shawn Rice <zooley@purdue.edu>
2012-08-31 17:50:13 -0400

    New styles for installation

--- installation/template/index.php
+++ installation/template/index.php
@@ -1,14 +1,25 @@
 <?php
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
+
+include_once(JPATH_ROOT . DS . 'libraries' . DS . 'Hubzero' . DS . 'Browser.php');
+
+$browser = new Hubzero_Browser();
+$b = $browser->getBrowser();
+$v = $browser->getBrowserMajorVersion();
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html dir="<?php echo  $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html dir="<?php echo  $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html dir="<?php echo  $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html dir="<?php echo  $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html dir="<?php echo $this->direction; ?>" lang="<?php echo  $this->language; ?>" class="<?php echo $b . ' ' . $b . $v; ?>"> <!--<![endif]-->
 	<head>
 		<jdoc:include type="head" />
 
+		<link href="../media/system/css/fontcons.css" rel="stylesheet" type="text/css" />
 		<link href="template/css/template.css" rel="stylesheet" type="text/css" />
-		<?php if($this->direction == 'rtl') : ?>
+		<?php if ($this->direction == 'rtl') : ?>
 		<link href="template/css/template_rtl.css" rel="stylesheet" type="text/css" />
 		<?php endif; ?>
 
@@ -17,29 +28,25 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 		<script type="text/javascript" src="template/js/validation.js"></script>
 
 		<script type="text/javascript">
-			Window.onDomReady(function(){ new Accordion($$('h3.moofx-toggler'), $$('div.moofx-slider'), {onActive: function(toggler, i) { toggler.addClass('moofx-toggler-down'); },onBackground: function(toggler, i) { toggler.removeClass('moofx-toggler-down'); },duration: 300,opacity: false, alwaysHide:true, show: 1}); });
-  		</script>
+			window.onDomReady(function(){ new Accordion($$('h3.moofx-toggler'), $$('div.moofx-slider'), {onActive: function(toggler, i) { toggler.addClass('moofx-toggler-down'); },onBackground: function(toggler, i) { toggler.removeClass('moofx-toggler-down'); },duration: 300,opacity: false, alwaysHide:true, show: 1}); });
+		</script>
 	</head>
 	<body>
-		<div id="header1">
-			<div id="header2">
-				<div id="header3">
-					<div id="version"><?php echo JVERSION ?></div>
+		<div id="wrap">
+			<div id="header">
+				<h1>
 					<span><?php echo JText::_('Installation') ?></span>
+				</h1>
+				<span id="version">1.2</span>
+			</div>
+			<div id="content-box">
+				<div id="content-pad">
+					<jdoc:include type="installation" />
 				</div>
 			</div>
 		</div>
-		<div id="content-box">
-			<div id="content-pad">
-				<jdoc:include type="installation" />
-			</div>
-		</div>
-		<div id="footer1">
-			<div id="footer2">
-				<div id="footer3"></div>
-			</div>
-		</div>
-		<div id="copyright"><a href="http://www.joomla.org" target="_blank">Joomla!</a>
+		<div id="copyright">
+			<a href="http://hubzer.org" rel="external">HUBzero&reg;</a>
 			<?php echo JText::_('ISFREESOFTWARE') ?>
 		</div>
 	</body>
