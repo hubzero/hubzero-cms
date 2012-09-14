075e648b8d67406217c63dd77bf5372172fb0c1f
Shawn Rice <zooley@purdue.edu>
2012-09-14 17:02:26 -0400

    Fancybox syntax fix for close

--- plugins/groups/messages/messages.jquery.js
+++ plugins/groups/messages/messages.jquery.js
@@ -63,7 +63,7 @@ HUB.MembersMsg = {
 						}
 						
 						$.post($(this).attr('action'),$(this).serialize(), function(data) {
-							$.fancybox().close();  
+							$.fancybox.close();
 						});
 					});
 				}
