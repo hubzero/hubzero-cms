0daca613223a874299d46f0252bb3daef44a570f
Shawn Rice <zooley@purdue.edu>
2012-09-17 08:35:27 -0400

    Wrapped content in main section div

--- components/com_user/views/remind/tmpl/default.php
+++ components/com_user/views/remind/tmpl/default.php
@@ -6,43 +6,44 @@
 </div>
 <?php endif; ?>
 
-<form action="<?php echo JRoute::_( 'index.php?option=com_user&task=remindusername' ); ?>" method="post" class="josForm form-validate" name="hubForm" id="hubForm">
-
-	<div class="explaination">
-		<p class="info">
-			If you already know your username, and only need your password reset, <a href="<?php echo JRoute::_('/login/reset'); ?>">go here now</a>.
-		</p>
-	</div>
-	<fieldset>
-		<legend>Recover Username(s)</legend>
-
-		<label for="email" class="hasTip" title="<?php echo JText::_('REMIND_USERNAME_EMAIL_TIP_TITLE'); ?>::<?php echo JText::_('REMIND_USERNAME_EMAIL_TIP_TEXT'); ?>"><?php echo JText::_('Email Address'); ?>:</label>
-		<input id="email" name="email" type="text" size="36" class="required validate-email" />
-			
-		<p><?php echo JText::_('REMIND_USERNAME_DESCRIPTION'); ?></p>
-
-		<div class="help">
-		<h4>What if I have also lost my password?</h4>
-		<p>
-			Fill out this form to retrieve your username(s). The email you 
-			receive will contain instructions on how to reset your password as well.
-		</p>
-			
-		<h4>What if I have multiple accounts?</h4>
-		<p>
-			All accounts registered to your email address will be located, and you will be given a 
-			list of all of those usernames.
-		</p>
-			
-		<h4>What if this cannot find my account?</h4>
-		<p>
-			It is possible you registered under a different email address.  Please try any other email 
-			addresses you have.
-		</p>
+<div class="main section">
+	<form action="<?php echo JRoute::_( 'index.php?option=com_user&task=remindusername' ); ?>" method="post" class="josForm form-validate" name="hubForm" id="hubForm">
+		<div class="explaination">
+			<p class="info">
+				If you already know your username, and only need your password reset, <a href="<?php echo JRoute::_('/login/reset'); ?>">go here now</a>.
+			</p>
 		</div>
-	</fieldset>
-	<div class="clear"></div>
+		<fieldset>
+			<legend>Recover Username(s)</legend>
+
+			<label for="email" class="hasTip" title="<?php echo JText::_('REMIND_USERNAME_EMAIL_TIP_TITLE'); ?>::<?php echo JText::_('REMIND_USERNAME_EMAIL_TIP_TEXT'); ?>"><?php echo JText::_('Email Address'); ?>:</label>
+			<input id="email" name="email" type="text" size="36" class="required validate-email" />
+
+			<p><?php echo JText::_('REMIND_USERNAME_DESCRIPTION'); ?></p>
+
+			<div class="help">
+				<h4>What if I have also lost my password?</h4>
+				<p>
+					Fill out this form to retrieve your username(s). The email you 
+					receive will contain instructions on how to reset your password as well.
+				</p>
+			
+				<h4>What if I have multiple accounts?</h4>
+				<p>
+					All accounts registered to your email address will be located, and you will be given a 
+					list of all of those usernames.
+				</p>
+			
+				<h4>What if this cannot find my account?</h4>
+				<p>
+					It is possible you registered under a different email address.  Please try any other email 
+					addresses you have.
+				</p>
+			</div>
+		</fieldset>
+		<div class="clear"></div>
 	
-	<p class="submit"><button type="submit" class="validate"><?php echo JText::_('Submit'); ?></button></p>
-	<?php echo JHTML::_( 'form.token' ); ?>
-</form>
+		<p class="submit"><button type="submit" class="validate"><?php echo JText::_('Submit'); ?></button></p>
+		<?php echo JHTML::_( 'form.token' ); ?>
+	</form>
+</div><!-- / .main section -->
--- components/com_user/views/reset/tmpl/default.php
+++ components/com_user/views/reset/tmpl/default.php
@@ -5,18 +5,24 @@
 		<h2><?php echo $this->escape($this->params->get('page_title')); ?></h2>
 	</div>
 <?php endif; ?>
+	<div class="main section">
+		<form action="<?php echo JRoute::_( 'index.php?option=com_user&task=requestreset' ); ?>" method="post" class="josForm form-validate" name="hubForm" id="hubForm">
+			<div class="explaination">
+				<p class="info">
+					<?php echo JText::_('Forgot your username? Go <a href="/user/remind">here</a> to recover it.'); ?>
+				</p>
+			</div>
+			<fieldset>
+				<legend><?php echo JText::_('Email Verification Token'); ?></legend>
 
-	<form action="<?php echo JRoute::_( 'index.php?option=com_user&task=requestreset' ); ?>" method="post" class="josForm form-validate" name="hubForm" id="hubForm">
-		<fieldset>
-			<legend><?php echo JText::_('Email Verification Token'); ?></legend>
-
-			<p><?php echo JText::_('RESET_PASSWORD_REQUEST_DESCRIPTION'); ?></p>
-			<label for="username" class="hasTip" title="<?php echo JText::_('RESET_PASSWORD_USERNAME_TIP_TITLE'); ?>::<?php echo JText::_('RESET_PASSWORD_USERNAME_TIP_TEXT'); ?>"><?php echo JText::_('User Name'); ?>:</label>
-			<input id="username" name="username" type="text" class="required" size="36" />
+				<p><?php echo JText::_('RESET_PASSWORD_REQUEST_DESCRIPTION'); ?></p>
+				<label for="username" class="hasTip" title="<?php echo JText::_('RESET_PASSWORD_USERNAME_TIP_TITLE'); ?>::<?php echo JText::_('RESET_PASSWORD_USERNAME_TIP_TEXT'); ?>"><?php echo JText::_('User Name'); ?>:</label>
+				<input id="username" name="username" type="text" class="required" size="36" />
 	
-		</fieldset>
-		<div class="clear"></div>
+			</fieldset>
+			<div class="clear"></div>
 			
-		<p class="submit"><button type="submit" class="validate"><?php echo JText::_('Submit'); ?></button></p>
-		<?php echo JHTML::_( 'form.token' ); ?>
-	</form>
+			<p class="submit"><button type="submit" class="validate"><?php echo JText::_('Submit'); ?></button></p>
+			<?php echo JHTML::_( 'form.token' ); ?>
+		</form>
+	</div><!-- / .main section -->
