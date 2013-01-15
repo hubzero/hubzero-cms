ecf6bad7a7128066c5368696ea65d2c89743b0db
Shawn Rice <zooley@purdue.edu>
2013-01-15 15:36:50 -0500

    Added create method for submission through the API

--- components/com_support/controllers/api.php
+++ components/com_support/controllers/api.php
@@ -34,6 +34,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 JLoader::import('Hubzero.Api.Controller');
+require_once(JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_support' . DS . 'tables' . DS . 'ticket.php');
 
 /**
  * API controller class for support tickets
@@ -53,12 +54,18 @@ class SupportApiController extends Hubzero_Api_Controller
 		$this->segments = $this->getRouteSegments();
 		$this->response = $this->getResponse();
 
+		$this->config = JComponentHelper::getParams('com_support');
+		$this->database = JFactory::getDBO();
+
+		include_once(JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_support' . DS . 'helpers' . DS . 'acl.php');
 		$this->acl = SupportACL::getACL();
 
 		switch ($this->segments[0]) 
 		{
 			case 'ticket':  $this->ticket();  break;
 			case 'tickets': $this->tickets(); break;
+			case 'stats':   $this->stats();   break;
+			case 'create':  $this->create();  break;
 			default:        $this->error();   break;
 		}
 	}
@@ -76,11 +83,105 @@ class SupportApiController extends Hubzero_Api_Controller
 	}
 
 	/**
+	 * Displays ticket stats
+	 *
+	 * @return    void
+	 */
+	private function stats()
+	{
+		$format = JRequest::getVar('format', 'json');
+
+		$type = JRequest::getVar('type', 'submitted');
+		$type = ($type == 'automatic') ? 1 : 0;
+
+		$group = JRequest::getVar('group', '');
+
+		// Set up some dates
+		$jconfig =& JFactory::getConfig();
+		$this->offset = $jconfig->getValue('config.offset');
+
+		$year  = JRequest::getInt('year', strftime("%Y", time()+($this->offset*60*60)));
+		$month = strftime("%m", time()+($this->offset*60*60));
+		if ($month <= "9"&preg_match("#(^[1-9]{1})#",$month)) 
+		{
+			$month = "0$month";
+		}
+		$day   = strftime("%d", time()+($this->offset*60*60));
+		if ($day <= "9"&preg_match("#(^[1-9]{1})#",$day)) 
+		{
+			$day = "0$day";
+		}
+
+		/*$startday = 0;
+		$numday = ((date("w",mktime(0,0,0,$month,$day,$year))-$startday)%7);
+		if ($numday == -1) 
+		{
+			$numday = 6;
+		}
+		$week_start = mktime(0, 0, 0, $month, ($day - $numday), $year);
+		$week = strftime("%d", $week_start);*/
+
+		$stats = new stdClass;
+		$stats->open = 0;
+		$stats->new = 0;
+		$stats->unassigned = 0;
+		$stats->closed = 0;
+		$stats->tickets = new stdClass;
+		$stats->tickets->opened = array();
+		$stats->tickets->closed = array();
+
+		$st = new SupportTicket($this->database);
+
+
+		$sql = "SELECT id, created, YEAR(created) AS `year`, MONTH(created) AS `month`, status, owner 
+				FROM #__support_tickets
+				WHERE report!='' 
+				AND type=" . $type . " AND open=1";
+		if (!$group) 
+		{
+			$sql .= " AND (`group`='' OR `group` IS NULL)";
+		} 
+		else 
+		{
+			$sql .= " AND `group`='{$group}'";
+		}
+		$sql .= " ORDER BY created ASC";
+		$this->database->setQuery($sql);
+		$openTickets = $this->database->loadObjectList();
+		foreach ($openTickets as $o)
+		{
+			if (!isset($stats->tickets->opened[$o->year]))
+			{
+				$stats->tickets->opened[$o->year] = array();
+			}
+			if (!isset($stats->tickets->opened[$o->year][$o->month]))
+			{
+				$stats->tickets->opened[$o->year][$o->month] = 0;
+			}
+			$stats->tickets->opened[$o->year][$o->month]++;
+
+			$stats->open++;
+
+			if (!$o->status)
+			{
+				$stats->new++;
+			}
+			if (!$o->owner)
+			{
+				$stats->unassigned++;
+			}
+		}
+
+		$this->setMessageType($format);
+		$this->setMessage($stats);
+	}
+
+	/**
 	 * Displays a list of tickets
 	 *
 	 * @return    void
 	 */
-	public function tickets()
+	private function tickets()
 	{
 		//get request vars
 		$format = JRequest::getVar('format', 'json');
@@ -117,6 +218,88 @@ class SupportApiController extends Hubzero_Api_Controller
 	 */
 	private function create()
 	{
-		$this->getResponse()->setErrorMessage(404, 'Not Found');
+		$this->setMessageType(JRequest::getVar('format', 'json'));
+
+		// Create an object for returning messages
+		$msg = new stdClass;
+
+		// Initiate class and bind data to database fields
+		$ticket = new SupportTicket($this->database);
+
+		// Set the created date
+		$ticket->created   = $msg->submitted = date("Y-m-d H:i:s");
+
+		// Incoming
+		$ticket->report   = JRequest::getVar('report', '', 'post', 'none', 2);
+		if (!$ticket->report)
+		{
+			$msg->success = false;
+			$msg->errors  = array(JText::_('Error: Report contains no text.'));
+
+			$this->setMessage($msg);
+			return;
+		}
+		$ticket->os        = JRequest::getVar('os', 'unknown', 'post');
+		$ticket->browser   = JRequest::getVar('browser', 'unknown', 'post');
+		$ticket->severity  = JRequest::getVar('severity', 'normal', 'post');
+
+		// Cut suggestion at 70 characters
+		$ticket->summary   = substr($ticket->report, 0, 70);
+		if (strlen($ticket->summary) >= 70) 
+		{
+			$ticket->summary .= '...';
+		}
+
+		// Get user data
+		$juser = JFactory::getUser();
+		$ticket->name      = $juser->get('name');
+		$ticket->email     = $juser->get('email');
+		$ticket->login     = $juser->get('username');
+
+		// Set some helpful info
+		$ticket->instances = 1;
+		$ticket->section   = 1;
+		$ticket->open      = 1;
+		$ticket->status    = 0;
+
+		ximport('Hubzero_Environment');
+		$ticket->ip        = Hubzero_Environment::ipAddress();
+		$ticket->hostname  = gethostbyaddr(JRequest::getVar('REMOTE_ADDR','','server'));
+
+		// Check the data
+		if (!$ticket->check()) 
+		{
+			$msg->success = false;
+			$msg->errors  = $row->getErrors();
+
+			$this->setMessage($msg);
+			return;
+		}
+
+		// Save the data
+		if (!$ticket->store()) 
+		{
+			$msg->success = false;
+			$msg->errors  = $row->getErrors();
+
+			$this->setMessage($msg);
+			return;
+		}
+
+		// Any tags?
+		$tags = trim(JRequest::getVar('tags', '', 'post'));
+		if ($tags)
+		{
+			require_once(JPATH_ROOT . DS . 'components' . DS . 'com_support' . DS . 'helpers' . DS . 'tags.php');
+
+			$st = new SupportTags($this->database);
+			$st->tag_object($juser->get('id'), $ticket->id, $tags, 0, true);
+		}
+
+		// Set the response
+		$msg->success = true;
+		$msg->ticket  = $ticket->id;
+
+		$this->setMessage($msg);
 	}
 }
