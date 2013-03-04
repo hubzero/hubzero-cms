1bd42a13d481a433aa052e656290d8f268c8eebf
Shawn Rice <zooley@purdue.edu>
2013-03-04 14:43:07 -0500

    Added year to smaller people charts

--- components/com_support/views/tickets/tmpl/stats.php
+++ components/com_support/views/tickets/tmpl/stats.php
@@ -666,7 +666,7 @@ function getMonthName($month)
 								xaxis: { mode: "time", tickLength: 0, tickDecimals: 0,
 									tickFormatter: function (val, axis) {
 										var d = new Date(val);
-										return month_short[d.getUTCMonth()];//d.getUTCDate() + "/" + (d.getUTCMonth() + 1);
+										return month_short[d.getUTCMonth()] + ' \'' + d.getFullYear().toString().substring(2);//d.getUTCDate() + "/" + (d.getUTCMonth() + 1);
 									}
 								},
 								yaxis: { min: 0, max: <?php echo $top; ?> }
