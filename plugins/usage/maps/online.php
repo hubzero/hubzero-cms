22c7dd254289ca547d1ab6257701c0a7ed876fa3
Swaroop Shivarajapura <swaroop@purdue.edu>
2013-03-18 12:35:23 -0400

    new google maps api support

--- plugins/usage/maps/online.php
+++ plugins/usage/maps/online.php
@@ -42,80 +42,55 @@ $html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>
-	<script type="text/javascript" src="http://maps.google.com/maps?file=api&amp;v=2&amp;key='. $key .'"></script>
-	<script type="text/javascript" src="/components/'.$option.'/maps/js/Clusterer2.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key='. $key .'&sensor=false"> </script>
+	<script type="text/javascript" src="http://gmaps-samples-v3.googlecode.com/svn/trunk/xmlparsing/util.js"> </script>
+
 	<script type="text/javascript">
-	function load() 
-	{
-		if (GBrowserIsCompatible()) {
-			map = new GMap2(document.getElementById(\'div_map\'));
-			map.addControl(new GLargeMapControl());
-			map.addControl(new GMapTypeControl());
-			map.addControl(new GOverviewMapControl());
-			// map.setCenter(new GLatLng(".$lat.",".$long."), ".$zoom.", G_PHYSICAL_MAP);
-			// map.setCenter(new GLatLng(".$lat.",".$long."), ".$zoom.", G_SATELLITE_MAP);
-			map.setCenter(new GLatLng('. $lat .','. $long .'), '. $zoom .', G_HYBRID_MAP);
-			GEvent.addListener(map, \'click\', function(overlay, point) {
-				if (overlay){     // marker clicked
-					overlay.openInfoWindowHtml(overlay.infowindow);   // open InfoWindow
-				} else if (point) {
-					
-				}      // background clicked
-			});
-			getMarkers();
-		}
-	}
-	
-	function getMarkers() 
-	{'."\n";
-	if ($mappath) {
-		$html .= 'var urlstr=\''.$mappath.'/whoisonline.xml\';'."\n";
-	} else {
-		$html .= 'var urlstr="'.$dataurl.'";'."\n";
-	}
-$html .= '		var request = GXmlHttp.create();
-        request.open(\'GET\', urlstr , true); // request XML from PHP with AJAX call
-        request.onreadystatechange = function () {
-       		if (request.readyState == 4) {
-        		var xmlDoc = request.responseXML;
-            	locations = xmlDoc.documentElement.getElementsByTagName(\'marker\');
-            	markers = [];
-            	if (locations.length){
-		   			for (var i = 0; i < locations.length; i++) { // cycle thru locations
-						var info = locations[i].getAttribute(\'info\');
-						var bot = locations[i].getAttribute(\'bot\');
-						var icon = new GIcon();
-						if (bot == \'1\') {
-							icon.image = \'/plugins/usage/maps/images/markerB.png\';
-						} else {
-							icon.image = \'/plugins/usage/maps/images/markerU.png\';
-						}
-						icon.iconSize = new GSize(18, 30);
-						icon.iconAnchor = new GPoint(9, 30);
-						icon.infoWindowAnchor = new GPoint(9, 15);
-           				markers[i] = new GMarker(new GLatLng(locations[i].getAttribute(\'lat\'),locations[i].getAttribute(\'lng\')),icon);
-						var lat = locations[i].getAttribute(\'lat\');
-						var lng = locations[i].getAttribute(\'lng\');
-						var info = locations[i].getAttribute(\'info\');
-						info = info.replace(/_br_/g,\'<br/>\');
-						info = info.replace(/_hr_/g,\'<hr/>\');
-						info = info.replace(/_b_/g,\'<b>\');
-						info = info.replace(/_bb_/g,\'</b>\');
-						markers[i].infowindow = info.replace(/_br_/g,\'<br/>\');
-						map.addOverlay(markers[i]);
-						if (lat=='.$lat.' && lng=='.$long.'){
-							GEvent.trigger(markers[i], \'click\'); 
-						}
-		   			}
-				}
-	   		}
-     	}
-       request.send(null);
-	}
-	</script>
+
+  		var infowindow;
+  		var map;
+
+  		function initialize() {
+    		var myLatlng = new google.maps.LatLng('. $lat .','. $long .');
+    		var myOptions = {
+      		zoom: '. $zoom .',
+      		center: myLatlng,
+      		mapTypeId: google.maps.MapTypeId.ROADMAP
+    		}
+    		map = new google.maps.Map(document.getElementById("div_map"), myOptions);
+			'."\n";
+			if ($mappath) {
+				$html .= 'var urlstr=\''.$mappath.'/whoisonline.xml\';'."\n";
+			} else {
+				$html .= 'var urlstr="'.$dataurl.'";'."\n";
+			}
+
+			$html .= 'downloadUrl(urlstr, function(data) {
+      			var markers = data.documentElement.getElementsByTagName("marker");
+      			for (var i = 0; i < markers.length; i++) {
+        			var latlng = new google.maps.LatLng(parseFloat(markers[i].getAttribute("lat")), parseFloat(markers[i].getAttribute("lng")));
+        			var marker = createMarker(markers[i].getAttribute("info"), latlng);
+       			}
+     		});
+  		}
+
+		function createMarker(info, latlng) {
+     		info = info.replace(/_br_/g,\'<br/>\');
+     		info = info.replace(/_hr_/g,\'<hr/>\');
+     		info = info.replace(/_b_/g,\'<b>\');
+     		info = info.replace(/_bb_/g,\'</b>\');
+    		var marker = new google.maps.Marker({position: latlng, map: map});
+    		google.maps.event.addListener(marker, "click", function() {
+      			if (infowindow) infowindow.close();
+     			infowindow = new google.maps.InfoWindow({content: info});
+     			infowindow.open(map, marker);
+    		});
+    		return marker;
+  		}
+
+		</script>
  </head>
- <body onload="load()" onunload="GUnload()">
+ <body onload="initialize()">
 	<div id="div_map" style="width:100%; height:600px"></div>
  </body>
 </html>';
-
