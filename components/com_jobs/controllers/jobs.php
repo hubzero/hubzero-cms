8f1182bf9bad2252855f9d3b562145687aa0e5af
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2013-04-16 20:27:04 -0400

    remove call-time pass by references in com_jobs

--- components/com_jobs/controllers/jobs.php
+++ components/com_jobs/controllers/jobs.php
@@ -993,7 +993,7 @@ class JobsControllerJobs extends Hubzero_Controller
 		else 
 		{
 			// get service params like maxads
-			$this->getServiceParams(&$service);
+			$this->getServiceParams($service);
 		}
 
 		// Get current stats for dashboard
@@ -2197,7 +2197,7 @@ class JobsControllerJobs extends Hubzero_Controller
 		} 
 		else 
 		{
-			$this->getServiceParams(&$service);
+			$this->getServiceParams($service);
 			$maxads = $service->maxads > 0 ? $service->maxads : 1;
 			$activejobs = $job->countMyActiveOpenings($juser->get('id'), 1);
 			$allowed_ads = $maxads - $activejobs;
