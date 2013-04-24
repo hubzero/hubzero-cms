62510a6c00c67d0649ffadcd41e3d8eed3200fcc
Shawn Rice <zooley@purdue.edu>
2013-04-24 10:50:32 -0400

    Changed styling and rewrote JS to be a jquery plugin

--- plugins/hubzero/wikieditortoolbar/wikieditortoolbar.php
+++ plugins/hubzero/wikieditortoolbar/wikieditortoolbar.php
@@ -90,7 +90,7 @@ class plgHubzeroWikiEditorToolbar extends JPlugin
 	public function onDisplayEditor($name, $id, $content, $cls='wiki-toolbar-content', $col=10, $row=35)
 	{
 		$cls = ($cls) ? 'wiki-toolbar-content ' . $cls : 'wiki-toolbar-content';
-		$editor  = '<ul id="wiki-toolbar-' . $id . '" class="wiki-toolbar hidden"></ul>' . "\n";
+		$editor  = '<ul id="wiki-toolbar-' . $id . '" class="wiki-toolbar hide"></ul>' . "\n";
 		$editor .= '<textarea id="' . $id . '" name="' . $name . '" cols="' . $col . '" rows="' . $row . '" class="' . $cls . '">' . $content . '</textarea>' . "\n";
 
 		return $editor;
