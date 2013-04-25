ba1e754c97c4b5c17d196b77ab568c3a07bdbda6
Sam Wilson <samwilson@purdue.edu>
2013-04-25 16:15:13 -0400

    [COM_COURSES] add migrations for default roles and pages plugin

---
+++ migrations/Migration20130423204715ComCourses.php
@@ -0,0 +1,32 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for creating default member roles if none exist
+ **/
+class Migration20130423204715ComCourses extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query = "SELECT * FROM `#__courses_roles`";
+
+		$db->setQuery($query);
+
+		if (!$db->loadResult())
+		{
+			$query = "INSERT INTO `jos_courses_roles` (`offering_id`, `alias`, `title`, `permissions`)
+						VALUES
+							(0, 'instructor', 'Instructor', ''),
+							(0, 'manager', 'Manager', ''),
+							(0, 'student', 'Student', '');";
+
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+}
