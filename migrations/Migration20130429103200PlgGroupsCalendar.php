ecbe956b21a5541f82ed00e8d417a8bbace77628
Sam Wilson <samwilson@purdue.edu>
2013-04-29 10:56:33 -0400

    Minor updates to plg_groups_calendar migration script

--- migrations/Migration20130429103200PlgGroupsCalendar.php
+++ migrations/Migration20130429103200PlgGroupsCalendar.php
@@ -4,7 +4,7 @@
 defined('_JEXEC') or die('Restricted access');
 
 /**
- * Migration script for ...
+ * Migration script for new group calendar plugin
  **/
 class Migration20130429103200PlgGroupsCalendar extends Hubzero_Migration
 {
@@ -13,56 +13,59 @@ class Migration20130429103200PlgGroupsCalendar extends Hubzero_Migration
 	 **/
 	protected static function up($db)
 	{
+		$query = '';
+
 		// create event calendars table
-		$query = "CREATE TABLE `jos_events_calendars` (
-			         `id` int(11) NOT NULL AUTO_INCREMENT,
-			         `scope` varchar(100) DEFAULT NULL,
-			         `scope_id` int(11) DEFAULT NULL,
-			         `title` varchar(100) DEFAULT NULL,
-			         `color` varchar(100) DEFAULT NULL,
-			         `published` int(11) DEFAULT 1,
-			         PRIMARY KEY (`id`)
-			     ) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
-		
-		// add calendar_id, scope, and scope id to events so we can have them belong to other sections
-		$query .= "ALTER TABLE jos_events ADD COLUMN calendar_id int(11) AFTER catid;";
-		$query .= "ALTER TABLE jos_events ADD COLUMN scope VARCHAR(100) AFTER calendar_id;"
-		$query .= "ALTER TABLE jos_events ADD COLUMN scope_id INT(11)  AFTER scope;"
-		
-		// set scope on all current site events
-		$query .= "UPDATE jos_events SET scope='event' WHERE (scope IS NULL OR scope='');"
-		
-		// move group events to events table
-		$query .= "INSERT INTO jos_events(scope, scope_id, title, content, state, created, created_by, publish_up, publish_down)
-		           SELECT
-		               'group',
-		               gidNumber AS scope_id,
-		               title,
-		               details as content,
-		               active AS state,
-		               created, 
-		               actorid AS created,
-		               start AS publish_up,
-		               end AS publish_down
-		           FROM
-		               jos_xgroups_events;"
-		
-		// drop group events table
-		$query .= "DROP TABLE jos_xgroups_events";
-
-		if (!empty($query))
+		if (!$db->tableExists('#__events_calendars'))
 		{
-			$db->setQuery($query);
-			$db->query();
+			$query .= "CREATE TABLE `#__events_calendars` (
+							`id` int(11) NOT NULL AUTO_INCREMENT,
+							`scope` varchar(100) DEFAULT NULL,
+							`scope_id` int(11) DEFAULT NULL,
+							`title` varchar(100) DEFAULT NULL,
+							`color` varchar(100) DEFAULT NULL,
+							`published` int(11) DEFAULT 1,
+							PRIMARY KEY (`id`)
+						) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
 		}
-	}
 
-	/**
-	 * Down
-	 **/
-	protected static function down($db)
-	{
-		$query = "";
+		// add calendar_id, scope, and scope id to events so we can have them belong to other sections
+		if (!$db->tableHasField('#__events', 'calendar_id'))
+		{
+			$query .= "ALTER TABLE `#__events` ADD COLUMN calendar_id int(11) AFTER catid;\n";
+		}
+		if (!$db->tableHasField('#__events', 'scope'))
+		{
+			$query .= "ALTER TABLE `#__events` ADD COLUMN scope VARCHAR(100) AFTER calendar_id;\n";
+
+			// set scope on all current site events
+			$query .= "UPDATE `#__events` SET scope='event' WHERE (scope IS NULL OR scope='');";
+		}
+		if (!$db->tableHasField('#__events', 'scope_id'))
+		{
+			$query .= "ALTER TABLE `#__events` ADD COLUMN scope_id INT(11)  AFTER scope;\n";
+		}
+
+		if ($db->tableExists('#__xgroups_events'))
+		{
+			// move group events to events table
+			$query .= "INSERT INTO `#__events`(scope, scope_id, title, content, state, created, created_by, publish_up, publish_down)
+						SELECT
+							'group',
+							gidNumber AS scope_id,
+							title,
+							details as content,
+							active AS state,
+							created, 
+							actorid AS created,
+							start AS publish_up,
+							end AS publish_down
+						FROM
+							`#__xgroups_events`;";
+
+			// drop group events table
+			$query .= "DROP TABLE `#__xgroups_events`";
+		}
 
 		if (!empty($query))
 		{
