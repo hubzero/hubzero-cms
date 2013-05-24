a40e11df886b0484201e9b7b03b3b925180e89cc
Shawn Rice <zooley@purdue.edu>
2013-05-24 13:12:59 -0400

    Fix wrong datatype on column

---
+++ migrations/Migration20130524101101PlgCoursesDiscussions.php
@@ -0,0 +1,24 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for fixing wrong datatype on column
+ **/
+class Migration20130524101101PlgCoursesDiscussions extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query .= "ALTER TABLE #__courses_member_notes CHANGE COLUMN `timestamp` `timestamp` time NOT NULL DEFAULT '00:00:00';";
+
+		if (!empty($query))
+		{
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+}
