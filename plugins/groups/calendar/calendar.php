d4b5b46f74cb0732bc36a8b3faacb5ecf8d728cc
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2013-05-29 11:44:55 -0400

    Removed references to force login module in calendar group plugin. Now redirects to login page with return url.

--- plugins/groups/calendar/calendar.php
+++ plugins/groups/calendar/calendar.php
@@ -36,7 +36,7 @@ jimport('joomla.plugin.plugin');
 /**
  * Groups Plugin class for calendar
  */
-class plgGroupsCalendar extends JPlugin
+class plgGroupsCalendar extends Hubzero_Plugin
 {
 	/**
 	 * Constructor
@@ -135,10 +135,10 @@ class plgGroupsCalendar extends JPlugin
 			if ($juser->get('guest') 
 			 && ($group_plugin_acl == 'registered' || $group_plugin_acl == 'members')) 
 			{
-				ximport('Hubzero_Module_Helper');
-				$arr['html']  = '<p class="info">' . JText::sprintf('GROUPS_PLUGIN_REGISTERED', ucfirst($active)) . '</p>';
-				$arr['html'] .= Hubzero_Module_Helper::renderModules('force_mod');
-				return $arr;
+				$url = JRoute::_('index.php?option=com_groups&cn='.$group->get('cn').'&active='.$active);
+				$message = JText::sprintf('GROUPS_PLUGIN_REGISTERED', ucfirst($active));
+				$this->redirect( "/login?return=".base64_encode($url), $message, 'warning' );
+				return;
 			}
 
 			//check to see if user is member and plugin access requires members
