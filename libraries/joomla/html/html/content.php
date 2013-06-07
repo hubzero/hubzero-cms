81f3424adcb8ba7e39d37a822a0cc1121fcc4547
Shawn Rice <zooley@purdue.edu>
2013-06-07 14:19:47 -0400

    [JOOMLA] Autoloader causing admin com_content helper class to not be loaded due to having the same class name. Copied method to prevent errors being thrown.

--- libraries/joomla/html/html/content.php
+++ libraries/joomla/html/html/content.php
@@ -27,6 +27,57 @@ JLoader::register('JTableContent', JPATH_LIBRARIES . DS . 'joomla' . DS . 'datab
 class JHTMLContent
 {
 	/**
+	 * [HUBZERO] Copied from /administrator/components/com_content/helpers/content.php
+	 * Autoloader is loading *this* file first which is causing the helper file to nto be loaded
+	 *
+	 * Displays the publishing state legend for articles
+	 */
+	function Legend( )
+	{
+		?>
+		<table cellspacing="0" cellpadding="4" border="0" align="center">
+		<tr align="center">
+			<td>
+			<img src="images/publish_y.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Pending' ); ?>" />
+			</td>
+			<td>
+			<?php echo JText::_( 'Published, but is' ); ?> <u><?php echo JText::_( 'Pending' ); ?></u> |
+			</td>
+			<td>
+			<img src="images/publish_g.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Visible' ); ?>" />
+			</td>
+			<td>
+			<?php echo JText::_( 'Published and is' ); ?> <u><?php echo JText::_( 'Current' ); ?></u> |
+			</td>
+			<td>
+			<img src="images/publish_r.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Finished' ); ?>" />
+			</td>
+			<td>
+			<?php echo JText::_( 'Published, but has' ); ?> <u><?php echo JText::_( 'Expired' ); ?></u> |
+			</td>
+			<td>
+			<img src="images/publish_x.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Finished' ); ?>" />
+			</td>
+			<td>
+			<?php echo JText::_( 'Not Published' ); ?> |
+			</td>
+			<td>
+			<img src="images/disabled.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Archived' ); ?>" />
+			</td>
+			<td>
+			<?php echo JText::_( 'Archived' ); ?>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="10" align="center">
+			<?php echo JText::_( 'Click on icon to toggle state.' ); ?>
+			</td>
+		</tr>
+		</table>
+		<?php
+	}
+
+	/**
 	 * Fire onPrepareContent for content that isn't part of an article.
 	 *
 	 * @param string The content to be transformed.
