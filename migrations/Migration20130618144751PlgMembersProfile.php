fedd055578d3364f87ced325403d6b9ee9d49feb
Chris Smoak <csmoak@purdue.edu>
2013-06-18 11:51:27 -0400

    Migration for PlgMembersProfile

---
+++ migrations/Migration20130618144751PlgMembersProfile.php
@@ -0,0 +1,39 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20130618144751PlgMembersProfile extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query  = "ALTER TABLE `#__xprofiles` ALTER COLUMN `mailPreferenceOption` SET DEFAULT -1;";
+		$query .= "UPDATE `#__xprofiles` SET `mailPreferenceOption`=1 WHERE `mailPreferenceOption`=2;";
+
+		if (!empty($query))
+		{
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	protected static function down($db)
+	{
+		$query = "";
+
+		if (!empty($query))
+		{
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+}
