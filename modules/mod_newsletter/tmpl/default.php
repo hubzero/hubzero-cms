28f15890ef045696e0cdc14945f96d061a44397a
Chris Smoak <csmoak@purdue.edu>
2013-07-17 11:21:24 -0400

    fixed issue with newsletter mod without mailing list selected

--- modules/mod_newsletter/tmpl/default.php
+++ modules/mod_newsletter/tmpl/default.php
@@ -26,32 +26,38 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 
-<div class="mailinglist-details">
-	<span class="name">
-		<?php echo $this->mailinglist->name; ?>
-	</span>
-	<span class="description">
-		<?php echo nl2br($this->mailinglist->description); ?>
-	</span>
-</div>
+<?php if (is_object($this->mailinglist)) : ?>
+	<div class="mailinglist-details">
+		<span class="name">
+			<?php echo $this->mailinglist->name; ?>
+		</span>
+		<span class="description">
+			<?php echo nl2br($this->mailinglist->description); ?>
+		</span>
+	</div>
 
-<form class="mailinglist-signup" action="index.php" method="post">
-	<?php if (is_object($this->subscription)) : ?>
-		<span>It seems you are already subscribed to this mailing list. <a href="<?php echo JRoute::_('index.php?option=com_newsletter&task=subscribe'); ?>">Click here</a> to manage your newsletter mailing list subscriptions.</span>
-	<?php else : ?>
-		<label>Email Address: <span class="required">Required</span>
-			<input type="text" name="email_<?php echo JUtility::getToken(); ?>" id="email" value="<?php echo $this->juser->get('email'); ?>" />
-			<input type="hidden" name="list_<?php echo JUtility::getToken(); ?>" value="<?php echo $this->mailinglist->id; ?>" />
-		</label>
-		<label id="hp1">Honey Pot: <span class="optional">Please leave blank.</span>
-			<input type="text" name="hp1" value="" />
-		</label>
-		<input type="submit" value="Sign Up!" id="sign-up-submit" />
-		<input type="hidden" name="option" value="com_newsletter" />
-		<input type="hidden" name="controller" value="mailinglist" />
-		<input type="hidden" name="subscriptionid" value="<?php echo $this->subscriptionId; ?>" />
-		<input type="hidden" name="task" value="dosinglesubscribe" />
-		<input type="hidden" name="return" value="<?php echo base64_encode($_SERVER['REQUEST_URI']); ?>">
-		<?php echo JHTML::_( 'form.token' ); ?>
-	<?php endif; ?>
-</form>
+	<form class="mailinglist-signup" action="index.php" method="post">
+		<?php if (is_object($this->subscription)) : ?>
+			<span>It seems you are already subscribed to this mailing list. <a href="<?php echo JRoute::_('index.php?option=com_newsletter&task=subscribe'); ?>">Click here</a> to manage your newsletter mailing list subscriptions.</span>
+		<?php else : ?>
+			<label>Email Address: <span class="required">Required</span>
+				<input type="text" name="email_<?php echo JUtility::getToken(); ?>" id="email" value="<?php echo $this->juser->get('email'); ?>" />
+				<input type="hidden" name="list_<?php echo JUtility::getToken(); ?>" value="<?php echo $this->mailinglist->id; ?>" />
+			</label>
+			<label id="hp1">Honey Pot: <span class="optional">Please leave blank.</span>
+				<input type="text" name="hp1" value="" />
+			</label>
+			<input type="submit" value="Sign Up!" id="sign-up-submit" />
+			<input type="hidden" name="option" value="com_newsletter" />
+			<input type="hidden" name="controller" value="mailinglist" />
+			<input type="hidden" name="subscriptionid" value="<?php echo $this->subscriptionId; ?>" />
+			<input type="hidden" name="task" value="dosinglesubscribe" />
+			<input type="hidden" name="return" value="<?php echo base64_encode($_SERVER['REQUEST_URI']); ?>">
+			<?php echo JHTML::_( 'form.token' ); ?>
+		<?php endif; ?>
+	</form>
+<?php else : ?>
+	<p class="warning">
+		<?php echo JText::_('The newsletter mailing list module setup is not complete.'); ?>
+	</p>
+<?php endif; ?>
