02d85bc26cb57c05a2e2b1cef8dcc36343c42783
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2013-07-19 17:07:42 -0400

    [J25] bring back com_poll

---
+++ administrator/components/com_poll/poll.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* @version		$Id: poll.php 14401 2010-01-26 14:10:00Z louis $
+* @package		Joomla
+* @subpackage	Polls
+* @copyright	Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die('Restricted access');
+
+// Authorization check
+if (version_compare(JVERSION, '1.6', 'lt'))
+{
+	$jacl = JFactory::getACL();
+	$jacl->addACL($option, 'manage', 'users', 'super administrator');
+	$jacl->addACL($option, 'manage', 'users', 'administrator');
+	$jacl->addACL($option, 'manage', 'users', 'manager');
+
+	$user = JFactory::getUser();
+	if (!$user->authorize($option, 'manage'))
+	{
+		$app = JFactory::getApplication();
+		$app->redirect('index.php', JText::_('ALERTNOTAUTH'));
+	}
+}
+else 
+{
+	if (!JFactory::getUser()->authorise('core.manage', $option)) 
+	{
+		return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+	}
+}
+
+require_once(JPATH_COMPONENT . DS . 'controller.php');
+require_once(JPATH_COMPONENT . DS . 'helpers' . DS . 'poll.php');
+
+// Set the table directory
+JTable::addIncludePath(JPATH_COMPONENT . DS . 'tables');
+
+// Create the controller
+$controller	= new PollController();
+
+$controller->execute(JRequest::getCmd('task'));
+$controller->redirect();
---
+++ components/com_poll/views/poll/tmpl/default_graph.php
@@ -0,0 +1,51 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<div class="aside">
+	<p>
+		<strong><?php echo JText::_( 'Number of Voters' ); ?></strong><br />
+		<?php echo (isset($this->votes[0])) ? $this->votes[0]->voters : '--'; ?>
+	</p>
+	<p>
+		<strong><?php echo JText::_( 'First Vote' ); ?></strong><br />
+		<?php echo ($this->first_vote) ? $this->escape($this->first_vote) : '--'; ?>
+	</p>
+	<p>
+		<strong><?php echo JText::_( 'Last Vote' ); ?></strong><br />
+		<?php echo ($this->last_vote) ? $this->escape($this->last_vote) : '--'; ?>
+	</p>
+</div><!-- / .aside -->
+<div class="subject">
+<?php if ($this->poll->id) { ?>
+	<table class="pollresults" summary="<?php echo JText::_('Results for this pull'); ?>">
+		<thead>
+			<tr>
+				<th colspan="3" class="sectiontableheader">
+					<?php echo $this->escape($this->poll->title); ?>
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+	<?php foreach ($this->votes as $vote) : ?>
+			<tr class="sectiontableentry<?php echo $vote->odd; ?>">
+				<td>
+					<div class="graph">
+						<strong class="bar <?php echo $tdclass; ?>" style="width: <?php echo $this->escape($vote->percent); ?>%;"><span><?php echo $this->escape($vote->percent); ?>%</span></strong>
+					</div>
+				</td>
+				<td>
+					<?php echo $this->escape(stripslashes($vote->text)); ?>
+				</td>
+				<td class="votes">
+					<?php echo $this->escape($vote->hits); ?>
+				</td>
+			</tr>
+	<?php endforeach; ?>
+		</tbody>
+	</table>
+<?php } else { ?>
+	<p>
+		<?php echo JText::_('Select a poll from the list.'); ?>
+	</p>
+<?php } ?>
+</div><!-- / .subject -->
