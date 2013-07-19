1ae6def129343ad4000eb99da96ce9a3d6878b25
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2013-07-19 17:07:52 -0400

    [J25] bring back mod_poll

---
+++ modules/mod_poll/helper.php
@@ -0,0 +1,70 @@
+<?php
+/**
+* @version		$Id: helper.php 14401 2010-01-26 14:10:00Z louis $
+* @package		Joomla
+* @copyright	Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+class modPollHelper
+{
+	function getPoll($id)
+	{
+		$db		=& JFactory::getDBO();
+		$result	= null;
+
+		if ($id)
+		{
+			$query = 'SELECT id, title,'
+				.' CASE WHEN CHAR_LENGTH(alias) THEN CONCAT_WS(\':\', id, alias) ELSE id END as slug '
+				.' FROM #__polls'
+				.' WHERE id = '.(int) $id
+				.' AND published = 1'
+				;
+		}
+		else 
+		{
+			$query = 'SELECT id, title,'
+				.' CASE WHEN CHAR_LENGTH(alias) THEN CONCAT_WS(\':\', id, alias) ELSE id END as slug '
+				.' FROM #__polls'
+				.' WHERE published = 1 AND open = 1 ORDER BY id DESC Limit 1'
+				;
+		}
+		$db->setQuery($query);
+		$result = $db->loadObject();
+
+		if ($db->getErrorNum()) {
+			JError::raiseWarning( 500, $db->stderr() );
+		}
+
+		return $result;
+	}
+
+	function getPollOptions($id)
+	{
+		$db	=& JFactory::getDBO();
+
+		$query = 'SELECT id, text' .
+			' FROM #__poll_data' .
+			' WHERE pollid = ' . (int) $id .
+			' AND text <> ""' .
+			' ORDER BY id';
+		$db->setQuery($query);
+
+		if (!($options = $db->loadObjectList())) {
+			echo "MD ".$db->stderr();
+			return;
+		}
+
+		return $options;
+	}
+}
+?>
---
+++ modules/mod_poll/tmpl/default.php
@@ -0,0 +1,31 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<form id="<?php echo ($params->get('moduleclass_sfx')) ? $params->get('moduleclass_sfx') : 'poll' . rand(); ?>" method="post" action="<?php echo JRoute::_('index.php?option=com_poll'); ?>">
+	<fieldset>
+		<h4><?php echo $poll->title; ?></h4>
+		<ul class="poll">
+	<?php for ($i = 0, $n = count($options); $i < $n; $i ++) : ?>
+			<li class="<?php echo $tabclass_arr[$tabcnt]; ?><?php echo $params->get('moduleclass_sfx'); ?>">
+				<input type="radio" name="voteid" id="voteid<?php echo $options[$i]->id;?>" value="<?php echo $options[$i]->id;?>" alt="<?php echo $options[$i]->id;?>" />
+				<label for="voteid<?php echo $options[$i]->id; ?>" class="<?php echo $tabclass_arr[$tabcnt]; ?><?php echo $params->get('moduleclass_sfx'); ?>">
+					<?php echo stripslashes($options[$i]->text); ?>
+				</label>
+			</li>
+			<?php
+				$tabcnt = 1 - $tabcnt;
+			?>
+	<?php endfor; ?>
+		</ul>
+		<p>
+			<input type="submit" name="task_button" class="button" value="<?php echo JText::_('Vote'); ?>" />
+			 &nbsp; 
+			<a href="<?php echo JRoute::_("index.php?option=com_poll&view=poll&id=" . $poll->slug); ?>"><?php echo JText::_('Results...'); ?></a>
+		</p>
+
+		<input type="hidden" name="option" value="com_poll" />
+		<input type="hidden" name="task" value="vote" />
+		<input type="hidden" name="id" value="<?php echo $poll->id;?>" />
+		<?php echo JHTML::_( 'form.token' ); ?>
+	</fieldset>
+</form>
