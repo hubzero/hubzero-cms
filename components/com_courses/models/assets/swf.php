ec0fbab9c981c02148d997fb913ec2148776a3cb
Sam Wilson <samwilson@purdue.edu>
2013-07-25 14:02:17 -0400

    [COM_COURSES] add minimal swf support

---
+++ components/com_courses/models/assets/swf.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Sam Wilson <samwilson@purdue.edu>
+ * @copyright Copyright 2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+* Video Asset handler class
+*/
+class SwfAssetHandler extends FileAssetHandler
+{
+	/**
+	 * Class info
+	 *
+	 * Action message - what the user will see if presented with multiple handlers for this extension
+	 * Responds to    - what extensions this handler responds to
+	 *
+	 * @var array
+	 **/
+	protected static $info = array(
+			'action_message' => 'As an Adobe Shockwave Flash file',
+			'responds_to'    => array('swf'),
+		);
+
+	/**
+	 * Create method for this handler
+	 *
+	 * @return array of assets created
+	 **/
+	public function create()
+	{
+		// Set the asset type to video
+		$this->asset['type']    = 'file';
+		$this->asset['subtype'] = 'swf';
+
+		// Call the primary create method on the file asset handler
+		return parent::create();
+	}
+}
---
+++ components/com_courses/views/assets/tmpl/file_swf.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ * All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+?>
+
+<div id="content-header">
+	<h2><?= $this->asset->title ?></h2>
+</div>
+
+<object type="application/x-shockwave-flash" width="100%" height="100%"> 
+	<param name="movie" value="<?php echo JRoute::_($this->model->path($this->course->get('id'))); ?>"></param> 
+	<param name="wmode" value="opaque"></param> 
+</object>
