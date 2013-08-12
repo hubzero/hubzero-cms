181c45249a351c6659a38ef8342b4e8040c6bdb0
Ilya Shunko <ishunko@purdue.edu>
2013-08-12 16:37:08 -0400

    Migrations for com_cart & com_storefront

---
+++ migrations/Migration20130812182339ComCart.php
@@ -0,0 +1,221 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20130812182339ComCart extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		/*Table structure for table `#__cart` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart` (
+		  `id` int(10) NOT NULL AUTO_INCREMENT,
+		  `uid` int(11) NOT NULL DEFAULT '0',
+		  `itemid` int(11) NOT NULL DEFAULT '0',
+		  `type` varchar(20) DEFAULT NULL,
+		  `quantity` int(11) NOT NULL DEFAULT '0',
+		  `added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+		  `selections` text,
+		  PRIMARY KEY (`id`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+
+		/*Table structure for table `#__cart_cart_items` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_cart_items` (
+		  `crtId` int(16) NOT NULL,
+		  `sId` int(16) NOT NULL,
+		  `crtiQty` int(5) DEFAULT NULL,
+		  `crtiOldQty` int(5) DEFAULT NULL,
+		  `crtiPrice` decimal(10,2) DEFAULT NULL,
+		  `crtiOldPrice` decimal(10,2) DEFAULT NULL,
+		  `crtiName` varchar(255) DEFAULT NULL,
+		  `crtiAvailable` tinyint(1) DEFAULT '1',
+		  PRIMARY KEY (`crtId`,`sId`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__cart_carts` */		
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_carts` (
+		  `crtId` int(16) NOT NULL AUTO_INCREMENT,
+		  `crtCreated` datetime DEFAULT NULL,
+		  `crtLastUpdated` datetime DEFAULT NULL,
+		  `uidNumber` int(16) DEFAULT NULL,
+		  PRIMARY KEY (`crtId`),
+		  UNIQUE KEY `uidNumber` (`uidNumber`)
+		) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__cart_coupons` */		
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_coupons` (
+		  `crtId` int(16) NOT NULL,
+		  `cnId` int(16) NOT NULL,
+		  `crtCnAdded` datetime DEFAULT NULL,
+		  `crtCnStatus` char(15) DEFAULT NULL
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__cart_memberships` */		
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_memberships` (
+		  `crtmId` int(16) NOT NULL AUTO_INCREMENT,
+		  `pId` int(16) DEFAULT NULL,
+		  `crtId` int(16) DEFAULT NULL,
+		  `crtmExpires` datetime DEFAULT NULL,
+		  PRIMARY KEY (`crtmId`),
+		  UNIQUE KEY `pId` (`pId`,`crtId`)
+		) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__cart_saved_addresses` */		
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_saved_addresses` (
+		  `saId` int(16) NOT NULL AUTO_INCREMENT,
+		  `uidNumber` int(16) NOT NULL,
+		  `saToFirst` char(100) NOT NULL,
+		  `saToLast` char(100) NOT NULL,
+		  `saAddress` char(255) NOT NULL,
+		  `saCity` char(25) NOT NULL,
+		  `saState` char(2) NOT NULL,
+		  `saZip` char(10) NOT NULL,
+		  PRIMARY KEY (`saId`),
+		  UNIQUE KEY `uidNumber` (`uidNumber`,`saToFirst`,`saToLast`,`saAddress`,`saCity`,`saState`,`saZip`)
+		) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__cart_transaction_info` */		
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_transaction_info` (
+		  `tId` int(16) NOT NULL,
+		  `tiShippingToFirst` char(100) DEFAULT NULL,
+		  `tiShippingToLast` char(100) DEFAULT NULL,
+		  `tiShippingAddress` char(255) DEFAULT NULL,
+		  `tiShippingCity` char(25) DEFAULT NULL,
+		  `tiShippingState` char(2) DEFAULT NULL,
+		  `tiShippingZip` char(10) DEFAULT NULL,
+		  `tiTotal` decimal(10,2) DEFAULT NULL,
+		  `tiSubtotal` decimal(10,2) DEFAULT NULL,
+		  `tiTax` decimal(10,2) DEFAULT NULL,
+		  `tiShipping` decimal(10,2) DEFAULT NULL,
+		  `tiShippingDiscount` decimal(10,2) DEFAULT NULL,
+		  `tiDiscounts` decimal(10,2) DEFAULT NULL,
+		  `tiItems` text,
+		  `tiPerks` text,
+		  `tiMeta` text,
+		  `tiCustomerStatus` char(15) DEFAULT 'unconfirmed',
+		  PRIMARY KEY (`tId`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__cart_transaction_items` */		
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_transaction_items` (
+		  `tId` int(16) NOT NULL,
+		  `sId` int(16) NOT NULL,
+		  `tiQty` int(5) DEFAULT NULL,
+		  `tiPrice` decimal(10,2) DEFAULT NULL,
+		  PRIMARY KEY (`tId`,`sId`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__cart_transaction_steps` */		
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_transaction_steps` (
+		  `tsId` int(16) NOT NULL AUTO_INCREMENT,
+		  `tId` int(16) NOT NULL,
+		  `tsStep` char(16) NOT NULL,
+		  `tsStatus` tinyint(1) DEFAULT '0',
+		  PRIMARY KEY (`tsId`),
+		  UNIQUE KEY `tId` (`tId`,`tsStep`)
+		) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__cart_transactions` */		
+		$query = "CREATE TABLE IF NOT EXISTS `#__cart_transactions` (
+		  `tId` int(16) NOT NULL AUTO_INCREMENT,
+		  `crtId` int(16) DEFAULT NULL,
+		  `tCreated` datetime DEFAULT NULL,
+		  `tLastUpdated` datetime DEFAULT NULL,
+		  `tStatus` char(32) DEFAULT NULL,
+		  PRIMARY KEY (`tId`)
+		) ENGINE=MyISAM AUTO_INCREMENT=44 DEFAULT CHARSET=utf8";
+		
+		$db->setQuery($query);
+		$db->query();
+				
+	}
+
+	/**
+	 * Down
+	 **/
+	protected static function down($db)
+	{
+		$query = "DROP TABLE IF EXISTS `#__cart`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_cart_items`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_carts`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_coupons`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_memberships`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_saved_addresses`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_transaction_info`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_transaction_items`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_transaction_steps`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__cart_transactions`";
+
+		$db->setQuery($query);
+		$db->query();
+	}
+}
---
+++ migrations/Migration20130812201731ComStorefront.php
@@ -0,0 +1,262 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20130812201731ComStorefront extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query = "";
+
+		if (!empty($query))
+		{
+			$db->setQuery($query);
+			$db->query();
+		}
+		
+		/*Table structure for table `#__storefront_collections` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_collections` (
+		  `cId` char(50) NOT NULL,
+		  `cName` varchar(64) DEFAULT NULL,
+		  `cParent` int(16) DEFAULT NULL,
+		  `cActive` tinyint(1) DEFAULT NULL,
+		  `cType` char(10) DEFAULT NULL,
+		  PRIMARY KEY (`cId`),
+		  KEY `cActive` (`cActive`),
+		  KEY `cParent` (`cParent`)
+		) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_coupon_actions` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_coupon_actions` (
+		  `cnId` int(16) NOT NULL,
+		  `cnaAction` char(25) DEFAULT NULL,
+		  `cnaVal` char(255) DEFAULT NULL,
+		  UNIQUE KEY `cnId` (`cnId`,`cnaAction`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_coupon_conditions` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_coupon_conditions` (
+		  `cnId` int(16) NOT NULL,
+		  `cncRule` char(100) DEFAULT NULL,
+		  `cncVal` char(255) DEFAULT NULL
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_coupon_objects` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_coupon_objects` (
+		  `cnId` int(16) NOT NULL,
+		  `cnoObjectId` int(16) DEFAULT NULL,
+		  `cnoObjectsLimit` int(5) DEFAULT '0' COMMENT 'How many objects can be applied to. 0 - unlimited',
+		  UNIQUE KEY `cnId` (`cnId`,`cnoObjectId`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_coupons` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_coupons` (
+		  `cnId` int(16) NOT NULL AUTO_INCREMENT,
+		  `cnCode` char(25) DEFAULT NULL,
+		  `cnDescription` char(255) DEFAULT NULL,
+		  `cnExpires` date DEFAULT NULL,
+		  `cnUseLimit` int(5) unsigned DEFAULT NULL,
+		  `cnObject` char(15) NOT NULL,
+		  `cnActive` tinyint(1) DEFAULT '1',
+		  PRIMARY KEY (`cnId`),
+		  UNIQUE KEY `Unique code` (`cnCode`)
+		) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_option_groups` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_option_groups` (
+		  `ogId` int(16) NOT NULL AUTO_INCREMENT,
+		  `ogName` char(16) DEFAULT NULL,
+		  PRIMARY KEY (`ogId`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_options` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_options` (
+		  `oId` int(16) NOT NULL AUTO_INCREMENT,
+		  `ogId` int(16) DEFAULT NULL COMMENT 'Foreign key to option-groups',
+		  `oName` char(255) DEFAULT NULL,
+		  PRIMARY KEY (`oId`),
+		  UNIQUE KEY `ogId` (`ogId`,`oName`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_product_collections` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_product_collections` (
+		  `cllId` int(16) NOT NULL AUTO_INCREMENT,
+		  `pId` int(16) NOT NULL,
+		  `cId` char(50) NOT NULL,
+		  PRIMARY KEY (`cllId`,`pId`,`cId`),
+		  UNIQUE KEY `pId` (`pId`,`cId`)
+		) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_product_option_groups` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_product_option_groups` (
+		  `pId` int(16) NOT NULL,
+		  `ogId` int(16) NOT NULL,
+		  PRIMARY KEY (`pId`,`ogId`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_product_types` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_product_types` (
+		  `ptId` int(16) NOT NULL AUTO_INCREMENT,
+		  `ptName` char(128) DEFAULT NULL,
+		  `ptModel` char(25) DEFAULT 'normal',
+		  PRIMARY KEY (`ptId`)
+		) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_products` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_products` (
+		  `pId` int(16) NOT NULL AUTO_INCREMENT,
+		  `ptId` int(16) NOT NULL COMMENT 'Product type ID. Foreign key to product_types table',
+		  `pName` char(128) DEFAULT NULL,
+		  `pTagline` tinytext,
+		  `pDescription` text,
+		  `pFeatures` text,
+		  `pActive` tinyint(1) DEFAULT '1',
+		  PRIMARY KEY (`pId`),
+		  KEY `pActive` (`pActive`)
+		) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_sku_meta` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_sku_meta` (
+		  `smId` int(16) NOT NULL AUTO_INCREMENT,
+		  `sId` int(16) NOT NULL,
+		  `smKey` varchar(100) DEFAULT NULL,
+		  `smValue` varchar(100) DEFAULT NULL,
+		  PRIMARY KEY (`smId`),
+		  UNIQUE KEY `sId` (`sId`,`smKey`)
+		) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_sku_options` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_sku_options` (
+		  `sId` int(16) NOT NULL,
+		  `oId` int(16) NOT NULL,
+		  PRIMARY KEY (`sId`,`oId`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		$db->setQuery($query);
+		$db->query();
+		
+		/*Table structure for table `#__storefront_skus` */
+		$query = "CREATE TABLE IF NOT EXISTS `#__storefront_skus` (
+		  `sId` int(16) NOT NULL AUTO_INCREMENT,
+		  `pId` int(16) DEFAULT NULL COMMENT 'Foreign key to products',
+		  `sSku` char(16) DEFAULT NULL,
+		  `sWeight` decimal(10,2) DEFAULT NULL,
+		  `sPrice` decimal(10,2) DEFAULT NULL,
+		  `sDescriprtion` text,
+		  `sFeatures` text,
+		  `sTrackInventory` tinyint(1) DEFAULT '0',
+		  `sInventory` int(11) DEFAULT '0',
+		  `sEnumerable` tinyint(1) DEFAULT '1',
+		  `sAllowMultiple` tinyint(1) DEFAULT '1',
+		  `sActive` tinyint(1) DEFAULT '1',
+		  PRIMARY KEY (`sId`)
+		) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8";
+
+	}
+
+	/**
+	 * Down
+	 **/
+	protected static function down($db)
+	{
+		$query = "DROP TABLE IF EXISTS `#__storefront_collections`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_coupon_actions`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_coupon_conditions`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_coupon_objects`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_coupons`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_option_groups`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_options`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_product_collections`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_product_option_groups`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_product_types`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_products`";
+
+		$db->setQuery($query);
+		$db->query();
+
+		$query = "DROP TABLE IF EXISTS `#__storefront_sku_meta`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_sku_options`";
+
+		$db->setQuery($query);
+		$db->query();
+		
+		$query = "DROP TABLE IF EXISTS `#__storefront_skus`";
+
+		$db->setQuery($query);
+		$db->query();
+	}
+}
