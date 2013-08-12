ba288ca21c29e3447d055ef97948bd455ddd02b4
Sam Wilson <samwilson@purdue.edu>
2013-08-12 13:07:02 -0400

    Make migration rerunable, especially on nanoHUB

--- migrations/Migration20130801182039ComPdf2form.php
+++ migrations/Migration20130801182039ComPdf2form.php
@@ -15,16 +15,45 @@ class Migration20130801182039ComPdf2form extends Hubzero_Migration
 	{
 		if ($db->tableExists('#__pdf_forms'))
 		{
-			foreach (array(
-				'create unique index jos_pdf_form_deployments_crumb_uidx on jos_pdf_form_deployments(crumb)',
-				'create index jos_pdf_form_responses_respondent_id_idx on jos_pdf_form_responses(respondent_id)',
-				'create index jos_pdf_form_responses_question_id_idx on jos_pdf_form_responses(question_id)',
-				'create index jos_pdf_form_responses_answer_id_idx on jos_pdf_form_responses(answer_id)',
-				'create index jos_pdf_form_answers_question_id_idx on jos_pdf_form_answers(question_id)',
-				'create index jos_pdf_form_respondents_user_id_idx on jos_pdf_form_respondents(user_id)',
-				'create index jos_pdf_form_respondents_deployment_id_idx on jos_pdf_form_respondents(deployment_id)'
-			) as $query) 
+			if (!$db->tableHasKey('#__pdf_form_deployments', 'jos_pdf_form_deployments_crumb_uidx'))
 			{
+				$query = "create unique index jos_pdf_form_deployments_crumb_uidx on #__pdf_form_deployments(crumb)";
+				$db->setQuery($query);
+				$db->query();
+			}
+			if (!$db->tableHasKey('#__pdf_form_responses', 'jos_pdf_form_responses_respondent_id_idx'))
+			{
+				$query = "create index jos_pdf_form_responses_respondent_id_idx on #__pdf_form_responses(respondent_id)";
+				$db->setQuery($query);
+				$db->query();
+			}
+			if (!$db->tableHasKey('#__pdf_form_responses', 'jos_pdf_form_responses_question_id_idx'))
+			{
+				$query = "create index jos_pdf_form_responses_question_id_idx on #__pdf_form_responses(question_id)";
+				$db->setQuery($query);
+				$db->query();
+			}
+			if (!$db->tableHasKey('#__pdf_form_responses', 'jos_pdf_form_responses_answer_id_idx'))
+			{
+				$query = "create index jos_pdf_form_responses_answer_id_idx on #__pdf_form_responses(answer_id)";
+				$db->setQuery($query);
+				$db->query();
+			}
+			if (!$db->tableHasKey('#__pdf_form_answers', 'jos_pdf_form_answers_question_id_idx'))
+			{
+				$query = "create index jos_pdf_form_answers_question_id_idx on #__pdf_form_answers(question_id)";
+				$db->setQuery($query);
+				$db->query();
+			}
+			if (!$db->tableHasKey('#__pdf_form_respondents', 'jos_pdf_form_respondents_user_id_idx'))
+			{
+				$query = "create index jos_pdf_form_respondents_user_id_idx on #__pdf_form_respondents(user_id)";
+				$db->setQuery($query);
+				$db->query();
+			}
+			if (!$db->tableHasKey('#__pdf_form_respondents', 'jos_pdf_form_respondents_deployment_id_idx'))
+			{
+				$query = "create index jos_pdf_form_respondents_deployment_id_idx on #__pdf_form_respondents(deployment_id)";
 				$db->setQuery($query);
 				$db->query();
 			}
