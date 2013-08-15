5908cc6558420658a606bb6d580ffaa367d13e2b
Chris Smoak <csmoak@purdue.edu>
2013-08-15 14:21:31 -0400

    [TPL_HUBBASIC2012] Fixed mobile styles in hubbasic 2012 template

--- templates/hubbasic2012/js/mobile.js
+++ templates/hubbasic2012/js/mobile.js
@@ -11,15 +11,21 @@ HUB.Mobile = {
 	
 	init: function()
 	{
-		if( $("#true-menu").length )
+		if( $("#nav").length )
 		{
 			var select = "",
-				menu = $("#true-menu").children("ul");
+				menu = $("#nav").children("ul");
 			
 			menu.children("li").each(function(i, el) {
 				var menuItem = $(el),
 					name = menuItem.children("a").children("span").html(),
 					link = menuItem.children("a").attr("href");
+					
+				//support for joomla 2.5
+				if ($('html').hasClass('joomla2.5'))
+				{
+					name = menuItem.children("a").html();
+				}
 				
 				selected = (menuItem.hasClass("active")) ? "selected" : "";
 				select += "<option " + selected + " value=\"" + link + "\">" + name + "</option>";
@@ -32,6 +38,13 @@ HUB.Mobile = {
 						var subMenuItem = $(el),
 							name = subMenuItem.children("a").children("span").html(),
 							link = subMenuItem.children("a").attr("href");
+						
+						//support for joomla 2.5
+						if ($('html').hasClass('joomla2.5'))
+						{
+							name = subMenuItem.children("a").html();
+						}
+						
 						if(name != "" && name != null)
 						{
 							select += "<option " + selected + " value=\"" + link + "\"> &mdash; " + name + "</option>";
@@ -41,10 +54,10 @@ HUB.Mobile = {
 			});
 			
 			//set the content of the select box
-			$("#menu").html(select);
+			$("#mobile-nav").html(select);
 			
 			//add on change event to go to new link after changing
-			$("#menu").on("change", function() {
+			$("#mobile-nav").on("change", function() {
 				var index = this.selectedIndex,
 					href  = this.options[index].value;
 					
--- templates/hubbasic2012/mobile.php
+++ templates/hubbasic2012/mobile.php
@@ -13,36 +13,86 @@ $this->template = 'hubbasic2012';
 
 //get device info
 $hd = new Hubzero_Device();
+
+//get joomla version
+$joomlaVersion = new JVersion();
+$joomlaRelease = 'joomla' . $joomlaVersion->RELEASE;
 ?>
 <!DOCTYPE html>
-<html class="<?php echo strtolower($hd->getDeviceFamily() . ' ' . $hd->getDeviceOS() . ' ' . $hd->getDeviceOSVersion()); ?>">
+<html class="<?php echo strtolower($hd->getDeviceFamily() . ' ' . $hd->getDeviceOS() . ' ' . $hd->getDeviceOSVersion()); ?> <?php echo $joomlaRelease; ?>">
 	<head>
-		<jdoc:include type="head" />
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo Hubzero_Document::getSystemStylesheet(array('reset', 'fontcons', 'columns', 'notifications')); /* reset MUST come before all others except fontcons */ ?>" />
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo Hubzero_Document::getSystemStylesheet(array('fontcons', 'reset', 'columns', 'notifications', 'pagination', 'tabs', 'tags', 'tooltip', 'comments', 'voting', 'icons', 'buttons', 'layout')); /* reset MUST come before all others except fontcons */ ?>" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/main.css" />
 		<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/mobile.css" />
+		<jdoc:include type="head" />
 		<script src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/js/mobile.js"></script>
 	</head>
 	<body>
-		<div id="mobile-header">
-			<h1>
-				<a href="<?php echo $this->baseurl; ?>" title="<?php echo $config->getValue('config.sitename'); ?>">
-					<span><?php echo $config->getValue('config.sitename'); ?></span>
-					<span class="tagline"><?php echo JText::_('TPL_HUBBASIC_TAGLINE'); ?></span>
-				</a>
-			</h1>
-			
-			<div id="true-menu">
-				<jdoc:include type="modules" name="user3" />
-			</div>
-			<select name="menu" id="menu">
-			</select>
-		</div>
-		<jdoc:include type="message" />
-		<jdoc:include type="component" />
-		
-		<div id="mobile-footer">
+		<jdoc:include type="modules" name="notices" />
+		<jdoc:include type="modules" name="helppane" />
+		<div id="top" class="mobile-top">
+			<div class="inner-wrap">
+				<div class="inner">
+					<div id="masthead" role="banner">
+						<div class="inner">
+							<h1>
+								<a href="<?php echo $this->baseurl; ?>" title="<?php echo $config->getValue('config.sitename'); ?>">
+									<span><?php echo $config->getValue('config.sitename'); ?></span>
+								</a>
+								<span class="tagline"><?php echo JText::_('TPL_HUBBASIC_TAGLINE'); ?></span>
+							</h1>
+							
+							<div class="mobile-search">
+								<jdoc:include type="modules" name="search" />
+							</div>
+							
+							<div id="nav" role="main navigation">
+								<a name="nav"></a>
+								<jdoc:include type="modules" name="user3" />
+							</div><!-- / #nav -->
+							<select name="menu" id="mobile-nav">
+							</select>
+						</div><!-- / .inner -->
+					</div><!-- / #header -->
+				</div><!-- / .inner -->
+			</div><!-- / .inner-wrap -->
+		</div><!-- / #top -->
+		<div id="wrap" class="mobile-wrap">
+			<div id="content" class="<?php echo JRequest::getVar('option', ''); ?>" role="main">
+				<div class="inner">
+					<a name="content" id="content-anchor"></a>
+<?php if ($this->countModules('left')) : ?>
+					<div class="main section withleft">
+						<div class="aside">
+							<jdoc:include type="modules" name="left" />
+						</div><!-- / #column-left -->
+						<div class="subject">
+<?php endif; ?>
+<?php if ($this->countModules('right')) : ?>
+					<div class="main section">
+						<div class="aside">
+							<jdoc:include type="modules" name="right" />
+						</div><!-- / .aside -->
+						<div class="subject">
+<?php endif; ?>
+							<!-- start component output -->
+							<jdoc:include type="component" />
+							<!-- end component output -->
+<?php if ($this->countModules('left or right')) : ?>
+						</div><!-- / .subject -->
+						<div class="clear"></div>
+					</div><!-- / .main section -->
+<?php endif; ?>
+				</div><!-- / .inner -->
+			</div><!-- / #content -->
+		</div><!-- / #wrap -->
+
+		<div id="footer" class="mobile-footer">
+			<a name="footer" id="footer-anchor"></a>
 			<a href="?tmpl=fullsite">View Full Site</a>
-		</div>
+		</div><!-- / #footer -->
+
+		<jdoc:include type="modules" name="endpage" />
 	</body>
 </html>
 	
