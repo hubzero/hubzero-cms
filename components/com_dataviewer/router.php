77d62577ce71e8a8e197119d12af8233f2455312
Shawn Rice <zooley@purdue.edu>
2013-08-29 13:34:57 -0400

    [BC] Added buildSefRoute method to router

--- components/com_dataviewer/router.php
+++ components/com_dataviewer/router.php
@@ -10,17 +10,37 @@
 
 defined('_JEXEC') or die;
 
+/**
+ * Turn querystring parameters into an SEF route
+ * 
+ * @param  array &$query Querystring bits
+ * @return array 
+ */
+function dataviewerBuildRoute(&$query)
+{
+	$segments = array();
 
-function dataviewerParseRoute($segments) {
+	return $segments;
+}
+
+/**
+ * Parse a SEF route
+ * 
+ * @param  array $segments Exploded SEF URL
+ * @return array 
+ */
+function dataviewerParseRoute($segments) 
+{
 	$vars = array();
 
-	if (empty($segments)) {
+	if (empty($segments)) 
+	{
 		return $vars;
 	}
 
-	$vars['task'] =  isset($segments[0]) ? $segments[0] : 'view';
-	$vars['db'] = isset($segments[1]) ? $segments[1] : false;
-	$vars['dv'] = isset($segments[2]) ? $segments[2] : false;
+	$vars['task'] = isset($segments[0]) ? $segments[0] : 'view';
+	$vars['db']   = isset($segments[1]) ? $segments[1] : false;
+	$vars['dv']   = isset($segments[2]) ? $segments[2] : false;
 
 	return $vars;
 }
