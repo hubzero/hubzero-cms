fa58630384707b1c89cc44b17a69e20b937ffcf2
Sam Wilson <samwilson@purdue.edu>
2013-09-09 10:30:45 -0400

    [COM_COURSES] one more bug fix in migration to allow re-runs

--- migrations/Migration20130905195600ComCourses.php
+++ migrations/Migration20130905195600ComCourses.php
@@ -13,42 +13,42 @@ class Migration20130905195600ComCourses extends Hubzero_Migration
 	 **/
 	protected static function up($db)
 	{
-		// Fix gradebook entires
-		$query = "SELECT * FROM `#__courses_grade_book`";
-		$db->setQuery($query);
-		$results = $db->loadObjectList();
-
-		if ($results && count($results) > 0)
+		if ($db->tableHasField('#__courses_grade_book', 'user_id'))
 		{
-			foreach ($results as $r)
+			// Fix gradebook entires
+			$query = "SELECT * FROM `#__courses_grade_book`";
+			$db->setQuery($query);
+			$results = $db->loadObjectList();
+
+			if ($results && count($results) > 0)
 			{
-				switch ($r->scope)
+				foreach ($results as $r)
 				{
-					case 'asset':
-						$query = "SELECT `course_id` FROM `#__courses_assets` WHERE `id` = '{$r->scope_id}'";
-						$db->setQuery($query);
-						$course_id = $db->loadResult();
-					break;
+					switch ($r->scope)
+					{
+						case 'asset':
+							$query = "SELECT `course_id` FROM `#__courses_assets` WHERE `id` = '{$r->scope_id}'";
+							$db->setQuery($query);
+							$course_id = $db->loadResult();
+						break;
 
-					case 'unit':
-						$query = "SELECT `course_id` FROM `#__courses_units` AS cu, `#__courses_offerings` AS co WHERE cu.offering_id = co.id AND cu.id = '{$r->scope_id}'";
-						$db->setQuery($query);
-						$course_id = $db->loadResult();
-					break;
+						case 'unit':
+							$query = "SELECT `course_id` FROM `#__courses_units` AS cu, `#__courses_offerings` AS co WHERE cu.offering_id = co.id AND cu.id = '{$r->scope_id}'";
+							$db->setQuery($query);
+							$course_id = $db->loadResult();
+						break;
 
-					case 'course':
-						$course_id = $r->scope_id;
-					break;
-				}
+						case 'course':
+							$course_id = $r->scope_id;
+						break;
+					}
 
-				$query  = "SELECT `id` FROM `#__courses_members` WHERE `user_id` = '{$r->user_id}'";
-				$query .= " AND `course_id` = '{$course_id}'";
-				$query .= " ORDER BY student DESC, first_visit DESC";
-				$db->setQuery($query);
-				$id = $db->loadResult();
+					$query  = "SELECT `id` FROM `#__courses_members` WHERE `user_id` = '{$r->user_id}'";
+					$query .= " AND `course_id` = '{$course_id}'";
+					$query .= " ORDER BY student DESC, first_visit DESC";
+					$db->setQuery($query);
+					$id = $db->loadResult();
 
-				if ($db->tableHasField('#__courses_grade_book', 'user_id'))
-				{
 					$query = "UPDATE `#__courses_grade_book` SET `user_id` = '{$id}' WHERE `id` = '{$r->id}'";
 					$db->setQuery($query);
 					$db->query();
@@ -109,34 +109,34 @@ class Migration20130905195600ComCourses extends Hubzero_Migration
 			}
 		}
 
-		// Fix form respondents
-		$query = "SELECT * FROM `#__courses_form_respondents`";
-		$db->setQuery($query);
-		$results = $db->loadObjectList();
-
-		if ($results && count($results) > 0)
+		if ($db->tableHasField('#__courses_form_respondents', 'user_id'))
 		{
-			foreach ($results as $r)
+			// Fix form respondents
+			$query = "SELECT * FROM `#__courses_form_respondents`";
+			$db->setQuery($query);
+			$results = $db->loadObjectList();
+
+			if ($results && count($results) > 0)
 			{
-				$query  = "SELECT `course_id` FROM `#__courses_form_respondents` AS cfr,";
-				$query .= " `#__courses_form_deployments` AS cfd,";
-				$query .= " `#__courses_forms` AS cf,";
-				$query .= " `#__courses_assets` AS ca";
-				$query .= " WHERE cfr.deployment_id = cfd.id";
-				$query .= " AND cfd.form_id = cf.id";
-				$query .= " AND cf.asset_id = ca.id";
-				$query .= " AND cfr.id = '{$r->id}'";
-				$db->setQuery($query);
-				$course_id = $db->loadResult();
-
-				$query  = "SELECT `id` FROM `#__courses_members` WHERE `user_id` = '{$r->user_id}'";
-				$query .= " AND `course_id` = '{$course_id}'";
-				$query .= " ORDER BY student DESC, first_visit DESC";
-				$db->setQuery($query);
-				$id = $db->loadResult();
-
-				if ($db->tableHasField('#__courses_form_respondents', 'user_id'))
+				foreach ($results as $r)
 				{
+					$query  = "SELECT `course_id` FROM `#__courses_form_respondents` AS cfr,";
+					$query .= " `#__courses_form_deployments` AS cfd,";
+					$query .= " `#__courses_forms` AS cf,";
+					$query .= " `#__courses_assets` AS ca";
+					$query .= " WHERE cfr.deployment_id = cfd.id";
+					$query .= " AND cfd.form_id = cf.id";
+					$query .= " AND cf.asset_id = ca.id";
+					$query .= " AND cfr.id = '{$r->id}'";
+					$db->setQuery($query);
+					$course_id = $db->loadResult();
+
+					$query  = "SELECT `id` FROM `#__courses_members` WHERE `user_id` = '{$r->user_id}'";
+					$query .= " AND `course_id` = '{$course_id}'";
+					$query .= " ORDER BY student DESC, first_visit DESC";
+					$db->setQuery($query);
+					$id = $db->loadResult();
+
 					$query = "UPDATE `#__courses_form_respondents` SET `user_id` = '{$id}' WHERE `id` = '{$r->id}'";
 					$db->setQuery($query);
 					$db->query();
