0015e27cd642c0cd46a58d794953b4ecf4ca87c3
Shawn Rice <zooley@purdue.edu>
2013-09-25 08:22:10 -0400

    [COM_COLLECTIONS] Added missing admin interface to adjust parameters. Also needed for Joomla's Discover o work (db entry missing in open source release?)

---
+++ administrator/components/com_collections/controllers/posts.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+ximport('Hubzero_Controller');
+
+/**
+ * Controller class for collection posts
+ */
+class CollectionsControllerPosts extends Hubzero_Controller
+{
+	/**
+	 * Display a list of articles
+	 * 
+	 * @return     void
+	 */
+	public function displayTask()
+	{
+		// Set any errors
+		if ($this->getError()) 
+		{
+			foreach ($this->getErrors() as $error)
+			{
+				$this->view->setError($error);
+			}
+		}
+
+		// Output the HTML
+		$this->view->display();
+	}
+
+	/**
+	 * Cancels a task and redirects to listing
+	 * 
+	 * @return     void
+	 */
+	public function cancelTask()
+	{
+		$this->setRedirect(
+			'index.php?option=' . $this->_option . '&controller=' . $this->_controller
+		);
+	}
+}
+
