735369e38f9a72c47dbd7506b9dac3fca1a1ae22
Shawn Rice <zooley@purdue.edu>
2013-10-11 09:38:25 -0400

    [PLG_MEMBERS_POINTS] Made sure all language strings in language file.

--- plugins/members/points/views/history/tmpl/default.php
+++ plugins/members/points/views/history/tmpl/default.php
@@ -33,61 +33,59 @@ defined('_JEXEC') or die( 'Restricted access' );
 ?>
 
 <h3 class="section-header">
-	<a name="favorites"></a>
 	<?php echo JText::_('PLG_MEMBERS_POINTS'); ?>
 </h3>
 
-<div class="two columns first">
-	<div class="point-balance-container">
-		<h4>Point Balance</h4>
-		<div class="point-balance">
-			<strong><?php echo number_format($this->sum); ?> <span><?php echo strtolower(JText::_('PLG_MEMBERS_POINTS')); ?></span></strong>
-			<span class="spend">( <?php echo number_format($this->funds) . ' ' . strtolower(JText::_('PLG_MEMBERS_POINTS_AVAILABLE')); ?> )</span>
+<div class="grid">
+	<div class="col span-half">
+		<div class="point-balance-container">
+			<h4><?php echo JText::_('PLG_MEMBERS_POINTS_BALANCE'); ?></h4>
+			<div class="point-balance">
+				<strong><?php echo number_format($this->sum); ?> <span><?php echo strtolower(JText::_('PLG_MEMBERS_POINTS')); ?></span></strong>
+				<span class="spend">( <?php echo number_format($this->funds) . ' ' . strtolower(JText::_('PLG_MEMBERS_POINTS_AVAILABLE')); ?> )</span>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="two columns second">
-	<p class="help">
-		<strong><?php echo JText::_('PLG_MEMBERS_POINTS_HOW_ARE_POINTS_AWARDED'); ?></strong><br />
-		<?php echo JText::_('PLG_MEMBERS_POINTS_AWARDED_EXPLANATION'); ?>
-	</p>
-</div>
-<div class="clear"></div>
-
-	<div class="container">
-		<table class="entries transactions">
-			<caption><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_CAPTION'); ?></caption>
-			<thead>
-				<tr>
-					<th scope="col" class="textual-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_DATE'); ?></th>
-					<th scope="col" class="textual-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_DESCRIPTION'); ?></th>
-					<th scope="col" class="textual-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_TYPE'); ?></th>
-					<th scope="col" class="numerical-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_AMOUNT'); ?></th>
-					<th scope="col" class="numerical-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_BALANCE'); ?></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php if ($this->hist) : ?>
-					<?php foreach ($this->hist as $item) : ?>
-						<tr>
-							<td><?php echo JHTML::_('date',$item->created, JText::_('DATE_FORMAT_HZ1')); ?></td>
-							<td><?php echo $item->description; ?></td>
-							<td><?php echo $item->type; ?></td>
-						
-							<?php if ($item->type == 'withdraw') : ?>
-								<td class="numerical-data"><span class="withdraw">-<?php echo $item->amount; ?></span></td>
-							<?php elseif ($item->type == 'hold') : ?>
-								<td class="numerical-data"><span class="hold">(<?php echo $item->amount; ?>)</span></td>
-							<?php else : ?>
-								<td class="numerical-data"><span class="deposit">+<?php echo $item->amount; ?></span></td>
-							<?php endif; ?>
-						
-							<td class="numerical-data"><?php echo $item->balance; ?></td>
-						</tr>
-					<?php endforeach; ?>
-				<?php else : ?>
-					<tr><td colspan="5"><?php echo JText::_('PLG_MEMBERS_POINTS_NO_TRANSACTIONS'); ?></td></tr>
-				<?php endif; ?>
-			</tbody>
-		</table>
+	<div class="col span-half omega">
+		<p class="help">
+			<strong><?php echo JText::_('PLG_MEMBERS_POINTS_HOW_ARE_POINTS_AWARDED'); ?></strong><br />
+			<?php echo JText::_('PLG_MEMBERS_POINTS_AWARDED_EXPLANATION'); ?>
+		</p>
 	</div>
+</div>
+
+<div class="container">
+	<table class="entries transactions">
+		<caption><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_CAPTION'); ?></caption>
+		<thead>
+			<tr>
+				<th scope="col" class="textual-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_DATE'); ?></th>
+				<th scope="col" class="textual-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_DESCRIPTION'); ?></th>
+				<th scope="col" class="textual-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_TYPE'); ?></th>
+				<th scope="col" class="numerical-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_AMOUNT'); ?></th>
+				<th scope="col" class="numerical-data"><?php echo JText::_('PLG_MEMBERS_POINTS_TRANSACTIONS_TBL_TH_BALANCE'); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php if ($this->hist) : ?>
+				<?php foreach ($this->hist as $item) : ?>
+					<tr>
+						<td><?php echo JHTML::_('date', $item->created, JText::_('DATE_FORMAT_HZ1')); ?></td>
+						<td><?php echo $this->escape($item->description); ?></td>
+						<td><?php echo $this->escape($item->type); ?></td>
+					<?php if ($item->type == 'withdraw') : ?>
+						<td class="numerical-data"><span class="withdraw">-<?php echo $this->escape($item->amount); ?></span></td>
+					<?php elseif ($item->type == 'hold') : ?>
+						<td class="numerical-data"><span class="hold">(<?php echo $this->escape($item->amount); ?>)</span></td>
+					<?php else : ?>
+						<td class="numerical-data"><span class="deposit">+<?php echo $this->escape($item->amount); ?></span></td>
+					<?php endif; ?>
+						<td class="numerical-data"><?php echo $this->escape($item->balance); ?></td>
+					</tr>
+				<?php endforeach; ?>
+			<?php else : ?>
+				<tr><td colspan="5"><?php echo JText::_('PLG_MEMBERS_POINTS_NO_TRANSACTIONS'); ?></td></tr>
+			<?php endif; ?>
+		</tbody>
+	</table>
+</div>
