ef7b4d40160d7f2db6811c68f1339ad0bcba7a8f
Chris Smoak <csmoak@purdue.edu>
2013-10-15 17:32:47 -0400

    [HUBZERO][#3211] Fixed email for saved group

--- components/com_groups/views/emails/tmpl/saved.php
+++ components/com_groups/views/emails/tmpl/saved.php
@@ -33,7 +33,7 @@ defined('_JEXEC') or die( 'Restricted access' );
 
 $juri =& JURI::getInstance();
 
-$sef = JRoute::_('index.php?option='.$this->option.'&cn='. $this->g_cn.'&active=members');
+$sef = JRoute::_('index.php?option='.$this->option.'&cn='. $this->g_cn);
 if (substr($sef,0,1) == '/') {
 	$sef = substr($sef,1,strlen($sef));
 }
