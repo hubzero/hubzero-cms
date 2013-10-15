05dac04dc0f2aa533a7c678138b7b11f11514083
Shawn Rice <zooley@purdue.edu>
2013-10-15 14:53:24 -0400

    Fixing some namespace issues

--- libraries/Hubzero/Antispam/Adapter/Mollom/AbstractMollom.php
+++ libraries/Hubzero/Antispam/Adapter/Mollom/AbstractMollom.php
@@ -1,5 +1,13 @@
 <?php
 
+namespace Hubzero\Antispam\Adapter\Mollom;
+
+use Hubzero\Antispam\Adapter\Mollom\MollomException;
+use Hubzero\Antispam\Adapter\Mollom\MollomAuthenticationException;
+use Hubzero\Antispam\Adapter\Mollom\MollomBadRequestException;
+use Hubzero\Antispam\Adapter\Mollom\MollomNetworkException;
+use Hubzero\Antispam\Adapter\Mollom\MollomResponseException;
+
 /**
  * @file
  * Mollom client class.
@@ -429,7 +437,7 @@ abstract class Mollom {
 		}
 		else {
 			$headers = array();
-			$response = new stdClass;
+			$response = new \stdClass;
 			$response->code = self::AUTH_ERROR;
 			$response->message = 'Missing API keys.';
 			$response->isError = TRUE;
@@ -443,7 +451,7 @@ abstract class Mollom {
 				$response->data = json_decode($response->body, TRUE);
 			}
 			elseif (strstr($response->headers['content-type'], 'application/xml')) {
-				$response->elements = new SimpleXmlIterator($response->body);
+				$response->elements = new \SimpleXmlIterator($response->body);
 				$response->data = $this->parseXML($response->elements);
 			}
 		}
@@ -686,7 +694,7 @@ abstract class Mollom {
 	 * @return array
 	 *   An associative, possibly multidimensional array.
 	 */
-	public static function parseXML(SimpleXMLIterator $sxi) {
+	public static function parseXML(\SimpleXMLIterator $sxi) {
 		$a = array();
 		$remove = array();
 		for ($sxi->rewind(); $sxi->valid(); $sxi->next()) {
@@ -1337,151 +1345,3 @@ abstract class Mollom {
 		return $this->lastResponseCode === TRUE;
 	}
 }
-
-/**
- * A catchable Mollom exception.
- *
- * The Mollom class internally uses exceptions to handle HTTP request errors
- * within the Mollom::handleRequest() method. All exceptions thrown in the
- * Mollom class and derived classes should be instances of the MollomException
- * class if they pertain to errors that can be catched/handled within the class.
- * Other errors should not use the MollomException class and handled
- * differently.
- *
- * No MollomException is supposed to pile up as a user-facing fatal error. All
- * functions that invoke Mollom::handleRequest() have to catch Mollom
- * exceptions.
- *
- * @see Mollom::query()
- * @see Mollom::handleRequest()
- *
- * @param $message
- *   The Exception message to throw.
- * @param $code
- *   The Exception code.
- * @param $previous
- *   (optional) The previous Exception, if any.
- * @param $instance
- *   The Mollom class instance the Exception is thrown in.
- * @param $arguments
- *   (optional) A associative array containing information about a performed
- *   HTTP request that failed:
- *   - request: (string) The HTTP method and URI of the performed request; e.g.,
- *     "GET http://server.mollom.com/v1/foo/bar". In case of GET requests, do
- *     not add query parameters to the URI; pass them in 'data' instead.
- *   - data: (array) An associative array containing HTTP GET/POST/PUT request
- *     query parameters that were sent to the server.
- *   - response: (mixed) The server response, either as string, or the already
- *     parsed response; i.e., an array.
- */
-class MollomException extends Exception {
-	/**
-	 * @var Mollom
-	 */
-	protected $mollom;
-
-	/**
-	 * The severity of this exception.
-	 *
-	 * By default, all exceptions should be logged and appear as errors (unless
-	 * overridden by a later log entry).
-	 *
-	 * @var string
-	 */
-	protected $severity = 'error';
-
-	/**
-	 * Overrides Exception::__construct().
-	 */
-	function __construct($message = '', $code = 0, Exception $previous = NULL, Mollom $mollom, array $request_info = array()) {
-		// Fatal error on PHP <5.3 when passing more arguments to Exception.
-		if (version_compare(phpversion(), '5.3') >= 0) {
-			parent::__construct($message, $code, $previous);
-		}
-		else {
-			parent::__construct($message, $code);
-		}
-		$this->mollom = $mollom;
-
-		// Set the error code on the Mollom class.
-		$mollom->lastResponseCode = $code;
-
-		// Log the exception.
-		// To aid Mollom technical support, include the IP address of the server we
-		// tried to reach in case a request fails.
-		// PHP's native gethostbyname() is available on all platforms, but its DNS
-		// lookup and caching behavior is undocumented and unclear. User comments on
-		// php.net mention that it does not have an own cache and also does not use
-		// the OS/platform's native DNS name resolver. Due to that, we only use it
-		// under error conditions.
-		$message = array(
-			'severity' => $this->severity,
-			'message' => 'Error @code: %message (@server-ip)',
-			'arguments' => array(
-				'@code' => $code,
-				'%message' => $message,
-				'@server-ip' => gethostbyname($mollom->server),
-			),
-		);
-		// Add HTTP request information, if available.
-		if (!empty($request_info)) {
-			$message += $request_info;
-		}
-		$mollom->log[] = $message;
-	}
-}
-
-/**
- * Mollom network error exception.
- *
- * Thrown in case a HTTP request results in code <= 0, denoting a low-level
- * communication error.
- */
-class MollomNetworkException extends MollomException {
-	/**
-	 * Overrides MollomException::$severity.
-	 *
-	 * The client may be able to recover from this error, so use a warning level.
-	 */
-	protected $severity = 'warning';
-}
-
-/**
- * Mollom authentication error exception.
- *
- * Thrown in case API keys or other authentication parameters are invalid.
- */
-class MollomAuthenticationException extends MollomException {
-}
-
-/**
- * Mollom error due to bad client request exception.
- *
- * Thrown in case the local time diverges too much from UTC.
- *
- * @see Mollom::TIME_OFFSET_MAX
- * @see Mollom::REQUEST_ERROR
- * @see Mollom::handleRequest()
- */
-class MollomBadRequestException extends MollomException {
-}
-
-/**
- * Mollom server response exception.
- *
- * Thrown when a request to a Mollom server succeeds, but the response does not
- * contain an expected element; e.g., a backend configuration or execution
- * error that possibly exists on one server only.
- *
- * @see Mollom::handleRequest()
- */
-class MollomResponseException extends MollomException {
-	/**
-	 * Overrides MollomException::$severity.
-	 *
-	 * Might be a client-side error, but more likely a server-side error. The
-	 * client may be able to recover from this error.
-	 */
-	protected $severity = 'debug';
-}
-
