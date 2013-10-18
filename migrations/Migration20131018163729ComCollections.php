25ae78f0f65364559beecf5e4a38d860070c8e6c
Sam Wilson <samwilson@purdue.edu>
2013-10-18 12:40:59 -0400

    [HUBZERO][#3276][COM_COLLECTIONS] run migration to add collections component entry again, as it looks like it was only partially added on some hubs

---
+++ migrations/Migration20131018163729ComCollections.php
@@ -0,0 +1,26 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Re-add collections component entry to fix up instances where it was only partially added in the Joomla 2.5 version
+ **/
+class Migration20131018163729ComCollections extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		self::addComponentEntry('Collections');
+	}
+
+	/**
+	 * Down
+	 **/
+	protected static function down($db)
+	{
+		self::deleteComponentEntry('Collections');
+	}
+}
