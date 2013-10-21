3dd68c23daa1f2579add9bc9a84a0f3d3cf625e8
Shawn Rice <zooley@purdue.edu>
2013-10-21 07:56:20 -0400

    [HUBZERO][#3266][MOD_REPORTPROBLEMS] Added param for designating CSS selector of element to act as slider trigger

--- administrator/components/com_members/controllers/plugins.php
+++ administrator/components/com_members/controllers/plugins.php
@@ -195,6 +195,21 @@ class MembersControllerPlugins extends Hubzero_Controller
 			return false;
 		}
 
+		$lang = JFactory::getLanguage();
+		if ($this->view->rows)
+		{
+			foreach ($this->view->rows as &$item) 
+			{
+				$source = JPATH_PLUGINS . '/' . $item->folder . '/' . $item->element;
+				$extension = 'plg_' . $item->folder . '_' . $item->element;
+					$lang->load($extension . '.sys', JPATH_ADMINISTRATOR, null, false, false)
+				||	$lang->load($extension . '.sys', $source, null, false, false)
+				||	$lang->load($extension . '.sys', JPATH_ADMINISTRATOR, $lang->getDefault(), false, false)
+				||	$lang->load($extension . '.sys', $source, $lang->getDefault(), false, false);
+				$item->name = JText::_($item->name);
+			}
+		}
+
 		// Get related plugins
 		JPluginHelper::importPlugin('members');
 		$dispatcher =& JDispatcher::getInstance();
