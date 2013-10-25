fb717512711a731fd41c5b357b3bfc689d2dd31b
Shawn Rice <zooley@purdue.edu>
2013-10-25 07:18:43 -0400

    Fixed some minor formatting

--- libraries/Hubzero/Announcement.php
+++ libraries/Hubzero/Announcement.php
@@ -47,91 +47,91 @@ class Hubzero_Announcement extends JTable
 	 * @var integer
 	 */
 	var $id = NULL;
-	
+
 	/**
 	 * varchar(100)
 	 * 
 	 * @var string
 	 */
 	var $scope = NULL;
-	
+
 	/**
 	 * int(11)
 	 * 
 	 * @var integer
 	 */
 	var $scope_id = NULL;
-	
+
 	/**
 	 * text
 	 * 
 	 * @var string
 	 */
 	var $content = NULL;
-	
+
 	/**
 	 * int(11)
 	 * 
 	 * @var integer
 	 */
 	var $priority = NULL;
-	
+
 	/**
 	 * datetime
 	 * 
 	 * @var date
 	 */
 	var $created = NULL;
-	
+
 	/**
 	 * int(11)
 	 * 
 	 * @var integer
 	 */
 	var $created_by = NULL;
-	
+
 	/**
 	 * int(11)
 	 * 
 	 * @var integer
 	 */
 	var $state = NULL;
-	
+
 	/**
 	 * datetime
 	 * 
 	 * @var date
 	 */
 	var $publish_up = NULL;
-	
+
 	/**
 	 * datetime
 	 * 
 	 * @var date
 	 */
 	var $publish_down = NULL;
-	
+
 	/**
 	 * int(11)
 	 * 
 	 * @var integer
 	 */
 	var $sticky = NULL;
-	
+
 	/**
 	 * int(11)
 	 * 
 	 * @var integer
 	 */
 	var $email = NULL;
-	
+
 	/**
 	 * int(11)
 	 * 
 	 * @var integer
 	 */
 	var $sent = NULL;
-	
+
 	/**
 	 * Constructor method for JTable class
 	 * 
@@ -142,7 +142,7 @@ class Hubzero_Announcement extends JTable
 	{
 		parent::__construct('#__announcements', 'id', $db);
 	}
-	
+
 	/**
 	 * Overloaded Check method. Verify we have all needed things to save
 	 */
@@ -151,23 +151,23 @@ class Hubzero_Announcement extends JTable
 		//make sure we have content
 		if (!isset($this->content) || $this->content == '')
 		{
-			$this->setError( JText::_('Announcement must contain some content.') );
+			$this->setError(JText::_('Announcement must contain some content.'));
 			return false;
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Method to check if announcement belongs to entity
 	 */
-	public function belongsToObject( $entity_scope, $entity_id )
+	public function belongsToObject($entity_scope, $entity_id)
 	{
 		//make sure we have an id
-		if(!isset($this->id) || $this->id == null || $this->id == 0)
+		if (!isset($this->id) || $this->id == null || $this->id == 0)
 		{
 			return true;
 		}
-		
+
 		//make sure scope and id match
 		if ($this->scope == $entity_scope && $this->scope_id == $entity_id)
 		{
@@ -175,32 +175,31 @@ class Hubzero_Announcement extends JTable
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Get Announcement Count
 	 * 
 	 * @param    array    $filters
 	 */
-	public function count( $filters = array() )
+	public function count($filters = array())
 	{
 		$query  = "SELECT COUNT(*)";
 		$query .= $this->_buildQuery($filters);
-		
+
 		$this->_db->setQuery($query);
 		return $this->_db->loadResult();
 	}
-	
+
 	/**
 	 * Get Announcement Records
 	 * 
 	 * @param    array    $filters
 	 */
-	public function find( $filters = array() )
+	public function find($filters = array())
 	{
 		$query  = "SELECT a.*";
-		$query .= $this->_buildQuery( $filters );
-		
-		
+		$query .= $this->_buildQuery($filters);
+
 		//$query .= " ORDER BY a.priority DESC, a.created DESC";
 		$query .= " ORDER BY a.created DESC";
 		if (isset($filters['limit']))
@@ -211,24 +210,24 @@ class Hubzero_Announcement extends JTable
 			}
 			$query .= " LIMIT " . intval($filters['start']) . "," . intval($filters['limit']);
 		}
-		
+
 		$this->_db->setQuery($query);
 		return $this->_db->loadObjectList();
 	}
-	
+
 	/**
 	 * Build Query to get Announcements
 	 * 
 	 * @param    array    $filters
 	 */
-	private function _buildQuery( $filters = array() )
+	private function _buildQuery($filters = array())
 	{
 		//array to hold where statements
 		$where = array();
-		
+
 		//start query
 		$query = " FROM $this->_tbl AS a";
-		
+
 		//apply filters based on filters passed in
 		if (isset($filters['scope']) && $filters['scope'])
 		{
@@ -262,7 +261,7 @@ class Hubzero_Announcement extends JTable
 		{
 			$where[] = "a.`sent` = " . $this->_db->Quote(intval($filters['sent']));
 		}
-		
+
 		//published
 		if (isset($filters['published']))
 		{
@@ -270,7 +269,7 @@ class Hubzero_Announcement extends JTable
 			$where[] = "(a.`publish_up` = '0000-00-00 00:00:00' OR a.`publish_up` <= " . $this->_db->Quote($now) . ")";
 			$where[] = "(a.`publish_down` = '0000-00-00 00:00:00' OR a.`publish_down` >= " . $this->_db->Quote($now) . ")";
 		}
-		
+
 		//search
 		if (isset($filters['search']) && $filters['search'])
 		{
@@ -283,39 +282,38 @@ class Hubzero_Announcement extends JTable
 				$where[] = "(LOWER(a.content) LIKE '%" . $this->_db->getEscaped(strtolower($filters['search'])) . "%')";
 			}
 		}
-		
+
 		//if we have an wheres append them
 		if (count($where) > 0)
 		{
 			$query .= " WHERE " . implode(' AND ', $where);
 		}
-		
+
 		return $query;
 	}
-	
-	
+
 	/**
 	 * Check if date is within announcement publish up/down
 	 * 
 	 * @param    array    $filters
 	 */
-	public function announcementPublishedForDate( $announcement = null, $date = null )
+	public function announcementPublishedForDate($announcement = null, $date = null)
 	{
 		// var to hold if announcment is published
 		$published = false;
-		
+
 		// make sure we have an announcement
 		if ($announcement === null)
 		{
 			$announcement = $this;
 		}
-		
+
 		// make sure we have a date
 		if ($date === null)
 		{
 			$date = time();
 		}
-		
+
 		//get up and down times
 		$up = $down = null;
 		if ($announcement->publish_up != '' && $announcement->publish_up != '0000-00-00 00:00:00')
@@ -326,14 +324,14 @@ class Hubzero_Announcement extends JTable
 		{
 			$down = strtotime($announcement->publish_down);
 		}
-		
+
 		// if we have a null uptime or uptime less then our date
 		// and if down is null or downtime is greater then our date
 		if (($up == null || $date >= $up) && ($down == null || $date <= $down))
 		{
 			$published = true;
 		}
-		
+
 		return $published;
 	}
 	
@@ -342,20 +340,20 @@ class Hubzero_Announcement extends JTable
 	 * 
 	 * @param    array    $filters
 	 */
-	public function emailAnnouncement( $announcement = null )
+	public function emailAnnouncement($announcement = null)
 	{
 		// make sure we have an announcement
 		if ($announcement === null)
 		{
 			$announcement = $this;
 		}
-		
+
 		ini_set('display_errors', 1);
 		error_reporting(E_ALL);
-		
+
 		// load group
-		$group = Hubzero_Group::getInstance( $announcement->scope_id );
-		
+		$group = Hubzero_Group::getInstance($announcement->scope_id);
+
 		//create view object
 		ximport('Hubzero_Plugin_View');
 		$eview = new Hubzero_Plugin_View(
@@ -370,9 +368,9 @@ class Hubzero_Announcement extends JTable
 		$eview->boundary     = md5(date('U'));
 		$message             = $eview->loadTemplate();
 		$message             = str_replace("\n", "\r\n", $message);
-		
+
 		$jconfig =& JFactory::getConfig();
-		
+
 		// define headers
 		$headers  = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-Type: multipart/alternative; boundary=' . $eview->boundary . "\r\n";
@@ -380,17 +378,17 @@ class Hubzero_Announcement extends JTable
 		$headers .= "X-Priority: 3\n";
 		$headers .= "X-MSMail-Priority: High\n";
 		$headers .= 'X-Mailer: '. $jconfig->getValue('config.sitename') ."\n";
-		
+
 		// define subject
 		$subject = $group->get('description') . ' Group Announcement';
-		
+
 		// send to all group members
 		foreach ($group->get('members') as $member)
 		{
-			$profile = Hubzero_User_Profile::getInstance( $member );
+			$profile = Hubzero_User_Profile::getInstance($member);
 			mail($profile->get('email'), $subject, $message, $headers);
 		}
-		
+
 		return true; 
 	}
 }
