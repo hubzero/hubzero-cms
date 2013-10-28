b92c2beb0012359fb7ceee398b6be1133cc1ae6c
Shawn Rice <zooley@purdue.edu>
2013-10-28 07:46:40 -0400

    [HUBZERO][#3405][COM_RESOURCES] Rewrote javascript for Type field builder to use jquery

--- administrator/components/com_resources/views/types/tmpl/edit.php
+++ administrator/components/com_resources/views/types/tmpl/edit.php
@@ -30,6 +30,8 @@
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
+$base = str_replacE('/administrator', '', rtrim(JURI::getInstance()->base(true), '/'));
+
 // Push some styles to the template
 $document =& JFactory::getDocument();
 $document->addStyleSheet('components' . DS . $this->option . DS . 'assets' . DS . 'css' . DS . 'resources.css');
@@ -70,152 +72,6 @@ function submitbutton(pressbutton)
 		submitform( pressbutton );
 	}
 }
-
-var Fields = {
-	isIE8: function() {
-	    var rv = -1,
-			ua = navigator.userAgent,
-			re = new RegExp("Trident\/([0-9]{1,}[\.0-9]{0,})");
-	    if (re.exec(ua) != null) {
-	        rv = parseFloat(RegExp.$1);
-	    }
-	    return (rv == 4);
-	},
-	
-	addRow: function(id) {
-		var tbody = document.getElementById(id).tBodies[0];
-		var counter = tbody.rows.length;
-		var newNode = tbody.rows[0].cloneNode(true);
-
-		var replaceme = null;
-
-		var newField = newNode.childNodes;
-		for (var i=0;i<newField.length;i++) 
-	    {
-	        var inputs = newField[i].childNodes;
-	        for (var k=0;k<inputs.length;k++) 
-	        {
-	            var theName = inputs[k].name;
-	            if (theName) {
-	                tokens = theName.split('[');
-	                n = tokens[2];
-	                inputs[k].name = id + '[' + counter + ']['+ n;
-					inputs[k].id = id + '-' + counter + '-' + n.replace(']', '');
-					
-					if (Fields.isIE8() && inputs[k].type == 'select-one') {
-						inputs[k].id = id + '-' + counter + '-' + n.replace(']', '')+'-tmp';
-						replaceme = id + '-' + counter + '-' + n.replace(']', '')+'-tmp';
-					}
-	            }
-	            var n = id + '[' + counter + '][type]';
-	            var z = id + '[' + counter + '][required]';
-	            if (inputs[k].value && inputs[k].name != z) {
-	                inputs[k].value = '';
-					inputs[k].selectedIndex = 0;
-					inputs[k].selected = false;
-	            }
-				if (inputs[k].checked) {
-					inputs[k].checked = false;
-				}
-	        }
-			if (newField[i].id) {
-				newField[i].id = 'fields-'+counter+'-options';
-			}
-	    }
-
-		tbody.appendChild(newNode);
-		
-		// Make a clone of the clone. Why? Because IE 8 is dumb.
-		// IE still retains a reference to the original object for change events
-		// So, when calling onChange, the event gets fired for the clone AND the 
-		// original. Cloning the clone seems to fix this.
-		if (replaceme) {
-			var replace = $(replaceme);
-			var select = Element.clone(replace).injectAfter(replace);
-			Element.remove(replace);
-		}
-		
-		Fields.initSelect();
-		
-		jq('#fields tbody').sortable('enable');
-
-		return false;
-	},
-	
-	addOption: function(id) {
-		var tbody = document.getElementById(id).tBodies[0];
-		var counter = tbody.rows.length;
-		var newNode = tbody.rows[0].cloneNode(true);
-
-		var newField = newNode.childNodes;
-		for (var i=0;i<newField.length;i++) 
-	    {
-	        var inputs = newField[i].childNodes;
-	        for (var k=0;k<inputs.length;k++) 
-	        {
-	            var theName = inputs[k].name;
-	            if (theName) {
-	                tokens = theName.split('[');
-	                n = tokens[2];
-	                inputs[k].name = 'fields['+id+'][' +n+ '[' + counter + '][label]';
-	            }
-	            if (inputs[k].value) {
-	                inputs[k].value = '';
-	            }
-	        }
-	    }
-
-		tbody.appendChild(newNode);
-		
-		return false;
-	},
-	
-	initOptions: function() {
-		$$('.add-custom-option').each(function(el){
-			$(el).removeEvents('click');
-			$(el).addEvent('click', function(e){
-				new Event(e).stop();
-				Fields.addOption(el.getProperty('rel'));
-			});
-		});
-	},
-	
-	tiner: 0,
-	
-	clear: function() {
-		Fields.timer = 0;
-	},
-	
-	initSelect: function() {
-		$$('#fields select').each(function(el){
-			$(el).removeEvents('change');
-			$(el).addEvent('change', function(){
-				var i = this.name.replace(/^fields\[(\d+)\]\[type\]/g,"$1");
-				var myAjax1 = new Ajax('index.php?option=com_resources&controller=types&no_html=1&task=element&ctrl=fields&type='+this.value+'&name='+i,{
-					update: $('fields-'+i+'-options')
-				}).request();
-				myAjax1.addEvent('onComplete', function(){
-					Fields.initOptions();
-				});
-			})
-		});
-	},
-
-	initialise: function() {
-		$('add-custom-field').addEvent('click', function(e){
-			new Event(e).stop();
-			
-			Fields.addRow('fields');
-		});
-		
-		Fields.initSelect();
-		
-		Fields.initOptions();
-	}
-}
-
-
-window.addEvent('domready', Fields.initialise);
 </script>
 
 <form action="index.php" method="post" id="item-form" name="adminForm">
@@ -414,17 +270,164 @@ window.addEvent('domready', Fields.initialise);
 			?>
 			</tbody>
 		</table>
-		<!-- <script src="components/com_resources/assets/js/xsortables.js"></script> -->
-		<script src="/media/system/js/jquery.js"></script> 
-		<script src="/media/system/js/jquery.noconflict.js"></script> 
-		<script src="/media/system/js/jquery.ui.js"></script> 
-		<script>
+
+		<script type="text/javascript" src="<?php echo $base; ?>/media/system/js/jquery.js"></script> 
+		<script type="text/javascript" src="<?php echo $base; ?>/media/system/js/jquery.noconflict.js"></script> 
+		<script type="text/javascript" src="<?php echo $base; ?>/media/system/js/jquery.ui.js"></script> 
+		<script type="text/javascript">
 			if (!jq) {
 				var jq = $;
 			}
 			
+			var Fields = {
+				isIE8: function() {
+					var rv = -1,
+						ua = navigator.userAgent,
+						re = new RegExp("Trident\/([0-9]{1,}[\.0-9]{0,})");
+					if (re.exec(ua) != null) {
+						rv = parseFloat(RegExp.$1);
+					}
+					return (rv == 4);
+				},
+
+				addRow: function(id) {
+					var tbody = document.getElementById(id).tBodies[0],
+						counter = tbody.rows.length,
+						newNode = tbody.rows[0].cloneNode(true),
+						replaceme = null;
+
+					var newField = newNode.childNodes;
+					for (var i=0;i<newField.length;i++) 
+					{
+						var inputs = newField[i].childNodes;
+						for (var k=0;k<inputs.length;k++) 
+						{
+							var theName = inputs[k].name;
+							if (theName) {
+								tokens = theName.split('[');
+								n = tokens[2];
+								inputs[k].name = id + '[' + counter + ']['+ n;
+								inputs[k].id = id + '-' + counter + '-' + n.replace(']', '');
+
+								if (Fields.isIE8() && inputs[k].type == 'select-one') {
+									inputs[k].id = id + '-' + counter + '-' + n.replace(']', '')+'-tmp';
+									replaceme = id + '-' + counter + '-' + n.replace(']', '')+'-tmp';
+								}
+							}
+							var n = id + '[' + counter + '][type]';
+							var z = id + '[' + counter + '][required]';
+							if (inputs[k].value && inputs[k].name != z) {
+								inputs[k].value = '';
+								inputs[k].selectedIndex = 0;
+								inputs[k].selected = false;
+							}
+							if (inputs[k].checked) {
+								inputs[k].checked = false;
+							}
+						}
+						if (newField[i].id) {
+							newField[i].id = 'fields-'+counter+'-options';
+						}
+					}
+
+					tbody.appendChild(newNode);
+
+					// Make a clone of the clone. Why? Because IE 8 is dumb.
+					// IE still retains a reference to the original object for change events
+					// So, when calling onChange, the event gets fired for the clone AND the 
+					// original. Cloning the clone seems to fix this.
+					if (replaceme) {
+						var replace = jq(replaceme);
+						var select = jq.clone(replace).appendAfter(replace);
+						jq.remove(replace);
+					}
+
+					Fields.initSelect();
+
+					//jq('#fields tbody').sortable(); //'enable');
+
+					return false;
+				},
+
+				addOption: function(id) {
+					var tbody = document.getElementById(id).tBodies[0];
+					var counter = tbody.rows.length;
+					var newNode = tbody.rows[0].cloneNode(true);
+
+					var newField = newNode.childNodes;
+					for (var i=0;i<newField.length;i++) 
+					{
+						var inputs = newField[i].childNodes;
+						for (var k=0;k<inputs.length;k++) 
+						{
+							var theName = inputs[k].name;
+							if (theName) {
+								tokens = theName.split('[');
+								n = tokens[2];
+								inputs[k].name = 'fields['+id+'][' +n+ '[' + counter + '][label]';
+							}
+							if (inputs[k].value) {
+								inputs[k].value = '';
+							}
+						}
+					}
+
+					tbody.appendChild(newNode);
+
+					return false;
+				},
+
+				initOptions: function() {
+					jq('.add-custom-option').each(function(i, el){
+						jq(el)
+							.off('click')
+							.on('click', function(e){
+								e.preventDefault();
+
+								Fields.addOption(jq(this).attr('rel'));
+							});
+					});
+				},
+
+				timer: 0,
+
+				clear: function() {
+					Fields.timer = 0;
+				},
+
+				initSelect: function() {
+					jq('#fields select').each(function(i, el){
+						jq(el)
+							.off('change')
+							.on('change', function(){
+								var i = this.name.replace(/^fields\[(\d+)\]\[type\]/g,"$1");
+								jq.get('index.php?option=com_resources&controller=types&no_html=1&task=element&ctrl=fields&type='+this.value+'&name='+i,{}, function (response){
+									jq('#fields-'+i+'-options').html(response);
+									Fields.initOptions();
+								});
+							})
+					});
+				},
+
+				initialise: function() {
+					jq('#add-custom-field').on('click', function (e){
+						e.preventDefault();
+
+						Fields.addRow('fields');
+					});
+
+					Fields.initSelect();
+					Fields.initOptions();
+
+					jq('#fields tbody').sortable();
+				}
+			}
+
 			jQuery(document).ready(function(jq){
 				var $ = jq;
+
+				Fields.initialise();
+
 				$("#fields tbody").sortable({
 					handle: '.handle',
 					helper: function(e, tr) {
