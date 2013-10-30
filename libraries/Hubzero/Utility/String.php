8dac88aeed183d9827c2d00ccd3d506fba1a8abc
Shawn Rice <zooley@purdue.edu>
2013-10-30 07:48:52 -0400

    Added obfuscate method

--- libraries/Hubzero/Utility/String.php
+++ libraries/Hubzero/Utility/String.php
@@ -500,4 +500,35 @@ class String
 
 		return $excerpt;
 	}
+
+	/**
+	 * Obfuscate a string to prevent spam-bots from sniffing it.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	public static function obfuscate($value)
+	{
+		$safe = '';
+
+		foreach (str_split($value) as $letter)
+		{
+			// To properly obfuscate the value, we will randomly convert each letter to
+			// its entity or hexadecimal representation, keeping a bot from sniffing
+			// the randomly obfuscated letters out of the string on the responses.
+			switch (rand(1, 3))
+			{
+				case 1:
+					$safe .= '&#' . ord($letter) . ';'; break;
+
+				case 2:
+					$safe .= '&#x' . dechex(ord($letter)) . ';'; break;
+
+				case 3:
+					$safe .= $letter;
+			}
+		}
+
+		return $safe;
+	}
 }
