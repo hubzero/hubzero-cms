e98e7107100f4a6734ec3b3412725251ddfa24be
Chris Smoak <csmoak@purdue.edu>
2013-10-31 08:51:47 -0400

    [NEES][#6772] Fixed affiliated count on citations with database migration

---
+++ migrations/Migration20131031124923ComCitations.php
@@ -0,0 +1,38 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20131031124923ComCitations extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query = "ALTER TABLE `#__citations` MODIFY `affiliated` int(11) NOT NULL DEFAULT 0;";
+
+		if (!empty($query))
+		{
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	protected static function down($db)
+	{
+		$query = "ALTER TABLE `#__citations` MODIFY `affiliated` int(11) DEFAULT NULL;";
+
+		if (!empty($query))
+		{
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+}
