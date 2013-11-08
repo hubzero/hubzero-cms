bbd10f9ff1b63b01a5367af92a2e8c17e9e115e1
Shawn Rice <zooley@purdue.edu>
2013-11-08 15:39:50 -0500

    [PLG_WHATSNEW_RESOURCES] Fix for resource param getting overridden by previous resource in list

--- plugins/whatsnew/resources/resources.php
+++ plugins/whatsnew/resources/resources.php
@@ -316,11 +316,11 @@ class plgWhatsnewResources extends JPlugin
 		}
 
 		$rparams = new $paramClass($row->params);
-		$params = $config;
-		$params->merge($rparams);
+		//$params = $config;
+		//$params->merge($rparams);
 
 		// Set the display date
-		switch ($params->get('show_date'))
+		switch ($rparams->get('show_date', $config->get('show_date')))
 		{
 			case 0: $thedate = ''; break;
 			case 1: $thedate = JHTML::_('date', $row->created, JText::_('DATE_FORMAT_HZ1'));    break;
@@ -333,7 +333,7 @@ class plgWhatsnewResources extends JPlugin
 		// Start building HTML
 		$html  = "\t" . '<li class="resource">' . "\n";
 		$html .= "\t\t" . '<p class="title"><a href="' . $row->href . '">' . stripslashes($row->title) . '</a></p>' . "\n";
-		if ($params->get('show_ranking')) 
+		if ($rparams->get('show_ranking', $config->get('show_ranking'))) 
 		{
 			$helper->getCitationsCount();
 			$helper->getLastCitationDate();
@@ -369,7 +369,7 @@ class plgWhatsnewResources extends JPlugin
 			$html .= "\t\t\t" . '</dl>' . "\n";
 			$html .= "\t\t" . '</div>' . "\n";
 		} 
-		elseif ($params->get('show_rating')) 
+		elseif ($rparams->get('show_rating', $config->get('show_rating'))) 
 		{
 			switch ($row->rating)
 			{
