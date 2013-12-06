44bfef9927ed95e1e218e85217c9f3e9b50d3747
Sam Wilson <samwilson@purdue.edu>
2013-12-06 11:53:58 -0500

    [MOD_LOGIN] fix to reference users component, rather than old user component

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -64,12 +64,12 @@ if (!empty($error_message))
 					<form action="<?php echo JRoute::_('index.php', true, true); ?>" method="post" id="login_form">
 						<div class="labelInputPair">
 							<label for="username"><?php echo JText::_('Username'); ?>:</label>
-							<a class="forgots" href="<?php echo JRoute::_('index.php?option=com_user&view=remind'); ?>"><?php echo JText::_('Lost username?');?></a>
+							<a class="forgots" href="<?php echo JRoute::_('index.php?option=com_users&view=remind'); ?>"><?php echo JText::_('Lost username?');?></a>
 							<input tabindex="1" type="text" name="username" id="username" placeholder="username" />
 						</div>
 						<div class="labelInputPair">
 							<label for="password"><?php echo JText::_('Password'); ?>:</label>
-							<a class="forgots" href="<?php echo JRoute::_('index.php?option=com_user&view=reset'); ?>"><?php echo JText::_('Forgot password?'); ?></a>
+							<a class="forgots" href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>"><?php echo JText::_('Forgot password?'); ?></a>
 							<input tabindex="2" type="password" name="passwd" id="password" placeholder="password" />
 						</div>
 						<div class="submission">
