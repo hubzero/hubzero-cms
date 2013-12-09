cd470f724cc41ce08000285f310ca514a1c7070f
Alissa Nedossekina <alisa@purdue.edu>
2013-12-09 14:22:43 -0500

    [COM_TOOLS] another follow-up fix to license screen

--- components/com_tools/views/pipeline/tmpl/license.php
+++ components/com_tools/views/pipeline/tmpl/license.php
@@ -28,16 +28,16 @@ defined('_JEXEC') or die( 'Restricted access' );
 $open 					= ($this->code == '@OPEN') ? 1 : 0 ;
 $this->codeaccess 		= ($this->code == '@OPEN') ? 'open' : 'closed';
 $newstate   			= ($this->action == 'confirm') ? 'Approved' :  $this->status['state'];
-//$this->statuspath 		= JRoute::_('index.php?option=' . $this->option . '&controller=' . $this->controller . '&task=status&app='.$this->status['toolid']);
 
 $codeChoices = array(
-	'@OPEN' => 'open source (anyone can access code)',
-	'@DEV'  => 'closed code'
+	'@OPEN' => JText::_('COM_TOOLS_OPEN_SOURCE'),
+	'@DEV'  => JText::_('COM_TOOLS_CLOSED_SOURCE')
 );
 
 $licenseChoices = array(
-	'c1' => JText::_('Load a standard license')
+	'0' => JText::_('Choose a template')
 );
+
 if ($this->licenses) 
 {
 	foreach ($this->licenses as $l) 
@@ -55,8 +55,8 @@ if ($this->licenses)
 
 <div id="content-header-extra">
 	<ul id="useroptions">
-		<li><a class="icon-status status btn" href="<?php echo JRoute::_('index.php?option='.$this->option.'&controller='.$this->controller.'&task=status&app='.$this->status['toolname']); ?>"><?php echo JText::_('COM_TOOLS_TOOL_STATUS'); ?></a></li>
-		<li class="last"><a class="icon-add add btn" href="<?php echo JRoute::_('index.php?option='.$this->option.'&controller='.$this->controller.'&task=create'); ?>"><?php echo JText::_('COM_TOOLS_CONTRIBTOOL_NEW_TOOL'); ?></a></li>
+		<li><a class="icon-config btn" href="<?php echo JRoute::_('index.php?option='.$this->option.'&controller='.$this->controller.'&task=status&app='.$this->status['toolname']); ?>"><?php echo JText::_('COM_TOOLS_TOOL_STATUS'); ?></a></li>
+		<li class="last"><a class="icon-add btn" href="<?php echo JRoute::_('index.php?option='.$this->option.'&controller='.$this->controller.'&task=create'); ?>"><?php echo JText::_('COM_TOOLS_CONTRIBTOOL_NEW_TOOL'); ?></a></li>
 	</ul>
 </div><!-- / #content-header-extra -->
 
@@ -75,48 +75,59 @@ if ($this->licenses)
 			<h3>
 				<?php echo ($this->action == 'edit') ? JText::_('Specify license for next tool release:') : JText::_('Please confirm your license for this tool release:'); ?>
 			</h3>
-			<form action="<?php echo JRoute::_('index.php?option=' . $this->option . '&controller=' . $this->controller . '&task=license&app=' . $this->status['toolname']); ?>" method="post" id="versionForm" name="versionForm">
+			<form action="<?php echo JRoute::_('index.php?option=' . $this->option . '&controller=' . $this->controller . '&task=license&app=' . $this->status['toolname']); ?>" method="post" id="licenseForm" name="licenseForm">
 				<fieldset class="versionfield">
 					<label><?php echo JText::_('COM_TOOLS_CODE_ACCESS'); ?>:</label> 
 					<?php echo ToolsHelperHtml::formSelect('t_code', 't_code', $codeChoices, $this->code, 'shifted', ''); ?>
-
-					<div id="lic_cl"><?php echo JText::_('COM_TOOLS_LICENSE'); ?>:</div>
-					<div class="licinput" >
-						<textarea name="license" cols="50" rows="15" id="license"><?php echo $this->escape(stripslashes($this->license_choice['text'])); ?></textarea>
-						<?php 
-						if ($this->licenses) 
-						{
-							foreach ($this->licenses as $l) 
+					<span id="choice-icon">&nbsp;</span>
+					<div id="closed-source">
+						<h4><?php echo JText::_('COM_TOOLS_LICENSE_ARE_YOU_SURE'); ?></h4>
+						<?php echo JText::_('COM_TOOLS_LICENSE_WHY_OPEN_SOURCE'); ?>
+						<label><?php echo JText::_('COM_TOOLS_LICENSE_CLOSED_REASON'); ?> <span class="required"><?php echo JText::_('COM_TOOLS_REQUIRED'); ?></span>
+							<textarea name="reason" cols="30" rows="5" id="reason"></textarea>
+						</label>
+					</div>
+					<div id="open-source">
+						<div id="lic">
+							<label><?php echo JText::_('COM_TOOLS_LICENSE_TEMPLATE'); ?>:</label> 
+							<?php echo ToolsHelperHtml::formSelect('templates', 'templates',  $licenseChoices, $this->license_choice['template'], 'shifted', ''); ?>
+						</div>
+						<div class="licinput" >
+							<label><span class="required"><?php echo JText::_('COM_TOOLS_REQUIRED'); ?></span>
+							<textarea name="license" cols="50" rows="15" id="license" placeholder="<?php echo JText::_('COM_TOOLS_ENTER_LICENSE_TEXT'); ?>"> <?php echo $this->escape(stripslashes($this->license_choice['text'])); ?></textarea>
+							</label>
+							<?php 
+							if ($this->licenses) 
 							{
-								echo '<input type="hidden" name="' . $l->name . '" id="' . $l->name . '" value="'.$this->escape(stripslashes($l->text)).'" />' . "\n";
-							}
-						} 
-						?>
-						<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
-						<input type="hidden" name="controller" value="<?php echo $this->controller; ?>" />
-						<input type="hidden" name="task" value="savelicense" />
-						<input type="hidden" name="curcode" id="curcode" value="<?php echo $open; ?>" />
-						<input type="hidden" name="newstate" value="<?php echo $newstate; ?>" />
-						<input type="hidden" name="action" value="<?php echo $this->action; ?>" />
-						<input type="hidden" name="toolid" value="<?php echo $this->status['toolid']; ?>" />
-						<input type="hidden" name="alias" value="<?php echo $this->status['toolname']; ?>" />
-					</div>
-					<div id="lic">
-						<label><?php echo JText::_('COM_TOOLS_LICENSE_TEMPLATE'); ?>:</label> 
-						<?php echo ToolsHelperHtml::formSelect('templates', 'templates',  $licenseChoices, $this->license_choice['template'], 'shifted', ''); ?>
-					</div>
-					<div id="legendnotes">
-						<p>
-							<?php echo JText::_('COM_TOOLS_LICENSE_TEMPLATE_TIP'); ?>:
-							<br />[<?php echo strtoupper(JText::_('COM_TOOLS_YEAR')); ?>]
-							<br />[<?php echo strtoupper(JText::_('COM_TOOLS_OWNER')); ?>]
-							<br />[<?php echo strtoupper(JText::_('COM_TOOLS_ORGANIZATION')); ?>]
-							<br />[<?php echo strtoupper(JText::_('COM_TOOLS_ONE_LINE_DESCRIPTION')); ?>]
-							<br />[<?php echo strtoupper(JText::_('COM_TOOLS_URL')); ?>]
-						</p>
+								foreach ($this->licenses as $l) 
+								{
+									echo '<div class="hidden" id="' . $l->name . '" >' . $this->escape(stripslashes($l->text)) . '</div>' . "\n";
+								}
+							} 
+							?>
+							<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
+							<input type="hidden" name="controller" value="<?php echo $this->controller; ?>" />
+							<input type="hidden" name="task" value="savelicense" />
+							<input type="hidden" name="curcode" id="curcode" value="<?php echo $open; ?>" />
+							<input type="hidden" name="newstate" value="<?php echo $newstate; ?>" />
+							<input type="hidden" name="action" value="<?php echo $this->action; ?>" />
+							<input type="hidden" name="toolid" value="<?php echo $this->status['toolid']; ?>" />
+							<input type="hidden" name="alias" value="<?php echo $this->status['toolname']; ?>" />
+						</div>
+						<div id="legendnotes">
+							<h3><?php echo JText::_('COM_TOOLS_CONTRIBTOOL_LICENSE_TEMPLATE_CHOICE'); ?></h3>
+							<p>
+								<?php echo JText::_('COM_TOOLS_LICENSE_TEMPLATE_TIP'); ?>:
+								<br />[<?php echo strtoupper(JText::_('COM_TOOLS_YEAR')); ?>]
+								<br />[<?php echo strtoupper(JText::_('COM_TOOLS_OWNER')); ?>]
+								<br />[<?php echo strtoupper(JText::_('COM_TOOLS_ORGANIZATION')); ?>]
+								<br />[<?php echo strtoupper(JText::_('COM_TOOLS_ONE_LINE_DESCRIPTION')); ?>]
+								<br />[<?php echo strtoupper(JText::_('COM_TOOLS_URL')); ?>]
+							</p>
+						</div>
 						<label for="field-authorize">
 							<input type="checkbox" name="authorize" id="field-authorize" value="1" /> 
-							<?php echo JText::_('COM_TOOLS_LICENSE_CERTIFY').' <strong>'.JText::_('COM_TOOLS_OPEN_SOURCE').'</strong> '.JText::_('COM_TOOLS_LICENSE_UNDER_SPECIFIED'); ?>
+							<span class="required"><?php echo JText::_('COM_TOOLS_REQUIRED'); ?></span><?php echo JText::_('COM_TOOLS_LICENSE_CERTIFY') . ' '.JText::_('COM_TOOLS_LICENSE_UNDER_SPECIFIED'); ?>
 						</label>
 					</div>
 					<div class="moveon">
@@ -129,9 +140,6 @@ if ($this->licenses)
 			<h3><?php echo JText::_('COM_TOOLS_CONTRIBTOOL_LICENSE_WHAT_OPTIONS'); ?></h3>
 			<p class="opensource">
 				<?php echo '<strong>'.ucfirst(JText::_('COM_TOOLS_OPEN_SOURCE')).'</strong><br />'.JText::_('COM_TOOLS_CONTRIBTOOL_LICENSE_IF_YOU_CHOOSE').' <a href="http://www.opensource.org/" rel="external" title="Open Source Initiative">'.strtolower(JText::_('COM_TOOLS_OPEN_SOURCE')).'</a>, '.JText::_('COM_TOOLS_CONTRIBTOOL_LICENSE_OPEN_TXT'); ?>
-			</p>
-			<p class="error">
-				<?php echo JText::_('COM_TOOLS_CONTRIBTOOL_LICENSE_ATTENTION'); ?>
 			</p>
 			<p class="closedsource">
 				<strong><?php echo ucfirst(JText::_('COM_TOOLS_CLOSED_SOURCE')); ?></strong><br /><?php echo JText::_('COM_TOOLS_CONTRIBTOOL_LICENSE_CLOSED_TXT'); ?>
