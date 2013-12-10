9824c2b3f3a907e6cf479f87b8c1c5a47614110e
Sam Wilson <samwilson@purdue.edu>
2013-12-10 09:31:27 -0500

    [NANOHUB][#265634][COM_USERS] fix custom footer content to reference com_users, rather than com_user if applicable

---
+++ migrations/Migration20131209221353ComUsers.php
@@ -0,0 +1,37 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for updating custom footer module links to point to com_users, rather than com_user
+ **/
+class Migration20131209221353ComUsers extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query = "SELECT `id`, `content` FROM `#__modules` WHERE `position` = 'footer' AND `module` = 'mod_custom'";
+		$db->setQuery($query);
+		$results = $db->loadObjectList();
+
+		if ($results && count($results) > 0)
+		{
+			foreach ($results as $r)
+			{
+				$look_for     = array('/user/remind', '/user/reset');
+				$replace_with = array('/users/remind', '/users/reset');
+				$new_content  = str_replace($look_for, $replace_with, $r->content);
+
+				if ($new_content != $r->content)
+				{
+					$query = "UPDATE `#__modules` SET `content` = " . $db->quote($new_content) . " WHERE `id` = " . $db->quote($r->id);
+					$db->setQuery($query);
+					$db->query();
+				}
+			}
+		}
+	}
+}
