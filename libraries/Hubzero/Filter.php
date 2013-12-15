a6f0d4a9813bb2c3327b07d002fecaffbe578c21
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2013-12-15 16:56:10 -0500

    [LIB_HUBZERO] correct 'public public' typo that idiot nick introduced

--- libraries/Hubzero/Filter.php
+++ libraries/Hubzero/Filter.php
@@ -42,7 +42,7 @@ class Hubzero_Filter
 	 * @param      unknown $string Parameter description (if any) ...
 	 * @return     unknown Return description (if any) ...
 	 */
-	public public function cleanXss($string)
+	public static function cleanXss($string)
 	{
 		if (get_magic_quotes_gpc()) 
 		{
