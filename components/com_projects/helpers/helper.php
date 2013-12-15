84130878d9e67088db678e4aba25afee2cdf56d7
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2013-12-15 13:50:53 -0500

    [COM_PROJECTS] make getSuggestions() static

--- components/com_projects/helpers/helper.php
+++ components/com_projects/helpers/helper.php
@@ -322,7 +322,7 @@ class ProjectsHelper extends JObject {
 	 * @param      array $params
 	 * @return     void
 	 */	
-	public function getSuggestions( $project, $option, $uid = 0, $config, $params ) 
+	public static function getSuggestions( $project, $option, $uid = 0, $config, $params ) 
 	{	
 		$suggestions = array();
 		$creator = $project->created_by_user == $uid ? 1 : 0;
