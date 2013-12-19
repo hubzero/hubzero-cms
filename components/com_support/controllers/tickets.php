77cc0aba71fe6025c831ec182aa92f47aaeea9fc
Shawn Rice <zooley@purdue.edu>
2013-12-19 10:20:10 -0500

    [HUBZERO][#3674][COM_SUPPORT] Fixed case where table class was renamed.

--- components/com_support/controllers/tickets.php
+++ components/com_support/controllers/tickets.php
@@ -1712,7 +1712,7 @@ class SupportControllerTickets extends Hubzero_Controller
 
 		// Get tag/group associations
 		//include_once(JPATH_ROOT . DS . 'components' . DS . 'com_tags' . DS . 'helpers' . DS . 'handler.php');
-		$tt = new TagsGroup($this->database);
+		$tt = new TagsTableGroup($this->database);
 		$tgas = $tt->getRecords();
 
 		if (!$tgas) 
