1d415da50ee856c9833a9cdbd2843c980730dd08
Sam Wilson <samwilson@purdue.edu>
2013-12-20 13:24:30 -0500

    [HUBZERO][#3698][COM_REGISTER] generated username shouldn't have upper case letters

--- libraries/Hubzero/Registration.php
+++ libraries/Hubzero/Registration.php
@@ -1288,6 +1288,7 @@ class Hubzero_Registration
 	public function generateUsername($email)
 	{
 		$loginMaxLen = 32;
+		$email = strtolower($email);
 		
 		ximport('Hubzero_Registration_Helper');
 		$email = explode('@', $email);
