d85e5386a88d587b29ccead4e70faf569afd5eb7
Christopher Smoak <csmoak@purdue.edu>
2014-01-03 15:26:01 -0500

    [MOD_GROUPPAGES] Module for admin panel to display pages and modules needing approval.

---
+++ administrator/modules/mod_grouppages/helper.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Christopher Smoak <csmoak@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Module class for showing users online
+ */
+class modGroupPages extends Hubzero_Module
+{
+	/**
+	 * Display module contents
+	 * 
+	 * @return     void
+	 */
+	public function display()
+	{
+		//create objects
+		$this->juser    = JFactory::getUser();
+		$this->database = JFactory::getDBO();
+		
+		// include group page archive model
+		require_once JPATH_ROOT . DS . 'components' . DS . 'com_groups' . DS . 'models' . DS . 'page' . DS . 'archive.php';
+		
+		// include group module archive model
+		require_once JPATH_ROOT . DS . 'components' . DS . 'com_groups' . DS . 'models' . DS . 'module' . DS . 'archive.php';
+		
+		// get unapproved pages
+		$groupModelPageArchive = new GroupsModelPageArchive();
+		$this->unapprovedPages = $groupModelPageArchive->pages('unapproved', array(
+			'state' => array(0,1)
+		), true);
+		
+		// get unapproved modules
+		$groupModelModuleArchive = new GroupsModelModuleArchive();
+		$this->unapprovedModules = $groupModelModuleArchive->modules('unapproved', array(
+			'state' => array(0,1)
+		), true);
+		
+		//add stylesheets to view
+		$document = JFactory::getDocument();
+		$document->addStyleSheet('/administrator/modules/' . $this->module->module . '/' . $this->module->module . '.css');
+
+		// Get the view
+		require(JModuleHelper::getLayoutPath($this->module->module));
+	}
+}
---
+++ administrator/modules/mod_grouppages/tmpl/default.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Christopher Smoak <csmoak@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+$rows = array();
+foreach ($this->unapprovedPages as $unapprovedPage)
+{
+	$gidNumber = $unapprovedPage->get('gidNumber');
+	$rows[$gidNumber]['pages']++;
+}
+foreach ($this->unapprovedModules as $unapprovedModule)
+{
+	$gidNumber = $unapprovedModule->get('gidNumber');
+	$rows[$gidNumber]['modules']++;
+}
+$rows = array();
+?>
+<!--
+<table class="whosonline-summary">
+	<thead>
+		<tr>
+			<th width="50%" scope="col"><?php echo JText::_( 'Unapproved Pages' ); ?></th>
+			<th scope="col"><?php echo JText::_( 'Unapproved Modules' ); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="front-end"><?php echo $this->unapprovedPages->count(); ?></td>
+			<td class="back-end"><?php echo $this->unapprovedModules->count(); ?></td>
+		</tr>
+	</tbody>
+</table>
+-->
+
+<table class="adminlist whosonline-list">
+	<thead>
+		<tr>
+			<td class="title" width="60%">
+				<strong><?php echo JText::_( 'Group' ); ?></strong>
+			</td>
+			<td class="title">
+				<strong><?php echo JText::_( '# Pages' ); ?></strong>
+			</td>
+			<td class="title">
+				<strong><?php echo JText::_( '# Modules' ); ?></strong>
+			</td>
+		</tr>
+	</thead>
+	<tbody>
+		<?php if(count($rows) > 0) : ?>
+			<?php foreach ($rows as $gidNumber => $row) : ?>
+				<tr>
+					<td>
+						<?php 
+							$group = Hubzero_Group::getInstance($gidNumber);
+							echo $group->get('description'); 
+						?>
+					</td>
+					<td>
+						<a class="page" href="index.php?option=com_groups&amp;gid=<?php echo $group->get('cn'); ?>&amp;controller=pages">
+							<?php echo ($row['pages']) ? $row['pages'] : 0; ?>
+						</a>
+					</td>
+					<td>
+						<a class="module" href="index.php?option=com_groups&amp;gid=<?php echo $group->get('cn'); ?>&amp;controller=modules">
+							<?php echo ($row['modules']) ? $row['modules'] : 0; ?>
+						</a>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		<?php else : ?>
+			<tr>
+				<td colspan="3">
+					<em>There are currently no group pages or modules needing approval.</em>
+				</td>
+			</tr>
+		<?php endif; ?>
+	</tbody>
+</table>
