bfa9f255b9cef6b6ff201db90b8ab1de6943902f
Christopher Smoak <csmoak@purdue.edu>
2014-01-09 16:47:30 -0500

    [COM_GROUPS] Added migration for pages checkout

---
+++ migrations/Migration20140109024336ComGroups.php
@@ -0,0 +1,50 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20140109024336ComGroups extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query = "CREATE TABLE IF NOT EXISTS `#__xgroups_pages_checkout` (
+					`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+					`pageid` int(11) DEFAULT NULL,
+					`userid` int(11) DEFAULT NULL,
+					`when` datetime DEFAULT NULL,
+				PRIMARY KEY (`id`)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+
+		if (!empty($query))
+		{
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	protected static function down($db)
+	{
+		// delete categories table
+		if ($db->tableExists('#__xgroups_pages_checkout'))
+		{
+			$query = "DROP TABLE #__xgroups_pages_checkout;";
+			$db->setQuery($query);
+			$db->query();
+		}
+		
+		if (!empty($query))
+		{
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+}
