1f2301d89e661c93da05f85ffa23d4be998c545d
Shawn Rice <zooley@purdue.edu>
2014-01-10 13:32:09 -0500

    [COM_WEBLINKS] Removal of unused stock Joomla component and associated plugins, modules

---
+++ migrations/Migration20140110132217ComWeblinks.php
@@ -0,0 +1,110 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20140110132217ComWeblinks extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query = "SELECT `extension_id` FROM `#__extensions` WHERE `type`='component' AND `element`='com_weblinks';";
+
+		$db->setQuery($query);
+
+		if ($id = $db->loadResult())
+		{
+			self::deleteComponentEntry('weblinks');
+
+			self::deletePluginEntry('search', 'weblinks');
+
+			self::deleteModuleEntry('mod_weblinks');
+
+			$query = "SELECT `id` FROM `#__modules` WHERE `module`='mod_weblinks';";
+			$db->setQuery($query);
+			if ($results = $db->loadResultArray())
+			{
+				$query = "DELETE FROM `#__modules_menu` WHERE `moduleid` IN (" . implode(',', $results) . ");";
+				$db->setQuery($query);
+				$db->query();
+
+				$query = "DELETE FROM `#__modules` WHERE `module`='mod_weblinks';";
+				$db->setQuery($query);
+				$db->query();
+			}
+
+			$query = "DROP TABLE IF EXISTS `#__weblinks`;";
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	protected static function down($db)
+	{
+		$query = "SELECT `extension_id` FROM `#__extensions` WHERE `type`='component' AND `element`='com_weblinks';";
+
+		$db->setQuery($query);
+
+		if (!($id = $db->loadResult()))
+		{
+			self::addComponentEntry('weblinks');
+
+			self::addPluginEntry('weblinks', 'contacts', 0);
+
+			if (!$db->tableExists('#__weblinks'))
+			{
+				$query = "CREATE TABLE `#__weblinks` (
+					  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+					  `catid` int(11) NOT NULL DEFAULT '0',
+					  `sid` int(11) NOT NULL DEFAULT '0',
+					  `title` varchar(250) NOT NULL DEFAULT '',
+					  `alias` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+					  `url` varchar(250) NOT NULL DEFAULT '',
+					  `description` text NOT NULL,
+					  `date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+					  `hits` int(11) NOT NULL DEFAULT '0',
+					  `state` tinyint(1) NOT NULL DEFAULT '0',
+					  `checked_out` int(11) NOT NULL DEFAULT '0',
+					  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+					  `ordering` int(11) NOT NULL DEFAULT '0',
+					  `archived` tinyint(1) NOT NULL DEFAULT '0',
+					  `approved` tinyint(1) NOT NULL DEFAULT '1',
+					  `access` int(11) NOT NULL DEFAULT '1',
+					  `params` text NOT NULL,
+					  `language` char(7) NOT NULL DEFAULT '',
+					  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+					  `created_by` int(10) unsigned NOT NULL DEFAULT '0',
+					  `created_by_alias` varchar(255) NOT NULL DEFAULT '',
+					  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+					  `modified_by` int(10) unsigned NOT NULL DEFAULT '0',
+					  `metakey` text NOT NULL,
+					  `metadesc` text NOT NULL,
+					  `metadata` text NOT NULL,
+					  `featured` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'Set if link is featured.',
+					  `xreference` varchar(50) NOT NULL COMMENT 'A reference to enable linkages to external data sets.',
+					  `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+					  `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+					  PRIMARY KEY (`id`),
+					  KEY `idx_access` (`access`),
+					  KEY `idx_checkout` (`checked_out`),
+					  KEY `idx_state` (`state`),
+					  KEY `idx_catid` (`catid`),
+					  KEY `idx_createdby` (`created_by`),
+					  KEY `idx_featured_catid` (`featured`,`catid`),
+					  KEY `idx_language` (`language`),
+					  KEY `idx_xreference` (`xreference`)
+					) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+				$db->setQuery($query);
+				$db->query();
+			}
+		}
+	}
+}
