b0b161537a5d211bb2e0022b940c42383617d450
Shawn Rice <zooley@purdue.edu>
2014-01-10 13:15:59 -0500

    [COM_WRAPPER] Removal of unused stock Joomla component

---
+++ migrations/Migration20140110125436ComWrapper.php
@@ -0,0 +1,40 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20140110125436ComWrapper extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query = "SELECT `extension_id` FROM `#__extensions` WHERE `type`='component' AND `element`='com_wrapper';";
+
+		$db->setQuery($query);
+
+		if ($id = $db->loadResult())
+		{
+			self::deleteComponentEntry('wrapper');
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	protected static function down($db)
+	{
+		$query = "SELECT `extension_id` FROM `#__extensions` WHERE `type`='component' AND `element`='com_wrapper';";
+
+		$db->setQuery($query);
+
+		if (!($id = $db->loadResult()))
+		{
+			self::addComponentEntry('wrapper');
+		}
+	}
+}
