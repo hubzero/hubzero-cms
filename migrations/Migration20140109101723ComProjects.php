4d8c0dfe918bc2d9cfe073b4b218e828832f1179
Alissa Nedossekina <alisa@purdue.edu>
2014-01-14 16:37:33 -0500

    [COM_PROJECTS] changing type of blogentry in jos_project_microblog to TEXT to allow for extended entries

---
+++ migrations/Migration20140109101723ComProjects.php
@@ -0,0 +1,20 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script to change column type to TEXT to allow extended blog entry
+ **/
+class Migration20140109101723ComProjects extends Hubzero_Migration
+{
+	/**
+	 * Up
+	 **/
+	protected static function up($db)
+	{
+		$query = "ALTER TABLE `#__project_microblog` MODIFY `blogentry` TEXT DEFAULT NULL;";
+		$db->setQuery($query);
+		$db->query();
+	}
+}
