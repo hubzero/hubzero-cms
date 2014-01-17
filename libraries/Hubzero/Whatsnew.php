9cb1e1e56fac75f6c07faee0386176d0e936f74a
Shawn Rice <zooley@purdue.edu>
2014-01-17 09:11:58 -0500

    [LIB_HUBZERO] Fixed formatting, filled in docblock

--- libraries/Hubzero/Whatsnew.php
+++ libraries/Hubzero/Whatsnew.php
@@ -29,61 +29,79 @@
  */
 
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
+/**
+ * Helper class for returning "what's new" results
+ */
 class Hubzero_Whatsnew
 {
-	public static function getWhatsNewBasedOnPeriodAndCategory( $period = 'month', $category = '', $limit = 0 )
+	/**
+	 * Get what's new based on a time period and category
+	 * 
+	 * @param   string  $period   Time period to return results for
+	 * @param   string  $category Category to filter by
+	 * @param   integer $limit    Limit number of results returned
+	 * @return  array
+	 */
+	public static function getWhatsNewBasedOnPeriodAndCategory($period = 'month', $category = '', $limit = 0)
 	{
-		//parse the time period for use by the whats new plugins
-		$p = Hubzero_Whatsnew::parseTimePeriod( $period );
-		
-		//load whats new plugins
+		// parse the time period for use by the whats new plugins
+		$p = self::parseTimePeriod( $period );
+
+		// load whats new plugins
 		JPluginHelper::importPlugin('whatsnew');
 		$dispatcher = JDispatcher::getInstance();
-		
-		//get the search areas
+
+		// get the search areas
 		$areas = array();
 		$search_areas = $dispatcher->trigger('onWhatsNewAreas');
-		foreach($search_areas as $search_area)
+		foreach ($search_areas as $search_area)
 		{
 			$areas = array_merge($areas, $search_area);
 		}
-		
-		//get the results
+
+		// get the results
 		$config = array($p, $limit, 0, $areas);
-		$results = $dispatcher->trigger( 'onWhatsNew', $config );
-		
+		$results = $dispatcher->trigger('onWhatsNew', $config);
+
 		$new = array();
 		$i = 0;
 		foreach($areas as $k => $area)
 		{
-			$new[$i]['alias'] = $k;
-			$new[$i]['title'] = ($k == 'resources') ? 'Resources' : $area;
+			$new[$i]['alias']   = $k;
+			$new[$i]['title']   = ($k == 'resources') ? 'Resources' : $area;
 			$new[$i]['results'] = $results[$i];
 			$i++;
 		}
-		
-		//check to see if we only want to return results for a certain category
-		if($category != '')
+
+		// check to see if we only want to return results for a certain category
+		if ($category != '')
 		{
 			$index = 0;
-			foreach($areas as $k => $area)
+			foreach ($areas as $k => $area)
 			{
-				if($category == $k)
+				if ($category == $k)
 				{
 					return $new[$index];
 				}
 				$index++;
 			}
 		}
-		
+
 		return $new;
 	}
-	
-	//-----
-	
-	public static function parseTimePeriod( $period )
+
+	/**
+	 * Parse a string into a time period
+	 *
+	 * For example, "month" will calculate a timestamp (YYYY-MM-DD hh:mm:ss)
+	 * of 1 month from "now"
+	 * 
+	 * @param   string $period Time period
+	 * @return  object
+	 */
+	public static function parseTimePeriod($period)
 	{
 		require_once(JPATH_ROOT . DS . 'components' . DS . 'com_whatsnew' . DS . 'helpers' . DS . 'period.php'); 
 		$p = new WhatsnewPeriod($period);
