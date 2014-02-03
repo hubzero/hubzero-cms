26cbc10949a05a6ba31c474c8b6b4ecf2d5c0b9e
Alissa Nedossekina <alisa@purdue.edu>
2014-02-03 16:16:43 -0500

    [NANOHUB][#266025][COM_TOOLS] make sure delete icon shows in Chrome

--- components/com_tools/views/attachments/tmpl/display.php
+++ components/com_tools/views/attachments/tmpl/display.php
@@ -141,7 +141,7 @@ if ($this->children) {
   		    	$out .= '&nbsp;';
 			}
 			$out .= '</td>';
-			$out .= '  <td class="t"><a href="index.php?option='.$this->option.'&amp;controller='.$this->controller.'&amp;task=delete&amp;tmpl=component&amp;id='.$child->id.'&amp;pid='.$this->resource->id.'"><img src="/components/com_tools/assets/img/trash.gif" alt="'.JText::_('COM_TOOLS_DELETE').'" /></a></td>';
+			$out .= '  <td class="t"><a href="index.php?option='.$this->option.'&amp;controller='.$this->controller.'&amp;task=delete&amp;tmpl=component&amp;id='.$child->id.'&amp;pid='.$this->resource->id.'" class="delete_ss"><span> ' . JText::_('COM_TOOLS_DELETE') . '</span></a></td>';
 		}
 		$out .= ' </tr>';
 
