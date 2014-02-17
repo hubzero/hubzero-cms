dc71826a7f95e03ef608a7f8868bb8e539753557
Sam Wilson <samwilson@purdue.edu>
2014-02-17 14:54:16 -0500

    Fix up migration to conform to new standards

--- migrations/Migration20140217101623ModIncrementalRegistration.php
+++ migrations/Migration20140217101623ModIncrementalRegistration.php
@@ -1,40 +1,66 @@
 <?php
 
+use Hubzero\Content\Migration\Base;
+
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
 /**
  * Migration script for ...
  **/
-class Migration20140217101623ModIncrementalRegistration extends Hubzero_Migration
+class Migration20140217101623ModIncrementalRegistration extends Base
 {
 	/**
 	 * Up
 	 **/
-	protected static function up($db)
+	public function up()
 	{
-		foreach (array(
-				'INSERT INTO #__incremental_registration_labels(field, label) VALUES (\'location\', \'Postal Code\')',
-				'ALTER TABLE #__xprofiles ADD COLUMN `location` VARCHAR(50)',
-				'ALTER TABLE #__profile_completion_awards ADD COLUMN `location` TINYINT NOT NULL DEFAULT 0'
-			) as $query) {
-			$db->setQuery($query);
-			$db->query();
+		if ($this->db->tableExists('#__incremental_registration_labels'))
+		{
+			$query = "INSERT INTO `#__incremental_registration_labels`(field, label) VALUES ('location', 'Postal Code')";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+
+		if ($this->db->tableExists('#__xprofiles') && !$this->db->tableHasField('#__xprofiles', 'location'))
+		{
+			$query = "ALTER TABLE `#__xprofiles` ADD COLUMN `location` VARCHAR(50)";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+
+		if ($this->db->tableExists('#__profile_completion_awards') && !$this->db->tableHasField('#__profile_completion_awards', 'location'))
+		{
+			$query = "ALTER TABLE `#__profile_completion_awards` ADD COLUMN `location` TINYINT NOT NULL DEFAULT 0";
+			$this->db->setQuery($query);
+			$this->db->query();
 		}
 	}
 
 	/**
 	 * Down
 	 **/
-	protected static function down($db)
+	public function down()
 	{
-		foreach (array(
-				'DELETE FROM #__incremental_registration_labels WHERE field = \'location\'',
-				'ALTER TABLE #__xprofiles DROP COLUMN `location`',
-				'ALTER TABLE #__profile_completion_awards DROP COLUMN `location`'
-			) as $query) {
-			$db->setQuery($query);
-			$db->query();
+		if ($this->db->tableExists('#__incremental_registration_labels'))
+		{
+			$query = "DELETE FROM `#__incremental_registration_labels` WHERE field = 'location'";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+
+		if ($this->db->tableExists('#__xprofiles') && $this->db->tableHasField('#__xprofiles', 'location'))
+		{
+			$query = "ALTER TABLE `#__xprofiles` DROP COLUMN `location`";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+
+		if ($this->db->tableExists('#__profile_completion_awards') && $this->db->tableHasField('#__profile_completion_awards', 'location'))
+		{
+			$query = "ALTER TABLE `#__profile_completion_awards` DROP COLUMN `location`";
+			$this->db->setQuery($query);
+			$this->db->query();
 		}
 	}
 }
