0cc3a344c8fd498bc444fc4acd339017ec6e3acf
Shawn Rice <zooley@purdue.edu>
2014-02-17 11:26:25 -0500

    [COM_CRON] Extend SiteController instead of Hubzero_Cotnroller

--- components/com_cron/controllers/jobs.php
+++ components/com_cron/controllers/jobs.php
@@ -31,12 +31,10 @@
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
-ximport('Hubzero_Controller');
-
 /**
  * Controller class for bulletin boards
  */
-class CronControllerJobs extends Hubzero_Controller
+class CronControllerJobs extends \Hubzero\Component\SiteController
 {
 	/**
 	 * Determines task being called and attempts to execute it
