e90b04c9c66aa07fe6dc71d19b47c6bf48d5b032
Shawn Rice <zooley@purdue.edu>
2014-02-18 11:11:35 -0500

    [HUBZERO][#4029][PLG_HUBZERO_WIKIPARSER] Hopefully fixed issue with time not being dipslayed properly

--- plugins/hubzero/wikiparser/macros/file.php
+++ plugins/hubzero/wikiparser/macros/file.php
@@ -657,7 +657,7 @@ class FileMacro extends WikiMacro
 						}
 						if (isset($attr['created']))
 						{
-							$html .= ' ' . Hubzero_View_Helper_Html::timeAgo($attr['created']);
+							$html .= ' ' . Hubzero_View_Helper_Html::timeAgo(Hubzero_View_Helper_Html::mkt(JHTML::_('date', $attr['created'], JFactory::getDBO()->getDateFormat())));
 						}
 						$html .= '</span>)';
 					}
--- plugins/hubzero/wikiparser/macros/fileindex.php
+++ plugins/hubzero/wikiparser/macros/fileindex.php
@@ -97,7 +97,7 @@ class FileIndexMacro extends WikiMacro
 				/*$html .= ' * ['.$url;
 				$html .= ($row->title) ? ' '.stripslashes($row->title) : ' '.$row->pagename;
 				$html .= ']'."\n";*/
-				$html .= '<li><a href="' . JRoute::_($link) . '">' . $row->filename . '</a> (' . Hubzero_View_Helper_Html::formatSize(filesize($fpath)) . ') ';
+				$html .= '<li><a href="' . JRoute::_($link) . '">' . $row->filename . '</a> (' . (file_exists($fpath) ? Hubzero_View_Helper_Html::formatSize(filesize($fpath)) : '-- file not found --') . ') ';
 				$huser = JUser::getInstance($row->created_by);
 				if ($huser->get('id'))
 				{
@@ -105,7 +105,7 @@ class FileIndexMacro extends WikiMacro
 				}
 				if ($row->created && $row->created != '0000-00-00 00:00:00')
 				{
-					$html .= Hubzero_View_Helper_Html::timeAgo($row->created) . '. ';
+					$html .= Hubzero_View_Helper_Html::timeAgo(Hubzero_View_Helper_Html::mkt(JHTML::_('date', $row->created, JFactory::getDBO()->getDateFormat()))) . '. ';
 				}
 				$html .= ($row->description) ? '<span>"' . stripslashes($row->description) . '"</span>' : '';
 				$html .= '</li>' . "\n";
