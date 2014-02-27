2f4fd720279a4d46d4f37dcb5ef2d81e266e13aa
Shawn Rice <zooley@purdue.edu>
2014-02-27 08:57:15 -0500

    [STEMEDHUB][#261][PLG_GROUPS_COLLECTIONS] Fixed issue where trying to removeAsset from post object which should have been post's item object

--- plugins/groups/collections/collections.php
+++ plugins/groups/collections/collections.php
@@ -963,9 +963,9 @@ class plgGroupsCollections extends \Hubzero\Plugin\Plugin
 
 		if ($remove = JRequest::getInt('remove', 0))
 		{
-			if (!$view->item->removeAsset($remove))
+			if (!$view->entry->item()->removeAsset($remove))
 			{
-				$view->setError($view->item->getError());
+				$view->setError($view->entry->item()->getError());
 			}
 		}
 
