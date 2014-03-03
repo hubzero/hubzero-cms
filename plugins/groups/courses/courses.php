1269e36215cbb60a02c5d28861455630b4e1e9a4
Shawn Rice <zooley@purdue.edu>
2014-03-03 12:43:55 -0500

    [PLG_GROUPS_COURSES] Adding groups plugin for listing courses associated with a group

---
+++ plugins/groups/courses/courses.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Members Plugin class for courses
+ */
+class plgGroupsCourses extends \Hubzero\Plugin\Plugin
+{
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 */
+	protected $_autoloadLanguage = true;
+
+	/**
+	 * Return the alias and name for this category of content
+	 * 
+	 * @return     array
+	 */
+	public function &onGroupAreas()
+	{
+		$area = array(
+			'name'             => $this->_name,
+			'title'            => JText::_('PLG_GROUPS_COURSES'),
+			'default_access'   => $this->params->get('plugin_access', 'anyone'),
+			'display_menu_tab' => $this->params->get('display_tab', 1),
+			'icon'             => 'f09c'
+		);
+		return $area;
+	}
+
+	/**
+	 * Return data on a group view (this will be some form of HTML)
+	 * 
+	 * @param      object  $group      Current group
+	 * @param      string  $option     Name of the component
+	 * @param      string  $authorized User's authorization level
+	 * @param      integer $limit      Number of records to pull
+	 * @param      integer $limitstart Start of records to pull
+	 * @param      string  $action     Action to perform
+	 * @param      array   $access     What can be accessed
+	 * @param      array   $areas      Active area(s)
+	 * @return     array
+	 */
+	public function onGroup($group, $option, $authorized, $limit=0, $limitstart=0, $action='', $access, $areas=null)
+	{
+		$return = 'html';
+		$active = $this->_name;
+		$active_real = 'discussion';
+
+		// The output array we're returning
+		$arr = array(
+			'html'=>'',
+			'name' => $active
+		);
+
+		//get this area details
+		$this_area = $this->onGroupAreas();
+
+		// Check if our area is in the array of areas we want to return results for
+		if (is_array($areas) && $limit) 
+		{
+			if (!in_array($this_area['name'], $areas)) 
+			{
+				$return = 'metadata';
+			}
+		}
+
+		$this->group    = $group;
+		$this->database = JFactory::getDBO();
+
+		require_once(JPATH_ROOT . DS . 'components' . DS . 'com_courses' . DS . 'models' . DS . 'courses.php');
+		$model = CoursesModelCourses::getInstance();
+
+		$filters = array(
+			'group'    => $group->get('cn'),
+			'group_id' => $group->get('gidNumber'),
+			'count'    => true
+		);
+
+		$arr['metadata']['count'] = $model->courses($filters);
+
+		// Build the HTML
+		if ($return == 'html') 
+		{
+			$this->app = JFactory::getApplication();
+			$this->jconfig = JFactory::getConfig();
+
+			$view = new \Hubzero\Plugin\View(
+				array(
+					'folder'  => $this->_type,
+					'element' => $this->_name,
+					'name'    => 'display'
+				)
+			);
+			$view->option = $option;
+			$view->group  = $group;
+
+			$view->filters = $filters;
+			$view->filters['count'] = false;
+			$view->filters['limit'] = $this->app->getUserStateFromRequest(
+				$option . '.plugin.courses.limit',
+				'limit',
+				$this->jconfig->getValue('config.list_limit'),
+				'int'
+			);
+			$view->filters['start'] = $this->app->getUserStateFromRequest(
+				$option . '.plugin.courses.limitstart',
+				'limitstart',
+				0,
+				'int'
+			);
+			$view->filters['sortby'] = $this->app->getUserStateFromRequest(
+				$option . '.plugin.courses.sortby',
+				'sortby',
+				''
+			);
+			$view->filters['search'] = $this->app->getUserStateFromRequest(
+				$option . '.plugin.courses.search',
+				'search',
+				''
+			);
+			$view->filters['index'] = '';
+			$view->filters['tag'] = '';
+
+			if (!in_array($view->filters['sortby'], array('alias', 'title', 'popularity')))
+			{
+				$view->filters['sortby'] = 'title';
+			}
+			switch ($view->filters['sortby'])
+			{
+				case 'popularity':
+					$view->filters['sort']  = 'students';
+					$view->filters['sort_Dir'] = 'DESC';
+				break;
+				case 'title':
+				case 'alias':
+				default:
+					$view->filters['sort']  = $view->filters['sortby'];
+					$view->filters['sort_Dir'] = 'ASC';
+				break;
+			}
+
+			$view->total   = $arr['metadata']['count'];
+			$view->results = $model->courses($view->filters);
+
+			$arr['html'] = $view->loadTemplate();
+		}
+
+		return $arr;
+	}
+}
---
+++ plugins/groups/courses/views/display/tmpl/default.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2011 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+$this->css();
+
+$base = 'index.php?option=' . $this->option . '&cn=' . $this->group->get('cn') . '&active=courses';
+?>
+<h3 class="section-header">
+	<?php echo JText::_('PLG_GROUPS_COURSES'); ?>
+</h3>
+
+<div class="section">
+<?php if (count($this->results) > 0) { ?>
+	<div class="container" id="courses-container">
+		<form method="get" action="<?php JRoute::_($base); ?>">
+
+			<?php
+			$qs  = ($this->filters['search'] ? '&search=' . $this->escape($this->filters['search']) : '');
+			//$qs .= ($this->filters['index']  ? '&index=' . $this->escape($this->filters['index'])   : '');
+			//$qs .= ($this->filters['tag']    ? '&tag=' . $this->escape($this->filters['tag'])       : '');
+			//$qs .= ($this->filters['group']  ? '&group=' . $this->escape($this->filters['group'])   : '');
+			?>
+			<ul class="entries-menu order-options">
+				<li><a<?php echo ($this->filters['sortby'] == 'title') ? ' class="active"' : ''; ?> href="<?php echo JRoute::_($base . '&sortby=title' . $qs); ?>" title="Sort by title">&darr; Title</a></li>
+				<li><a<?php echo ($this->filters['sortby'] == 'alias') ? ' class="active"' : ''; ?> href="<?php echo JRoute::_($base . '&sortby=alias' . $qs); ?>" title="Sort by alias">&darr; Alias</a></li>
+				<li><a<?php echo ($this->filters['sortby'] == 'popularity') ? ' class="active"' : ''; ?> href="<?php echo JRoute::_($base . '&sortby=popularity' . $qs); ?>" title="Sort by popularity">&darr; Popularity</a></li>
+			</ul>
+
+			<table class="courses entries" summary="<?php echo JText::_('PLG_GROUPS_COURSES_RESULTS_SUMMARY'); ?>">
+				<caption>
+					<?php 
+					$s = ($this->total > 0) ? $this->filters['start']+1 : 0; //($this->filters['start'] > 0) ? $this->filters['start']+1 : $this->filters['start'];
+					$e = ($this->total > ($this->filters['start'] + $this->filters['limit'])) ? ($this->filters['start'] + $this->filters['limit']) : $this->total;
+
+					echo $this->escape(JText::_('PLG_GROUPS_COURSES')); //JText::_('PLG_GROUPS_COURSES_' . strtoupper($this->filters['task'])); 
+					?>
+					<span>(<?php echo JText::sprintf('PLG_GROUPS_COURSES_RESULTS_TOTAL', $s, $e, $this->total); ?>)</span>
+				</caption>
+				<tbody>
+			<?php
+				foreach ($this->results as $course)
+				{
+			?>
+					<tr class="course<?php echo ($cls) ? ' ' . $cls : ''; ?>">
+						<th>
+							<span class="entry-id"><?php echo $course->get('id'); ?></span>
+						</th>
+						<td>
+							<a class="entry-title" href="<?php echo JRoute::_($course->link()); ?>">
+								<?php echo $this->escape(stripslashes($course->get('title'))); ?>
+							</a><br />
+						<?php
+							$instructors = $course->instructors();
+							if (count($instructors) > 0) 
+							{
+								$names = array();
+								foreach ($instructors as $i)
+								{
+									$instructor = \Hubzero\User\Profile::getInstance($i->get('user_id'));
+
+									$names[] = '<a href="' . JRoute::_('index.php?option=com_members&id=' . $i->get('user_id')) . '">' . $this->escape(stripslashes($instructor->get('name'))) . '</a>';
+								}
+						?>
+							<span class="entry-details">
+								Instructors: <span class="entry-instructors"><?php echo implode(', ', $names); ?></span>
+							</span>
+						<?php
+							}
+						?>
+							<span class="entry-content">
+								<?php echo \Hubzero\Utility\String::truncate(stripslashes($course->get('blurb')), 200); ?>
+							</span>
+						</td>
+					</tr>
+			<?php
+				}
+			?>
+				</tbody>
+			</table>
+
+			<?php 
+			jimport('joomla.html.pagination');
+			$pageNav = new JPagination(
+				$this->total, 
+				$this->filters['start'], 
+				$this->filters['limit']
+			);
+			$pageNav->setAdditionalUrlParam('cn', $this->group->get('cn'));
+			$pageNav->setAdditionalUrlParam('active', 'courses');
+			$pageNav->setAdditionalUrlParam('action', '');
+			$pageNav->setAdditionalUrlParam('sortby', $this->filters['sortby']);
+
+			echo $pageNav->getListFooter();
+			?>
+			<div class="clearfix"></div>
+		</form>
+	</div>
+<?php } else { ?>
+	<div id="courses-introduction">
+		<div class="instructions">
+			<ol>
+				<li><?php echo JText::sprintf('PLG_GROUPS_COURSES_BUILD', JRoute::_('index.php?option=com_courses')); ?></li>
+				<li><?php echo JText::_('PLG_GROUPS_COURSES_PUBLISH'); ?></li>
+				<li><?php echo JText::_('PLG_GROUPS_COURSES_TEACH'); ?></li>
+			</ol>
+		</div><!-- / .instructions -->
+		<div class="questions">
+			<p><strong><?php echo JText::_('PLG_GROUPS_COURSES_WHAT_ARE_COURSES'); ?></strong></p>
+			<p><?php echo JText::_('PLG_GROUPS_COURSES_EXPLANATION'); ?><p>
+		</div><!-- / .post-type -->
+	</div><!-- / #collection-introduction -->
+<?php } ?>
+</div>
