1128e8f20285fb8120d9b41289dd279193c27995
Shawn Rice <zooley@purdue.edu>
2014-03-05 08:15:49 -0500

    [COM_MEMBERS] Drop table that hasn't been used in years

---
+++ migrations/Migration20140305081320ComMembers.php
@@ -0,0 +1,39 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20140305081320ComMembers extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$query = "DROP TABLE IF EXISTS `#__xprofiles_tags`;";
+		$this->db->setQuery($query);
+		$this->db->query();
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$query = "CREATE TABLE `#__xprofiles_tags` (
+		  `id` int(11) NOT NULL AUTO_INCREMENT,
+		  `uidNumber` int(11) DEFAULT NULL,
+		  `tagid` int(11) DEFAULT NULL,
+		  `taggerid` int(11) DEFAULT '0',
+		  `taggedon` datetime DEFAULT '0000-00-00 00:00:00',
+		  PRIMARY KEY (`id`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+		$this->db->setQuery($query);
+		$this->db->query();
+	}
+}
