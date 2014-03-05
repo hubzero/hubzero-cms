3f42e18d642e2ff9b8537b2fdf44a0f00c854e10
Shawn Rice <zooley@purdue.edu>
2014-03-05 08:15:05 -0500

    [PLG_USAGE_CHART] Remove plugin entry for nonexistent plugin

---
+++ migrations/Migration20140305081020PlgUsageChart.php
@@ -0,0 +1,28 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20140305081020PlgUsageChart extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->deletePluginEntry('usage', 'chart');
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->addPluginEntry('usage', 'chart', 0);
+	}
+}
