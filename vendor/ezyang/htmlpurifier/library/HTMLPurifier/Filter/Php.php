6ca80c7caf5376d614bf2ea18fa125eaae5ab031
Christopher Smoak <csmoak@purdue.edu>
2014-03-10 12:44:50 -0400

    [COMPOSER][HTMLPURIFIER] Fixed php filter to allow -> in content and not have it encoded.

--- vendor/ezyang/htmlpurifier/library/HTMLPurifier/Filter/Php.php
+++ vendor/ezyang/htmlpurifier/library/HTMLPurifier/Filter/Php.php
@@ -14,9 +14,13 @@ class HTMLPurifier_Filter_Php extends HTMLPurifier_Filter
 	
 	public function postFilter($html, $config, $context)
 	{
-		$html = str_replace('[php]', '<?php', $html);
-		$html = str_replace('[/php]', '?>', $html);
-		return $html;
+		$html = preg_replace_callback(
+					"#\\[php]([^\\[]*)\\[/php]#us",  
+					function($matches) {
+						return "<?php\n" . trim(html_entity_decode($matches[1])) . "\n?>";
+					},
+					$html);
+		return $html;		
 	}
 }
 
