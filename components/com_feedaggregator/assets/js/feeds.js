b65ab455ff31b8adc1854666db41812bf95e7479
Kevin Wojkovich <kevinw@purdue.edu>
2014-03-18 14:00:51 -0400

    [com_feedaggregator] Fixed required fields issue.

---
+++ components/com_feedaggregator/assets/js/feeds.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function() 
+{	
+	jQuery('.required-field').blur(function(){
+		if (jQuery(this).val() == '')
+		{
+			jQuery(this).attr('style', 'background-color: #FFD6CC;');
+		}
+		else
+		{
+			jQuery(this).attr('style', 'background-color: #FFFF;');
+		}
+	});
+	
+	jQuery('#submitBtn').click(function(){
+		event.preventDefault();
+		var submitToken = 0;
+		
+		jQuery('.required-field').each(function()
+		{
+			if(jQuery(this).val() == '')
+			{
+				submitToken = 0;
+			}
+			else if(jQuery(this).val() != '')
+			{
+				submitToken = 1;
+			}
+		});
+		
+		if(submitToken == 1)
+		{
+			jQuery('#hubForm').submit();
+		}
+		else
+		{
+			alert("Please check all required fields.");
+		}
+		
+	});
+	
+}); 
