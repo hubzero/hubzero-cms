870402887c7c403572f645873357322dc8f5e18d
Christopher Smoak <csmoak@purdue.edu>
2014-03-28 11:57:40 -0400

    [PLG_GROUPS_CALENDAR] Properly display event timezone identifiers.

--- plugins/groups/calendar/views/calendar/tmpl/details.php
+++ plugins/groups/calendar/views/calendar/tmpl/details.php
@@ -94,16 +94,14 @@ $month = date("m", strtotime($this->event->get('publish_up')));
 
 <table class="group-event-details">
 	<tbody>
+		<?php $timezone = timezone_name_from_abbr('',$this->event->get('time_zone')*3600, NULL); ?>
 		<?php if ($this->event->get('publish_down') != '0000-00-00 00:00:00') : ?>
 			<tr>
 				<th class="date"></th>
 				<td colspan="3">
-					<?php $tz = plgGroupsCalendarHelper::getTimezoneNameAndAbbreviation( $this->event->get('time_zone') ); ?>
-					<?php echo JHTML::_('date', $this->event->get('publish_up'), 'l, F d, Y @ g:i a'); ?>
-					<abbr title="<?php echo $tz['name']; ?>"><?php echo $tz['abbreviation']; ?></abbr>
+					<?php echo JHTML::_('date', $this->event->get('publish_up'), 'l, F d, Y @ g:i a T', $timezone); ?>
 					&mdash;
-					<?php echo JHTML::_('date', $this->event->get('publish_down'), 'l, F d, Y @ g:i a'); ?>
-					<abbr title="<?php echo $tz['name']; ?>"><?php echo $tz['abbreviation']; ?></abbr>
+					<?php echo JHTML::_('date', $this->event->get('publish_down'), 'l, F d, Y @ g:i a T', $timezone); ?>
 				</td>
 			</tr>
 		<?php else : ?>
@@ -114,9 +112,7 @@ $month = date("m", strtotime($this->event->get('publish_up')));
 				</td>
 				<th class="time"></th>
 				<td>
-					<?php echo JHTML::_('date', $this->event->get('publish_up'), JText::_('TIME_FORMAT_HZ1')); ?>
-					<?php $tz = plgGroupsCalendarHelper::getTimezoneNameAndAbbreviation( $this->event->get('time_zone') ); ?>
-					<abbr title="<?php echo $tz['name']; ?>"><?php echo $tz['abbreviation']; ?></abbr>
+					<?php echo JHTML::_('date', $this->event->get('publish_up'), 'g:i a T', $timezone); ?>
 				</td>
 			</tr>
 		<?php endif; ?>
