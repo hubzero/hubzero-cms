cfecbfa131b7bcd9e13fa03acfa43b33370cfe7c
Sam Wilson <samwilson@purdue.edu>
2014-04-02 15:27:25 -0400

    [HUBZERO][#1210][COM_POLL] add disclaimer about validity of results on backend too (credit: slashdot)

--- administrator/components/com_poll/views/poll/tmpl/form.php
+++ administrator/components/com_poll/views/poll/tmpl/form.php
@@ -105,6 +105,7 @@ JFilterOutput::objectHTMLSafe( $this->poll, ENT_QUOTES );
 		</tr>
 	</table>
 	</fieldset>
+	<p class="warning">This whole thing is wildly inaccurate. Rounding errors, ballot stuffers, dynamic IPs, firewalls. If you're using these numbers to do anything important, you're insane.</p>
 </div>
 <div class="col width-45 fltrt">
 	<fieldset class="adminform">
