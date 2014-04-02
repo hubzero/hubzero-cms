a174e6e52897b553816b6628a0d522c5bbfb0e00
Shawn Rice <zooley@purdue.edu>
2014-04-02 14:14:15 -0400

    [HABRI][#390][COM_TAGS] Don't limit list of tag aliases when editing a tag

--- administrator/components/com_tags/views/entries/tmpl/edit.php
+++ administrator/components/com_tags/views/entries/tmpl/edit.php
@@ -94,7 +94,7 @@ if ($this->getError())
 					</tr>
 					<tr>
 						<th class="key" style="vertical-align:top;"><label><?php echo JText::_('ALIAS'); ?>:</label></th>
-						<td><?php echo $editor->display('fields[substitutions]', stripslashes($this->tag->substitutes('string')), '100%', '200px', '50', '10'); ?></td>
+						<td><?php echo $editor->display('fields[substitutions]', stripslashes($this->tag->substitutes('string', array('limit' => 0))), '100%', '200px', '50', '10'); ?></td>
 					</tr>
 				</tbody>
 			</table>
