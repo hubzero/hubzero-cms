677ca5bb721c779bb82f7fdefa6174f96b720a2f
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2014-04-13 22:00:17 -0400

    [COM_SUPPORT] fix view variable references (matches 1.2.1 branch)

--- components/com_support/views/abuse/tmpl/display.php
+++ components/com_support/views/abuse/tmpl/display.php
@@ -64,7 +64,7 @@ if ($this->report) {
 				<div class="abuseitem">
 					<h4><?php 
 						echo ($this->report->href) ? '<a href="' . $this->report->href . '">': '';
-						echo ucfirst($this->report->parent_category) . ' by ';
+						echo ucfirst($this->cat) . ' by ';
 						echo ($this->report->anon != 0) ? JText::_('ANONYMOUS') : $name;
 						echo ($this->report->href) ? '</a>': '';
 					?></h4>
@@ -85,7 +85,7 @@ if ($this->report) {
 			<input type="hidden" name="option" value="<?php echo $this->escape($this->option); ?>" />
 			<input type="hidden" name="controller" value="<?php echo $this->escape($this->controller); ?>" />
 			<input type="hidden" name="task" value="save" />
-			<input type="hidden" name="category" value="<?php echo $this->escape($this->report->parent_category); ?>" />
+			<input type="hidden" name="category" value="<?php echo $this->escape($this->cat); ?>" />
 			<input type="hidden" name="referenceid" value="<?php echo $this->escape($this->refid); ?>" />
 			<input type="hidden" name="link" value="<?php echo $this->escape($this->report->href); ?>" />
 
