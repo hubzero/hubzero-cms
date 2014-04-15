57a71a0c35fedcce26f4e35a52717f021fa21c69
Shawn Rice <zooley@purdue.edu>
2014-04-15 09:15:34 -0400

    [MOD_MYRESOURCES] Removing mootools based javascript

--- modules/mod_myresources/mod_myresources.js
+++ modules/mod_myresources/mod_myresources.js
@@ -1,58 +1,42 @@
 /**
  * @package     hubzero-cms
  * @file        modules/mod_myresources/mod_myresources.js
- * @copyright   Copyright 2005-2011 Purdue University. All rights reserved.
+ * @copyright   Copyright 2005-2013 Purdue University. All rights reserved.
  * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
-//-----------------------------------------------------------
-//  Ensure we have our namespace
-//-----------------------------------------------------------
-if (!HUB) {
-	var HUB = {};
-}
-if (!HUB.Modules) {
-	HUB.Modules = {};
+if (!jq) {
+	var jq = $;
 }
 
-HUB.Modules.MyResources = {
-	baseURL: '/index.php?option=com_members&active=dashboard&no_html=1&init=1',
-	
-	initialize: function() {
-		this.form = $('myresources-form');
-		this.sort = $('myresources-sort');
-		this.limit = $('myresources-limit');
-		
-		this.container = $(this.form.getParent().getParent().getParent());
-		this.id = this.container.getProperty('id').replace('mod_','');
-		
-		// AJAX: send request to server-side script to generate and return contents of new LIst item
-		this.uid = $('uid').value;
-
-		this.sort.addEvent('change', function() {
-			//var myAjax1 = new Ajax(HUB.Myhub.baseURL+'&task=addmodule&uid='+uid+'&id='+id,{update:'myresources-content'}).request();
-			//HUB.Myhub.saveModule(this.form, this.id);
-			allNodes = this.form.toQueryString();
-			//var myAjax1 = new Ajax(HUB.Modules.MyResources.baseURL+'&task=saveparams&update=1&id='+id+'&uid='+uid+'&'+allNodes,{update:'myresources-content'}).request();
-			var myAjax1 = new Ajax(
-				HUB.Modules.MyResources.baseURL+'&action=saveparams&update=1&id='+this.id+'&uid='+this.uid+'&'+allNodes,
-				{update:'myresources-content'}
-			).request();
-		}.bind(this));
-			
-		this.limit.addEvent('change', function() {
-			//var myAjax1 = new Ajax(HUB.Myhub.baseURL+'&task=addmodule&uid='+uid+'&id='+id,{update:'myresources-content'}).request();
-			//HUB.Myhub.saveModule(this.form, this.id);
-			allNodes = this.form.toQueryString();
-			var myAjax1 = new Ajax(
-				HUB.Modules.MyResources.baseURL+'&action=saveparams&update=1&id='+this.id+'&uid='+this.uid+'&'+allNodes,
-				{update:'myresources-content'}
-			).request();
-		}.bind(this));
+String.prototype.nohtml = function () {
+	if (this.indexOf('?') == -1) {
+		return this + '?no_html=1';
+	} else {
+		return this + '&no_html=1';
 	}
 };
 
-//----------------------------------------------------------
+jQuery(document).ready(function(jq){
+	var $ = jq,
+		form = $('#myresources-form');
 
-window.addEvent('domready', HUB.Modules.MyResources.initialize);
+	if (form.length <= 0) {
+		return;
+	}
+
+	var url = form.attr('action').nohtml() + '&action=saveparams&update=1&mid=' + form.attr('data-module') + '&uid=' + $('#uid').val();
+
+	$('#myresources-sort').on('change', function (e) {
+		$.get(url + '&' + form.serialize(), {}, function(data) {
+			$('#myresources-content').html(data);
+		});
+	});
+
+	$('#myresources-limit').on('change', function (e) {
+		$.get(url + '&' + form.serialize(), {}, function(data) {
+			$('#myresources-content').html(data);
+		});
+	});
+});
 
