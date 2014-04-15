8d7af87e415a4443e79ce969b3d36a6dbf66a184
Shawn Rice <zooley@purdue.edu>
2014-04-15 09:13:44 -0400

    [MOD_NOTICES] Removing mootools based javascript

--- modules/mod_notices/mod_notices.js
+++ modules/mod_notices/mod_notices.js
@@ -1,28 +1,31 @@
 /**
  * @package     hubzero-cms
- * @file        modules/mod_mysessions/mod_mysessions.js
+ * @file        modules/mod_reportproblems/mod_reportproblems.js
  * @copyright   Copyright 2005-2011 Purdue University. All rights reserved.
  * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
+if (!jq) {
+	var jq = $;
+}
+
 //-------------------------------------------------------------
 // Add an event to close the notice
 //-------------------------------------------------------------
-window.addEvent('domready', function(){
-	$$('.modnotices .close').each(function(item){
-		$(item).addEvent('click', function(e) {
-			new Event(e).stop();
+jQuery(document).ready(function(jq){
+	var $ = jq;
 
-			var id = $($(this).parentNode.parentNode).getProperty('id');
-			var days = $(this).getProperty('data-duration');
+	$('.modnotices .close').on('click', function(e) {
+		e.preventDefault();
 
-			$($(this).parentNode.parentNode).remove();
+		var id = $($(this).parent().parent()).attr('id'),
+			days = $(this).attr('data-duration');
 
-			var date = new Date();
-			date.setTime(date.getTime()+(days*24*60*60*1000));
+		$($(this).parent().parent()).slideUp();
 
-			document.cookie = id + '=closed; expires=' + date.toGMTString() + ';';
-		});
-	})
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+
+		document.cookie = id + '=closed; expires=' + date.toGMTString() + ';';
+	});
 });
-
