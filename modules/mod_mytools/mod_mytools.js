c5e451036a6f8fe1d23ff59bc5a15a58566fb2e0
Shawn Rice <zooley@purdue.edu>
2014-04-15 09:14:37 -0400

    [MOD_MYTOOLS] Removing mootools based javascript

--- modules/mod_mytools/mod_mytools.js
+++ modules/mod_mytools/mod_mytools.js
@@ -1,128 +1,167 @@
 /**
- * Copyright 2005-2009 by Purdue Research Foundation, West Lafayette, IN 47906.
- * All rights reserved.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License,
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * @package     hubzero-cms
+ * @file        modules/mod_reportproblems/mod_reportproblems.js
+ * @copyright   Copyright 2005-2011 Purdue University. All rights reserved.
+ * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
-var MyToolsTabs = new Class({
-	
-	initialize: function(element, options) {
-		this.options = Object.extend({
-			width:				'300px',
-			height:				'200px',
-			changeTransition:	Fx.Transitions.Bounce.easeOut,
-			duration:			1000,
-			mouseOverClass:		'active',
-			activateOnLoad:		'first',
-			useAjax: 			false,
-			ajaxUrl: 			'',
-			ajaxOptions: 		{method:'get'},
-			ajaxLoadingText: 	'Loading...'
-		}, options || {});
-		
-		this.el = $(element);
-		this.elid = element;
-		
-		this.titles = $$('#' + this.elid + ' ul.tab_titles li');
-		this.panels = $$('#' + this.elid + ' .tab_panel');
-		
-		this.titles.each(function(item) {
-			item.addEvent('click', function(){
-					item.removeClass(this.options.mouseOverClass);
-					this.activate(item);
-				}.bind(this)
-			);
-			
-			item.addEvent('mouseover', function() {
-				if(item != this.activeTitle)
-				{
-					item.addClass(this.options.mouseOverClass);
-				}
-			}.bind(this));
-			
-			item.addEvent('mouseout', function() {
-				if(item != this.activeTitle)
-				{
-					item.removeClass(this.options.mouseOverClass);
-				}
-			}.bind(this));
-		}.bind(this));
-		
-		this.favs = $$('.fav');
-		this.favs.each(function(item) {
-			item.addEvent('click', function(){
-					this.toggle(item);
-					return false;
-				}.bind(this)
-			);
-		}.bind(this));
-	},
-	
-	toggle: function(lnk) {
-		var li = $(lnk.parentNode);
-		if (li.hasClass('favd')) {
-			li.removeClass('favd');
-		} else {
-			li.addClass('favd');
-		}
-		this._updateFavs();
-	},
-	
-	_updateFavs: function() {
-		var favd = $$('.favd');
-		var f = [];
-		for (i = 0; i < favd.length; i++) 
-		{
-			f.push(favd[i].id);
-		}
-		var fs = f.join(',');
-		var uid = $('uid').value;
-		var id = $(this.el.parentNode.parentNode.parentNode).getProperty('id').replace('mod_','');
-
-		var myAjax2 = new Ajax('/index.php?option=com_members&active=dashboard&no_html=1&init=1&action=saveparams&mid='+id+'&id='+uid+'&params[myhub_favs]='+fs).request();
-		var myAjax2 = new Ajax('/index.php?option=com_members&active=dashboard&no_html=1&init=1&action=refresh&mid='+id+'&id='+uid+'&fav='+fs,{update:'favtools'}).request();
-	},
-	
-	activate: function(tab){
-
-		if($type(tab) == 'string') 
-		{
-			myTab = $$('#' + this.elid + ' ul li').filterByAttribute('title', '=', tab)[0];
-			tab = myTab;
-		}
-		
-		if($type(tab) == 'element')
-		{
-			var newTab = tab.getProperty('title');
-			this.panels.removeClass('active');
-			
-			this.activePanel = this.panels.filterById(newTab)[0];
-			
-			this.activePanel.addClass('active');
-			
-			this.titles.removeClass('active');
-			
-			tab.addClass('active');
-			
-			this.activeTitle = tab;
-		}
-	}
-});
-
-function initMyToolsTabs() {
-	MTT = new MyToolsTabs('myToolsTabs', {height: '27em', width: 'auto', changeTransition: 'none', mouseOverClass: 'over'});
+//----------------------------------------------------------
+// Establish the namespace if it doesn't exist
+//----------------------------------------------------------
+if (!HUB) {
+	var HUB = {
+		Modules: {}
+	};
+} else if (!HUB.Modules) {
+	HUB.Modules = {};
 }
 
-window.addEvent('domready', initMyToolsTabs);
+if (!jq) {
+	var jq = $;
+}
+
+//----------------------------------------------------------
+// Trouble Report form
+//----------------------------------------------------------
+HUB.Modules.MyTools	 = {
+
+	jQuery: jq,
+
+	settings: { 
+		mouseOverClass:	'over',
+		ajax: {
+			enabled: false,
+			url: '/index.php?option=com_members&active=dashboard&no_html=1&init=1&action=',
+			options: {
+				method:'get'
+			},
+			loadingText: 'Loading...'
+		},
+		titles: '#myToolsTabs ul.tab_titles li',
+		container: '#myToolsTabs',
+		panels: '#myToolsTabs .tab_panel',
+		favs: '.fav',
+		favd: '.favd'
+	},
+
+	initialize: function() {
+		var mod = this,
+			$ = this.jQuery,
+			settings = this.settings;
+
+		if (!$(settings.container)) {
+			return;
+		}
+
+		$(settings.titles).each(function(i, item) {
+			$(item).click(function () {
+				$(this).removeClass(settings.mouseOverClass);
+				mod.activate($(this));
+			});
+		});
+		
+		$(settings.favs).each(function(i, item) {
+			$(item).toggle(function (e) {
+				e.preventDefault();
+				if ($(this).parent().hasClass('favd')) {
+					$(this).parent().removeClass('favd');
+				} else {
+					$(this).parent().addClass('favd');
+				}
+				mod.updateFavs();
+                return false;
+			},function (e) {
+                e.preventDefault();
+				if ($(this).parent().hasClass('favd')) {
+					$(this).parent().removeClass('favd');
+				} else {
+					$(this).parent().addClass('favd');
+				}
+				mod.updateFavs();
+                return false;
+            })
+		});
+		
+		HUB.Modules.MyTools.toolSearch();
+	},
+
+	updateFavs: function() {
+		var mod = this,
+			$ = this.jQuery,
+			settings = this.settings,
+			f = [],
+			uid = $('#uid').val(),
+			id = $($(settings.container).parent().parent().parent()).attr('id').replace('mod_','');
+		
+		$(settings.favd).each(function(i, elm) {
+			f.push($(elm).attr('id'));
+		});
+		
+		$.get(settings.ajax.url + 'saveparams&mid='+id+'&id='+uid, {'params[myhub_favs]': f.join(',')}, function(data){
+			$.get(settings.ajax.url + 'refresh&mid='+id+'&id='+uid+'&fav='+f.join(','), {}, function(data) {
+	            $('#favtools').html(data);
+			});
+		});
+	},
+	
+	activate: function(tab) {
+		var mod = this,
+			$ = this.jQuery,
+			settings = this.settings;
+
+		var newTab = $(tab).attr('title');
+			
+		$(settings.panels).each(function(i, item) {
+			$(this).removeClass('active');
+		});
+		mod.activePanel = $('#'+newTab+'');
+		$(mod.activePanel).addClass('active');
+			
+		$(settings.titles).each(function(i, item) {
+			$(this).removeClass('active');
+		});
+		mod.activeTitle = $(tab);
+		$(tab).addClass('active');
+	},
+	
+	toolSearch: function()
+	{
+		var $ = this.jQuery;
+		
+		jQuery.expr[':'].caseInsensitiveContains = function(a,i,m) {
+			return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0; 
+		};
+		
+		if( $('#filter-mytools').length )
+		{
+			$('#filter-mytools input').on("keyup", function(event){
+				var search = $(this).val();
+				if(search != '')
+				{
+					$("#alltools ul li a:not(:caseInsensitiveContains('"+search+"'))").parent().hide();
+					$("#alltools ul li a:caseInsensitiveContains('"+search+"')").parent().show();
+					
+					//add no results node
+					if( $("#alltools ul li:visible").length == 0 )
+					{
+						$("#alltools ul").append("<li id=\"none\">Sorry No Tools Matching Your Search.</li>");
+					}
+					
+					//remove no results node if we have results
+					if( $("#alltools ul li:visible").length > 1 && $("#alltools ul #none").length == 1 )
+					{
+						$("#alltools ul #none").remove();
+					}
+				}
+				else
+				{
+					$("#alltools ul li").show();
+				}
+			});
+		}
+	}
+};
+
+jQuery(document).ready(function($){
+	HUB.Modules.MyTools.initialize();
+});
