9c84a1b5b0455b1aa454e481fafe03b5348e45c7
Shawn Rice <zooley@purdue.edu>
2014-04-24 17:05:00 -0400

    [HUBZERO][#4853][COM_SUPPORT] Pass UTC time to relative formatter instead of local time.

--- administrator/components/com_support/views/tickets/tmpl/display.php
+++ administrator/components/com_support/views/tickets/tmpl/display.php
@@ -307,7 +307,7 @@ $this->css();
 									</span>
 			<?php if ($lastcomment && $lastcomment != '0000-00-00 00:00:00') { ?>
 									<span class="ticket-activity">
-										<time datetime="<?php echo $lastcomment; ?>"><?php echo JHTML::_('date.relative', JHTML::_('date', $lastcomment, $tsformat)); ?></time>
+										<time datetime="<?php echo $lastcomment; ?>"><?php echo JHTML::_('date.relative', $lastcomment); ?></time>
 									</span>
 			<?php } ?>
 								</p>
