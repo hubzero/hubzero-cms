5814c6ca200858de452248d08afa40fbb8bfefc6
Shawn Rice <zooley@purdue.edu>
2014-04-25 13:20:14 -0400

    [COM_PROJECTS] Cleaning up HTML to be simpler, more semantic, and easier to style. Removed unnecessary XML files.

--- administrator/components/com_projects/views/projects/tmpl/default.php
+++ administrator/components/com_projects/views/projects/tmpl/default.php
@@ -36,14 +36,14 @@ function submitbutton(pressbutton)
 	<p class="error"><?php echo $this->getError(); ?></p>
 <?php } ?>
 <form action="index.php" method="post" name="adminForm" id="adminForm">
-	<fieldset id="filter">
+	<fieldset id="filter-bar">
 		<span><?php echo JText::_('Total projects'); ?>: <strong><?php echo $this->total; ?></strong></span> &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;
-		<label for="search">
-			<?php echo JText::_('Search'); ?>: 
-			<input type="text" name="search" id="search" value="<?php echo $this->filters['search']; ?>" />
-		</label>	
+		<label for="filter_search"><?php echo JText::_('Search'); ?>:</label>
+		<input type="text" name="search" id="filter_search" value="<?php echo $this->escape($this->filters['search']); ?>" placeholder="<?php echo JText::_('Search...'); ?>" />
+
 		<input type="submit" name="filter_submit" id="filter_submit" value="<?php echo JText::_('Go'); ?>" />
 	</fieldset>
+
 	<table class="adminlist" id="projects-admin">
 		<thead>
 			<tr>
@@ -69,61 +69,60 @@ function submitbutton(pressbutton)
 
 			$database = JFactory::getDBO();
 			$now = JFactory::getDate()->toSql();
-			
+
 			$database = JFactory::getDBO();
 			$pt = new ProjectTags($database);
-			
+
 			for ($i=0, $n=count( $this->rows ); $i < $n; $i++) 
 			{
 				$row = $this->rows[$i];
-				
+
 				$thumb = ProjectsHtml::getThumbSrc($row->id, $row->alias, $row->picture, $this->config);
-				
-				if($row->owned_by_group && !$row->groupcn) {
+
+				if ($row->owned_by_group && !$row->groupcn) {
 					$row->groupname = '<span class="italic pale">'.JText::_('COM_PROJECTS_INFO_DELETED_GROUP').'</span>';
 				}
-				$owner = ($row->owned_by_group) ? $row->groupname.'<span class="block  prominent">'.$row->groupcn.'</span>' : $row->authorname;			
+				$owner = ($row->owned_by_group) ? $row->groupname.'<span class="block  prominent">'.$row->groupcn.'</span>' : $row->authorname;
 				$ownerclass = ($row->owned_by_group) ? '<span class="i_group">&nbsp;</span>' : '<span class="i_user">&nbsp;</span>';
-				
+
 				// Determine status
 				$status = '';
-				if($row->state == 1 && $row->setup_stage >= $setup_complete) {
+				if ($row->state == 1 && $row->setup_stage >= $setup_complete) {
 					$status = '<span class="active">'.JText::_('Active').'</span> '.JText::_('since').' '.JHTML::_('date', $row->created, $dateFormat, $tz);
 				}
-				else if($row->state == 2) {
+				else if ($row->state == 2) {
 					$status  = '<span class="deleted">'.JText::_('Deleted').'</span> ';
 				}
 				else if ($row->setup_stage < $setup_complete) {
 					$status = '<span class="setup">'.JText::_('Setup').'</span> '.JText::_('in progress');
 				}
-				else if($row->state == 0) {
+				else if ($row->state == 0) {
 					$status = '<span class="faded italic">'.JText::_('Inactive/Suspended').'</span> ';
 				}
-				else if($row->state == 5) {
+				else if ($row->state == 5) {
 					$status = '<span class="inactive">'.JText::_('Pending approval').'</span> ';
 				}
-				
+
 				$tags = $pt->get_tag_cloud(3, 1, $row->id);
-	
 			?>
-						<tr class="<?php echo "row$k"; ?>">
-							<td style="width: 10px;"><?php echo JHTML::_('grid.id', $i, $row->id, false, 'id' ); ?></td>
-							<td style="width: 30px;"><?php echo $row->id; ?></td>
-							<td style="width: 30px;"><?php echo '<img src="'.$thumb.'" width="30" height="30" alt="'.htmlentities($row->alias).'" />'; ?></td>
-							<td>
-								<a href="index.php?option=<?php echo $this->option ?>&amp;task=edit&amp;id[]=<?php echo $row->id;  echo $filterstring; ?>"><?php echo stripslashes($row->title); ?></a><br /><strong><?php echo stripslashes($row->alias); ?></strong>
-								<?php if ($tags) { ?>
-														<span class="project-tags block">
-															<?php echo $tags; ?>
-														</span>
-								<?php } ?>
-							</td>
-							<td style="width: 20px;"><?php echo $ownerclass; ?></td>
-							<td><?php echo $owner; ?></td>
-							<td><?php echo $status; ?></td>						
-							<td><?php echo ($row->private == 1) ? '<span class="private">&nbsp;</span>' : ''; ?></td>
-							<td class="centeralign"><?php echo $row->activity; ?></td>
-						</tr>
+			<tr class="<?php echo "row$k"; ?>">
+				<td style="width: 10px;"><?php echo JHTML::_('grid.id', $i, $row->id, false, 'id' ); ?></td>
+				<td style="width: 30px;"><?php echo $row->id; ?></td>
+				<td style="width: 30px;"><?php echo '<img src="'.$thumb.'" width="30" height="30" alt="'.$this->escape($row->alias).'" />'; ?></td>
+				<td>
+					<a href="index.php?option=<?php echo $this->option ?>&amp;task=edit&amp;id[]=<?php echo $row->id;  echo $filterstring; ?>"><?php echo stripslashes($row->title); ?></a><br /><strong><?php echo stripslashes($row->alias); ?></strong>
+					<?php if ($tags) { ?>
+						<span class="project-tags block">
+							<?php echo $tags; ?>
+						</span>
+					<?php } ?>
+				</td>
+				<td style="width: 20px;"><?php echo $ownerclass; ?></td>
+				<td><?php echo $owner; ?></td>
+				<td><?php echo $status; ?></td>
+				<td><?php echo ($row->private == 1) ? '<span class="private">&nbsp;</span>' : ''; ?></td>
+				<td class="centeralign"><?php echo $row->activity; ?></td>
+			</tr>
 			<?php
 				$k = 1 - $k;
 			}
