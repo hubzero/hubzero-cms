cfba383758350a89fae151771dbe1a0166f61661
Shawn Rice <zooley@purdue.edu>
2014-04-28 11:33:49 -0400

    [COM_CATEGORIES] Cleaning up HTML to be simpler, more semantic, and easier to style.

--- administrator/components/com_categories/views/category/tmpl/edit.php
+++ administrator/components/com_categories/views/category/tmpl/edit.php
@@ -135,17 +135,18 @@ JHtml::_('behavior.keepalive');
 	<div class="clr"></div>
 
 	<?php if ($this->canDo->get('core.admin')): ?>
-		<div  class="width-100 fltlft">
+		<div class="width-100 fltlft">
 
-			<?php echo JHtml::_('sliders.start', 'permissions-sliders-'.$this->item->id, array('useCookie'=>1)); ?>
+			<?php //echo JHtml::_('sliders.start', 'permissions-sliders-'.$this->item->id, array('useCookie'=>1)); ?>
 
-			<?php echo JHtml::_('sliders.panel', JText::_('COM_CATEGORIES_FIELDSET_RULES'), 'access-rules'); ?>
+			<?php //echo JHtml::_('sliders.panel', JText::_('COM_CATEGORIES_FIELDSET_RULES'), 'access-rules'); ?>
 			<fieldset class="panelform">
+				
 				<?php echo $this->form->getLabel('rules'); ?>
 				<?php echo $this->form->getInput('rules'); ?>
 			</fieldset>
 
-			<?php echo JHtml::_('sliders.end'); ?>
+			<?php //echo JHtml::_('sliders.end'); ?>
 		</div>
 	<?php endif; ?>
 
