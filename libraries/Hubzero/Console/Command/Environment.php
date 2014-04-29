97ba76a0e58e97d107ab18cbd52a5508be4221b7
Sam Wilson <samwilson@purdue.edu>
2014-04-29 10:36:28 -0400

    [LIB_HUBZERO] add simple environment command (because Chris asked nicely)

---
+++ libraries/Hubzero/Console/Command/Environment.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2013 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Sam Wilson <samwilson@purdue.edu>
+ * @copyright Copyright 2005-2013 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+namespace Hubzero\Console\Command;
+
+use Hubzero\Console\Output;
+use Hubzero\Console\Arguments;
+use Hubzero\Console\Config;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Environment class
+ **/
+class Environment implements CommandInterface
+{
+	/**
+	 * Output object, implements the Output interface
+	 *
+	 * @var object
+	 **/
+	private $output;
+
+	/**
+	 * Arguments object, implements the Argument interface
+	 *
+	 * @var object
+	 **/
+	private $arguments;
+
+	/**
+	 * Constructor - sets output and arguments for use by command
+	 *
+	 * @return void
+	 **/
+	public function __construct(Output $output, Arguments $arguments)
+	{
+		$this->output    = $output;
+		$this->arguments = $arguments;
+	}
+
+	/**
+	 * Default (required) command
+	 *
+	 * @return void
+	 **/
+	public function execute()
+	{
+		$config = new \JConfig();
+		$this->output->addLine('Current user     : ' . Config::get('user_name') . ' <' . Config::get('user_email') . '>');
+		$this->output->addLine('Current database : ' . $config->db);
+	}
+
+	/**
+	 * Help output
+	 *
+	 * @return void
+	 **/
+	public function help()
+	{
+		$this->output->addOverview('Environment display/management functions');
+	}
+}
