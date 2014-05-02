c5b58978e6c2a8d537eeade6d6a9407d3265ff7e
Shawn Rice <zooley@purdue.edu>
2014-05-02 14:27:07 -0400

    [COM_STORE] Removed mootools based script and IE 6 CSS hacks because, really? We still had IE 6 hacks?

--- components/com_store/assets/js/store.js
+++ components/com_store/assets/js/store.js
@@ -1,61 +1,27 @@
 /**
  * @package     hubzero-cms
- * @file        components/com_store/store.js
- * @copyright   Copyright 2005-2011 Purdue University. All rights reserved.
+ * @file        components/com_store/assets/js/store.js
+ * @copyright   Copyright 2005-2014 Purdue University. All rights reserved.
  * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
-//-----------------------------------------------------------
-//  Ensure we have our namespace
-//-----------------------------------------------------------
-if (!HUB) {
-	var HUB = {};
-}
-
-//----------------------------------------------------------
-// Store js
-//----------------------------------------------------------
-HUB.Store = {
-	form: 'myCart',
-
-	hubform: 'hubForm',
-
-	initialize: function() {
-		if ($('updatecart')) {
-			$('updatecart').addEvent('click', function(){
-					var form = document.getElementById(HUB.Store.form);
-					form.task.value = 'cart';
-					form.action.value = 'update';
-					//alert(form.action.value);
-					form.submit( );
-					return false;
-					
-				}
-			);
-		}
-		if ($('change_address')) {
-			$('change_address').addEvent('click', function(){
-					var form = document.getElementById(HUB.Store.hubform);
-					form.task.value = 'checkout';
-					form.submit( );
-					return false;
-					
-				}
-			);
-		}
-	},
-
-	hide: function(obj) {
-		$(obj).style.display = 'none';
-	},
-
-	show: function(obj) {
-		$(obj).style.display = 'block';
+jQuery(document).ready(function($){
+	if ($('#updatecart')) {
+		$('#updatecart').on('click', function(){
+			var form = document.getElementById('myCart');
+			form.task.value = 'cart';
+			form.action.value = 'update';
+			form.submit();
+			return false;
+		});
 	}
-}
-
-
-//----------------------------------------------------------
-
-window.addEvent('domready', HUB.Store.initialize);
+	if ($('#change_address')) {
+		$('#change_address').on('click', function(){
+			var form = document.getElementById('hubForm');
+			form.task.value = 'checkout';
+			form.submit( );
+			return false;
+		});	
+	}
+});
 
