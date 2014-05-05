ddc517c2c291ffb1b7dc1c6bacd5f786454da78e
Shawn Rice <zooley@purdue.edu>
2014-05-05 15:52:49 -0400

    [COM_TAGS] Removing old 1.5 code

--- administrator/components/com_tags/tags.php
+++ administrator/components/com_tags/tags.php
@@ -33,27 +33,9 @@ defined('_JEXEC') or die('Restricted access');
 
 $option = 'com_tags';
 
-if (version_compare(JVERSION, '1.6', 'lt'))
+if (!JFactory::getUser()->authorise('core.manage', $option)) 
 {
-	$jacl = JFactory::getACL();
-	$jacl->addACL($option, 'manage', 'users', 'super administrator');
-	$jacl->addACL($option, 'manage', 'users', 'administrator');
-	$jacl->addACL($option, 'manage', 'users', 'manager');
-
-	// Authorization check
-	$user = JFactory::getUser();
-	if (!$user->authorize($option, 'manage'))
-	{
-		$app = JFactory::getApplication();
-		$app->redirect('index.php', JText::_('ALERTNOTAUTH'));
-	}
-}
-else 
-{
-	if (!JFactory::getUser()->authorise('core.manage', $option)) 
-	{
-		return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
-	}
+	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
 require_once(JPATH_COMPONENT_SITE . DS . 'models' . DS . 'cloud.php');
