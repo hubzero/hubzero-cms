aa2d2e81201b7c3dfd5c1c74ab72490d317ad27a
Shawn Rice <zooley@purdue.edu>
2014-05-05 14:02:17 -0400

    [COM_CONTENT] Adjusting batch stylings.

--- administrator/components/com_content/views/articles/tmpl/default_batch.php
+++ administrator/components/com_content/views/articles/tmpl/default_batch.php
@@ -15,17 +15,29 @@ $published = $this->state->get('filter.published');
 <fieldset class="batch">
 	<legend><?php echo JText::_('COM_CONTENT_BATCH_OPTIONS');?></legend>
 	<p><?php echo JText::_('COM_CONTENT_BATCH_TIP'); ?></p>
-	<?php echo JHtml::_('batch.access');?>
-	<?php echo JHtml::_('batch.language'); ?>
 
+	<div class="col width-50 fltlft">
+		<div class="input-wrap">
+			<?php echo JHtml::_('batch.access');?>
+		</div>
+
+		<div class="input-wrap">
+			<?php echo JHtml::_('batch.language'); ?>
+		</div>
+	</div>
+	<div class="col width-50 fltrt">
 	<?php if ($published >= 0) : ?>
 		<?php echo JHtml::_('batch.item', 'com_content');?>
 	<?php endif; ?>
 
-	<button type="submit" onclick="Joomla.submitbutton('article.batch');">
-		<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
-	</button>
-	<button type="button" onclick="$('#batch-category-id').val('');$('#batch-access').val('');$('#batch-language-id').val('');">
-		<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>
-	</button>
+		<div class="input-wrap">
+			<button type="submit" onclick="Joomla.submitbutton('article.batch');">
+				<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
+			</button>
+			<button type="button" onclick="$('#batch-category-id').val('');$('#batch-access').val('');$('#batch-language-id').val('');">
+				<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>
+			</button>
+		</div>
+	</div>
+	<div class="clr"></div>
 </fieldset>
