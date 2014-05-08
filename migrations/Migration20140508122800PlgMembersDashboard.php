3d62187ce3c06fdd7a383274396064e92bc2d931
Christopher Smoak <csmoak@purdue.edu>
2014-05-08 12:34:30 -0400

    [PLG_MEMBERS_DASHBOARD] Add missing id primary key to jos_xprofiles_dashboard_preferences table.

---
+++ migrations/Migration20140508122800PlgMembersDashboard.php
@@ -0,0 +1,33 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for ...
+ **/
+class Migration20140508122800PlgMembersDashboard extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if (!$this->db->tableHasKey('#__xprofiles_dashboard_preferences', 'id'))
+		{
+			$query  = "ALTER TABLE `#__xprofiles_dashboard_preferences` ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (id);";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		// do nothing
+	}
+}
