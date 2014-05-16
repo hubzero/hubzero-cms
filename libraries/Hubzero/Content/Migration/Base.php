2544877bcc3aeea6a17d8e4ca4a8c6c46cc6e10f
Sam Wilson <samwilson@purdue.edu>
2014-05-16 13:52:46 -0400

    [LIB_HUBZERO] use new hubzero ini parser

--- libraries/Hubzero/Content/Migration/Base.php
+++ libraries/Hubzero/Content/Migration/Base.php
@@ -30,6 +30,8 @@
 
 namespace Hubzero\Content\Migration;
 
+use Hubzero\Config\Processor\Ini;
+
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
@@ -114,7 +116,7 @@ class Base
 
 		if (is_file($secrets) && is_readable($secrets))
 		{
-			$conf = parse_ini_file($secrets);
+			$conf = Ini::parse($secrets);
 			$user = 'root';
 			$pw   = $conf['MYSQL-ROOT'];
 
@@ -126,7 +128,7 @@ class Base
 
 		if (is_file($conf_file) && is_readable($conf_file))
 		{
-			$conf = parse_ini_file($conf_file, true);
+			$conf = Ini::parse($conf_file, true);
 			$user = $conf['client']['user'];
 			$pw   = $conf['client']['password'];
 
@@ -138,7 +140,7 @@ class Base
 
 		if (is_file($hub_maint) && is_readable($hub_maint))
 		{
-			$conf = parse_ini_file($hub_maint, true);
+			$conf = Ini::parse($hub_maint, true);
 			$user = $conf['client']['user'];
 			$pw   = $conf['client']['password'];
 
