92cae80c53669c1b519dcc8c60855ae8def90742
Shawn Rice <zooley@purdue.edu>
2014-06-02 13:56:21 -0400

    [COM_MEMBERS] Removed hard-coded jos_ prefix

--- components/com_members/controllers/api.php
+++ components/com_members/controllers/api.php
@@ -1,403 +1,283 @@
 <?php
-JLoader::import('Hubzero.Api.Controller');
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2014 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2014 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ * 
+ */
 
-class MembersControllerApi extends \Hubzero\Component\ApiController
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+JLoader::import('Hubzero.Component.ApiController');
+require_once(JPATH_ROOT . DS . 'components' . DS . 'com_wiki' . DS . 'models' . DS . 'book.php');
+
+/**
+ * API controller class for support tickets
+ */
+class BlogControllerApi extends \Hubzero\Component\ApiController
 {
-	function execute()
+	/**
+	 * Execute a request
+	 *
+	 * @return    void
+	 */
+	public function execute()
 	{
 		JLoader::import('joomla.environment.request');
 		JLoader::import('joomla.application.component.helper');
 
-		switch($this->segments[0]) 
+		$this->config   = JComponentHelper::getParams('com_wiki');
+		$this->database = JFactory::getDBO();
+
+		switch ($this->segments[0]) 
 		{
-			case 'myprofile':		$this->myprofile();					break;
-			case 'mygroups':		$this->mygroups();					break;
-			case 'mysessions':		$this->mysessions();				break;
-			case 'recenttools':		$this->recenttools();				break;
-			case 'checkpass':		$this->checkpass();					break;
-			case 'diskusage':		$this->diskusage();					break;
-			default:				$this->not_found();
+			case 'search':  $this->archiveTask();  break;
+			case 'archive': $this->archiveTask();  break;
+
+			default:
+				$this->serviceTask();
+			break;
 		}
 	}
-	
+
 	/**
-	 * Short description for 'not_found'
+	 * Method to report errors. creates error node for response body as well
 	 *
-	 * Long description (if any) ...
+	 * @param	$code		Error Code
+	 * @param	$message	Error Message
+	 * @param	$format		Error Response Format
 	 *
 	 * @return     void
 	 */
-	private function not_found()
+	private function errorMessage($code, $message, $format = 'json')
 	{
-		$response = $this->getResponse();
-		$response->setErrorMessage(404,'Not Found');
-	}
-	
-	private function not_authorized()
-	{
-		$response = $this->getResponse();
-		$response->setErrorMessage(401,'Not Authorized');
-	}
-	
-	private function error( $code, $message )
-	{
-		if($code != '' && $message != '')
-		{
-			$response = $this->getResponse();
-			$response->setErrorMessage( $code, $message );
-		}
-	}
-	
-	function myprofile()
-	{
-		//get the userid from authentication token
-		//load user profile from userid
-		$userid = JFactory::getApplication()->getAuthn('user_id');
-		$result = \Hubzero\User\Profile::getInstance($userid);
-		
-		//check to make sure we have a profile
-		if ($result === false)	return $this->not_found();
-		
-		//get any request vars
-		$format = JRequest::getVar('format', 'json');
-		
-		//
-		$profile = array(
-			'id' => $result->get('uidNumber'),
-			'username' => $result->get('username'),
-			'name' => $result->get('name'),
-			'first_name' => $result->get('givenName'),
-			'middle_name' => $result->get('middleName'),
-			'last_name' => $result->get('surname'),
-			'bio' => $result->getBio('clean'),
-			'email' => $result->get('email'),
-			'phone' => $result->get('phone'),
-			'url' => $result->get('url'),
-			'gender' => $result->get('gender'),
-			'organization' => $result->get('organization'),
-			'organization_type' => $result->get('orgtype'),
-			'country_resident' => $result->get('countryresident'),
-			'country_origin' => $result->get('countryorigin'),
-			'member_since' => $result->get('registerDate'),
-			'picture' => array(
-				'thumb' => \Hubzero\User\Profile\Helper::getMemberPhoto( $result, 0, true ),
-				'full' => \Hubzero\User\Profile\Helper::getMemberPhoto( $result, 0, false )
-			)
-		);
-		
-		//encode and return result
+		//build error code and message
 		$object = new stdClass();
-		$object->profile = $profile;
-		$this->setMessageType( $format );
-		$this->setMessage( $object );
-	}
-	
-	private function mygroups()
-	{
-		$userid = JFactory::getApplication()->getAuthn('user_id');
-		$result = \Hubzero\User\Profile::getInstance($userid);
+		$object->error->code    = $code;
+		$object->error->message = $message;
 
-		if ($result === false)	return $this->not_found();
-		
-		$groups = \Hubzero\User\Helper::getGroups( $result->get('uidNumber'), 'members', 0);
-		
-		$g = array();
-		foreach($groups as $k => $group)
-		{
-			$g[$k]['gidNumber'] 	= $group->gidNumber;
-			$g[$k]['cn'] 			= $group->cn;
-			$g[$k]['description'] 	= $group->description;
-		}
-		
-		//encode and return result
-		$obj = new stdClass();
-		$obj->groups = $g;
-		$this->setMessageType("application/json");
-		$this->setMessage($obj);
-	}
-	
-	private function mysessions()
-	{
-		//get user from authentication and load their profile
-		$userid = JFactory::getApplication()->getAuthn('user_id');
-		$result = \Hubzero\User\Profile::getInstance($userid);
-		
-		//make sure we have a user
-		if ($result === false)
-		{
-			return $this->not_authorized();
-		}
-		
-		//include middleware utilities
-		JLoader::import("joomla.database.table");
-		include_once( JPATH_ROOT.DS.'components'.DS.'com_tools'.DS.'models'.DS.'mw.utils.php' );
-		include_once( JPATH_ROOT.DS.'components'.DS.'com_tools'.DS.'models'.DS.'mw.class.php' ); 
+		//set http status code and reason
+		$this->getResponse()
+		     ->setErrorMessage($object->error->code, $object->error->message);
 
-		//get db connection
-		$db = JFactory::getDBO();
-
-		//get Middleware DB connection
-		$mwdb = MwUtils::getMWDBO();
-
-		//get com_tools params
-		$mconfig = JComponentHelper::getParams( 'com_tools' );
-
-		//check to make sure we have a connection to the middleware and its on
-		if(!$mwdb || !$mconfig->get('mw_on') || $mconfig->get('mw_on') > 1)
-		{
-			return $this->error( 503, 'Middleware Service Unavailable' );
-		}
-		
-		//get request vars
-		$format = JRequest::getVar('format', 'json');
-		$order = JRequest::getVar('order', 'id_asc' );
-
-		//get my sessions
-		$ms = new MwSession( $mwdb );
-		$sessions = $ms->getRecords( $result->get("username"), '', false );
-		
-		//run middleware command to create screenshots
-		$cmd = "/bin/sh ". JPATH_SITE . "/components/com_tools/scripts/mw screenshot " . $result->get('username') . " 2>&1 </dev/null";
-		exec($cmd, $results, $status);
-		
-		//
-		$results = array();
-		foreach($sessions as $session)
-		{
-			$r = array(
-				'id' => $session->sessnum,
-				'app' => $session->appname,
-				'name' => $session->sessname,
-				'started' => $session->start,
-				'accessed' => $session->accesstime,
-				'owner' => ($result->get('username') == $session->username) ? 1 : 0,
-				'ready-only' => ($session->readonly == 'No') ? 0 : 1
-			);
-			$results[] = $r;
-		}
-		
-		//make sure we have an acceptable ordering
-		$accepted_ordering = array('id_asc', 'id_desc', 'started_asc', 'started_desc', 'accessed_asc', 'accessed_desc');
-		if(in_array($order, $accepted_ordering))
-		{
-			switch( $order )
-			{
-				case 'id_asc':
-					break;
-				case 'id_desc':
-					usort($results, array("MembersControllerApi", "id_sort_desc"));
-					break;
-				case 'started_asc':
-					break;
-				case 'started_desc':
-					usort($results, array("MembersControllerApi", "started_date_sort_desc"));
-					break;
-				case 'accessed_asc':
-					usort($results, array("MembersControllerApi", "accessed_date_sort_asc"));
-					break;
-				case 'accessed_desc':
-					usort($results, array("MembersControllerApi", "accessed_date_sort_desc"));
-					break;
-			}
-		}
-		
-		//encode sessions for return
-		$object = new stdClass();
-		$object->sessions = $results;
-
-		//set format and content
-		$this->setMessageType( $format );
-		$this->setMessage( $object );
-	}
-	
-	private function id_sort_desc($a, $b)
-	{
-		return $a['id'] < $b['id'] ? 1 : -1;
-	}
-	
-	private function started_date_sort_desc($a, $b)
-	{
-		return (strtotime($a['started']) < strtotime($b['started'])) ? 1 : -1;
-	}
-	
-	private function accessed_date_sort_asc($a, $b)
-	{
-		return (strtotime($a['accessed']) < strtotime($b['accessed'])) ? -1 : 1;
-	}
-	
-	private function accessed_date_sort_desc($a, $b)
-	{
-		return (strtotime($a['accessed']) < strtotime($b['accessed'])) ? 1 : -1;
-	}
-	
-	//------
-	
-	private function recenttools()
-	{
-		$userid = JFactory::getApplication()->getAuthn('user_id');
-		$result = \Hubzero\User\Profile::getInstance($userid);
-
-		if ($result === false)	return $this->not_found();
-		
-		//load database object
-		$database = JFactory::getDBO();
-		
-		//get the supported tag
-		$rconfig = JComponentHelper::getParams('com_resources');
-		$supportedtag = $rconfig->get('supportedtag', '');
-
-		//get supportedtag usage
-		include_once(JPATH_ROOT . DS . 'components' . DS . 'com_resources' . DS . 'helpers' . DS . 'tags.php');
-		$this->rt = new ResourcesTags($database);
-		$supportedtagusage = $this->rt->getTagUsage($supportedtag, 'alias');
-		
-		//load users recent tools
-		$sql = "SELECT r.alias, tv.toolname, tv.title, tv.description, tv.toolaccess as access, tv.mw, tv.instance, tv.revision 
-				FROM #__resources as r, #__recent_tools as rt, #__tool_version as tv 
-				WHERE r.published=1
-				AND r.type=7
-				AND r.standalone=1
-				AND r.access!=4
-				AND r.alias=tv.toolname
-				AND tv.state=1
-				AND rt.uid={$result->get("uidNumber")}
-				AND rt.tool=r.alias
-				GROUP BY r.alias
-				ORDER BY rt.created DESC";
-		
-		$database->setQuery($sql);
-		$recent_tools = $database->loadObjectList();
-		
-		$r = array();
-		foreach($recent_tools as $k => $recent)
-		{
-			$r[$k]['alias'] = $recent->alias;
-			$r[$k]['title'] = $recent->title;
-			$r[$k]['description'] = $recent->description;
-			$r[$k]['version'] = $recent->revision;
-			$r[$k]['supported'] = (in_array($recent->alias, $supportedtagusage)) ? 1 : 0;
-		}
-		
-		//encode sessions for return
-		$object = new stdClass();
-		$object->recenttools = $r;
-		$this->setMessageType( "json" );
-		$this->setMessage( $object );
-	}
-
-	//------
-
-	private function checkpass()
-	{
-		$userid = JFactory::getApplication()->getAuthn('user_id');
-		$result = \Hubzero\User\Profile::getInstance($userid);
-
-		if ($result === false)	return $this->not_found();
-
-		// Get the password rules
-		$password_rules = \Hubzero\Password\Rule::getRules();
-
-		$pw_rules = array();
-
-		// Get the password rule descriptions
-		foreach($password_rules as $rule)
-		{
-			if (!empty($rule['description']))
-			{
-				$pw_rules[] = $rule['description'];
-			}
-		}
-
-		// Get the password
-		$pw = JRequest::getCmd('password1', null, 'post');
-
-		// Validate the password
-		if (!empty($pw))
-		{
-			$msg = \Hubzero\Password\Rule::validate($pw, $password_rules, $result);
-		}
-		else
-		{
-			$msg = array();
-		}
-
-		$html = '';
-
-		// Iterate through the rules and add the appropriate classes (passed/error)
-		if (count($pw_rules) > 0) {
-			foreach ($pw_rules as $rule)
-			{
-				if (!empty($rule))
-				{
-					if (!empty($msg) && is_array($msg)) {
-						$err = in_array($rule, $msg);
-					} else {
-						$err = '';
-					}
-					$mclass = ($err)  ? ' class="error"' : 'class="passed"';
-					$html .= "<li $mclass>".$rule."</li>";
-				}
-			}
-			if (!empty($msg) && is_array($msg)) {
-				foreach ($msg as $message)
-				{
-					if (!in_array($message, $pw_rules)) {
-						$html .= '<li class="error">'.$message."</li>";
-					}
-				}
-			}
-		}
-
-		// Encode sessions for return
-		$object = new stdClass();
-		$object->html = $html;
-		$this->setMessageType("json");
+		//add error to message body
+		$this->setMessageType(JRequest::getWord('format', $format));
 		$this->setMessage($object);
 	}
 
-	private function diskusage()
+	/**
+	 * Displays a available options and parameters the API
+	 * for this comonent offers.
+	 *
+	 * @return  void
+	 */
+	private function serviceTask()
 	{
-		$userid = JFactory::getApplication()->getAuthn('user_id');
-		$result = \Hubzero\User\Profile::getInstance($userid);
+		$response = new stdClass();
+		$response->component = 'wiki';
+		$response->tasks = array(
+			'pages' => array(
+				'description' => JText::_('Get a list of pages.'),
+				'parameters'  => array(
+					'sort' => array(
+						'description' => JText::_('Field to sort results by.'),
+						'type'        => 'string',
+						'default'     => 'created',
+						'accepts'     => array('created', 'title', 'alias', 'id', 'publish_up', 'publish_down', 'state')
+					),
+					'sort_Dir' => array(
+						'description' => JText::_('Direction to sort results by.'),
+						'type'        => 'string',
+						'default'     => 'desc',
+						'accepts'     => array('asc', 'desc')
+					),
+					'search' => array(
+						'description' => JText::_('A word or phrase to search for.'),
+						'type'        => 'string',
+						'default'     => 'null'
+					),
+					'limit' => array(
+						'description' => JText::_('Number of result to return.'),
+						'type'        => 'integer',
+						'default'     => '25'
+					),
+					'limitstart' => array(
+						'description' => JText::_('Number of where to start returning results.'),
+						'type'        => 'integer',
+						'default'     => '0'
+					),
+				),
+			),
+			'page' => array(
+				'description' => JText::_('Get the contents of a page. If no revision number is provided, the page will default to the most current revision.'),
+				'parameters'  => array(
+					'pagename' => array(
+						'description' => JText::_('Page name to retrieve data for.'),
+						'type'        => 'string',
+						'default'     => ''
+					),
+					'scope' => array(
+						'description' => JText::_('Page scope.'),
+						'type'        => 'string',
+						'default'     => ''
+					),
+					'revision' => array(
+						'description' => JText::_('Revision number of page.'),
+						'type'        => 'integer',
+						'default'     => '0'
+					),
+				),
+			),
+			'revisions' => array(
+				'description' => JText::_('Get a list of revisions for a page.'),
+				'parameters'  => array(
+					'pageid' => array(
+						'description' => JText::_('The ID of the page to return revisions for.'),
+						'type'        => 'integer',
+						'default'     => '0',
+						'required'    => 'true'
+					),
+					'sort' => array(
+						'description' => JText::_('Field to sort results by.'),
+						'type'        => 'string',
+						'default'     => 'created',
+						'accepts'     => array('created', 'title', 'alias', 'id', 'publish_up', 'publish_down', 'state')
+					),
+					'sort_Dir' => array(
+						'description' => JText::_('Direction to sort results by.'),
+						'type'        => 'string',
+						'default'     => 'desc',
+						'accepts'     => array('asc', 'desc')
+					),
+					'search' => array(
+						'description' => JText::_('A word or phrase to search for.'),
+						'type'        => 'string',
+						'default'     => 'null'
+					),
+					'limit' => array(
+						'description' => JText::_('Number of result to return.'),
+						'type'        => 'integer',
+						'default'     => '25'
+					),
+					'limitstart' => array(
+						'description' => JText::_('Number of where to start returning results.'),
+						'type'        => 'integer',
+						'default'     => '0'
+					),
+				),
+			),
+		);
 
-		if ($result === false)
-		{
-			return $this->not_found();
-		}
-
-		require_once JPATH_ROOT . DS . 'components' . DS . 'com_tools' . DS . 'helpers' . DS . 'utils.php';
-		$du = ToolsHelperUtils::getDiskUsage($result->get('username'));
-		if (count($du) <=1) 
-		{
-			// error
-			$percent = 0;
-		} 
-		else 
-		{
-			bcscale(6);
-			$val = (isset($du['softspace']) && $du['softspace'] != 0) ? bcdiv($du['space'], $du['softspace']) : 0;
-			$percent = round($val * 100);
-		}
-
-		$amt = ($percent > 100) ? '100' : $percent;
-		$total = (isset($du['softspace'])) ? $du['softspace'] / 1024000000 : 0;
-
-		//encode sessions for return
-		$object = new stdClass();
-		$object->amount = $amt;
-		$object->total  = $total;
-		$this->setMessageType( "json" );
-		$this->setMessage( $object );
+		$this->setMessageType(JRequest::getWord('format', 'json'));
+		$this->setMessage($response);
 	}
 
-	//------
-
-	private function getResourceFromAppname( $appname, $database )
+	/**
+	 * Displays a list of tags
+	 *
+	 * @return    void
+	 */
+	private function pagesTask()
 	{
-		$sql = "SELECT r.*, tv.id as revisionid FROM jos_resources as r, jos_tool_version as tv WHERE tv.toolname=r.alias and tv.instance='".$appname."'";
+		$this->setMessageType(JRequest::getWord('format', 'json'));
 
-		$database->setQuery($sql);
+		$group = JRequest::getVar('group', '');
 
-		return $database->loadObject();
+		$book = new WikiModelBook(($group ? $group : '__site__'));
+
+		$filters = array(
+			'limit'      => JRequest::getInt('limit', 25),
+			'start'      => JRequest::getInt('limitstart', 0),
+			'search'     => JRequest::getVar('search', ''),
+			'sort'       => JRequest::getWord('sort', 'title'),
+			'sort_Dir'   => strtoupper(JRequest::getWord('sortDir', 'ASC')),
+			'group'      => JRequest::getWord('sort', 'title')
+		);
+		$filters['state'] = array(0, 1);
+		$filters['sortby'] = $filters['sort'] . ' ' . $filters['sort_Dir'];
+
+		$response = new stdClass;
+		$response->pages = array();
+		$response->total = $book->pages('count', $filters);
+
+		if ($response->total)
+		{
+			$juri = JURI::getInstance();
+
+			foreach ($book->pages('list', $filters) as $i => $entry)
+			{
+				$obj = new stdClass;
+				$obj->id        = $entry->get('id');
+				$obj->title     = $entry->get('title');
+				$obj->name      = $entry->get('name');
+				$obj->scope     = $entry->get('scope');
+				$obj->url       = str_replace('/api', '', rtrim($juri->base(), DS) . DS . ltrim(JRoute::_($entry->link()), DS));
+				$obj->revisions = $entry->revisions('count');
+
+				$response->pages[] = $obj;
+			}
+		}
+
+		$response->success = true;
+
+		$this->setMessage($response);
+	}
+
+	/**
+	 * Displays a list of tags
+	 *
+	 * @return    void
+	 */
+	private function pageTask()
+	{
+		$this->setMessageType(JRequest::getWord('format', 'json'));
+
+		$group = JRequest::getVar('group', '');
+
+		$book = new WikiModelBook(($group ? $group : '__site__'));
+		$page = $book->page();
+
+		$revision = $page->revision(JRequest::getInt('revision', 0));
+
+		$response = new stdClass;
+		$response->page = new stdClass;
+		$response->page->id = $page->get('id');
+		$response->page->name = $page->get('pagename');
+		$response->page->title = $page->get('title');
+		$response->page->scope = $page->get('scope');
+		$response->page->content = $revision->content('raw');
+		$response->page->scope = $page->get('scope');
+		$response->page->revision_id = $page->get('version_id');
+		$response->revisions = 0;
+
+		$response->page->url = str_replace('/api', '', rtrim($juri->base(), DS) . DS . ltrim(JRoute::_($page->link()), DS));
+
+		$response->success = true;
+
+		$this->setMessage($response);
 	}
 }
