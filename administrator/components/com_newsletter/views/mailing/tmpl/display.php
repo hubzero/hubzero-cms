546e0127158c9feeb300fd47f1b93109026bd99d
Shawn Rice <zooley@purdue.edu>
2014-06-04 10:45:49 -0400

    [HUBZERO][#5246][COM_NEWSLETTER] Remove HTML from header.

--- administrator/components/com_newsletter/views/mailing/tmpl/display.php
+++ administrator/components/com_newsletter/views/mailing/tmpl/display.php
@@ -32,7 +32,7 @@
 defined('_JEXEC') or die('Restricted access');                              
 
 //set title
-JToolBarHelper::title('<a href="index.php?option='.$this->option.'">' . JText::_( 'Newsletter Mailings' ) . '</a>', 'mailing.png');
+JToolBarHelper::title(JText::_( 'Newsletter Mailings' ), 'mailing.png');
 
 //add buttons to toolbar
 //JToolBarHelper::custom('process', 'options', '', 'Process', false);
--- administrator/components/com_newsletter/views/mailing/tmpl/tracking.php
+++ administrator/components/com_newsletter/views/mailing/tmpl/tracking.php
@@ -29,10 +29,10 @@
  */
 
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die('Restricted access');                              
+defined('_JEXEC') or die('Restricted access');
 
 //set title
-JToolBarHelper::title('<a href="index.php?option='.$this->option.'">' . JText::_( 'Newsletter Mailing Stats' ) . '</a>', 'stats.png');
+JToolBarHelper::title(JText::_( 'Newsletter Mailing Stats' ), 'stats.png');
 
 //add buttons
 JToolBarHelper::custom('cancel', 'back', '', 'Back', false);
