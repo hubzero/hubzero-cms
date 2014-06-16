a508c529be3e204d88c6cd32cb696eae6ae2fd77
Shawn Rice <zooley@purdue.edu>
2014-06-16 08:55:03 -0400

    [COM_GROUPS] Moving text into language files.

--- components/com_groups/views/groups/tmpl/view.php
+++ components/com_groups/views/groups/tmpl/view.php
@@ -35,58 +35,55 @@ $this->css()
      ->js();
 
 //get objects
-$config 	= JFactory::getConfig();
-$database 	= JFactory::getDBO();
+$config   = JFactory::getConfig();
+$database = JFactory::getDBO();
 
 //get no_html request var
 $no_html = JRequest::getInt( 'no_html', 0 );
 ?>
 
 <?php if (!$no_html) : ?>
-	
-	<?php echo GroupsHelperView::displayBeforeSectionsContent( $this->group ); ?>
-	
+	<?php echo GroupsHelperView::displayBeforeSectionsContent($this->group); ?>
+
 	<div class="innerwrap">
 		<div id="page_container">
 			<div id="page_sidebar">
 				<?php
 					//logo link - links to group overview page
-					$link = JRoute::_('index.php?option='.$this->option.'&cn='.$this->group->get('cn'));
+					$link = JRoute::_('index.php?option=' . $this->option . '&cn=' . $this->group->get('cn'));
 				?>
 				<div id="page_identity">
-					<a href="<?php echo $link; ?>" title="<?php echo $this->group->get('description'); ?> Home">
-						<img src="<?php echo $this->group->getLogo(); ?>" alt="<?php echo $this->group->get('description'); ?> Logo" />
+					<a href="<?php echo $link; ?>" title="<?php echo JText::sprintf('COM_GROUPS_OVERVIEW_HOME', $this->group->get('description')); ?>">
+						<img src="<?php echo $this->group->getLogo(); ?>" alt="<?php echo JText::sprintf('COM_GROUPS_OVERVIEW_LOGO', $this->group->get('description')); ?>" />
 					</a>
 				</div><!-- /#page_identity -->
-				
+
 				<?php
 					// output group options
 					echo GroupsHelperView::displayToolbar( $this->group );
-				?>
-				
-				<?php
+
 					// output group menu
 					echo GroupsHelperView::displaySections( $this->group );
 				?>
-				
+
 				<div id="page_info">
 					<?php 
 						// Determine the join policy
 						switch ($this->group->get('join_policy'))
 						{
-							case 3: $policy = JText::_('Closed');      break;
-							case 2: $policy = JText::_('Invite Only'); break;
-							case 1: $policy = JText::_('Restricted');  break;
+							case 3: $policy = JText::_('COM_GROUPS_MEMBERSHIP_SETTINGS_CLOSED_SETTING');     break;
+							case 2: $policy = JText::_('COM_GROUPS_MEMBERSHIP_SETTINGS_INVITE_SETTING');     break;
+							case 1: $policy = JText::_('COM_GROUPS_MEMBERSHIP_SETTINGS_RESTRICTED_SETTING'); break;
 							case 0:
-							default: $policy = JText::_('Open'); break;
+							default: $policy = JText::_('COM_GROUPS_MEMBERSHIP_SETTINGS_OPEN_SETTING');      break;
 						}
 
 						// Determine the discoverability
 						switch ($this->group->get('discoverability'))
 						{
-							case 1: $discoverability = JText::_('Hidden'); break;
+							case 1: $discoverability = JText::_('COM_GROUPS_DISCOVERABILITY_SETTINGS_HIDDEN_SETTING');   break;
 							case 0:
-							default: $discoverability = JText::_('Visible'); break;
+							default: $discoverability = JText::_('COM_GROUPS_DISCOVERABILITY_SETTINGS_VISIBLE_SETTING'); break;
 						}
 
 						// use created date
@@ -95,16 +92,16 @@ $no_html = JRequest::getInt( 'no_html', 0 );
 					<div class="group-info">
 						<ul>
 							<li class="info-discoverability">
-								<span class="label">Discoverability</span>
+								<span class="label"><?php echo JText::_('COM_GROUPS_INFO_DISCOVERABILITY'); ?></span>
 								<span class="value"><?php echo $discoverability; ?></span>
 							</li>
 							<li class="info-join-policy">
-								<span class="label">Join Policy</span>
+								<span class="label"><?php echo JText::_('COM_GROUPS_INFO_JOIN_POLICY'); ?></span>
 								<span class="value"><?php echo $policy; ?></span>
 							</li>
-							<?php if($created) : ?>
+							<?php if ($created) : ?>
 								<li class="info-created">
-									<span class="label">Created</span>
+									<span class="label"><?php echo JText::_('COM_GROUPS_INFO_CREATED'); ?></span>
 									<span class="value"><?php echo $created; ?></span>
 								</li>
 							<?php endif; ?>
@@ -112,7 +109,7 @@ $no_html = JRequest::getInt( 'no_html', 0 );
 					</div>
 				</div>
 			</div><!-- /#page_sidebar --> 
-			
+
 			<div id="page_main">
 				<div id="page_header">
 					<h2><a href="<?php echo $link; ?>"><?php echo $this->group->get('description'); ?></a></h2>
@@ -120,11 +117,11 @@ $no_html = JRequest::getInt( 'no_html', 0 );
 					<h3>
 						<?php echo GroupsHelperView::displayTab( $this->group ); ?>
 					</h3>
-					
+
 					<?php
 						if ($this->tab == 'overview') : 
-							$gt = new GroupsTags( $database );
-							echo $gt->get_tag_cloud(0,0,$this->group->get('gidNumber'));
+							$gt = new GroupsTags($database);
+							echo $gt->get_tag_cloud(0, 0, $this->group->get('gidNumber'));
 						endif;
 					?>
 				</div><!-- /#page_header -->
@@ -136,16 +133,16 @@ $no_html = JRequest::getInt( 'no_html', 0 );
 						}
 					?>
 				</div><!-- /#page_notifications -->
-				
+
 				<div id="page_content" class="group_<?php echo $this->tab; ?>">
-					<?php endif; ?>
-					
+<?php endif; ?>
+
 					<?php
 						// output content
 						echo $this->content; 
 					?>
-					
-					<?php if (!$no_html) : ?>
+
+<?php if (!$no_html) : ?>
 				</div><!-- /#page_content -->
 			</div><!-- /#page_main -->
 			<br class="clear" />
