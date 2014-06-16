23182acd902c2979f6b85a94f7a7d720bba43bda
Christopher Smoak <csmoak@purdue.edu>
2014-06-16 15:19:33 -0400

    [HABRI][#422]] Updated hub zero highlight plugin for CKEditor to not run in ie8. Cant get it to stop throwing error on highlight, causes form to not submit correctly.

--- media/editors/ckeditor/plugins/hubzerohighlight/plugin.js
+++ media/editors/ckeditor/plugins/hubzerohighlight/plugin.js
@@ -2,37 +2,28 @@
 	//CKEDITOR
 	CKEDITOR.plugins.add( 'hubzerohighlight', {
 		init: function( editor ) {
-			var mode = '';
-			//console.log(editor);
+			var $      = jq,
+				mode   = '', 
+				plugin = this;
+
+			// dont highlight for ie8
+			if ($('html').hasClass('ie8'))
+			{
+				return;
+			}
+
 			editor.on('mode', function(event) {
-				// run
-				highlight();
+				plugin.highlight(editor);
 
 				// add css for mark elements
 				if (editor.mode != 'source')
 				{
 					this.document.appendStyleSheet('/media/editors/ckeditor/plugins/hubzerohighlight/plugin.css');
 				}
-			});
-
-			editor.on('beforeSave', function(event) {
-				
-				var data  = this.getData();
-
-				// remove old mark tags
-				data = data.replace(/<mark class="macro">/g, '');
-				data = data.replace(/<\/mark>/g, '');
-				data = data.replace(/<mark class="group-include">/g, '');
-				data = data.replace(/<\/mark>/g, '');
-				data = data.replace(/<mark class="xhubtag">/g, '');
-				data = data.replace(/<\/mark>/g, '');
-
-				// set new data
-				this.setData(data);
 			});
 
 			editor.on('blur', function(event) {
-				highlight();
+				plugin.highlight(editor);
 
 				// add css for mark elements
 				if (editor.mode != 'source')
@@ -40,9 +31,9 @@
 					this.document.appendStyleSheet('/media/editors/ckeditor/plugins/hubzerohighlight/plugin.css');
 				}
 			});
-
-			function highlight()
-			{
+			
+			var form = $(editor.element.$.form);
+			form.submit(function(event) {
 				var data  = editor.getData();
 
 				// remove old mark tags
@@ -52,18 +43,37 @@
 				data = data.replace(/<\/mark>/g, '');
 				data = data.replace(/<mark class="xhubtag">/g, '');
 				data = data.replace(/<\/mark>/g, '');
-				
-				// add new mark tags
-				if (editor.mode == 'wysiwyg')
-				{
-					data = data.replace(/(\[\[[^\]]*]])/g, '<mark class="macro">$1</mark>');
-					data = data.replace(/(<group:include[^>]*>)/g, '<mark class="group-include">$1</mark>');
-					data = data.replace(/({xhub:[^}]*})/g, '<mark class="xhubtag">$1</mark>');
-				}
 
 				// set new data
 				editor.setData(data);
+			});
+		},
+
+		highlight: function( editor )
+		{
+			console.log('highlight');
+
+			// get current data
+			var data  = editor.getData();
+
+			// remove old mark tags
+			data = data.replace(/<mark class="macro">/g, '');
+			data = data.replace(/<\/mark>/g, '');
+			data = data.replace(/<mark class="group-include">/g, '');
+			data = data.replace(/<\/mark>/g, '');
+			data = data.replace(/<mark class="xhubtag">/g, '');
+			data = data.replace(/<\/mark>/g, '');
+
+			// add new mark tags
+			if (editor.mode == 'wysiwyg')
+			{
+				data = data.replace(/(\[\[[^\]]*]])/g, '<mark class="macro">$1</mark>');
+				data = data.replace(/(<group:include[^>]*>)/g, '<mark class="group-include">$1</mark>');
+				data = data.replace(/({xhub:[^}]*})/g, '<mark class="xhubtag">$1</mark>');
 			}
+
+			// set new data
+			editor.setData(data);
 		}
 	});
 })();
