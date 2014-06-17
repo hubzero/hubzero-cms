dfd37a42a369b431d1b104a13ad6b18d85f9cd8f
Christopher Smoak <csmoak@purdue.edu>
2014-06-17 11:24:58 -0400

    [MOD_MYRESOURCES] Removed params that are now in module settings.

--- modules/mod_myresources/tmpl/default.php
+++ modules/mod_myresources/tmpl/default.php
@@ -36,27 +36,6 @@ $juser = JFactory::getUser();
 if (!$this->no_html) {
 ?>
 	<form method="get" action="<?php echo JRoute::_('index.php?option=com_members&id=' . $juser->get('id') . '&active=dashboard'); ?>" data-module="<?php echo $this->module->id; ?>" id="myresources-form" enctype="multipart/form-data">
-		<fieldset>
-			<label for="myresources-sort">
-				<?php echo JText::_('MOD_MYRESOURCES_SORT_BY'); ?>
-				<select name="params[sort]" id="myresources-sort">
-					<option value="publish_up"<?php if ($this->sort == 'publish_up') { echo ' selected="selected"'; } ?>><?php echo JText::_('MOD_MYRESOURCES_DATE'); ?></option>
-					<option value="title"<?php if ($this->sort == 'title') { echo ' selected="selected"'; } ?>><?php echo JText::_('MOD_MYRESOURCES_TITLE'); ?></option>
-					<option value="usage"<?php if ($this->sort == 'usage') { echo ' selected="selected"'; } ?>><?php echo JText::_('MOD_MYRESOURCES_USAGE'); ?></option>
-				</select>
-			</label>
-			<label for="myresources-limit">
-				<?php echo JText::_('MOD_MYRESOURCES_SHOW'); ?>
-				<select name="params[limit]" id="myresources-limit">
-					<option value="5"<?php if ($this->limit == 5) { echo ' selected="selected"'; } ?>>5</option>
-					<option value="10"<?php if ($this->limit == 10) { echo ' selected="selected"'; } ?>>10</option>
-					<option value="20"<?php if ($this->limit == 20) { echo ' selected="selected"'; } ?>>20</option>
-					<option value="50"<?php if ($this->limit == 50) { echo ' selected="selected"'; } ?>>50</option>
-					<option value="100"<?php if ($this->limit == 100) { echo ' selected="selected"'; } ?>>100</option>
-					<option value="all"<?php if ($this->limit == 0) { echo ' selected="selected"'; } ?>><?php echo JText::_('MOD_MYRESOURCES_ALL'); ?></option>
-				</select>
-			</label>
-		</fieldset>
 <?php } ?>
 		<div id="myresources-content">
 		<?php if (!$this->contributions) { ?>
