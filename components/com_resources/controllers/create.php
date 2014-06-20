15487c54ed62a94a82408e321c09bc0af88ae859
Shawn Rice <zooley@purdue.edu>
2014-06-20 15:35:30 -0400

    [NEES][#7664][HABRI][#435][COM_RESOURCES] Removed txtAutoP() method as is no longer needed and could result in double encodings of entities.

--- components/com_resources/controllers/create.php
+++ components/com_resources/controllers/create.php
@@ -748,7 +748,7 @@ class ResourcesControllerCreate extends \Hubzero\Component\SiteController
 		$row->introtext = $row->introtext 
 						? \Hubzero\Utility\String::truncate(strip_tags($row->introtext), 500) 
 						: \Hubzero\Utility\String::truncate(strip_tags($row->fulltxt), 500);
-		$row->fulltxt   = $this->_txtAutoP($row->fulltxt, 1);
+		//$row->fulltxt   = $this->_txtAutoP($row->fulltxt, 1);
 
 		// Get custom areas, add wrapper tags, and compile into fulltxt
 		$type = new ResourcesType($this->database);
@@ -796,7 +796,7 @@ class ResourcesControllerCreate extends \Hubzero\Component\SiteController
 				$f = trim($tagcontent);
 				if ($f)
 				{
-					$row->fulltxt .= (isset($fields[$tagname]) && $fields[$tagname]->type == 'textarea') ? $this->_txtAutoP(trim($tagcontent), 1) : trim($tagcontent);
+					$row->fulltxt .= trim($tagcontent); //(isset($fields[$tagname]) && $fields[$tagname]->type == 'textarea') ? $this->_txtAutoP(trim($tagcontent), 1) : trim($tagcontent);
 				}
 			}
 			$row->fulltxt .= '</nb:' . $tagname . '>' . "\n";
