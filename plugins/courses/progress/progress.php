9ca7aae24c64a6277d395bf4c56138c8bdf59cef
Sam Wilson <samwilson@purdue.edu>
2014-06-27 12:00:05 -0400

    [PLG_COURSES_PROGRESS] fix to control structure spacing

--- plugins/courses/progress/progress.php
+++ plugins/courses/progress/progress.php
@@ -565,7 +565,7 @@ class plgCoursesProgress extends JPlugin
 			$row   = array();
 			$row[] = JFactory::getUser($m->get('user_id'))->get('name');
 			$row[] = JFactory::getUser($m->get('user_id'))->get('email');
-			foreach($assets as $a)
+			foreach ($assets as $a)
 			{
 				$row[] = (isset($grades[$m->get('id')]['assets'][$a->id]['score'])) ? $grades[$m->get('id')]['assets'][$a->id]['score'] : '-';
 			}
