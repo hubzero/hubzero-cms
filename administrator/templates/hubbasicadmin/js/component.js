28e8fcd5f08aca3bb0b3abb9663a241bfcf395ce
Shawn Rice <zooley@purdue.edu>
2014-07-07 16:25:14 -0400

    [TPL_HUBBASICADMIN] Fix for script that can cause infinite loops under certain circumstances

--- administrator/templates/hubbasicadmin/js/component.js
+++ administrator/templates/hubbasicadmin/js/component.js
@@ -1,12 +1,13 @@
 if (typeof(Joomla) == 'undefined')
 {
 	Joomla = {};
-}
-Joomla.submitbutton = function(pressbutton)
-{
-	return submitbutton(pressbutton);
-}
-Joomla.submitform = function(pressbutton)
-{
-	return submitform(pressbutton);
+
+	Joomla.submitbutton = function(pressbutton)
+	{
+		return submitbutton(pressbutton);
+	}
+	Joomla.submitform = function(pressbutton)
+	{
+		return submitform(pressbutton);
+	}
 }
