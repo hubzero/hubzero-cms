7b847946d01cc006043e3e671eeaf727b1cbacfd
Shawn Rice <zooley@purdue.edu>
2014-07-11 08:34:57 -0400

    [COM_USAGE] A little JS clean-up and reworking.

--- components/com_usage/assets/js/usage.js
+++ components/com_usage/assets/js/usage.js
@@ -1,67 +1,42 @@
 /**
  * @package     hubzero-cms
- * @file        components/com_usage/usage.js
+ * @file        components/com_usage/assets/js/usage.js
  * @copyright   Copyright 2005-2011 Purdue University. All rights reserved.
  * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
-//-----------------------------------------------------------
-//  Ensure we have our namespace
-//-----------------------------------------------------------
 if (!HUB) {
 	var HUB = {};
 }
 
-//----------------------------------------------------------
-// Usage stats
-//----------------------------------------------------------
 if (!jq) {
 	var jq = $;
 }
 
 HUB.Usage = {
-	jQuery: jq,
-
-	initialize: function() {
-		var $ = this.jQuery;
-		
-		$('.fixedImgTip').tooltip({
-			position:'TOP RIGHT',
-			//offset: [10,-20],
-			onBeforeShow: function(event, position) {
-				var tip = this.getTip(),
-					tipText = tip[0].innerHTML;
-
-				if (tipText.indexOf(" :: ") != -1) {
-					var parts = tipText.split(" :: ");
-					tip[0].innerHTML = "<span class=\"tooltip-title\">"+parts[0]+"</span><span>"+parts[1]+"</span>";
-				}
-			}
-		}).dynamic({ bottom: { direction: 'down' }, right: { direction: 'left' } });
-	},
-
 	cutup: function(p, h) {
-		var $ = this.jQuery;
-		
-		var pArray = p.split('.');
-		var k = null;
-		switch(h)
+		var pArray = p.split('.'),
+			k = null;
+
+		switch (h)
 		{
 			case '1year': k = '-1'; break;
 			case '2year': k = '-2'; break;
 			case '0year': k = ''; break;
 			default: k = ''; break;
 		}
+
 		var e = pArray.pop();
 		pArray[pArray.length - 1] += k;
 		pArray.push(e);
+
 		return pArray.join('.');
 	},
 
 	deactivate: function(cls, pv) {
-		var $ = this.jQuery;
-		
-		var cLinks = document.getElementsByClassName(cls);
+		var $ = jq,
+			cLinks = document.getElementsByClassName(cls);
+
 		if (cLinks) {
 			for (var k=0; k < cLinks.length; k++)
 			{
@@ -76,16 +51,40 @@ HUB.Usage = {
 			}
 		}
 	},
-	
+
 	loadOFC: function() {
-		var $ = this.jQuery;
-		
-		p = $('#period').val();
-		swfobject.embedSWF("/libraries/ofc/open-flash-chart.swf", "chart", "600", "350", "9.0.0", "expressInstall.swf", {"data-file":"/usage/chart/"+p+"/?no_html=1"});
+		var $ = jq;
+
+		swfobject.embedSWF("/libraries/ofc/open-flash-chart.swf", "chart", "600", "350", "9.0.0", "expressInstall.swf", {
+			"data-file":"/usage/chart/" + $('#period').val() + "/?no_html=1"
+		});
 	}
 }
 
-jQuery(document).ready(function($){
-	HUB.Usage.initialize();
+jQuery(document).ready(function(jq) {
+	var $ = jq;
+
+	if (jQuery.ui && jQuery.ui.tooltip) {
+		$('.fixedImgTip').tooltip({
+			position: {
+				my: 'center bottom',
+				at: 'center top'
+			},
+			// When moving between hovering over many elements quickly, the tooltip will jump around
+			// because it can't start animating the fade in of the new tip until the old tip is
+			// done. Solution is to disable one of the animations.
+			hide: false,
+			create: function(event, ui) {
+				var tip = $(this),
+					tipText = tip.attr('title');
+
+				if (tipText.indexOf('::') != -1) {
+					var parts = tipText.split('::');
+					tip.attr('title', parts[1]);
+				}
+			},
+			tooltipClass: 'tooltip'
+		});
+	}
 });
 
