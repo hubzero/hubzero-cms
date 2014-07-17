1b5ef64edb09f276e64d0644119c719486385f92
Christopher Smoak <csmoak@purdue.edu>
2014-07-17 14:45:52 -0400

    [HABRICENTRAL][#469] Allow forum threads to save with html

--- administrator/components/com_forum/controllers/threads.php
+++ administrator/components/com_forum/controllers/threads.php
@@ -520,7 +520,7 @@ class ForumControllerThreads extends \Hubzero\Component\AdminController
 		JRequest::checkToken() or jexit('Invalid Token');
 
 		// Incoming
-		$fields = JRequest::getVar('fields', array(), 'post');
+		$fields = JRequest::getVar('fields', array(), 'post', 'none', 2);
 		$fields = array_map('trim', $fields);
 
 		if ($fields['id'])
