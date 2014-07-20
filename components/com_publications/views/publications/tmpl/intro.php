932fd29433cf975910a12e35aaddbd878eea0a45
Alissa Nedossekina <alisa@purdue.edu>
2014-07-20 12:14:56 -0400

    [COM_PUBLICATIONS] follow-up commit, fixing whitespaces

--- components/com_publications/views/publications/tmpl/intro.php
+++ components/com_publications/views/publications/tmpl/intro.php
@@ -19,7 +19,7 @@ $i = 1;
 	if ($this->getError()) {
 		echo ('<p class="witherror">' . $this->getError().'</p>');
 	}
- ?>
+?>
 </div>
 <?php } ?>
 <div class="clear block">&nbsp;</div>
@@ -27,18 +27,18 @@ $i = 1;
 	<div class="grid">
 		<div class="col <?php echo ($this->contributable) ? 'span4' : 'span6';  ?>">
 			<h3><?php echo JText::_('Recent Publications'); ?></h3>
-			<?php if($this->results && count($this->results) > 0) { ?>
+			<?php if ($this->results && count($this->results) > 0) { ?>
 				<ul class="mypubs">
-					<?php foreach($this->results as $row) {
+					<?php foreach ($this->results as $row) {
 						// Get version authors
 						$pa = new PublicationAuthor( $this->database );
-						$authors = $pa->getAuthors($row->version_id); 
+						$authors = $pa->getAuthors($row->version_id);
 						$info = array();
-						$info[] =  JHTML::_('date', $row->published_up, 'd M Y');	
-						$info[] = $row->cat_name;	
+						$info[] =  JHTML::_('date', $row->published_up, 'd M Y');
+						$info[] = $row->cat_name;
 						$info[] = JText::_('COM_PUBLICATIONS_CONTRIBUTORS').': '. $this->helper->showContributors( $authors, false, true );
-						
-						$pubthumb = $this->helper->getThumb($row->id, $row->version_id, $this->config, false, $row->cat_url);			
+
+						$pubthumb = $this->helper->getThumb($row->id, $row->version_id, $this->config, false, $row->cat_url);
 					?>
 					<li>
 						<span class="pub-thumb"><img src="<?php echo $pubthumb; ?>" alt=""/></span>
@@ -46,27 +46,27 @@ $i = 1;
 							<a href="<?php echo JRoute::_('index.php?option=com_publications'.a.'id='.$row->id); ?>" title="<?php echo stripslashes($row->abstract); ?>"><?php echo \Hubzero\Utility\String::truncate(stripslashes($row->title), 100); ?></a>
 							<span class="block details"><?php echo implode(' <span>|</span> ', $info); ?></span>
 						</span>
-					</li>	
+					</li>
 					<?php }?>
 				</ul>
-			<?php } else { 
-				echo ('<p class="noresults">'.JText::_('COM_PUBLICATIONS_NO_RELEVANT_PUBS_FOUND').'</a></p>'); 
+			<?php } else {
+				echo ('<p class="noresults">'.JText::_('COM_PUBLICATIONS_NO_RELEVANT_PUBS_FOUND').'</a></p>');
 			} ?>
 		</div>
 		<div class="col <?php echo ($this->contributable) ? 'span4' : 'span6';  ?>">
 			<h3><?php echo JText::_('COM_PUBLICATIONS_PUPULAR'); ?></h3>
 			<?php if ($this->best && count($this->best) > 0) { ?>
 				<ul class="mypubs">
-					<?php foreach($this->best as $row) {
+					<?php foreach ($this->best as $row) {
 						// Get version authors
 						$pa = new PublicationAuthor( $this->database );
-						$authors = $pa->getAuthors($row->version_id); 
+						$authors = $pa->getAuthors($row->version_id);
 						$info = array();
-						$info[] =  JHTML::_('date', $row->published_up, 'd M Y');	
-						$info[] = $row->cat_name;	
+						$info[] =  JHTML::_('date', $row->published_up, 'd M Y');
+						$info[] = $row->cat_name;
 						$info[] = JText::_('COM_PUBLICATIONS_CONTRIBUTORS').': '. $this->helper->showContributors( $authors, false, true );
-						
-						$pubthumb = $this->helper->getThumb($row->id, $row->version_id, $this->config, false, $row->cat_url);			
+
+						$pubthumb = $this->helper->getThumb($row->id, $row->version_id, $this->config, false, $row->cat_url);
 					?>
 					<li>
 						<span class="pub-thumb"><img src="<?php echo $pubthumb; ?>" alt=""/></span>
@@ -74,11 +74,11 @@ $i = 1;
 							<a href="<?php echo JRoute::_('index.php?option=com_publications'.a.'id='.$row->id); ?>" title="<?php echo stripslashes($row->abstract); ?>"><?php echo \Hubzero\Utility\String::truncate(stripslashes($row->title), 100); ?></a>
 							<span class="block details"><?php echo implode(' <span>|</span> ', $info); ?></span>
 						</span>
-					</li>	
+					</li>
 					<?php }?>
 				</ul>
-			<?php } else { 
-				echo ('<p class="noresults">'.JText::_('COM_PUBLICATIONS_NO_RELEVANT_PUBS_FOUND').'</a></p>'); 
+			<?php } else {
+				echo ('<p class="noresults">'.JText::_('COM_PUBLICATIONS_NO_RELEVANT_PUBS_FOUND').'</a></p>');
 			} ?>
 		</div>
 		<?php if ($this->contributable) { ?>
