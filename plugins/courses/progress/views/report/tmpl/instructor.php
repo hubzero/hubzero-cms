2b39f4bfe89164b7906b139ed529f93746bb930a
Sam Wilson <samwilson@purdue.edu>
2014-07-28 08:44:44 -0400

    [HUBZERO][#5659][PLG_COURSES_PROGRESS] include jquery ui

--- plugins/courses/progress/views/report/tmpl/instructor.php
+++ plugins/courses/progress/views/report/tmpl/instructor.php
@@ -31,6 +31,9 @@
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
+\Hubzero\Document\Assets::addSystemScript('jquery.ui');
+\Hubzero\Document\Assets::addSystemStylesheet('jquery.ui.css');
+
 $base = $this->course->offering()->link() . '&active=progress';
 ?>
 
