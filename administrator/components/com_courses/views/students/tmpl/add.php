fe1bbd6a28d921e723de018f7a8f9bdc587f79d2
Shawn Rice <zooley@purdue.edu>
2014-07-28 08:43:53 -0400

    [HUBZERO][#5709][COM_COURSES] Fixing date format issues for date/time picker

--- administrator/components/com_courses/views/students/tmpl/add.php
+++ administrator/components/com_courses/views/students/tmpl/add.php
@@ -171,7 +171,7 @@ function submitbutton(pressbutton)
 			</div>
 			<div class="input-wrap">
 				<label for="enrolled"><?php echo JText::_('COM_COURSES_FIELD_ENROLLED'); ?></label><br />
-				<?php echo JHTML::_('calendar', $this->row->get('enrolled'), 'fields[enrolled]', 'enrolled', "%Y-%m-%d", array('class' => 'inputbox')); ?>
+				<?php echo JHTML::_('calendar', $this->row->get('enrolled'), 'fields[enrolled]', 'enrolled', 'Y-m-d H:i:s', array('class' => 'inputbox')); ?>
 			</div>
 		</fieldset>
 	</div>
