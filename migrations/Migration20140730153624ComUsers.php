9fe646f7745573e6fbff5fff11b74eeb0eddb9ab
Sam Wilson <samwilson@purdue.edu>
2014-07-30 12:44:09 -0400

    [MIGRATIONS] add migration to update new user type parameter from string to id (if applicable)

---
+++ migrations/Migration20140730153624ComUsers.php
@@ -0,0 +1,43 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for fixing bad users parameter from older versions of joomla
+ **/
+class Migration20140730153624ComUsers extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$params    = \JComponentHelper::getParams('com_users');
+		$user_type = $params->get('new_usertype');
+
+		if (is_string($user_type) && strlen($user_type) > 2)
+		{
+			$query = "SELECT `id` FROM `#__usergroups` WHERE `title` = " . $this->db->quote($user_type);
+			$this->db->setQuery($query);
+			if ($id = $this->db->loadResult())
+			{
+				$params->set('new_usertype', $id);
+				$component = new \JTableExtension($this->db);
+				$component->load(array('element'=>'com_users'));
+				$component->set('params', (string) $params);
+				$component->store();
+			}
+			else
+			{
+				$return = new \stdClass();
+				$return->error = new \stdClass();
+				$return->error->type = 'warning';
+				$return->error->message = 'Failed to convert new user type paramter of "' . $user_type . '" to an ID.';
+				return $return;
+			}
+		}
+	}
+}
