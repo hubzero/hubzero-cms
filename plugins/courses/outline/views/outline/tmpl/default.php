178b1dd8e9b79ab6399457861336e7f26fae69d0
Shawn Rice <zooley@purdue.edu>
2014-07-30 13:05:24 -0400

    [HUBZERO][#5767][PLG_COURSES_OUTLINE] Bending to societal pressure and its obsession with spelling and grammar.

--- plugins/courses/outline/views/outline/tmpl/default.php
+++ plugins/courses/outline/views/outline/tmpl/default.php
@@ -460,9 +460,9 @@ if (!$this->course->offering()->access('view') && !$sparams->get('preview', 0)) 
 		<?php } ?>
 	<?php } // close foreach ?>
 <?php elseif($this->course->offering()->access('manage')) : ?>
-		<p class="info">Your outline is currently empty. Go to the <a href="<?php echo JRoute::_($base . '&active=outline&action=build'); ?>">Outline Builder</a> to being creating your course outline</p>
+		<p class="info">Your outline is currently empty. Go to the <a href="<?php echo JRoute::_($base . '&active=outline&action=build'); ?>">Outline Builder</a> to begin creating your course outline.</p>
 <?php else : ?>
-		<p class="info">There is currently no outline available for this course</p>
+		<p class="info">There is currently no outline available for this course.</p>
 <?php endif; ?>
 	</div><!-- / #course-outline -->
 
