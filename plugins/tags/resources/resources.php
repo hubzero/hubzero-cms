bbbbd11b4f0adffeb1db5ee4949de499bf841d36
Shawn Rice <zooley@purdue.edu>
2014-08-01 11:44:03 -0400

    [PLG_TAGS_RESOURCES] Strip potentially bad HTML from output

--- plugins/tags/resources/resources.php
+++ plugins/tags/resources/resources.php
@@ -509,13 +509,13 @@ class plgTagsResources extends JPlugin
 			$html .= ' <span>|</span> ' . JText::_('PLG_TAGS_RESOURCES_CONTRIBUTORS') . ' ' . stripslashes($helper->contributors);
 		}
 		$html .= '</p>' . "\n";
-		if ($row->itext) 
+		if ($row->itext)
 		{
-			$html .= "\t\t" . \Hubzero\Utility\String::truncate(\Hubzero\Utility\Sanitize::stripAll(stripslashes($row->itext)), 200) . "\n";
-		} 
-		else if ($row->ftext) 
+			$html .= "\t\t" . '<p>' . \Hubzero\Utility\String::truncate(strip_tags(stripslashes($row->itext)), 200) . '</p>' . "\n";
+		}
+		else if ($row->ftext)
 		{
-			$html .= "\t\t" . \Hubzero\Utility\String::truncate(\Hubzero\Utility\Sanitize::stripAll(stripslashes($row->ftext)), 200) . "\n";
+			$html .= "\t\t" . '<p>' . \Hubzero\Utility\String::truncate(strip_tags(stripslashes($row->ftext)), 200) . '</p>' . "\n";
 		}
 		$html .= "\t\t" . '<p class="href">' . $juri->base() . trim($row->href, '/') . '</p>' . "\n";
 		$html .= "\t" . '</li>'."\n";
