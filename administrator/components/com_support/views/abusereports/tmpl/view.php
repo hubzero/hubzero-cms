1b242ffa20c49b35c4ce48f4bb3492c1190f35ae
Shawn Rice <zooley@purdue.edu>
2014-08-05 11:09:35 -0400

    [COM_SUPPORT] Fix report abuse links back to original content from admin side

--- administrator/components/com_support/views/abusereports/tmpl/view.php
+++ administrator/components/com_support/views/abusereports/tmpl/view.php
@@ -48,8 +48,10 @@ if (is_object($this->reported)) {
 	}
 	$this->title .= ($this->reported->anon) ? '('.JText::_('ANONYMOUS').')':'';
 
-	$link = '../'.$this->reported->href;
+	$link = str_replace('/administrator', '', $this->reported->href);
 }
+
+JHTML::_('behavior.modal');
 
 ?>
 <script type="text/javascript">
@@ -74,9 +76,9 @@ function submitbutton(pressbutton)
 				<tbody>
 					<tr>
 						<td>
-							<h4><?php echo '<a href="'.$link.'">'.$this->escape($this->title).'</a>: ' ?></h4>
+							<h4><?php echo '<a class="modal" href="'.$link.'">'.$this->escape($this->title).'</a>: ' ?></h4>
 							<p><?php echo (is_object($this->reported)) ? stripslashes($this->reported->text) : ''; ?></p>
-		                    <?php if (is_object($this->reported) && isset($this->reported->subject) && $this->reported->subject!='') {
+							<?php if (is_object($this->reported) && isset($this->reported->subject) && $this->reported->subject!='') {
 								echo '<p>'.$this->escape(stripslashes($this->reported->subject)) .'</p>';
 							} ?>
 						</td>
