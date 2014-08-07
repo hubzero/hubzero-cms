dbb18ff9e61ec023dbb8f13eb3dd6c626d274b84
Shawn Rice <zooley@purdue.edu>
2014-08-07 09:47:59 -0400

    [TPL_HUBBASIC*] Slight adjustment to tooltip script to allow for HTML in tooltips (e.g., user pictures)

--- templates/hubbasic2012/js/hub.js
+++ templates/hubbasic2012/js/hub.js
@@ -10,6 +10,7 @@
 //-----------------------------------------------------------
 if (!HUB) {
 	var HUB = {};
+	HUB.Base = {};
 }
 
 var alertFallback = true;
@@ -35,67 +36,62 @@ if (!jq) {
 	};
 }
 
-HUB.Base = {
-	// Container for jquery. 
-	// Needed for noconflict mode compatibility
-	jQuery: jq,
+jQuery(document).ready(function(jq){
+	var $ = jq,
+		w = 760,
+		h = 520,
+		templatepath = '/templates/hubbasic2012/';
 
-	// Set the base path to this template
-	templatepath: '/templates/hubbasic2012/',
+	// Set focus on username field for login form
+	if ($('#username').length > 0) {
+		$('#username').focus();
+	}
 
-	// launch functions
-	initialize: function() {
-		var $ = this.jQuery, w = 760, h = 520;
+	// Set the search box's placeholder text color
+	if ($('#searchword').length > 0) {
+		$('#searchword')
+			.css('color', '#777')
+			.on('focus', function(){
+				if ($(this).val() == 'Search') {
+					$(this).val('').css('color', '#ddd');
+				}
+			})
+			.on('blur', function(){
+				if ($(this).val() == '' || $(this).val() == 'Search') {
+					$(this).val('Search').css('color', '#777');
+				}
+			});
+	}
 
-		// Set focus on username field for login form
-		if ($('#username').length > 0) {
-			$('#username').focus();
+	// Turn links with specific classes into popups
+	$('a').each(function(i, trigger) {
+		if ($(trigger).is('.demo, .popinfo, .popup, .breeze')) {
+			$(trigger).on('click', function (e) {
+				e.preventDefault();
+
+				if ($(this).attr('class')) {
+					var sizeString = $(this).attr('class').split(' ').pop();
+					if (sizeString && sizeString.match(/\d+x\d+/gi)) {
+						var sizeTokens = sizeString.split('x');
+						w = parseInt(sizeTokens[0]);
+						h = parseInt(sizeTokens[1]);
+					}
+					else if(sizeString && sizeString == 'fullxfull')
+					{
+						w = screen.width;
+						h = screen.height;
+					}
+				}
+
+				window.open($(this).attr('href'), 'popup', 'resizable=1,scrollbars=1,height='+ h + ',width=' + w);
+			});
 		}
-
-		// Set the search box's placeholder text color
-		if ($('#searchword').length > 0) {
-			$('#searchword')
-				.css('color', '#777')
-				.on('focus', function(){
-					if ($(this).val() == 'Search') {
-						$(this).val('').css('color', '#ddd');
-					}
-				})
-				.on('blur', function(){
-					if ($(this).val() == '' || $(this).val() == 'Search') {
-						$(this).val('Search').css('color', '#777');
-					}
-				});
+		if ($(trigger).attr('rel') && $(trigger).attr('rel').indexOf('external') !=- 1) {
+			$(trigger).attr('target', '_blank');
 		}
+	});
 
-		// Turn links with specific classes into popups
-		$('a').each(function(i, trigger) {
-			if ($(trigger).is('.demo, .popinfo, .popup, .breeze')) {
-				$(trigger).on('click', function (e) {
-					e.preventDefault();
-
-					if ($(this).attr('class')) {
-						var sizeString = $(this).attr('class').split(' ').pop();
-						if (sizeString && sizeString.match(/\d+x\d+/gi)) {
-							var sizeTokens = sizeString.split('x');
-							w = parseInt(sizeTokens[0]);
-							h = parseInt(sizeTokens[1]);
-						}
-						else if(sizeString && sizeString == 'fullxfull')
-						{
-							w = screen.width;
-							h = screen.height;
-						}
-					}
-
-					window.open($(this).attr('href'), 'popup', 'resizable=1,scrollbars=1,height='+ h + ',width=' + w);
-				});
-			}
-			if ($(trigger).attr('rel') && $(trigger).attr('rel').indexOf('external') !=- 1) {
-				$(trigger).attr('target', '_blank');
-			}
-		});
-
+	if (jQuery.fancybox) {
 		// Set the overlay trigger for launch tool links
 		$('.launchtool').on('click', function(e) {
 			$.fancybox({
@@ -106,8 +102,10 @@ HUB.Base = {
 
 		// Set overlays for lightboxed elements
 		$('a[rel=lightbox]').fancybox();
+	}
 
-		// Init tooltips
+	// Init tooltips
+	if (jQuery.ui && jQuery.ui.tooltip) {
 		$('.hasTip, .tooltips').tooltip({
 			position: {
 				my: 'center bottom',
@@ -117,6 +115,9 @@ HUB.Base = {
 			// because it can't start animating the fade in of the new tip until the old tip is
 			// done. Solution is to disable one of the animations.
 			hide: false,
+			content: function () {
+				return $(this).attr('title');
+			},
 			create: function(event, ui) {
 				var tip = $(this),
 					tipText = tip.attr('title');
@@ -133,56 +134,43 @@ HUB.Base = {
 		$('.fixedToolTip').tooltip({
 			relative: true
 		});
-		
-		HUB.Base.placeholderSupport();
-	},
-
-	placeholderSupport: function() {
-		var $ = this.jQuery;
-		
-		//test for placeholder support
-		var test = document.createElement('input');
-		var placeholder_supported  = ('placeholder' in test);
-		
-		//if we dont have placeholder support mimic it with focus and blur events
-		if (!placeholder_supported) {
-			$('input[type=text]:not(.no-legacy-placeholder-support)').each(function(i, el) {
-				var placeholderText = $(this).attr('placeholder');
-
-				//make sure we have placeholder text
-				if (placeholderText != '' && placeholderText != null) {
-					//add plceholder text and class
-					if ($(this).val() == '') {
-						$(this).addClass('placeholder-support').val( placeholderText );
-					}
-
-					//attach event listeners to input
-					$(this)
-						.on('focus', function() {
-							if($(this).val() == placeholderText)
-							{
-								$(this).removeClass('placeholder-support').val('');
-							}
-						})
-						.on('blur', function(){
-							if($(this).val() == '')
-							{
-								$(this).addClass('placeholder-support').val( placeholderText );
-							}
-						});
-				}
-			});
-
-			$('form').on('submit', function(event){
-				$('.placeholder-support').each(function(i, el){
-					$(this).val('');
-				});
-			});
-		}
 	}
-};
 
-jQuery(document).ready(function($){
-	HUB.Base.initialize();
+	//test for placeholder support
+	var test = document.createElement('input'),
+		placeholder_supported = ('placeholder' in test);
+
+	//if we dont have placeholder support mimic it with focus and blur events
+	if (!placeholder_supported) {
+		$('input[type=text]:not(.no-legacy-placeholder-support)').each(function(i, el) {
+			var placeholderText = $(el).attr('placeholder');
+
+			//make sure we have placeholder text
+			if (placeholderText != '' && placeholderText != null) {
+				//add plceholder text and class
+				if ($(el).val() == '') {
+					$(el).addClass('placeholder-support').val(placeholderText);
+				}
+
+				//attach event listeners to input
+				$(el)
+					.on('focus', function() {
+						if ($(el).val() == placeholderText) {
+							$(el).removeClass('placeholder-support').val('');
+						}
+					})
+					.on('blur', function(){
+						if ($(el).val() == '') {
+							$(el).addClass('placeholder-support').val(placeholderText);
+						}
+					});
+			}
+		});
+
+		$('form').on('submit', function(event){
+			$('.placeholder-support').each(function (i, el) {
+				$(this).val('');
+			});
+		});
+	}
 });
-
