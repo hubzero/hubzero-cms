9623b0dd42325ec41e28bc45e0cb871630de9ee3
Sam Wilson <samwilson@purdue.edu>
2014-08-08 16:03:21 -0400

    [HUBZERO][#5857][COM_TOOLS] add max_uses field to middleware host table

---
+++ migrations/Migration20140808195514ComTools.php
@@ -0,0 +1,72 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for adding max_uses column to middleware host table
+ **/
+class Migration20140808195514ComTools extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		require_once(JPATH_ROOT . DS . 'components' . DS . 'com_tools' . DS . 'models' . DS . 'mw.utils.php');
+
+		$mwdb = MwUtils::getMWDBO();
+
+		if (!$mwdb->connected())
+		{
+			$this->setError('Failed to connect to the middleware database', 'warning');
+			return false;
+		}
+
+		if ($mwdb->tableExists('host') && !$mwdb->tableHasField('host', 'max_uses'))
+		{
+			$query = "ALTER TABLE `host` ADD COLUMN `max_uses` int(11) UNSIGNED NOT NULL DEFAULT 0";
+			$mwdb->setQuery($query);
+			$mwdb->query();
+		}
+
+		if ($mwdb->tableExists('host') && $mwdb->tableHasField('host', 'uses'))
+		{
+			$query = "ALTER TABLE `host` CHANGE `uses` `uses` INT(11) UNSIGNED NOT NULL DEFAULT 0";
+			$mwdb->setQuery($query);
+			$mwdb->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		require_once(JPATH_ROOT . DS . 'components' . DS . 'com_tools' . DS . 'models' . DS . 'mw.utils.php');
+
+		$mwdb = MwUtils::getMWDBO();
+
+		if (!$mwdb->connected())
+		{
+			$this->setError('Failed to connect to the middleware database', 'warning');
+			return false;
+		}
+
+		if ($mwdb->tableExists('host') && $mwdb->tableHasField('host', 'max_uses'))
+		{
+			$query = "ALTER TABLE `host` DROP COLUMN `max_uses`";
+			$mwdb->setQuery($query);
+			$mwdb->query();
+		}
+
+		if ($mwdb->tableExists('host') && $mwdb->tableHasField('host', 'uses'))
+		{
+			$query = "ALTER TABLE `host` CHANGE `uses` `uses` SMALLINT(5) UNSIGNED NOT NULL DEFAULT 0";
+			$mwdb->setQuery($query);
+			$mwdb->query();
+		}
+	}
+}
