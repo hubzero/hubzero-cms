b7f23d7c11a3f67cf021c6d1377cf83501cb10d5
Sam Wilson <samwilson@purdue.edu>
2014-08-14 09:35:34 -0400

    [HUBZERO][#5716][COM_TOOLS] ensure correct primary key on joblog table

---
+++ migrations/Migration20140813200031ComTools.php
@@ -0,0 +1,54 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for fixing joblob primary key
+ **/
+class Migration20140813200031ComTools extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		require_once(JPATH_ROOT . DS . 'components' . DS . 'com_tools' . DS . 'models' . DS . 'mw.utils.php');
+
+		$mwdb = MwUtils::getMWDBO();
+
+		if (!$mwdb->connected())
+		{
+			$this->setError('Failed to connect to the middleware database', 'warning');
+			return false;
+		}
+
+		if ($mwdb->tableExists('joblog'))
+		{
+			$keys    = $mwdb->getTableKeys('joblog');
+			$primary = array();
+			if ($keys && count($keys) > 0)
+			{
+				foreach ($keys as $key)
+				{
+					if ($key->Key_name == "PRIMARY")
+					{
+						$primary[] = $key->Column_name;
+					}
+				}
+
+				if (!in_array('venue', $primary))
+				{
+					$query = "ALTER TABLE `joblog` DROP PRIMARY KEY";
+					$mwdb->setQuery($query);
+					$mwdb->query();
+					$query = "ALTER TABLE `joblog` ADD PRIMARY KEY (`sessnum`, `job`, `event`, `venue`)";
+					$mwdb->setQuery($query);
+					$mwdb->query();
+				}
+			}
+		}
+	}
+}
