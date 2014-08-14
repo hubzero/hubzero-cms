207bdeafa4454e74fa80fea8133aee6b35528259
Sam Wilson <samwilson@purdue.edu>
2014-08-14 14:23:53 -0400

    [HUBZERO][#5857][COM_TOOLS] INTs should be signed

--- migrations/Migration20140808195514ComTools.php
+++ migrations/Migration20140808195514ComTools.php
@@ -27,14 +27,14 @@ class Migration20140808195514ComTools extends Base
 
 		if ($mwdb->tableExists('host') && !$mwdb->tableHasField('host', 'max_uses'))
 		{
-			$query = "ALTER TABLE `host` ADD COLUMN `max_uses` int(11) UNSIGNED NOT NULL DEFAULT 0";
+			$query = "ALTER TABLE `host` ADD COLUMN `max_uses` int(11) NOT NULL DEFAULT 0";
 			$mwdb->setQuery($query);
 			$mwdb->query();
 		}
 
 		if ($mwdb->tableExists('host') && $mwdb->tableHasField('host', 'uses'))
 		{
-			$query = "ALTER TABLE `host` CHANGE `uses` `uses` INT(11) UNSIGNED NOT NULL DEFAULT 0";
+			$query = "ALTER TABLE `host` CHANGE `uses` `uses` INT(11) NOT NULL DEFAULT 0";
 			$mwdb->setQuery($query);
 			$mwdb->query();
 		}
@@ -64,7 +64,7 @@ class Migration20140808195514ComTools extends Base
 
 		if ($mwdb->tableExists('host') && $mwdb->tableHasField('host', 'uses'))
 		{
-			$query = "ALTER TABLE `host` CHANGE `uses` `uses` SMALLINT(5) UNSIGNED NOT NULL DEFAULT 0";
+			$query = "ALTER TABLE `host` CHANGE `uses` `uses` SMALLINT(5) NOT NULL DEFAULT 0";
 			$mwdb->setQuery($query);
 			$mwdb->query();
 		}
