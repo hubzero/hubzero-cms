e26f18ecd24fdb3c9ff83a82766bddf574827904
Alissa Nedossekina <alisa@purdue.edu>
2014-08-15 09:29:02 -0400

    [CATALYZECARE][#422][PLG_GROUPS_PROJECTS] and onAfterStoreGroups event to sync project membership at time of group membership changes

--- plugins/groups/projects/projects.php
+++ plugins/groups/projects/projects.php
@@ -36,12 +36,12 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 {
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @param      object &$subject Event observer
 	 * @param      array  $config   Optional config values
 	 * @return     void
 	 */
-	public function __construct(&$subject, $config) 
+	public function __construct(&$subject, $config)
 	{
 		parent::__construct($subject, $config);
 
@@ -66,10 +66,10 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 
 	/**
 	 * Return the alias and name for this category of content
-	 * 
+	 *
 	 * @return     array
 	 */
-	public function &onGroupAreas() 
+	public function &onGroupAreas()
 	{
 		$area = array(
 			'name' => 'projects',
@@ -83,7 +83,7 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 
 	/**
 	 * Return data on a group view (this will be some form of HTML)
-	 * 
+	 *
 	 * @param      object  $group      Current group
 	 * @param      string  $option     Name of the component
 	 * @param      string  $authorized User's authorization level
@@ -94,7 +94,7 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 	 * @param      array   $areas      Active area(s)
 	 * @return     array
 	 */
-	public function onGroup($group, $option, $authorized, $limit=0, $limitstart=0, $action='', $access, $areas=null) 
+	public function onGroup($group, $option, $authorized, $limit=0, $limitstart=0, $action='', $access, $areas=null)
 	{
 		$return = 'html';
 		$active = 'projects';
@@ -109,28 +109,28 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		$this_area = $this->onGroupAreas();
 
 		// Check if our area is in the array of areas we want to return results for
-		if (is_array($areas) && $limit) 
+		if (is_array($areas) && $limit)
 		{
-			if (!in_array($this_area['name'], $areas)) 
+			if (!in_array($this_area['name'], $areas))
 			{
 				$return = 'metadata';
 			}
 		}
-		
-		require_once(JPATH_ROOT . DS . 'administrator' . DS . 'components' 
+
+		require_once(JPATH_ROOT . DS . 'administrator' . DS . 'components'
 			. DS . 'com_projects' . DS . 'tables' . DS . 'project.php');
-		
+
 		// Set filters
 		$filters = array();
 		$filters['group']  = $group->get('gidNumber');
 		$filters['mine']     = 1;
-		
+
 		// Get a record count
 		$obj 				= new Project($this->_database);
 		$this->_projects 	= $obj->getGroupProjectIds($group->get('gidNumber'), $this->_juser->get('id'));
 
 		//if we want to return content
-		if ($return == 'html') 
+		if ($return == 'html')
 		{
 			//set group members plugin access level
 			$group_plugin_acl = $access[$active];
@@ -150,15 +150,15 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 			$this->action     = $action;
 
 			//if set to nobody make sure cant access
-			if ($group_plugin_acl == 'nobody') 
+			if ($group_plugin_acl == 'nobody')
 			{
 				$arr['html'] = '<p class="info">' . JText::sprintf('GROUPS_PLUGIN_OFF', ucfirst($active)) . '</p>';
 				return $arr;
 			}
 
 			//check if guest and force login if plugin access is registered or members
-			if ($juser->get('guest') 
-			 && ($group_plugin_acl == 'registered' || $group_plugin_acl == 'members')) 
+			if ($juser->get('guest')
+			 && ($group_plugin_acl == 'registered' || $group_plugin_acl == 'members'))
 			{
 				$url = JRoute::_('index.php?option=com_groups&cn='.$group->get('cn').'&active='.$active);
 				$message = JText::sprintf('GROUPS_PLUGIN_REGISTERED', ucfirst($active));
@@ -167,9 +167,9 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 			}
 
 			//check to see if user is member and plugin access requires members
-			if (!in_array($juser->get('id'), $members) && $group_plugin_acl == 'members' && $authorized != 'admin') 
+			if (!in_array($juser->get('id'), $members) && $group_plugin_acl == 'members' && $authorized != 'admin')
 			{
-				$arr['html'] = '<p class="info">' 
+				$arr['html'] = '<p class="info">'
 					. JText::sprintf('GROUPS_PLUGIN_REQUIRES_MEMBER', ucfirst($active)) . '</p>';
 				return $arr;
 			}
@@ -181,10 +181,10 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 			require_once(JPATH_ROOT . DS . 'components' . DS . 'com_projects' . DS . 'helpers' . DS . 'html.php');
 			require_once(JPATH_ROOT . DS . 'components' . DS . 'com_projects' . DS . 'helpers' . DS . 'imghandler.php');
 
-			// Which view 
+			// Which view
 			$task = $action ? strtolower(trim($action)) : JRequest::getVar('action', '');
 
-			switch ($task) 
+			switch ($task)
 			{
 				case 'all':     $arr['html'] = $this->_view('all');   break;
 				case 'owned':   $arr['html'] = $this->_view('owned'); break;
@@ -192,24 +192,53 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 				default:        $arr['html'] = $this->_view('all');   break;
 			}
 		}
-		
+
 		//get meta
 		$arr['metadata'] = array();
 
 		//return total message count
 		$arr['metadata']['count'] = count($this->_projects);
-		
+
 		// Return the output
 		return $arr;
 	}
 
 	/**
+	 * On after group membership changes - re-sync with projects
+	 *
+	 * @param      object  $group      Current group
+	 * @return     array
+	 */
+	public function onAfterStoreGroup($group)
+	{
+		require_once(JPATH_ROOT . DS . 'administrator' . DS . 'components'
+			. DS . 'com_projects' . DS . 'tables' . DS . 'project.php');
+		require_once(JPATH_ROOT . DS . 'administrator' . DS . 'components'
+			. DS . 'com_projects' . DS . 'tables' . DS . 'project.owner.php');
+
+		// Get group projects
+		$obj  = new Project($this->_database);
+		$objO = new ProjectOwner( $this->_database );
+		$projects = $obj->getGroupProjects($group->get('gidNumber'), $this->_juser->get('id'));
+
+		// Project-group sync
+		if ($projects)
+		{
+			foreach ($projects as $project)
+			{
+				$objO->reconcileGroups($project->id, $project->owned_by_group);
+				$objO->sysGroup($project->alias, $this->_config->get('group_prefix', 'pr-'));
+			}
+		}
+	}
+
+	/**
 	 * View entries
-	 * 
+	 *
 	 * @param      string $which The type of entries to display
 	 * @return     string
 	 */
-	protected function _view($which = 'owned') 
+	protected function _view($which = 'owned')
 	{
 		// Set filters
 		$filters = array();
@@ -229,26 +258,26 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		);
 
 		$obj = new Project($this->_database);
-		if ($which == 'all') 
+		if ($which == 'all')
 		{
 			$filters['which'] = 'owned';
-			$view->owned = $obj->getGroupProjects($this->group->get('gidNumber'), 
+			$view->owned = $obj->getGroupProjects($this->group->get('gidNumber'),
 				$this->_juser->get('id'), $filters, $this->_setup_complete);
 
 			$filters['which'] = 'other';
-			$view->rows = $obj->getGroupProjects($this->group->get('gidNumber'), 
+			$view->rows = $obj->getGroupProjects($this->group->get('gidNumber'),
 				$this->_juser->get('id'), $filters, $this->_setup_complete);
 		}
-		else 
+		else
 		{
 			// Get records
 			$options = array('all', 'owned', 'other');
-			if (!in_array($which, $options)) 
+			if (!in_array($which, $options))
 			{
 				$which = 'owned';
 			}
 			$filters['which'] = $which;
-			$view->rows = $obj->getGroupProjects($this->group->get('gidNumber'), 
+			$view->rows = $obj->getGroupProjects($this->group->get('gidNumber'),
 				$this->_juser->get('id'), $filters, $this->_setup_complete);
 		}
 
@@ -262,7 +291,7 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		$view->config  = $this->_config;
 		$view->option  = 'com_projects';
 		$view->group   = $this->group;
-		if ($this->getError()) 
+		if ($this->getError())
 		{
 			$view->setError($this->getError());
 		}
@@ -272,10 +301,10 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 
 	/**
 	 * Display updates
-	 * 
+	 *
 	 * @return     string
 	 */
-	protected function _updates() 
+	protected function _updates()
 	{
 		require_once(JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_projects' . DS . 'tables' . DS . 'project.comment.php');
 		require_once(JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_projects' . DS . 'tables' . DS . 'project.todo.php');
@@ -298,12 +327,12 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		$filters['getowner'] = 1;
 		$filters['sortdir']  = JRequest::getVar('sortdir', 'ASC');
 
-		// Get all projects group has access to	
+		// Get all projects group has access to
 		$obj = new Project($this->_database);
 		$projects = $obj->getGroupProjectIds($this->group->get('gidNumber'), $this->_juser->get('id'));
 		$view->projectcount = count($projects);
 
-		$projects = $obj->getGroupProjectIds($this->group->get('gidNumber'), 
+		$projects = $obj->getGroupProjectIds($this->group->get('gidNumber'),
 			$this->_juser->get('id'), 1); // active only
 		$view->newcount = $obj->getUpdateCount($projects, $this->_juser->get('id'));
 
@@ -316,10 +345,10 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		$view->limit = 25;
 
 		$afilters['limit'] = JRequest::getVar('limit', 25, 'request');
-		$view->filters = $afilters;	
+		$view->filters = $afilters;
 
 		$activities = $objAC->getActivities(0, $afilters, 0, $this->_juser->get('id'), $projects);
-		$view->activities = $this->prepActivities($activities, 'com_projects', 
+		$view->activities = $this->prepActivities($activities, 'com_projects',
 			$this->_juser->get('id'), $view->filters, $view->limit);
 
 		$view->uid      = $this->_juser->get('id');
@@ -327,7 +356,7 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		$view->option   = 'com_projects';
 		$view->database = $this->_database;
 		$view->group    = $this->group;
-		if ($this->getError()) 
+		if ($this->getError())
 		{
 			$view->setError($this->getError());
 		}
@@ -337,7 +366,7 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 
 	/**
 	 * View entries
-	 * 
+	 *
 	 * @param      string  $activities The type of entries to display
 	 * @param      string  $option     Component name
 	 * @param      integer $uid        User ID
@@ -360,12 +389,12 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		$prep    = array();
 
 		// Loop through activities
-		if ($activities && count($activities) > 0) 
+		if ($activities && count($activities) > 0)
 		{
-			foreach ($activities as $a) 
-			{ 
+			foreach ($activities as $a)
+			{
 				// Is this a comment?
-				if ($a->class == 'quote') 
+				if ($a->class == 'quote')
 				{
 					// Get comment
 					$c = $objC->getComments(NULL, NULL, $a->id);
@@ -374,31 +403,31 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 					$needle  = array('id' => $c->parent_activity);
 					$key     = ProjectsHTML::myArraySearch($needle, $activities);
 					$shown[] = $a->id;
-					if (!$key) 
+					if (!$key)
 					{
 						// get and add parent activity
 						$filters['id'] = $c->parent_activity;
 						$pa = $objAC->getActivities($a->projectid, $filters, 0, $uid);
-						if ($pa && count($pa) > 0) 
+						if ($pa && count($pa) > 0)
 						{
 							$a = $pa[0];
 						}
 					}
-					else 
+					else
 					{
-						$a = $activities[$key]; 
+						$a = $activities[$key];
 					}
 					$a->new = isset($c->newcount) ? $c->newcount : 0;
 				}
 
-				if (!in_array($a->id, $shown)) 
+				if (!in_array($a->id, $shown))
 				{
 					$shown[]   = $a->id;
 					$class     = $a->class ? $a->class : 'activity';
 					$timeclass = '';
 
 					// Display hyperlink
-					if ($a->highlighted && $a->url) 
+					if ($a->highlighted && $a->url)
 					{
 						$a->activity = str_replace($a->highlighted, '<a href="' . $a->url . '">' . $a->highlighted . '</a>', $a->activity);
 					}
@@ -410,10 +439,10 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 					$deletable = 0;
 
 					// Get blog entry
-					if ($class == 'blog') 
+					if ($class == 'blog')
 					{
 						$blog = $objM->getEntries($a->projectid, $bfilters = array('activityid' => $a->id), $a->referenceid);
-						if (!$blog) 
+						if (!$blog)
 						{
 							continue;
 						}
@@ -424,10 +453,10 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 					}
 
 					// Get todo item
-					if ($class == 'todo') 
+					if ($class == 'todo')
 					{
 						$todo = $objTD->getTodos($a->projectid, $tfilters = array('activityid' => $a->id), $a->referenceid);
-						if (!$todo) 
+						if (!$todo)
 						{
 							continue;
 						}
@@ -438,7 +467,7 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 					}
 
 					// Embed links
-					if ($body) 
+					if ($body)
 					{
 						$body = ProjectsHTML::replaceUrls($body, 'external');
 					}
@@ -449,11 +478,11 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 					$ebody .= $body ? '">' . $body . '</span>' : '';
 
 					// Get comments
-					if ($a->commentable) 
+					if ($a->commentable)
 					{
 						$comments = $objC->getComments($eid, $etbl);
 					}
-					else 
+					else
 					{
 						$comments = null;
 					}
@@ -462,7 +491,7 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 					$deletable = 0;
 
 					$prep[] = array(
-						'activity' => $a, 'eid' => $eid, 'etbl' => $etbl, 'body' => $ebody, 'deletable' => $deletable, 'comments' => $comments, 
+						'activity' => $a, 'eid' => $eid, 'etbl' => $etbl, 'body' => $ebody, 'deletable' => $deletable, 'comments' => $comments,
 						'class' => $class, 'timeclass' => $timeclass, 'projectid' => $a->projectid, 'recorded' => $a->recorded
 					);
 				}
@@ -470,18 +499,18 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		}
 
 		return $prep;
-	} 
+	}
 
 	/**
 	 * Browse entries
-	 * 
+	 *
 	 * @return     string
 	 */
 	private function browse()
 	{
 		//include a project helper file for handing project picture
 		$include = JPATH_ROOT . DS . 'components' . DS . 'com_projects' . DS . 'helpers' . DS . 'imghandler.php';
-		if (is_file($include)) 
+		if (is_file($include))
 		{
 			include_once($include);
 		}
@@ -501,23 +530,23 @@ class plgGroupsProjects extends \Hubzero\Plugin\Plugin
 		//select all projects either belonging to the group or the group is collaborating on
 		$sql = "SELECT DISTINCT
 					p.id as id,
-					p.alias as alias, 
-					p.title as title, 
-					p.picture as picture, 
+					p.alias as alias,
+					p.title as title,
+					p.picture as picture,
 					p.type as type,
 					p.private as private,
 					p.created as created,
 					p.owned_by_user as owned_by_user,
 					p.owned_by_group as owned_by_group
-				FROM #__projects as p, #__project_owners as po 
-				WHERE p.id=po.projectid 
-				AND p.state=1 
+				FROM #__projects as p, #__project_owners as po
+				WHERE p.id=po.projectid
+				AND p.state=1
 				AND po.groupid=" . $db->quote($this->group->get('gidNumber')) . " ORDER BY p.owned_by_group DESC";
 
 		//set the query
 		$db->setQuery($sql);
 
-		//get the results 
+		//get the results
 		$projects = $db->loadAssocList();
 
 		// Get the component parameters
