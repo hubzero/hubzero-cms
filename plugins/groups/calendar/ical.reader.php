41950a02919b58dde31504128499939d09e6a26e
Christopher Smoak <csmoak@purdue.edu>
2014-08-19 12:41:05 -0400

    [HUBZERO][#5985] Fixed event import.

--- plugins/groups/calendar/ical.reader.php
+++ plugins/groups/calendar/ical.reader.php
@@ -139,29 +139,10 @@ class iCalReader
 
 	public function iCalDateToUnixTimestamp( $icalDate )
 	{
-		$icalDate = str_replace('T', '', $icalDate);
-		$icalDate = str_replace('Z', '', $icalDate);
-
-		$pattern  = '/([0-9]{4})';   // 1: YYYY
-		$pattern .= '([0-9]{2})';    // 2: MM
-		$pattern .= '([0-9]{2})';    // 3: DD
-		$pattern .= '([0-9]{0,2})';  // 4: HH
-		$pattern .= '([0-9]{0,2})';  // 5: MM
-		$pattern .= '([0-9]{0,2})/'; // 6: SS
-		preg_match($pattern, $icalDate, $date);
-
-		// Unix timestamp can't represent dates before 1970
-		if ($date[1] <= 1970)
-		{
-			return false;
-		}
-
-		// Unix timestamps after 03:14:07 UTC 2038-01-19 might cause an overflow
-		// if 32 bit integers are used.
-		$timestamp = mktime((int)$date[4], (int)$date[5], (int)$date[6], (int)$date[2], (int)$date[3], (int)$date[1]);
-
-		//return time
-		return  $timestamp;
+		$date = JFactory::getDate($icalDate, new DateTimezone('UTC'));
+		$tz   = JFactory::getApplication()->getCfg('offset', 'UTC');
+		$date->setTimezone(new DateTimezone($tz));
+		return $date->toUnix();
 	}
 
 	/**
