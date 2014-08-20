f4e60ab2c14fbef2b52ac0bc8bb39f38a9d72d0d
Alissa Nedossekina <alisa@purdue.edu>
2014-08-20 13:39:26 -0400

    [PURR][#457][COM_PUBLICATIONS] fix to pub image gallery scrolling, plus remove .jquery file

--- components/com_publications/assets/js/publications.js
+++ components/com_publications/assets/js/publications.js
@@ -13,180 +13,144 @@ if (!HUB) {
 }
 
 //----------------------------------------------------------
-// Publication Ranking pop-ups
+// Resource Ranking pop-ups
 //----------------------------------------------------------
+if (!jq) {
+	var jq = $;
+}
 
 HUB.Publications = {
 
+	jQuery: jq,
+
+	settings: {
+	},
+
 	initialize: function() {
-		if (typeof(SqueezeBoxHub) != "undefined") {
-			if (!SqueezeBoxHub) {
-				SqueezeBoxHub.initialize({ size: {x: 750, y: 500} });
-			}
-			
-			// Modal boxes for presentations
-			$$('a.play').each(function(el) {
-				if (el.href.indexOf('?') == -1) {
-					el.href = el.href + '?no_html=1';
+		var $ = this.jQuery;
+
+		$('a.play').fancybox({
+			type: 'ajax',
+			width: 800,
+			height: 'auto',
+			autoSize: false,
+			fitToView: false,
+			wrapCSS: 'sbp-window',
+			beforeLoad: function() {
+				href = $(this).attr('href');
+				if (href.indexOf('?') == -1) {
+					href += '?no_html=1';
 				} else {
-					el.href = el.href + '&no_html=1';
+					href += '&no_html=1';
 				}
-				el.addEvent('click', function(e) {
-					new Event(e).stop();
+				$(this).attr('href', href);
 
-					w = 750;
-					h = 500;
-					if (this.className) {
-						var sizeString = this.className.split(' ').pop();
-						if (sizeString && sizeString != 'play') {
-							var sizeTokens = sizeString.split('x');
-							w = parseInt(sizeTokens[0]);
-							h = parseInt(sizeTokens[1]);
-						}
-					}
-
-					SqueezeBoxHub.fromElement(el,{handler: 'url', size: {x: w, y: h}, ajaxOptions: {method: 'get',evalScripts:true}});
-				});
-			});
-			
-			var win = new Element('a', {
-				id: 'sbox-newwindow',
-				href: '#',
-				title: 'Open in a new window',
-				events: {
-					'click': function(event) {
-						OpenWindow = window.open('', "newwin", "height=500,width=700,toolbar=no,scrollbars=yes,menubar=no,resizable=yes,location=no,scrollbars=no,directories=no,status=no");
-						OpenWindow.document.write('<html>');
-						OpenWindow.document.write('<title>Presentation</title>');
-						OpenWindow.document.write('<body style="margin:0;padding:0;border:0;">');
-						OpenWindow.document.write( $('sbox-window').innerHTML );
-						OpenWindow.document.write('</body>');
-						OpenWindow.document.write('</html>');
-						OpenWindow.document.close();
-						self.name = 'main';
-						SqueezeBoxHub.close();
+				if ($(this).attr('class')) {
+					var sizeString = $(this).attr('class').split(' ').pop();
+					if (sizeString && sizeString.match('/\d+x\d+/')) {
+						var sizeTokens = sizeString.split('x');
+						$.fancybox().width = parseInt(sizeTokens[0]);
+						$.fancybox().height = parseInt(sizeTokens[1]);
 					}
 				}
-			});
-			var res = new Element('div', {
-				id: 'sbox-resizehandle',
-				alt: 'Resize'
-			})
-			
-			if($('sbox-window'))
-			{
-				$('sbox-window').adopt(win, res);
-
-				// Init the resizing capabilities
-				$('sbox-window').makeResizable({
-					handle:$('sbox-resizehandle'),
-					onComplete: function(el) {
-						var size = el.getCoordinates();
-						SqueezeBoxHub.resize({x:size.width,y:size.height},false)
-					}
-				});	
-			}
-		}
-		
-		// Fixed resource tooltips
-		var rTTips = new MooTips($$('.fixedResourceTip'), {
-			showDelay: 500,
-			maxTitleChars: 100,
-			className: 'resource',
-			fixed: true,
-			offsets: {'x':20,'y':5}
-		});
-		
-		// Ranking info pop-up
-		var metadata = $$('.metadata');
-		if (metadata) {
-			/*
-			metadata.each(function(meta) {
-				meta.addEvent('mouseover', function(e) {
-					var el = this.getElement('.rankinfo');
-					el.addClass('active');
-				});
-				meta.addEvent('mouseout', function(e) {
-					var el = this.getElement('.rankinfo');
-					el.removeClass('active');
-				});
-			});
-			*/
-			metadata.each(function(meta) {
-				$$('.rankinfo').addEvent('mouseover', function(e) {
-						this.addClass('active');
+			},
+			afterShow: function() {
+				if ($('#embedded-content')) {
+					$('#embedded-content').contents().find("body").css({'max-height':'460px'});
+					$('#embedded-content').contents().find("video").css({'max-height':'450px'});
+					$('#embedded-content').contents().find("body").css({'max-width':'800px'});
+					$('#embedded-content').contents().find("video").css({'width':'100%'});
+				}
+				if ($('#hubForm-ajax')) {
+					$('#hubForm-ajax').submit(function(e) {
+						e.preventDefault();
+						$.post($(this).attr('action'));
+						$.fancybox().close();
+						return false;
 					});
-				$$('.rankinfo').addEvent('mouseout', function(e) {
-						this.removeClass('active');
-				});
+				}
+			}
+		});
+
+		$('.fixedResourceTip').tooltip({
+			position:'TOP RIGHT',
+			offset: [10,-20],
+			onBeforeShow: function(event, position) {
+				var tip = this.getTip(),
+					tipText = tip[0].innerHTML;
+
+				if (tipText.indexOf(" :: ") != -1) {
+					var parts = tipText.split(" :: ");
+					tip[0].innerHTML = "<span class=\"tooltip-title\">"+parts[0]+"</span><span>"+parts[1]+"</span>";
+				}
+			}
+		});
+
+		$('.metadata').each(function(i, meta) {
+			$('.rankinfo').live('mouseover', function(e) {
+				$(this).addClass('active');
 			});
-		}
-		
+			$('.rankinfo').live('mouseout', function(e) {
+				$(this).removeClass('active');
+			});
+		});
+
 		// Audience info pop-up
-		var explainscale = $$('.explainscale');
-		if (explainscale) {
-			var ex = metadata.getElement('.explainscale');		
-			$$('.usagescale').each(function(item) {
-					
-					item.addEvent('mouseover', function() {					
-						ex.addClass('active');
-					});
+		$('.explainscale').each(function(k, ex) {
+			$('.usagescale').each(function(i, item) {
+				$(item).live('mouseover', function() {
+					$(ex).addClass('active');
+				});
 			});
-			$$('.usagescale').each(function(item) {
-				
-					item.addEvent('mouseout', function() {					
-						ex.removeClass('active');
-					});
+			$('.usagescale').each(function(i, item) {
+				$(item).live('mouseout', function() {
+					$(ex).removeClass('active');
+				});
+			});
+		});
+
+		// Primary-document info pop-up
+		if ($('#primary-document') && $('#primary-document_pop')) {
+			$('#primary-document').live('mouseover', function(e) {
+				$('#primary-document_pop').show();
+			});
+			$('#primary-document').live('mouseout', function(e) {
+				$('#primary-document_pop').hide();
 			});
 		}
-		
-		// Primary-document info pop-up
-		var primarydoc = $('primary-document');
-		var primarydocpop = $('primary-document_pop');
-		if (primarydoc && primarydocpop) {
-			primarydoc.addEvent('mouseover', function(e) {
-				//new Event(e).stop();
-				primarydocpop.style.display = "block";
-			});
-			
-			primarydoc.addEvent('mouseout', function(e) {
-				primarydocpop.style.display = "none";
-			});
-		} 
-		
-		
+
 		//Hubpresenter
-		$$(".hubpresenter, .video").each(function(el) {
-			if (el.href.indexOf('?') == -1) {
-				el.href = el.href + '?tmpl=component';
+		$(".hubpresenter, .video").each(function(i, el) {
+			if ($(el).attr('href') && $(el).attr('href').indexOf('?') == -1) {
+				$(el).attr('href', $(el).attr('href') + '?tmpl=component');
 			} else {
-				el.href = el.href + '&tmpl=component';
+				$(el).attr('href', $(el).attr('href') + '&tmpl=component');
 			}
 		});
-		
+
 		//HUBpresenter open window
-		$$(".hubpresenter").addEvent("click", function(e) {
+		$(".hubpresenter").click(function(e) {
 			mobile = navigator.userAgent.match(/iPad|iPhone|iPod|Android/i) != null;
-			if(!mobile) {
-				new Event(e).stop();
-		 		HUBpresenter_window = window.open(this.href,'name','height=650,width=1100');
+			if (!mobile) {
+				e.preventDefault();
+		 		HUBpresenter_window = window.open($(this).attr('href'),'name','height=650,width=1100');
 			}
 		});
-		
-		$$(".video").addEvent("click", function(e) {
+
+		$(".video").click(function(e) {
 			mobile = navigator.userAgent.match(/iPad|iPhone|iPod|Android/i) != null;
-			if(!mobile) {
-				new Event(e).stop();
+			if (!mobile) {
+				e.preventDefault();
 				var w = 0,
 					h = 0,
 					dw = 900,
 					dh = 600;
-			
+
 				//get the dimensions from classs name
-				dim = this.className.split(" ").pop();
-				
+				dim = $(this).attr('class').split(" ").pop();
+
 				//if we have dimensions then parse them
-				if(dim.match(/[0-9]{2,}x[0-9]{2,}/g)) {
+				if (dim.match(/[0-9]{2,}x[0-9]{2,}/g)) {
 					dim = dim.split("x");
 					w = dim[0];
 					h = dim[1];
@@ -194,103 +158,95 @@ HUB.Publications = {
 					w = dw;
 					h = dh;
 				}
-				
+
 				//open poup
-		 		video_window = window.open( this.href,'videowindow','height=' + h + ', width=' + w + ', menubar=no, toolbar=yes, titlebar=no, resizable=yes');
+		 		video_window = window.open( $(this).attr('href'),'videowindow','height=' + h + ', width=' + w + ', menubar=no, toolbar=no, titlebar=no, resizable=no');
 			}
 		});
-		
+
 		//------------------------
 		// screenshot thumbnail slider
 		//------------------------
-		
-		var target = $$('.showcase-pane')[0];
-        	
-		if($('showcase') && target) {	
-			var sidemargin = 4;
-  			var thumbwidth = 110;
-			var moveto = 0;
-			var active = 0;
-			var panels = 0;
-			
-			var next = $('showcase-next');
-			var prev = $('showcase-prev');
-			
-			thwidth = $$('.thumbima').length * sidemargin * 2 + $$('.thumbima').length * thumbwidth;
-			var win_width = $('showcase-window').offsetWidth;
-			
-			if(thwidth/win_width < 1) {
+
+		var target = $('.showcase-pane')[0];
+
+		if ($('#showcase') && target) {
+			var sidemargin = 4,
+				thumbwidth = 110,
+				moveto = 0,
+				active = 0,
+				panels = 0;
+
+			var next = $('#showcase-next'),
+				prev = $('#showcase-prev');
+
+			thwidth = $('.thumbima').length * sidemargin * 2 + $('.thumbima').length * thumbwidth;
+			var win_width = $('#showcase-window').offsetWidth;
+
+			if (thwidth/win_width < 1) {
 				next.addClass('inactive');
 				prev.addClass('inactive');
 			}
-					
-			// go next		
-			if (next) {
-				next.addEvent('mouseover', function() {
-					//var win_width = $('showcase-window').getStyle('width').toInt();
-					var win_width = $('showcase-window').offsetWidth;
-					if(thwidth/win_width < 1) {
-						this.addClass('inactive');
+
+			// go next
+			if (next.length > 0) {
+				next.on('mouseover', function() {
+					var win_width = $('#showcase-window').offset().left;
+					if (thwidth/win_width < 1) {
+						$(this).addClass('inactive');
 						prev.addClass('inactive');
-					}
-					else {
-						this.removeClass('inactive');
+					} else {
+						$(this).removeClass('inactive');
 						prev.removeClass('inactive');
 					}
-				});								
-				next.addEvent('click', function() {
-					var win_width = $('showcase-window').offsetWidth;
-					if(thwidth/win_width < 1) {
-					 	panels = 0;	
-					}
-					else {
+				});
+
+				next.on('click', function() {
+					var win_width = $('#showcase-window').offset().left;
+					if (thwidth/win_width < 1) {
+					 	panels = 0;
+					} else {
 						panels = Math.round(thwidth/win_width);
 					}
-					
-					if(panels >= 1 && active < panels) {
+
+					if (panels >= 1 && active < panels) {
 						active ++;
 						moveto -= win_width;
-								
-						var fx = new Fx.Styles(target, {duration: 600, wait: false});
-						 fx.start({
-							'left': [moveto]
-						});
+
+						$(target).css('left', moveto);
 					}
 				});
 			}
-			
+
 			// go prev
-			if (prev) {
-				prev.addEvent('mouseover', function() {
-					var win_width = $('showcase-window').offsetWidth;
-					if(thwidth/win_width < 1) {
-						this.addClass('inactive');
+			if (prev.length > 0) {
+				prev.on('mouseover', function() {
+					var win_width = $('#showcase-window').offset().left;
+					if (thwidth/win_width < 1) {
+						$(this).addClass('inactive');
 						next.addClass('inactive');
-					}
-					else {
-						this.removeClass('inactive');
+					} else {
+						$(this).removeClass('inactive');
 						next.removeClass('inactive');
 					}
-				});	
-				prev.addEvent('click', function() {
-					var win_width = $('showcase-window').offsetWidth;
-					var panels = Math.round(thwidth/win_width);	
-					
-					if(panels >= 1 && active > 0) {
-									
+				});
+
+				prev.on('click', function() {
+					var win_width = $('#showcase-window').offset().left,
+						panels = Math.round(thwidth/win_width);
+
+					if (panels >= 1 && active > 0) {
 						active --;
 						moveto += win_width;
-	
-						var fxright = new Fx.Styles(target, {duration: 600, wait: false});
-						 fxright.start({
-							'left': [moveto]
-						});
+
+						$(target).css('left', moveto);
 					}
 				});
 			}
-		
 		}
 	} // end initialize
 }
 
-window.addEvent('domready', HUB.Publications.initialize);
+jQuery(document).ready(function($){
+	HUB.Publications.initialize();
+});
