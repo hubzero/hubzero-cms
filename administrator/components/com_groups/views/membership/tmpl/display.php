8506d43761c9ac9e29dfd2fd9a4e26994a43df05
Christopher Smoak <csmoak@purdue.edu>
2014-08-21 08:47:23 -0400

    [NEES][#8069][1.2.2] Made new member popup height taller to avoid scrollbar.

--- administrator/components/com_groups/views/membership/tmpl/display.php
+++ administrator/components/com_groups/views/membership/tmpl/display.php
@@ -36,7 +36,7 @@ JToolBarHelper::title(JText::_('COM_GROUPS'), 'groups.png');
 
 $bar =  JToolBar::getInstance('toolbar');
 // Add an upload button
-$bar->appendButton( 'Popup', 'new', JText::_('New'), 'index.php?option=' . $this->option . '&controller=' . $this->controller . '&tmpl=component&task=new&gid=' . $this->filters['gid'], 570, 170 );
+$bar->appendButton( 'Popup', 'new', JText::_('New'), 'index.php?option=' . $this->option . '&controller=' . $this->controller . '&tmpl=component&task=new&gid=' . $this->filters['gid'], 570, 225 );
 
 JToolBarHelper::spacer();
 switch ($this->filters['status'])
