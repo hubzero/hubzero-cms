1fe54c7e38a88ffc4ffbdce6b586ea907d84e9bf
Shawn Rice <zooley@purdue.edu>
2014-08-26 09:48:55 -0400

    [HUBZERO][#6118][PLG_USAGE_OVERVIEW] Try to get empty table cell count accurate

--- plugins/usage/overview/overview.php
+++ plugins/usage/overview/overview.php
@@ -117,16 +117,17 @@ class plgUsageOverview extends JPlugin
 	{
 		$html = '';
 
+		$i = 0;
+
 		$sql = "SELECT value, valfmt FROM summary_user_vals WHERE rowid='$id' AND period='$period' AND datetime='$datetime' ORDER BY colid";
 		$db->setQuery($sql);
 		$results = $db->loadObjectList();
 		if ($results) 
 		{
-			$i = 0;
 			foreach ($results as $row)
 			{
 				$i++;
-				$cls = ($i >= 7) ? ' class="group"' : '';
+				$cls = ($i >= 6) ? ' class="group"' : '';
 				if ($i == 1) 
 				{
 					$cls = ' class="highlight"';
@@ -163,10 +164,10 @@ class plgUsageOverview extends JPlugin
 				}
 				$html .= "\t\t\t" . '<td' . $cls . '>' . trim($this->_fmt_result($val, $row->valfmt)) . '</td>' . "\n";
 			}
-			if ($i == 1) 
-			{
-				$html .= $this->_empty_rows(10);
-			}
+		}
+		if ($i == 0) 
+		{
+			$html .= $this->_empty_rows(11);
 		}
 		return $html;
 	}
@@ -238,7 +239,7 @@ class plgUsageOverview extends JPlugin
 		$i = 0;
 		for ($i=0, $n; $i < $n; $i++)
 		{
-			$cls = ($i >= 5) ? ' class="group"' : '';
+			$cls = ($i >= 6) ? ' class="group"' : '';
 			$html .= "\t\t\t" . '<td' . $cls . '>-</td>' . "\n";
 		}
 		return $html;
