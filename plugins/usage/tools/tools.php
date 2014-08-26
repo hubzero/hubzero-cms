460eb5be307c6a930c5db594c2919eb4cc6493f3
Shawn Rice <zooley@purdue.edu>
2014-08-26 10:13:54 -0400

    [HUBZERO][#6120][PLG_USAGE_TOOLS] Spaces. Dumb ol' spaces again.

--- plugins/usage/tools/tools.php
+++ plugins/usage/tools/tools.php
@@ -456,7 +456,7 @@ class plgUsageTools extends \Hubzero\Plugin\Plugin
 							{
 								$html .= $i - 1;
 							}
-						   	$html .= ' - ' . $month . ' ' . $i . '</option>' . "\n";
+							$html .= ' - ' . $month . ' ' . $i . '</option>' . "\n";
 						}
 					}
 				}
