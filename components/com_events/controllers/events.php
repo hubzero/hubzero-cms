461423e3cdfaf8a86afc1ea3f3488542f223e3dd
Christopher Smoak <csmoak@purdue.edu>
2014-08-28 12:44:28 -0400

    [HUBZERO][#6136] Fixed events php warning.

--- components/com_events/controllers/events.php
+++ components/com_events/controllers/events.php
@@ -522,7 +522,6 @@ class EventsControllerEvents extends \Hubzero\Component\SiteController
 			$week['month'] = sprintf("%02d", $this_currentdate->month);
 			$week['year']  = sprintf("%4d",  $this_currentdate->year);
 
-			$filters['select_date'] = sprintf("%4d-%02d-%02d", $week['year'], $week['month'], $week['day']);
 			$select_date     = sprintf("%4d-%02d-%02d 00:00:00", $week['year'], $week['month'], $week['day']);
 			$select_date_fin = sprintf("%4d-%02d-%02d 23:59:59", $week['year'], $week['month'], $week['day']);
 			$select_date     = JFactory::getDate($select_date, JFactory::getConfig()->get('offset'));
@@ -596,9 +595,15 @@ class EventsControllerEvents extends \Hubzero\Component\SiteController
 		// Get the events for this day
 		$filters = array();
 		$filters['gid'] = $this->gid;
-		$filters['select_date'] = sprintf("%4d-%02d-%02d", $year, $month, $day);
 		$filters['category'] = $this->category;
 		$filters['scope'] = 'event';
+
+		$select_date     = sprintf("%4d-%02d-%02d 00:00:00", $year, $month, $day);
+		$select_date_fin = sprintf("%4d-%02d-%02d 23:59:59", $year, $month, $day);
+		$select_date     = JFactory::getDate($select_date, JFactory::getConfig()->get('offset'));
+		$select_date_fin = JFactory::getDate($select_date_fin, JFactory::getConfig()->get('offset'));
+		$filters['select_date'] = $select_date->toSql();
+		$filters['select_date_fin'] = $select_date_fin->toSql();
 
 		$ee = new EventsEvent($this->database);
 		$events = $ee->getEvents('day', $filters);
