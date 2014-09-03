b8964a28c46e09ffd382193dc10f76a71c9a1a6a
Sam Wilson <samwilson@purdue.edu>
2014-09-03 16:52:38 -0400

    [MIGRATIONS] make migration a little more resilient

--- migrations/Migration20131108095500ComAnswers.php
+++ migrations/Migration20131108095500ComAnswers.php
@@ -17,16 +17,47 @@ class Migration20131108095500ComAnswers extends Base
 	{
 		if ($this->db->tableExists('#__answers_questions'))
 		{
-			$query = "ALTER TABLE `#__answers_questions`
-					CHANGE `id` `id` INT(11)  UNSIGNED  NOT NULL  AUTO_INCREMENT,
-					CHANGE `subject` `subject` VARCHAR(250)  NOT NULL  DEFAULT '',
-					CHANGE `question` `question` TEXT  NOT NULL,
-					CHANGE `created_by` `created_by` VARCHAR(50)  NOT NULL  DEFAULT '',
-					CHANGE `email` `email` TINYINT(2)  UNSIGNED  NOT NULL  DEFAULT '0',
-					CHANGE `helpful` `helpful` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'
-			;";
-			$this->db->setQuery($query);
-			$this->db->query();
+			if ($this->db->tableHasField('#__answers_questions', 'id'))
+			{
+				$query = "ALTER TABLE `#__answers_questions` CHANGE `id` `id` INT(11)  UNSIGNED  NOT NULL  AUTO_INCREMENT";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_questions', 'subject'))
+			{
+				$query = "ALTER TABLE `#__answers_questions` CHANGE `subject` `subject` VARCHAR(250)  NOT NULL  DEFAULT ''";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_questions', 'question'))
+			{
+				$query = "ALTER TABLE `#__answers_questions` CHANGE `question` `question` TEXT  NOT NULL";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_questions', 'created_by'))
+			{
+				$query = "ALTER TABLE `#__answers_questions` CHANGE `created_by` `created_by` VARCHAR(50)  NOT NULL  DEFAULT ''";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_questions', 'email'))
+			{
+				$query = "ALTER TABLE `#__answers_questions` CHANGE `email` `email` TINYINT(2)  UNSIGNED  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_questions', 'helpful'))
+			{
+				$query = "ALTER TABLE `#__answers_questions` CHANGE `helpful` `helpful` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
 
 			if (!$this->db->tableHasKey('#__answers_questions', 'idx_created_by'))
 			{
@@ -45,16 +76,47 @@ class Migration20131108095500ComAnswers extends Base
 
 		if ($this->db->tableExists('#__answers_responses'))
 		{
-			$query = "ALTER TABLE `#__answers_responses`
-					CHANGE `id` `id` INT(11)  UNSIGNED  NOT NULL  AUTO_INCREMENT,
-					CHANGE `qid` `qid` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0',
-					CHANGE `created_by` `created_by` VARCHAR(50)  NOT NULL  DEFAULT '',
-					CHANGE `state` `state` TINYINT(3)  NOT NULL  DEFAULT '0',
-					CHANGE `nothelpful` `nothelpful` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0',
-					CHANGE `helpful` `helpful` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'
-			;";
-			$this->db->setQuery($query);
-			$this->db->query();
+			if ($this->db->tableHasField('#__answers_responses', 'id'))
+			{
+				$query = "ALTER TABLE `#__answers_responses` CHANGE `id` `id` INT(11)  UNSIGNED  NOT NULL  AUTO_INCREMENT";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_responses', 'qid'))
+			{
+				$query = "ALTER TABLE `#__answers_responses` CHANGE `qid` `qid` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_responses', 'created_by'))
+			{
+				$query = "ALTER TABLE `#__answers_responses` CHANGE `created_by` `created_by` VARCHAR(50)  NOT NULL  DEFAULT ''";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_responses', 'state'))
+			{
+				$query = "ALTER TABLE `#__answers_responses` CHANGE `state` `state` TINYINT(3)  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_responses', 'nothelpful'))
+			{
+				$query = "ALTER TABLE `#__answers_responses` CHANGE `nothelpful` `nothelpful` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_responses', 'helpful'))
+			{
+				$query = "ALTER TABLE `#__answers_responses` CHANGE `helpful` `helpful` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
 
 			if (!$this->db->tableHasKey('#__answers_responses', 'idx_created_by'))
 			{
@@ -80,14 +142,33 @@ class Migration20131108095500ComAnswers extends Base
 
 		if ($this->db->tableExists('#__answers_questions_log'))
 		{
-			$query = "ALTER TABLE `#__answers_questions_log`
-					CHANGE `id` `id` INT(11)  UNSIGNED  NOT NULL  AUTO_INCREMENT,
-					CHANGE `voter` `voter` INT(11)  NOT NULL  DEFAULT '0',
-					CHANGE `ip` `ip` VARCHAR(15)  NOT NULL  DEFAULT '',
-					CHANGE `qid` `qid` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'
-			;";
-			$this->db->setQuery($query);
-			$this->db->query();
+			if ($this->db->tableHasField('#__answers_questions_log', 'id'))
+			{
+				$query = "ALTER TABLE `#__answers_questions_log` CHANGE `id` `id` INT(11)  UNSIGNED  NOT NULL  AUTO_INCREMENT";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_questions_log', 'voter'))
+			{
+				$query = "ALTER TABLE `#__answers_questions_log` CHANGE `voter` `voter` INT(11)  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_questions_log', 'ip'))
+			{
+				$query = "ALTER TABLE `#__answers_questions_log` CHANGE `ip` `ip` VARCHAR(15)  NOT NULL  DEFAULT ''";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_questions_log', 'qid'))
+			{
+				$query = "ALTER TABLE `#__answers_questions_log` CHANGE `qid` `qid` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
 
 			if (!$this->db->tableHasKey('#__answers_questions_log', 'idx_voter'))
 			{
@@ -106,14 +187,33 @@ class Migration20131108095500ComAnswers extends Base
 
 		if ($this->db->tableExists('#__answers_log'))
 		{
-			$query = "ALTER TABLE `#__answers_log`
-					CHANGE `id` `id` INT(11)  UNSIGNED  NOT NULL  AUTO_INCREMENT,
-					CHANGE `ip` `ip` VARCHAR(15)  NOT NULL  DEFAULT '',
-					CHANGE `rid` `rid` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0',
-					CHANGE `helpful` `helpful` VARCHAR(10)  NOT NULL  DEFAULT ''
-			;";
-			$this->db->setQuery($query);
-			$this->db->query();
+			if ($this->db->tableHasField('#__answers_log', 'id'))
+			{
+				$query = "ALTER TABLE `#__answers_log` CHANGE `id` `id` INT(11)  UNSIGNED  NOT NULL  AUTO_INCREMENT";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_log', 'ip'))
+			{
+				$query = "ALTER TABLE `#__answers_log` CHANGE `ip` `ip` VARCHAR(15)  NOT NULL  DEFAULT ''";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_log', 'rid'))
+			{
+				$query = "ALTER TABLE `#__answers_log` CHANGE `rid` `rid` INT(11)  UNSIGNED  NOT NULL  DEFAULT '0'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasField('#__answers_log', 'helpful'))
+			{
+				$query = "ALTER TABLE `#__answers_log` CHANGE `helpful` `helpful` VARCHAR(10)  NOT NULL  DEFAULT ''";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
 
 			if (!$this->db->tableHasKey('#__answers_log', 'idx_rid'))
 			{
