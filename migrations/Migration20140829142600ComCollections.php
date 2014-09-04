45a5b53bb2bb1513c54e57e67a9af1c29134042e
Shawn Rice <zooley@purdue.edu>
2014-09-04 13:45:17 -0400

    [NANOHUB][#267563][MIGRATION] Extra double-paranoid data check

--- migrations/Migration20140829142600ComCollections.php
+++ migrations/Migration20140829142600ComCollections.php
@@ -20,6 +20,10 @@ class Migration20140829142600ComCollections extends Base
 			$query = "UPDATE `#__collections` SET `created_by`=`object_id` WHERE `object_type`='member' AND `created_by`=0";
 			$this->db->setQuery($query);
 			$this->db->query();
+
+			$query = "UPDATE `#__collections_posts` AS p LEFT JOIN `#__collections` AS c ON p.`collection_id`=c.id SET p.`created_by`=c.`created_by` WHERE p.`created_by`=0 AND c.`is_default`=1";
+			$this->db->setQuery($query);
+			$this->db->query();
 		}
 	}
 }
