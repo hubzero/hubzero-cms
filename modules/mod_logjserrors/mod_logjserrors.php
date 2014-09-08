f5e9103d15412e178ed1361191df0a16a747aeb0
Shawn Rice <zooley@purdue.edu>
2014-09-08 09:19:30 -0400

    [MOD_LOGJSERRORS] Reformatted. Modified paths to handle non-standard installs better (ie, sub directories of webroot). Make sure log path exists before writing to it. Updated manifest.

--- modules/mod_logjserrors/mod_logjserrors.php
+++ modules/mod_logjserrors/mod_logjserrors.php
@@ -1,15 +1,56 @@
 <?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2014 Purdue University. All rights reserved.
+ * All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Steve Snyder
+ * @copyright Copyright 2005-2014 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	// Write to hubzero logs if available.
+	// Otherwise fallback to the Joomla logs.
+	$path = JFactory::getConfig()->getValue('config.log_path');
+	if (is_dir('/var/log/hubzero'))
+	{
+		$path = '/var/log/hubzero';
+	}
+
 	$log = array();
-	array_map(function($k) use(&$log) {
-		if (!array_key_exists($k, $_POST)) {
+	array_map(function($k) use(&$log)
+	{
+		if (!array_key_exists($k, $_POST))
+		{
 			header('HTTP/1.1 422 Unprocessable Entity');
 			exit();
 		}
 		$log[$k] = $_POST[$k];
 	}, array('message', 'file', 'line', 'url', 'navigator'));
-	$fh = fopen('/var/log/hubzero/client_error.log', 'a');
+
+	$fh = fopen($path . '/client_error.log', 'a');
 	fwrite($fh, print_r($log, 1));
 	fclose($fh);
 	exit();
@@ -18,11 +59,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <script type="text/javascript">
 jQuery(function($) {
 	var handlingError = false;
+
 	window.onerror = function(msg, file, line) {
 		if (handlingError) {
 			return;
 		}
 		handlingError = true;
+
 		try {
 			msg = JSON.stringify(msg);
 		}
@@ -30,7 +73,7 @@ jQuery(function($) {
 			// probably threw a recursive structure. oh well.
 		}
 
-		$.post('/modules/mod_logjserrors/mod_logjserrors.php', {
+		$.post('<?php echo rtrim(JURI::base(true), "/"); ?>/modules/mod_logjserrors/mod_logjserrors.php', {
 			'message': msg,
 			'file': file,
 			'line': line,
