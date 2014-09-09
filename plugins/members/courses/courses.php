38957d2fce9767d70bc378cb9f5497977820584d
Shawn Rice <zooley@purdue.edu>
2014-09-09 09:40:28 -0400

    [PURDUENEXT][#668][PLG_MEMBERS_COURSES] Make sure all portions of a course (course, offering, section) are published to appear in list of coruses for student

--- plugins/members/courses/courses.php
+++ plugins/members/courses/courses.php
@@ -254,8 +254,8 @@ class plgMembersCourses extends \Hubzero\Plugin\Plugin
 							LEFT JOIN `#__courses_offerings` AS o ON o.id=m.offering_id
 							LEFT JOIN `#__courses_offering_sections` AS s on s.id=m.section_id
 							LEFT JOIN `#__courses_roles` AS r ON r.id=m.role_id
-						WHERE c.state IN (1, 3) AND m.user_id=" . (int) $this->member->get('uidNumber') . " AND m.student=1
-							AND (s.publish_down='0000-00-00 00:00:00' OR s.publish_down < " . $this->database->quote($now) .")");
+						WHERE c.state=1 AND m.user_id=" . (int) $this->member->get('uidNumber') . " AND m.student=1
+							AND (s.publish_down='0000-00-00 00:00:00' OR s.publish_down < " . $this->database->quote($now) .") AND o.state=1");
 					$results = $this->database->loadResult();
 				}
 				else
@@ -267,8 +267,8 @@ class plgMembersCourses extends \Hubzero\Plugin\Plugin
 							LEFT JOIN `#__courses_offerings` AS o ON o.id=m.offering_id
 							LEFT JOIN `#__courses_offering_sections` AS s on s.id=m.section_id
 							LEFT JOIN `#__courses_roles` AS r ON r.id=m.role_id
-						WHERE c.state IN (1, 3) AND m.user_id=" . (int) $this->member->get('uidNumber') . " AND m.student=1
-							AND (s.publish_down='0000-00-00 00:00:00' OR s.publish_down < " . $this->database->quote($now) .") AND o.state!=2
+						WHERE c.state=1 AND m.user_id=" . (int) $this->member->get('uidNumber') . " AND m.student=1
+							AND (s.publish_down='0000-00-00 00:00:00' OR s.publish_down < " . $this->database->quote($now) .") AND o.state=1
 						ORDER BY " . $filters['sort'] . " ASC LIMIT " . $filters['start'] . "," . $filters['limit']);
 					$results = $this->database->loadObjectList();
 				}
