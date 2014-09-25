35d1c72f761f0e96a6eabac7937a219071029386
Shawn Rice <zooley@purdue.edu>
2014-09-25 13:49:46 -0400

    [PLG_TOOLS_JAVA] Adding in missing param for host.

--- plugins/tools/java/views/session/tmpl/default.php
+++ plugins/tools/java/views/session/tmpl/default.php
@@ -37,10 +37,13 @@ $this->js('jquery.editable.min.js', 'system')
 $base = rtrim(JURI::base(true), '/');
 
 ?>
-<p id="troubleshoot" class="help">If your application fails to appear within a minute, <a target="_blank" href="http://www.java.com/en/download/testjava.jsp">troubleshoot this problem.</a></p>
+<p id="troubleshoot" class="help">If your application fails to appear within a minute, <a target="_blank" rel="external" href="http://www.java.com/en/download/testjava.jsp">troubleshoot this problem</a>.</p>
 
 <applet id="<?php echo $this->output->id ?>" class="<?php echo $this->output->class; ?>" code="<?php echo $this->output->code; ?>" archive="<?php echo $this->output->archive; ?>" width="<?php echo $this->output->width; ?>" height="<?php echo $this->output->height; ?>" MAYSCRIPT>
 	<param name="name" value="<?php echo $this->output->name; ?>" />
+	<?php if (!empty($this->output->host)) { ?>
+		<param name="HOST" value="<?php echo $this->output->host; ?>" />
+	<?php } ?>
 	<param name="PORT" value="<?php echo $this->output->port; ?>" />
 	<param name="ENCPASSWORD" value="<?php echo $this->output->encpassword; ?>" />
 	<param name="CONNECT" value="<?php echo $this->output->connect; ?>" />
