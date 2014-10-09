e5b4e902838e8b55ae10c4bfba794730e3930760
Sam Wilson <samwilson@purdue.edu>
2014-10-09 17:00:15 -0400

    [MIGRATIONS] fix up migration to use installModule method

--- migrations/Migration20141009072712ModCollect.php
+++ migrations/Migration20141009072712ModCollect.php
@@ -47,23 +47,7 @@ class Migration20141009072712ModCollect extends Base
 
 			if ($found)
 			{
-				$query  = "SELECT `ordering` FROM `#__modules` WHERE `position`='{$position}' ORDER BY `ordering` DESC LIMIT 1";
-				$this->db->setQuery($query);
-				$ordering = intval($this->db->loadResult());
-				$ordering++;
-
-				$query  = "INSERT INTO `#__modules` (`title`, `note`, `content`, `ordering`, `position`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, `published`, `module`, `access`, `showtitle`, `params`, `client_id`, `language`)";
-				$query .= " VALUES ('Collect', '', '', '{$ordering}', '{$position}', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'mod_collect', 1, 0, '', 0, '*')";
-				$this->db->setQuery($query);
-				$this->db->query();
-
-				$query  = "SELECT id FROM `#__modules` WHERE `module`='mod_collect' LIMIT 1";
-				$this->db->setQuery($query);
-				$id = $this->db->loadResult();
-
-				$query  = "INSERT INTO `#__modules_menu` (`moduleid`, `menuid`) VALUES ({$id}, 0)";
-				$this->db->setQuery($query);
-				$this->db->query();
+				$this->installModule('collect', $position);
 			}
 		}
 	}
