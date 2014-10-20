5a49cf86faae83a808b1faa4185d6fe251be1d88
Shawn Rice <zooley@purdue.edu>
2014-10-20 08:06:32 -0400

    [NANOHUB][#269902][COM_TOOLS] Fixing sorting controls for tools pipeline admin interface

--- administrator/components/com_tools/views/pipeline/tmpl/display.php
+++ administrator/components/com_tools/views/pipeline/tmpl/display.php
@@ -151,6 +151,8 @@ for ($i=0, $n=count($this->rows); $i < $n; $i++)
 	<input type="hidden" name="controller" value="<?php echo $this->controller; ?>" />
 	<input type="hidden" name="task" value="view" />
 	<input type="hidden" name="boxchecked" value="0" />
+	<input type="hidden" name="filter_order" value="<?php echo $this->filters['sort']; ?>" />
+	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->filters['sort_Dir']; ?>" />
 
 	<?php echo JHTML::_('form.token'); ?>
 </form>
