7e210223425ee98690aced5bf09348f59f98e2a4
Shawn Rice <zooley@purdue.edu>
2014-10-22 16:43:23 -0400

    [MOD_RAPID_CONTACT] Updating HTML and styles to be  alittle more flexible. Removed unused J1.5 params.

--- modules/mod_rapid_contact/tmpl/default.php
+++ modules/mod_rapid_contact/tmpl/default.php
@@ -33,10 +33,11 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 $this->css();
 ?>
+<div class="<?php echo $this->module->module; ?>">
 <?php if ($this->recipient === '') { ?>
 	<p class="error"><?php echo JText::_('MOD_RAPID_CONTACT_ERROR_NO_RECIPIENT'); ?></p>
 <?php } else { ?>
-	<form method="post" action="<?php echo $this->url; ?>" id="contactform" class="<?php echo $this->mod_class_suffix; ?>">
+	<form method="post" action="<?php echo $this->url; ?>" id="<?php echo $this->module->module; ?>-form-<?php echo $this->module->id; ?>" class="<?php echo $this->mod_class_suffix; ?>">
 		<fieldset>
 			<legend><?php echo JText::_('MOD_RAPID_CONTACT_FORM'); ?></legend>
 
@@ -50,44 +51,57 @@ $this->css();
 				<p class="error"><?php echo $this->error; ?></p>
 			<?php } ?>
 
-			<label for="contact-name">
-				<?php echo $this->name_label; ?>
-			</label>
-			<span class="input">
-				<input type="text" id="contact-name" name="rp[name]" value="<?php echo $this->escape($this->posted['name']); ?>" />
-			</span>
-
-			<label for="contact-email">
-				<?php echo $this->email_label; ?>
-			</label>
-			<span class="input">
-				<input type="text" id="contact-email" name="rp[email]" value="<?php echo $this->escape($this->posted['email']); ?>" />
-			</span>
-
-			<label for="contact-subject">
-				<?php echo $this->subject_label; ?>
-			</label>
-			<span class="input">
-				<input type="text" id="contact-subject" name="rp[subject]" value="<?php echo $this->escape($this->posted['subject']); ?>" />
-			</span>
-
-			<label for="contact-comments">
-				<?php echo $this->message_label; ?>
-			</label>
-			<span class="input">
-				<textarea name="rp[message]" id="contact-comments" cols="35" rows="10"><?php echo $this->escape($this->posted['message']); ?></textarea>
-			</span>
-
-			<?php if ($this->enable_anti_spam) { ?>
-				<label for="contact-antispam">
-					<?php echo $this->anti_spam_q; ?>
+			<div class="input-wrap">
+				<label for="contact-name<?php echo $this->module->id; ?>">
+					<?php echo $this->name_label; ?>
 				</label>
 				<span class="input">
-					<input type="text" id="contact-antispam" name="rp[anti_spam_answer]" />
+					<input type="text" id="contact-name<?php echo $this->module->id; ?>" name="rp[name]" value="<?php echo $this->escape($this->posted['name']); ?>" />
 				</span>
+			</div>
+
+			<div class="input-wrap">
+				<label for="contact-email<?php echo $this->module->id; ?>">
+					<?php echo $this->email_label; ?> <span class="required"><?php echo JText::_('JREQUIRED'); ?></span>
+				</label>
+				<span class="input">
+					<input type="text" id="contact-email<?php echo $this->module->id; ?>" name="rp[email]" value="<?php echo $this->escape($this->posted['email']); ?>" />
+				</span>
+			</div>
+
+			<div class="input-wrap">
+				<label for="contact-subject<?php echo $this->module->id; ?>">
+					<?php echo $this->subject_label; ?>
+				</label>
+				<span class="input">
+					<input type="text" id="contact-subject<?php echo $this->module->id; ?>" name="rp[subject]" value="<?php echo $this->escape($this->posted['subject']); ?>" />
+				</span>
+			</div>
+
+			<div class="input-wrap">
+				<label for="contact-comments<?php echo $this->module->id; ?>">
+					<?php echo $this->message_label; ?>
+				</label>
+				<span class="input">
+					<textarea name="rp[message]" id="contact-comments<?php echo $this->module->id; ?>" cols="35" rows="10"><?php echo $this->escape($this->posted['message']); ?></textarea>
+				</span>
+			</div>
+
+			<?php if ($this->enable_anti_spam) { ?>
+				<div class="input-wrap">
+					<label for="contact-antispam<?php echo $this->module->id; ?>">
+						<?php echo $this->anti_spam_q; ?> <span class="required"><?php echo JText::_('JREQUIRED'); ?></span>
+					</label>
+					<span class="input">
+						<input type="text" id="contact-antispam<?php echo $this->module->id; ?>" name="rp[anti_spam_answer]" />
+					</span>
+				</div>
 			<?php } ?>
 
-			<span class="submit"><input type="submit" name="submit" value="<?php echo $this->button_text; ?>" /></span>
+			<span class="submit">
+				<input type="submit" class="btn" value="<?php echo $this->button_text; ?>" />
+			</span>
 		</fieldset>
 	</form>
 <?php } ?>
+</div>
