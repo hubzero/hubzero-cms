aef8c832127a2839db0e053c6c39c9a0489af720
Alissa Nedossekina <alisa@purdue.edu>
2014-11-10 11:52:27 -0500

    [PLG_PROJECTS_PUBLICATIONS] show full license text in curation view

--- plugins/projects/publications/views/freeze/tmpl/license.php
+++ plugins/projects/publications/views/freeze/tmpl/license.php
@@ -70,6 +70,9 @@ echo $complete ? ' el-complete' : ' el-incomplete'; ?> freezeblock">
 			<?php if ($this->license) { echo '<img src="' . $this->license->icon . '" alt="' . htmlentities($this->license->title) . '" />'; } ?><?php echo $this->license->title; ?>
 					<span class="item-details"><?php echo $info; ?></span>
 				</p>
+				<?php if ($this->license->text) { ?>
+				<pre><?php echo $this->license->text; ?></pre>	
+				<?php } ?>
 			</div>
 
 		<?php } else { ?>
