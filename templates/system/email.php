084ef37c52c1126e47405b3cf7960bd618567660
Shawn Rice <zooley@purdue.edu>
2014-11-20 10:23:20 -0500

    [TPL_SYSTEM] Moved some inline styles to style element for easier maintainability

--- templates/system/email.php
+++ templates/system/email.php
@@ -38,19 +38,57 @@ if ($title = $this->getTitle())
 	$this->setTitle($config->getValue('config.sitename') . ' - ' . $title);
 }
 $this->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
+
+$bgColor  = '#ffffff';
+$tblColor = '#ffffff';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" style="background-color: #fff; margin: 0; padding: 0;">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
 		<style type="text/css">
 		<?php /* Client-specific Styles */ ?>
-		body { width: 100% !important; font-family: 'Helvetica Neue', Helvetica, Verdana, Arial, sans-serif !important; background-color: #ffffff !important; margin: 0 !important; padding: 0 !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; }
+		html {
+			background-color: <?php echo $bgColor; ?>;
+			margin: 0;
+			padding: 0;
+			height: 100%;
+		}
+		body {
+			width: 100% !important;
+			font-family: 'Helvetica Neue', Helvetica, Verdana, Arial, sans-serif !important;
+			background-color: <?php echo $bgColor; ?> !important;
+			margin: 0 !important;
+			padding: 0 !important;
+			-webkit-text-size-adjust: 100%;
+			-ms-text-size-adjust: 100%;
+			font-size: 12px;
+			line-height: 1.4em;
+			color: #666;
+			text-rendering: optimizeLegibility;
+			height: 100%;
+		}
 		<?php /* Prevent Webkit and Windows Mobile platforms from changing default font sizes, while not breaking desktop design. */ ?>
 		<?php /* Force Hotmail to display emails at full width */ ?>
-		.ExternalClass { width:100%; }
+		.ExternalClass {
+			width: 100%;
+		}
 		<?php /* Force Hotmail to display normal line spacing.  More on that: http://www.emailonacid.com/forum/viewthread/43/ */?>
-		.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div { line-height: 100%; }
-		#backgroundTable { margin:0; padding:0; width:100% !important; line-height: 100% !important; }
+		.ExternalClass,
+		.ExternalClass p,
+		.ExternalClass span,
+		.ExternalClass font,
+		.ExternalClass td,
+		.ExternalClass div {
+			line-height: 100%;
+		}
+		#backgroundTable {
+			margin: 0;
+			padding: 0;
+			width: 100% !important;
+			min-width: 100%;
+			line-height: 100% !important;
+			background-color: <?php echo $bgColor; ?>;
+		}
 		<?php /* End reset */ ?>
 
 		<?php
@@ -62,18 +100,95 @@ $this->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
 		Bring inline: Yes.
 		*/
 		?>
-		img { outline: none !important; text-decoration: none !important; -ms-interpolation-mode: bicubic; }
-		a img { border: none; }
-		.image_fix { display: block !important; }
+		img {
+			outline: none !important;
+			text-decoration: none !important;
+			-ms-interpolation-mode: bicubic;
+		}
+		a img {
+			border: none;
+		}
+		.image_fix {
+			display: block !important;
+		}
 
 		<?php /* Yahoo paragraph fix: removes the proper spacing or the paragraph (p) tag. To correct we set the top/bottom margin to 1em in the head of the document. */ ?>
-		p { margin: 1em 0; }
-
-		<?php /* Outlook 07, 10 Padding issue */ ?>
-		table td { border-collapse: collapse; }
+		p {
+			margin: 1em 0;
+		}
 
 		<?php /* Remove spacing around Outlook 07, 10 tables */ ?>
-		table { border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
+		table {
+			border-collapse: collapse;
+			mso-table-lspace: 0pt;
+			mso-table-rspace: 0pt;
+		}
+		table tr {
+			border-collapse: collapse;
+		}
+		<?php /* Outlook 07, 10 Padding issue */ ?>
+		table td {
+			border-collapse: collapse;
+		}
+		table.tbl-wrap {
+			background-color: <?php echo $tblColor; ?>;
+		}
+		table.tbl-spacer td div {
+			color: <?php echo $tblColor; ?> !important;
+			height: 30px !important;
+			visibility: hidden;
+		}
+		table.tbl-header {
+			border-bottom: 2px solid #e1e1e1;
+		}
+		table.tbl-header td.sitename {
+			font-size: 1.4em;
+			color: #999;
+			padding: 0 10px 5px 0;
+			text-align: left;
+		}
+		table.tbl-header td.tagline {
+			line-height: 1;
+			padding: 0 0 5px 10px;
+		}
+		table.tbl-header span.home {
+			font-weight: bold;
+			font-size: 0.85em;
+			color: #666;
+			-webkit-text-size-adjust: none;
+		}
+		table.tbl-header span.home a {
+			color: #666;
+			font-weight: bold;
+			text-decoration: none;
+			border: none;
+		}
+		table.tbl-header span.description {
+			font-size: 0.85em;
+			color: #666;
+			-webkit-text-size-adjust: none;
+		}
+		table.tbl-header td.component {
+			border-left: 1px solid #e1e1e1;
+			font-size: 1.2em;
+			color: #999;
+			padding: 0 0 5px 10px;
+			text-align: right;
+			vertical-align: bottom;
+		}
+		table.tbl-footer {
+			border-collapse: collapse;
+			border-top: 2px solid #e1e1e1;
+		}
+		table.tbl-footer td {
+			line-height: 1;
+			padding: 5px 0 0 0;
+		}
+		table.tbl-footer td span {
+			font-size: 0.85em;
+			color: #666;
+			-webkit-text-size-adjust: none;
+		}
 
 		@media only screen and (max-device-width: 480px) {
 			body {
@@ -93,6 +208,7 @@ $this->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
 				display: none !important;
 			}
 		}
+
 		</style>
 
 		<?php /* Targeting Windows Mobile
@@ -115,25 +231,27 @@ $this->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
 
 		<jdoc:include type="head" />
 	</head>
-	<body style="width: 100% !important; font-family: 'Helvetica Neue', Helvetica, Verdana, Arial, sans-serif; font-size: 12px; -webkit-text-size-adjust: none; color: #616161; line-height: 1.4em; color: #666; background: #fff; text-rendering: optimizeLegibility;" bgcolor="#ffffff">
+	<body bgcolor="<?php echo $bgColor; ?>">
 		<!-- Start Body Wrapper Table -->
-		<table width="100%" cellpadding="0" cellspacing="0" border="0"  id="backgroundTable" style="background-color: #ffffff; min-width: 100%;" bgcolor="#ffffff">
+		<table width="100%" cellpadding="0" cellspacing="0" border="0" id="backgroundTable" bgcolor="<?php echo $bgColor; ?>">
 			<tbody>
-				<tr style="border-collapse: collapse;">
-					<td bgcolor="#ffffff" align="center" style="border-collapse: collapse;">
+				<tr>
+					<td bgcolor="<?php echo $bgColor; ?>" align="center">
 
 						<!-- Start Content Wrapper Table -->
-						<table class="tbl-wrap" width="670" cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse;">
+						<table class="tbl-wrap" width="670" cellpadding="0" cellspacing="0" border="0">
 							<tbody>
-								<tr style="border-collapse: collapse;">
-									<td bgcolor="#ffffff" width="10" style="border-collapse: collapse;"></td>
-									<td class="tbl-body" bgcolor="#ffffff" width="650" align="left" style="border-collapse: collapse;">
+								<tr>
+									<td bgcolor="<?php echo $tblColor; ?>" width="10"></td>
+									<td class="tbl-body" bgcolor="<?php echo $tblColor; ?>" width="650" align="left">
 
 										<!-- Start Header Spacer -->
-										<table  width="100%" cellpadding="0" cellspacing="0" border="0">
-											<tr style="border-collapse: collapse;">
-												<td height="30" style="border-collapse: collapse;"></td>
-											</tr>
+										<table class="tbl-spacer" width="100%" cellpadding="0" cellspacing="0" border="0">
+											<tbody>
+												<tr>
+													<td height="30"></td>
+												</tr>
+											</tbody>
 										</table>
 										<!-- End Header Spacer -->
 
@@ -142,17 +260,17 @@ $this->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
 										<!-- End content -->
 
 										<!-- Start Footer Spacer -->
-										<table width="100%" cellpadding="0" cellspacing="0" border="0">
+										<table class="tbl-spacer" width="100%" cellpadding="0" cellspacing="0" border="0">
 											<tbody>
-												<tr style="border-collapse: collapse;">
-													<td height="30" style="border-collapse: collapse; color: #fff !important;"><div style="height: 30px !important; visibility: hidden;">----</div></td>
+												<tr>
+													<td height="30"><div>----</div></td>
 												</tr>
 											</tbody>
 										</table>
 										<!-- End Footer Spacer -->
 
 									</td>
-									<td bgcolor="#ffffff" width="10" style="border-collapse: collapse;"></td>
+									<td bgcolor="<?php echo $tblColor; ?>" width="10"></td>
 								</tr>
 							</tbody>
 						</table>
