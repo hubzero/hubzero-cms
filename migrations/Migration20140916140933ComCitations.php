32f97fc7aa56a9b97ca7d84f16847499a12342f3
Kevin Wojkovich <kevinw@purdue.edu>
2014-11-24 09:04:03 -0500

    [PLG_GROUPS_CITATIONS] Cleaning up lint errors.

--- migrations/Migration20140916140933ComCitations.php
+++ migrations/Migration20140916140933ComCitations.php
@@ -17,21 +17,21 @@ class Migration20140916140933ComCitations extends Base
 	{
 		//checks whether table exists and if the 'scope' field already exists
 		if ($this->db->tableExists('#__citations')
-				&& !$this->db->tableHasField('#__citations', 'scope'))
+			&& !$this->db->tableHasField('#__citations', 'scope'))
 		{
 			$query = "ALTER TABLE `#__citations` ADD COLUMN `scope` VARCHAR(45) NULL DEFAULT NULL AFTER `gid`;";
 			$this->db->setQuery($query);
 			$this->db->query();
 		}
 	}
-	
+
 	/**
 	 * Down
 	 **/
 	public function down()
 	{
 		//checks to see if gid field exists and removes it
-		if ($this->db->tableExists('#__citations') 
+		if ($this->db->tableExists('#__citations')
 				&& $this->db->tableHasField('#__citations', 'scope'))
 		{
 			$query = "ALTER TABLE `#__citations` DROP COLUMN `scope`;";
