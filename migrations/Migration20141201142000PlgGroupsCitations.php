e3b16cbe9d6db5adcb3f6507fdac1de38e3ff902
Kevin Wojkovich <kevinw@purdue.edu>
2014-12-01 14:26:36 -0500

    [PLG_GROUPS_CITATIONS] Registers the plugin with the CMS.

---
+++ migrations/Migration20141201142000PlgGroupsCitations.php
@@ -0,0 +1,28 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for csv time plugin
+ **/
+class Migration201412011142000PlgGroupsCitations extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->addPluginEntry('groups', 'citations', 0);
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->deletePluginEntry('groups', 'citations');
+	}
+}
