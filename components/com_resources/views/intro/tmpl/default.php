5167f7db2a7af379c026ed4cc69469bbfa1fb7c9
Alissa Nedossekina <alisa@purdue.edu>
2014-12-03 13:44:22 -0500

    [#270054][NANOHUB][COM_RESOURCES][1.3.0] proper display of resource type description text

--- components/com_resources/views/intro/tmpl/default.php
+++ components/com_resources/views/intro/tmpl/default.php
@@ -137,6 +137,7 @@ if ($this->categories) {
 				{
 					$cls = rtrim($category->alias, 's');
 				}
+
 				?>
 				<div class="col span-third <?php echo $clm; ?>">
 					<div class="resource-type <?php echo $cls; ?>">
@@ -146,7 +147,7 @@ if ($this->categories) {
 							</a>
 						</h3>
 						<p>
-							<?php echo $this->escape(strip_tags(stripslashes($category->description))); ?>
+							<?php echo html_entity_decode(strip_tags(stripslashes($this->escape($category->description)))); ?>
 						</p>
 						<p>
 							<a class="read-more" href="<?php echo JRoute::_('index.php?option=' . $this->option . '&type=' . $category->alias); ?>" title="<?php echo JText::sprintf('Browse %s', $this->escape(stripslashes($category->type))); ?>">
