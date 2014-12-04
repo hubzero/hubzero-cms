d88de66d9e3fcae0d5d55c3fde16cf5499c84251
Shawn Rice <zooley@purdue.edu>
2014-12-04 11:15:55 -0500

    [HUBZERO][#6798][COM_TEMPLATES] Adjusting styles and layout. Allow CSS editor to list sub directories and files

--- administrator/components/com_templates/views/template/tmpl/default.php
+++ administrator/components/com_templates/views/template/tmpl/default.php
@@ -13,90 +13,93 @@ defined('_JEXEC') or die;
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.modal');
-$canDo	= TemplatesHelper::getActions();
+$canDo = TemplatesHelper::getActions();
+
+\Hubzero\Document\Assets::addComponentStylesheet('com_templates');
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_templates&view=templates'); ?>" method="post" name="adminForm" id="adminForm">
+<div id="item-form">
 	<div class="width-50 fltlft">
-		<fieldset class="adminform" id="template-manager">
-			<legend><?php echo JText::_('COM_TEMPLATES_TEMPLATE_DESCRIPTION');?></legend>
+		<form action="<?php echo JRoute::_('index.php?option=com_templates&view=templates'); ?>" method="post" name="adminForm" id="adminForm">
+			<fieldset class="adminform" id="template-manager-description">
+				<legend><?php echo JText::_('COM_TEMPLATES_TEMPLATE_DESCRIPTION');?></legend>
 
-			<?php echo JHtml::_('templates.thumb', $this->template->element, $this->template->client_id); ?>
+				<div class="input-wrap">
+					<?php echo JHtml::_('templates.thumb', $this->template->element, $this->template->client_id); ?>
 
+					<h2><?php echo ucfirst($this->template->element); ?></h2>
+					<?php $client = JApplicationHelper::getClientInfo($this->template->client_id); ?>
+					<p><?php $this->template->xmldata = TemplatesHelper::parseXMLTemplateFile($client->path, $this->template->element);?></p>
+					<p><?php  echo JText::_($this->template->xmldata->description); ?></p>
+				</div>
+			</fieldset>
+			<fieldset class="adminform" id="template-manager-files">
+				<legend><?php echo JText::_('COM_TEMPLATES_TEMPLATE_MASTER_FILES');?></legend>
 
-			<h2><?php echo ucfirst($this->template->element); ?></h2>
-			<?php $client = JApplicationHelper::getClientInfo($this->template->client_id); ?>
-			<p><?php $this->template->xmldata = TemplatesHelper::parseXMLTemplateFile($client->path, $this->template->element);?></p>
-			<p><?php  echo JText::_($this->template->xmldata->description); ?></p>
-		</fieldset>
-		<fieldset class="adminform" id="template-manager">
-			<legend><?php echo JText::_('COM_TEMPLATES_TEMPLATE_MASTER_FILES');?></legend>
-
-			<ul>
-				<li>
-					<?php $id = $this->files['main']['index']->id; ?>
-					<?php if ($canDo->get('core.edit')) : ?>
-					<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id='.$id);?>">
-					<?php endif; ?>
-						<?php echo JText::_('COM_TEMPLATES_TEMPLATE_EDIT_MAIN');?>
-					<?php if ($canDo->get('core.edit')) : ?>
-						</a>
-					<?php endif; ?>
-				</li>
-				<?php if ($this->files['main']['error']->exists) : ?>
-				<li>
-					<?php $id = $this->files['main']['error']->id; ?>
-					<?php if ($canDo->get('core.edit')) : ?>
-						<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id='.$id);?>">
-					<?php endif; ?>
-						<?php echo JText::_('COM_TEMPLATES_TEMPLATE_EDIT_ERROR');?>
-					<?php if ($canDo->get('core.edit')) : ?>
-						</a>
-					<?php endif; ?>
-				</li>
-				<?php endif; ?>
-				<?php if ($this->files['main']['offline']->exists) :  ;?>
+				<ul class="item-list layout">
 					<li>
-						<?php $id = $this->files['main']['offline']->id; ?>
+						<?php $id = $this->files['main']['index']->id; ?>
 						<?php if ($canDo->get('core.edit')) : ?>
-							<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id='.$id);?>">
+						<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id=' . $id); ?>">
 						<?php endif; ?>
-						<?php echo JText::_('COM_TEMPLATES_TEMPLATE_EDIT_OFFLINEVIEW');?>
+							<?php echo JText::_('COM_TEMPLATES_TEMPLATE_EDIT_MAIN');?>
 						<?php if ($canDo->get('core.edit')) : ?>
 							</a>
 						<?php endif; ?>
 					</li>
-				<?php endif; ?>
-				<?php if ($this->files['main']['print']->exists) : ?>
-				<li>
-					<?php $id = $this->files['main']['print']->id; ?>
-					<?php if ($canDo->get('core.edit')) : ?>
-						<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id='.$id);?>">
+					<?php if ($this->files['main']['error']->exists) : ?>
+					<li>
+						<?php $id = $this->files['main']['error']->id; ?>
+						<?php if ($canDo->get('core.edit')) : ?>
+							<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id=' . $id); ?>">
+						<?php endif; ?>
+							<?php echo JText::_('COM_TEMPLATES_TEMPLATE_EDIT_ERROR');?>
+						<?php if ($canDo->get('core.edit')) : ?>
+							</a>
+						<?php endif; ?>
+					</li>
 					<?php endif; ?>
-						<?php echo JText::_('COM_TEMPLATES_TEMPLATE_EDIT_PRINTVIEW');?>
-					<?php if ($canDo->get('core.edit')) : ?>
-						</a>
+					<?php if ($this->files['main']['offline']->exists) :  ;?>
+						<li>
+							<?php $id = $this->files['main']['offline']->id; ?>
+							<?php if ($canDo->get('core.edit')) : ?>
+								<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id=' . $id); ?>">
+							<?php endif; ?>
+								<?php echo JText::_('COM_TEMPLATES_TEMPLATE_EDIT_OFFLINEVIEW');?>
+							<?php if ($canDo->get('core.edit')) : ?>
+								</a>
+							<?php endif; ?>
+						</li>
 					<?php endif; ?>
-				</li>
-				<?php endif; ?>
-			</ul>
-		</fieldset>
-
+					<?php if ($this->files['main']['print']->exists) : ?>
+					<li>
+						<?php $id = $this->files['main']['print']->id; ?>
+						<?php if ($canDo->get('core.edit')) : ?>
+							<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id=' . $id); ?>">
+						<?php endif; ?>
+							<?php echo JText::_('COM_TEMPLATES_TEMPLATE_EDIT_PRINTVIEW');?>
+						<?php if ($canDo->get('core.edit')) : ?>
+							</a>
+						<?php endif; ?>
+					</li>
+					<?php endif; ?>
+				</ul>
+			</fieldset>
+			<input type="hidden" name="task" value="" />
+		</form>
 		<div class="clr"></div>
 	</div>
 
 	<div class="width-50 fltrt">
-
 		<fieldset class="adminform" id="template-manager-css">
 			<legend><?php echo JText::_('COM_TEMPLATES_TEMPLATE_CSS');?></legend>
 
 			<?php if (!empty($this->files['css'])) : ?>
-			<ul>
+			<ul class="item-list css">
 				<?php foreach ($this->files['css'] as $file) : ?>
 				<li>
 					<?php if ($canDo->get('core.edit')) : ?>
-					<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id='.$file->id);?>">
+					<a href="<?php echo JRoute::_('index.php?option=com_templates&task=source.edit&id=' . $file->id); ?>">
 					<?php endif; ?>
-
 						<?php echo JText::sprintf('COM_TEMPLATES_TEMPLATE_EDIT_CSS', $file->name);?>
 					<?php if ($canDo->get('core.edit')) : ?>
 					</a>
@@ -110,21 +113,20 @@ $canDo	= TemplatesHelper::getActions();
 				<a href="#" class="modal">
 					<?php echo JText::sprintf('COM_TEMPLATES_TEMPLATE_ADD_CSS');?></a>
 			</div>-->
-
 		</fieldset>
-		<div class="clr"></div>
-		<input type="hidden" name="task" value="" />
+
+		<form action="<?php echo JRoute::_('index.php?option=com_templates&task=template.copy&id=' . JRequest::getInt('id')); ?>" method="post" name="copyForm">
+			<fieldset class="adminform" id="template-manager-css">
+				<legend><?php echo JText::_('COM_TEMPLATES_TEMPLATE_COPY');?></legend>
+				<div class="input-wrap">
+					<label id="new_name" class="hasTip" title="<?php echo JText::_('COM_TEMPLATES_TEMPLATE_NEW_NAME_DESC'); ?>"><?php echo JText::_('COM_TEMPLATES_TEMPLATE_NEW_NAME_LABEL')?></label>
+					<input class="inputbox" type="text" id="new_name" name="new_name"  />
+					<button type="submit"><?php echo JText::_('COM_TEMPLATES_TEMPLATE_COPY'); ?></button>
+				</div>
+			</fieldset>
+			<input type="hidden" name="task" value="" />
+			<?php echo JHtml::_('form.token'); ?>
+		</form>
 	</div>
-<div class="width-50 fltrt">
-</form>
-<form action="<?php echo JRoute::_('index.php?option=com_templates&task=template.copy&id=' . JRequest::getInt('id')); ?>"
-		method="post" name="adminForm" id="adminForm">
-	<fieldset class="adminform" id="template-manager-css">
-		<legend><?php echo JText::_('COM_TEMPLATES_TEMPLATE_COPY');?></legend>
-		<label id="new_name" class="hasTip"  title="<?php echo JText::_('COM_TEMPLATES_TEMPLATE_NEW_NAME_DESC'); ?>"><?php echo JText::_('COM_TEMPLATES_TEMPLATE_NEW_NAME_LABEL')?></label>
-		<input class="inputbox" type="text" id="new_name" name="new_name"  />
-		<button type="submit"><?php echo JText::_('COM_TEMPLATES_TEMPLATE_COPY'); ?></button>
-	</fieldset>
-	<?php echo JHtml::_('form.token'); ?>
-</form>
+	<div class="clr"></div>
 </div>
