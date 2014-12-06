c86edacc12e3f0ec2fbb534305f1bdc826db530e
Alissa Nedossekina <alisa@purdue.edu>
2014-12-06 10:08:29 -0500

    [HUBZERO][#6863][COM_WIKI] add hover text for wiki buttons

--- components/com_wiki/views/page/tmpl/submenu.php
+++ components/com_wiki/views/page/tmpl/submenu.php
@@ -50,7 +50,7 @@ if ($tmpl != 'component') { ?>
 			</li>
 		<?php } ?>
 			<li class="page-index" data-title="<?php echo JText::_('COM_WIKI_PAGE_INDEX'); ?>">
-				<a class="icon-index index btn" href="<?php echo JRoute::_($this->page->link('base') . '&pagename=Special:AllPages'); ?>">
+				<a class="icon-index index btn" href="<?php echo JRoute::_($this->page->link('base') . '&pagename=Special:AllPages'); ?>" title="<?php echo JText::_('COM_WIKI_INDEX'); ?>">
 					<span><?php echo JText::_('COM_WIKI_INDEX'); ?></span>
 				</a>
 			</li>
@@ -77,7 +77,7 @@ if ($tmpl != 'component') { ?>
 
 	<ul class="sub-menu">
 		<li class="page-text<?php if ($this->controller == 'page' && ($this->task == 'display' || !$this->task)) { echo ' active'; } ?>">
-			<a href="<?php echo JRoute::_($this->page->link()); ?>">
+			<a href="<?php echo JRoute::_($this->page->link()); ?>" title="<?php echo JText::_('COM_WIKI_TAB_ARTICLE'); ?>">
 				<span><?php echo JText::_('COM_WIKI_TAB_ARTICLE'); ?></span>
 			</a>
 		</li>
@@ -85,25 +85,25 @@ if ($tmpl != 'component') { ?>
 	<?php if ($this->page->exists() && !$this->page->isDeleted() && $this->page->get('namespace') != 'special') { ?>
 		<?php if (($this->page->isLocked() && $this->page->access('manage')) || (!$this->page->isLocked() && $this->page->access('edit'))) { ?>
 			<li class="page-edit<?php if ($this->controller == 'page' && $this->task == 'edit') { echo ' active'; } ?>">
-				<a href="<?php echo JRoute::_($this->page->link('edit')); ?>">
+				<a href="<?php echo JRoute::_($this->page->link('edit')); ?>" title="<?php echo JText::_('COM_WIKI_TAB_EDIT'); ?>">
 					<span><?php echo JText::_('COM_WIKI_TAB_EDIT'); ?></span>
 				</a>
 			</li>
 		<?php } ?>
 		<?php if ($this->page->access('view', 'comment')) { ?>
 			<li class="page-comments<?php if ($this->controller == 'comments') { echo ' active'; } ?>">
-				<a href="<?php echo JRoute::_($this->page->link('comments')); ?>">
+				<a href="<?php echo JRoute::_($this->page->link('comments')); ?>" title="<?php echo JText::_('COM_WIKI_TAB_COMMENTS'); ?>">
 					<span><?php echo JText::_('COM_WIKI_TAB_COMMENTS'); ?></span>
 				</a>
 			</li>
 		<?php } ?>
 			<li class="page-history<?php if ($this->controller == 'history') { echo ' active'; } ?>">
-				<a href="<?php echo JRoute::_($this->page->link('history')); ?>">
+				<a href="<?php echo JRoute::_($this->page->link('history')); ?>" title="<?php echo JText::_('COM_WIKI_TAB_HISTORY'); ?>">
 					<span><?php echo JText::_('COM_WIKI_TAB_HISTORY'); ?></span>
 				</a>
 			</li>
 			<li class="page-pdf">
-				<a href="<?php echo JRoute::_($this->page->link('pdf')); ?>">
+				<a href="<?php echo JRoute::_($this->page->link('pdf')); ?>" title="<?php echo JText::_('COM_WIKI_TAB_PDF'); ?>">
 					<span><?php echo JText::_('COM_WIKI_TAB_PDF'); ?></span>
 				</a>
 			</li>
@@ -111,7 +111,7 @@ if ($tmpl != 'component') { ?>
 			if (($this->page->isLocked() && $this->page->access('manage', 'page'))
 				|| (!$this->page->isLocked() && $this->page->access('delete', 'page'))) { ?>
 			<li class="page-delete<?php if ($this->controller == 'page' && $this->task == 'delete') { echo ' active'; } ?>">
-				<a href="<?php echo JRoute::_($this->page->link('delete')); ?>">
+				<a href="<?php echo JRoute::_($this->page->link('delete')); ?>" title="<?php echo JText::_('COM_WIKI_DELETE_PAGE'); ?>">
 					<span><?php echo JText::_('COM_WIKI_DELETE_PAGE'); ?></span>
 				</a>
 			</li>
