c98c69eea029328a8d6afc9579ace2f6e046767b
Shawn Rice <zooley@purdue.edu>
2014-12-09 11:09:00 -0500

    [HUBZERO][#6758][COM_SUPPORT] Don't log notifications to ticket watchers. Watching should be anonymous.

--- administrator/components/com_support/controllers/tickets.php
+++ administrator/components/com_support/controllers/tickets.php
@@ -778,6 +778,12 @@ class SupportControllerTickets extends \Hubzero\Component\AdminController
 					{
 						$this->setError(JText::sprintf('COM_SUPPORT_ERROR_FAILED_TO_MESSAGE', $to['name'] . '(' . $to['role'] . ')'));
 					}
+
+					// Watching should be anonymous
+					if ($to['role'] == 'watcher')
+					{
+						continue;
+					}
 					$rowc->changelog()->notified(
 						$to['role'],
 						$to['name'],
@@ -805,6 +811,11 @@ class SupportControllerTickets extends \Hubzero\Component\AdminController
 						SupportUtilities::sendEmail($to['email'], $subject, $message, $from);
 					}
 
+					// Watching should be anonymous
+					if ($to['role'] == 'watcher')
+					{
+						continue;
+					}
 					$rowc->changelog()->notified(
 						$to['role'],
 						$to['name'],
