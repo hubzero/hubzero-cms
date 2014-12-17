ad88b39273373c41a06baf4bcb5149a3068c8330
Shawn Rice <zooley@purdue.edu>
2014-12-17 07:18:09 -0500

    [HUBZERO][#6998][PLG_GROUPS_FORUM][MEDIA] Fixing issue with delete post link not showing and making links visible all the time

--- plugins/groups/forum/forum.php
+++ plugins/groups/forum/forum.php
@@ -1092,6 +1092,7 @@ class plgGroupsForum extends \Hubzero\Plugin\Plugin
 		// Get authorization
 		$this->_authorize('category', $this->view->category->get('id'));
 		$this->_authorize('thread', $this->view->thread->get('id'));
+		$this->_authorize('post');
 
 		// If the access is anything beyond public, 
 		// make sure they're logged in.
@@ -1518,13 +1519,13 @@ class plgGroupsForum extends \Hubzero\Plugin\Plugin
 		}
 
 		// Update replies if this is a parent (thread starter)
-		if (!$model->parent)
-		{
+		//if (!$model->parent)
+		//{
 			if (!$model->updateReplies(array('state' => 2), $model->id))  /* 0 = unpublished, 1 = published, 2 = deleted */
 			{
 				$this->setError($model->getError());
 			}
-		}
+		//}
 
 		// Delete the topic itself
 		$model->state = 2;  /* 0 = unpublished, 1 = published, 2 = deleted */
