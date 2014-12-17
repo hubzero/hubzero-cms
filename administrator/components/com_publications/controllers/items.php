ea22a09e3209976e6c98a62f9cae7805e49e54df
Alissa Nedossekina <alisa@purdue.edu>
2014-12-17 10:30:23 -0500

    [#7001][HUBZERO][COM_PUBLICATIONS] fix to php error when saving author order

--- administrator/components/com_publications/controllers/items.php
+++ administrator/components/com_publications/controllers/items.php
@@ -769,6 +769,7 @@ class PublicationsControllerItems extends \Hubzero\Component\AdminController
 			if ($row && $row->doi)
 			{
 				// Get updated authors
+				$pAuthor = new PublicationAuthor( $this->database );
 				$authors = $pAuthor->getAuthors($row->id);
 
 				// Collect DOI metadata
