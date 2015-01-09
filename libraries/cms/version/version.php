d4e1434ec39d3cef86d6a62b24d377ab18816456
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2015-01-09 10:50:50 -0500

    Joomla! 2.5.26

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -23,7 +23,7 @@ final class JVersion
 	public $RELEASE = '2.5';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '25';
+	public $DEV_LEVEL = '26';
 
 	/** @var  string  Development STATUS. */
 	public $DEV_STATUS = 'Stable';
