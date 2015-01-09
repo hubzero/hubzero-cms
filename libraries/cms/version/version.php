a328b696946a51a29b9094fcb2fa147174f1bd39
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2015-01-09 10:51:25 -0500

    Joomla! 2.5.27

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -23,7 +23,7 @@ final class JVersion
 	public $RELEASE = '2.5';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '26';
+	public $DEV_LEVEL = '27';
 
 	/** @var  string  Development STATUS. */
 	public $DEV_STATUS = 'Stable';
@@ -35,7 +35,7 @@ final class JVersion
 	public $CODENAME = 'Ember';
 
 	/** @var  string  Release date. */
-	public $RELDATE = '23-September-2014';
+	public $RELDATE = '30-September-2014';
 
 	/** @var  string  Release time. */
 	public $RELTIME = '14:00';
