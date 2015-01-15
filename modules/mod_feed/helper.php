ed37d61f3ffe94dff01b2cb89a2cd6f0c0d4ab78
Shawn Rice <zooley@purdue.edu>
2015-01-15 12:27:50 -0500

    [MOD_FEED] Initial rewrite to be a hubzero module.

--- modules/mod_feed/helper.php
+++ modules/mod_feed/helper.php
@@ -1,35 +1,66 @@
 <?php
 /**
- * @package		Joomla.Site
- * @subpackage	mod_feed
- * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 Purdue University. All rights reserved.
+ * All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
-// no direct access
-defined('_JEXEC') or die;
+namespace Modules\Feed;
 
-class modFeedHelper
+use Hubzero\Module\Module;
+use JModuleHelper;
+use JFactory;
+use stdClass;
+use JText;
+
+/**
+ * Module class for displaying items from a feed
+ */
+class Helper extends Module
 {
-	static function getFeed($params)
+	/**
+	 * Get contents of a feed
+	 *
+	 * @return  mixed
+	 */
+	public function getFeed()
 	{
+		$rssurl = $this->params->get('rssurl', '');
 
-
-		// module params
-		$rssurl	= $params->get('rssurl', '');
-
-		// get RSS parsed object
+		// Get RSS parsed object
 		$cache_time = 0;
-		if ($params->get('cache'))
+		if ($this->params->get('cache'))
 		{
-			/*
-			 * The cache_time will get fed into JCache to initiate the feed_parser cache group and eventually
-			 * JCacheStorage will multiply the value by 60 and use that for its lifetime. The only way to sync
-			 * the feed_parser cache (which caches with an empty dataset anyway) with the module cache is to
-			 * first divide the module's cache time by 60 then inject that forward, which once stored into the
-			 * JCacheStorage object, will be the correct value in minutes.
-			 */
-			$cache_time  = $params->get('cache_time', 15) / 60;
+			// The cache_time will get fed into JCache to initiate the feed_parser cache group and eventually
+			// JCacheStorage will multiply the value by 60 and use that for its lifetime. The only way to sync
+			// the feed_parser cache (which caches with an empty dataset anyway) with the module cache is to
+			// first divide the module's cache time by 60 then inject that forward, which once stored into the
+			// JCacheStorage object, will be the correct value in minutes.
+			$cache_time = $this->params->get('cache_time', 15) / 60;
 		}
 
 		$rssDoc = JFactory::getFeedParser($rssurl, $cache_time);
@@ -38,25 +69,56 @@ class modFeedHelper
 
 		if ($rssDoc != false)
 		{
-			// channel header and link
-			$feed->title = $rssDoc->get_title();
-			$feed->link = $rssDoc->get_link();
-			$feed->description = $rssDoc->get_description();
+			// Channel header and link
+			$feed->title        = $rssDoc->get_title();
+			$feed->link         = $rssDoc->get_link();
+			$feed->description  = $rssDoc->get_description();
 
-			// channel image if exists
-			$feed->image        = new stdClass;
+			// Channel image if exists
+			$feed->image = new stdClass;
 			$feed->image->url   = $rssDoc->get_image_url();
 			$feed->image->title = $rssDoc->get_image_title();
 
-			// items
+			// Items
 			$items = $rssDoc->get_items();
 
-			// feed elements
+			// Feed elements
 			$feed->items = array_slice($items, 0, $params->get('rssitems', 5));
-		} else {
+		}
+		else
+		{
 			$feed = false;
 		}
 
 		return $feed;
+	}
+
+	/**
+	 * Display module
+	 *
+	 * @return  void
+	 */
+	public function display()
+	{
+		// Legacy compatibility for older view overrides
+		$params = $this->params;
+		$module = $this->module;
+
+		$rssurl = $params->get('rssurl', '');
+		$rssrtl = $params->get('rssrtl', 0);
+
+		// Check if feed URL has been set
+		if (empty($rssurl))
+		{
+			echo '<p class="warning">';
+			echo JText::_('MOD_FEED_ERR_NO_URL');
+			echo '</p>';
+			return;
+		}
+
+		$feed = $this->getFeed();
+		$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+		require JModuleHelper::getLayoutPath($module->module, $params->get('layout', 'default'));
 	}
 }
--- modules/mod_feed/tmpl/default.php
+++ modules/mod_feed/tmpl/default.php
@@ -1,20 +1,41 @@
 <?php
 /**
- * @package		Joomla.Site
- * @subpackage	mod_feed
- * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 Purdue University. All rights reserved.
+ * All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
 // no direct access
 defined('_JEXEC') or die;
-?>
 
-<?php
 if ($feed != false)
 {
 	//image handling
-	$iUrl	= isset($feed->image->url)	? $feed->image->url	: null;
+	$iUrl   = isset($feed->image->url)   ? $feed->image->url   : null;
 	$iTitle = isset($feed->image->title) ? $feed->image->title : null;
 	?>
 	<div style="direction: <?php echo $rssrtl ? 'rtl' :'ltr'; ?>; text-align: <?php echo $rssrtl ? 'right' :'left'; ?> ! important"  class="feed<?php echo $moduleclass_sfx; ?>">
@@ -24,8 +45,9 @@ if ($feed != false)
 		?>
 
 				<h4>
-					<a href="<?php echo str_replace('&', '&amp', $feed->link); ?>" target="_blank">
-					<?php echo $feed->title; ?></a>
+					<a href="<?php echo str_replace('&', '&amp', $feed->link); ?>" rel="external">
+						<?php echo $feed->title; ?>
+					</a>
 				</h4>
 
 		<?php
@@ -35,24 +57,26 @@ if ($feed != false)
 	if ($params->get('rssdesc', 1)) {
 	?>
 		<?php echo $feed->description; ?>
-
 		<?php
 	}
 
 	// feed image
 	if ($params->get('rssimage', 1) && $iUrl) {
 	?>
-		<img src="<?php echo $iUrl; ?>" alt="<?php echo @$iTitle; ?>"/>
+		<img src="<?php echo $iUrl; ?>" alt="<?php echo @$iTitle; ?>" />
 
 	<?php
 	}
 
 	$actualItems = count($feed->items);
-	$setItems	= $params->get('rssitems', 5);
+	$setItems    = $params->get('rssitems', 5);
 
-	if ($setItems > $actualItems) {
+	if ($setItems > $actualItems)
+	{
 		$totalItems = $actualItems;
-	} else {
+	}
+	else
+	{
 		$totalItems = $setItems;
 	}
 	?>
@@ -66,56 +90,61 @@ if ($feed != false)
 				// item title
 				?>
 				<li class="newsfeed-item">
-					<?php	if (!is_null($currItem->get_link())) {
-					?>
-				<?php if (!is_null($feed->title) && $params->get('rsstitle', 1))
-
-					{ echo '<h5 class="feed-link">';}
-				else
-				{
-				echo '<h4 class="feed-link">';
-				}
-				?>
-
-				<a href="<?php echo $currItem->get_link(); ?>" target="_blank">
-					<?php echo $currItem->get_title(); ?></a>
-					<?php if (!is_null($feed->title) && $params->get('rsstitle', 1))
-
-					{ echo '</h5>';}
-						else
-						{ echo '</h4>';}
-				?>
-				<?php
-				}
-
-				// item description
-				if ($params->get('rssitemdesc', 1))
-				{
-					// item description
-					$text = $currItem->get_description();
-					$text = str_replace('&apos;', "'", $text);
-					$text=strip_tags($text);
-					// word limit check
-					if ($words)
+					<?php
+					if (!is_null($currItem->get_link()))
 					{
-						$texts = explode(' ', $text);
-						$count = count($texts);
-						if ($count > $words)
+						if (!is_null($feed->title) && $params->get('rsstitle', 1))
 						{
-							$text = '';
-							for ($i = 0; $i < $words; $i ++) {
-								$text .= ' '.$texts[$i];
-							}
-							$text .= '...';
+							echo '<h5 class="feed-link">';
+						}
+						else
+						{
+							echo '<h4 class="feed-link">';
+						}
+						?>
+						<a href="<?php echo $currItem->get_link(); ?>" target="_blank">
+							<?php echo $currItem->get_title(); ?>
+						</a>
+						<?php
+						if (!is_null($feed->title) && $params->get('rsstitle', 1))
+						{
+							echo '</h5>';
+						}
+						else
+						{
+							echo '</h4>';
 						}
 					}
+
+					// item description
+					if ($params->get('rssitemdesc', 1))
+					{
+						// item description
+						$text = $currItem->get_description();
+						$text = str_replace('&apos;', "'", $text);
+						$text = strip_tags($text);
+						// word limit check
+						if ($words)
+						{
+							$texts = explode(' ', $text);
+							$count = count($texts);
+							if ($count > $words)
+							{
+								$text = '';
+								for ($i = 0; $i < $words; $i ++)
+								{
+									$text .= ' '.$texts[$i];
+								}
+								$text .= '...';
+							}
+						}
+						?>
+
+							<p><?php echo $text; ?></p>
+
+						<?php
+					}
 					?>
-
-						<p><?php echo $text; ?></p>
-
-					<?php
-				}
-				?>
 				</li>
 				<?php
 			}
@@ -123,4 +152,5 @@ if ($feed != false)
 			</ul>
 
 	</div>
-<?php } ?>
+	<?php
+}
