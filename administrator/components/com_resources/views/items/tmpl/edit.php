bb92fc78723e70089e7fa7cf8e3c57356bab0f19
Alissa Nedossekina <alisa@purdue.edu>
2015-01-20 14:48:32 -0500

    [#7195][HUBZERO][COM_RESOURCES] allow to save tool resource in the back-end

--- administrator/components/com_resources/views/items/tmpl/edit.php
+++ administrator/components/com_resources/views/items/tmpl/edit.php
@@ -185,17 +185,22 @@ function popratings()
 			<div class="input-wrap">
 				<p class="warning"><?php echo JText::_('COM_RESOURCES_WARNING_TOOLS_USE_PIPELINE'); ?></p>
 			</div>
-
+			<div class="input-wrap">
+				<label for="field-alias"><?php echo JText::_('COM_RESOURCES_FIELD_ALIAS'); ?>:</label><br />
+				<input type="text" name="alias" id="field-alias" maxlength="250" value="<?php echo $this->escape(stripslashes($this->row->alias)); ?>" />
+			</div>
 			<div class="input-wrap" data-hint="<?php echo JText::_('COM_RESOURCES_FIELD_CANONICAL_HINT'); ?>">
 				<label for="attrib-canonical"><?php echo JText::_('COM_RESOURCES_FIELD_CANONICAL'); ?>:</label><br />
 				<input type="text" name="attrib[canonical]" id="attrib-canonical" maxlength="250" value="<?php echo $this->attribs->get('canonical', ''); ?>" />
 				<span class="hint"><?php echo JText::_('COM_RESOURCES_FIELD_CANONICAL_HINT'); ?></span>
 			</div>
+			<input type="hidden" name="title" id="field-title" maxlength="250" value="<?php echo $this->escape(stripslashes($this->row->title)); ?>" />
+			<input type="hidden" name="type" id="type" maxlength="250" value="7" />
 	<?php } else { ?>
 
 			<div class="input-wrap">
 				<label for="field-title"><?php echo JText::_('COM_RESOURCES_FIELD_TITLE'); ?>: <span class="required"><?php echo JText::_('JOPTION_REQUIRED'); ?></span></label><br />
-				<input type="text" name="title" id="field-title" maxlength="250" value="<?php echo $this->escape(stripslashes($this->row->title)); ?>" />
+				<input type="text" name="title" id="type" value="<?php echo $this->escape(stripslashes($this->row->title)); ?>" />
 			</div>
 
 			<div class="input-wrap">
