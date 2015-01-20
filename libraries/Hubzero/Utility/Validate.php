939aaaba2411449fcc3c301ea81de8046ca4de4d
Christopher Smoak <csmoak@purdue.edu>
2015-01-20 16:35:09 -0500

    [LIB_HUBZERO] Remove case-insensitive flag on group validator method.

--- libraries/Hubzero/Utility/Validate.php
+++ libraries/Hubzero/Utility/Validate.php
@@ -294,7 +294,7 @@ class Validate
 	 */
 	static public function group($cn, $allowDashes = false)
 	{
-		$pattern = '/^[0-9a-z]+[' . ($allowDashes ? '-' : '') . '_0-9a-z]*$/i';
+		$pattern = '/^[0-9a-z]+[' . ($allowDashes ? '-' : '') . '_0-9a-z]*$/';
 
 		if (!preg_match($pattern, $cn))
 		{
