bbca89d821c97f64ffab0ffbc265bc2d1866df8e
Kevin Wojkovich <kevinw@purdue.edu>
2015-02-05 13:56:35 -0500

    [HUBZERO][KNOWINNOVATION][#7269][COM_EVENTS] Adds the ability for 'administrators' and not just super admins to create,update,delete events.

--- components/com_events/controllers/events.php
+++ components/com_events/controllers/events.php
@@ -2178,7 +2178,8 @@ class EventsControllerEvents extends \Hubzero\Component\SiteController
 		}
 
 		// Check if they're a site admin from Joomla
-		if ($this->juser->authorise('core.admin', $this->_option . '.component'))
+		if ($this->juser->authorise('core.admin', $this->_option . '.component')
+			|| $this->juser->authorise('core.manage', $this->_option . '.component'))
 		{
 			return true;
 		}
