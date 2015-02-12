dc827bbdaad02ec407823e1c31d8f5d9484f2b19
Shawn Rice <zooley@purdue.edu>
2015-02-12 09:15:14 -0500

    [COM_USAGE] Namespacing component because why not

--- administrator/components/com_usage/controllers/data.php
+++ administrator/components/com_usage/controllers/data.php
@@ -2,7 +2,7 @@
 /**
  * HUBzero CMS
  *
- * Copyright 2005-2011 Purdue University. All rights reserved.
+ * Copyright 2005-2015 Purdue University. All rights reserved.
  *
  * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
  *
@@ -24,49 +24,28 @@
  *
  * @package   hubzero-cms
  * @author    Shawn Rice <zooley@purdue.edu>
- * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
  * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die('Restricted access');
+namespace Components\Usage\Controllers;
+
+use Hubzero\Component\AdminController;
 
 /**
  * Controller class for usage
  */
-class UsageControllerData extends \Hubzero\Component\AdminController
+class Data extends AdminController
 {
 	/**
 	 * Display primary page
 	 *
-	 * @return     void
+	 * @return  void
 	 */
 	public function displayTask()
 	{
-		// Set any errors
-		if ($this->getError())
-		{
-			foreach ($this->getError() as $error)
-			{
-				$this->view->setError($error);
-			}
-		}
-
 		// Output the HTML
 		$this->view->display();
-	}
-
-	/**
-	 * Cancel a task
-	 *
-	 * @return     void
-	 */
-	public function cancel()
-	{
-		// Redirect
-		$this->setRedirect(
-			'index.php?option=' . $this->_option . '&controller=' . $this->_controller
-		);
 	}
 }
 
--- administrator/components/com_usage/usage.php
+++ administrator/components/com_usage/usage.php
@@ -2,7 +2,7 @@
 /**
  * HUBzero CMS
  *
- * Copyright 2005-2011 Purdue University. All rights reserved.
+ * Copyright 2005-2015 Purdue University. All rights reserved.
  *
  * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
  *
@@ -24,28 +24,25 @@
  *
  * @package   hubzero-cms
  * @author    Shawn Rice <zooley@purdue.edu>
- * @copyright Copyright 2005-2011 Purdue University. All rights reserved.
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
  * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die('Restricted access');
-
-$option = JRequest::getCmd('option', 'com_usage');
+namespace Components\Usage;
 
 // Authorization check
-if (!JFactory::getUser()->authorise('core.manage', $option))
+if (!\JFactory::getUser()->authorise('core.manage', 'com_usage'))
 {
-	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+	throw new \Exception(\JText::_('JERROR_ALERTNOAUTHOR'), 404);
 }
 
-$controllerName = JRequest::getCmd('controller', 'data');
-if (!file_exists(JPATH_COMPONENT_ADMINISTRATOR . DS . 'controllers' . DS . $controllerName . '.php'))
+$controllerName = \JRequest::getCmd('controller', 'data');
+if (!file_exists(__DIR__ . DS . 'controllers' . DS . $controllerName . '.php'))
 {
 	$controllerName = 'data';
 }
-require_once(JPATH_COMPONENT_ADMINISTRATOR . DS . 'controllers' . DS . $controllerName . '.php');
-$controllerName = 'UsageController' . ucfirst($controllerName);
+require_once(__DIR__ . DS . 'controllers' . DS . $controllerName . '.php');
+$controllerName = __NAMESPACE__ . '\\Controllers\\' . ucfirst($controllerName);
 
 // Instantiate controller
 $controller = new $controllerName();
