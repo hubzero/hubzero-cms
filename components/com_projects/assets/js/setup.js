bfb21ef34e49bfa6b25e40fe90984281426a9574
Alissa Nedossekina <alisa@purdue.edu>
2015-02-19 13:20:18 -0500

    [COM_PROJECTS] changes to setup.js and projects.css

--- components/com_projects/assets/js/setup.js
+++ components/com_projects/assets/js/setup.js
@@ -33,28 +33,32 @@ if (!jq) {
 
 HUB.ProjectSetup = {
 	jQuery: jq,
-	
-	initialize: function() 
+
+	initialize: function()
 	{
 		var $ 				= this.jQuery;
 		var hubfrm 			= $('#hubForm');
-		var sbjt 			= $('#verificationarea');
-		var moveon 			= $('#moveon');
-		var describe 		= $('#describe');
-		var describearea 	= $('#describearea');
-		var ptitle 			= $('#ptitle');
-		var sbjt_t 			= $('#verificationarea_title');
 		var next_desc		= $('#next_desc');
 		var next_step		= $('#next_step');
-		
+
 		// Setup
 		var rest  = $('.restricted-opt');
 		var resta = $('.restricted-answer');
-		
-		if(rest.length > 0 && resta.length > 0)
+
+		// Enable ajax upload of project image
+		HUB.ProjectSetup.enableImageUpload();
+
+		// Enable ajax delete of project image
+		HUB.ProjectSetup.enableImageDelete();
+
+		// Determine next step in setup process
+		HUB.ProjectSetup.advance();
+
+		// Restrictions
+		if (rest.length > 0 && resta.length > 0)
 		{
 			HUB.ProjectSetup.enableButton();
-			
+
 			$('.restricted-opt').each(function(i, item) {
 				$(item).off('click');
 				HUB.ProjectSetup.showStopSigns(item);
@@ -71,23 +75,23 @@ HUB.ProjectSetup = {
 			$('#restricted-no').on('click', function(e) {
 				$('.restricted-opt').each(function(i, item) {
 					$(item).removeAttr("checked");
-				});	
-			});	
-						
+				});
+			});
+
 			// Check if can proceed
 			if($('.option').length > 0)
 			{
-				$('.option').each(function(i, item) 
+				$('.option').each(function(i, item)
 				{
 					$(item).on('click', function(e) {
 						HUB.ProjectSetup.enableButton();
 					});
 				});
-			}				
-		}		
-	
+			}
+		}
+
 		// Setup pre-screen
-		if ($('#f-restricted-no').length && $('#f-restricted-explain'))
+		if ($('#f-restricted-no').length && $('#f-restricted-explain').length)
 		{
 			$('#f-restricted-no').on('click', function(e) {
 				$('#f-restricted-explain').addClass('hidden');
@@ -97,16 +101,15 @@ HUB.ProjectSetup = {
 				$('#f-restricted-explain').addClass('hidden');
 			}
 		}
-
-		if ($('#f-restricted-yes').length && $('#f-restricted-explain'))
+		if ($('#f-restricted-yes').length && $('#f-restricted-explain').length)
 		{
 			$('#f-restricted-yes').on('click', function(e) {
 				$('#f-restricted-explain').removeClass('hidden');
 			});
-		}	
-				
+		}
+
 		// Show/hide options to describe project
-		if ($('#pid') && ($('#pid').val() == '' || $('#pid').val() == 0))  
+		if ($('#pid').length && ($('#pid').val() == '' || $('#pid').val() == 0))
 		{
 			// Show by default for those with JS enabled
 			if ($('#moveon')) {
@@ -116,150 +119,271 @@ HUB.ProjectSetup = {
 			if ($('#describearea')) {
 				$('#describearea').css('display', 'none');
 			}
-			
-			// Title verification
-			if ($('#describe') && $('#ptitle')) {
-				var keyupTimerA = '';
-				$('#ptitle').on('keyup', function(e) {
-					 var keyupTimerA = setTimeout((function() { 
-						if ($('#ptitle').val().length > 2) {
-							if ($('#verificationarea_title').length) {
-								$('#verificationarea_title').html('<p class="verify_passed">Title looks good &rarr;</p>');
-							}
-						} else {
-							if ($('#verificationarea_title').length) {
-								$('#verificationarea_title').html('<p class="verify_failed">Title too short &rarr;</p>');
-							}
-						}
-						HUB.ProjectSetup.watchInput($('#verified').val(), $('#ptitle').val().length, $('#describe'), $('#moveon'));
-					}), 500);
-				});
-				
-				$('#ptitle').on('keydown', function(e) {
-					if (keyupTimerA) {
-						clearTimeout(keyupTimerA);
-					}
-				});
-			}
-			
+
 			// Show description fields
 			if ($('#next_desc')) {
 				$('#next_desc').on('click', function(e) {
 					e.preventDefault();
-					$('#extended').val(1);
-					$('#save_stage').val(0);
+					$('#step').val(0);
 					$(hubfrm).submit();
 				});
 			}
-			
+
 			// Go to next step
 			if ($('#next_step')) {
 				$('#next_step').on('click', function(e) {
 					e.preventDefault();
-					$('#extended').val(0);
-					$('#save_stage').val(1);
+					$('#step').val(1);
 					$(hubfrm).submit();
 				});
 			}
 		}
 
-		// Verifier for project alias
-		if ($('#verificationarea')) {
-			var keyupTimerB = '';			
-			
-			$('#name').on('keydown', function(eventInstance) {
-				if (keyupTimerB) {
-					clearTimeout(keyupTimerB);
-				}
-				
-				var eventInstance = eventInstance || window.event;
-				var key = eventInstance.keyCode || eventInstance.which;
+		// Verification
+		if ($('.verifyme').length)
+		{
+			$('.verifyme').each(function(i, item) {
+				var keyupTimer = '';
+				var id = $(item).attr('id');
+				var output = $(item).parent().find('.verification')[0];
 
-				// Disallow spaces
-				if (key == 32 )
-				{
-				  	eventInstance.preventDefault(); 
-				}
-			});
-				
-			$('#name').on('keyup', function(e) {
+				$(item).on('keydown', function(eventInstance) {
+					if (keyupTimer) {
+						clearTimeout(keyupTimer);
+					}
+					if (id == 'field-alias')
+					{
+						var eventInstance = eventInstance || window.event;
+						var key = eventInstance.keyCode || eventInstance.which;
 
-				// Clean up entered value
-				//$(this).val(HUB.Projects.cleanupText($(this).val()));
-				
-				$('#verificationarea').empty();
-				var keyupTimerB = setTimeout((function() {  
-					$('#verificationarea').append('<p id="loading-section"></p>');
+						// Disallow spaces
+						if (key == 32 )
+						{
+						  	eventInstance.preventDefault();
+						}
+					}
+				});
 
-					$.get('index.php?option=com_projects&task=verify&no_html=1&ajax=1&name=' 
-					+ $('#name').val() + '&pid=' + $('#pid').val(), {}, function(data) {
-						if (data) {
-							var bits = data.split('::');
-							var out  = '<p class="' + bits[0] + '">' + bits[1] + ' &rarr;</p>';
-							$('#verificationarea').html(out);
+				$(item).on('keyup', function(eventInstance) {
+					var result = '';
+
+					// Clear status message
+					if ($(output).length)
+					{
+						$(output).html('');
+					}
+
+					// Verify for alias name
+					var keyupTimer1 = setTimeout((function()
+					{
+						if ($(item).val() == '')
+						{
+							$(item).css('background', '#fbe5e5');
+						}
+						else
+						{
+							$(item).css('background', '#f9fdf6');
 						}
 
-						if (data.search("passed") >= 0) { 
-							$('#verified').val(1);
-						} else { 
-							$('#verified').val(0);
+						if (id == 'field-alias')
+						{
+							// Via AJAX
+							url = '/projects/verify/?no_html=1&ajax=1&text='
+							+ $(item).val() + '&pid=' + $('#pid').val();
+							$.post(url, {},
+								function (response) {
+
+								response = $.parseJSON(response);
+
+								if (response.error)
+								{
+									$(output).html(response.error);
+									$(output).css('color', 'red');
+									$('#verified').val(0);
+									$(item).css('background', '#fbe5e5');
+									HUB.ProjectSetup.watchInput($('#describe'), $('#moveon'));
+								}
+								else if (response.message)
+								{
+									$(output).html(response.message);
+									$(output).css('color', 'green');
+									$('#verified').val(1);
+									$(item).css('background', '#f9fdf6');
+									HUB.ProjectSetup.watchInput($('#describe'), $('#moveon'));
+								}
+
+							});
+						}
+						else
+						{
+							HUB.ProjectSetup.suggestAlias();
 						}
 
-						if ($('#pid') && ($('#pid').val() == '' || $('#pid').val() == 0)) {
-							HUB.ProjectSetup.watchInput($('#verified').val(), $('#ptitle').val().length, $('#describe'), $('#moveon'));
-						}
-					});
-				}), 1000);
+					}), 1000);
+				});
 			});
 		}
-		
-		// Activate provisioned project
-		if ($('#verify-alias')) 
-		{
-			HUB.ProjectSetup.enableButtonActivate();
-			var keyupTimer1 = '';
-			$('#new-alias').on('keyup', function(e) {
-				if (keyupTimer1) {
-					clearTimeout(keyupTimer1);
-				}
-				
-				$('#new-alias').val(HUB.Projects.cleanupText($('#new-alias').val()));
-				$('#verify-alias').empty();
-				
-				var keyupTimer1 = setTimeout((function() {  
-					$('#verify-alias').append('<p id="loading-section"></p>');
-
-					$.get('index.php?option=com_projects&task=verify&no_html=1&ajax=1&name='
-					+ $('#new-alias').val()+'&pid='+$('#projectid').val(), {}, function(data) {
-						if (data) {
-							var bits = data.split('::');
-							var out  = '<p class="' + bits[0] + '">' + bits[1] + '</p>';
-							$('#verify-alias').html(out);
-						}
-
-						if (data.search("passed") >= 0) { 
-							$('#verified').val(1);
-						} else { 
-							$('#verified').val(0);
-						}
-
-						HUB.ProjectSetup.enableButtonActivate();
-					});
-				}), 1000);
-			});
-
-			if ($('#agree')) {
-				$('#agree').off('click');
-				$('#agree').on('click', function(e) {
-					HUB.ProjectSetup.enableButtonActivate();
-				});
-			}
-		}		
 	},
-	
-	watchInput: function(verified, supplied, elshow, elhide) 
+
+	suggestAlias: function()
 	{
-		if (verified == 1 && supplied > 2) {
+		if ($('#field-alias').length && $('#field-alias').val() == ''
+		&& $('#field-title').length && $('#field-title').val().length > 15)
+		{
+			var output = $('#field-alias').parent().find('.verification')[0];
+			// Via AJAX
+			url = '/projects/suggestalias/?no_html=1&ajax=1&text=' + escape($('#field-title').val());
+			$.post(url, {},
+				function (response) {
+					if (response)
+					{
+						$('#field-alias').val(response);
+						$('#verified').val(1);
+						$('#field-alias').css('background', '#f9fdf6');
+						HUB.ProjectSetup.watchInput($('#describe'), $('#moveon'));
+						// Clear status message
+						if ($(output).length)
+						{
+							$(output).html('');
+						}
+					}
+			});
+		}
+	},
+
+	enableImageDelete: function()
+	{
+		if (!$('#deleteimg').length)
+		{
+			return false;
+		}
+		// Delete
+		$('#deleteimg').on('click', function(e)
+		{
+			e.preventDefault();
+			var url = $('#deleteimg').attr('href') + '?ajax=1&no_html=1';
+
+			// Ajax call 
+			$.post(url, {},
+				function (response) {
+				response = $.parseJSON(response);
+				if (response.success)
+				{
+					// Reload image
+					var d = new Date();
+					var src = $("#project-image-content").attr('src');
+					$("#project-image-content").attr('src', src + '?' + d.getTime());
+				}
+			});
+		});
+	},
+
+	enableImageUpload: function()
+	{
+		if (!$('#ajax-upload').length)
+		{
+			return false;
+		}
+
+		var action = $("#ajax-upload").attr("data-action");
+		var btn    = $('#upload-file');
+
+		// Send data
+		btn.on('click', function(e)
+		{
+			e.preventDefault();
+			var formData = new FormData();
+			formData.append("qqfile", $("#uploader")[0].files[0]);
+			$('#status-box').html('');
+
+			$.ajax({
+		           type: "POST",
+		           url: action,
+		           data: formData,
+				   contentType: false,
+				   processData: false,
+		           success: function(response)
+		           {
+						var success = 0;
+						var error = 'There was a problem uploading file(s)';
+
+						if (response)
+						{
+							try {
+								response = $.parseJSON(response);
+								if (response.error || response.error != false)
+								{
+									error = response.error;
+								}
+								if (response.success)
+								{
+									success = response.success;
+								}
+							}
+							catch (e)
+							{
+								// problem
+							}
+						}
+						// Success or error
+						if (success)
+						{
+							// Reload image
+							var d = new Date();
+							var src = $("#project-image-content").attr('src');
+							$("#project-image-content").attr('src', src + '?' + d.getTime());
+						}
+						else
+						{
+							$('#status-box').html(error);
+						}
+					}
+			});
+		});
+
+	},
+
+	advance: function()
+	{
+		if (!$('#insetup').length || $('#insetup').val() == 0)
+		{
+			return false;
+		}
+
+		if ($('#gonext').length)
+		{
+			$('#gonext').on('click', function(eventInstance) {
+				eventInstance.preventDefault();
+				var step = $('#step').val();
+				var next = Number(step) + 1;
+				$('#step').val(next);
+
+				if ($('#hubForm')) {
+					$('#hubForm').submit();
+				}
+			});
+		}
+	},
+
+	watchInput: function(elshow, elhide)
+	{
+		if (!$('#pid').length || $('#pid').val() != 0)
+		{
+			return;
+		}
+		var passed = 1;
+		$('.verifyme').each(function(i, item) {
+			if ($(item).val() == '')
+			{
+				passed = 0;
+			}
+		});
+		if ($('#verified').val() != 1)
+		{
+			passed = 0;
+		}
+
+		if (passed == 1) {
 			elhide.css('display', 'none');
 			elshow.css('display', 'block');
 		} else {
@@ -267,13 +391,13 @@ HUB.ProjectSetup = {
 			elshow.css('display', 'none');
 		}
 	},
-	
-	enableButton: function() 
+
+	enableButton: function()
 	{
 		var $ = this.jQuery;
 		var con = $('#btn-finalize');
 		var passed = 1;
-		
+
 		if($('#export') && $('#export').attr('checked') == 'checked')
 		{
 			passed = 0;
@@ -291,13 +415,13 @@ HUB.ProjectSetup = {
 			passed = 0;
 		}
 
-		if(passed == 1 && con.hasClass('disabled')) { 
-			con.removeClass('disabled'); 
+		if(passed == 1 && con.hasClass('disabled')) {
+			con.removeClass('disabled');
 		}
 		if(passed == 0 && !con.hasClass('disabled')) {
 			con.addClass('disabled');
 		}
-		
+
 		con.off('click');
 		con.on('click', function(e) {
 			e.preventDefault();
@@ -308,14 +432,14 @@ HUB.ProjectSetup = {
 			}
 		});
 	},
-	
+
 	showStopSigns: function(el)
 	{
 		var $ = this.jQuery;
-		
+
 		var oid = $(el).attr('id');
 		var obox = '#stop-' + oid;
-		
+
 		if($(el).attr('checked') == 'checked' && $(obox).hasClass('hidden'))
 		{
 			$(obox).removeClass('hidden');
@@ -325,12 +449,12 @@ HUB.ProjectSetup = {
 			$(obox).addClass('hidden');
 		}
 	},
-	
-	enableButtonActivate: function() 
+
+	enableButtonActivate: function()
 	{
 		var $ = this.jQuery;
 		var con = $('#b-continue');
-		
+
 		if (con)
 		{
 			var passed = 1;
@@ -338,9 +462,9 @@ HUB.ProjectSetup = {
 			if (($('#verified') && $('#verified').val() == 0) || ($('#new-alias') && $('#new-alias').val() == '')) {
 				passed = 0;
 			}
-			
-			if (passed == 1 && con.hasClass('disabled')) { 
-				con.removeClass('disabled'); 
+
+			if (passed == 1 && con.hasClass('disabled')) {
+				con.removeClass('disabled');
 			}
 			if (passed == 0 && !con.hasClass('disabled')) {
 				con.addClass('disabled');
@@ -358,7 +482,7 @@ HUB.ProjectSetup = {
 		}
 	}
 }
-	
+
 jQuery(document).ready(function($){
 	HUB.ProjectSetup.initialize();
 });
