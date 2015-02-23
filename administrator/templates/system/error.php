7b79588e318df5df4a47c684112d2c7791cb99e3
Shawn Rice <zooley@purdue.edu>
2015-02-23 06:54:56 -0500

    [TPL_*] Make sure all asset paths are relative to root

--- administrator/templates/system/error.php
+++ administrator/templates/system/error.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<title><?php echo $this->error->getCode(); ?> - <?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></title>
-		<link rel="stylesheet" href="templates/system/css/error.css" type="text/css" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/system/css/error.css" type="text/css" />
 	</head>
 	<body>
 		<table width="550" align="center" class="outline">
