8ed5c010e7c193176757d3c936fa71fa459d2592
Shawn Rice <zooley@purdue.edu>
2015-02-23 16:11:22 -0500

    [LIB_HUBZERO] Creating ErrorBag trait ... this may chnage later on. Hmmm.

--- libraries/Hubzero/Base/Object.php
+++ libraries/Hubzero/Base/Object.php
@@ -30,6 +30,7 @@
 
 namespace Hubzero\Base;
 
+use Hubzero\Base\Traits\ErrorBag;
 use Exception;
 
 /**
@@ -40,12 +41,7 @@ use Exception;
  */
 class Object
 {
-	/**
-	 * An array of error messages or Exception objects.
-	 *
-	 * @var    array
-	 */
-	protected $_errors = array();
+	use ErrorBag;
 
 	/**
 	 * Class constructor, overridden in descendant classes.
@@ -124,50 +120,6 @@ class Object
 	}
 
 	/**
-	 * Get the most recent error message.
-	 *
-	 * @param   integer  $i         Option error index.
-	 * @param   boolean  $toString  Indicates if JError objects should return their error message.
-	 * @return  string   Error message
-	 */
-	public function getError($i = null, $toString = true)
-	{
-		// Find the error
-		if ($i === null)
-		{
-			// Default, return the last message
-			$error = end($this->_errors);
-		}
-		elseif (!array_key_exists($i, $this->_errors))
-		{
-			// If $i has been specified but does not exist, return false
-			return false;
-		}
-		else
-		{
-			$error = $this->_errors[$i];
-		}
-
-		// Check if only the string is requested
-		if ($error instanceof Exception && $toString)
-		{
-			return (string) $error;
-		}
-
-		return $error;
-	}
-
-	/**
-	 * Return all errors, if any.
-	 *
-	 * @return  array  Array of error messages or JErrors.
-	 */
-	public function getErrors()
-	{
-		return $this->_errors;
-	}
-
-	/**
 	 * Modifies a property of the object, creating it if it does not already exist.
 	 * Returns $this so set() can be chained
 	 *
@@ -204,25 +156,5 @@ class Object
 		}
 
 		return false;
-	}
-
-	/**
-	 * Add an error message.
-	 *
-	 * @param   string  $error  Error message.
-	 * @param   string  $key    Specific key to set the value to
-	 * @return  object
-	 */
-	public function setError($error, $key=null)
-	{
-		if ($key)
-		{
-			$this->_errors[$key] = $error;
-		}
-		else
-		{
-			array_push($this->_errors, $error);
-		}
-		return $this;
 	}
 }
---
+++ libraries/Hubzero/Base/Traits/ErrorBag.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2014 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2014 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+namespace Hubzero\Base\Traits;
+
+use Exception;
+
+/**
+ * Creator trait for models
+ */
+trait ErrorBag
+{
+	/**
+	 * An array of error messages or Exception objects.
+	 *
+	 * @var    array
+	 */
+	protected $errors = array();
+
+	/**
+	 * Get the most recent error message.
+	 *
+	 * @param   integer  $i         Option error index.
+	 * @param   boolean  $toString  Indicates if JError objects should return their error message.
+	 * @return  string   Error message
+	 */
+	public function getError($i = null, $toString = true)
+	{
+		// Find the error
+		if ($i === null)
+		{
+			// Default, return the last message
+			$error = end($this->errors);
+		}
+		elseif (!array_key_exists($i, $this->errors))
+		{
+			// If $i has been specified but does not exist, return false
+			return false;
+		}
+		else
+		{
+			$error = $this->errors[$i];
+		}
+
+		// Check if only the string is requested
+		if ($error instanceof Exception && $toString)
+		{
+			return (string) $error;
+		}
+
+		return $error;
+	}
+
+	/**
+	 * Return all errors, if any.
+	 *
+	 * @return  array  Array of error messages or JErrors.
+	 */
+	public function getErrors()
+	{
+		return $this->errors;
+	}
+
+	/**
+	 * Add an error message.
+	 *
+	 * @param   string  $error  Error message.
+	 * @param   string  $key    Specific key to set the value to
+	 * @return  object
+	 */
+	public function setError($error, $key=null)
+	{
+		if ($key)
+		{
+			$this->errors[$key] = $error;
+		}
+		else
+		{
+			array_push($this->errors, $error);
+		}
+		return $this;
+	}
+}
+
