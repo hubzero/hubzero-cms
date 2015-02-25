b60e745a242de63faf265f4743251006c7eadacb
Shawn Rice <zooley@purdue.edu>
2015-02-25 12:38:09 -0500

    [NANOHUB][#272822][COM_WISHLIST] Changing entity for icon as webmail clients didn't care for the lightbulb.

--- components/com_wishlist/views/emails/tmpl/comment_html.php
+++ components/com_wishlist/views/emails/tmpl/comment_html.php
@@ -48,6 +48,7 @@ $this->css(
 		table#wish-comments>tbody>tr>td {
 			padding: 0 !important;
 		}
+<<<<<<< HEAD
 	}'
 );
 ?>
@@ -187,3 +188,203 @@ $this->css(
 		</tbody>
 	</table>
 	<!-- End Footer -->
+=======
+		</style>
+
+		<!--[if IEMobile 7]>
+		<style type="text/css">
+		/* Targeting Windows Mobile */
+		</style>
+		<![endif]-->
+
+		<!--[if gte mso 9]>
+		<style type="text/css" >
+		/* Outlook 2007/10 List Fix */
+		.article-content ol, .article-content ul {
+		  margin: 0 0 0 24px;
+		  padding: 0;
+		  list-style-position: inside;
+		}
+		</style>
+		<![endif]-->
+	</head>
+	<body style="width: 100% !important; font-family: 'Helvetica Neue', Helvetica, Verdana, Arial, sans-serif; font-size: 12px; -webkit-text-size-adjust: none; color: #616161; line-height: 1.4em; color: #666; background: #fff; text-rendering: optimizeLegibility;" bgcolor="#ffffff">
+
+		<!-- Start Body Wrapper Table -->
+		<table width="100%" cellpadding="0" cellspacing="0" border="0"  id="backgroundTable" style="background-color: #ffffff; min-width: 100%;" bgcolor="#ffffff">
+			<tbody>
+				<tr style="border-collapse: collapse;">
+					<td bgcolor="#ffffff" align="center" style="border-collapse: collapse;">
+
+						<!-- Start Content Wrapper Table -->
+						<table class="tbl-wrap" width="670" cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse;">
+							<tbody>
+								<tr style="border-collapse: collapse;">
+									<td bgcolor="#ffffff" width="10" style="border-collapse: collapse;"></td>
+									<td class="tbl-body" bgcolor="#ffffff" width="650" align="left" style="border-collapse: collapse;">
+
+										<!-- Start Header Spacer -->
+										<table  width="100%" cellpadding="0" cellspacing="0" border="0">
+											<tr style="border-collapse: collapse;">
+												<td height="30" style="border-collapse: collapse;"></td>
+											</tr>
+										</table>
+										<!-- End Header Spacer -->
+
+										<!-- Start Header -->
+										<table class="tbl-header" cellpadding="2" cellspacing="3" border="0" width="100%" style="border-collapse: collapse; border-bottom: 2px solid #e1e1e1;">
+											<tbody>
+												<tr>
+													<td width="10%" nowrap="nowrap" align="left" valign="bottom" style="font-size: 1.4em; color: #999; padding: 0 10px 5px 0; text-align: left;">
+														<?php echo $jconfig->getValue('config.sitename'); ?>
+													</td>
+													<td class="mobilehide" width="80%" align="left" valign="bottom" style="line-height: 1; padding: 0 0 5px 10px;">
+														<span style="font-weight: bold; font-size: 0.85em; color: #666; -webkit-text-size-adjust: none;">
+															<a href="<?php echo $juri->base(); ?>" style="color: #666; font-weight: bold; text-decoration: none; border: none;"><?php echo $juri->base(); ?></a>
+														</span>
+														<br />
+														<span style="font-size: 0.85em; color: #666; -webkit-text-size-adjust: none;"><?php echo $jconfig->getValue('config.MetaDesc'); ?></span>
+													</td>
+													<td width="10%" nowrap="nowrap" align="right" valign="bottom" style="border-left: 1px solid #e1e1e1; font-size: 1.2em; color: #999; padding: 0 0 5px 10px; text-align: right; vertical-align: bottom;">
+														<?php echo JText::_('COM_WISHLIST'); ?>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+										<!-- End Header -->
+
+										<!-- Start Header Spacer -->
+										<table width="100%" cellpadding="0" cellspacing="0" border="0">
+											<tr style="border-collapse: collapse;">
+												<td height="30" style="border-collapse: collapse;"></td>
+											</tr>
+										</table>
+										<!-- End Header Spacer -->
+
+										<table id="ticket-info" width="100%" cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; border: 1px solid #c8e3c2; background: #eafbe6; font-size: 0.9em; line-height: 1.6em; background-image: -webkit-gradient(linear, 0 0, 100% 100%,
+																			color-stop(.25, rgba(255, 255, 255, .075)), color-stop(.25, transparent),
+																			color-stop(.5, transparent), color-stop(.5, rgba(255, 255, 255, .075)),
+																			color-stop(.75, rgba(255, 255, 255, .075)), color-stop(.75, transparent),
+																			to(transparent));
+										background-image: -webkit-linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
+																		transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
+																		transparent 75%, transparent);
+										background-image: -moz-linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
+																		transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
+																		transparent 75%, transparent);
+										background-image: -ms-linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
+																		transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
+																		transparent 75%, transparent);
+										background-image: -o-linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
+																		transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
+																		transparent 75%, transparent);
+										background-image: linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
+																		transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
+																		transparent 75%, transparent);
+																		-webkit-background-size: 30px 30px;
+																		-moz-background-size: 30px 30px;
+																		background-size: 30px 30px;">
+											<thead class="mobilehide">
+												<tr>
+													<th style="font-weight: normal; border-bottom: 1px solid #c8e3c2; padding: 8px; text-align: left" align="left">
+														<?php echo $this->escape($this->wish->get('subject')); ?>
+													</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td width="100%" style="padding: 8px;">
+														<div id="wish-number" class="mobilehide" style="float: left; width: 5em; font-size: 2.5em; font-weight: bold; text-align: center; padding: 30px;" align="center">
+															<?php /* &#x1f4a1; */ ?>&#x2736;
+														</div>
+														<table style="border-collapse: collapse; font-size: 0.9em;" cellpadding="0" cellspacing="0" border="0">
+															<tbody>
+																<tr>
+																	<th style="text-align: right; padding: 0 0.5em; font-weight: bold; white-space: nowrap;" align="right">Wish:</th>
+																	<td style="text-align: left; padding: 0 0.5em;" width="100%" align="left"># <?php echo $this->wish->get('id'); ?></td>
+																</tr>
+																<tr>
+																	<th style="text-align: right; padding: 0 0.5em; font-weight: bold; white-space: nowrap;" align="right">Created:</th>
+																	<td style="text-align: left; padding: 0 0.5em;" width="100%" align="left">@ <?php echo $this->wish->proposed('time'); ?> on <?php echo $this->wish->proposed('date'); ?></td>
+																</tr>
+																<tr>
+																	<th style="text-align: right; padding: 0 0.5em; font-weight: bold; white-space: nowrap;" align="right">Creator:</th>
+																	<td style="text-align: left; padding: 0 0.5em;" width="100%" align="left"><?php echo $this->wish->get('anonymous') ? JText::_('COM_WISHLIST_ANONYMOUS') : $this->escape(stripslashes($this->wish->proposer('name'))); ?></td>
+																</tr>
+																<tr>
+																	<th style="text-align: right; padding: 0 0.5em; font-weight: bold; white-space: nowrap;" align="right">Status:</th>
+																	<td style="text-align: left; padding: 0 0.5em;" width="100%" align="left"><?php echo $this->wish->status('text'); ?></td>
+																</tr>
+																<tr>
+																	<th style="text-align: right; padding: 0 0.5em; font-weight: bold; white-space: nowrap; vertical-align: top;" align="right">Tags:</th>
+																	<td style="text-align: left; padding: 0 0.5em;" width="100%" align="left"><?php echo $this->escape($this->wish->tags('string')); ?></td>
+																</tr>
+																<tr>
+																	<th style="text-align: right; padding: 0 0.5em; font-weight: bold; white-space: nowrap;" align="right">Link:</th>
+																	<td style="text-align: left; padding: 0 0.5em;" width="100%" align="left"><a href="<?php echo $link; ?>"><?php echo $link; ?></a></td>
+																</tr>
+															</tbody>
+														</table>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+
+										<table width="100%" id="wish-comments" style="border-collapse: collapse; margin: 2em 0 0 0; padding: 0" cellpadding="0" cellspacing="0" border="0">
+											<tbody>
+												<tr>
+													<th style="text-align: left;" align="left"><?php echo ($this->comment->get('anonymous') ? JText::_('COM_WISHLIST_UNKNOWN') : $this->escape($this->comment->creator('name'))); ?></th>
+													<th class="timestamp" style="color: #999; text-align: right;" align="right"><span class="mobilehide"><?php echo JText::sprintf('@%s on %s', $this->comment->created('time'), $this->comment->created('date')); ?></span></th>
+												</tr>
+												<tr>
+													<td colspan="2" style="padding: 0 2em;">
+														<div style="line-height: 1.6em; margin: 1em 0; padding: 0; text-align: left;"><?php echo $this->comment->content('parsed'); ?></div>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+
+										<!-- Start Footer Spacer -->
+										<table width="100%" cellpadding="0" cellspacing="0" border="0">
+											<tr style="border-collapse: collapse;">
+												<td height="30" style="border-collapse: collapse;"></td>
+											</tr>
+										</table>
+										<!-- End Footer Spacer -->
+
+										<!-- Start Header -->
+										<table width="100%" cellpadding="2" cellspacing="3" border="0" style="border-collapse: collapse; border-top: 2px solid #e1e1e1;">
+											<tbody>
+												<tr>
+													<td align="left" valign="bottom" style="line-height: 1; padding: 5px 0 0 0; ">
+														<span style="font-size: 0.85em; color: #666; -webkit-text-size-adjust: none;"><?php echo $jconfig->getValue('config.sitename'); ?> sent this email because you were added to the list of recipients on <a href="<?php echo $link; ?>">wish #<?php echo $this->wish->get('id'); ?></a>. Visit our <a href="<?php echo rtrim($juri->base(), '/'); ?>/legal/privacy">Privacy Policy</a> and <a href="<?php echo rtrim($juri->base(), '/'); ?>/support">Support Center</a> if you have any questions.</span>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+										<!-- End Header -->
+
+										<!-- Start Footer Spacer -->
+										<table width="100%" cellpadding="0" cellspacing="0" border="0">
+											<tbody>
+												<tr style="border-collapse: collapse;">
+													<td height="30" style="border-collapse: collapse; color: #fff !important;"><div style="height: 30px !important; visibility: hidden;">----</div></td>
+												</tr>
+											</tbody>
+										</table>
+										<!-- End Footer Spacer -->
+
+									</td>
+									<td bgcolor="#ffffff" width="10" style="border-collapse: collapse;"></td>
+								</tr>
+							</tbody>
+						</table>
+						<!-- End Content Wrapper Table -->
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<!-- End Body Wrapper Table -->
+	</body>
+</html>
+>>>>>>> 899817b... [NANOHUB][#272822][COM_WISHLIST] Changing entity for icon as webmail clients didn't care for the lightbulb.
