05f1d9c49e5962a82ac1c75423b832468ec91678
Shawn Rice <zooley@purdue.edu>
2015-02-27 14:53:59 -0500

    [LIB] Removing deprecated ximport() function.

--- libraries/loader.php
+++ libraries/loader.php
@@ -390,22 +390,3 @@ function jimport($path)
 }
 
 include_once(JPATH_ROOT . DS . 'vendor' . DS . 'autoload.php');
-
-/**
- * Intelligent HUBzero file importer
- *
- * @access public
- * @param string $path A underscore syntax path
- */
-function ximport($path)
-{
-	if (class_exists($path))
-	{
-		return true;
-	}
-
-	if (substr(strtolower($path),0,7) == 'hubzero')
-	{
-		return JLoader::import(str_replace('_', '.', $path), JPATH_ROOT . DS . 'libraries');
-	}
-}
