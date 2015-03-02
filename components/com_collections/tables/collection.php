2add7e9fee9c2629f1b138d92927fe78dd90fcc5
Shawn Rice <zooley@purdue.edu>
2015-03-02 10:52:33 -0500

    [QUBESHUB][#170][COM_COLLECTIONS] When loading by alias, only load non-trashed collections

--- components/com_collections/tables/collection.php
+++ components/com_collections/tables/collection.php
@@ -158,7 +158,7 @@ class CollectionsTableCollection extends JTable
 
 		$oid = trim($oid);
 
-		$query = "SELECT * FROM $this->_tbl WHERE alias=" . $this->_db->Quote($oid);
+		$query = "SELECT * FROM $this->_tbl WHERE state!=2 AND alias=" . $this->_db->Quote($oid);
 		if ($object_id !== null)
 		{
 			$query .= " AND object_id=" . $this->_db->Quote(intval($object_id));
