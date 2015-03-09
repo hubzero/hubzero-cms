a8205f2f8cd3854d727f74a234300f79260bd76a
Alissa Nedossekina <alisa@purdue.edu>
2015-03-09 11:08:48 -0400

    [PURR][#566][COM_PUBLICATIONS][1.3.1] another fix to archival notice

--- components/com_publications/helpers/html.php
+++ components/com_publications/helpers/html.php
@@ -1028,14 +1028,19 @@ class PublicationsHtml
 		// Show archival status (mkAIP)
 		if ($config->get('repository', 0))
 		{
-			if ($publication->doi && $publication->archived != '0000-00-00 00:00:00')
+			if ($publication->doi && $publication->archived && $publication->archived != '0000-00-00 00:00:00')
 			{
 				$text .= "\t\t".'<span class="archival-notice archived">'. JText::_('COM_PUBLICATIONS_VERSION_ARCHIVED_ON_DATE') . ' ' . JHTML::_('date', $publication->archived, $dateFormat, $tz) . "\n";
 			}
 			elseif ($config->get('graceperiod', 0))
 			{
 				$archiveDate  = $publication->accepted && $publication->accepted != '0000-00-00 00:00:00' ? JFactory::getDate($publication->accepted . '+1 month')->toSql() : NULL;
-				$text .= $archiveDate ? "\t\t".'<span class="archival-notice unarchived">'. JText::_('COM_PUBLICATIONS_VERSION_TO_BE_ARCHIVED') . ' ' . JHTML::_('date', $archiveDate, $dateFormat) . "\n" : '';
+
+				// Skip notice if archive date passed
+				if (strtotime($archiveDate) > strtotime(JFactory::getDate()))
+				{
+					$text .= $archiveDate ? "\t\t".'<span class="archival-notice unarchived">'. JText::_('COM_PUBLICATIONS_VERSION_TO_BE_ARCHIVED') . ' ' . JHTML::_('date', $archiveDate, $dateFormat) . "\n" : '';
+				}
 			}
 		}
 
