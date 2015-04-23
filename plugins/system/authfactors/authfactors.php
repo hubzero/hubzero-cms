008f81f9fc34459aa791260cc4b4e56973f7b3be
Sam Wilson <samwilson@purdue.edu>
2015-04-23 18:33:25 -0400

    [PLG_SYSTEM_AUTHFACTORS][COM_USERS] add initial ability to support factors on front end

--- plugins/system/authfactors/authfactors.php
+++ plugins/system/authfactors/authfactors.php
@@ -43,13 +43,16 @@ class plgSystemAuthfactors extends \Hubzero\Plugin\Plugin
 	 */
 	public function onAfterRoute()
 	{
-		if (App::isAdmin())
+		if (in_array(App::get('client')->id, $this->params->get('clients', [1])))
 		{
 			$exceptions = [
-				'com_login.logout'
+				'com_login.logout',
+				'com_users.logout'
 			];
 
-			$current = Request::getWord('option', '') . '.' . Request::getWord('task', '');
+			$current  = Request::getWord('option', '');
+			$current .= ($task = Request::getWord('task', false)) ? '.' . $task : '';
+			$current .= ($view = Request::getWord('view', false)) ? '.' . $view : '';
 
 			// If guest, proceed as normal and they'll land on the login page
 			if (!User::isGuest() && !in_array($current, $exceptions))
@@ -61,19 +64,62 @@ class plgSystemAuthfactors extends \Hubzero\Plugin\Plugin
 				{
 					// If not a guest, and auth factors checks are done and have failed,
 					// log out so we start over
-					Request::setVar('option', 'com_login');
-					Request::setVar('task', 'logout');
-
-					return false;
+					$logout = 'logout' . ucfirst(App::get('client')->alias);
+					self::$logout();
 				}
 				else if ($status === null)
 				{
 					// If not a guest, but no factor verification has been completed,
 					// procede with auth factor checks as applicable
-					Request::setVar('option', 'com_login');
-					Request::setVar('task', 'factors');
+					$factors = 'factors' . ucfirst(App::get('client')->alias);
+					self::$factors();
 				}
 			}
 		}
+	}
+
+	/**
+	 * Logs out of the admin client
+	 *
+	 * @return void
+	 **/
+	private function logoutAdmin()
+	{
+		Request::setVar('option', 'com_login');
+		Request::setVar('task', 'logout');
+	}
+
+	/**
+	 * Logs out of the site client
+	 *
+	 * @return void
+	 **/
+	private function logoutSite()
+	{
+		Request::setVar('option', 'com_users');
+		Request::setVar('task', 'user.logout');
+		Request::setVar('return', base64_encode('/'));
+	}
+
+	/**
+	 * Sends to factor input view
+	 *
+	 * @return void
+	 **/
+	private function factorsAdmin()
+	{
+		Request::setVar('option', 'com_login');
+		Request::setVar('task', 'factors');
+	}
+
+	/**
+	 * Sends to factor input view
+	 *
+	 * @return void
+	 **/
+	private function factorsSite()
+	{
+		Request::setVar('option', 'com_users');
+		Request::setVar('view', 'factors');
 	}
 }
