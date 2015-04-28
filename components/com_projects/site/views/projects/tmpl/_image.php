992ab1fd8a898c293ae9f1878c61b799046deea5
Alissa Nedossekina <alisa@purdue.edu>
2015-04-28 11:08:25 -0400

    [COM_PROJECTS] escape project title and change css for project image

--- components/com_projects/site/views/projects/tmpl/_image.php
+++ components/com_projects/site/views/projects/tmpl/_image.php
@@ -27,6 +27,6 @@ defined('_JEXEC') or die( 'Restricted access' );
 
 ?>
 	<div id="pimage" class="pimage">
-		<a href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias')); ?>" title="<?php echo $this->model->get('title') . ' - ' . Lang::txt('COM_PROJECTS_VIEW_UPDATES'); ?>"><img src="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&controller=media&media=master');  ?>" alt="<?php echo $this->model->get('title'); ?>" /></a>
+		<a href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias')); ?>" title="<?php echo $this->escape($this->model->get('title')) . ' - ' . Lang::txt('COM_PROJECTS_VIEW_UPDATES'); ?>"><img src="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&controller=media&media=master');  ?>" alt="<?php echo $this->model->get('title'); ?>" /></a>
 	</div>
 
