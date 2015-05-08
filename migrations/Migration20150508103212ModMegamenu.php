41e4c93c297ce796d24d1f6b6d163a98676fdd26
Shawn Rice <zooley@purdue.edu>
2015-05-08 06:33:54 -0400

    [MOD_MEGAMENU] Removing unused (never used?) deprecated module

---
+++ migrations/Migration20150508103212ModMegamenu.php
@@ -0,0 +1,28 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Hubzero!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for removing unused/deprecated mod_megamenu module
+ **/
+class Migration20150508103212ModMegamenu extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->deleteModuleEntry('mod_megamenu');
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->addModuleEntry('mod_megamenu', 1, '');
+	}
+}
