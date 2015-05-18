5dca2e3de0604b130cd1169477cf06faa90def30
Kevin Wojkovich <kevinw@purdue.edu>
2015-05-18 14:00:02 -0400

    [NANOHUB][#279673][COM_GROUPS] Corrects the behavior of the inherit group setting for comments.

--- components/com_groups/views/pages/tmpl/edit.php
+++ components/com_groups/views/pages/tmpl/edit.php
@@ -71,7 +71,7 @@ $parent    = $this->page->get('parent', 0);
 $groupParams = new JRegistry($this->group->get('params'));
 $groupCommentSetting = $groupParams->get('page_comments', $this->config->get('page_comments', 0));
 $groupCommentSettingString = ($groupCommentSetting == 1) ? 'Yes' : 'No';
-$comments  = intval($this->page->get('comments', $groupCommentSetting));
+$comments  = intval($this->page->get('comments', 3));
 
 // default some form vars
 $pageHeading = JText::_("COM_GROUPS_PAGES_ADD_PAGE");
@@ -236,7 +236,7 @@ if ($this->page->get('id'))
 						</select>
 						<span class="hint"><?php echo JText::_('COM_GROUPS_PAGES_PAGE_CATEGORY_HINT'); ?></span>
 					</label>
-					
+
 					<?php if ($this->page->get('home') == 0) : ?>
 						<label for="page-parent" class="page-parent-label">
 							<strong><?php echo JText::_('COM_GROUPS_PAGES_PAGE_PARENT'); ?>:</strong> <span class="optional"><?php echo JText::_('COM_GROUPS_FIELD_OPTIONAL'); ?></span>
@@ -259,7 +259,7 @@ if ($this->page->get('id'))
 							<strong><?php echo JText::_('COM_GROUPS_PAGES_PAGE_ORDER'); ?>:</strong> <span class="optional"><?php echo JText::_('COM_GROUPS_FIELD_OPTIONAL'); ?></span>
 							<select name="page[left]" class="page-ordering fancy-select">
 								<?php foreach ($this->pages as $page) : ?>
-									
+
 									<?php $sel = ($page->get('title') == $title) ? 'selected="selected"' : ''; ?>
 									<option <?php echo $sel; ?> data-parent="<?php echo $page->get('parent'); ?>" value="<?php echo $page->get('lft'); ?>">
 										<?php echo $page->get('lft') . ' ' . $page->get('title'); ?>
@@ -275,14 +275,14 @@ if ($this->page->get('id'))
 					<label>
 						<strong><?php echo JText::_('COM_GROUPS_PAGES_PAGE_COMMENTS'); ?>:</strong> <span class="optional"><?php echo JText::_('COM_GROUPS_FIELD_OPTIONAL'); ?></span>
 						<select name="page[comments]" class="fancy-select">
-							<option><?php echo JText::sprintf('COM_GROUPS_PAGES_PAGE_COMMENTS_INHERIT', $groupCommentSettingString); ?></option>
+							<option value="3" <?php if ($comments === 3) { echo "selected"; } ?>><?php echo JText::sprintf('COM_GROUPS_PAGES_PAGE_COMMENTS_INHERIT', $groupCommentSettingString); ?></option>
 							<option value="0" <?php if ($comments === 0) { echo "selected"; } ?>><?php echo JText::_('COM_GROUPS_PAGES_PAGE_COMMENTS_NO'); ?></option>
 							<option value="1" <?php if ($comments === 1) { echo "selected"; } ?>><?php echo JText::_('COM_GROUPS_PAGES_PAGE_COMMENTS_YES'); ?></option>
 							<option value="2" <?php if ($comments === 2) { echo "selected"; } ?>><?php echo JText::_('COM_GROUPS_PAGES_PAGE_COMMENTS_LOCK'); ?></option>
 						</select>
 						<span class="hint"><?php echo JText::_('COM_GROUPS_PAGES_PAGE_COMMENTS_HINT'); ?></span>
 					</label>
-					
+
 					<?php if ($this->group->isSuperGroup() && count($this->pageTemplates) > 0) : ?>
 						<hr class="divider" />
 
