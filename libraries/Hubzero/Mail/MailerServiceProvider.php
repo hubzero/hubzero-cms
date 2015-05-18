25f80d2cc5226adfb5756a0e5b6ea90737f4bd02
Sam Wilson <samwilson@purdue.edu>
2015-05-18 14:13:29 -0400

    [LIB_HUBZERO] add a mailer service provider

---
+++ libraries/Hubzero/Mail/MailerServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Sam Wilson <samwilson@purdue.edu>
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+namespace Hubzero\Mail;
+
+use Hubzero\Base\ServiceProvider;
+
+/**
+ * Mail service provider
+ */
+class MailerServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register the service provider
+	 *
+	 * @return  void
+	 */
+	public function register()
+	{
+		$this->app['mailer'] = $this->app->factory(function($app)
+		{
+			return new Message();
+		});
+	}
+
+	/**
+	 * Add the transporters to the message
+	 *
+	 * @return  void
+	 */
+	public function boot()
+	{
+		if ($this->app->has('dispatcher'))
+		{
+			$this->app['dispatcher']->trigger('mail.onMailersRegister');
+		}
+	}
+}
