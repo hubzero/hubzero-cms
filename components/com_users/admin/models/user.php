7faa4afa8e7901227272751da435b7444a70feca
Shawn Rice <zooley@purdue.edu>
2015-05-20 16:34:37 -0400

    [COM_USERS] Fixing bug that would prevent logging out of user when they're blocked

--- components/com_users/admin/models/user.php
+++ components/com_users/admin/models/user.php
@@ -346,7 +346,7 @@ class UsersModelUser extends JModelAdmin
 
 				// Prepare the logout options.
 				$options = array(
-					'clientid' => array(0, 1)
+					'clientid' => 0
 				);
 
 				if ($allow)
