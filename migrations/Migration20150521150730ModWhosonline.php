36752614acf26bb0925e8a1153ea6119013f0ff9
Shawn Rice <zooley@purdue.edu>
2015-05-21 11:09:33 -0400

    [HUBZERO][#8003][MOD_WHOSONLINE] Make sure admin mod_whosonline module has an entry in the extensions table

---
+++ migrations/Migration20150521150730ModWhosonline.php
@@ -0,0 +1,45 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for adding extension entry for admin mod_whosonline
+ **/
+class Migration20150521150730ModWhosonline extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__extensions'))
+		{
+			$query = "SELECT `extension_id` FROM `#__extensions` WHERE `element` = 'mod_whosonline' AND `client_id` = 1";
+			$this->db->setQuery($query);
+			$id = $this->db->loadResult();
+
+			if (!$id)
+			{
+				$this->addModuleEntry('mod_whosonline', 1, '', 1);
+			}
+			else
+			{
+				// Set the first zone as default
+				$query = "UPDATE `#__extensions` SET `state`=0 WHERE `extension_id` = " . $this->db->quote($id);
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->deleteModuleEntry('mod_whosonline', 1);
+	}
+}
