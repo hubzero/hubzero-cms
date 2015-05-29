2773ca965bc06db4c277f8113d624a31577753d4
Shawn Rice <zooley@purdue.edu>
2015-05-29 14:39:37 -0400

    [COM_MENUS] Fixing path to .sys language file

--- components/com_menus/admin/views/items/view.html.php
+++ components/com_menus/admin/views/items/view.html.php
@@ -62,7 +62,8 @@ class MenusViewItems extends JViewLegacy
 				default:
 					// load language
 						$lang->load($item->componentname . '.sys', JPATH_ADMINISTRATOR, null, false, true)
-					||	$lang->load($item->componentname . '.sys', JPATH_ADMINISTRATOR . '/components/' . $item->componentname, null, false, true);
+					||	$lang->load($item->componentname . '.sys', JPATH_ADMINISTRATOR . '/components/' . $item->componentname, null, false, true)
+					||	$lang->load($item->componentname . '.sys', PATH_CORE . '/components/' . $item->componentname . '/admin', null, false, true);
 
 					if (!empty($item->componentname))
 					{
