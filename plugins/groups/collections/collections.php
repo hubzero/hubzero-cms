e7e73c5c2720348fc4e964c2588e5b9d7536a1f7
Shawn Rice <zooley@purdue.edu>
2015-05-29 07:57:33 -0400

    [HUBZERO][#8035][PLG_GROUPS_COLLECTIONS] Adjusting routing to take into account menu aliased groups

--- plugins/groups/collections/collections.php
+++ plugins/groups/collections/collections.php
@@ -258,10 +258,22 @@ class plgGroupsCollections extends \Hubzero\Plugin\Plugin
 			{
 				$path = str_replace(Request::base(true), '', $path);
 				$path = str_replace('index.php', '', $path);
-				$path = '/' . trim($path, '/');
-				$path = str_replace('/groups/' . $this->group->get('cn') . '/' . $this->_name, '', $path);
-				$path = ltrim($path, '/');
-				$bits = explode('/', $path);
+				$path = trim($path, '/');
+				$parts = explode('/', $path);
+				$start = false;
+				$bits = array();
+				foreach ($parts as $p)
+				{
+					if ($p == $this->_name)
+					{
+						$start = true;
+						continue;
+					}
+					if ($start)
+					{
+						$bits[] = $p;
+					}
+				}
 
 				if (isset($bits[0]) && $bits[0])
 				{
