551814e81daf6194789937fec9bab3555e7631a4
Shawn Rice <zooley@purdue.edu>
2015-06-04 16:03:58 -0400

    [LIB_HUBZERO] Load base language file

--- libraries/Hubzero/Language/TranslationServiceProvider.php
+++ libraries/Hubzero/Language/TranslationServiceProvider.php
@@ -54,4 +54,79 @@ class TranslationServiceProvider extends ServiceProvider
 
 		$this->app['language.filter'] = false;
 	}
+
+	/**
+	 * Add the plugin loader to the event dispatcher.
+	 *
+	 * @return  void
+	 */
+	public function boot()
+	{
+		$translator = $this->app['language'];
+
+		$language = null;
+
+		// If a language was specified it has priority
+		if (!$language && $this->app->has('request') && $this->app->isSite())
+		{
+			$lang = $this->app['request']->getString('language', null);
+
+			if ($lang && $translator->exists($lang))
+			{
+				$language = $lang;
+			}
+		}
+
+		// Detect user specified language
+		if (!$language && $this->app->has('user'))
+		{
+			$lang = \User::getParam($this->app['client']->alias . '_language');
+
+			if ($lang && $translator->exists($lang))
+			{
+				$language = $lang;
+			}
+		}
+
+		// Detect browser language
+		if (!$language && $this->app->has('browser') && $this->app->isSite())
+		{
+			$lang = $translator->detectLanguage();
+
+			if ($lang && $translator->exists($lang))
+			{
+				$language = $lang;
+			}
+		}
+
+		// Detect default language
+		if (!$language && $this->app->has('component'))
+		{
+			$params = $this->app['component']->params('com_languages');
+
+			$language = $params->get(
+				$this->app['client']->name,
+				$this->app['config']->get('language', 'en-GB')
+			);
+		}
+
+		// One last check to make sure we have something
+		if (!$language || !$translator->exists($language))
+		{
+			$lang = $this->app['config']->get('language', 'en-GB');
+
+			if ($translator->exists($lang))
+			{
+				$language = $lang;
+			}
+		}
+
+		if ($language)
+		{
+			$translator->setLanguage($language);
+		}
+
+		$translator->load('lib_joomla', JPATH_BASE, null, false, true) ||
+		$translator->load('lib_joomla', JPATH_ADMINISTRATOR, null, false, true);
+	}
 }
