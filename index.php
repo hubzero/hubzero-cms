d6049f206951cf195edb37122454174c86b5ed84
Shawn Rice <zooley@purdue.edu>
2015-06-06 14:24:56 -0400

    [CORE] Adding initial app directory.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Define directories
+|--------------------------------------------------------------------------
+|
+| First thing we need to do is set some constants for the app's directory
+| and the path to the parent directory containing the app and core.
+|
+*/
+
+// Set some needed defines
+define('PATH_APP', __DIR__);
+define('PATH_ROOT', dirname(__DIR__));
+
+// Load the webroot application
+require_once PATH_ROOT . DS . 'index.php';
