f09540bb2d269cf1594d8b46c79c974d5e2ca008
Shawn Rice <zooley@purdue.edu>
2015-06-08 11:35:39 -0400

    [COM_INSTALLER] Fixing namespacing issue

--- components/com_installer/admin/controllers/database.php
+++ components/com_installer/admin/controllers/database.php
@@ -31,9 +31,11 @@
 namespace Components\Installer\Admin\Controllers;
 
 use Hubzero\Component\AdminController;
-use Components\Installer\Admin\Models\Database;
+use Components\Installer\Admin\Models;
 use Route;
 use App;
+
+include_once(dirname(__DIR__) . DS . 'models' . DS . 'database.php');
 
 /**
  * Controller for database
@@ -41,13 +43,63 @@ use App;
 class Database extends AdminController
 {
 	/**
+	 * Display a list of uninstalled extensions
+	 *
+	 * @return  void
+	 */
+	public function displayTask()
+	{
+		$model = new Models\Database();
+
+		// Get data from the model
+		$this->view->state         = $model->getState();
+		$this->view->changeSet     = $model->getItems();
+		$this->view->errors        = $this->view->changeSet->check();
+		$this->view->results       = $this->view->changeSet->getStatus();
+		$this->view->schemaVersion = $this->getSchemaVersion();
+		$this->view->updateVersion = $this->getUpdateVersion();
+		$this->view->filterParams  = $this->getDefaultTextFilters();
+		$this->view->schemaVersion = ($this->view->schemaVersion) ?  $this->view->schemaVersion : Lang::txt('JNONE');
+		$this->view->updateVersion = ($this->view->updateVersion) ?  $this->view->updateVersion : Lang::txt('JNONE');
+		$this->view->pagination    = $model->getPagination();
+		$this->view->errorCount    = count($this->errors);
+
+		$errors = count($this->view->errors);
+		if ($this->view->schemaVersion != $this->view->changeSet->getSchema())
+		{
+			$this->view->errorCount++;
+		}
+		if (!$this->view->filterParams)
+		{
+			$this->view->errorCount++;
+		}
+		if (version_compare($this->view->updateVersion, JVERSION) != 0)
+		{
+			$this->view->errorCount++;
+		}
+
+		$this->view->ftp = \JClientHelper::setCredentialsFromRequest('ftp');
+
+		$showMessage = false;
+		if (is_object($this->view->state))
+		{
+			$message1    = $this->view->state->get('message');
+			$message2    = $this->view->state->get('extension_message');
+			$showMessage = ($message1 || $message2);
+		}
+		$this->view->showMessage = $showMessage;
+
+		$this->view->display();
+	}
+
+	/**
 	 * Tries to fix missing database updates
 	 *
 	 * @return  void
 	 */
 	public function fixTask()
 	{
-		$model = new Database();
+		$model = new Models\Database();
 		$model->fix();
 
 		App::redirect(
