f50fa13d49481f268ce15e641f74aad9bbbd4641
Shawn Rice <zooley@purdue.edu>
2015-06-11 11:47:07 -0400

    [MOD_LANGUAGES] Moving assets to module's directory

--- modules/mod_languages/tmpl/default.php
+++ modules/mod_languages/tmpl/default.php
@@ -31,7 +31,7 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-Html::asset('stylesheet', 'mod_languages/template.css', array(), true);
+$this->css('template.css');
 ?>
 <div class="mod-languages<?php echo $moduleclass_sfx ?>">
 <?php if ($headerText) : ?>
@@ -39,29 +39,29 @@ Html::asset('stylesheet', 'mod_languages/template.css', array(), true);
 <?php endif; ?>
 
 <?php if ($params->get('dropdown', 1)) : ?>
-	<form name="lang" method="post" action="<?php echo htmlspecialchars(JURI::current()); ?>">
-		<select class="inputbox" onchange="document.location.replace(this.value);" >
-		<?php foreach ($list as $language): ?>
-			<option dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr'; ?>" value="<?php echo $language->link;?>" <?php echo $language->active ? 'selected="selected"' : ''?>>
-			<?php echo $language->title_native;?></option>
-		<?php endforeach; ?>
+	<form name="lang" method="post" action="<?php echo htmlspecialchars(Request::current()); ?>">
+		<select class="inputbox" onchange="document.location.replace(this.value);">
+			<?php foreach ($list as $language): ?>
+				<option dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr'; ?>" value="<?php echo $language->link;?>" <?php echo $language->active ? 'selected="selected"' : ''?>>
+				<?php echo $language->title_native;?></option>
+			<?php endforeach; ?>
 		</select>
 	</form>
 <?php else : ?>
 	<ul class="<?php echo $params->get('inline', 1) ? 'lang-inline' : 'lang-block';?>">
-	<?php foreach ($list as $language): ?>
-		<?php if ($params->get('show_active', 0) || !$language->active):?>
-			<li class="<?php echo $language->active ? 'lang-active' : '';?>" dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr' ?>">
-			<a href="<?php echo $language->link;?>">
-			<?php if ($params->get('image', 1)):?>
-				<?php echo Html::asset('image', 'mod_languages/' . $language->image . '.gif', $language->title_native, array('title'=>$language->title_native), true);?>
-			<?php else : ?>
-				<?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef);?>
-			<?php endif; ?>
-			</a>
-			</li>
-		<?php endif;?>
-	<?php endforeach;?>
+		<?php foreach ($list as $language): ?>
+			<?php if ($params->get('show_active', 0) || !$language->active):?>
+				<li class="<?php echo $language->active ? 'lang-active' : '';?>" dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr' ?>">
+					<a href="<?php echo $language->link;?>">
+						<?php if ($params->get('image', 1)):?>
+							<img src="<?php echo $this->img($language->image . '.gif'); ?>" alt="<?php echo $language->title_native; ?>" />
+						<?php else : ?>
+							<?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef);?>
+						<?php endif; ?>
+					</a>
+				</li>
+			<?php endif;?>
+		<?php endforeach;?>
 	</ul>
 <?php endif; ?>
 
