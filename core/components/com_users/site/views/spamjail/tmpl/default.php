40736bb76659db9066651dbde2a34f7f1672a66b
Sam Wilson <samwilson@purdue.edu>
2015-06-24 10:46:57 -0400

    [COM_USERS] add spamjail view

---
+++ core/components/com_users/site/views/spamjail/tmpl/default.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Sam Wilson <samwilson@purdue.edu>
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// No direct access.
+defined('_HZEXEC_') or die();
+
+$params = Plugin::params('system', 'spamjail');
+?>
+
+<header id="content-header">
+	<h2><?php echo Lang::txt('COM_USERS_SPAM_DETECTED'); ?></h2>
+</header>
+
+<section class="section">
+	<p><?php echo Lang::txt('COM_USERS_SPAM_MESSAGE'); ?></p>
+
+	<?php if ($video = $params->get('spam_video', false)) : ?>
+		<p><?php echo Lang::txt('COM_USERS_SPAM_VIDEO'); ?></p>
+		<div class="video" style="text-align:center;">
+			<iframe width="420" height="315" src="https://www.youtube.com/embed/<?php echo $video; ?>" frameborder="0" allowfullscreen></iframe>
+		</div>
+	<?php endif; ?>
+</section>
