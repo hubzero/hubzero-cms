3630bfdaab6d1ccc795b861a2aa43fbed7b67daa
Sam Wilson <samwilson@purdue.edu>
2015-06-29 11:40:14 -0400

    [CORE] cli wants to use words too!

---
+++ core/bootstrap/cli/language/en-GB/en-GB.localise.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+/**
+ * en-GB localise class
+ */
+abstract class en_GBLocalise
+{
+	/**
+	 * Returns the potential suffixes for a specific number of items
+	 *
+	 * @param   integer  $count  The number of items.
+	 * @return  array    An array of potential suffixes.
+	 */
+	public static function getPluralSuffixes($count)
+	{
+		if ($count == 0)
+		{
+			$return =  array('0');
+		}
+		elseif ($count == 1)
+		{
+			$return =  array('1');
+		}
+		else
+		{
+			$return = array('MORE');
+		}
+		return $return;
+	}
+
+	/**
+	 * Returns the ignored search words
+	 *
+	 * @return  array  An array of ignored search words.
+	 */
+	public static function getIgnoredSearchWords()
+	{
+		$search_ignore = array(
+			'and',
+			'in',
+			'on'
+		);
+
+		return $search_ignore;
+	}
+
+	/**
+	 * Returns the lower length limit of search words
+	 *
+	 * @return  integer  The lower length limit of search words.
+	 */
+	public static function getLowerLimitSearchWord()
+	{
+		return 3;
+	}
+
+	/**
+	 * Returns the upper length limit of search words
+	 *
+	 * @return  integer  The upper length limit of search words.
+	 */
+	public static function getUpperLimitSearchWord()
+	{
+		return 20;
+	}
+
+	/**
+	 * Returns the number of chars to display when searching
+	 *
+	 * @return  integer  The number of chars to display when searching.
+	 */
+	public static function getSearchDisplayedCharactersNumber()
+	{
+		return 200;
+	}
+}
