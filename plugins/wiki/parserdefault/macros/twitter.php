d6b36728e46d97985fe572f5f7d3e9a3d7964b03
Zach Weidner <zach.weidner@gmail.com>
2015-07-02 14:30:12 -0400

    [HUBZERO][#8141][PLG_HTMLPARSER] Update the HTML and Wiki macros for Twitter to support widget IDs from Twitter.

--- plugins/wiki/parserdefault/macros/twitter.php
+++ plugins/wiki/parserdefault/macros/twitter.php
@@ -45,7 +45,8 @@ class TwitterMacro extends WikiMacro
 	{
 		$txt = array();
 		$txt['wiki'] = "Embeds a Twitter Feed into the page";
-		$txt['html'] = '<p>Embeds a Twitter Feed into the page. Can be a user feed(@hubzero) or search by trend(#hubzero), followed by a comma(,) and then the number of tweets to display.</p>
+		$txt['html'] = '<p>Embeds a Twitter Feed into the page. Can be a user feed(@hubzero) or a Twitter widget ID(see below), followed by a comma(,) and then the number of tweets to display.</p>
+						<p>A Twitter widget may be set up by going to <a href="https://www.twitter.com/settings/widgets">Twitter\'s widget configuration page</a>.  After setting a widget up, the widget ID can be found as an 18 digit number in the URL of the settings, or within the HTML in the text box labeled "Copy and paste the code into the HTML of your site" as the item data-widget-id="####".</p>
 						<p>Examples:</p>
 						<ul>
 							<li><code>[[Twitter(@hubzeroplatform,2)]]</code></li>
@@ -71,26 +72,56 @@ class TwitterMacro extends WikiMacro
 	public function render()
 	{
 		//get the args passed in
-		$args = explode(',', $this->args);
+		$args = array_map('trim', explode(',', $this->args));
 
 		//get screen name & num tweets
 		$screenName = (isset($args[0])) ? ltrim($args[0], '@') : '';
-		$numItems   = (isset($args[1]) && is_numeric($args[1])) ? $args[1] : 3;
+		$widgetId   = (preg_match('/widgetid="([^"]*)"/', $this->args, $matches)) ? $matches[1] : '';
+		$chrome     = (preg_match('/chrome="([^"]*)"/', $this->args, $matches)) ? $matches[1] : '';
+		$width      = (preg_match('/width="([^"]*)"/', $this->args, $matches)) ? $matches[1] : '100%';
+		$height     = (preg_match('/height="([^"]*)"/', $this->args, $matches)) ? $matches[1] : 500;
 
 		//make sure we have a user name
-		if ($screenName == '')
+		if ($screenName == '' && $widgetId == '' || strpos($screenName, '#') !== FALSE)
 		{
-			return "(Please enter a valid Twitter Username or ID)";
+			return '(Please enter a valid Twitter Username/ID or Widget ID)';
+		}
+
+		// code for screename
+		$atts   = array();
+		$atts[] = 'data-widget-id="346714310770302976"';
+
+		// twitter does not allow numeric usernames, this must be a widget id
+		if (is_numeric($screenName))
+		{
+			$atts = array('data-widget-id="' . $screenName . '"');
+			$screenName = '';
+		}
+
+		// pass already configured widget
+		if ($widgetId)
+		{
+			$atts = array('data-widget-id="' . $widgetId . '"');
+		}
+		//no widget id, set up the screen name to show tweets from
+		else
+		{
+			$atts[] = 'href="https://twitter.com/'. $screenName . '"';
+			$atts[] = 'data-screen-name="' . $screenName . '"';
+		}
+
+
+		$atts[] = 'width="' . $width . '"';
+		$atts[] = 'height="' . $height . '"';
+		$atts[] = 'data-chrome="' . $chrome . '"';
+
+		if (isset($args[1]) && is_numeric($args[1]))
+		{
+			$atts[] = 'data-tweet-limit="' . $args[1] . '"';
 		}
 
 		//output embeded timeline
-		return '<a class="twitter-timeline"
-					href="https://twitter.com/'. $screenName .'"
-					data-widget-id="346714310770302976"
-					data-screen-name="' . $screenName . '"
-					data-tweet-limit="' . trim($numItems) . '"
-					data-chrome=""
-					>Loading Tweets...</a>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+		return '<a class="twitter-timeline" ' . implode(' ', $atts) . '>Loading Tweets...</a>
+				<script>!function(d,s,id) {var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if (!d.getElementById(id)) {js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 	}
 }
