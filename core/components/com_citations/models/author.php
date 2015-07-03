38131fd3792497589830b9a41215465a9fd19839
Kevin Wojkovich <kevinw@purdue.edu>
2015-07-03 15:42:54 -0400

    [COM_CITATIONS] Initial commit of Relational Models for citations.

---
+++ core/components/com_citations/models/author.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Kevin Wojkovich <kevinw@purdue.edu>
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ * @since     Class available since release 1.3.2
+ */
+
+namespace Components\Citations\Models;
+
+use Hubzero\Database\Relational;
+use Hubzero\Utility\String;
+use Hubzero\Base\Object;
+
+/**
+ * Hubs database model
+ *
+ * @uses \Hubzero\Database\Relational
+ */
+class Author extends Relational
+{
+	/**
+	 * The table namespace
+	 *
+	 * @var string
+	 **/
+	protected $namespace = 'citations';
+    // table name jos_citations
+
+	/**
+	 * Default order by for model
+	 *
+	 * @var string
+	 **/
+	public $orderBy = 'name';
+
+	/**
+	 * Fields and their validation criteria
+	 *
+	 * @var array
+	 **/
+	protected $rules = array(
+		//'name'    => 'notempty',
+		//'liaison' => 'notempty'
+	);
+
+	/**
+	 * Automatically fillable fields
+	 *
+	 * @var array
+	 **/
+	public $always = array(
+		//'name_normalized',
+		//'asset_id'
+	);
+
+
+    /**
+	 * Defines the inverse relationship between a record and a task
+	 *
+	 * @return \Hubzero\Database\Relationship\belongsToOne
+	 * @author
+	 **/
+	public function citation()
+	{
+		return $this->belongsToOne('Citation', 'id', 'cid');
+        //public function belongsToOne($model, $thisKey=null, $parentKey=null)
+
+	}
+}
+
+/**
+ * Retrieves a belongs to one model relationship
+ *
+ * @param  string $model the name of the model to relate to the current one
+ * @param  string|null $thisKey the local key used to associate the many back to the model
+ * @param  string|null $parentKey the parent key used to associate the model to its parent
+ * @return \Hubzero\Database\Relationship\BelongsToOne
+ * @since  1.3.2
+ **/
