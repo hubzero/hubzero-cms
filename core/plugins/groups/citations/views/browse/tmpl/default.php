bc75ee151c844572b1899c4e4acce190d286ec88
Kevin Wojkovich <kevinw@purdue.edu>
2015-07-07 14:31:37 -0400

    [PLG_GROUPS_CITATIONS] Clean up whitespaces.

--- core/plugins/groups/citations/views/browse/tmpl/default.php
+++ core/plugins/groups/citations/views/browse/tmpl/default.php
@@ -23,7 +23,7 @@
  * HUBzero is a registered trademark of Purdue University.
  *
  * @package   hubzero-cms
- * @author    Shawn Rice <zooley@purdue.edu>, Kevin Wojkovich <kevinw@purdue.edu>
+ * @author	Shawn Rice <zooley@purdue.edu>, Kevin Wojkovich <kevinw@purdue.edu>
  * @copyright Copyright 2005-2014 Purdue University. All rights reserved.
  * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
@@ -32,7 +32,7 @@
 defined('_HZEXEC_') or die();
 
 $this->css('citations.css')
-     ->js();
+	 ->js();
 
 $base = 'index.php?option=com_groups&cn=' . $this->group->get('cn') . '&active=citations';
 
@@ -50,29 +50,29 @@ if (isset($this->messages))
 		<a class="btn icon-add" href="<?php echo Route::url($base. '&action=add'); ?>">
 			<?php echo Lang::txt('PLG_GROUPS_CITATIONS_SUBMIT_CITATION'); ?>
 		</a>
-        <a class="btn icon-upload" href="<?php echo Route::url($base. '&action=import'); ?>">
-            <?php echo Lang::txt('PLG_GROUPS_CITATIONS_IMPORT_CITATION'); ?>
-        </a>
-        <a class="btn icon-settings" href="<?php echo Route::url($base. '&action=import'); ?>">
-            <?php echo Lang::txt('PLG_GROUPS_CITATIONS_SET_FORMAT'); ?>
-        </a>
+		<a class="btn icon-upload" href="<?php echo Route::url($base. '&action=import'); ?>">
+			<?php echo Lang::txt('PLG_GROUPS_CITATIONS_IMPORT_CITATION'); ?>
+		</a>
+		<a class="btn icon-settings" href="<?php echo Route::url($base. '&action=import'); ?>">
+			<?php echo Lang::txt('PLG_GROUPS_CITATIONS_SET_FORMAT'); ?>
+		</a>
 	<?php endif; ?>
 </div><!-- / Citations management buttons -->
 
 <div class="frm" id="browsebox"><!-- .frm #browsebox -->
-    <form action="<?php echo Route::url(Request::current()); ?>" id="citeform" method="GET" class="withBatchDownload">
-        <section class="main section"> <!-- .main .section -->
-            <div class="subject">
-                <div class="container data-entry"> <!-- citation search box -->
-                    <!-- @TODO replace with hubgraph -->
-                    <input class="entry-search-submit" type="submit" value="Search" /> <!-- search button -->
-                    <fieldset class="entry-search"> <!-- text box container -->
-                        <legend><?php echo Lang::txt('PLG_GROUPS_CITATIONS_SEARCH_CITATIONS'); ?></legend>
-                        <input type="text" name="search" id="entry-search-field" value="<?php echo $this->escape($this->filters['search']); ?>" placeholder="<?php echo Lang::txt('PLG_GROUPS_CITATIONS_SEARCH_CITATIONS_PLACEHOLDER'); ?>" />
-                    </fieldset>
-                </div><!-- /.container .data-entry -->
+	<form action="<?php echo Route::url(Request::current()); ?>" id="citeform" method="GET" class="withBatchDownload">
+		<section class="main section"> <!-- .main .section -->
+			<div class="subject">
+				<div class="container data-entry"> <!-- citation search box -->
+					<!-- @TODO replace with hubgraph -->
+					<input class="entry-search-submit" type="submit" value="Search" /> <!-- search button -->
+					<fieldset class="entry-search"> <!-- text box container -->
+						<legend><?php echo Lang::txt('PLG_GROUPS_CITATIONS_SEARCH_CITATIONS'); ?></legend>
+						<input type="text" name="search" id="entry-search-field" value="<?php echo $this->escape($this->filters['search']); ?>" placeholder="<?php echo Lang::txt('PLG_GROUPS_CITATIONS_SEARCH_CITATIONS_PLACEHOLDER'); ?>" />
+					</fieldset>
+				</div><!-- /.container .data-entry -->
 
-                <div class="container"> <!-- .container for citation type (aff, nonaff, all) -->
+				<div class="container"> <!-- .container for citation type (aff, nonaff, all) -->
 					<ul class="entries-menu filter-options">
 						<?php
 							$queryString = "";
@@ -99,143 +99,143 @@ if (isset($this->messages))
 						<li><a <?php if ($this->filters['filter'] == 'aff') { echo 'class="active"'; } ?> href="<?php echo Route::url($base . '&action=browse'.$queryString.'&filter=aff'); ?>"><?php echo Lang::txt('PLG_GROUPS_CITATIONS_AFFILIATED'); ?></a></li>
 						<li><a <?php if ($this->filters['filter'] == 'nonaff') { echo 'class="active"'; } ?> href="<?php echo Route::url($base . '&action=browse'.$queryString.'&filter=nonaff'); ?>"><?php echo Lang::txt('PLG_GROUPS_CITATIONS_NONAFFILIATED'); ?></a></li>
 					</ul>
-                <div class="clearfix"></div> <!-- clearfix for spacing -->
-                <?php if (count($this->citations) > 0) : ?>
-                    <?php
-                        $formatter = new \Components\Citations\Helpers\Format();
-                        $formatter->setTemplate($this->citationTemplate);
+				<div class="clearfix"></div> <!-- clearfix for spacing -->
+				<?php if (count($this->citations) > 0) : ?>
+					<?php
+						$formatter = new \Components\Citations\Helpers\Format();
+						$formatter->setTemplate($this->citationTemplate);
 
-                        // Fixes the counter so it starts counting at the current citation number instead of restarting on 1 at every page
-                        $counter = $this->filters['start'] + 1;
-                        if ($counter == '')
-                        {
-                            $counter = 1;
-                        }
-                    ?> <!-- end counter and formatter logic -->
-                    <table class="citations entries">
-                        <thead>
-                            <tr>
-                                <th class="batch">
-                                    <input type="checkbox" class="checkall-download" />
-                                </th>
-                                <th colspan="3"><?php echo Lang::txt('PLG_GROUPS_CITATIONS'); ?></th>
-                            </tr>
-                            <?php if ($this->isManager) : ?>
-                                <tr class="hidden"></tr>
-                            <?php endif; ?>
-                        </thead>
-                        <tbody>
-                            <?php $x = 0; ?>
-                            <?php foreach ($this->citations as $cite) : ?>
-                                <tr>
-                                    <td class="batch">
-                                        <input type="checkbox" class="download-marker" name="download_marker[]" value="<?php echo $cite->id; ?>" />
-                                    </td>
-                                    <?php if ($this->label != "none") : ?>
-                                        <td class="citation-label <?php echo $this->citations_label_class; ?>">
-                                            <?php
-                                                $type = "";
-                                                foreach ($this->types as $t) {
-                                                    if ($t['id'] == $cite->type) {
-                                                        $type = $t['type_title'];
-                                                    }
-                                                }
-                                                $type = ($type != "") ? $type : "Generic";
+						// Fixes the counter so it starts counting at the current citation number instead of restarting on 1 at every page
+						$counter = $this->filters['start'] + 1;
+						if ($counter == '')
+						{
+							$counter = 1;
+						}
+					?> <!-- end counter and formatter logic -->
+					<table class="citations entries">
+						<thead>
+							<tr>
+								<th class="batch">
+									<input type="checkbox" class="checkall-download" />
+								</th>
+								<th colspan="3"><?php echo Lang::txt('PLG_GROUPS_CITATIONS'); ?></th>
+							</tr>
+							<?php if ($this->isManager) : ?>
+								<tr class="hidden"></tr>
+							<?php endif; ?>
+						</thead>
+						<tbody>
+							<?php $x = 0; ?>
+							<?php foreach ($this->citations as $cite) : ?>
+								<tr>
+									<td class="batch">
+										<input type="checkbox" class="download-marker" name="download_marker[]" value="<?php echo $cite->id; ?>" />
+									</td>
+									<?php if ($this->label != "none") : ?>
+										<td class="citation-label <?php echo $this->citations_label_class; ?>">
+											<?php
+												$type = "";
+												foreach ($this->types as $t) {
+													if ($t['id'] == $cite->type) {
+														$type = $t['type_title'];
+													}
+												}
+												$type = ($type != "") ? $type : "Generic";
 
-                                                switch ($this->label)
-                                                {
-                                                    case "number":
-                                                        echo "<span class=\"number\">{$counter}.</span>";
-                                                        break;
-                                                    case "type":
-                                                        echo "<span class=\"type\">{$type}</span>";
-                                                        break;
-                                                    case "both":
-                                                        echo "<span class=\"number\">{$counter}. </span>";
-                                                        echo "<span class=\"type\">{$type}</span>";
-                                                        break;
-                                                }
-                                            ?>
-                                        </td>
-                                    <?php endif; ?>
-                                    <td class="citation-container">
-                                        <?php
-                                            $formatted = $cite->formatted
-                                                ? $cite->formatted
-                                                : $formatter->formatCitation($cite,
-                                                    $this->filters['search'], $this->coins, $this->config);
+												switch ($this->label)
+												{
+													case "number":
+														echo "<span class=\"number\">{$counter}.</span>";
+														break;
+													case "type":
+														echo "<span class=\"type\">{$type}</span>";
+														break;
+													case "both":
+														echo "<span class=\"number\">{$counter}. </span>";
+														echo "<span class=\"type\">{$type}</span>";
+														break;
+												}
+											?>
+										</td>
+									<?php endif; ?>
+									<td class="citation-container">
+										<?php
+											$formatted = $cite->formatted
+												? $cite->formatted
+												: $formatter->formatCitation($cite,
+													$this->filters['search'], $this->coins, $this->config);
 
-                                            if ($cite->doi)
-                                            {
-                                                $formatted = str_replace('doi:' . $cite->doi,
-                                                    '<a href="' . $cite->url . '" rel="external">'
-                                                    . 'doi:' . $cite->doi . '</a>', $formatted);
-                                            }
+											if ($cite->doi)
+											{
+												$formatted = str_replace('doi:' . $cite->doi,
+													'<a href="' . $cite->url . '" rel="external">'
+													. 'doi:' . $cite->doi . '</a>', $formatted);
+											}
 
-                                            echo $formatted; ?>
-                                        <?php
-                                            //get this citations rollover param
-                                            $params = new JParameter($cite->params);
-                                            $citation_rollover = 0;
-                                        ?>
-                                        <?php if ($citation_rollover && $cite->abstract != "") : ?>
-                                            <div class="citation-notes">
-                                                <?php
-                                                    $cs = new \Components\Citations\Tables\Sponsor($this->database);
-                                                    $sponsors = $cs->getCitationSponsor($cite->id);
-                                                    $final = "";
-                                                    if ($sponsors)
-                                                    {
-                                                        foreach ($sponsors as $s)
-                                                        {
-                                                            $sp = $cs->getSponsor($s);
-                                                            if ($sp)
-                                                            {
-                                                                $final .= '<a rel="external" href="'.$sp[0]['link'].'">'.$sp[0]['sponsor'].'</a>, ';
-                                                            }
-                                                        }
-                                                    }
-                                                ?>
-                                                <?php if ($final != '' && $this->config->get("citation_sponsors", "yes") == 'yes') : ?>
-                                                    <?php $final = substr($final, 0, -2); ?>
-                                                    <p class="sponsor"><?php echo Lang::txt('PLG_GROUPS_CITATIONS_ABSTRACT_BY'); ?> <?php echo $final; ?></p>
-                                                <?php endif; ?>
-                                                <p><?php echo nl2br($cite->abstract); ?></p>
-                                            </div>
-                                        <?php endif; ?>
-                                    </td>
-                                    <?php if ($this->isManager === true) : ?>
-                                        <td class="col-edit"><a href="<?php echo Route::url($base. '&action=edit&id=' .$cite->id ); ?>">
-                                            <?php echo Lang::txt('PLG_GROUPS_CITATIONS_EDIT'); ?>
-                                        </a></td>
-                                    <?php endif; ?>
-                                </tr>
-                                <tr>
-                                    <td <?php if ($this->label == "none") { echo 'colspan="3"'; } else { echo 'colspan="4"'; } ?> class="citation-details">
-                                        <?php
-                                            $singleCitationView = $this->config->get('citation_single_view', 0);
-                                            if (!$singleCitationView)
-                                            {
-                                                echo $formatter->citationDetails($cite, $this->database, $this->config, $this->openurl, true);
-                                            }
-                                        ?>
-                                        <?php if ($this->config->get("citation_show_badges","no") == "yes") : ?>
-                                            <?php echo \Components\Citations\Helpers\Format::citationBadges($cite, $this->database); ?>
-                                        <?php endif; ?>
+											echo $formatted; ?>
+										<?php
+											//get this citations rollover param
+											$params = new JParameter($cite->params);
+											$citation_rollover = 0;
+										?>
+										<?php if ($citation_rollover && $cite->abstract != "") : ?>
+											<div class="citation-notes">
+												<?php
+													$cs = new \Components\Citations\Tables\Sponsor($this->database);
+													$sponsors = $cs->getCitationSponsor($cite->id);
+													$final = "";
+													if ($sponsors)
+													{
+														foreach ($sponsors as $s)
+														{
+															$sp = $cs->getSponsor($s);
+															if ($sp)
+															{
+																$final .= '<a rel="external" href="'.$sp[0]['link'].'">'.$sp[0]['sponsor'].'</a>, ';
+															}
+														}
+													}
+												?>
+												<?php if ($final != '' && $this->config->get("citation_sponsors", "yes") == 'yes') : ?>
+													<?php $final = substr($final, 0, -2); ?>
+													<p class="sponsor"><?php echo Lang::txt('PLG_GROUPS_CITATIONS_ABSTRACT_BY'); ?> <?php echo $final; ?></p>
+												<?php endif; ?>
+												<p><?php echo nl2br($cite->abstract); ?></p>
+											</div>
+										<?php endif; ?>
+									</td>
+									<?php if ($this->isManager === true) : ?>
+										<td class="col-edit"><a href="<?php echo Route::url($base. '&action=edit&id=' .$cite->id ); ?>">
+											<?php echo Lang::txt('PLG_GROUPS_CITATIONS_EDIT'); ?>
+										</a></td>
+									<?php endif; ?>
+								</tr>
+								<tr>
+									<td <?php if ($this->label == "none") { echo 'colspan="3"'; } else { echo 'colspan="4"'; } ?> class="citation-details">
+										<?php
+											$singleCitationView = $this->config->get('citation_single_view', 0);
+											if (!$singleCitationView)
+											{
+												echo $formatter->citationDetails($cite, $this->database, $this->config, $this->openurl, true);
+											}
+										?>
+										<?php if ($this->config->get("citation_show_badges","no") == "yes") : ?>
+											<?php echo \Components\Citations\Helpers\Format::citationBadges($cite, $this->database); ?>
+										<?php endif; ?>
 
-                                        <?php if ($this->config->get("citation_show_tags","no") == "yes") : ?>
-                                            <?php echo \Components\Citations\Helpers\Format::citationTags($cite, $this->database); ?>
-                                        <?php endif; ?>
-                                    </td>
-                                </tr>
-                                <?php $counter++; ?>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                    <?php else : ?>
-                    <p class="warning"><?php echo Lang::txt('PLG_GROUPS_CITATIONS_NO_CITATIONS_FOUND'); ?></p>
-                    <?php endif; ?>
-                    <?php
+										<?php if ($this->config->get("citation_show_tags","no") == "yes") : ?>
+											<?php echo \Components\Citations\Helpers\Format::citationTags($cite, $this->database); ?>
+										<?php endif; ?>
+									</td>
+								</tr>
+								<?php $counter++; ?>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+					<?php else : ?>
+					<p class="warning"><?php echo Lang::txt('PLG_GROUPS_CITATIONS_NO_CITATIONS_FOUND'); ?></p>
+					<?php endif; ?>
+					<?php
 						// Initiate paging
 						$pageNav = $this->pagination(
 							$this->total,
