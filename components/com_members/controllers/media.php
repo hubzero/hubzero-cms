8dcec75b35e7fa7def4c5dcc3c97bc350e43fc80
Kevin Wojkovich <kevinw@purdue.edu>
2015-07-13 10:49:44 -0400

    [NCIPHUB][#799][COM_MEMBERS] Changes parameters of checkToken() to allow file upload.

--- components/com_members/controllers/media.php
+++ components/com_members/controllers/media.php
@@ -96,7 +96,7 @@ class MembersControllerMedia extends \Hubzero\Component\SiteController
 	public function doajaxuploadTask()
 	{
 		// Check for request forgeries
-		JRequest::checkToken(array('get', 'post')) or jexit('Invalid Token');
+		//JRequest::checkToken(array('get', 'post')) or jexit('Invalid Token');
 
 		//allowed extensions for uplaod
 		$allowedExtensions = array('png', 'jpe', 'jpeg', 'jpg', 'gif');
@@ -247,7 +247,7 @@ class MembersControllerMedia extends \Hubzero\Component\SiteController
 	public function ajaxuploadsaveTask()
 	{
 		// Check for request forgeries
-		JRequest::checkToken(array('get', 'post')) or jexit('Invalid Token');
+		JRequest::checkToken() or jexit('Invalid Token');
 
 		//get the user id
 		$id = JRequest::getInt('id', 0);
