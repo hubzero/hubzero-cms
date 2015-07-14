0db2f11a371281df6ea8aaf3366f3f5c333ad939
Shawn Rice <zooley@purdue.edu>
2015-07-14 15:35:09 -0400

    [COM_CITATIONS] Renaming importer model and moving the majority of logic from controller to importer model. Adding CSRF token.

--- core/components/com_citations/site/views/import/tmpl/display.php
+++ core/components/com_citations/site/views/import/tmpl/display.php
@@ -39,37 +39,40 @@ $this->css()
 </header>
 
 <section id="import" class="section">
-	<?php foreach ($this->messages as $message) { ?>
-		<p class="<?php echo $message['type']; ?>"><?php echo $message['message']; ?></p>
-	<?php } ?>
+	<div class="section-inner">
+		<?php foreach ($this->messages as $message) { ?>
+			<p class="<?php echo $message['type']; ?>"><?php echo $message['message']; ?></p>
+		<?php } ?>
 
-	<ul id="steps">
-		<li><a href="<?php echo Request::base(true); ?>/citations/import" class="active"><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP1'); ?><span><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP1_NAME'); ?></span></a></li>
-		<li><a><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP2'); ?><span><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP2_NAME'); ?></span></a></li>
-		<li><a><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP3'); ?><span><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP3_NAME'); ?></span></a></li>
-	</ul><!-- / #steps -->
+		<ul id="steps">
+			<li><a href="<?php echo Request::base(true); ?>/citations/import" class="active"><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP1'); ?><span><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP1_NAME'); ?></span></a></li>
+			<li><a><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP2'); ?><span><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP2_NAME'); ?></span></a></li>
+			<li><a><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP3'); ?><span><?php echo Lang::txt('COM_CITATIONS_IMPORT_STEP3_NAME'); ?></span></a></li>
+		</ul><!-- / #steps -->
 
-	<form id="hubForm" enctype="multipart/form-data" method="post" action="<?php echo Route::url('index.php?option='. $this->option . '&task=import_upload'); ?>">
-		<p class="explaination">
-			<strong><u><?php echo Lang::txt('COM_CITATIONS_IMPORT_ACCEPTABLE'); ?></u></strong><br />
-			<?php echo implode($this->accepted_files, "<br />"); ?>
-		</p>
-		<fieldset>
-			<legend><?php echo Lang::txt('COM_CITATIONS_IMPORT_UPLOAD'); ?>:</legend>
-			<label><?php echo Lang::txt('COM_CITATIONS_IMPORT_UPLOAD_FILE'); ?>: <span class="required"><?php echo Lang::txt('JREQUIRED'); ?></span>
-				<input type="file" name="citations_file" />
-				<span class="hint"><?php echo Lang::txt('COM_CITATIONS_IMPORT_UPLOAD_MAX'); ?></span>
-			</label>
-		</fieldset>
+		<form id="hubForm" enctype="multipart/form-data" method="post" action="<?php echo Route::url('index.php?option='. $this->option . '&task=import_upload'); ?>">
+			<p class="explaination">
+				<strong><u><?php echo Lang::txt('COM_CITATIONS_IMPORT_ACCEPTABLE'); ?></u></strong><br />
+				<?php echo implode($this->accepted_files, "<br />"); ?>
+			</p>
+			<fieldset>
+				<legend><?php echo Lang::txt('COM_CITATIONS_IMPORT_UPLOAD'); ?>:</legend>
+				<label><?php echo Lang::txt('COM_CITATIONS_IMPORT_UPLOAD_FILE'); ?>: <span class="required"><?php echo Lang::txt('JREQUIRED'); ?></span>
+					<input type="file" name="citations_file" />
+					<span class="hint"><?php echo Lang::txt('COM_CITATIONS_IMPORT_UPLOAD_MAX'); ?></span>
+				</label>
+			</fieldset>
 
-		<p class="submit">
-			<input type="submit" name="submit" value="<?php echo Lang::txt('COM_CITATIONS_IMPORT_UPLOAD'); ?>" />
-		</p>
+			<p class="submit">
+				<input type="submit" name="submit" value="<?php echo Lang::txt('COM_CITATIONS_IMPORT_UPLOAD'); ?>" />
+			</p>
 
-		<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
-		<?php if (isset($this->gid)): ?>
-			<input type="hidden" name="group" value="<?php echo $this->gid; ?>" />
-		<?php endif; ?>
-		<input type="hidden" name="task" value="import_upload" />
-	</form>
+			<?php echo Html::input('token'); ?>
+			<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
+			<?php if (isset($this->gid)): ?>
+				<input type="hidden" name="group" value="<?php echo $this->gid; ?>" />
+			<?php endif; ?>
+			<input type="hidden" name="task" value="import_upload" />
+		</form>
+	</div>
 </section><!-- / .section -->
