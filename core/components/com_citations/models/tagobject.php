6d12baa851a8c95f380437f6a2f9802b71bde22e
Kevin Wojkovich <kevinw@purdue.edu>
2015-07-20 14:01:45 -0400

    [COM_CITATIONS] Initial commit of the relational models for tags and tab objects (for citations).

---
+++ core/components/com_citations/models/tagobject.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author	Kevin Wojkovich <kevinw@purdue.edu>
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ * @since	 Class available since release 1.3.2
+ */
+
+namespace Components\Citations\Models;
+
+use Hubzero\Database\Relational;
+use Hubzero\Utility\String;
+use Hubzero\Base\Object;
+
+/**
+ * Tag (for citations) database model
+ *
+ * @uses \Hubzero\Database\Relational
+ */
+class TagObject extends Relational
+{
+	/**
+	 * The table namespace
+	 *
+	 * @var string
+	 **/
+	protected $namespace = '';
+
+	/**
+	 * Default order by for model
+	 *
+	 * @var string
+	 **/
+	public $orderBy = 'name';
+
+	/**
+	 * Override the table
+	 *
+	 * @var string
+	 **/
+	protected $table = '#__tags_object';
+
+	/**
+	 * Fields and their validation criteria
+	 *
+	 * @var array
+	 **/
+	protected $rules = array(
+		//'name'	=> 'notempty',
+		//'liaison' => 'notempty'
+	);
+
+	/**
+	 * Automatically fillable fields
+	 *
+	 * @var array
+	 **/
+	public $always = array(
+		//'name_normalized',
+		//'asset_id'
+	);
+
+
+	/**
+	 * Defines a one to one relationship with citation
+	 *
+	 * @return $this
+	 * @since  1.3.2
+	 **/
+	public function tag()
+	{
+		return $this->belongsToOne('Tag', 'tagid', 'id');
+	}
+
+}
