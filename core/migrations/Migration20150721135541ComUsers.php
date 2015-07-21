9a1ad7b7f843439e90ba3bb44a6d255b8343ddd2
Sam Wilson <samwilson@purdue.edu>
2015-07-21 09:57:40 -0400

    [COM_USERS] add migration for creating auth log table

---
+++ core/migrations/Migration20150721135541ComUsers.php
@@ -0,0 +1,43 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+/**
+ * Migration script for creating table jos_users_log_auth
+ **/
+class Migration20150721135541ComUsers extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if (!$this->db->tableExists('#__users_log_auth'))
+		{
+			$query = "CREATE TABLE `#__users_log_auth` (
+				  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				  `user_id` int(11) NOT NULL,
+				  `username` varchar(150) DEFAULT NULL,
+				  `status` enum('success','failure') DEFAULT NULL,
+				  `ip` varchar(15) DEFAULT NULL,
+				  `logged` datetime DEFAULT NULL,
+				  PRIMARY KEY (`id`)
+				) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if ($this->db->tableExists('#__users_log_auth'))
+		{
+			$query = "DROP TABLE `#__users_log_auth`";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+}
