da78b2aabb728c4429504b6f794d6d72cb978424
Kevin Wojkovich <kevinw@purdue.edu>
2015-07-22 16:05:09 -0400

    [PLG_CRON_GEOSEACH][HUBZERO][#8289] Adds the extension entry for the geosearch cron plugin.

---
+++ core/migrations/Migration20150722155100PlgCronGeosearch.php
@@ -0,0 +1,48 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding basic antispam plugin.
+ **/
+class Migration20150722155100PlgCronGeosearch extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__extensions'))
+		{
+			$query = "SELECT `extension_id` FROM `#__extensions` WHERE `folder` = 'cron' AND `element` = 'geosearch' AND `type` = 'plugin'";
+			$this->db->setQuery($query);
+			$id = $this->db->loadResult();
+
+			if (!$id)
+			{
+				$this->addPluginEntry('cron', 'geosearch');
+			}
+    }
+}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if ($this->db->tableExists('#__extensions'))
+		{
+			$query = "SELECT `extension_id` FROM `#__extensions` WHERE `folder` = 'cron' AND `element` = 'geosearch' AND `type` = 'plugin'";
+			$this->db->setQuery($query);
+			$id = $this->db->loadResult();
+
+			if ($id)
+			{
+				$this->deletePluginEntry('cron', 'geosearch');
+			}
+		}
+	}
+}
