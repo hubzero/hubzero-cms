9bd0f6f727dd419aacbe73ba1f687f1eff3b5521
Shawn Rice <zooley@purdue.edu>
2015-07-27 08:55:49 -0400

    [HUBZERO][#8386][COM_EVENTS] Make sure record exists before attempting to parse its data

--- components/com_events/controllers/events.php
+++ components/com_events/controllers/events.php
@@ -647,7 +647,7 @@ class EventsControllerEvents extends \Hubzero\Component\SiteController
 		$row->load($id);
 
 		// Ensure we have an event
-		if (!$row)
+		if (!$row || !$row->id)
 		{
 			JError::raiseError(404, JText::_('EVENTS_CAL_LANG_NO_EVENTFOR') . ' ' . JText::_('EVENTS_CAL_LANG_THIS_DAY'));
 			return;
