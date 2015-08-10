30d9ed74b19992f55f8b683fd29e17e6c830f440
Shawn Rice <zooley@purdue.edu>
2015-08-10 08:24:25 -0400

    [COM_PROJECTS] Minor adjustments to HTML and CSS to make styling more consistent with other components and allow for easier styling with new default template

--- core/components/com_projects/site/views/projects/tmpl/_statusmsg.php
+++ core/components/com_projects/site/views/projects/tmpl/_statusmsg.php
@@ -27,8 +27,8 @@ defined('_HZEXEC_') or die();
 
 $error = isset($this->error) && $this->error ? $this->error : $this->getError();
 
-?>
-
+if ($error || $this->msg) { ?>
 <div id="status-msg" class="status-msg">
-<?php if ($error) { echo '<p class="witherror">' . $error.'</p>'; } elseif ($this->msg) { echo '<p>' . $this->msg . '</p>'; } ?>
+	<?php if ($error) { echo '<p class="witherror">' . $error.'</p>'; } elseif ($this->msg) { echo '<p>' . $this->msg . '</p>'; } ?>
 </div>
+<?php }
--- core/components/com_projects/site/views/projects/tmpl/intro.php
+++ core/components/com_projects/site/views/projects/tmpl/intro.php
@@ -32,17 +32,15 @@ $this->css('introduction.css', 'system')
 $rows = $this->model->entries('list', $this->filters);
 
 ?>
-<div id="content-header">
+<header id="content-header">
 	<h2><?php echo $this->title; ?></h2>
-</div><!-- / #content-header -->
 
-<div id="content-header-extra">
-    <ul id="useroptions">
-    	<li><a class="btn icon-browse" href="<?php echo Route::url('index.php?option=' . $this->option . '&task=browse'); ?>"><?php echo Lang::txt('COM_PROJECTS_BROWSE_PUBLIC_PROJECTS'); ?></a></li>
-	</ul>
-</div><!-- / #content-header-extra -->
-
-<div class="clear"></div>
+	<nav id="content-header-extra">
+		<ul id="useroptions">
+			<li><a class="btn icon-browse" href="<?php echo Route::url('index.php?option=' . $this->option . '&task=browse'); ?>"><?php echo Lang::txt('COM_PROJECTS_BROWSE_PUBLIC_PROJECTS'); ?></a></li>
+		</ul>
+	</nav><!-- / #content-header-extra -->
+</header><!-- / #content-header -->
 
 <?php
 	// Display status message
@@ -52,59 +50,54 @@ $rows = $this->model->entries('list', $this->filters);
 	     ->display();
 ?>
 
-<div class="clear block">&nbsp;</div>
-
 <section id="introduction" class="section">
- <div id="introbody">
-	<div class="grid">
-		<div class="col span5">
-			<h3><?php echo Lang::txt('COM_PROJECTS_INTRO_COLLABORATION_MADE_EASY'); ?></h3>
-			<p><?php echo Lang::txt('COM_PROJECTS_INTRO_COLLABORATION_HOW'); ?></p>
-			<p><a href="<?php echo Route::url('index.php?option=' . $this->option . '&task=start'); ?>" id="projects-intro-start" class="btn icon-next"><?php echo Lang::txt('COM_PROJECTS_START_PROJECT'); ?></a></p>
-		</div>
-		<div class="col span4 omega">
-			<h3><?php echo Lang::txt('COM_PROJECTS_INTRO_WHAT_YOU_GET'); ?></h3>
-			<ul>
-				<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_REPOSITORY'); ?></li>
-				<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_WIKI'); ?></li>
-				<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_TODO'); ?></li>
-				<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_BLOG'); ?></li>
-				<?php if ($this->publishing) { ?>
-				<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_PUBLISHING'); ?></li>
-				<?php } ?>
-			</ul>
-			<p><a href="<?php echo Route::url('index.php?option=' . $this->option . '&task=features'); ?>" id="projects-intro-features" class="btn"><?php echo Lang::txt('COM_PROJECTS_LEARN_MORE'); ?></a></p>
+	<div id="introbody">
+		<div class="grid">
+			<div class="col span5">
+				<h3><?php echo Lang::txt('COM_PROJECTS_INTRO_COLLABORATION_MADE_EASY'); ?></h3>
+				<p><?php echo Lang::txt('COM_PROJECTS_INTRO_COLLABORATION_HOW'); ?></p>
+				<p><a href="<?php echo Route::url('index.php?option=' . $this->option . '&task=start'); ?>" id="projects-intro-start" class="btn icon-next"><?php echo Lang::txt('COM_PROJECTS_START_PROJECT'); ?></a></p>
+			</div>
+			<div class="col span4 omega">
+				<h3><?php echo Lang::txt('COM_PROJECTS_INTRO_WHAT_YOU_GET'); ?></h3>
+				<ul>
+					<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_REPOSITORY'); ?></li>
+					<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_WIKI'); ?></li>
+					<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_TODO'); ?></li>
+					<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_BLOG'); ?></li>
+					<?php if ($this->publishing) { ?>
+					<li><?php echo Lang::txt('COM_PROJECTS_INTRO_GET_PUBLISHING'); ?></li>
+					<?php } ?>
+				</ul>
+				<p><a href="<?php echo Route::url('index.php?option=' . $this->option . '&task=features'); ?>" id="projects-intro-features" class="btn"><?php echo Lang::txt('COM_PROJECTS_LEARN_MORE'); ?></a></p>
+			</div>
 		</div>
 	</div>
- </div>
 </section><!-- / #introduction.section -->
 
-<div class="clear"></div>
 <section class="section myprojects">
 	<div class="grid">
-	<div class="col span2">
-		<h2><?php echo Lang::txt('COM_PROJECTS_MY_PROJECTS'); ?></h2>
+		<div class="col span2">
+			<h2><?php echo Lang::txt('COM_PROJECTS_MY_PROJECTS'); ?></h2>
+		</div>
+		<div class="col span10 omega">
+			<?php
+			if (count($rows) > 0) { ?>
+				<ul class="flow">
+					<?php foreach ($rows as $row)
+					{
+						$setup = ($row->inSetup()) ? Lang::txt('COM_PROJECTS_COMPLETE_SETUP') : '';
+					?>
+					<li <?php if ($setup) { echo 'class="s-dev"'; } else if ($row->get('state') == 0) { echo 'class="s-inactive"'; } else if ($row->get('state') == 5) { echo 'class="s-pending"'; } ?>>
+					<?php  if (!$setup && !$row->isPublic()) { ?><span class="s-private">&nbsp;</span><?php }  ?>
+						<a href="<?php echo Route::url('index.php?option=' . $this->option . '&task=view&alias=' . $row->get('alias')); ?>"><img src="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $row->get('alias') . '&task=media'); ?>" alt="" /><span class="block"><?php echo \Hubzero\Utility\String::truncate($this->escape($row->get('title')), 30); ?></span></a><?php if ($setup) { ?><span class="s-complete"><?php echo Lang::txt('COM_PROJECTS_COMPLETE_SETUP'); ?></span><?php } else if ($row->get('state') == 0) { ?><span class="s-suspended"><?php echo Lang::txt('COM_PROJECTS_STATUS_INACTIVE'); ?></span> <?php } else if ($row->get('state') == 5) { ?><span class="s-suspended"><?php echo Lang::txt('COM_PROJECTS_STATUS_PENDING'); ?></span> <?php } ?>
+					<?php if ($row->get('newactivity') && $row->isActive() && !$setup) { ?><span class="s-new"><?php echo $row->get('newactivity'); ?></span><?php } ?>
+					</li>
+					<?php }	?>
+				</ul>
+			<?php } else { ?>
+				<div class="noresults"><?php echo (User::isGuest()) ? Lang::txt('COM_PROJECTS_PLEASE').' <a href="' . Route::url('index.php?option=' . $this->option . '&task=intro&action=login') . '" id="projects-intro-login">' . Lang::txt('COM_PROJECTS_LOGIN') . '</a> ' . Lang::txt('COM_PROJECTS_TO_VIEW_YOUR_PROJECTS') : Lang::txt('COM_PROJECTS_YOU_DONT_HAVE_PROJECTS'); ?></div>
+			<?php }	?>
+		</div>
 	</div>
-	<div class="col span10 omega">
-		<?php
-		if (count($rows) > 0) { ?>
-			<ul class="flow">
-				<?php foreach ($rows as $row)
-				{
-					$setup = ($row->inSetup()) ? Lang::txt('COM_PROJECTS_COMPLETE_SETUP') : '';
-				?>
-				<li <?php if ($setup) { echo 'class="s-dev"'; } else if ($row->get('state') == 0) { echo 'class="s-inactive"'; } else if ($row->get('state') == 5) { echo 'class="s-pending"'; } ?>>
-				<?php  if (!$setup && !$row->isPublic()) { ?><span class="s-private">&nbsp;</span><?php }  ?>
-					<a href="<?php echo Route::url('index.php?option=' . $this->option . '&task=view&alias=' . $row->get('alias')); ?>"><img src="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $row->get('alias') . '&task=media'); ?>" alt="" /><span class="block"><?php echo \Hubzero\Utility\String::truncate($this->escape($row->get('title')), 30); ?></span></a><?php if ($setup) { ?><span class="s-complete"><?php echo Lang::txt('COM_PROJECTS_COMPLETE_SETUP'); ?></span><?php } else if ($row->get('state') == 0) { ?><span class="s-suspended"><?php echo Lang::txt('COM_PROJECTS_STATUS_INACTIVE'); ?></span> <?php } else if ($row->get('state') == 5) { ?><span class="s-suspended"><?php echo Lang::txt('COM_PROJECTS_STATUS_PENDING'); ?></span> <?php } ?>
-				<?php if ($row->get('newactivity') && $row->isActive() && !$setup) { ?><span class="s-new"><?php echo $row->get('newactivity'); ?></span><?php } ?>
-				</li>
-				<?php }	?>
-			</ul>
-		<?php } else { ?>
-			<div class="noresults"><?php echo (User::isGuest()) ? Lang::txt('COM_PROJECTS_PLEASE').' <a href="' . Route::url('index.php?option=' . $this->option . '&task=intro&action=login') . '" id="projects-intro-login">' . Lang::txt('COM_PROJECTS_LOGIN') . '</a> ' . Lang::txt('COM_PROJECTS_TO_VIEW_YOUR_PROJECTS') : Lang::txt('COM_PROJECTS_YOU_DONT_HAVE_PROJECTS'); ?></div>
-		<?php }	?>
-	</div>
-</div>
 </section>
-<div class="clear"></div>
-
