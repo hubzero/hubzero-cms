3a17dc97a746a7c1b672bd10e1d4da287e170ed3
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2015-08-26 11:55:41 -0400

    [MIGRATIONS] add constantcontact plugin entry

--- migrations/Migration20130201000000PlgUserConstantcontact.php
+++ migrations/Migration20130201000000PlgUserConstantcontact.php
@@ -5,6 +5,9 @@ use Hubzero\Content\Migration\Base;
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
+/**
+ * Migration script for adding constant contact plugin entry
+ **/
 class Migration20130201000000PlgUserConstantcontact extends Base
 {
 	public function up()
