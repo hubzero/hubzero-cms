65242ece5aac92b9b4f401bc19393e814309474f
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2015-09-11 11:43:19 -0400

    update copyright ownership to HUBzero Foundation, LLC.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * HUBzero CMS
  *
  * @package    hubzero-cms
- * @copyright  Copyright 2005-2015 Purdue University. All rights reserved.
+ * @copyright  Copyright 2005-2015 HUBzero Foundation, LLC.
  * @license    http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
 
