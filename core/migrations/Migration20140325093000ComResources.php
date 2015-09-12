14e8c1c2f8eff6fd4c189a04bd70703466079765
Shawn Rice <zooley@purdue.edu>
2015-09-12 18:00:38 +0000

    [MIGRATIONS] Try to detect if function already exists before adding it

--- core/migrations/Migration20140325093000ComResources.php
+++ core/migrations/Migration20140325093000ComResources.php
@@ -23,7 +23,7 @@ class Migration20140325093000ComResources extends Base
 
 		if (!is_dir($const . DS . trim($upload, DS)))
 		{
-			mkdir($const . DS . trim($upload, DS));
+			mkdir($const . DS . trim($upload, DS), 0775, true);
 		}
 
 		$path = $const . DS . trim($upload, DS) . DS . 'import' . DS;
@@ -32,70 +32,89 @@ class Migration20140325093000ComResources extends Base
 			mkdir($path);
 		}
 
-		// levenshtein func
-		$query = 'CREATE FUNCTION `LEVENSHTEIN`(s1 VARCHAR(255) CHARACTER SET utf8, s2 VARCHAR(255) CHARACTER SET utf8) RETURNS int(11)
-    DETERMINISTIC
+		$found = false;
+
+		$this->db->setQuery("SHOW FUNCTION STATUS");
+		$results = $this->db->loadObjectList();
+		if ($results)
+		{
+			foreach ($results as $result)
+			{
+				if ($result->Db == \App::get('config')->get('db') && $result->Name == 'LEVENSHTEIN')
+				{
+					$found = true;
+					break;
+				}
+			}
+		}
+
+		if (!$found)
+		{
+			// levenshtein func
+			$query = 'CREATE FUNCTION `LEVENSHTEIN`(s1 VARCHAR(255) CHARACTER SET utf8, s2 VARCHAR(255) CHARACTER SET utf8) RETURNS int(11)
+	DETERMINISTIC
 BEGIN
-    DECLARE s1_len, s2_len, i, j, c, c_temp, cost INT;
-    DECLARE s1_char CHAR CHARACTER SET utf8;
-    -- max strlen=255 for this function
-    DECLARE cv0, cv1 VARBINARY(256);
+	DECLARE s1_len, s2_len, i, j, c, c_temp, cost INT;
+	DECLARE s1_char CHAR CHARACTER SET utf8;
+	-- max strlen=255 for this function
+	DECLARE cv0, cv1 VARBINARY(256);
 
-    SET s1_len = CHAR_LENGTH(s1),
-        s2_len = CHAR_LENGTH(s2),
-        cv1 = 0x00,
-        j = 1,
-        i = 1,
-        c = 0;
+	SET s1_len = CHAR_LENGTH(s1),
+		s2_len = CHAR_LENGTH(s2),
+		cv1 = 0x00,
+		j = 1,
+		i = 1,
+		c = 0;
 
-    IF (s1 = s2) THEN
-      RETURN (0);
-    ELSEIF (s1_len = 0) THEN
-      RETURN (s2_len);
-    ELSEIF (s2_len = 0) THEN
-      RETURN (s1_len);
-    END IF;
+	IF (s1 = s2) THEN
+	  RETURN (0);
+	ELSEIF (s1_len = 0) THEN
+	  RETURN (s2_len);
+	ELSEIF (s2_len = 0) THEN
+	  RETURN (s1_len);
+	END IF;
 
-    WHILE (j <= s2_len) DO
-      SET cv1 = CONCAT(cv1, CHAR(j)),
-          j = j + 1;
-    END WHILE;
+	WHILE (j <= s2_len) DO
+	  SET cv1 = CONCAT(cv1, CHAR(j)),
+		  j = j + 1;
+	END WHILE;
 
-    WHILE (i <= s1_len) DO
-      SET s1_char = SUBSTRING(s1, i, 1),
-          c = i,
-          cv0 = CHAR(i),
-          j = 1;
+	WHILE (i <= s1_len) DO
+	  SET s1_char = SUBSTRING(s1, i, 1),
+		  c = i,
+		  cv0 = CHAR(i),
+		  j = 1;
 
-      WHILE (j <= s2_len) DO
-        SET c = c + 1,
-            cost = IF(s1_char = SUBSTRING(s2, j, 1), 0, 1);
+	  WHILE (j <= s2_len) DO
+		SET c = c + 1,
+			cost = IF(s1_char = SUBSTRING(s2, j, 1), 0, 1);
 
-        SET c_temp = ORD(SUBSTRING(cv1, j, 1)) + cost;
-        IF (c > c_temp) THEN
-          SET c = c_temp;
-        END IF;
+		SET c_temp = ORD(SUBSTRING(cv1, j, 1)) + cost;
+		IF (c > c_temp) THEN
+		  SET c = c_temp;
+		END IF;
 
-        SET c_temp = ORD(SUBSTRING(cv1, j+1, 1)) + 1;
-        IF (c > c_temp) THEN
-          SET c = c_temp;
-        END IF;
+		SET c_temp = ORD(SUBSTRING(cv1, j+1, 1)) + 1;
+		IF (c > c_temp) THEN
+		  SET c = c_temp;
+		END IF;
 
-        SET cv0 = CONCAT(cv0, CHAR(c)),
-            j = j + 1;
-      END WHILE;
+		SET cv0 = CONCAT(cv0, CHAR(c)),
+			j = j + 1;
+	  END WHILE;
 
-      SET cv1 = cv0,
-          i = i + 1;
-    END WHILE;
+	  SET cv1 = cv0,
+		  i = i + 1;
+	END WHILE;
 
-    RETURN (c);
+	RETURN (c);
   END;';
 
-		if ($query != '')
-		{
-			$this->db->setQuery($query);
-			$this->db->query();
+			if ($query != '')
+			{
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
 		}
 	}
 
