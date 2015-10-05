298e5caf14f990bcdef475c64b4c53228042924d
Shawn Rice <zooley@purdue.edu>
2015-10-05 12:51:12 +0000

    [COM_ANSWERS] Updating admin view to use newer grid system

--- core/components/com_answers/admin/views/answers/tmpl/edit.php
+++ core/components/com_answers/admin/views/answers/tmpl/edit.php
@@ -78,73 +78,74 @@ function submitbutton(pressbutton)
 </script>
 
 <form action="<?php echo Route::url('index.php?option=' . $this->option . '&controller=' . $this->controller); ?>" method="post" name="adminForm" id="item-form">
-	<div class="col width-60 fltlft">
-		<fieldset class="adminform">
-			<legend><span><?php echo Lang::txt('JDETAILS'); ?></span></legend>
+	<div class="grid">
+		<div class="col span6">
+			<fieldset class="adminform">
+				<legend><span><?php echo Lang::txt('JDETAILS'); ?></span></legend>
 
-			<div class="input-wrap">
-				<input type="checkbox" name="answer[anonymous]" id="field-anonymous" value="1" <?php echo ($this->row->get('anonymous')) ? 'checked="checked"' : ''; ?> />
-				<label for="field-anonymous"><?php echo Lang::txt('COM_ANSWERS_FIELD_ANONYMOUS'); ?></label>
-			</div>
-			<div class="input-wrap">
-				<label for="field-question"><?php echo Lang::txt('COM_ANSWERS_FIELD_QUESTION'); ?></label><br />
-				<input type="text" id="field-question" disabled="disabled" readonly="readonly" value="<?php echo $this->escape($this->question->subject('clean')); ?>" />
-			</div>
-			<div class="input-wrap">
-				<label for="field-answer"><?php echo Lang::txt('COM_ANSWERS_FIELD_ANSWER'); ?> <span class="required"><?php echo Lang::txt('JOPTION_REQUIRED'); ?></span></label><br />
-				<?php echo $this->editor('answer[answer]', $this->escape($this->row->content('raw')), 50, 15, 'field-answer', array('class' => 'minimal no-footer')); ?>
-			</div>
-		</fieldset>
+				<div class="input-wrap">
+					<input type="checkbox" name="answer[anonymous]" id="field-anonymous" value="1" <?php echo ($this->row->get('anonymous')) ? 'checked="checked"' : ''; ?> />
+					<label for="field-anonymous"><?php echo Lang::txt('COM_ANSWERS_FIELD_ANONYMOUS'); ?></label>
+				</div>
+				<div class="input-wrap">
+					<label for="field-question"><?php echo Lang::txt('COM_ANSWERS_FIELD_QUESTION'); ?></label><br />
+					<input type="text" id="field-question" disabled="disabled" readonly="readonly" value="<?php echo $this->escape($this->question->subject('clean')); ?>" />
+				</div>
+				<div class="input-wrap">
+					<label for="field-answer"><?php echo Lang::txt('COM_ANSWERS_FIELD_ANSWER'); ?> <span class="required"><?php echo Lang::txt('JOPTION_REQUIRED'); ?></span></label><br />
+					<?php echo $this->editor('answer[answer]', $this->escape($this->row->content('raw')), 50, 15, 'field-answer', array('class' => 'minimal no-footer')); ?>
+				</div>
+			</fieldset>
+		</div>
+		<div class="col span6">
+			<table class="meta">
+				<tbody>
+					<tr>
+						<th><?php echo Lang::txt('COM_ANSWERS_FIELD_ID'); ?>:</th>
+						<td><?php echo $this->row->get('id'); ?></td>
+					</tr>
+				<?php if ($this->row->get('id')) { ?>
+					<tr>
+						<th><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATED'); ?>:</th>
+						<td><?php echo $this->row->get('created'); ?></td>
+					</tr>
+					<tr>
+						<th><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATOR'); ?>:</th>
+						<td><?php echo $this->escape(stripslashes($this->row->creator('name'))); ?></td>
+					</tr>
+				<?php } ?>
+					<tr>
+						<th><?php echo Lang::txt('COM_ANSWERS_FIELD_HELPFUL'); ?>:</th>
+						<td>
+							<span class="votes up">+<?php echo $this->row->get('helpful'); ?></span>
+							<span class="votes down">-<?php echo $this->row->get('nothelpful'); ?></span>
+							<?php if ($this->row->get('helpful') > 0 || $this->row->get('nothelpful') > 0) { ?>
+								<input type="button" name="reset_helpful" value="<?php echo Lang::txt('COM_ANSWERS_FIELD_RESET'); ?>" onclick="submitbutton('reset');" />
+							<?php } ?>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<fieldset class="adminform">
+				<legend><span><?php echo Lang::txt('JGLOBAL_FIELDSET_PUBLISHING'); ?></span></legend>
+
+				<div class="input-wrap">
+					<label for="field-state"><?php echo Lang::txt('COM_ANSWERS_FIELD_ACCEPT'); ?></label><br />
+					<input type="checkbox" name="answer[state]" id="field-state" value="1" <?php echo $this->row->get('state') ? 'checked="checked"' : ''; ?> /> (<?php echo ($this->row->get('state') == 1) ? Lang::txt('COM_ANSWERS_STATE_ACCEPTED') : Lang::txt('COM_ANSWERS_STATE_UNACCEPTED'); ?>)
+				</div>
+
+				<div class="input-wrap">
+					<label for="field-created_by"><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATOR'); ?></label><br />
+					<input type="text" name="answer[created_by]" id="field-created_by" size="25" maxlength="50" value="<?php echo $this->escape($this->row->get('created_by', User::get('id'))); ?>" />
+				</div>
+
+				<div class="input-wrap">
+					<label for="field-created"><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATED'); ?></label><br />
+					<?php echo Html::input('calendar', 'answer[created]', $this->row->get('created', Date::toSql()), array('id' => 'field-created')); ?>
+				</div>
+			</fieldset>
+		</div>
 	</div>
-	<div class="col width-40 fltrt">
-		<table class="meta">
-			<tbody>
-				<tr>
-					<th><?php echo Lang::txt('COM_ANSWERS_FIELD_ID'); ?>:</th>
-					<td><?php echo $this->row->get('id'); ?></td>
-				</tr>
-			<?php if ($this->row->get('id')) { ?>
-				<tr>
-					<th><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATED'); ?>:</th>
-					<td><?php echo $this->row->get('created'); ?></td>
-				</tr>
-				<tr>
-					<th><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATOR'); ?>:</th>
-					<td><?php echo $this->escape(stripslashes($this->row->creator('name'))); ?></td>
-				</tr>
-			<?php } ?>
-				<tr>
-					<th><?php echo Lang::txt('COM_ANSWERS_FIELD_HELPFUL'); ?>:</th>
-					<td>
-						<span class="votes up">+<?php echo $this->row->get('helpful'); ?></span>
-						<span class="votes down">-<?php echo $this->row->get('nothelpful'); ?></span>
-						<?php if ($this->row->get('helpful') > 0 || $this->row->get('nothelpful') > 0) { ?>
-							<input type="button" name="reset_helpful" value="<?php echo Lang::txt('COM_ANSWERS_FIELD_RESET'); ?>" onclick="submitbutton('reset');" />
-						<?php } ?>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-		<fieldset class="adminform">
-			<legend><span><?php echo Lang::txt('JGLOBAL_FIELDSET_PUBLISHING'); ?></span></legend>
-
-			<div class="input-wrap">
-				<label for="field-state"><?php echo Lang::txt('COM_ANSWERS_FIELD_ACCEPT'); ?></label><br />
-				<input type="checkbox" name="answer[state]" id="field-state" value="1" <?php echo $this->row->get('state') ? 'checked="checked"' : ''; ?> /> (<?php echo ($this->row->get('state') == 1) ? Lang::txt('COM_ANSWERS_STATE_ACCEPTED') : Lang::txt('COM_ANSWERS_STATE_UNACCEPTED'); ?>)
-			</div>
-
-			<div class="input-wrap">
-				<label for="field-created_by"><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATOR'); ?></label><br />
-				<input type="text" name="answer[created_by]" id="field-created_by" size="25" maxlength="50" value="<?php echo $this->escape($this->row->get('created_by', User::get('id'))); ?>" />
-			</div>
-
-			<div class="input-wrap">
-				<label for="field-created"><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATED'); ?></label><br />
-				<?php echo Html::input('calendar', 'answer[created]', $this->row->get('created', Date::toSql()), array('id' => 'field-created')); ?>
-			</div>
-		</fieldset>
-	</div>
-	<div class="clr"></div>
 
 	<input type="hidden" name="answer[question_id]" value="<?php echo $this->question->get('id'); ?>" />
 	<input type="hidden" name="answer[id]" value="<?php echo $this->row->get('id'); ?>" />
--- core/components/com_answers/admin/views/questions/tmpl/edit.php
+++ core/components/com_answers/admin/views/questions/tmpl/edit.php
@@ -71,87 +71,89 @@ function submitbutton(pressbutton)
 </script>
 
 <form action="<?php echo Route::url('index.php?option=' . $this->option . '&controller=' . $this->controller); ?>" method="post" name="adminForm" id="item-form">
-	<div class="col width-60 fltlft">
-		<fieldset class="adminform">
-			<legend><span><?php echo Lang::txt('JDETAILS'); ?></span></legend>
+	<div class="grid">
+		<div class="col span7">
+			<fieldset class="adminform">
+				<legend><span><?php echo Lang::txt('JDETAILS'); ?></span></legend>
 
-			<div class="col width-50 fltlft">
-				<div class="input-wrap">
-					<input type="checkbox" name="question[anonymous]" id="field-anonymous" value="1" <?php echo ($this->row->get('anonymous')) ? 'checked="checked"' : ''; ?> />
-					<label for="field-anonymous"><?php echo Lang::txt('COM_ANSWERS_FIELD_ANONYMOUS'); ?></label>
+				<div class="grid">
+					<div class="col span6">
+						<div class="input-wrap">
+							<input type="checkbox" name="question[anonymous]" id="field-anonymous" value="1" <?php echo ($this->row->get('anonymous')) ? 'checked="checked"' : ''; ?> />
+							<label for="field-anonymous"><?php echo Lang::txt('COM_ANSWERS_FIELD_ANONYMOUS'); ?></label>
+						</div>
+					</div>
+					<div class="col span6">
+						<div class="input-wrap">
+							<input type="checkbox" name="question[email]" id="field-email" value="1" <?php echo ($this->row->get('email')) ? 'checked="checked"' : ''; ?> />
+							<label for="field-email"><?php echo Lang::txt('COM_ANSWERS_FIELD_NOTIFY'); ?></label>
+						</div>
+					</div>
 				</div>
-			</div>
-			<div class="col width-50 fltrt">
+
 				<div class="input-wrap">
-					<input type="checkbox" name="question[email]" id="field-email" value="1" <?php echo ($this->row->get('email')) ? 'checked="checked"' : ''; ?> />
-					<label for="field-email"><?php echo Lang::txt('COM_ANSWERS_FIELD_NOTIFY'); ?></label>
+					<label for="field-subject"><?php echo Lang::txt('COM_ANSWERS_FIELD_SUBJECT'); ?>: <span class="required"><?php echo Lang::txt('JOPTION_REQUIRED'); ?></span></label><br />
+					<input type="text" name="question[subject]" id="field-subject" size="30" maxlength="250" value="<?php echo $this->escape($this->row->subject('raw')); ?>" />
 				</div>
-			</div>
-			<div class="clr"></div>
 
-			<div class="input-wrap">
-				<label for="field-subject"><?php echo Lang::txt('COM_ANSWERS_FIELD_SUBJECT'); ?>: <span class="required"><?php echo Lang::txt('JOPTION_REQUIRED'); ?></span></label><br />
-				<input type="text" name="question[subject]" id="field-subject" size="30" maxlength="250" value="<?php echo $this->escape($this->row->subject('raw')); ?>" />
-			</div>
+				<div class="input-wrap">
+					<label for="field-question"><?php echo Lang::txt('COM_ANSWERS_FIELD_QUESTION'); ?>:</label><br />
+					<?php echo $this->editor('question[question]', $this->escape($this->row->content('raw')), 50, 15, 'field-question', array('class' => 'minimal no-footer')); ?>
+				</div>
 
-			<div class="input-wrap">
-				<label for="field-question"><?php echo Lang::txt('COM_ANSWERS_FIELD_QUESTION'); ?>:</label><br />
-				<?php echo $this->editor('question[question]', $this->escape($this->row->content('raw')), 50, 15, 'field-question', array('class' => 'minimal no-footer')); ?>
-			</div>
+				<div class="input-wrap" data-hint="<?php echo Lang::txt('COM_ANSWERS_FIELD_TAGS_HINT'); ?>">
+					<label for="field-tags"><?php echo Lang::txt('COM_ANSWERS_FIELD_TAGS'); ?>: <span class="required"><?php echo Lang::txt('JOPTION_REQUIRED'); ?></span></label><br />
+					<textarea name="question[tags]" id="field-tags" cols="50" rows="3"><?php echo $this->escape(stripslashes($this->row->tags('string'))); ?></textarea>
+					<span class="hint"><?php echo Lang::txt('COM_ANSWERS_FIELD_TAGS_HINT'); ?></span>
+				</div>
+			</fieldset>
+		</div>
+		<div class="col span5">
+			<table class="meta">
+				<tbody>
+					<tr>
+						<th><?php echo Lang::txt('COM_ANSWERS_FIELD_ID'); ?>:</th>
+						<td>
+							<?php echo $this->row->get('id', 0); ?>
+							<input type="hidden" name="question[id]" value="<?php echo $this->row->get('id'); ?>" />
+						</td>
+					</tr>
+				<?php if ($this->row->get('id')) { ?>
+					<tr>
+						<th><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATED'); ?>:</th>
+						<td><?php echo $this->row->get('created'); ?></td>
+					</tr>
+					<tr>
+						<th><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATOR'); ?>:</th>
+						<td><?php echo $this->escape(stripslashes($this->row->creator('name'))); ?></td>
+					</tr>
+				<?php } ?>
+				</tbody>
+			</table>
 
-			<div class="input-wrap" data-hint="<?php echo Lang::txt('COM_ANSWERS_FIELD_TAGS_HINT'); ?>">
-				<label for="field-tags"><?php echo Lang::txt('COM_ANSWERS_FIELD_TAGS'); ?>: <span class="required"><?php echo Lang::txt('JOPTION_REQUIRED'); ?></span></label><br />
-				<textarea name="question[tags]" id="field-tags" cols="50" rows="3"><?php echo $this->escape(stripslashes($this->row->tags('string'))); ?></textarea>
-				<span class="hint"><?php echo Lang::txt('COM_ANSWERS_FIELD_TAGS_HINT'); ?></span>
-			</div>
-		</fieldset>
+			<fieldset class="adminform">
+				<legend><span><?php echo Lang::txt('COM_ANSWERS_PARAMETERS'); ?></span></legend>
+
+				<div class="input-wrap">
+					<label for="field-created_by"><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATOR'); ?>:</label><br />
+					<input type="text" name="question[created_by]" id="field-created_by" size="25" maxlength="50" value="<?php echo $this->row->get('created_by', User::get('id')); ?>" />
+				</div>
+
+				<div class="input-wrap">
+					<label for="field-created"><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATED'); ?>:</label><br />
+					<?php echo Html::input('calendar', 'question[created]', $this->row->get('created', Date::toSql()), array('id' => 'field-created')); ?>
+				</div>
+
+				<div class="input-wrap">
+					<label for="field-state"><?php echo Lang::txt('COM_ANSWERS_FIELD_STATE'); ?>:</label><br />
+					<select name="question[state]" id="field-state">
+						<option value="0"<?php echo ($this->row->get('state') == 0) ? ' selected="selected"' : ''; ?>><?php echo Lang::txt('COM_ANSWERS_STATE_OPEN'); ?></option>
+						<option value="1"<?php echo ($this->row->get('state') == 1) ? ' selected="selected"' : ''; ?>><?php echo Lang::txt('COM_ANSWERS_STATE_CLOSED'); ?></option>
+					</select>
+				</div>
+			</fieldset>
+		</div>
 	</div>
-	<div class="col width-40 fltrt">
-		<table class="meta">
-			<tbody>
-				<tr>
-					<th><?php echo Lang::txt('COM_ANSWERS_FIELD_ID'); ?>:</th>
-					<td>
-						<?php echo $this->row->get('id', 0); ?>
-						<input type="hidden" name="question[id]" value="<?php echo $this->row->get('id'); ?>" />
-					</td>
-				</tr>
-			<?php if ($this->row->get('id')) { ?>
-				<tr>
-					<th><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATED'); ?>:</th>
-					<td><?php echo $this->row->get('created'); ?></td>
-				</tr>
-				<tr>
-					<th><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATOR'); ?>:</th>
-					<td><?php echo $this->escape(stripslashes($this->row->creator('name'))); ?></td>
-				</tr>
-			<?php } ?>
-			</tbody>
-		</table>
-
-		<fieldset class="adminform">
-			<legend><span><?php echo Lang::txt('COM_ANSWERS_PARAMETERS'); ?></span></legend>
-
-			<div class="input-wrap">
-				<label for="field-created_by"><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATOR'); ?>:</label><br />
-				<input type="text" name="question[created_by]" id="field-created_by" size="25" maxlength="50" value="<?php echo $this->row->get('created_by', User::get('id')); ?>" />
-			</div>
-
-			<div class="input-wrap">
-				<label for="field-created"><?php echo Lang::txt('COM_ANSWERS_FIELD_CREATED'); ?>:</label><br />
-				<?php echo Html::input('calendar', 'question[created]', $this->row->get('created', Date::toSql()), array('id' => 'field-created')); ?>
-			</div>
-
-			<div class="input-wrap">
-				<label for="field-state"><?php echo Lang::txt('COM_ANSWERS_FIELD_STATE'); ?>:</label><br />
-				<select name="question[state]" id="field-state">
-					<option value="0"<?php echo ($this->row->get('state') == 0) ? ' selected="selected"' : ''; ?>><?php echo Lang::txt('COM_ANSWERS_STATE_OPEN'); ?></option>
-					<option value="1"<?php echo ($this->row->get('state') == 1) ? ' selected="selected"' : ''; ?>><?php echo Lang::txt('COM_ANSWERS_STATE_CLOSED'); ?></option>
-				</select>
-			</div>
-		</fieldset>
-	</div>
-	<div class="clr"></div>
 
 	<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
 	<input type="hidden" name="controller" value="<?php echo $this->controller; ?>" />
