a271f6ae0f5d1886ba930de44acb5e23315dbd41
Kevin Wojkovich <kevinw@purdue.edu>
2015-10-13 12:37:12 -0400

    [HUBZERO][#8116][PLG_CONTENT_MOLLOM] Logs that the service is not available and continues on.

--- core/plugins/antispam/mollom/Service/Provider.php
+++ core/plugins/antispam/mollom/Service/Provider.php
@@ -155,7 +155,9 @@ class Provider extends AbstractService
 
 		if (!is_array($result) || !isset($result['id']))
 		{
-			throw new Exception('The content moderation system is currently unavailable. Please try again later.');
+			// log the error, continue on instead of throwing exception.
+			error_log('The content moderation system is currently unavailable. Please try again later.');
+			return true;
 		}
 
 		// Check the final spam classification.
