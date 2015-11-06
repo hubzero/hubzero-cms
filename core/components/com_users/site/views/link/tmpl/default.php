7c4a73b64588cd0e951ebdfbbeb1ee6b493c7a16
Sam Wilson <samwilson@purdue.edu>
2015-11-06 11:19:48 -0500

    [COM_USERS] fix up layout/styles on the users link account view

--- core/components/com_users/site/views/link/tmpl/default.php
+++ core/components/com_users/site/views/link/tmpl/default.php
@@ -40,53 +40,55 @@ $step = (int) Request::getInt('step', 1);
 	<h2>Account Setup</h2>
 </header>
 
-<div class="prompt-wrap">
-	<div class="prompt-container prompt1" style="display:<?php echo ($step === 1) ? 'block': 'none'; ?>">
-		<div class="prompt">
-			Have you ever logged into <?php echo $this->sitename; ?> before?
+<section class="section">
+	<div class="prompt-wrap">
+		<div class="prompt-container prompt1" style="display:<?php echo ($step === 1) ? 'block': 'none'; ?>">
+			<div class="prompt">
+				Have you ever logged into <?php echo $this->sitename; ?> before?
+			</div>
+			<div class="responses">
+				<a href="<?php echo Route::url('index.php?option=com_users&view=link&step=2'); ?>">
+					<div data-step="1" class="button next forward">Yes</div>
+				</a>
+				<a href="<?php echo Route::url('index.php?option=com_members&controller=register&task=update'); ?>">
+					<div data-step="1" class="button backwards">No</div>
+				</a>
+			</div>
 		</div>
-		<div class="responses">
-			<a href="<?php echo Route::url('index.php?option=com_users&view=link&step=2'); ?>">
-				<div data-step="1" class="button next forward">Yes</div>
-			</a>
-			<a href="<?php echo Route::url('index.php?option=com_members&controller=register&task=update'); ?>">
-				<div data-step="1" class="button backwards">No</div>
-			</a>
-		</div>
-	</div>
 
-	<div class="prompt-container prompt2" style="display:<?php echo ($step === 2) ? 'block': 'none'; ?>">
-		<div class="prompt">
-			Great! Did you want to link your <?php echo $this->display_name; ?> account to that existing account or create a new account?
+		<div class="prompt-container prompt2" style="display:<?php echo ($step === 2) ? 'block': 'none'; ?>">
+			<div class="prompt">
+				Great! Did you want to link your <?php echo $this->display_name; ?> account to that existing account or create a new account?
+			</div>
+			<div class="responses">
+				<a href="<?php echo Route::url('index.php?option=com_users&view=link&step=3'); ?>">
+					<div data-step="2" class="button next link">Link</div>
+				</a>
+				<a href="<?php echo Route::url('index.php?option=com_members&controller=register&task=update'); ?>">
+					<div data-step="2" class="button create-new">Create new</div>
+				</a>
+			</div>
 		</div>
-		<div class="responses">
-			<a href="<?php echo Route::url('index.php?option=com_users&view=link&step=3'); ?>">
-				<div data-step="2" class="button next link">Link</div>
-			</a>
-			<a href="<?php echo Route::url('index.php?option=com_members&controller=register&task=update'); ?>">
-				<div data-step="2" class="button create-new">Create new</div>
-			</a>
-		</div>
-	</div>
 
-	<div class="prompt-container prompt3" style="display:<?php echo ($step === 3) ? 'block': 'none'; ?>">
-		<div class="prompt">
-			We can do that. Just login with that existing account now and we'll link them up!
-		</div>
-		<div class="responses">
-			<a href="<?php echo Route::url('index.php?option=com_users&view=logout&return=' .
-				base64_encode(
-					Route::url('index.php?option=com_users&view=login&reset=1&return=' .
-						base64_encode(
-							Route::url('index.php?option=com_users&view=login&authenticator=' . $this->hzad->authenticator, false)
-						),
-					false)
-				)); ?>">
-				<div data-step="3" class="button ok">OK</div>
-			</a>
-			<a href="<?php echo Route::url('index.php?option=com_users&view=link&step=2'); ?>">
-				<div data-step="3" class="button previous back">Go back</div>
-			</a>
+		<div class="prompt-container prompt3" style="display:<?php echo ($step === 3) ? 'block': 'none'; ?>">
+			<div class="prompt">
+				We can do that. Just login with that existing account now and we'll link them up!
+			</div>
+			<div class="responses">
+				<a href="<?php echo Route::url('index.php?option=com_users&view=logout&return=' .
+					base64_encode(
+						Route::url('index.php?option=com_users&view=login&reset=1&return=' .
+							base64_encode(
+								Route::url('index.php?option=com_users&view=login&authenticator=' . $this->hzad->authenticator, false)
+							),
+						false)
+					)); ?>">
+					<div data-step="3" class="button ok">OK</div>
+				</a>
+				<a href="<?php echo Route::url('index.php?option=com_users&view=link&step=2'); ?>">
+					<div data-step="3" class="button previous back">Go back</div>
+				</a>
+			</div>
 		</div>
 	</div>
-</div>
+</section>
