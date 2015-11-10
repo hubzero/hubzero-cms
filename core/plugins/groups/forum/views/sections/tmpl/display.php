5edd0ac4f466b6640a9cc29022bafabbbe30062e
Sam Wilson <samwilson@purdue.edu>
2015-11-10 13:25:26 -0500

    [PLG_CRON_FORUM][PLG_GROUP_FORUM] fix up minor 2.0.0-related issues for group forum email digest

--- core/plugins/groups/forum/views/sections/tmpl/display.php
+++ core/plugins/groups/forum/views/sections/tmpl/display.php
@@ -244,7 +244,7 @@ if ($this->config->get('access-manage-section')) {
 				</tbody>
 			</table>
 		</div>
-		<div class="container">
+		<div class="container relative-container">
 			<h3><?php echo Lang::txt('PLG_GROUPS_FORUM_EMAIL_SETTINGS'); ?></h3>
 			<?php if (Component::params('com_groups')->get('email_comment_processing') && $this->config->get('access-view-section')) : ?>
 				<?php if (Component::params('com_groups')->get('enable_forum_email_digest', 0)) : ?>
