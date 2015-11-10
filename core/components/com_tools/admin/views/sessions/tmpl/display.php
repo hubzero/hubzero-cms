e6dcf7bd1c65ef10a1d5b7d765f2c6397b1f670d
Shawn Rice <zooley@purdue.edu>
2015-11-10 11:27:05 +0000

    [NANOHUB][#288858][COM_TOOLS] Display session viewer otherwise shared sessions can appear as duplicated entries

--- core/components/com_tools/admin/views/sessions/tmpl/display.php
+++ core/components/com_tools/admin/views/sessions/tmpl/display.php
@@ -71,18 +71,19 @@ function submitbutton(pressbutton)
 		<thead>
 			<tr>
 				<th scope="col"><input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count($this->rows);?>);" /></th>
-				<th scope="col"><?php echo $this->grid('sort', 'COM_TOOLS_COL_SESSION', 'sessnum', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
-				<th scope="col" class="priority-2"><?php echo $this->grid('sort', 'COM_TOOLS_COL_OWNER', 'username', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
-				<th scope="col" class="priority-4"><?php echo $this->grid('sort', 'COM_TOOLS_COL_STARTED', 'start', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
-				<th scope="col" class="priority-3"><?php echo $this->grid('sort', 'COM_TOOLS_COL_LAST_ACCESSED', 'accesstime', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
-				<th scope="col" class="priority-3"><?php echo $this->grid('sort', 'COM_TOOLS_COL_TOOL', 'appname', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
-				<th scope="col" class="priority-4"><?php echo $this->grid('sort', 'COM_TOOLS_COL_EXEC_HOST', 'exechost', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
+				<th scope="col"><?php echo Html::grid('sort', 'COM_TOOLS_COL_SESSION', 'sessnum', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
+				<th scope="col" class="priority-2"><?php echo Html::grid('sort', 'COM_TOOLS_COL_OWNER', 'username', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
+				<th scope="col" class="priority-2"><?php echo Html::grid('sort', 'COM_TOOLS_COL_VIEWER', 'viewuser', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
+				<th scope="col" class="priority-4"><?php echo Html::grid('sort', 'COM_TOOLS_COL_STARTED', 'start', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
+				<th scope="col" class="priority-3"><?php echo Html::grid('sort', 'COM_TOOLS_COL_LAST_ACCESSED', 'accesstime', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
+				<th scope="col" class="priority-3"><?php echo Html::grid('sort', 'COM_TOOLS_COL_TOOL', 'appname', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
+				<th scope="col" class="priority-4"><?php echo Html::grid('sort', 'COM_TOOLS_COL_EXEC_HOST', 'exechost', @$this->filters['sort_Dir'], @$this->filters['sort']); ?></th>
 				<th scope="col"><?php echo Lang::txt('COM_TOOLS_COL_STOP'); ?></th>
 			</tr>
 		</thead>
 		<tfoot>
 			<tr>
-				<td colspan="8">
+				<td colspan="9">
 					<?php
 					// Initiate paging
 					echo $this->pagination(
@@ -95,58 +96,61 @@ function submitbutton(pressbutton)
 			</tr>
 		</tfoot>
 		<tbody>
-<?php
-if ($this->rows)
-{
-	$i = 0;
-	foreach ($this->rows as $row)
-	{
-?>
-			<tr>
-				<td>
-					<input type="checkbox" name="id[]" id="cb<?php echo $i; ?>" value="<?php echo $row->sessnum; ?>" onclick="isChecked(this.checked, this);" />
-				</td>
-				<td>
-					<span class="editlinktip hasTip" title="<?php echo $this->escape(stripslashes($row->sessname)); ?>::Host: <?php echo $row->exechost; ?>&lt;br /&gt;IP: <?php echo $row->remoteip; ?>">
-						<span><?php echo $this->escape($row->sessnum); ?></span>
-					</span>
-				</td>
-				<td class="priority-2">
-					<a href="<?php echo Route::url('index.php?option=' . $this->option  . '&controller=' . $this->controller . '&username=' . $row->username); ?>">
-						<span><?php echo $this->escape($row->username); ?></span>
-					</a>
-				</td>
-				<td class="priority-4">
-					<time datetime="<?php echo $this->escape($row->start); ?>">
-						<?php echo $this->escape($row->start); ?>
-					</time>
-				</td>
-				<td class="priority-3">
-					<time datetime="<?php echo $this->escape($row->accesstime); ?>">
-						<?php echo $this->escape($row->accesstime); ?>
-					</time>
-				</td>
-				<td class="priority-3">
-					<a class="tool" href="<?php echo Route::url('index.php?option=' . $this->option  . '&controller=' . $this->controller . '&appname=' . $row->appname); ?>">
-						<span><?php echo $this->escape($row->appname); ?></span>
-					</a>
-				</td>
-				<td class="priority-4">
-					<a class="tool" href="<?php echo Route::url('index.php?option=' . $this->option  . '&controller=' . $this->controller . '&exechost=' . $row->exechost); ?>">
-						<span><?php echo $this->escape($row->exechost); ?></span>
-					</a>
-				</td>
-				<td>
-					<a class="state trash" href="<?php echo Route::url('index.php?option=' . $this->option  . '&controller=' . $this->controller . '&task=remove&id[]=' . $row->sessnum . '&' . Session::getFormToken() . '=1'); ?>" title="<?php echo Lang::txt('COM_TOOLS_TERMINATE'); ?>">
-						<span><?php echo Lang::txt('COM_TOOLS_TERMINATE'); ?></span>
-					</a>
-				</td>
-			</tr>
-<?php
-		$i++;
-	}
-}
-?>
+		<?php
+		if ($this->rows)
+		{
+			$i = 0;
+			foreach ($this->rows as $row)
+			{
+				?>
+				<tr>
+					<td>
+						<input type="checkbox" name="id[]" id="cb<?php echo $i; ?>" value="<?php echo $row->sessnum; ?>" onclick="isChecked(this.checked, this);" />
+					</td>
+					<td>
+						<span class="editlinktip hasTip" title="<?php echo $this->escape(stripslashes($row->sessname)); ?>::Host: <?php echo $row->exechost; ?>&lt;br /&gt;IP: <?php echo $row->remoteip; ?>">
+							<span><?php echo $this->escape($row->sessnum); ?></span>
+						</span>
+					</td>
+					<td class="priority-2">
+						<a href="<?php echo Route::url('index.php?option=' . $this->option  . '&controller=' . $this->controller . '&username=' . $row->username); ?>">
+							<span><?php echo $this->escape($row->username); ?></span>
+						</a>
+					</td>
+					<td class="priority-2">
+						<span><?php echo $this->escape($row->viewuser); ?></span>
+					</td>
+					<td class="priority-4">
+						<time datetime="<?php echo $this->escape($row->start); ?>">
+							<?php echo $this->escape($row->start); ?>
+						</time>
+					</td>
+					<td class="priority-3">
+						<time datetime="<?php echo $this->escape($row->accesstime); ?>">
+							<?php echo $this->escape($row->accesstime); ?>
+						</time>
+					</td>
+					<td class="priority-3">
+						<a class="tool" href="<?php echo Route::url('index.php?option=' . $this->option  . '&controller=' . $this->controller . '&appname=' . $row->appname); ?>">
+							<span><?php echo $this->escape($row->appname); ?></span>
+						</a>
+					</td>
+					<td class="priority-4">
+						<a class="tool" href="<?php echo Route::url('index.php?option=' . $this->option  . '&controller=' . $this->controller . '&exechost=' . $row->exechost); ?>">
+							<span><?php echo $this->escape($row->exechost); ?></span>
+						</a>
+					</td>
+					<td>
+						<a class="state trash" href="<?php echo Route::url('index.php?option=' . $this->option  . '&controller=' . $this->controller . '&task=remove&id=' . $row->sessnum . '&' . Session::getFormToken() . '=1'); ?>" title="<?php echo Lang::txt('COM_TOOLS_TERMINATE'); ?>">
+							<span><?php echo Lang::txt('COM_TOOLS_TERMINATE'); ?></span>
+						</a>
+					</td>
+				</tr>
+				<?php
+				$i++;
+			}
+		}
+		?>
 		</tbody>
 	</table>
 
