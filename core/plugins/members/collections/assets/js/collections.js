f57a058711adca5b9d3cb7438725a68caf7341dd
Shawn Rice <zooley@purdue.edu>
2015-12-11 16:15:17 +0000

    [QUBES][#452][PLG_*_COLLECTIONS] Disabling view toggle javascript as it seems to cause confusion. Stick to links.

--- core/plugins/members/collections/assets/js/collections.js
+++ core/plugins/members/collections/assets/js/collections.js
@@ -43,7 +43,7 @@ jQuery(document).ready(function(jq){
 			}
 		});
 
-		var opts = $('.view-options a');
+		/*var opts = $('.view-options a');
 		opts.each(function(i, el) {
 			if ($(this).hasClass('selected') && $(this).hasClass('icon-list')) {
 				isActive = false;
@@ -70,7 +70,7 @@ jQuery(document).ready(function(jq){
 			}
 
 			isActive = !isActive;
-		});
+		});*/
 
 		// Masonry
 		if (isActive) {
