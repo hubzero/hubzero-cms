3a5de64a6f8b4b372943fb648bed2cf7744d3611
Shawn Rice <zooley@purdue.edu>
2015-12-15 16:37:03 +0000

    [HABRI][#646][COM_TAGS] Make sure log entry has data before attempting to decode and use it

--- core/components/com_tags/admin/views/entries/tmpl/edit.php
+++ core/components/com_tags/admin/views/entries/tmpl/edit.php
@@ -75,124 +75,128 @@ if ($this->getError())
 ?>
 
 <form action="<?php echo Route::url('index.php?option=' . $this->option . '&controller=' . $this->controller); ?>" method="post" name="adminForm" id="item-form">
-	<div class="grid">
-		<div class="col span7">
-			<fieldset class="adminform">
-				<legend><span><?php echo Lang::txt('JDETAILS'); ?></span></legend>
+	<div class="col width-60 fltlft">
+		<fieldset class="adminform">
+			<legend><span><?php echo Lang::txt('JDETAILS'); ?></span></legend>
 
-				<div class="input-wrap" data-hint="<?php echo Lang::txt('COM_TAGS_FIELD_ADMIN_HINT'); ?>">
-					<input type="checkbox" name="fields[admin]" id="field-admin" value="1" <?php if ($this->tag->get('admin') == 1) { echo 'checked="checked"'; } ?> />
-					<label for="field-admin"><?php echo Lang::txt('COM_TAGS_FIELD_ADMIN'); ?></label>
-				</div>
+			<div class="input-wrap" data-hint="<?php echo Lang::txt('COM_TAGS_FIELD_ADMIN_HINT'); ?>">
+				<input type="checkbox" name="fields[admin]" id="field-admin" value="1" <?php if ($this->tag->get('admin') == 1) { echo 'checked="checked"'; } ?> />
+				<label for="field-admin"><?php echo Lang::txt('COM_TAGS_FIELD_ADMIN'); ?></label>
+			</div>
 
-				<div class="input-wrap" data-hint="<?php echo Lang::txt('COM_TAGS_FIELD_TAG_HINT'); ?>">
-					<label for="field-raw_tag"><?php echo Lang::txt('COM_TAGS_FIELD_RAW_TAG'); ?>: <span class="required"><?php echo Lang::txt('JOPTION_REQUIRED'); ?></span></label><br />
-					<input type="text" name="fields[raw_tag]" id="field-raw_tag" size="30" maxlength="250" value="<?php echo $this->escape(stripslashes($this->tag->get('raw_tag'))); ?>" />
-					<span class="hint"><?php echo Lang::txt('COM_TAGS_FIELD_TAG_HINT'); ?></span>
-				</div>
+			<div class="input-wrap" data-hint="<?php echo Lang::txt('COM_TAGS_FIELD_TAG_HINT'); ?>">
+				<label for="field-raw_tag"><?php echo Lang::txt('COM_TAGS_FIELD_RAW_TAG'); ?>: <span class="required"><?php echo Lang::txt('JOPTION_REQUIRED'); ?></span></label><br />
+				<input type="text" name="fields[raw_tag]" id="field-raw_tag" size="30" maxlength="250" value="<?php echo $this->escape(stripslashes($this->tag->get('raw_tag'))); ?>" />
+				<span class="hint"><?php echo Lang::txt('COM_TAGS_FIELD_TAG_HINT'); ?></span>
+			</div>
 
-				<div class="input-wrap">
-					<label for="field-tag"><?php echo Lang::txt('COM_TAGS_FIELD_TAG'); ?>:</label><br />
-					<input type="text" disabled="disabled" class="disabled" name="fields[tag]" id="field-tag" placeholder="<?php echo Lang::txt('COM_TAGS_FIELD_TAG_PLACEHOLDER'); ?>" maxlength="250" value="<?php echo $this->escape($this->tag->get('tag')); ?>" />
-				</div>
+			<div class="input-wrap">
+				<label for="field-tag"><?php echo Lang::txt('COM_TAGS_FIELD_TAG'); ?>:</label><br />
+				<input type="text" disabled="disabled" class="disabled" name="fields[tag]" id="field-tag" placeholder="<?php echo Lang::txt('COM_TAGS_FIELD_TAG_PLACEHOLDER'); ?>" maxlength="250" value="<?php echo $this->escape($this->tag->get('tag')); ?>" />
+			</div>
 
-				<div class="input-wrap" data-hint="<?php echo Lang::txt('COM_TAGS_FIELD_ALIAS_HINT'); ?>">
-					<label for="field-substitutions"><?php echo Lang::txt('COM_TAGS_FIELD_ALIAS'); ?>:</label><br />
-					<textarea name="fields[substitutions]" id="field-substitutions" cols="50" rows="5"><?php echo $this->escape(stripslashes($this->tag->substitutes('string', array('limit' => 0)))); ?></textarea>
-					<span class="hint"><?php echo Lang::txt('COM_TAGS_FIELD_ALIAS_HINT'); ?></span>
-				</div>
+			<div class="input-wrap" data-hint="<?php echo Lang::txt('COM_TAGS_FIELD_ALIAS_HINT'); ?>">
+				<label for="field-substitutions"><?php echo Lang::txt('COM_TAGS_FIELD_ALIAS'); ?>:</label><br />
+				<textarea name="fields[substitutions]" id="field-substitutions" cols="50" rows="5"><?php echo $this->escape(stripslashes($this->tag->substitutes('string', array('limit' => 0)))); ?></textarea>
+				<span class="hint"><?php echo Lang::txt('COM_TAGS_FIELD_ALIAS_HINT'); ?></span>
+			</div>
 
-				<div class="input-wrap">
-					<label for="field-description"><?php echo Lang::txt('COM_TAGS_FIELD_DESCRIPTION'); ?>:</label><br />
-					<?php echo $this->editor('fields[description]', stripslashes($this->tag->get('description')), 50, 4, 'field-description', array('class' => 'minimal')); ?>
-				</div>
-			</fieldset>
-		</div>
-		<div class="col span5">
-			<table class="meta">
-				<tbody>
-					<tr>
-						<th><?php echo Lang::txt('COM_TAGS_FIELD_ID'); ?>:</th>
-						<td>
-							<?php echo $this->tag->get('id'); ?>
-							<input type="hidden" name="fields[id]" value="<?php echo $this->tag->get('id'); ?>" />
-						</td>
-					</tr>
-					<tr>
-						<th><?php echo Lang::txt('COM_TAGS_FIELD_CREATOR'); ?>:</th>
-						<td>
-							<?php
-							if (!$this->tag->get('created_by') && $this->tag->exists())
+			<div class="input-wrap">
+				<label for="field-description"><?php echo Lang::txt('COM_TAGS_FIELD_DESCRIPTION'); ?>:</label><br />
+				<?php echo $this->editor('fields[description]', stripslashes($this->tag->get('description')), 50, 4, 'field-description', array('class' => 'minimal')); ?>
+			</div>
+		</fieldset>
+	</div>
+	<div class="col width-40 fltrt">
+		<table class="meta">
+			<tbody>
+				<tr>
+					<th class="key"><?php echo Lang::txt('COM_TAGS_FIELD_ID'); ?>:</th>
+					<td>
+						<?php echo $this->tag->get('id'); ?>
+						<input type="hidden" name="fields[id]" value="<?php echo $this->tag->get('id'); ?>" />
+					</td>
+				</tr>
+				<tr>
+					<th class="key"><?php echo Lang::txt('COM_TAGS_FIELD_CREATOR'); ?>:</th>
+					<td>
+						<?php
+						if (!$this->tag->get('created_by') && $this->tag->exists())
+						{
+							if ($logs = $this->tag->logs('list'))
 							{
-								if ($logs = $this->tag->logs('list'))
+								foreach ($logs as $log)
 								{
-									foreach ($logs as $log)
+									if ($log->get('action') == 'tag_created')
 									{
-										if ($log->get('action') == 'tag_created')
-										{
-											$this->tag->set('created_by', $log->get('user_id'));
-											$this->tag->set('created', $log->get('timestamp'));
-											break;
-										}
+										$this->tag->set('created_by', $log->get('user_id'));
+										$this->tag->set('created', $log->get('timestamp'));
+										break;
 									}
 								}
 							}
-							$name = $this->tag->creator('name');
-							echo $this->escape(($name ? $name : Lang::txt('COM_TAGS_UNKNOWN')));
+						}
+						$name = $this->tag->creator('name');
+						echo $this->escape(($name ? $name : Lang::txt('COM_TAGS_UNKNOWN')));
+						?>
+						<input type="hidden" name="fields[created_by]" id="field-created_by" value="<?php echo $this->escape($this->tag->get('created_by')); ?>" />
+					</td>
+				</tr>
+				<tr>
+					<th class="key"><?php echo Lang::txt('COM_TAGS_FIELD_CREATED'); ?>:</th>
+					<td>
+						<?php echo ($this->tag->created() != '0000-00-00 00:00:00' ? $this->tag->created() : Lang::txt('COM_TAGS_UNKNOWN')); ?>
+						<input type="hidden" name="fields[created]" id="field-created" value="<?php echo $this->escape($this->tag->get('created')); ?>" />
+					</td>
+				</tr>
+				<?php if ($this->tag->exists() && $this->tag->wasModified()) { ?>
+					<tr>
+						<th class="key"><?php echo Lang::txt('COM_TAGS_FIELD_MODIFIER'); ?>:</th>
+						<td>
+							<?php
+							if ($this->tag->get('modified_by'))
+							{
+								$editor = User::getInstance($this->tag->get('modified_by'));
+								echo $this->escape(stripslashes($editor->get('name')));
+							}
+							else
+							{
+								echo Lang::txt('COM_TAGS_UNKNOWN');
+							}
 							?>
-							<input type="hidden" name="fields[created_by]" id="field-created_by" value="<?php echo $this->escape($this->tag->get('created_by')); ?>" />
+							<input type="hidden" name="fields[modified_by]" id="field-modified_by" value="<?php echo $this->escape($this->tag->get('modified_by')); ?>" />
 						</td>
 					</tr>
 					<tr>
-						<th><?php echo Lang::txt('COM_TAGS_FIELD_CREATED'); ?>:</th>
+						<th class="key"><?php echo Lang::txt('COM_TAGS_FIELD_MODIFIED'); ?>:</th>
 						<td>
-							<?php echo ($this->tag->created() != '0000-00-00 00:00:00' ? $this->tag->created() : Lang::txt('COM_TAGS_UNKNOWN')); ?>
-							<input type="hidden" name="fields[created]" id="field-created" value="<?php echo $this->escape($this->tag->get('created')); ?>" />
+							<?php echo ($this->tag->modified() != '0000-00-00 00:00:00' ? $this->tag->modified() : Lang::txt('COM_TAGS_UNKNOWN')); ?>
+							<input type="hidden" name="fields[modified]" id="field-modified" value="<?php echo $this->escape($this->tag->get('modified')); ?>" />
 						</td>
 					</tr>
-					<?php if ($this->tag->exists() && $this->tag->wasModified()) { ?>
-						<tr>
-							<th><?php echo Lang::txt('COM_TAGS_FIELD_MODIFIER'); ?>:</th>
-							<td>
-								<?php
-								if ($this->tag->get('modified_by'))
-								{
-									$editor = User::getInstance($this->tag->get('modified_by'));
-									echo $this->escape(stripslashes($editor->get('name')));
-								}
-								else
-								{
-									echo Lang::txt('COM_TAGS_UNKNOWN');
-								}
-								?>
-								<input type="hidden" name="fields[modified_by]" id="field-modified_by" value="<?php echo $this->escape($this->tag->get('modified_by')); ?>" />
-							</td>
-						</tr>
-						<tr>
-							<th><?php echo Lang::txt('COM_TAGS_FIELD_MODIFIED'); ?>:</th>
-							<td>
-								<?php echo ($this->tag->modified() != '0000-00-00 00:00:00' ? $this->tag->modified() : Lang::txt('COM_TAGS_UNKNOWN')); ?>
-								<input type="hidden" name="fields[modified]" id="field-modified" value="<?php echo $this->escape($this->tag->get('modified')); ?>" />
-							</td>
-						</tr>
-					<?php } ?>
-				</tbody>
-			</table>
+				<?php } ?>
+			</tbody>
+		</table>
 
-			<div class="data-wrap">
-				<?php
-				if ($this->tag->exists())
+		<div class="data-wrap">
+			<?php
+			if ($this->tag->exists())
+			{
+				if ($logs = $this->tag->logs('list'))
 				{
-					if ($logs = $this->tag->logs('list'))
-					{
-						?>
-						<h4><?php echo Lang::txt('COM_TAGS_LOG'); ?></h4>
-						<ul class="entry-log">
-							<?php
-							foreach ($logs as $log)
-							{
-								$actor = $this->escape(stripslashes($log->actor('name')));
+					?>
+					<h4><?php echo Lang::txt('COM_TAGS_LOG'); ?></h4>
+					<ul class="entry-log">
+						<?php
+						foreach ($logs as $log)
+						{
+							$actor = $this->escape(stripslashes($log->actor('name')));
 
+							$s = null;
+							$c = '';
+
+							if ($log->get('comments'))
+							{
 								$data = json_decode($log->get('comments'));
 								if (!is_object($data))
 								{
@@ -206,17 +210,17 @@ if ($this->getError())
 								{
 									case 'substitute_created':
 										$c = 'created';
-										$s = Lang::txt('COM_TAGS_LOG_ALIAS_CREATED', $data->raw_tag, $log->get('timestamp'), $actor);
+										$s = Lang::txt('COM_TAGS_LOG_ALIAS_CREATED', (isset($data->raw_tag) ? $data->raw_tag : ''), $log->get('timestamp'), $actor);
 									break;
 
 									case 'substitute_edited':
 										$c = 'edited';
-										$s = Lang::txt('COM_TAGS_LOG_ALIAS_EDITED', $data->raw_tag, $log->get('timestamp'), $actor);
+										$s = Lang::txt('COM_TAGS_LOG_ALIAS_EDITED', (isset($data->raw_tag) ? $data->raw_tag : ''), $log->get('timestamp'), $actor);
 									break;
 
 									case 'substitute_deleted':
 										$c = 'deleted';
-										$s = Lang::txt('COM_TAGS_LOG_ALIAS_DELETED', implode(', ', $data->tags), $log->get('timestamp'), $actor);
+										$s = Lang::txt('COM_TAGS_LOG_ALIAS_DELETED', (isset($data->raw_tag) ? $data->raw_tag : ''), $log->get('timestamp'), $actor);
 									break;
 
 									case 'substitute_moved':
@@ -256,24 +260,30 @@ if ($this->getError())
 										$s = Lang::txt('COM_TAGS_LOG_TAG_EDITED', str_replace('_', ' ', $log->get('action')), $log->get('timestamp'), $actor);
 									break;
 								}
-								if ($s)
-								{
-									?>
-									<li class="<?php echo $c; ?>">
-										<span class="entry-log-data"><?php echo $s; ?></span>
-									</li>
-									<?php
-								}
 							}
-							?>
-						</ul>
-						<?php
-					}
+							else
+							{
+								$c = 'edited';
+								$s = Lang::txt('COM_TAGS_LOG_TAG_EDITED', str_replace('_', ' ', $log->get('action')), $log->get('timestamp'), $actor);
+							}
+							if ($s)
+							{
+								?>
+								<li class="<?php echo $c; ?>">
+									<span class="entry-log-data"><?php echo $s; ?></span>
+								</li>
+								<?php
+							}
+						}
+						?>
+					</ul>
+					<?php
 				}
-				?>
-			</div>
+			}
+			?>
 		</div>
 	</div>
+	<div class="clr"></div>
 
 	<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
 	<input type="hidden" name="controller" value="<?php echo $this->controller; ?>" />
