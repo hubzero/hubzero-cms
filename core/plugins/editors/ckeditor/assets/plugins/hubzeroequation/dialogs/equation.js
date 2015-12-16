548943ba757fdaf27a39d3def5b8263f1e74cb31
Zach Weidner <zach.weidner@gmail.com>
2015-12-16 16:30:52 +0000

    [QUBES][#394][COM_RESOURCES][CKEDITOR] Added API call to convert latex to png, changed CKEditor hubzeroequation plugin to use this call

--- core/plugins/editors/ckeditor/assets/plugins/hubzeroequation/dialogs/equation.js
+++ core/plugins/editors/ckeditor/assets/plugins/hubzeroequation/dialogs/equation.js
@@ -1,3 +1,6 @@
+Hubzero.initApi();
+var equationChanged;
+var equationLimiter;
 CKEDITOR.dialog.add( 'hubzeroEquationDialog', function( editor ) {
 	return {
 		title: 'Equation Editor',
@@ -13,7 +16,8 @@ CKEDITOR.dialog.add( 'hubzeroEquationDialog', function( editor ) {
 						type: 'textarea',
 						id: 'equationField',
 						label: 'Equation',
-						validate: CKEDITOR.dialog.validate.notEmpty( "Equation field cannot be empty" )
+						validate: CKEDITOR.dialog
+						.validate.notEmpty( "Equation field cannot be empty" )
 					},
 					{
 						type: 'html',
@@ -29,44 +33,70 @@ CKEDITOR.dialog.add( 'hubzeroEquationDialog', function( editor ) {
 		onShow: function() {
 			var sel = editor.getSelection(),
 				image = sel.getStartElement(),
-				latexUrl = 'https://latex.codecogs.com/gif.latex?',
 				equationElement = this.getContentElement('equationbasic', 'equationField'),
 				equationTextarea = $('#' + equationElement.domId).find('textarea'),
 				equationPreview  = $('#equationPreview');
-			
 			if (image.is('img'))
 			{
 				// get source from data attr
-				var equation = image.getAttribute('data-equation');
+				var equation = decodeURIComponent(image.getAttribute('data-equation'));
 
 				// get source from image url
 				if (equation == null)
 				{
 					var source = image.getAttribute('src');
-					source = decodeURIComponent(source);
-					equation = source.replace(latexUrl, '').trim();
+					equation = decodeURIComponent(source);
 				}
 				
 				equationTextarea.val(equation);
-				equationPreview.attr('src', latexUrl + equation);
+				equationPreview.attr('src', image.getAttribute('src'));
+			} else {
+				//we're making a new equation
+				var equation = '';
+				var source = '';
+				
+				equationTextarea.val(equation);
+				equationPreview.attr('src', image.getAttribute('src'));
 			}
 			
 			// show preview
 			$('#' + equationElement.domId).on('keyup', 'textarea', function(event) {
-				var equation = $(this).val();
-				equationPreview.attr('src', latexUrl + equation);
+				equationChanged = true;
 			});
+			
+			equationLimiter = setInterval( function() {
+				if (equationChanged) {
+					var equation = $('#' + equationElement.domId).find('textarea').val();
+					$.ajax({
+						url: "/api/resources/renderlatex",
+						data: "expression="+equation,
+						dataType: "json",
+						cache: false,
+						success: function(json) {
+							if (json.error == '') {
+								//no error, display the image
+								equationPreview.attr('src', json.img);
+							} else {
+								//there was an error, display an error icon
+								equationPreview.attr('src', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAABsUlEQVQ4T62UzytEURTHv+c+jadkjxT5ce9b2Pk7/ChM0mSjFEX8HxQiCwtZSBjK32GnzH0ZDMJeMn7Mu0fvlmnMeM9g7vKecz7nfM+95xC+OVft7e6L6yYBDBBzHzM3h25E9MBEJ0x01JDP73fkci/l4VR+kVFqDMYsQggLiToGuHeY55Xv75X6FIEMOFqpNQBTcaCKipjXpe/PEhBYFZ8OGaU2omBOb691C05Pv81FzOvK92eKQCsT2ImqrOn42Joe+/sjiyfmZCif7AMkEpdxPasGaIC7RKHQSRmlJgBsxfWtGqCVy5wKgYcAhmoBNECatJQ3TNRWCyCMuaZMT887hKirEfCt9sBqJDdubloBT5OTsX/eALmqHuUXk3NAZ56XIubtuKCfJqUkdtx+7GfXvRBAyz8n5dYJgm47y2eelyTm3Shgw9ycNeWXl+OEDHtap4vLQUu5xkTTv+hXqeuqp/Xsl20Tri9fypU/QFeU1gsV6+sznZZyNCBaEkBrXLXEfMtE86HMUr+KjR0az7u66gPHGQmIBoUxfQDs9jZC3AvgBMBRXaGQ7s5mX8uTfgDCabpOjteIBgAAAABJRU5ErkJggg==');
+							}
+							
+						}
+					});
+					equationChanged = false;
+				}
+			}, 2500);
 		},
 		onOk: function() {
-			var latexUrl = 'https://latex.codecogs.com/gif.latex?',
-				equationTextarea = this.getContentElement('equationbasic', 'equationField'),
-				equation = $('#' + equationTextarea.domId).find('textarea').val(),
-				equationImage = editor.document.createElement('img');
-				
+			equationTextarea = this.getContentElement('equationbasic', 'equationField'),
+			equation = $('#' + equationTextarea.domId).find('textarea').val(),
+			equationPreview  = $('#equationPreview');
+			equationImage = editor.document.createElement('img');
+
 			// set attributes and insert
 			equationImage.setAttribute('class', 'hubzeroequation-result');
 			equationImage.setAttribute('data-equation', equation);
-			equationImage.setAttribute('src', latexUrl + equation);
+			equationImage.setAttribute('src', equationPreview.attr('src'));
 			editor.insertElement( equationImage );
 		}
 	}
