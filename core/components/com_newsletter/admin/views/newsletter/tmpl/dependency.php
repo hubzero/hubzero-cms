9f3d847b707a12099c3e153547ea4c7debb46bb1
Zach Weidner <zach.weidner@gmail.com>
2015-12-17 17:43:17 +0000

    [HUBZERO][#8996] Link to cron admin panel added to warning in Newsletter admin panel

--- core/components/com_newsletter/admin/views/newsletter/tmpl/dependency.php
+++ core/components/com_newsletter/admin/views/newsletter/tmpl/dependency.php
@@ -39,4 +39,6 @@ Toolbar::title(Lang::txt('COM_NEWSLETTER'), 'newsletter.png');
 
 <p class="warning">
 	<?php echo Lang::txt('COM_NEWSLETTER_DEPENDENCY'); ?>
+	</br>
+	<a href='<?php echo Route::url("index.php?option=com_cron"); ?>'><?php echo Lang::txt('COM_NEWSLETTER_CHECK_CRON'); ?></a>
 </p>
