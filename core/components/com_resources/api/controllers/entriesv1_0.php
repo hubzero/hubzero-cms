4466a9c61bb889fe61415eb0f1c1d10be73a4449
Zach Weidner <zach.weidner@gmail.com>
2015-12-17 19:22:22 +0000

    [QUBES][#394][COM_RESOURCES][CKEDITOR] Fixed an error in a cleanup command in previous commit

--- core/components/com_resources/api/controllers/entriesv1_0.php
+++ core/components/com_resources/api/controllers/entriesv1_0.php
@@ -153,7 +153,7 @@ class Entriesv1_0 extends ApiController
 		$object->expression = $expression;
 
 		//clean up our cache mess
-		shell_exec('rm' . $dir .DS. $filename . '.*');
+		shell_exec('rm ' . $dir . $filename . '.*');
 		$this->send($object);
 
 	}
