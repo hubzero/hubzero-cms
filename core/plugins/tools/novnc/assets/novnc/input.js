5dd94cb0dd80fc8ed35e1d7a31127778bda84076
Shawn Rice <zooley@purdue.edu>
2016-01-06 15:44:57 +0000

    [PLG_TOOLS_NOVNC] Attempting to fix keyboard/mouse control issue with touch-enabled computers

--- core/plugins/tools/novnc/assets/novnc/input.js
+++ core/plugins/tools/novnc/assets/novnc/input.js
@@ -337,14 +337,14 @@ var Keyboard, Mouse;
                 Util.addEvent(window, 'touchend', this._eventHandlers.mouseup);
                 Util.addEvent(c, 'touchend', this._eventHandlers.mouseup);
                 Util.addEvent(c, 'touchmove', this._eventHandlers.mousemove);
-            } else {
+            }// else {
                 Util.addEvent(c, 'mousedown', this._eventHandlers.mousedown);
                 Util.addEvent(window, 'mouseup', this._eventHandlers.mouseup);
                 Util.addEvent(c, 'mouseup', this._eventHandlers.mouseup);
                 Util.addEvent(c, 'mousemove', this._eventHandlers.mousemove);
                 Util.addEvent(c, (Util.Engine.gecko) ? 'DOMMouseScroll' : 'mousewheel',
                               this._eventHandlers.mousewheel);
-            }
+            //}
 
             /* Work around right and middle click browser behaviors */
             Util.addEvent(document, 'click', this._eventHandlers.mousedisable);
@@ -359,14 +359,14 @@ var Keyboard, Mouse;
                 Util.removeEvent(window, 'touchend', this._eventHandlers.mouseup);
                 Util.removeEvent(c, 'touchend', this._eventHandlers.mouseup);
                 Util.removeEvent(c, 'touchmove', this._eventHandlers.mousemove);
-            } else {
+            }// else {
                 Util.removeEvent(c, 'mousedown', this._eventHandlers.mousedown);
                 Util.removeEvent(window, 'mouseup', this._eventHandlers.mouseup);
                 Util.removeEvent(c, 'mouseup', this._eventHandlers.mouseup);
                 Util.removeEvent(c, 'mousemove', this._eventHandlers.mousemove);
                 Util.removeEvent(c, (Util.Engine.gecko) ? 'DOMMouseScroll' : 'mousewheel',
                                  this._eventHandlers.mousewheel);
-            }
+            //}
 
             /* Work around right and middle click browser behaviors */
             Util.removeEvent(document, 'click', this._eventHandlers.mousedisable);
