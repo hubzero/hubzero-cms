162fb5b9986eb82673730f88441041c44a9564f2
Sam Wilson <samwilson@purdue.edu>
2016-01-20 11:31:08 -0500

    [NANOHUB][#302071][PLG_SYSTEM_INCOMPLETE] add yet another route to the whitelist for accepting terms of use

--- core/plugins/system/incomplete/incomplete.php
+++ core/plugins/system/incomplete/incomplete.php
@@ -54,6 +54,7 @@ class plgSystemIncomplete extends \Hubzero\Plugin\Plugin
 				'com_support.tickets.new.index',
 				'com_members.media.download.profiles',
 				'com_members.save.profiles',
+				'com_members.profiles.save',
 				'com_members.profiles.save.profiles'
 			];
 
