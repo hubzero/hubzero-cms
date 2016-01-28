b391d26f1dca95e838a92f6dcfe593c6ad0faa51
Kevin Wojkovich <kevinw@purdue.edu>
2016-01-28 15:21:52 -0500

    [NANOHUB][#301323][PLG_SYSTEM_UNCONFIRMED] Adds exception to allow users to get resent confirmation email.

--- core/plugins/system/unconfirmed/unconfirmed.php
+++ core/plugins/system/unconfirmed/unconfirmed.php
@@ -56,6 +56,7 @@ class plgSystemUnconfirmed extends \Hubzero\Plugin\Plugin
 				'com_members.register.unconfirmed.profiles',
 				'com_members.register.change.profiles',
 				'com_members.register.resend.profiles',
+				'com_members.register.resend',
 				'com_members.register.confirm.profiles'
 			];
 
