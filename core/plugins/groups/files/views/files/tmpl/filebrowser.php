dfdaf55ce5257e2b9693b8293dd120087e05c1e7
Shawn Rice <zooley@purdue.edu>
2016-02-01 12:21:46 +0000

    [HUBZERO][#9096][PLG_GROUPS_FILES] Reworking CSS to require fewer overrides and fix display with super groups

--- core/plugins/groups/files/views/files/tmpl/filebrowser.php
+++ core/plugins/groups/files/views/files/tmpl/filebrowser.php
@@ -45,46 +45,47 @@ $ckeditor      = Request::getVar('CKEditor', '', 'get');
 $ckeditorFunc  = Request::getInt('CKEditorFuncNum', 0, 'get');
 $ckeditorQuery = '&type='.$type.'&CKEditor=' . $ckeditor . '&CKEditorFuncNum=' . $ckeditorFunc;
 ?>
+<div class="files-wrap">
+	<div class="upload-browser cf">
+		<?php
+			foreach ($this->notifications as $notification)
+			{
+				echo "<p class=\"{$notification['type']}\">{$notification['message']}</p>";
+			}
+		?>
 
-<div class="upload-browser cf">
-	<?php
-		foreach ($this->notifications as $notification)
-		{
-			echo "<p class=\"{$notification['type']}\">{$notification['message']}</p>";
-		}
-	?>
-
-	<div class="upload-browser-col left">
-		<div class="toolbar cf">
-			<div class="title"><?php echo Lang::txt('COM_GROUPS_MEDIA_GROUP_FILES'); ?></div>
-			<div class="buttons">
-				<a href="<?php echo Route::url('index.php?option=com_groups&cn='.$this->group->get('cn').'&controller=media&task=addfolder&tmpl=component'); ?>" class="icon-add action-addfolder"><?php echo Lang::txt('Add folder'); ?></a>
-			</div>
-		</div>
-		<div class="foldertree" data-activefolder="<?php echo $this->activeFolder; ?>">
-			<?php echo $this->folderTree; ?>
-		</div>
-		<div class="foldertree-list">
-			<?php echo $this->folderList; ?>
-		</div>
-		<form action="<?php echo Route::url('index.php?option=' . $this->option); ?>" method="post" enctype="multipart/form-data" class="upload-browser-uploader">
-			<fieldset>
-				<div id="ajax-uploader" data-instructions="<?php echo Lang::txt('Click or drop file'); ?>" data-action="<?php echo Route::url('index.php?option=com_groups&cn='.$this->group->get('cn').'&controller=media&task=ajaxupload&no_html=1&' . Session::getFormToken() . '=1'); ?>">
-					<noscript>
-						<p><input type="file" name="upload" id="upload" /></p>
-						<p><input type="submit" value="<?php echo Lang::txt('UPLOAD'); ?>" /></p>
-					</noscript>
+		<div class="upload-browser-col left">
+			<div class="toolbar cf">
+				<div class="title"><?php echo Lang::txt('COM_GROUPS_MEDIA_GROUP_FILES'); ?></div>
+				<div class="buttons">
+					<a href="<?php echo Route::url('index.php?option=com_groups&cn='.$this->group->get('cn').'&controller=media&task=addfolder&tmpl=component'); ?>" class="icon-add action-addfolder"><?php echo Lang::txt('Add folder'); ?></a>
 				</div>
-				<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
-				<input type="hidden" name="controller" value="media" />
-				<input type="hidden" name="task" value="upload" />
-				<input type="hidden" name="listdir" id="listdir" value="<?php echo $this->group->get('gidNumber'); ?>" />
-				<input type="hidden" name="tmpl" value="component" />
-				<?php echo Html::input('token'); ?>
-			</fieldset>
-		</form>
-	</div>
-	<div class="upload-browser-col right">
-		<iframe class="upload-browser-filelist-iframe" src="<?php echo Route::url('index.php?option=com_groups&cn='.$this->group->get('cn').'&controller=media&task=listfiles&tmpl=component&type=' . $ckeditorQuery); ?>"></iframe>
+			</div>
+			<div class="foldertree" data-activefolder="<?php echo $this->activeFolder; ?>">
+				<?php echo $this->folderTree; ?>
+			</div>
+			<div class="foldertree-list">
+				<?php echo $this->folderList; ?>
+			</div>
+			<form action="<?php echo Route::url('index.php?option=' . $this->option); ?>" method="post" enctype="multipart/form-data" class="upload-browser-uploader">
+				<fieldset>
+					<div id="ajax-uploader" data-instructions="<?php echo Lang::txt('Click or drop file'); ?>" data-action="<?php echo Route::url('index.php?option=com_groups&cn='.$this->group->get('cn').'&controller=media&task=ajaxupload&no_html=1&' . Session::getFormToken() . '=1'); ?>">
+						<noscript>
+							<p><input type="file" name="upload" id="upload" /></p>
+							<p><input type="submit" value="<?php echo Lang::txt('UPLOAD'); ?>" /></p>
+						</noscript>
+					</div>
+					<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
+					<input type="hidden" name="controller" value="media" />
+					<input type="hidden" name="task" value="upload" />
+					<input type="hidden" name="listdir" id="listdir" value="<?php echo $this->group->get('gidNumber'); ?>" />
+					<input type="hidden" name="tmpl" value="component" />
+					<?php echo Html::input('token'); ?>
+				</fieldset>
+			</form>
+		</div>
+		<div class="upload-browser-col right">
+			<iframe class="upload-browser-filelist-iframe" src="<?php echo Route::url('index.php?option=com_groups&cn='.$this->group->get('cn').'&controller=media&task=listfiles&tmpl=component&type=' . $ckeditorQuery); ?>"></iframe>
+		</div>
 	</div>
 </div>
