1da482bb2650a7fd7b11a7816c318439ee840e47
Shawn Rice <zooley@purdue.edu>
2016-02-03 12:31:07 +0000

    [CDMHUB][#910][COM_BLOG] Fixing issue with calculating permissions and allow users who can manage blogs access to edit/delete on front-end

--- core/components/com_blog/site/views/entries/tmpl/display.php
+++ core/components/com_blog/site/views/entries/tmpl/display.php
@@ -126,7 +126,7 @@ $rows = $this->model->entries('list', $this->filters);
 									<a href="<?php echo Route::url($row->link()); ?>">
 										<?php echo $this->escape(stripslashes($row->get('title'))); ?>
 									</a>
-									<?php if (User::get('id') == $row->get('created_by')) { ?>
+									<?php if (User::get('id') == $row->get('created_by') || User::authorise('core.manage', $this->option)) { ?>
 										<a class="edit" href="<?php echo Route::url($row->link('edit')); ?>" title="<?php echo Lang::txt('COM_BLOG_EDIT'); ?>">
 											<?php echo Lang::txt('COM_BLOG_EDIT'); ?>
 										</a>
