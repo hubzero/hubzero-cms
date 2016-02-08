6fcb0861562c43401a775c3d3601371aedb0348a
Kevin Wojkovich <kevinw@purdue.edu>
2016-02-08 14:25:29 -0500

    [COM_FEEDAGGREGATOR] Rudimentary ORM for posts.

---
+++ core/components/com_feedaggregator/models/orm/post.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 HUBzero Foundation, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author	Kevin Wojkovich <kevinw@purdue.edu>
+ * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @since	 Class available since release 1.3.2
+ */
+
+namespace Components\Feedaggregator\Models\Orm;
+
+use Hubzero\Database\Relational;
+use Hubzero\Utility\String;
+use Hubzero\Base\Object;
+
+/**
+ * Hubs database model
+ *
+ * @uses \Hubzero\Database\Relational
+ */
+class Post extends Relational
+{
+	/**
+	 * The table namespace
+	 *
+	 * @var string
+	 **/
+	protected $namespace = 'feedaggregator';
+
+	/**
+	 * Default order by for model
+	 *
+	 * @var string
+	 **/
+	public $orderBy = 'id';
+
+	/**
+	 * Fields and their validation criteria
+	 *
+	 * @var array
+	 **/
+	protected $rules = array(
+		'title' => 'notempty'
+	);
+
+	/**
+	 * Automatically fillable fields
+	 *
+	 * @var array
+	 **/
+	public $always = array(
+	);
+
+	public static function getLatest($limit = 10, $dateField = 'created', $sort = 'DESC')
+	{
+		$rows = Posts::all()->where('standalone', '=', '1')->order($dateField, $sort)->limit($limit);
+
+		return $rows;
+	}
+}
