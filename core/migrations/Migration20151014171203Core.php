d46a01eed7a69a879d81cfa8c853598dee4b359e
Sam Wilson <samwilson@purdue.edu>
2016-02-10 10:27:42 -0500

    [HUBZERO][#9061][PLG_AUTHENTICATION_*] pull all plugins, not just enabled ones

--- core/migrations/Migration20151014171203Core.php
+++ core/migrations/Migration20151014171203Core.php
@@ -15,7 +15,9 @@ class Migration20151014171203Core extends Base
 		// If a plugin is enabled, we'll assume that it should also be enabled for site login if not already saved
 		// We changed the CMS to enforce the configuration option of site_login enabled.  Prior to this, it was
 		// assuming true.  We need to default the database to emulate current behavior.
-		$plugins = Plugin::byType('authentication');
+		$query = "SELECT `element`, `params` FROM `#__extensions` WHERE `type` = 'plugin' AND `folder` = 'authentication'";
+		$this->db->setQuery($query);
+		$plugins = $this->db->loadObjectList();
 
 		if (count($plugins) > 0)
 		{
@@ -32,7 +34,7 @@ class Migration20151014171203Core extends Base
 				{
 					$params->site_login = "1";
 
-					$this->saveParams('plg_authentication_' . $plugin->name, (array) $params);
+					$this->saveParams('plg_authentication_' . $plugin->element, (array) $params);
 				}
 			}
 		}
