70e08f34797f7018e594cabcde62c158b0fae5d9
Shawn Rice <zooley@purdue.edu>
2016-02-15 13:06:06 +0000

    [COM_USERS] Fixing some styling issues and invalid usage of view helper (not a Hubzero view object so no view helpers to ... help)

--- core/components/com_users/admin/views/debuguser/tmpl/default.php
+++ core/components/com_users/admin/views/debuguser/tmpl/default.php
@@ -15,66 +15,66 @@ Html::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 // Load the tooltip behavior.
 Html::behavior('tooltip');
 
-$listOrder	= $this->escape($this->state->get('list.ordering'));
-$listDirn	= $this->escape($this->state->get('list.direction'));
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
 ?>
 
 <form action="<?php echo Route::url('index.php?option=com_users&view=debuguser&user_id='.(int) $this->state->get('filter.user_id'));?>" method="post" name="adminForm" id="adminForm">
 	<fieldset id="filter-bar">
-		<div class="filter-search fltlft">
-			<label class="filter-search-lbl" for="filter_search"><?php echo Lang::txt('COM_USERS_SEARCH_ASSETS'); ?></label>
-			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" placeholder="<?php echo Lang::txt('COM_USERS_SEARCH_USERS'); ?>" />
-			<button type="submit"><?php echo Lang::txt('JSEARCH_FILTER_SUBMIT'); ?></button>
-			<button type="button" onclick="$('#filter_search').val('');this.form.submit();"><?php echo Lang::txt('JSEARCH_RESET'); ?></button>
-		</div>
+		<div class="grid">
+			<div class="filter-search col span5">
+				<label class="filter-search-lbl" for="filter_search"><?php echo Lang::txt('COM_USERS_SEARCH_ASSETS'); ?></label>
+				<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" placeholder="<?php echo Lang::txt('COM_USERS_SEARCH_USERS'); ?>" />
+				<button type="submit"><?php echo Lang::txt('JSEARCH_FILTER_SUBMIT'); ?></button>
+				<button type="button" onclick="$('#filter_search').val('');this.form.submit();"><?php echo Lang::txt('JSEARCH_RESET'); ?></button>
+			</div>
+			<div class="filter-select col span7">
+				<select name="filter_component" class="inputbox" onchange="this.form.submit()">
+					<option value=""><?php echo Lang::txt('COM_USERS_OPTION_SELECT_COMPONENT');?></option>
+					<?php if (!empty($this->components)) {
+						echo Html::select('options', $this->components, 'value', 'text', $this->state->get('filter.component'));
+					}?>
+				</select>
 
-		<div class="filter-select fltrt">
-			<select name="filter_component" class="inputbox" onchange="this.form.submit()">
-				<option value=""><?php echo Lang::txt('COM_USERS_OPTION_SELECT_COMPONENT');?></option>
-				<?php if (!empty($this->components)) {
-					echo Html::select('options', $this->components, 'value', 'text', $this->state->get('filter.component'));
-				}?>
-			</select>
+				<select name="filter_level_start" class="inputbox" onchange="this.form.submit()">
+					<option value=""><?php echo Lang::txt('COM_USERS_OPTION_SELECT_LEVEL_START');?></option>
+					<?php echo Html::select('options', $this->levels, 'value', 'text', $this->state->get('filter.level_start'));?>
+				</select>
 
-			<select name="filter_level_start" class="inputbox" onchange="this.form.submit()">
-				<option value=""><?php echo Lang::txt('COM_USERS_OPTION_SELECT_LEVEL_START');?></option>
-				<?php echo Html::select('options', $this->levels, 'value', 'text', $this->state->get('filter.level_start'));?>
-			</select>
-
-			<select name="filter_level_end" class="inputbox" onchange="this.form.submit()">
-				<option value=""><?php echo Lang::txt('COM_USERS_OPTION_SELECT_LEVEL_END');?></option>
-				<?php echo Html::select('options', $this->levels, 'value', 'text', $this->state->get('filter.level_end'));?>
-			</select>
+				<select name="filter_level_end" class="inputbox" onchange="this.form.submit()">
+					<option value=""><?php echo Lang::txt('COM_USERS_OPTION_SELECT_LEVEL_END');?></option>
+					<?php echo Html::select('options', $this->levels, 'value', 'text', $this->state->get('filter.level_end'));?>
+				</select>
+			</div>
 		</div>
 	</fieldset>
 
-	<div>
-		<?php echo Lang::txt('COM_USERS_DEBUG_LEGEND'); ?>
-		<span class="swatch"><?php echo Lang::txt('COM_USERS_DEBUG_NO_CHECK', '-');?></span>
-		<span class="check-0 swatch"><?php echo Lang::txt('COM_USERS_DEBUG_IMPLICIT_DENY', '-');?></span>
-		<span class="check-a swatch"><?php echo Lang::txt('COM_USERS_DEBUG_EXPLICIT_ALLOW', '&#10003;');?></span>
-		<span class="check-d swatch"><?php echo Lang::txt('COM_USERS_DEBUG_EXPLICIT_DENY', '&#10007;');?></span>
-	</div>
-
 	<table class="adminlist">
+		<caption>
+			<?php echo Lang::txt('COM_USERS_DEBUG_LEGEND'); ?>
+			<span class="swatch"><?php echo Lang::txt('COM_USERS_DEBUG_NO_CHECK', '-');?></span>
+			<span class="check-0 swatch"><?php echo Lang::txt('COM_USERS_DEBUG_IMPLICIT_DENY', '-');?></span>
+			<span class="check-a swatch"><?php echo Lang::txt('COM_USERS_DEBUG_EXPLICIT_ALLOW', '&#10003;');?></span>
+			<span class="check-d swatch"><?php echo Lang::txt('COM_USERS_DEBUG_EXPLICIT_DENY', '&#10007;');?></span>
+		</caption>
 		<thead>
 			<tr>
-				<th class="left">
-					<?php echo $this->grid('sort', 'COM_USERS_HEADING_ASSET_TITLE', 'a.title', $listDirn, $listOrder); ?>
+				<th>
+					<?php echo Html::grid('sort', 'COM_USERS_HEADING_ASSET_TITLE', 'a.title', $listDirn, $listOrder); ?>
 				</th>
-				<th class="left">
-					<?php echo $this->grid('sort', 'COM_USERS_HEADING_ASSET_NAME', 'a.name', $listDirn, $listOrder); ?>
+				<th>
+					<?php echo Html::grid('sort', 'COM_USERS_HEADING_ASSET_NAME', 'a.name', $listDirn, $listOrder); ?>
 				</th>
 				<?php foreach ($this->actions as $key => $action) : ?>
-				<th width="5%">
-					<span class="hasTip" title="<?php echo htmlspecialchars(Lang::txt($key).'::'.Lang::txt($action[1]), ENT_COMPAT, 'UTF-8'); ?>"><?php echo Lang::txt($key); ?></span>
-				</th>
+					<th>
+						<span class="hasTip" title="<?php echo htmlspecialchars(Lang::txt($key).'::'.Lang::txt($action[1]), ENT_COMPAT, 'UTF-8'); ?>"><?php echo Lang::txt($key); ?></span>
+					</th>
 				<?php endforeach; ?>
-				<th class="nowrap" width="5%">
-					<?php echo $this->grid('sort', 'COM_USERS_HEADING_LFT', 'a.lft', $listDirn, $listOrder); ?>
+				<th>
+					<?php echo Html::grid('sort', 'COM_USERS_HEADING_LFT', 'a.lft', $listDirn, $listOrder); ?>
 				</th>
-				<th class="nowrap" width="3%">
-					<?php echo $this->grid('sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+				<th>
+					<?php echo Html::grid('sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
 				</th>
 			</tr>
 		</thead>
@@ -91,26 +91,26 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 				<td>
 					<?php echo $this->escape($item->title); ?>
 				</td>
-				<td class="nowrap">
-					<?php echo str_repeat('<span class="gi">|&mdash;</span>', $item->level) ?>
+				<td>
+					<?php echo str_repeat('<span class="gi">|&mdash;</span>', $item->level); ?>
 					<?php echo $this->escape($item->name); ?>
 				</td>
 				<?php foreach ($this->actions as $action) : ?>
 					<?php
-					$name	= $action[0];
-					$check	= $item->checks[$name];
+					$name  = $action[0];
+					$check = $item->checks[$name];
 					if ($check === true) :
-						$class	= 'check-a';
-						$text	= '&#10003;';
+						$class = 'check-a';
+						$text  = '<span class="state yes"><span>&#10003;</span></span>';
 					elseif ($check === false) :
-						$class	= 'check-d';
-						$text	= '&#10007;';
+						$class = 'check-d';
+						$text  = '<span class="state no"><span>&#10007;</span></span>';
 					elseif ($check === null) :
-						$class	= 'check-0';
-						$text	= '-';
+						$class = 'check-0';
+						$text  = '-';
 					else :
-						$class	= '';
-						$text	= '&#160;';
+						$class = '';
+						$text  = '&#160;';
 					endif;
 					?>
 				<td class="center <?php echo $class;?>">
