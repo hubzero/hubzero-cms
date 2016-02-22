7ea961ec1be05cbd62ef994ae18c5564de3480da
Kevin Wojkovich <kevinw@purdue.edu>
2016-02-22 14:50:27 -0500

    [COM_SEARCH] Cleans up controller, removes unnesseary methods, start using Hubzero/Search class

--- core/components/com_search/admin/controllers/solr.php
+++ core/components/com_search/admin/controllers/solr.php
@@ -32,49 +32,50 @@
 namespace Components\Search\Admin\Controllers;
 
 use Hubzero\Component\AdminController;
-use \Solarium;
-use \Hubzero\Utility\Sanitize;
+use \Hubzero\Search\Search;
+use \Hubzero\Search\SearchDocument;
 use Filesystem;
-use \Components\Search\Helpers\SearchHelper as Helper;
-use \Components\Search\Helpers\SolrEngine as SearchEngine;
 use \Components\Search\Models\Noindex;
-use \Components\Search\Models\HubType;
 use stdClass;
 
 /**
- *
+ * Solr AdminController Class
  */
 class Solr extends AdminController
 {
+	/**
+	 * adapterClass - the class path for the adapter used for this controller
+	 *
+	 * @var string
+	 * @access protected
+	 */
+	protected $adapterClass = 'Solr';
+
 	/**
 	 * Display the overview
 	 */
 	public function displayTask()
 	{
-
 		foreach ($this->getErrors() as $error)
 		{
 			$this->view->setError($error);
 		}
 
-		$hubSearch = new SearchEngine();
-		$hubSearch->getLog();
-		$this->view->logs = array_slice($hubSearch->logs, -10, 10, true);
+		// Instantiate Search class
+		$search = new Search($this->adapterClass);
 
-		// Need to fix permissions, www-data must be able to invoke start
-		//$hubSearch->start();
+		$this->view->logs = array_slice($search->getLog(), -10, 10, true);
 
-		$helper = new Helper;
-		$dataTypes = $helper->fetchDataTypes($hubSearch->getConfig());
-		$insertTime = Date::of($hubSearch->lastInsert())->format('relative');
+		$insertTime = Date::of($search->lastInsert())->format('relative');
 
-		$this->view->status = $hubSearch->ping();
+		$this->view->status = $search->status();
 		$this->view->lastInsert = $insertTime;
 		$this->view->setLayout('overview');
 
 		// Display the view
 		$this->view->display();
 	}
+
 	public function searchIndexTask()
 	{
 		// Display CMS errors
@@ -83,286 +84,31 @@ class Solr extends AdminController
 			$this->view->setError($error);
 		}
 
-		// Instantiate search engine
-		$hubSearch = new SearchEngine();
-
+		$search = new Search;
+		$searchDocument = new SearchDocument;
 		// Get all of the registered hubtypes
-		$this->view->types = HubType::all()->rows();
+		$this->view->types = $searchDocument->getHubTypes();
 
 		// Count indexed documents
-		foreach ($this->view->types as $type)
+		foreach ($this->view->types as &$type)
 		{
 			// Search across everything
 			$queryString = '*:*';
 
 			// Perform an overall search
-			$search = $hubSearch->search($queryString)->addFacet('count', 'hubtype:'.$type->type);
+			$search = $search->setQuery($queryString)->addFacet('count', 'hubtype:'.$type['hubtype']);
 
 			// Get the result, but don't chain it back
-			$result = $search->getResult();
+			$result = $search->runQuery();
 
 			// Get facet count
-			$type->docCount = $search->getFacetCount('count');
-
-			// Fields description 
-			$type->structure = $type->structure();
+			$type['docCount'] = $search->getFacetCount('count');
 		}
 
 		// Display the view
 		$this->view->display();
 	}
-	public function documentByTypeTask()
-	{
-		foreach ($this->getErrors() as $error)
-		{
-			$this->view->setError($error);
-		}
 
-		$additionalQuery = Request::getVar('filter', '');
-		$type = Request::getVar('type', '');
-
-		if ($type != '')
-		{
-			// Instatiate new Engine Class
-			$hubSearch = new SearchEngine();
-
-			// Automatically apply type filter
-			$queryString = 'hubtype:' . $type;
-
-			// For filtering within results
-			if ($additionalQuery != '')
-			{
-				$queryString .= ' AND ' . $additionalQuery;
-				$this->view->filter = $additionalQuery;
-			}
-			else
-			{
-				$this->view->filter = '';
-			}
-
-			// @FIXME acertain from real document type.
-			$fields = array('hubtype', 'hubid', 'scope', 'scope_id', 'id', 'title', 'author', 'created', 'timestamp', '_version_');
-
-			// Apply the query and push the results to the view.
-			$hubSearch->search($queryString);
-			$hubSearch->setFields($fields);
-			$this->view->documents = $hubSearch->getResult();
-			$this->view->type = $type;
-
-			// Display the document
-			$this->view->setLayout('document');
-			$this->view->display();
-		}
-		else
-		{
-			App::redirect(
-			Route::url('index.php?option=' . $this->_option . '&controller=' . $this->_controller. '&task=searchindex', false), 'Failed to locate HubType.', 'error');
-		}
-	}
-	public function addTypeTask()
-	{
-		// New type
-		$model = HubType::blank();
-		$this->view->model = $model->getStructure()->getTableColumns($model->getTableName());
-
-		// Display the view
-		$this->view->setLayout('addHubType');
-		$this->view->display();
-	}
-	public function editTypeTask()
-	{
-
-	}
-	public function saveHubTypeTask()
-	{
-		// Get Variables
-		$name = Request::getVar('type', '');
-		$class_path = Request::getVar('class_path', '');
-		$file_path = Request::getVar('file_path', '');
-		$row = Request::getVar('id', 0);
-
-		$model = HubType::oneOrNew($row);
-		$model->set('class_path', $class_path);
-		$model->set('file_path', $file_path);
-		$model->set('type', $name);
-
-		if (!$model->save())
-		{
-			// Fail
-			App::redirect(
-			Route::url('index.php?option=' . $this->_option . '&controller=' . $this->_controller. '&task=searchIndex', false),
-			 'Failed to add HubType.', 'error');
-		}
-			// Success
-			App::redirect(
-			Route::url('/administrator/index.php?option=' . $this->_option . '&controller=' . $this->_controller. '&task=searchIndex', false),
-			'Successfully added '. $name .   ' to the search index.', 'success');
-	}
-	public function deleteDocumentTask()
-	{
-		$id = Request::getVar('docID', 0);
-		$hubid = Request::getVar('hubid', 0);
-		$type = Request::getVar('type', '');
-		$hubSearch = new SearchEngine();
-		$noIndex = new Noindex;
-		$noIndex = $noIndex->oneOrNew(0);
-		$noIndex->set('hubtype', $type);
-		$noIndex->set('hubid', $hubid);
-
-		if ($hubSearch->delete($id) === TRUE && $noIndex->save())
-		{
-			App::redirect(
-			Route::url('index.php?option=' . $this->_option . '&controller=' . $this->_controller. '&task=documentByType&type='.$type, false), 'Successfully deleted Document ID: ' . $id . '.', 'success');
-		}
-		elseif ($type != '')
-		{
-			App::redirect(
-			Route::url('index.php?option=' . $this->_option . '&controller=' . $this->_controller. '&task=documentByType&type='. $type, false), 'Failed to Document ID: ' . $id . '.', 'error');
-		}
-	}
-	public function addDocumentTask()
-	{
-		// @TODO Verify the HubType is not new, use serialized params input to load last settings.
-		$type = Request::getVar('type', '');
-
-		if ($type != '')
-		{
-			//$hubSearch = new SearchEngine();
-			$helper = new Helper;
-			$this->view->typeStructure = $helper->fetchDataTypes(null, $type);
-			$searchDocument = array(
-				//'id' => '', // Not accessible / managed via user
-				'title' => '',
-				'doi' => '',
-				'isbn' => '',
-				'author' => '',
-				'created' => '', // According to the DB
-				'modified' => '', // According to the DB
-				'scope' => '', // From the DB
-				'scope_id' => '',  // From the DB
-				'fulltext' => '',
-				'description' => '',
-				'abstract' => '',
-				'location' => '',
-				'uid' => '',
-				//'hubtype' => '', // Not accessible
-				'note' => '',
-				'keywords' => '', // Could be tags if nothing, or differ
-				'tags' => '',
-				'badge' => '',
-				'date' => '',
-				'year' => '',
-				'month' => '',
-				'day' => '',
-				'access-level' => '', //
-				'access-group' => '', //
-				'permission-level' => '', // All, registered, group-members, project members,
-				'hub-assoc' => '', // Component, Module, or Plugin which inserts the data
-				'organization' => '',
-				'url' => '', // if the content is off-site
-				'cms-ranking' => '',
-				'cms-rating' => '',
-				'params' => '',
-				'hubID' => '', // the ID of the content within the HUB (differs from the Solr ID)
-				'hubURL' => '', // how to view the record on the hub
-				'cms-state' => '' // published, trashed, hidden, etc.
-													);
-
-			$this->view->type = $type;
-			$this->view->hubDocument = $searchDocument;
-			$this->view->setLayout('addDocuments');
-			$this->view->display();
-		}
-	}
-	public function saveSchemaTask()
-	{
-		/**
-		 * This method depends on an array of pairings
-		 * which match the Solr Schema to database fields.
-		 * @TODO Save serialized input array to Hubtype params field
-		 **/
-		$pairings = Request::getVar('input', array());
-
-		// The Hubtype
-		$type = Request::getVar('type', '');
-
-		// Instatiate helper
-		$helper = new Helper;
-		$rows = $helper->fetchHubTypeRows($type);
-
-		// Some informative counters
-		$docCount = 0;
-		$updateCount = 0;
-		$noIndexCount = 0;
-		$removedCount = 0;
-
-		// The blacklist
-		$noIndex = new Noindex;
-		$noIndex = $noIndex->all()->where('hubtype', '=', $type)->rows();
-		$blacklist = $noIndex->fieldsByKey('hubid');
-
-		foreach ($rows as $row)
-		{
-			$id = (get_class($row) != 'stdClass'? $row->get('id') : $row->id);
-
-			if (!in_array($id, $blacklist))
-			{
-				$hubSearch = new SearchEngine();
-				$document = new stdClass;
-
-				foreach ($pairings as $solrField => $hubField)
-				{
-					if (get_class($row) != 'stdClass')
-					{
-						$document->$solrField = $row->get($hubField, '');
-					}
-					else
-					{
-						$document->$solrField = $row->$hubField;
-					}
-				}
-				$document->hubtype = $type;
-
-				if (get_class($row) == 'stdClass')
-				{
-					$document->hubid = $row->id;
-				}
-				else
-				{
-					$document->hubid = $row->get('id');
-				}
-
-				// Check for duplicates, updated 
-				$query = 'hubtype:'.$type.' AND hubid:'.$document->hubid;
-				$duplicates = $hubSearch->search($query)->getResult();
-
-				if ($duplicates->count() == 0)
-				{
-					$solrID = NULL;
-					$docCount++;
-				}
-				elseif ($duplicates->count() > 0)
-				{
-					foreach ($duplicates->getDocuments() as $dup)
-					{
-						$solrID = $dup->getFields()['id'];
-						$updateCount++;
-					}
-				}
-				else
-				{
-					$solrID = NULL;
-				}
-				$hubSearch->add($document, $solrID);
-			} // end conditional 
-			else
-			{
-				$noIndexCount++;
-			}
-		}
-			App::redirect(
-			Route::url('index.php?option=' . $this->_option . '&controller=' . $this->_controller. '&task=documentByType&type='.$type, false), 'Successfully indexed ' . $docCount. ' documents; updated ' . $updateCount . ' documents; ignored '.$noIndexCount. ' documents.', 'success');
-	}
 	public function manageBlacklistTask()
 	{
 		$this->view->blacklist = NoIndex::all()->rows();
