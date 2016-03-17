d8e3d2c04430a447866c78a32051e3c8e5df5667
Sam Wilson <samwilson@purdue.edu>
2016-03-17 15:31:32 -0400

    [PLG_PROJECTS_FILES] parameterize handler base path for project file tool launching

--- core/plugins/projects/files/views/connected/tmpl/_items.php
+++ core/plugins/projects/files/views/connected/tmpl/_items.php
@@ -36,6 +36,8 @@ use \Components\Tools\Models\Orm\Handler;
 
 // No direct access
 defined('_HZEXEC_') or die();
+
+$handlerBase = DS . trim($this->config->get('handler_base_path','srv' . DS . 'projects'), DS) . DS;
 ?>
 
 <?php foreach ($this->items as $item) : ?>
@@ -49,7 +51,7 @@ defined('_HZEXEC_') or die();
 		<td class="middle_valign nobsp is-relative">
 			<?php echo \Components\Projects\Models\File::drawIcon($item->getExtension()); ?>
 			<?php if ($item->isFile()) : ?>
-				<div class="file-action-dropdown<?php echo ($handlers = Handler::getLaunchUrlsForFile(DS . 'srv' . DS . 'projects' . DS . $this->model->get('alias') . DS . $item->getPath())) ? ' hasMultiple' : ''; ?>">
+				<div class="file-action-dropdown<?php echo ($handlers = Handler::getLaunchUrlsForFile($handlerBase . $this->model->get('alias') . DS . $item->getPath())) ? ' hasMultiple' : ''; ?>">
 					<a href="<?php echo Route::url($this->model->link('files') . '&action=download&connection=' . $this->connection->id . $subdirPath . '&asset=' . urlencode($item->getName())); ?>" class="preview file:<?php echo urlencode($item->getName()); ?>">
 						<?php echo \Components\Projects\Helpers\Html::shortenFileName($item->getName(), 60); ?>
 					</a>
