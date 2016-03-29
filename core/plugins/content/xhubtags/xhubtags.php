e27121483f7af76446a70b11f7d26712e7d7e813
Shawn Rice <zooley@purdue.edu>
2016-03-29 18:08:49 +0000

    [PLG_CONTENT_XHUBTAGS] Adding param for setting default module style

--- core/plugins/content/xhubtags/xhubtags.php
+++ core/plugins/content/xhubtags/xhubtags.php
@@ -127,7 +127,7 @@ class plgContentXhubtags extends \Hubzero\Plugin\Plugin
 			return '';
 		}
 
-		$attribs = array('style' => $this->params->def('style', 'none'));
+		$attribs = array('style' => $this->params->get('style', 'none'));
 
 		$regex = "/style\s*=\s*(\"|&quot;)([^\"]+)(\"|&quot;)/i";
 
