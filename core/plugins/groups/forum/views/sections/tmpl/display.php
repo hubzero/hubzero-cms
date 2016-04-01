3d8f3f3e21e3c10f0aca056c6de2f800340eacb4
Shawn Rice <zooley@purdue.edu>
2016-04-01 19:03:45 +0000

    [PLG_GROUPS_FORUM] Hide the entire container if the user doesn't have access

--- core/plugins/groups/forum/views/sections/tmpl/display.php
+++ core/plugins/groups/forum/views/sections/tmpl/display.php
@@ -262,9 +262,9 @@ if ($this->config->get('access-manage-section')) {
 				</tbody>
 			</table>
 		</div>
-		<div class="container relative-container">
-			<h3><?php echo Lang::txt('PLG_GROUPS_FORUM_EMAIL_SETTINGS'); ?></h3>
-			<?php if (Component::params('com_groups')->get('email_comment_processing') && $this->config->get('access-view-section')) : ?>
+		<?php if (Component::params('com_groups')->get('email_comment_processing') && $this->config->get('access-view-section')) : ?>
+			<div class="container relative-container">
+				<h3><?php echo Lang::txt('PLG_GROUPS_FORUM_EMAIL_SETTINGS'); ?></h3>
 				<?php if (Component::params('com_groups')->get('enable_forum_email_digest', 0)) : ?>
 					<p>
 						<?php echo Lang::txt('PLG_GROUPS_FORUM_EMAIL_CURRENT_SETTINGS'); ?>
@@ -331,8 +331,8 @@ if ($this->config->get('access-manage-section')) {
 						</fieldset>
 					</form>
 				<?php endif; ?>
-			<?php endif; ?>
-		</div>
+			</div>
+		<?php endif; ?>
 		<div class="container">
 			<h3><?php echo Lang::txt('PLG_GROUPS_FORUM_LAST_POST'); ?></h3>
 			<p>
