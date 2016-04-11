e505562367cd6c8567bcbd4a348ea0b020f39eb5
Shawn Rice <zooley@purdue.edu>
2016-04-11 18:39:03 +0000

    [COM_GROUPS] Makign default super group template's menu mroe responsive

--- core/components/com_groups/super/default/template/includes/footer.php
+++ core/components/com_groups/super/default/template/includes/footer.php
@@ -5,13 +5,13 @@
  * Template used for Special Groups. Will now be auto-created
  * when admin switches group from type HUB to type Special.
  *
- * @author 		Christopher Smoak
- * @copyright	December 2013
+ * @author     HUBzero
+ * @copyright  December 2015
  */
 ?>
 
 <div class="super-group-footer-wrap">
 	<div class="super-group-footer">
-		&copy; 2013-2015 HUBzero Foundation, LLC. Template designed by the HUBzero Development Team.
+		&copy; <?php echo gmdate("Y"); ?> HUBzero Foundation, LLC. Template designed by the HUBzero Development Team.
 	</div>
 </div>
