c50fbcdb765c6790d0d52dac0878517a65536d80
Kevin Wojkovich <kevinw@purdue.edu>
2016-06-10 19:24:49 -0400

    [COM_WIKI] Migration to rename column to Relational standards.

---
+++ core/migrations/Migration20160610231100ComWiki.php
@@ -0,0 +1,36 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+/**
+ * Migration script for renaming the column to Relational standards
+ **/
+class Migration20160610231100ComWiki extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableHasField('#__wiki_versions', 'pageid'))
+		{
+			$query = "ALTER TABLE `jos_wiki_versions` CHANGE `pageid` `page_id` INT(11)  NOT NULL  DEFAULT '0';";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if ($this->db->tableHasField('#__wiki_versions', 'page_id'))
+		{
+			$query = "ALTER TABLE `jos_wiki_versions` CHANGE `page_id` `pageid` INT(11)  NOT NULL  DEFAULT '0';";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+}
+
