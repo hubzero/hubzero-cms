033e3fb4f786103f9c63e8c7ebb16709ce32b4c5
Kevin Wojkovich <kevinw@purdue.edu>
2016-06-29 18:05:05 -0400

    [COM_SEARCH] Creates the indexqueue table.

---
+++ core/migrations/Migration20160629215021ComSearch.php
@@ -0,0 +1,47 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+/**
+ * Migration script for creating the indexQueue table
+ **/
+class Migration20160629215021ComSearch extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if (!$this->db->tableExists('#__search_indexqueue'))
+		{
+			$createQuery = 'CREATE TABLE `jos_search_indexqueue` (
+			`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+			`hubtype` varchar(12) NOT NULL DEFAULT \'\',
+			`action` varchar(12) DEFAULT NULL,
+			`start` int(11) NOT NULL DEFAULT \'0\',
+			`lock` tinyint(1) NOT NULL DEFAULT \'0\',
+			`complete` tinyint(1) NOT NULL DEFAULT \'0\',
+			`created` timestamp NULL DEFAULT NULL,
+			`created_by` int(11) DEFAULT NULL,
+			`modified` timestamp NULL DEFAULT NULL,
+			PRIMARY KEY (`id`)
+			) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;';
+
+			$this->db->setQuery($createQuery);
+			$this->db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if ($this->db->tableExists('#__search_indexqueue'))
+		{
+			$dropQuery = 'DROP TABLE `#__search_indexqueue`';
+			$this->db->setQuery($dropQuery);
+			$this->db->query();
+		}
+	}
+}
