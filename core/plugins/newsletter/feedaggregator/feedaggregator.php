c1f9448b07ab4471952b8017e2daa8f46ba5eb41
Shawn Rice <zooley@purdue.edu>
2016-07-25 19:02:24 +0000

    [PLG_NEWSLETTER_*] Adding missing language files, XML manifests, and docblock

--- core/plugins/newsletter/feedaggregator/feedaggregator.php
+++ core/plugins/newsletter/feedaggregator/feedaggregator.php
@@ -25,31 +25,42 @@
  * HUBzero is a registered trademark of Purdue University.
  *
  * @package   hubzero-cms
- * @author 		Kevin Wojkovich <kevinw@purdue.edu>
+ * @author    Kevin Wojkovich <kevinw@purdue.edu>
  * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
  * @license   http://opensource.org/licenses/MIT MIT
  */
 
 // No direct access
-defined( '_HZEXEC_' ) or die();
+defined('_HZEXEC_') or die();
 
 use Components\Feedaggregator\Models\Orm\Post;
 
 require_once(PATH_CORE . DS . 'components' . DS . 'com_feedaggregator' . DS . 'models' . DS . 'orm' . DS . 'post.php');
 
+/**
+ * Plugin class for Newsletter feedaggregator
+ */
 class plgNewsletterFeedaggregator extends \Hubzero\Plugin\Plugin
 {
-	public function __construct(&$subject, $config)
-	{
-		parent::__construct($subject, $config);
-	}
-
+	/**
+	 * Event call to get the name
+	 *
+	 * @return  string
+	 */
 	public function onGetEnabledDigests()
 	{
 		$name = 'feedaggregator';
 		return $name;
 	}
 
+	/**
+	 * Event call to get the latest records
+	 *
+	 * @param   integer  $num
+	 * @param   string   $dateField
+	 * @param   string   $sort
+	 * @return  array
+	 */
 	public function onGetLatest($num = 5, $dateField = 'created', $sort = 'DESC')
 	{
 		$model = Post::getLatest($num, $dateField, $sort)->rows()->toObject();
@@ -60,10 +71,10 @@ class plgNewsletterFeedaggregator extends \Hubzero\Plugin\Plugin
 		{
 			$object = new stdClass;
 			$object->title = $m->title;
-			$object->body = preg_replace('/[^ .,;a-zA-Z0-9_-]|[,;]/', '', $m->description);
-			$object->date = Date::of($m->created)->toLocal("F j, Y");
-			$object->path = $m->url;
-			$object->id = $m->id;
+			$object->body  = preg_replace('/[^ .,;a-zA-Z0-9_-]|[,;]/', '', $m->description);
+			$object->date  = Date::of($m->created)->toLocal("F j, Y");
+			$object->path  = $m->url;
+			$object->id    = $m->id;
 
 			array_push($objects, $object);
 		}
