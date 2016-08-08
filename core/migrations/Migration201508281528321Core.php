315f787418bdc9e351b40c4f0f67029a3165f360
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2016-08-08 15:03:24 -0400

    [MIGRATIONS] cleanup upgrade migration

--- core/migrations/Migration201508281528321Core.php
+++ core/migrations/Migration201508281528321Core.php
@@ -22,8 +22,8 @@ class Migration201508281528321Core extends Base
 			$query[] = 'UPDATE jos_extensions SET params = REPLACE(params, "_HUB0_nW_", "") WHERE type="component" AND element="com_system";';
 			$query[] = 'UPDATE jos_extensions SET params = REPLACE(params, "hubzero_network", "") WHERE type="component" AND element="com_system";';
 			$query[] = 'UPDATE jos_extensions SET params = REPLACE(params, "hubzero.org", "") WHERE type="component" AND element="com_system";';
-    		$query[] = 'UPDATE jos_extensions SET params = REPLACE(params, "ABQIAAAAPq8QOefNUw20Lc6RX2gKqhQkcPnh--THxGDMaCLza-8u_rvH7hQmdZgwooOYuoIkEqFAtrnkoY4ElA","") WHERE type="component" AND element="com_usage";';
-    		$query[] = 'UPDATE jos_extensions SET manifest_cache = "" WHERE type="file" AND element="joomla";';		
+			$query[] = 'UPDATE jos_extensions SET params = REPLACE(params, "ABQIAAAAPq8QOefNUw20Lc6RX2gKqhQkcPnh--THxGDMaCLza-8u_rvH7hQmdZgwooOYuoIkEqFAtrnkoY4ElA","") WHERE type="component" AND element="com_usage";';
+			$query[] = 'UPDATE jos_extensions SET manifest_cache = "" WHERE type="file" AND element="joomla";';		
 			$query[] = 'UPDATE jos_extensions SET params = REPLACE(params, ":10,", ":\"10\",") WHERE type="component" AND element="com_media";';
 			$query[] = 'UPDATE jos_extensions SET params = REPLACE(params, "site\\\\/media", "site/media") WHERE type="component" AND element="com_media" ;';
 			$query[] = 'UPDATE jos_extensions SET params = REPLACE(params, "media\\\\/images", "media/images") WHERE type="component" AND element="com_media";';
@@ -34,9 +34,9 @@ class Migration201508281528321Core extends Base
 			foreach($query as $q)
 			{
 				$this->db->setQuery($q);
-    			$this->db->query();
+    				$this->db->query();
+    			}
     		}
-    	}
 	}
 
 	/**
