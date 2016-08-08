aac935275125975a0ec18812a038984356cdd591
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2016-08-08 15:30:40 -0400

    [MIGRATIONS] fix whitespace and debugging cruft

--- core/migrations/Migration201508281131531Core.php
+++ core/migrations/Migration201508281131531Core.php
@@ -20,30 +20,27 @@ class Migration201508281131531Core extends Base
 			$query = "SELECT MIN(extension_id) AS min, MAX(extension_id) as max, folder, element FROM #__extensions WHERE type='plugin' GROUP BY folder, element HAVING COUNT(*) > 1;";
 
 			$this->db->setQuery($query);
-    
-           	if ($results = $this->db->loadObjectList())
-          	{
-           		foreach($results as $result)
-           		{
-var_dump($result);
-           			if (empty($result) || empty($result->min) || empty($result->max) || empty($result->folder) || empty($result->element))
-           			{
-           				continue;
-           			}
 
-           			$query = "UPDATE #__extensions AS e1, #__extensions e2 SET e1.params = e2.params WHERE e1.extension_id = " . $this->db->quote($result->min) . " AND e2.extension_id = " . $this->db->quote($result->max) . ";";
-				echo "$query\n\n";
-           			$this->db->setQuery($query);
-           			$this->db->query();
+			if ($results = $this->db->loadObjectList())
+			{
+				foreach ($results as $result)
+				{
+					if (empty($result) || empty($result->min) || empty($result->max) || empty($result->folder) || empty($result->element))
+					{
+						continue;
+					}
 
-           			$query = "DELETE FROM #__extensions WHERE folder = " . $this->db->quote($result->folder) . " AND element = " . $this->db->quote($result->element) . " AND extension_id != " . $this->db->quote($result->min);
-           			$this->db->setQuery($query);
-				echo "$query\n\n";
-           			$this->db->query();
-              	}
-        	}
-        }
-    }
+					$query = "UPDATE #__extensions AS e1, #__extensions e2 SET e1.params = e2.params WHERE e1.extension_id = " . $this->db->quote($result->min) . " AND e2.extension_id = " . $this->db->quote($result->max) . ";";
+					$this->db->setQuery($query);
+					$this->db->query();
+
+					$query = "DELETE FROM #__extensions WHERE folder = " . $this->db->quote($result->folder) . " AND element = " . $this->db->quote($result->element) . " AND extension_id != " . $this->db->quote($result->min);
+					$this->db->setQuery($query);
+					$this->db->query();
+				}
+			}
+		}
+	}
 
 	/**
 	 * Down
