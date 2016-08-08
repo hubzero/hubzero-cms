ad621025ff8e4ae7dde8c81dcaa8a95132b29059
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2016-08-08 14:39:20 -0400

    [MIGRATIONS] update access field in default language row

---
+++ core/migrations/Migration20150826245312Core.php
@@ -0,0 +1,25 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for languages table addition
+ **/
+class Migration20150826245312Core extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__languages'))
+		{
+                        $query = "UPDATE #__languages SET access=1 WHERE lang_id=1 AND access=0;";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+}
