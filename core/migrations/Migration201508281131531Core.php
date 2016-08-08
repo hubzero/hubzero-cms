e49f53d1ab538d427cb592273b9d36cb85aeed2a
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2016-08-08 14:45:21 -0400

    [MIGRATIONS] additional 1.2.x era migrations

---
+++ core/migrations/Migration201508281131531Core.php
@@ -0,0 +1,55 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script for removing duplicate plugin entries while retaining proper parameters
+ **/
+class Migration201508281131531Core extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__extensions'))
+		{
+			$query = "SELECT MIN(extension_id) AS min, MAX(extension_id) as max, folder, element FROM #__extensions WHERE type='plugin' GROUP BY folder, element HAVING COUNT(*) > 1;";
+
+			$this->db->setQuery($query);
+    
+           	if ($results = $this->db->loadObjectList())
+          	{
+           		foreach($results as $result)
+           		{
+var_dump($result);
+           			if (empty($result) || empty($result->min) || empty($result->max) || empty($result->folder) || empty($result->element))
+           			{
+           				continue;
+           			}
+
+           			$query = "UPDATE #__extensions AS e1, #__extensions e2 SET e1.params = e2.params WHERE e1.extension_id = " . $this->db->quote($result->min) . " AND e2.extension_id = " . $this->db->quote($result->max) . ";";
+				echo "$query\n\n";
+           			$this->db->setQuery($query);
+           			$this->db->query();
+
+           			$query = "DELETE FROM #__extensions WHERE folder = " . $this->db->quote($result->folder) . " AND element = " . $this->db->quote($result->element) . " AND extension_id != " . $this->db->quote($result->min);
+           			$this->db->setQuery($query);
+				echo "$query\n\n";
+           			$this->db->query();
+              	}
+        	}
+        }
+    }
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+
+	}
+}
