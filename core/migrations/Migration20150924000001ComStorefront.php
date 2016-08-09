310c9ebaaf1ce7da01b8871c618c7b94d2aeae8c
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2016-08-09 11:23:04 -0400

    [MIGRATIONS] repeat of cId field schema change (maintained for migration id compat)

---
+++ core/migrations/Migration20150924000001ComStorefront.php
@@ -0,0 +1,25 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Migration script to add AUTO_INCREMENT to cId
+ **/
+class Migration20150924000001ComStorefront extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__storefront_collections') && $this->db->tableHasField('#__storefront_collections', 'cId'))
+		{
+			$query = "ALTER TABLE `#__storefront_collections` MODIFY COLUMN `cId` INT(16) UNSIGNED NOT NULL AUTO_INCREMENT";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+}
