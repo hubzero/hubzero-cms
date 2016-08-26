8c92358a3917e692bf30401a0d9e5f57e8d8d580
Shawn Rice <zooley@purdue.edu>
2016-08-26 10:51:49 +0000

    [PLG_PROJECTS_NOTES] Fixing paths to notes for side listings

--- core/plugins/projects/notes/views/mini/tmpl/default.php
+++ core/plugins/projects/notes/views/mini/tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package		HUBzero CMS
- * @author		Shawn Rice <zooley@purdue.edu>
+ * @author		Alissa Nedossekina <alisa@purdue.edu>
  * @copyright	Copyright 2005-2009 HUBzero Foundation, LLC.
  * @license		http://opensource.org/licenses/MIT MIT
  *
@@ -33,19 +33,21 @@ defined('_HZEXEC_') or die();
 
 ?>
 <div class="sidebox<?php if (count($this->notes) == 0) { echo ' suggestions'; } ?>">
-		<h4><a href="<?php echo Route::url($this->model->link('notes')); ?>" class="hlink" title="<?php echo Lang::txt('COM_PROJECTS_VIEW') . ' ' . strtolower(Lang::txt('COM_PROJECTS_PROJECT')) . ' ' . strtolower(Lang::txt('PLG_PROJECTS_NOTES')); ?>"><?php echo ucfirst(Lang::txt('PLG_PROJECTS_NOTES')); ?></a>
-<?php if (count($this->notes) > 0) { ?>
-	<span><a href="<?php echo Route::url($this->model->link('notes')); ?>"><?php echo ucfirst(Lang::txt('COM_PROJECTS_SEE_ALL')); ?> </a></span>
-<?php } ?>
-</h4>
-<?php if (count($this->notes) == 0) { ?>
+	<h4>
+		<a href="<?php echo Route::url($this->model->link('notes')); ?>" class="hlink" title="<?php echo Lang::txt('COM_PROJECTS_VIEW') . ' ' . strtolower(Lang::txt('COM_PROJECTS_PROJECT')) . ' ' . strtolower(Lang::txt('PLG_PROJECTS_NOTES')); ?>"><?php echo ucfirst(Lang::txt('PLG_PROJECTS_NOTES')); ?></a>
+		<?php if (count($this->notes) > 0) { ?>
+			<span><a href="<?php echo Route::url($this->model->link('notes')); ?>"><?php echo ucfirst(Lang::txt('COM_PROJECTS_SEE_ALL')); ?> </a></span>
+		<?php } ?>
+	</h4>
+	<?php if (count($this->notes) == 0) { ?>
 		<p class="s-notes"><a href="<?php echo Route::url($this->model->link('notes') . '&action=new'); ?>"><?php echo Lang::txt('PLG_PROJECTS_NOTES_ADD_NOTE'); ?></a></p>
-<?php } else { ?>
-	<ul>
-		<?php foreach ($this->notes as $note) {
-		?>
-		<li>
-			<a href="<?php echo Route::url($this->model->link('notes') . '&scope=' . $note->scope . '&pagename=' . $note->pagename); ?>" class="notes"><?php echo \Hubzero\Utility\String::truncate($note->title, 35); ?></a>
-		</li><?php } ?>
-	</ul><?php } ?>
+	<?php } else { ?>
+		<ul>
+			<?php foreach ($this->notes as $note) { ?>
+				<li>
+					<a href="<?php echo Route::url($this->model->link('notes') . '&pagename=' . ($note->path ? $note->path . '/' : '') . $note->pagename); ?>" class="notes"><?php echo $this->escape(\Hubzero\Utility\String::truncate($note->title, 35)); ?></a>
+				</li>
+			<?php } ?>
+		</ul>
+	<?php } ?>
 </div>
