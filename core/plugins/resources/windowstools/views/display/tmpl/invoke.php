cef991da122d54ea749a8552574eea0559491f6b
Shawn Rice <zooley@purdue.edu>
2016-08-29 14:36:33 +0000

    [PLG_RESOURCES_WINDOWSTOOLS] Moving some data to a view. Reworking to accommodate reconnect work flow

---
+++ core/plugins/resources/windowstools/views/display/tmpl/invoke.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 Purdue University. All rights reserved.
+ *
+ * This file is part of: The HUBzero(R) Platform for Scientific Collaboration
+ *
+ * The HUBzero(R) Platform for Scientific Collaboration (HUBzero) is free
+ * software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * HUBzero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Shawn Rice <zooley@purdue.edu>
+ * @copyright Copyright 2005-2015 Purdue University. All rights reserved.
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+?>
+<html>
+	<body>
+		<p><a id="runapplink" href="<?php echo $this->url; ?>">Run app</a></p>
+		<p>This page should go back ot the hub application page automatically. If it doesn't, click <a href="<?php echo $this->rurl; ?>">here.</a></p>
+		<script>
+		document.getElementById('runapplink').click();
+		window.setTimeout(function(){
+			window.location = "<?php echo $this->rurl; ?>";
+		},1000);
+		</script>
+	</body>
+</html>
