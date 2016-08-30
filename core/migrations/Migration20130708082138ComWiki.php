2917932e54a23d73c3f6183b51e45e04e0c1a8ed
Shawn Rice <zooley@purdue.edu>
2016-08-30 15:55:24 +0000

    [MIGRATIONS] Reworking to perform some table existence checking and not to use potentially deprecated models

--- core/migrations/Migration20130708082138ComWiki.php
+++ core/migrations/Migration20130708082138ComWiki.php
@@ -15,42 +15,26 @@ class Migration20130708082138ComWiki extends Base
 	 **/
 	public function up()
 	{
-		$query  = "SELECT * FROM `#__wiki_page` AS wp,";
-		$query .= " `#__wiki_version` AS wv";
-		$query .= " WHERE wp.id = wv.pageid";
-		$query .= " AND wp.pagename = 'MainPage' AND (wp.group_cn='' OR wp.group_cn IS NULL)";
-		$query .= " ORDER BY wv.version DESC";
-		$query .= " LIMIT 1;";
-
-		$this->db->setQuery($query);
-		$result = $this->db->loadObject();
-
-		require_once PATH_CORE . DS . 'components' . DS . 'com_wiki' . DS . 'tables' . DS . 'revision.php';
-
-		if ($result)
+		if ($this->db->tableExists('#__wiki_page') && $this->db->tableExists('#__wiki_version'))
 		{
-			$cls = 'WikiPageRevision';
-			if (class_exists('WikiTableRevision'))
-			{
-				$cls = 'WikiTableRevision';
-			}
-			else if (class_exists('\\Components\\Wiki\\Tables\\Revision'))
-			{
-				$cls = '\\Components\\Wiki\\Tables\\Revision';
-			}
+			$query  = "SELECT wv.* FROM `#__wiki_page` AS wp,";
+			$query .= " `#__wiki_version` AS wv";
+			$query .= " WHERE wp.id = wv.pageid";
+			$query .= " AND wp.pagename = 'MainPage' AND (wp.group_cn='' OR wp.group_cn IS NULL)";
+			$query .= " ORDER BY wv.version DESC";
+			$query .= " LIMIT 1;";
 
-			$version = new $cls($this->db);
-			$version->loadByVersion($result->pageid, $result->version);
+			$this->db->setQuery($query);
+			$result = $this->db->loadObject();
 
-			$hostname = php_uname('n');
-
-			// No need to run this on nanoHUB
-			if (stripos($hostname, 'nanohub') === false)
+			if ($result)
 			{
 				$pagetext = preg_replace('/(xsearch)/', 'search', $result->pagetext);
 				$pagehtml = preg_replace('/(xsearch)/', 'search', $result->pagehtml);
 
-				$version->save(array('pagetext'=>$pagetext, 'pagehtml'=>$pagehtml));
+				$query = "UPDATE `#__wiki_version` SET `pagetext`=" . $this->db->quote($pagetext) . ", `pagehtml`=" . $this->db->quote($pagehtml) . " WHERE `id`=" . $result->id;
+				$this->db->setQuery($query);
+				$this->db->query();
 			}
 		}
 	}
