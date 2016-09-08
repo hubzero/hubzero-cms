445b561922659d55ba67c66cb2fa970b0f2b037f
Shawn Rice <zooley@purdue.edu>
2016-09-08 17:25:34 +0000

    [QUBES][#683][MOD_MEMBERS] Fixing link to filtered view to reflect changes in com_members

--- core/modules/mod_members/tmpl/default.php
+++ core/modules/mod_members/tmpl/default.php
@@ -50,19 +50,19 @@ $total = $this->confirmed + $this->unconfirmed;
 			</tr>
 			<tr>
 				<td class="confirmed">
-					<a href="<?php echo Route::url('index.php?option=com_members&emailConfirmed=1&registerDate='); ?>" title="<?php echo Lang::txt('MOD_MEMBERS_CONFIRMED_TITLE'); ?>">
+					<a href="<?php echo Route::url('index.php?option=com_members&activation=1&registerDate='); ?>" title="<?php echo Lang::txt('MOD_MEMBERS_CONFIRMED_TITLE'); ?>">
 						<?php echo $this->escape($this->confirmed); ?>
 						<span><?php echo Lang::txt('MOD_MEMBERS_CONFIRMED'); ?></span>
 					</a>
 				</td>
 				<td class="unconfirmed">
-					<a href="<?php echo Route::url('index.php?option=com_members&emailConfirmed=-1&registerDate='); ?>" title="<?php echo Lang::txt('MOD_MEMBERS_UNCONFIRMED_TITLE'); ?>">
+					<a href="<?php echo Route::url('index.php?option=com_members&activation=-1&registerDate='); ?>" title="<?php echo Lang::txt('MOD_MEMBERS_UNCONFIRMED_TITLE'); ?>">
 						<?php echo $this->escape($this->unconfirmed); ?>
 						<span><?php echo Lang::txt('MOD_MEMBERS_UNCONFIRMED'); ?></span>
 					</a>
 				</td>
 				<td class="newest">
-					<a href="<?php echo Route::url('index.php?option=com_members&emailConfirmed=0&registerDate=' . gmdate("Y-m-d H:i:s", strtotime('-1 day'))); ?>" title="<?php echo Lang::txt('MOD_MEMBERS_NEW_TITLE'); ?>">
+					<a href="<?php echo Route::url('index.php?option=com_members&activation=0&registerDate=' . gmdate("Y-m-d H:i:s", strtotime('-1 day'))); ?>" title="<?php echo Lang::txt('MOD_MEMBERS_NEW_TITLE'); ?>">
 						<?php echo $this->escape($this->pastDay); ?>
 						<span><?php echo Lang::txt('MOD_MEMBERS_NEW'); ?></span>
 					</a>
