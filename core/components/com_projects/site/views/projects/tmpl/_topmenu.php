24a0ad46b8abf83f23e8d73d576eec3328e5f22d
Shawn Rice <zooley@purdue.edu>
2016-09-16 10:34:32 +0000

    [COM_PROJECTS] Removing hard-coded list item (now a plugin), minor fixes for nesting and formatting

--- core/components/com_projects/site/views/projects/tmpl/_topmenu.php
+++ core/components/com_projects/site/views/projects/tmpl/_topmenu.php
@@ -62,47 +62,48 @@ $counts = $this->model->get('counts');
 <div class="menu-wrapper">
 	<?php if ($this->publicView == false && isset($this->tabs) && $this->tabs) { ?>
 		<ul>
-		<?php foreach ($this->tabs as $tab)
-		{
-			if (isset($tab['submenu']) && $tab['submenu'] == 'Assets' && count($assetTabs) > 1)
+			<?php foreach ($this->tabs as $tab)
 			{
-				continue;
-			}
-			if (isset($tab['alias']) && trim($tab['alias']))
-			{
-				$tab['name'] = trim($tab['alias']);
-			}
-			$gopanel = $tab['name'] == 'assets' ? 'files' : $tab['name'];
-			$active = (($tab['name'] == $this->active) || ($tab['name'] == 'assets' && (isset($tab['submenu']) && $tab['submenu'] == 'Assets')))
-			?>
-			<li<?php if ($active) { echo ' class="active"'; } ?> id="tab-<?php echo $tab['name']; ?>">
-				<a class="<?php echo $tab['name']; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&active=' . $gopanel); ?>/" title="<?php echo ucfirst(Lang::txt('COM_PROJECTS_PROJECT')) . ' ' . ucfirst($tab['title']); ?>">
-					<span class="label"><?php echo $tab['title']; ?></span>
-				<?php if ($tab['name'] != 'feed' && isset($counts[$tab['name']]) && $counts[$tab['name']] != 0) { ?>
-					<span class="mini" id="c-<?php echo $tab['name']; ?>"><span id="c-<?php echo $tab['name']; ?>-num"><?php echo $counts[$tab['name']]; ?></span></span>
-				<?php } elseif ($tab['name'] == 'feed') { ?>
-					<span id="c-new" class="mini highlight <?php if (empty($counts['new'])) { echo 'hidden'; } ?>"><span id="c-new-num"><?php echo empty($counts['new']) ? 0 : $counts['new']; ?></span></span>
-				<?php } ?>
-				</a>
-				<?php if ($tab['name'] == 'assets') { ?>
-				<div id="asset-selection" class="submenu-wrap">
-					<?php foreach ($assetTabs as $aTab) { ?>
-						<p><a class="<?php echo $aTab['name']; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&active=' . $aTab['name']); ?>/" title="<?php echo ucfirst(Lang::txt('COM_PROJECTS_PROJECT')) . ' ' . ucfirst($aTab['title']); ?>" id="tab-<?php echo $aTab['name']; ?>"><span class="label"><?php echo $aTab['title']; ?></span><?php if (isset($counts[$aTab['name']]) && $counts[$aTab['name']] != 0) { ?>
-							<span class="mini" id="c-<?php echo $aTab['name']; ?>"><span id="c-<?php echo $aTab['name']; ?>-num"><?php echo $counts[$aTab['name']]; ?></span></span>
+				if (isset($tab['submenu']) && $tab['submenu'] == 'Assets' && count($assetTabs) > 1)
+				{
+					continue;
+				}
+				if (isset($tab['alias']) && trim($tab['alias']))
+				{
+					$tab['name'] = trim($tab['alias']);
+				}
+				$gopanel = $tab['name'] == 'assets' ? 'files' : $tab['name'];
+				$active = (($tab['name'] == $this->active) || ($tab['name'] == 'assets' && (isset($tab['submenu']) && $tab['submenu'] == 'Assets')))
+				?>
+				<li<?php if ($active) { echo ' class="active"'; } ?> id="tab-<?php echo $tab['name']; ?>">
+					<a class="tab-<?php echo $tab['name']; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&active=' . $gopanel); ?>/" title="<?php echo ucfirst(Lang::txt('COM_PROJECTS_PROJECT')) . ' ' . ucfirst($tab['title']); ?>">
+						<span class="label"><?php echo $tab['title']; ?></span>
+						<?php if ($tab['name'] != 'feed' && isset($counts[$tab['name']]) && $counts[$tab['name']] != 0) { ?>
+							<span class="mini" id="c-<?php echo $tab['name']; ?>"><span id="c-<?php echo $tab['name']; ?>-num"><?php echo $counts[$tab['name']]; ?></span></span>
+						<?php } elseif ($tab['name'] == 'feed') { ?>
+							<span id="c-new" class="mini highlight <?php if (empty($counts['new'])) { echo 'hidden'; } ?>"><span id="c-new-num"><?php echo empty($counts['new']) ? 0 : $counts['new']; ?></span></span>
 						<?php } ?>
-							</a>
-						</p>
+					</a>
+					<?php if ($tab['name'] == 'assets') { ?>
+						<div id="asset-selection" class="submenu-wrap">
+							<?php foreach ($assetTabs as $aTab) { ?>
+								<p>
+									<a class="<?php echo $aTab['name']; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&active=' . $aTab['name']); ?>/" title="<?php echo ucfirst(Lang::txt('COM_PROJECTS_PROJECT')) . ' ' . ucfirst($aTab['title']); ?>" id="tab-<?php echo $aTab['name']; ?>">
+										<span class="label"><?php echo $aTab['title']; ?></span>
+										<?php if (isset($counts[$aTab['name']]) && $counts[$aTab['name']] != 0) { ?>
+											<span class="mini" id="c-<?php echo $aTab['name']; ?>"><span id="c-<?php echo $aTab['name']; ?>-num"><?php echo $counts[$aTab['name']]; ?></span></span>
+										<?php } ?>
+									</a>
+								</p>
+							<?php } ?>
+						</div>
 					<?php } ?>
-				</div>
-				<?php } ?>
-			</li>
-		<?php } // end foreach ?>
-			<li class="sideli <?php if ($this->active == 'info') { echo ' active'; } ?>" id="tab-info"><a href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&active=info'); ?>/" title="<?php echo ucfirst(Lang::txt('COM_PROJECTS_ABOUT')); ?>">
-			<span class="label"><?php echo Lang::txt('COM_PROJECTS_ABOUT'); ?></span></a></li>
+				</li>
+			<?php } // end foreach ?>
 		</ul>
 	<?php } else {  ?>
 		<?php if (isset($this->guest) && $this->guest) { ?>
-		<p><?php echo Lang::txt('COM_PROJECTS_ARE_YOU_MEMBER'); ?> <a href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&task=view') . '?action=login'; ?>"><?php echo ucfirst(Lang::txt('COM_PROJECTS_LOGIN')).'</a> '.Lang::txt('COM_PROJECTS_LOGIN_TO_PRIVATE_AREA'); ?></p>
+			<p><?php echo Lang::txt('COM_PROJECTS_ARE_YOU_MEMBER'); ?> <a href="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&task=view') . '?action=login'; ?>"><?php echo ucfirst(Lang::txt('COM_PROJECTS_LOGIN')).'</a> '.Lang::txt('COM_PROJECTS_LOGIN_TO_PRIVATE_AREA'); ?></p>
 		<?php } ?>
 	<?php } ?>
 </div>
