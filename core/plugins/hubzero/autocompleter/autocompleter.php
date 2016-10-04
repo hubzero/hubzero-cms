b7fba554364dbefac9ad9ef5ce83a490a4644baf
Shawn Rice <zooley@purdue.edu>
2016-10-04 16:01:01 +0000

    [PURR][#1143][PLG_HUBZERO_AUTOCOMPLETER] Fixing typo to admin URL for members autocompleter

--- core/plugins/hubzero/autocompleter/autocompleter.php
+++ core/plugins/hubzero/autocompleter/autocompleter.php
@@ -81,7 +81,7 @@ class plgHubzeroAutocompleter extends \Hubzero\Plugin\Plugin
 		$dsabl = (isset($atts[8])) ? $atts[8] : '';      // Readonly input
 
 		$base = rtrim(Request::root(true), '/');
-		$datascript = $base . (App::isAdmin() ? '/adminsitrator' : '') . '/index.php';
+		$datascript = $base . (App::isAdmin() ? '/administrator' : '') . '/index.php';
 
 		if ($opt != 'members')
 		{
