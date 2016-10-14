2c6cac714633b134b91184b46399541292139901
Kevin Wojkovich <kevinw@purdue.edu>
2016-10-14 12:17:06 -0400

    [NANOHUB][#313251][COM_MEMBERS] Fixes object property

--- core/components/com_members/site/controllers/register.php
+++ core/components/com_members/site/controllers/register.php
@@ -1193,7 +1193,7 @@ class Register extends SiteController
 		$xprofile = User::getInstance();
 		$login = $xprofile->get('username');
 		$email = $xprofile->get('email');
-		$email_confirmed = $xprofile->get('emailConfirmed');
+		$email_confirmed = $xprofile->get('activation');
 
 		// Incoming
 		$return = urldecode(Request::getVar('return', '/'));
@@ -1335,10 +1335,11 @@ class Register extends SiteController
 						// Attempt to send a new confirmation code
 						$confirm = \Components\Members\Helpers\Utility::genemailconfirm();
 
-						$xprofile = new \Hubzero\User\Profile();
-						$xprofile->load($login);
-						$xprofile->set('emailConfirmed', $confirm);
-						$xprofile->update();
+						$xprofile = User::getInstance();
+						$xprofile->get('username');
+						$xprofile->get('email');
+						$xprofile->set('activation', $confirm);
+						$xprofile->save();
 
 						$subject  = Config::get('sitename').' '.Lang::txt('COM_MEMBERS_REGISTER_EMAIL_CONFIRMATION');
 
