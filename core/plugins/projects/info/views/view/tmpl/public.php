a585e809e4b979a764615f6d1f652344e58be15f
Shawn Rice <zooley@purdue.edu>
2016-10-28 20:21:54 +0000

    [HUBZERO][#10004][COM_PROJECTS] Make sure extended info shows on overview page when project is public

---
+++ core/plugins/projects/info/views/view/tmpl/public.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 HUBzero Foundation, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+?>
+<div id="basic_info">
+	<table id="infotbl">
+		<tbody>
+			<?php
+			// This is for the admin-defined project information
+			if ($this->info)
+			{
+				foreach ($this->info as $field)
+				{
+					?>
+					<tr>
+						<th class="htd"><?php echo $field->label; ?></th>
+						<td><?php echo $field->value; ?></td>
+					</tr>
+					<?php
+				} // end foreach
+			} // end if
+			?>
+		</tbody>
+	</table>
+</div><!-- / .basic info -->
