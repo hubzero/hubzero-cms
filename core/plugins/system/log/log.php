f540f37ee4eebb0f9af9bfd80380a45cc3dc11d3
Shawn Rice <zooley@purdue.edu>
2016-11-14 12:52:42 +0000

    [PLG_SYSTEM_LOG] Adding more checks, fixing copyright blocks

--- core/plugins/system/log/log.php
+++ core/plugins/system/log/log.php
@@ -1,17 +1,38 @@
 <?php
 /**
- * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 HUBzero Foundation, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
  */
 
-// no direct access
 defined('_HZEXEC_') or die;
 
 /**
- * Joomla! System Logging Plugin
- *
- * @package		Joomla.Plugin
- * @subpackage	System.log
+ * System Logging Plugin
  */
 class plgSystemLog extends \Hubzero\Plugin\Plugin
 {
@@ -23,24 +44,27 @@ class plgSystemLog extends \Hubzero\Plugin\Plugin
 	 */
 	public function onUserLoginFailure($response)
 	{
+		if (!App::has('log'))
+		{
+			return;
+		}
+
 		$errorlog = array();
 
 		switch ($response['status'])
 		{
-			case \Hubzero\Auth\Status::SUCCESS:
+			case Hubzero\Auth\Status::SUCCESS:
 				$errorlog['status']  = $response['type'] . ' CANCELED: ';
 				$errorlog['comment'] = $response['error_message'];
 			break;
 
-			case \Hubzero\Auth\Status::FAILURE:
+			case Hubzero\Auth\Status::FAILURE:
 				$errorlog['status']  = $response['type'] . ' FAILURE: ';
+				$errorlog['comment'] = $response['error_message'];
+
 				if ($this->params->get('log_username', 0))
 				{
-					$errorlog['comment'] = $response['error_message'] . ' ("' . $response['username'] . '")';
-				}
-				else
-				{
-					$errorlog['comment'] = $response['error_message'];
+					$errorlog['comment'] .= ' ("' . $response['username'] . '")';
 				}
 			break;
 
@@ -50,6 +74,8 @@ class plgSystemLog extends \Hubzero\Plugin\Plugin
 			break;
 		}
 
-		App::get('log')->logger('auth')->info(implode('', $errorlog));
+		App::get('log')
+			->logger('auth')
+			->info(implode('', $errorlog));
 	}
 }
