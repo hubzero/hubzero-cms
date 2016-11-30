31e554165b4e741d5607537ce03461090d202b06
Shawn Rice <zooley@purdue.edu>
2016-11-30 12:00:10 +0000

    [PLG_PROJECTS_TEAM] Fixing formatting, docblock, issues with sorting and counting team members. Some reworking of HTML/UI.

--- core/plugins/projects/team/views/view/tmpl/default.php
+++ core/plugins/projects/team/views/view/tmpl/default.php
@@ -50,63 +50,46 @@ $sortAppend = '&sortdir=' . urlencode($sortbyDir);
 	</ul>
 <?php } ?>
 <form id="plg-form" method="post" action="<?php echo Route::url($this->model->link('team')); ?>">
-	<div>
-		<input type="hidden" id="id" name="id" value="<?php echo $this->model->get('id'); ?>" />
-		<input type="hidden" name="task" value="view" />
-		<input type="hidden" name="action" value="team" />
-	</div>
+	<input type="hidden" id="id" name="id" value="<?php echo $this->model->get('id'); ?>" />
+	<input type="hidden" name="task" value="view" />
+	<input type="hidden" name="action" value="team" />
+
 	<div class="container">
-		<div class="list-menu">
-			<ul class="entries-menu order-options">
-				<li>
-					<a class="sort-title<?php if ($this->filters['sortby'] == 'date') { echo ' active'; } ?>" href="<?php echo Route::url($this->model->link('team') . $sortAppend . '&sortby=date'); ?>" title="<?php echo Lang::txt('PLG_PROJECTS_TEAM_SORT_BY') . ' ' . strtolower(Lang::txt('PLG_PROJECTS_TEAM_JOINED')); ?>">
-						&darr; <?php echo Lang::txt('PLG_PROJECTS_TEAM_JOINED'); ?>
-					</a>
-				</li>
-				<li>
-					<a class="sort-title<?php if ($this->filters['sortby'] == 'role') { echo ' active'; } ?>" href="<?php echo Route::url($this->model->link('team') . $sortAppend . '&sortby=role'); ?>" title="<?php echo Lang::txt('PLG_PROJECTS_TEAM_SORT_BY') . ' ' . strtolower(Lang::txt('PLG_PROJECTS_TEAM_ROLE')); ?>">
-						&darr; <?php echo Lang::txt('PLG_PROJECTS_TEAM_ROLE'); ?>
-					</a>
-				</li>
-				<li>
-					<a class="sort-title<?php if ($this->filters['sortby'] == 'name') { echo ' active'; } ?>" href="<?php echo Route::url($this->model->link('team') . $sortAppend . '&sortby=name'); ?>" title="<?php echo Lang::txt('PLG_PROJECTS_TEAM_SORT_BY') . ' ' . strtolower(Lang::txt('PLG_PROJECTS_TEAM_NAME')); ?>">
-						&darr; <?php echo Lang::txt('PLG_PROJECTS_TEAM_NAME'); ?>
-					</a>
-				</li>
-			</ul>
-			<p class="msg-total"><?php echo ucfirst(Lang::txt('PLG_PROJECTS_TEAM_SHOWING')); ?> <?php if ($this->total <= count($this->team)) { echo Lang::txt('PLG_PROJECTS_TEAM_ALL'); } ?> <span class="prominent"><?php echo count($this->team); ?></span>  <?php if ($this->total > count($this->team)) { echo Lang::txt('PLG_PROJECTS_TEAM_OUT_OF') . ' ' . $this->total; } ?> <?php echo Lang::txt('PLG_PROJECTS_TEAM_MEMBERS'); ?></p>
-		</div>
+		<!-- <p class="msg-total"><?php echo ucfirst(Lang::txt('PLG_PROJECTS_TEAM_SHOWING')); ?> <?php if ($this->total <= count($this->team)) { echo Lang::txt('PLG_PROJECTS_TEAM_ALL'); } ?> <span class="prominent"><?php echo count($this->team); ?></span>  <?php if ($this->total > count($this->team)) { echo Lang::txt('PLG_PROJECTS_TEAM_OUT_OF') . ' ' . $this->total; } ?> <?php echo Lang::txt('PLG_PROJECTS_TEAM_MEMBERS'); ?></p> -->
+
 		<table id="teamlist" class="listing">
 			<thead>
 				<tr>
-					<th class="imagebox"></th>
-					<th class="th_user i_user"><?php echo Lang::txt('PLG_PROJECTS_TEAM_NAME'); ?></th>
-					<th><?php echo Lang::txt('PLG_PROJECTS_TEAM_ROLE'); ?></th>
-					<th><?php echo Lang::txt('PLG_PROJECTS_TEAM_JOINED'); ?></th>
+					<th class="priority-3 imagebox"></th>
+					<th>
+						<a class="sort-title <?php if ($this->filters['sortby'] == 'name') { echo strtolower($this->filters['sortdir']) . ' active'; } ?>" href="<?php echo Route::url($this->model->link('team') . $sortAppend . '&sortby=name'); ?>" title="<?php echo Lang::txt('PLG_PROJECTS_TEAM_SORT_BY', strtolower(Lang::txt('PLG_PROJECTS_TEAM_NAME'))); ?>">
+							<?php echo Lang::txt('PLG_PROJECTS_TEAM_NAME'); ?>
+						</a>
+					</th>
+					<th>
+						<a class="sort-title <?php if ($this->filters['sortby'] == 'role') { echo strtolower($this->filters['sortdir']) . ' active'; } ?>" href="<?php echo Route::url($this->model->link('team') . $sortAppend . '&sortby=role'); ?>" title="<?php echo Lang::txt('PLG_PROJECTS_TEAM_SORT_BY', strtolower(Lang::txt('PLG_PROJECTS_TEAM_ROLE'))); ?>">
+							<?php echo Lang::txt('PLG_PROJECTS_TEAM_ROLE'); ?>
+						</a>
+					</th>
+					<th class="priority-5">
+						<a class="sort-title <?php if ($this->filters['sortby'] == 'date') { echo strtolower($this->filters['sortdir']) . ' active'; } ?>" href="<?php echo Route::url($this->model->link('team') . $sortAppend . '&sortby=date'); ?>" title="<?php echo Lang::txt('PLG_PROJECTS_TEAM_SORT_BY', strtolower(Lang::txt('PLG_PROJECTS_TEAM_JOINED'))); ?>">
+							<?php echo Lang::txt('PLG_PROJECTS_TEAM_JOINED'); ?>
+						</a>
+					</th>
 					<?php if ($this->count_groups) { ?>
-					<th class="i_group"></th>
+						<th class="priority-2">
+							<?php echo Lang::txt('PLG_PROJECTS_TEAM_GROUP'); ?>
+						</th>
 					<?php } ?>
-					<th><?php echo Lang::txt('PLG_PROJECTS_TEAM_LAST_VISIT'); ?></th>
+					<th class="priority-4">
+						<?php echo Lang::txt('PLG_PROJECTS_TEAM_LAST_VISIT'); ?>
+					</th>
 				</tr>
 			</thead>
 			<tbody>
 			<?php
 			foreach ($this->team as $owner)
 			{
-				// Get profile thumb image
-				$profile = User::getInstance($owner->userid);
-				$actor   = User::getInstance($this->uid);
-
-				$thumb   = $profile->get('id') ? $profile->picture() : $actor->picture(true);
-
-				$timecheck = date('Y-m-d H:i:s', time() - (15 * 60));
-				$lastvisit = $owner->lastvisit && $owner->lastvisit != '0000-00-00 00:00:00'
-							? \Components\Projects\Helpers\Html::timeAgo($owner->lastvisit) . ' ' . Lang::txt('PLG_PROJECTS_TEAM_AGO')
-							: Lang::txt('PLG_PROJECTS_TEAM_NEVER');
-				$lastvisit = $owner->userid == $this->uid || ($owner->online && $owner->lastvisit > $timecheck)
-							? '<span class="online">' . Lang::txt('PLG_PROJECTS_TEAM_ONLINE_NOW') . '</span>'
-							: $lastvisit;
-
 				// User deleted?
 				// Edge case!
 				if ($owner->userid && !$owner->username)
@@ -118,6 +101,8 @@ $sortAppend = '&sortdir=' . urlencode($sortbyDir);
 					continue;
 				}
 
+				$profile = User::getInstance($owner->userid);
+
 				$creator = $this->model->owner('id') == $owner->userid ? 1 : 0;
 
 				// Determine css class for user
@@ -127,44 +112,73 @@ $sortAppend = '&sortdir=' . urlencode($sortbyDir);
 						$role = Lang::txt('PLG_PROJECTS_TEAM_LABEL_OWNER');
 						break;
 					case '2':
-					default:
-						$role = Lang::txt('PLG_PROJECTS_TEAM_LABEL_COLLABORATOR');
-						break;
 					case '5':
 						$role = Lang::txt('PLG_PROJECTS_TEAM_LABEL_REVIEWER');
+						break;
+					default:
+						$role = Lang::txt('PLG_PROJECTS_TEAM_LABEL_COLLABORATOR');
 						break;
 				}
 				$username = $owner->username ? $owner->username : $owner->invited_email;
 				?>
 				<tr class="mline <?php if ($owner->userid == $this->uid) { echo 'native'; } ?>" id="tr_<?php echo $owner->id; ?>">
-					<td class="imagebox"><a href="<?php echo Route::url('index.php?option=com_members&id=' . $owner->userid); ?>" <?php if ($owner->fullname) { ?>title="<?php echo $this->escape($owner->fullname) . ' (' . $owner->userid . ')'; ?>"<?php } ?>><img src="<?php echo $thumb; ?>" alt="<?php echo $owner->fullname ? $this->escape($owner->fullname) : ''; ?>" /></a></td>
-					<td><?php echo $this->escape($owner->fullname); ?><span class="block mini short prominent"><?php echo $this->escape($username); ?></span></td>
-					<td class="mini"><?php echo $creator && !$this->model->groupOwner() ? '<span class="prominent">' . Lang::txt('PLG_PROJECTS_TEAM_OWNER') . '</span>/' : ''; echo $role; ?></td>
-					<td class="mini"><?php echo $owner->status == 1 ? Date::of($owner->added)->toLocal('M d, Y') : '<span class="invited">' . Lang::txt('PLG_PROJECTS_TEAM_INVITED').'</span>';  ?></td>
+					<td class="priority-3 imagebox">
+						<a class="user-img-wrap" href="<?php echo Route::url('index.php?option=com_members&id=' . $owner->userid); ?>" <?php if ($owner->fullname) { ?>title="<?php echo $this->escape($owner->fullname) . ' (' . $owner->userid . ')'; ?>"<?php } ?>>
+							<img src="<?php echo $profile->picture(); ?>" alt="<?php echo $owner->fullname ? $this->escape($owner->fullname) : ''; ?>" />
+						</a>
+					</td>
+					<td>
+						<?php echo $this->escape($owner->fullname); ?>
+						<span class="block mini short prominent"><?php echo $this->escape($username); ?></span>
+					</td>
+					<td>
+						<?php echo $creator && !$this->model->groupOwner() ? '<span class="prominent">' . Lang::txt('PLG_PROJECTS_TEAM_OWNER') . '</span>/' : ''; echo $role; ?>
+					</td>
+					<td class="priority-5">
+						<?php echo $owner->status == 1 ? '<time datetime="' . Date::of($owner->added)->format('Y-m-d\TH:i:s\Z') . '">' . Date::of($owner->added)->toLocal('M d, Y') . '</time>' : '<span class="invited">' . Lang::txt('PLG_PROJECTS_TEAM_INVITED').'</span>';  ?>
+					</td>
 					<?php if ($this->count_groups) { ?>
-					<td><?php echo $owner->groupdesc ? \Hubzero\Utility\String::truncate($owner->groupdesc, 30) : ''; ?><span class="block mini short prominent"><?php echo $owner->groupname; ?></span></td>
+						<td class="priority-2">
+							<?php echo $owner->groupdesc ? \Hubzero\Utility\String::truncate($owner->groupdesc, 30) : ''; ?><span class="block mini short prominent"><?php echo $owner->groupname; ?></span>
+						</td>
 					<?php } ?>
-					<td class="mini"><?php echo $lastvisit; ?></td>
+					<td class="priority-4">
+						<?php
+						$timecheck = date('Y-m-d H:i:s', time() - (15 * 60));
+						$lastvisit = Lang::txt('PLG_PROJECTS_TEAM_NEVER');
+						if ($owner->lastvisit && $owner->lastvisit != '0000-00-00 00:00:00')
+						{
+							$lastvisit = '<time datetime="' . Date::of($owner->lastvisit)->format('Y-m-d\TH:i:s\Z') . '">' . Components\Projects\Helpers\Html::timeAgo($owner->lastvisit) . ' ' . Lang::txt('PLG_PROJECTS_TEAM_AGO') . '</time>';
+						}
+						if ($owner->userid == $this->uid || ($owner->online && $owner->lastvisit > $timecheck))
+						{
+							$lastvisit = '<span class="online">' . Lang::txt('PLG_PROJECTS_TEAM_ONLINE_NOW') . '</span>';
+						}
+						echo $lastvisit;
+						?>
+					</td>
 				</tr>
 				<?php
 			}
 			?>
 			</tbody>
 		</table>
-		<?php
-		// Pagination
-		$pageNav = $this->pagination(
-			$this->total,
-			$this->filters['start'],
-			$this->filters['limit']
-		);
-		$pageNav->setAdditionalUrlParam('sortby', $this->filters['sortby']);
-		$pageNav->setAdditionalUrlParam('sortdir', $this->filters['sortdir']);
 
-		$pagenavhtml = $pageNav->render();
-		?>
 		<fieldset>
-			<?php echo $pagenavhtml; ?>
+			<?php
+			// Pagination
+			$pageNav = $this->pagination(
+				$this->total,
+				$this->filters['start'],
+				$this->filters['limit']
+			);
+			$pageNav->setAdditionalUrlParam('sortby', $this->filters['sortby']);
+			$pageNav->setAdditionalUrlParam('sortdir', $this->filters['sortdir']);
+
+			$pagenavhtml = $pageNav->render();
+
+			echo $pagenavhtml;
+			?>
 		</fieldset>
 	</div>
 </form>
