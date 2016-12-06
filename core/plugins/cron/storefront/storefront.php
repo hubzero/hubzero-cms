4a1f3f399a54b6725a275d7ac837cba40911ab14
Ilya Shunko <ishunko@purdue.edu>
2016-12-06 16:24:18 +0000

    [PLG_CRON_STOREFRONT] Fix code formatting

--- core/plugins/cron/storefront/storefront.php
+++ core/plugins/cron/storefront/storefront.php
@@ -71,11 +71,11 @@ class plgCronStorefront extends \Hubzero\Plugin\Plugin
 		if (is_object($params) &&
 			($params->get('publish_down_notification1') || $params->get('publish_down_notification2')))
 		{
-			if($params->get('publish_down_notification1'))
+			if ($params->get('publish_down_notification1'))
 			{
 				$days[] = $params->get('publish_down_notification1');
 			}
-			if($params->get('publish_down_notification2'))
+			if ($params->get('publish_down_notification2'))
 			{
 				$days[] = $params->get('publish_down_notification2');
 			}
