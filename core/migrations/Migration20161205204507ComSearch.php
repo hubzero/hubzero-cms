1da8de1d463c130929c3eedfe98f2f04f5536369
Kevin Wojkovich <kevinw@purdue.edu>
2016-12-12 20:13:49 +0000

    [COM_SEARCH][MIGRATIONS] Refactors queue table, blacklist, et. al

---
+++ core/migrations/Migration20161205204507ComSearch.php
@@ -0,0 +1,25 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+/**
+ * Migration script for ...
+ **/
+class Migration20161205204507ComSearch extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+
+	}
+}
