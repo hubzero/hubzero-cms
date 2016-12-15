48516e5bdb0c74f4c730fb22e0d6b1b918a8e59d
Kevin Wojkovich <kevinw@purdue.edu>
2016-12-15 13:52:51 +0000

    [HUBZERO][#10226][COM_SEARCH] Adds catch for malformed query.

--- core/components/com_search/site/controllers/solr.php
+++ core/components/com_search/site/controllers/solr.php
@@ -117,7 +117,17 @@ class Solr extends SiteController
 		$urlQuery .= '&start='.$start;
 
 		// Perform the query
-		$query = $query->run();
+		try
+		{
+			$query = $query->run();
+		}
+		catch (\Solarium\Exception\HttpException $e)
+		{
+			//@TODO: 'Did you mean' functionality.
+			$query->query('')->limit($limit)->start($start)->run();
+			\Notify::warning(Lang::txt('COM_SEARCH_MALFORMED_QUERY'));
+		}
+
 		$results = $query->getResults();
 		$numFound = $query->getNumFound();
 
