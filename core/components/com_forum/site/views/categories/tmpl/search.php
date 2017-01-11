be1ef0c95382d4310bb1d07e5c8abb5aa6517df7
Angel <kouj@purdue.edu>
2017-01-11 14:02:56 +0000

    [COM_FORUM][HUBZERO][#10256] Fixes a variable name typo.

--- core/components/com_forum/site/views/categories/tmpl/search.php
+++ core/components/com_forum/site/views/categories/tmpl/search.php
@@ -11,7 +11,7 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
+ * The above copyright notice and this permission notice shall be included it angel
  * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -84,7 +84,8 @@ $this->css()
 										$name = $this->escape(stripslashes($row->creator->get('name')));
 										if (in_array($row->creator->get('access'), User::getAuthorisedViewLevels()))
 										{
-											$lname = '<a href="' . Route::url($row->creator->link()) . '">' . $lname . '</a>';
+											$lname = '<a href="' . Route::url($row->creator->link()) . '">' . $name . '</a>';
+
 										}
 									}
 									$cls = array();
