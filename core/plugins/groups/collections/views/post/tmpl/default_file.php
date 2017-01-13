3980c9a7d8dfac70cd933cac0ff5318b72836299
Shawn Rice <zooley@purdue.edu>
2017-01-13 20:06:32 +0000

    [HUBZERO][#10298][PLG_*_COLLECTIONS] Fail a little mroe gracefully when image file can't be found

--- core/plugins/groups/collections/views/post/tmpl/default_file.php
+++ core/plugins/groups/collections/views/post/tmpl/default_file.php
@@ -74,42 +74,51 @@ if ($assets->total() > 0)
 		$isLocal = (filter_var($first->file('original'), FILTER_VALIDATE_URL)) ? false : true;
 		$imgPath = $isLocal ? $path . DS . $first->file('thumbnail') : $first->file('original');
 
-		list($originalWidth, $originalHeight) = getimagesize($imgPath);
-		$ratio = $originalWidth / $originalHeight;
-		?>
+		if (file_exists($imgPath))
+		{
+			list($originalWidth, $originalHeight) = getimagesize($imgPath);
+			$ratio = $originalWidth / $originalHeight;
 
-		<?php if ($isLocal) : ?>
-			<div class="holder">
-				<a class="img-link" data-rel="post<?php echo $this->row->get('id'); ?>" href="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($first->get('filename'), DS) . '&size=medium'); ?>" data-download="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($first->get('filename'), DS) . '&size=original'); ?>" data-downloadtext="<?php echo Lang::txt('PLG_GROUPS_COLLECTIONS_DOWNLOAD'); ?>">
-					<img src="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($first->get('filename'), DS) . '&size=thumb'); ?>" alt="<?php echo ($first->get('description')) ? $this->escape(stripslashes($first->get('description'))) : ''; ?>" class="img" style="height: <?php echo (!isset($this->actual) || !$this->actual) ? round($this->params->get('maxWidth', 290) / $ratio, 0, PHP_ROUND_HALF_UP) : $originalHeight; ?>px;" />
-				</a>
+			if ($isLocal) : ?>
+				<div class="holder">
+					<a class="img-link" data-rel="post<?php echo $this->row->get('id'); ?>" href="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($first->get('filename'), DS) . '&size=medium'); ?>" data-download="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($first->get('filename'), DS) . '&size=original'); ?>" data-downloadtext="<?php echo Lang::txt('PLG_GROUPS_COLLECTIONS_DOWNLOAD'); ?>">
+						<img src="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($first->get('filename'), DS) . '&size=thumb'); ?>" alt="<?php echo ($first->get('description')) ? $this->escape(stripslashes($first->get('description'))) : ''; ?>" class="img" style="height: <?php echo (!isset($this->actual) || !$this->actual) ? round($this->params->get('maxWidth', 290) / $ratio, 0, PHP_ROUND_HALF_UP) : $originalHeight; ?>px;" />
+					</a>
+				</div>
+			<?php else : ?>
+				<div class="holder">
+					<a target="_blank" class="img-link" data-rel="post<?php echo $this->row->get('id'); ?>" href="<?php echo $imgPath; ?>" data-download="<?php echo $imgPath; ?>" data-downloadtext="<?php echo Lang::txt('PLG_GROUPS_COLLECTIONS_DOWNLOAD'); ?>">
+						<img src="<?php echo $imgPath; ?>" alt="<?php echo ($first->get('description')) ? $this->escape(stripslashes($first->get('description'))) : ''; ?>" class="img" style="height: <?php echo (!isset($this->actual) || !$this->actual) ? round($this->params->get('maxWidth', 290) / $ratio, 0, PHP_ROUND_HALF_UP) : $originalHeight; ?>px;" />
+					</a>
+				</div>
+			<?php endif;
+		}
+		else
+		{
+			?>
+			<div class="holder notfound">
+				<p class="warning"><?php echo Lang::txt('Image not found.'); ?></p>
 			</div>
-		<?php else : ?>
-			<div class="holder">
-				<a target="_blank" class="img-link" data-rel="post<?php echo $this->row->get('id'); ?>" href="<?php echo $imgPath; ?>" data-download="<?php echo $imgPath; ?>" data-downloadtext="<?php echo Lang::txt('PLG_GROUPS_COLLECTIONS_DOWNLOAD'); ?>">
-					<img src="<?php echo $imgPath; ?>" alt="<?php echo ($first->get('description')) ? $this->escape(stripslashes($first->get('description'))) : ''; ?>" class="img" style="height: <?php echo (!isset($this->actual) || !$this->actual) ? round($this->params->get('maxWidth', 290) / $ratio, 0, PHP_ROUND_HALF_UP) : $originalHeight; ?>px;" />
-				</a>
-			</div>
-		<?php endif; ?>
+			<?php
+		}
 
-		<?php
 		if (count($images) > 0)
 		{
 			?>
 			<div class="gallery">
-			<?php
-			foreach ($images as $asset)
-			{
-				?>
-				<a class="img-link" data-rel="post<?php echo $this->row->get('id'); ?>" href="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($asset->get('filename'), DS) . '&size=medium'); ?>" data-download="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($asset->get('filename'), DS) . '&size=original'); ?>" data-downloadtext="<?php echo Lang::txt('PLG_GROUPS_COLLECTIONS_DOWNLOAD'); ?>">
-					<img src="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($asset->get('filename'), DS) . '&size=thumb'); ?>" alt="<?php echo ($asset->get('description')) ? $this->escape(stripslashes($asset->get('description'))) : ''; ?>" class="img" width="50" height="50" />
-				</a>
 				<?php
-			}
-			?>
+				foreach ($images as $asset)
+				{
+					?>
+					<a class="img-link" data-rel="post<?php echo $this->row->get('id'); ?>" href="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($asset->get('filename'), DS) . '&size=medium'); ?>" data-download="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($asset->get('filename'), DS) . '&size=original'); ?>" data-downloadtext="<?php echo Lang::txt('PLG_GROUPS_COLLECTIONS_DOWNLOAD'); ?>">
+						<img src="<?php echo Route::url($href . $this->row->get('id') . '&file=' . ltrim($asset->get('filename'), DS) . '&size=thumb'); ?>" alt="<?php echo ($asset->get('description')) ? $this->escape(stripslashes($asset->get('description'))) : ''; ?>" class="img" width="50" height="50" />
+					</a>
+					<?php
+				}
+				?>
 				<div class="clearfix"></div>
 			</div>
-		<?php
+			<?php
 		}
 	}
 	//$assets->rewind();
