59260334dfaf64aead2d1caa8bf2330f70c750c5
Shawn Rice <zooley@purdue.edu>
2017-01-20 12:12:14 +0000

    [COM_SEARCH] Fixing docblock, minor formatting clean-up, some very minor simplification of code.

--- core/components/com_search/helpers/solr.php
+++ core/components/com_search/helpers/solr.php
@@ -1,33 +1,68 @@
 <?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 HUBzero Foundation, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @since     2.1.1
+ */
+
 namespace Components\Search\Helpers;
 
 use Components\Search\Models\Hubtype;
 use stdClass;
-use \Solarium;
+use Solarium;
 
-
+/**
+ * Solr helper class
+ */
 class SolrHelper
 {
 	/**
 	 * enqueueDB
 	 * 
-	 * @param string $type 
-	 * @param int $id 
+	 * @param   string  $type 
+	 * @param   array   $ids
+	 * @param   string  $action
 	 * @static
-	 * @access public
-	 * @return void
+	 * @access  public
+	 * @return  void
+	 * @throws  Hubzero\Exception\Exception
 	 */
 	public static function enqueueDB($type = '', $ids = array(), $action='index')
 	{
 		if ($type != '' && !empty($ids))
 		{
 			$db = App::get('db');
-			$userID = User::getInstance()->get('uidNumber');
+			$userID = User::get('id');
 			$timestamp = Date::of()->toSql();
 
 			if ($db->tableExists('#__search_queue') && count($ids) > 0)
 			{
-				$sql = "INSERT INTO #__search_queue (type, type_id, status, action, created_by, created) VALUES ";
+				$sql = "INSERT INTO `#__search_queue` (type, type_id, status, action, created_by, created) VALUES ";
 
 				foreach ($ids as $key => $id)
 				{
@@ -63,8 +98,8 @@ class SolrHelper
 	 * queueStatus - a rudimentary report to check up on the queue
 	 * 
 	 * @static
-	 * @access public
-	 * @return void
+	 * @access  public
+	 * @return  void
 	 */
 	public static function queueStatus()
 	{
@@ -76,7 +111,7 @@ class SolrHelper
 		(SELECT count(*) FROM `#__search_queue` WHERE status = 0) as notstarted,
 		(SELECT count(*) FROM `#__search_queue` WHERE status = 1) as indexed,
 		(SELECT count(*) FROM `#__search_queue` WHERE status = 2) as failed
-		FROM #__search_queue;";
+		FROM `#__search_queue`;";
 
 		$db = App::get('db');
 		$db->setQuery($sql);
@@ -88,10 +123,10 @@ class SolrHelper
 	/**
 	 * parseDocumentID - returns a friendly way to access the type and id from a solr ID 
 	 * 
-	 * @param string $id 
+	 * @param   string  $id 
 	 * @static
-	 * @access public
-	 * @return void
+	 * @access  public
+	 * @return  mixed
 	 */
 	public static function parseDocumentID($id = '')
 	{
@@ -102,12 +137,12 @@ class SolrHelper
 			if (count($parts) == 3)
 			{
 				$type = $parts[0] . '-' . $parts[1];
-				$id = $parts[2];
+				$id   = $parts[2];
 			}
 			elseif (count($parts) == 2)
 			{
 				$type = $parts[0];
-				$id = $parts[1];
+				$id   = $parts[1];
 			}
 
 			return array('type' => $type, 'id' => $id);
