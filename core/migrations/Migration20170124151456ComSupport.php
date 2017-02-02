ec08d1ce491dd90bba563051bd970c6c590f28c7
Kevin Wojkovich <kwojkovi@krusty.hubzero.org>
2017-02-02 14:38:10 -0500

    [MIGRATION][COM_SUPPORT] Fixes styles adds some additional checks.

--- core/migrations/Migration20170124151456ComSupport.php
+++ core/migrations/Migration20170124151456ComSupport.php
@@ -2,6 +2,9 @@
 
 use Hubzero\Content\Migration\Base;
 
+// No direct access
+defined('_HZEXEC_') or die();
+
 /**
  * Migration script for adding MP4 extension to params
  **/
@@ -12,20 +15,26 @@ class Migration20170124151456ComSupport extends Base
 	 **/
 	public function up()
 	{
-		if($this->db->tableExists('#__extensions')){
+		if ($this->db->tableExists('#__extensions'))
+		{
 			$query = "SELECT params FROM #__extensions WHERE name = 'com_support';";
 			$this->db->setQuery($query);
 			$params = $this->db->query()->loadResult();
 			$params = json_decode($params);
-			$fileExt = explode(",",$params->file_ext);
-			if(!in_array('mp4', $fileExt)){
+			$fileExt = explode(",", $params->file_ext);
+
+			// Prevent duplicates
+			if (!in_array('mp4', $fileExt))
+			{
 				array_push($fileExt, 'mp4');
 			}
+
 			$params->file_ext = implode(",", $fileExt);
-            		$params = json_encode($params);
+		  $params = json_encode($params);
+
 			$query2 = "UPDATE `#__extensions` SET params=" . $this->db->quote($params) . " WHERE name='com_support';";
 			$this->db->setQuery($query2);
-			$this->db->query();	
+			$this->db->query();
 		}
 	}
 
@@ -34,17 +43,26 @@ class Migration20170124151456ComSupport extends Base
 	 **/
 	public function down()
 	{
-			$query = "SELECT params FROM #__extensions WHERE name = 'com_support';";
-                        $this->db->setQuery($query);
-                        $params = $this->db->query()->loadResult();
-                        $params = json_decode($params);
-			$fileExt = explode(",", $params->file_ext);
-			$index = array_search('mp4', $fileExt);
+		$query = "SELECT params FROM #__extensions WHERE name = 'com_support';";
+		$this->db->setQuery($query);
+
+		$params = $this->db->query()->loadResult();
+		$params = json_decode($params);
+
+		$fileExt = explode(",", $params->file_ext);
+		$index = array_search('mp4', $fileExt);
+
+		// Prevents invalid array access
+		if ($index !== false)
+		{
 			unset($fileExt[$index]);
-                        $params->file_ext = implode(",", $fileExt);
-                        $params = json_encode($params);
-                        $query2 = "UPDATE `#__extensions` SET params=" . $this->db->quote($params) . " WHERE name='com_support';";
-                        $this->db->setQuery($query2);
-                        $this->db->query();
+		}
+
+		$params->file_ext = implode(",", $fileExt);
+		$params = json_encode($params);
+
+		$query2 = "UPDATE `#__extensions` SET params=" . $this->db->quote($params) . " WHERE name='com_support';";
+		$this->db->setQuery($query2);
+		$this->db->query();
 	}
 }
