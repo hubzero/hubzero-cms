f0fab0a2caef9f3a19fd2a4db64861df39e2913e
Shawn Rice <zooley@purdue.edu>
2017-02-07 10:49:36 +0000

    [MIGRATIONS][COM_STOREFRONT] Formattng fixes

--- core/migrations/Migration20170207025712PlgFilesystemGithub.php
+++ core/migrations/Migration20170207025712PlgFilesystemGithub.php
@@ -3,7 +3,7 @@
 use Hubzero\Content\Migration\Base;
 
 /**
- * Migration script for ...
+ * Migration script for adding GitHub filesystem plugin
  **/
 class Migration20170207025712PlgFilesystemGithub extends Base
 {
@@ -13,11 +13,13 @@ class Migration20170207025712PlgFilesystemGithub extends Base
 	public function up()
 	{
 		$this->addPluginEntry('filesystem', 'github');
-		if($this->db->tableExists('#__projects_connection_providers'))
+
+		if ($this->db->tableExists('#__projects_connection_providers'))
 		{
 			$query = "SELECT * FROM `#__projects_connection_providers` WHERE `alias`='github'";
 			$this->db->setQuery($query);
 			$results = $this->db->loadObjectList();
+
 			if (count($results) < 1)
 			{
 				$query = "INSERT INTO `#__projects_connection_providers` (`alias`, `name`) VALUES ('github','Github')";
@@ -25,7 +27,6 @@ class Migration20170207025712PlgFilesystemGithub extends Base
 				$this->db->query();
 			}
 		}
-
 	}
 
 	/**
@@ -34,17 +35,20 @@ class Migration20170207025712PlgFilesystemGithub extends Base
 	public function down()
 	{
 		$this->deletePluginEntry('filesystem', 'github');
-		if($this->db->tableExists('#__projects_connection_providers'))
+
+		if ($this->db->tableExists('#__projects_connection_providers'))
 		{
 			$query = "SELECT * FROM `#__projects_connection_providers` WHERE `alias`='github'";
 			$this->db->setQuery($query);
 			$results = $this->db->loadObjectList();
-			foreach($results as $result)
+
+			foreach ($results as $result)
 			{
 				$query = "DELETE FROM `#__projects_connections` WHERE `provider_id`=" . $this->db->quote($result->id);
 				$this->db->setQuery($query);
 				$this->db->query();
 			}
+
 			if (count($results) > 0)
 			{
 				$query = "DELETE FROM `#__projects_connection_providers` WHERE `alias`='github'";
