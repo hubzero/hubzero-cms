ac863a2561440f4e5a2bd020bd5c8cd42791d3a6
Shawn Rice <zooley@purdue.edu>
2017-03-01 18:11:37 +0000

    [COM_CONTENT] Fixing some minor style issues with category layouts

--- core/components/com_content/site/views/category/tmpl/default.php
+++ core/components/com_content/site/views/category/tmpl/default.php
@@ -12,47 +12,53 @@ defined('_HZEXEC_') or die();
 Html::addIncludePath(JPATH_COMPONENT.'/helpers');
 
 ?>
-<div class="category-list<?php echo $this->pageclass_sfx;?>">
-
-	<?php if ($this->params->get('show_page_heading')) : ?>
-	<h1>
-		<?php echo $this->escape($this->params->get('page_heading')); ?>
-	</h1>
-	<?php endif; ?>
-
-	<?php if ($this->params->get('show_category_title', 1) or $this->params->get('page_subheading')) : ?>
-	<h2>
-		<?php echo $this->escape($this->params->get('page_subheading')); ?>
-		<?php if ($this->params->get('show_category_title')) : ?>
-			<span class="subheading-category"><?php echo $this->category->title;?></span>
+<?php if ($this->params->get('show_page_heading') or $this->params->get('show_category_title', 1) or $this->params->get('page_subheading')) : ?>
+	<header id="content-header">
+		<?php if ($this->params->get('show_page_heading')) : ?>
+			<h2>
+				<?php echo $this->escape($this->params->get('page_heading')); ?>
+			</h2>
 		<?php endif; ?>
-	</h2>
-	<?php endif; ?>
 
-	<?php if ($this->params->get('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
-	<div class="category-desc">
-		<?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image')) : ?>
-			<img src="<?php echo $this->category->getParams()->get('image'); ?>"/>
+		<?php if ($this->params->get('show_category_title', 1) or $this->params->get('page_subheading')) : ?>
+			<h2>
+				<?php echo $this->escape($this->params->get('page_subheading')); ?>
+				<?php if ($this->params->get('show_category_title')) : ?>
+					<span class="subheading-category"><?php echo $this->category->title; ?></span>
+				<?php endif; ?>
+			</h2>
 		<?php endif; ?>
-		<?php if ($this->params->get('show_description') && $this->category->description) : ?>
-			<?php echo Html::content('prepare', $this->category->description, '', 'com_content.category'); ?>
+	</header>
+<?php endif; ?>
+
+<section class="main section">
+	<div class="category-list<?php echo $this->pageclass_sfx;?>">
+
+		<?php if ($this->params->get('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
+			<div class="category-desc">
+				<?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image')) : ?>
+					<img src="<?php echo $this->category->getParams()->get('image'); ?>" alt="" />
+				<?php endif; ?>
+				<?php if ($this->params->get('show_description') && $this->category->description) : ?>
+					<?php echo Html::content('prepare', $this->category->description, '', 'com_content.category'); ?>
+				<?php endif; ?>
+				<div class="clr"></div>
+			</div>
 		<?php endif; ?>
-		<div class="clr"></div>
+
+		<div class="cat-items">
+			<?php echo $this->loadTemplate('articles'); ?>
+		</div>
+
+		<?php if (!empty($this->children[$this->category->id])&& $this->maxLevel != 0) : ?>
+			<div class="cat-children">
+				<?php if ($this->params->get('show_category_heading_title_text', 1) == 1) : ?>
+					<h3>
+						<?php echo Lang::txt('JGLOBAL_SUBCATEGORIES'); ?>
+					</h3>
+				<?php endif; ?>
+				<?php echo $this->loadTemplate('children'); ?>
+			</div>
+		<?php endif; ?>
 	</div>
-	<?php endif; ?>
-
-	<div class="cat-items">
-		<?php echo $this->loadTemplate('articles'); ?>
-	</div>
-
-	<?php if (!empty($this->children[$this->category->id])&& $this->maxLevel != 0) : ?>
-	<div class="cat-children">
-		<?php if ($this->params->get('show_category_heading_title_text', 1) == 1) : ?>
-		<h3>
-			<?php echo Lang::txt('JGLOBAL_SUBCATEGORIES'); ?>
-		</h3>
-		<?php endif; ?>
-		<?php echo $this->loadTemplate('children'); ?>
-	</div>
-	<?php endif; ?>
-</div>
+</section>
