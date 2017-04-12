2c319d66b4b7211d43b5af4f8c8d8bdc5af60e17
Shawn Rice <zooley@purdue.edu>
2017-04-12 20:56:01 +0000

    [COM_PROJECTS] Fleshing out ORM models

--- core/components/com_projects/models/orm/provider.php
+++ core/components/com_projects/models/orm/provider.php
@@ -35,7 +35,7 @@ namespace Components\Projects\Models\Orm;
 use Hubzero\Database\Relational;
 
 /**
- * Provider database model
+ * Connection provider model
  *
  * @uses  \Hubzero\Database\Relational
  */
@@ -47,4 +47,50 @@ class Provider extends Relational
 	 * @var  string
 	 **/
 	protected $namespace = 'projects_connection';
+
+	/**
+	 * Default order by for model
+	 *
+	 * @var string
+	 */
+	public $orderBy = 'name';
+
+	/**
+	 * Default order direction for select queries
+	 *
+	 * @var  string
+	 */
+	public $orderDir = 'asc';
+
+	/**
+	 * Fields and their validation criteria
+	 *
+	 * @var  array
+	 */
+	protected $rules = array(
+		'name' => 'notempty'
+	);
+
+	/**
+	 * Automatically fillable fields
+	 *
+	 * @var  array
+	 */
+	public $always = array(
+		'alias'
+	);
+
+	/**
+	 * Generates automatic owned by field value
+	 *
+	 * @param   array   $data  the data being saved
+	 * @return  string
+	 */
+	public function automaticAlias($data)
+	{
+		$alias = (isset($data['alias']) && $data['alias'] ? $data['alias'] : $data['name']);
+		$alias = strip_tags($alias);
+
+		return preg_replace("/[^a-zA-Z0-9\-]/", '', strtolower($alias));
+	}
 }
