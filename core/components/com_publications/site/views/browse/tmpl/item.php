5c83a171f1a1101ef8304ca9558b2d2d350054e8
Shawn Rice <zooley@purdue.edu>
2017-04-25 17:29:32 +0000

    [COM_PUBLICATIONS] Removing unused images and CSS. Some view clean-up.

--- core/components/com_publications/site/views/browse/tmpl/item.php
+++ core/components/com_publications/site/views/browse/tmpl/item.php
@@ -31,68 +31,22 @@
 // No direct access
 defined('_HZEXEC_') or die();
 
-$html  = "\t".'<li';
+$cls = array('publication');
 switch ($this->line->get('master_access'))
 {
-	case 1: $html .= ' class="registered"'; break;
-	case 2: $html .= ' class="protected"'; break;
-	case 3: $html .= ' class="private"'; break;
+	case 1:
+		$cls[] = 'registered';
+		break;
+	case 2:
+		$cls[] = 'protected';
+		break;
+	case 3:
+		$cls[] = 'private';
+		break;
 	case 0:
-	default: $html .= ' class="public"'; break;
-}
-$html .= '>' . "\n";
-$html .= "\t". "\t". '<div class="pub-thumb"><img src="' . Route::url($this->line->link('thumb')) . '" alt=""/></div>' . "\n";
-$html .= "\t" . "\t" . '<div class="pub-details">' . "\n";
-$html .= "\t\t".'<p class="title"><a href="' . Route::url($this->line->link()) . '">' .  $this->escape($this->line->title) . '</a>' . "\n";
-$html .= '</p>' . "\n";
-
-if ($this->params->get('show_ranking') && $this->config->get('show_ranking'))
-{
-	$database = \App::get('db');
-
-	$ranking = round($this->line->get('master_ranking'), 1);
-
-	$r = (10*$ranking);
-	if (intval($r) < 10)
-	{
-		$r = '0' . $r;
-	}
-
-	$html .= "\t\t" . '<div class="metadata">' . "\n";
-	$html .= "\t\t\t" . '<dl class="rankinfo">' . "\n";
-	$html .= "\t\t\t\t" . '<dt class="ranking"><span class="rank"><span class="rank-' . $r . '" style="width: ' . $r . '%;">' . Lang::txt('COM_PUBLICATIONS_THIS_HAS') . '</span></span> ' . number_format($ranking, 1) . ' '.Lang::txt('COM_PUBLICATIONS_RANKING') . '</dt>' . "\n";
-	$html .= "\t\t\t\t".'<dd>' . "\n";
-	$html .= "\t\t\t\t\t" . '<p>' . Lang::txt('COM_PUBLICATIONS_RANKING_EXPLANATION') . '</p>' . "\n";
-	$html .= "\t\t\t\t\t" . '<div>' . "\n";
-	$html .= "\t\t\t\t\t" . '</div>' . "\n";
-	$html .= "\t\t\t\t" . '</dd>' . "\n";
-	$html .= "\t\t\t" . '</dl>' . "\n";
-	$html .= "\t\t" . '</div>' . "\n";
-} elseif ($this->params->get('show_rating') && $this->config->get('show_rating')) {
-	switch ($this->line->get('master_rating'))
-	{
-		case 0.5: $class = ' half-stars';      break;
-		case 1:   $class = ' one-stars';       break;
-		case 1.5: $class = ' onehalf-stars';   break;
-		case 2:   $class = ' two-stars';       break;
-		case 2.5: $class = ' twohalf-stars';   break;
-		case 3:   $class = ' three-stars';     break;
-		case 3.5: $class = ' threehalf-stars'; break;
-		case 4:   $class = ' four-stars';      break;
-		case 4.5: $class = ' fourhalf-stars';  break;
-		case 5:   $class = ' five-stars';      break;
-		case 0:
-		default:  $class = ' no-stars';        break;
-	}
-
-	if ($this->line->get('master_rating') > 5)
-	{
-		$class = ' five-stars';
-	}
-
-	$html .= "\t\t" . '<div class="metadata">' . "\n";
-	$html .= "\t\t\t" . '<p class="rating"><span title="' . Lang::txt('COM_PUBLICATIONS_OUT_OF_5_STARS', $this->line->get('master_rating')) . '" class="avgrating' . $class . '"><span>' . Lang::txt('COM_PUBLICATIONS_OUT_OF_5_STARS', $this->line->get('master_rating')) . '</span>&nbsp;</span></p>' . "\n";
-	$html .= "\t\t" .'</div>' . "\n";
+	default:
+		$cls[] = 'public';
+		break;
 }
 
 $info = array();
@@ -100,7 +54,6 @@ if ($this->thedate)
 {
 	$info[] = $this->thedate;
 }
-
 if (($this->line->category && !intval($this->filters['category'])))
 {
 	$info[] = $this->line->cat_name;
@@ -114,13 +67,102 @@ if ($this->line->doi)
 	$info[] = 'doi:' . $this->line->doi;
 }
 
-$html .= "\t\t" . '<p class="details">' . implode(' <span>|</span> ', $info) . '</p>' . "\n";
-if ($this->line->get('abstract')) {
-	$html .= "\t\t" . \Hubzero\Utility\String::truncate( stripslashes($this->line->get('abstract')), 300 ) . "\n";
-} else if ($this->line->get('description')) {
-	$html .= "\t\t" . \Hubzero\Utility\String::truncate( stripslashes($this->line->get('description')), 300 ) . "\n";
-}
-$html .= "\t" . "\t" . '</div>' . "\n";
-$html .= "\t" . '</li>' . "\n";
-echo $html;
 ?>
+<li class="<?php echo implode(' ', $cls); ?>">
+	<div class="pub-thumb"><img width="40" height="40" src="<?php echo Route::url($this->line->link('thumb')); ?>" alt="" /></div>
+	<div class="pub-details">
+		<p class="title"><a href="<?php echo Route::url($this->line->link()); ?>"><?php echo $this->escape($this->line->title); ?></a></p>
+
+		<?php
+		if ($this->params->get('show_ranking') && $this->config->get('show_ranking'))
+		{
+			$ranking = round($this->line->get('master_ranking'), 1);
+
+			$r = (10 * $ranking);
+			if (intval($r) < 10)
+			{
+				$r = '0' . $r;
+			}
+			?>
+			<div class="metadata">
+				<dl class="rankinfo">
+					<dt class="ranking">
+						<span class="rank">
+							<span class="rank-<?php echo $r; ?>" style="width: <?php echo $r; ?>%;"><?php echo Lang::txt('COM_PUBLICATIONS_THIS_HAS'); ?></span>
+						</span><?php echo number_format($ranking, 1) . ' ' . Lang::txt('COM_PUBLICATIONS_RANKING'); ?>
+					</dt>
+					<dd>
+						<p><?php echo Lang::txt('COM_PUBLICATIONS_RANKING_EXPLANATION'); ?></p>
+						<div></div>
+					</dd>
+				</dl>
+			</div>
+			<?php
+		}
+		elseif ($this->params->get('show_rating') && $this->config->get('show_rating'))
+		{
+			switch ($this->line->get('master_rating'))
+			{
+				case 0.5:
+					$class = ' half-stars';
+					break;
+				case 1:
+					$class = ' one-stars';
+					break;
+				case 1.5:
+					$class = ' onehalf-stars';
+					break;
+				case 2:
+					$class = ' two-stars';
+					break;
+				case 2.5:
+					$class = ' twohalf-stars';
+					break;
+				case 3:
+					$class = ' three-stars';
+					break;
+				case 3.5:
+					$class = ' threehalf-stars';
+					break;
+				case 4:
+					$class = ' four-stars';
+					break;
+				case 4.5:
+					$class = ' fourhalf-stars';
+					break;
+				case 5:
+					$class = ' five-stars';
+					break;
+				case 0:
+				default:
+					$class = ' no-stars';
+					break;
+			}
+
+			if ($this->line->get('master_rating') > 5)
+			{
+				$class = ' five-stars';
+			}
+			?>
+			<div class="metadata">
+				<p class="rating"><span title="<?php echo Lang::txt('COM_PUBLICATIONS_OUT_OF_5_STARS', $this->line->get('master_rating')); ?>" class="avgrating<?php echo $class; ?>"><span><?php echo Lang::txt('COM_PUBLICATIONS_OUT_OF_5_STARS', $this->line->get('master_rating')); ?></span>&nbsp;</span></p>
+			</div>
+			<?php
+		}
+		?>
+
+		<p class="details"><?php echo implode(' <span class="separator">|</span> ', $info); ?></p>
+		<?php
+		$content = '';
+		if ($this->line->get('abstract'))
+		{
+			$content = $this->line->get('abstract');
+		}
+		else if ($this->line->get('description'))
+		{
+			$content = $this->line->get('description');
+		}
+		echo \Hubzero\Utility\String::truncate(stripslashes($content), 300);
+		?>
+	</div>
+</li>
