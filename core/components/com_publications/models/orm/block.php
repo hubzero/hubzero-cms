4f786be7ca6b8dabc0832229ee55f6fae88be047
Shawn Rice <zooley@purdue.edu>
2017-04-28 17:50:23 +0000

    [COM_PUBLICATIONS] Adding block model

--- core/components/com_publications/models/orm/block.php
+++ core/components/com_publications/models/orm/block.php
@@ -34,19 +34,17 @@ namespace Components\Publications\Models\Orm;
 
 use Hubzero\Database\Relational;
 
-require_once __DIR__ . DS . 'block.php';
-
 /**
- * Model class for publication type
+ * Model class for publication block
  */
-class Type extends Relational
+class Block extends Relational
 {
 	/**
 	 * The table namespace
 	 *
 	 * @var  string
 	 */
-	public $namespace = 'publication_master';
+	public $namespace = 'publication';
 
 	/**
 	 * Fields and their validation criteria
@@ -54,8 +52,16 @@ class Type extends Relational
 	 * @var  array
 	 */
 	protected $rules = array(
-		'type'  => 'notempty',
-		'alias' => 'notempty'
+		'label' => 'notempty'
+	);
+
+	/**
+	 * Automatically fillable fields
+	 *
+	 * @var  array
+	 */
+	public $always = array(
+		'block'
 	);
 
 	/**
@@ -64,46 +70,90 @@ class Type extends Relational
 	 * @var  array
 	 */
 	public $initiate = array(
-		'created',
+		'ordering'
 	);
 
 	/**
-	 * Get last order
+	 * Configuration registry
 	 *
-	 * @return  integer
+	 * @var  object
 	 */
-	public function getLastOrder()
+	protected $paramsRegistry = null;
+
+	/**
+	 * Decoded manifest
+	 *
+	 * @var  object
+	 */
+	protected $decodedManifest = null;
+
+	/**
+	 * Generates automatic owned by field value
+	 *
+	 * @param   array   $data  the data being saved
+	 * @return  string
+	 */
+	public function automaticBlock($data)
 	{
-		return self::all()
-			->order('ordering', 'desc')
-			->row()
-			->get('ordering', 0);
+		$alias = (isset($data['block']) && $data['block'] ? $data['block'] : $data['label']);
+		$alias = strip_tags($alias);
+		$alias = trim($alias);
+		if (strlen($alias) > 100)
+		{
+			$alias = substr($alias . ' ', 0, 100);
+			$alias = substr($alias, 0, strrpos($alias, ' '));
+		}
+		$alias = str_replace(' ', '-', $alias);
+
+		return preg_replace("/[^a-zA-Z0-9\-_]/", '', strtolower($alias));
 	}
 
 	/**
-	 * Check usage
+	 * Generates automatic ordering field value
 	 *
-	 * @return  integer
+	 * @param   array   $data  the data being saved
+	 * @return  string
 	 */
-	public function checkUsage()
+	public function automaticOrdering($data)
 	{
-		require_once __DIR__ . DS . 'publication.php';
+		if (!isset($data['ordering']))
+		{
+			$last = self::all()
+				->select('ordering')
+				->order('ordering', 'desc')
+				->row();
 
-		return Publication::all()
-			->whereEquals('master_type', $this->get('id'))
-			->total();
+			$data['ordering'] = $last->ordering + 1;
+		}
+
+		return $data['ordering'];
 	}
 
 	/**
-	 * Find one record by ordering
+	 * Get params as a Registry object
 	 *
-	 * @param   integer  $ordering
 	 * @return  object
 	 */
-	public static function oneByOrdering($ordering)
+	public function transformParams()
 	{
-		return self::all()
-			->whereEquals('ordering', (int)$ordering)
-			->row();
+		if (!($this->paramsRegistry instanceof Registry))
+		{
+			$this->paramsRegistry = new Registry($this->get('params'));
+		}
+		return $this->paramsRegistry;
+	}
+
+	/**
+	 * Get manifest as a json_decoded object
+	 *
+	 * @return  object
+	 */
+	public function transformManifest()
+	{
+		if (is_null($this->decodedManifest))
+		{
+			$this->decodedManifest = json_decode($this->get('manifest'));
+		}
+		return $this->decodedManifest;
 	}
 }
