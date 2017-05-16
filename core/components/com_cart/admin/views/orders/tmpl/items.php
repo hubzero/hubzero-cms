065f065f8d54dcc8ddc199f0a0f7b3182df40b4a
Ilya Shunko <ishunko@purdue.edu>
2017-05-16 02:03:28 +0000

    [COM_CART] Add date range selector to the list of items ordered in orders. Set the default range to the last 30 days

--- core/components/com_cart/admin/views/orders/tmpl/items.php
+++ core/components/com_cart/admin/views/orders/tmpl/items.php
@@ -55,6 +55,39 @@ function submitbutton(pressbutton)
 	submitform(pressbutton);
 }
 </script>
+<script>
+	$( function() {
+		var dateFormat = "mm/dd/yy",
+			from = $( "#filter-report-from" )
+				.datepicker({
+					defaultDate: "+1w",
+					changeMonth: true,
+					numberOfMonths: 1
+				})
+				.on( "change", function() {
+					to.datepicker( "option", "minDate", getDate( this ) );
+				}),
+			to = $( "#filter-report-to" ).datepicker({
+					defaultDate: "+1w",
+					changeMonth: true,
+					numberOfMonths: 1
+				})
+				.on( "change", function() {
+					from.datepicker( "option", "maxDate", getDate( this ) );
+				});
+
+		function getDate( element ) {
+			var date;
+			try {
+				date = $.datepicker.parseDate( dateFormat, element.value );
+			} catch( error ) {
+				date = null;
+			}
+
+			return date;
+		}
+	} );
+</script>
 
 <?php
 $this->view('_submenu')
@@ -62,6 +95,18 @@ $this->view('_submenu')
 ?>
 
 <form action="index.php" method="post" name="adminForm" id="adminForm">
+	<fieldset id="filter-bar">
+		<div class="grid">
+			<div class="col span12 align-right">
+				<label for="filter-report-from">From:</label>
+				<input type="text" name="report-from" id="filter-report-from" value="<?php echo $this->escape($this->filters['report-from']); ?>" placeholder="<?php echo Lang::txt('From'); ?>" />
+				&mdash;
+				<label for="filter-report-to">To:</label>
+				<input type="text" name="report-to" id="filter-report-to" value="<?php echo $this->escape($this->filters['report-to']); ?>" placeholder="<?php echo Lang::txt('To'); ?>" />
+				<input type="submit" value="<?php echo Lang::txt('Update'); ?>" />
+			</div>
+		</div>
+	</fieldset>
 	<table class="adminlist">
 		<thead>
 			<tr>
