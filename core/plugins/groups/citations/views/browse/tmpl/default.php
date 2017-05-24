fb3f96ef6cc3258e79de1a2a25592ff3c77992b2
Shawn Rice <zooley@purdue.edu>
2017-05-24 12:28:59 +0000

    [STEMEDHUB][#531][PLG_GROUPS_CITATIONS] Make sure default limit value is in the options for pagination

--- core/plugins/groups/citations/views/browse/tmpl/default.php
+++ core/plugins/groups/citations/views/browse/tmpl/default.php
@@ -264,7 +264,7 @@ if (isset($this->messages))
 					<?php else : ?>
 						<p class="warning"><?php echo Lang::txt('PLG_GROUPS_CITATIONS_NO_CITATIONS_FOUND'); ?></p>
 					<?php endif; ?>
-					<?php echo $this->citations->pagination->setLimits(array(10,25,50,100)); ?>
+					<?php echo $this->citations->pagination; ?>
 					<div class="clearfix"></div>
 				</div><!-- /.container -->
 			</div><!-- /.subject -->
