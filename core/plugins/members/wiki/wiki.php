77fc44ec0807ca273299063b2dba1cc79954becb
dthoe19 <drew.10s@aol.com>
2017-06-01 11:01:49 -0400

    Update wiki.php

--- core/plugins/members/wiki/wiki.php
+++ core/plugins/members/wiki/wiki.php
@@ -81,7 +81,6 @@ class plgMembersWiki extends \Hubzero\Plugin\Plugin
 	 *
 	 * @param   object   $member      Current member
 	 * @param   string   $option      Component name
-	 * @param   string   $authorized  Authorization level
 	 * @param   integer  $limit       Number of record to return
 	 * @param   integer  $limitstart  Record return start
 	 * @param   string   $sort        Field to sort records on
