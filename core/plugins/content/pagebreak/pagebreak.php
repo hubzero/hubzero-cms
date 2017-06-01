9df981250ac4f5bd68984194f5553e7b231b71cd
dthoe19 <drew.10s@aol.com>
2017-06-01 11:24:46 -0400

    Update pagebreak.php

--- core/plugins/content/pagebreak/pagebreak.php
+++ core/plugins/content/pagebreak/pagebreak.php
@@ -48,6 +48,8 @@ defined('_HZEXEC_') or die;
 class plgContentPagebreak extends \Hubzero\Plugin\Plugin
 {
 	/**
+	 * Prepares page?
+	 *
 	 * @param   string   $context  The context of the content being passed to the plugin.
 	 * @param   object   $row      The article object.  Note $article->text is also available
 	 * @param   object   $params   The article params
