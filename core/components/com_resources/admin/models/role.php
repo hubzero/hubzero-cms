136342bfe6d0f4eb5c7dccb2c6da4023c3c13e0f
dthoe19 <drew.10s@aol.com>
2017-06-02 15:52:41 -0400

    Update role.php

--- core/components/com_resources/admin/models/role.php
+++ core/components/com_resources/admin/models/role.php
@@ -88,9 +88,9 @@ class ResourcesModelRole extends JModelAdmin
 	/**
 		 * Returns a reference to the a Table object, always creating it.
 		 *
-		 * @param	type	The table type to instantiate
-		 * @param	string	A prefix for the table class name. Optional.
-		 * @param	array	Configuration array for model. Optional.
+		 * @param	string	$type    The table type to instantiate
+		 * @param	string	$prefix  A prefix for the table class name. Optional.
+		 * @param	array	$config  Configuration array for model. Optional.
 		 * @return	JTable	A database object
 		 * @since	1.7
 		 */
