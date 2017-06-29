c898477d9a1222595318fb2d47d9a6020eb5a036
jrharless <jrharless@email.wm.edu>
2017-06-29 16:17:50 -0400

    [APP/COM_PARTNERS] issue 20 fixed for liason and group fields

--- admin/views/partners/tmpl/edit.php
+++ admin/views/partners/tmpl/edit.php
@@ -85,6 +85,7 @@ function submitbutton(pressbutton)
 			<div class="input-wrap">
 					<label for="fields-groups_cn"><?php echo Lang::txt('COM_PARTNERS_FIELD_GROUP_CN'); ?></label>
 					<select name="fields[groups_cn]" id="fields-groups_cn">
+						<?php if ($this->row->get("groups_cn") == '') { ?> <option value = "">select</option> <?php }?>
 						<?php foreach ($this->grouprows as $val) { ?>
 							<option<?php if ($this->row->get('groups_cn') == $val->cn) { echo ' selected="selected"'; } ?> value="<?php echo $this->escape($val->cn); ?>"><?php echo $this->escape($val->cn); ?></option>
 						<?php } ?>
@@ -94,6 +95,7 @@ function submitbutton(pressbutton)
 				<div class="input-wrap">
 					<label for="fields-QUBES_liason"><?php echo Lang::txt('COM_PARTNERS_FIELD_QUBES_LIASON'); ?></label>
 					<select name="fields[QUBES_liason]" id="fields-QUBES_liason">
+						<?php if ($this->row->get("QUBES_liason") == '') { ?> <option value = "">select</option> <?php }?>
 						<?php foreach ($this->member_names as $val) { ?>
 							<option<?php if ($this->row->get('QUBES_liason') == $val->text) { echo ' selected="selected"'; }
 							?> value="<?php echo  $this->escape($val->text); ?>">
@@ -105,6 +107,7 @@ function submitbutton(pressbutton)
 				<div class="input-wrap">
 					<label for="fields-partner_liason"><?php echo Lang::txt('COM_PARTNERS_FIELD_PARTNER_LIASON'); ?></label>
 					<select name="fields[partner_liason]" id="fields-QUBES_liason">
+					<?php if ($this->row->get("partner_liason") == '') { ?> <option value = "">select</option> <?php }?>
 						<?php foreach ($this->member_names as $val) { ?>
 							<option<?php if ($this->row->get('partner_liason') == $val->text) { echo ' selected="selected"'; }
 							?> value="<?php echo  $this->escape($val->text); ?>">
