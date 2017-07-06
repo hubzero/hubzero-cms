6a87d90bc72072a1071e4d4f1ceaed6b1886b2ab
Shawn Rice <zooley@purdue.edu>
2017-07-06 14:18:22 +0000

    [QUBES][#897][COM_PUBLICATIONS] Cleaning up view some and adding fork attribution (if publicaiton was forked)

--- core/templates/kimera/html/com_publications/view/default.php
+++ core/templates/kimera/html/com_publications/view/default.php
@@ -30,10 +30,8 @@
 defined('_HZEXEC_') or die();
 
 $this->css()
-	 ->css('jquery.fancybox.css', 'system')
-     ->js();
-
-$html = '';
+	->css('jquery.fancybox.css', 'system')
+	->js();
 
 $this->publication->authors();
 $this->publication->attachments();
@@ -60,144 +58,168 @@ else
 {
 	?>
 	<div id="content-header">
-	<section class="main section upperpane">
-		<div class="subject">
-			<div class="grid overviewcontainer">
-				<div class="col span8">
-					<?php echo str_replace(' id="content-header"', '', \Components\Publications\Helpers\Html::title($this->publication)); ?>
+		<section class="main section upperpane">
+			<div class="subject">
+				<div class="grid overviewcontainer">
+					<div class="col span8">
+						<?php echo str_replace(' id="content-header"', '', \Components\Publications\Helpers\Html::title($this->publication)); ?>
 
-					<?php if ($this->publication->params->get('show_authors') && $this->publication->_authors) { ?>
-						<div id="authorslist">
-							<?php echo \Components\Publications\Helpers\Html::showContributors($this->publication->_authors, true, false, false, false, $this->publication->params->get('format_authors', 0)); ?>
-						</div>
-					<?php }	?>
+						<?php if ($this->publication->params->get('show_authors') && $this->publication->_authors) { ?>
+							<div id="authorslist">
+								<?php echo \Components\Publications\Helpers\Html::showContributors($this->publication->_authors, true, false, false, false, $this->publication->params->get('format_authors', 0)); ?>
+							</div>
+						<?php }	?>
 
-					<p class="ataglance"><?php echo $this->publication->description ? \Hubzero\Utility\String::truncate(stripslashes($this->publication->description), 250) : ''; ?></p>
+						<p class="ataglance"><?php echo $this->publication->description ? \Hubzero\Utility\String::truncate(stripslashes($this->publication->description), 250) : ''; ?></p>
 
-					<?php echo \Components\Publications\Helpers\Html::showSubInfo($this->publication); // Show published date and category ?>
-				</div><!-- / .overviewcontainer -->
-				<div class="col span4 omega launcharea">
-					<?php
-					// Sort out primary files and draw a launch button
-					if ($this->tab != 'play')
-					{
-						// Get primary elements
-						$elements = $this->publication->_curationModel->getElements(1);
-
-						// Get attachment type model
-						$attModel = new \Components\Publications\Models\Attachments($this->database);
-
-						if ($elements)
+						<?php echo \Components\Publications\Helpers\Html::showSubInfo($this->publication); // Show published date and category ?>
+					</div><!-- / .overviewcontainer -->
+					<div class="col span4 omega launcharea">
+						<?php
+						// Sort out primary files and draw a launch button
+						if ($this->tab != 'play')
 						{
-							$element = $elements[0];
+							// Get primary elements
+							$elements = $this->publication->_curationModel->getElements(1);
 
-							// Draw button
-							$launcher = $attModel->drawLauncher(
-								$element->manifest->params->type,
-								$this->publication,
-								$element,
-								$elements,
-								$this->publication->access('view-all')
-							);
+							// Get attachment type model
+							$attModel = new \Components\Publications\Models\Attachments($this->database);
 
-							$html .= $launcher;
+							if ($elements)
+							{
+								$element = $elements[0];
+
+								// Draw button
+								$launcher = $attModel->drawLauncher(
+									$element->manifest->params->type,
+									$this->publication,
+									$element,
+									$elements,
+									$this->publication->access('view-all')
+								);
+
+								echo $launcher;
+							}
+
+							// Show additional docs
+							echo \Components\Publications\Helpers\Html::drawSupportingItems($this->publication);
+
+							// Show version information
+							echo \Components\Publications\Helpers\Html::showVersionInfo($this->publication);
+
+							// Show license information
+							if ($this->publication->license() && $this->publication->license()->name != 'standard')
+							{
+								echo \Components\Publications\Helpers\Html::showLicense($this->publication, 'play');
+							}
 						}
+						?>
+					</div><!-- / .aside launcharea -->
+				</div>
+
+				<?php
+				// Show fork attribution
+				if ($v = $this->publication->forked_from)
+				{
+					$db = App::get('db');
+					$db->setQuery("SELECT publication_id FROM `#__publication_versions` WHERE `id`=" . $db->quote($v));
+
+					$p = $db->loadResult();
+
+					$ancestor = new Components\Publications\Models\Publication($p, 'default', $v);
+
+					$from = '';
+					if ($ancestor->version->get('state') == 1 &&
+						($ancestor->version->get('published_up') == '0000-00-00 00:00:00' || ($ancestor->version->get('published_up') != '0000-00-00 00:00:00' && $ancestor->version->get('published_up') <= Date::toSql())) &&
+						($ancestor->version->get('published_down') == '0000-00-00 00:00:00' || ($ancestor->version->get('published_down') != '0000-00-00 00:00:00' && $ancestor->version->get('published_down') > Date::toSql())))
+					{
+						$from = '<a href="' . Route::url('index.php?option=com_publications&id=' . $ancestor->get('id') . '&v=' . $ancestor->version->get('version_number')) . '">' . $this->escape($ancestor->version->get('title')) . '</a>';
 					}
+					else
+					{
+						$from = $this->escape($ancestor->version->get('title')) . ' <span class="publication-status">' . Lang::txt('(unpublished)') . '</span>';
+					}
+					$from .= ' <span class="publication-version"><abbr title="' . Lang::txt('Version') . '">v</abbr> ' . $this->escape($ancestor->version->get('version_label')) . '</span>';
 
-					// Show additional docs
-					$html .=  $this->tab != 'play' ? \Components\Publications\Helpers\Html::drawSupportingItems($this->publication) : '';
+					echo '<p class="icon-fork fork-source">' . Lang::txt('Forked from: %s', $from) . '</p>';
+				}
 
-					// Show version information
-					$html .=  $this->tab != 'play' ? \Components\Publications\Helpers\Html::showVersionInfo($this->publication) : '';
-
-					// Show license information
-					$html .= $this->tab != 'play' && $this->publication->license() && $this->publication->license()->name != 'standard'
-							? \Components\Publications\Helpers\Html::showLicense($this->publication, 'play') : '';
-
-					echo $html;
-					?>
-				</div><!-- / .aside launcharea -->
-			</div>
-
-			<?php
-			// Show status for authorized users
-			if ($this->contributable)
-			{
-				echo \Components\Publications\Helpers\Html::showAccessMessage($this->publication);
-			}
-			?>
-		</div><!-- / .subject -->
-		<div class="aside rankarea">
-			<?php
-			// Show metadata
-			$this->view('_metadata')
-			     ->set('option', $this->option)
-			     ->set('publication', $this->publication)
-			     ->set('config', $this->config)
-			     ->set('version', $this->version)
-			     ->set('sections', $this->sections)
-			     ->set('cats', $this->cats)
-			     ->set('params', $this->publication->params)
-			     ->set('lastPubRelease', $this->lastPubRelease)
-			     ->display();
-			?>
-		</div><!-- / .aside -->
-	</section><!-- / .main section -->
-</div>
+				// Show status for authorized users
+				if ($this->contributable)
+				{
+					echo \Components\Publications\Helpers\Html::showAccessMessage($this->publication);
+				}
+				?>
+			</div><!-- / .subject -->
+			<div class="aside rankarea">
+				<?php
+				// Show metadata
+				$this->view('_metadata')
+				     ->set('option', $this->option)
+				     ->set('publication', $this->publication)
+				     ->set('config', $this->config)
+				     ->set('version', $this->version)
+				     ->set('sections', $this->sections)
+				     ->set('cats', $this->cats)
+				     ->set('params', $this->publication->params)
+				     ->set('lastPubRelease', $this->lastPubRelease)
+				     ->display();
+				?>
+			</div><!-- / .aside -->
+		</section><!-- / .main section -->
+	</div>
 	<?php
 }
-
-$html = '';
 
 // Part below
 if ($this->publication->access('view-all'))
 {
-	$html .= '<div class="clear sep"></div>'."\n";
+	?>
+	<div class="clear sep"></div>
 
-	$html .= '<section class="main section noborder">'."\n";
-	$html .= ' <div class="subject tabbed">'."\n";
+	<section class="main section noborder">
+		<div class="subject tabbed">
+			<?php
+			echo \Components\Publications\Helpers\Html::tabs(
+				$this->option,
+				$this->publication->id,
+				$this->cats,
+				$this->tab,
+				$this->publication->alias,
+				$this->version
+			);
 
-	$html .= \Components\Publications\Helpers\Html::tabs(
-		$this->option,
-		$this->publication->id,
-		$this->cats,
-		$this->tab,
-		$this->publication->alias,
-		$this->version
-	);
+			echo \Components\Publications\Helpers\Html::sections($this->sections, $this->cats, $this->tab, 'hide', 'main');
 
-	$html .= \Components\Publications\Helpers\Html::sections($this->sections, $this->cats, $this->tab, 'hide', 'main');
+			// Add footer notice
+			if ($this->tab == 'about')
+			{
+				echo \Components\Publications\Helpers\Html::footer($this->publication);
+			}
+			?>
+		</div><!-- / .subject -->
+		<div class="aside extracontent">
+			<?php
+			// Show related content
+			$out = Event::trigger('publications.onPublicationSub', array($this->publication, $this->option, 1));
+			if (count($out) > 0)
+			{
+				foreach ($out as $ou)
+				{
+					if (isset($ou['html']))
+					{
+						echo $ou['html'];
+					}
+				}
+			}
 
-	// Add footer notice
-	if ($this->tab == 'about')
-	{
-		$html .= \Components\Publications\Helpers\Html::footer($this->publication);
-	}
-
-	$html .= '</div><!-- / .subject -->'."\n";
-	$html .= ' <div class="aside extracontent">'."\n";
+			// Show what's popular
+			if ($this->tab == 'about')
+			{
+				echo \Hubzero\Module\Helper::renderModules('extracontent');
+			}
+			?>
+		</div><!-- / .aside extracontent -->
+	</section><!-- / .main section -->
+	<?php
 }
-
-// Show related content
-$out = Event::trigger('publications.onPublicationSub', array($this->publication, $this->option, 1));
-if (count($out) > 0)
-{
-	foreach ($out as $ou)
-	{
-		if (isset($ou['html']))
-		{
-			$html .= $ou['html'];
-		}
-	}
-}
-
-// Show what's popular
-if ($this->tab == 'about')
-{
-	$html .= \Hubzero\Module\Helper::renderModules('extracontent');
-}
-$html .= ' </div><!-- / .aside extracontent -->'."\n";
-$html .= '</section><!-- / .main section -->'."\n";
-//$html .= '<div class="clear"></div>'."\n";
-
-echo $html;
