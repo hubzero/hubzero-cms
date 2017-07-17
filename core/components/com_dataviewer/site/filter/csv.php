df133a773956f0386d31b38abf810f0a3b16172a
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-07-17 16:41:16 -0400

    [NANOHUB][#309546][COM_DATAVIEWER] Update the CSV filter to include columns' metadata
    Will include metadata in the CSV file downloaded from the dataviewer
    dataviewer/view/... page

--- core/components/com_dataviewer/site/filter/csv.php
+++ core/components/com_dataviewer/site/filter/csv.php
@@ -56,7 +56,7 @@ function filter($res, &$dd, $ob_mode = false)
 		}
 	}
 	$csv .= $nl;
-
+	$csv .= columnsMetadata($dd);
 	print $csv;
 
 	mysqli_data_seek($data, 0);
@@ -83,4 +83,32 @@ function filter($res, &$dd, $ob_mode = false)
 	}
 	return '';
 }
-?>
+
+function columnsMetadata($dd)
+{
+	$w = '"';
+	$s = ",";
+	$columnsMetadata = "";
+
+	foreach ($dd["cols"] as $column => $metadata)
+	{
+		$columnsMetadata .= columnMetadata($metadata) . $w . $s;
+	}
+
+	$columnsMetadata = rtrim($columnsMetadata, ($w . $s));
+	$dataSeparator = "\r\nDATASTART\r\n";
+
+	return $columnsMetadata . '"""'. $dataSeparator;
+}
+
+function columnMetadata($metadata)
+{
+	unset($metadata['label']);
+	unset($metadata['idx']);
+	unset($metadata['styles']);
+
+	$columnMetadata = str_replace('"', '""', json_encode($metadata));
+	$columnMetadata = rtrim(ltrim($columnMetadata, '{'), '}');
+
+	return '"' . $columnMetadata;
+}
