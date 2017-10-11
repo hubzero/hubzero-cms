5e7b99aa6a9f2b45f76b7c62ca08948a5704a02e
Shawn Rice <zooley@purdue.edu>
2017-10-11 16:04:57 +0000

    [COM_PROJECTS] Fixing incorrect syntax

--- core/components/com_projects/helpers/html.php
+++ core/components/com_projects/helpers/html.php
@@ -155,9 +155,8 @@ class Html extends Obj
 		// Determine which period we should use, based on the number of seconds lapsed.
 		// If the difference divided by the seconds is more than 1, we use that. Eg 1 year / 1 decade = 0.1, so we move on
 		// Go from decades backwards to seconds
-		for ($val = sizeof($lengths) - 1; ($val >= 0) && (($number = $difference / $lengths[$val]) <= 1))
+		for ($val = sizeof($lengths) - 1; ($val >= 0) && (($number = $difference / $lengths[$val]) <= 1); $val--)
 		{
-			$val--;
 		}
 
 		// Ensure the script has found a match
