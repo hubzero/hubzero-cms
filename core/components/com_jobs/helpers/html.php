6112eddb8ef0310b401f72487d9b0a7c5119c5a4
Shawn Rice <zooley@purdue.edu>
2017-10-12 14:53:33 +0000

    [COM_JOBS] Formatting fixes

--- core/components/com_jobs/helpers/html.php
+++ core/components/com_jobs/helpers/html.php
@@ -40,8 +40,8 @@ class Html
 	/**
 	 * Remove paragraph tags and break tags
 	 *
-	 * @param      string $pee Text to unparagraph
-	 * @return     string
+	 * @param   string  $pee  Text to unparagraph
+	 * @return  string
 	 */
 	public static function txt_unpee($pee)
 	{
@@ -57,48 +57,50 @@ class Html
 	/**
 	 * Return a confirmation screen
 	 *
-	 * @param      string $returnurl URL to return to if they press 'no'
-	 * @param      string $actionurl URL to go to if they press 'yes'
-	 * @param      string $action    Action the confirmation is for
-	 * @return     string HTML
+	 * @param   string  $returnurl  URL to return to if they press 'no'
+	 * @param   string  $actionurl  URL to go to if they press 'yes'
+	 * @param   string  $action     Action the confirmation is for
+	 * @return  string  HTML
 	 */
 	public static function confirmscreen($returnurl, $actionurl, $action = 'cancelsubscription')
-	{ ?>
+	{
+		?>
 		<div class="confirmwrap">
 			<div class="confirmscreen">
-			<p class="warning"><?php echo Lang::txt('COM_JOBS_CONFIRM_ARE_YOU_SURE') . " ";
-		if ($action == 'cancelsubscription')
-		{
-			echo strtolower(Lang::txt('COM_JOBSSUBSCRIPTION_CANCEL_THIS'));
-		}
-		else if ($action == 'withdrawapp')
-		{
-			echo Lang::txt('COM_JOBS_APPLICATION_WITHDRAW');
-		}
-		else
-		{
-			echo Lang::txt('COM_JOBS_ACTION_PERFORM_THIS');
-		}
-		$yes  = strtoupper(Lang::txt('YES'));
-		$yes .= $action == 'cancelsubscription' ? ', ' . Lang::txt('COM_JOBS_ACTION_CANCEL_IT') : '';
-		$yes .= $action == 'withdrawapp' ? ', ' . Lang::txt('COM_JOBS_ACTION_WITHDRAW')  : '';
-		$no  = strtoupper(Lang::txt('NO'));
-		$no .= $action == 'cancelsubscription' ? ', ' . Lang::txt('COM_JOBS_ACTION_DO_NOT_CANCEL')   : '';
-		$no .= $action == 'withdrawapp' ? ', ' . Lang::txt('COM_JOBS_ACTION_DO_NOT_WITHDRAW') : ''; ?>
-		</p>
-		<p><span class="yes"><a href="<?php echo $actionurl ?>"><?php echo $yes ?></a></span> <span class="no"><a href="<?php echo $returnurl ?>"><?php echo $no ?></a></span></p>
+				<p class="warning"><?php echo Lang::txt('COM_JOBS_CONFIRM_ARE_YOU_SURE') . " ";
+					if ($action == 'cancelsubscription')
+					{
+						echo strtolower(Lang::txt('COM_JOBSSUBSCRIPTION_CANCEL_THIS'));
+					}
+					else if ($action == 'withdrawapp')
+					{
+						echo Lang::txt('COM_JOBS_APPLICATION_WITHDRAW');
+					}
+					else
+					{
+						echo Lang::txt('COM_JOBS_ACTION_PERFORM_THIS');
+					}
+					$yes  = strtoupper(Lang::txt('YES'));
+					$yes .= $action == 'cancelsubscription' ? ', ' . Lang::txt('COM_JOBS_ACTION_CANCEL_IT') : '';
+					$yes .= $action == 'withdrawapp' ? ', ' . Lang::txt('COM_JOBS_ACTION_WITHDRAW')  : '';
+					$no  = strtoupper(Lang::txt('NO'));
+					$no .= $action == 'cancelsubscription' ? ', ' . Lang::txt('COM_JOBS_ACTION_DO_NOT_CANCEL')   : '';
+					$no .= $action == 'withdrawapp' ? ', ' . Lang::txt('COM_JOBS_ACTION_DO_NOT_WITHDRAW') : ''; ?>
+				</p>
+				<p><span class="yes"><a href="<?php echo $actionurl ?>"><?php echo $yes ?></a></span> <span class="no"><a href="<?php echo $returnurl ?>"><?php echo $no ?></a></span></p>
+			</div>
 		</div>
-		</div>
-	<?php }
+		<?php
+	}
 
 	/**
 	 * Generate a select form
 	 *
-	 * @param      string $name  Field name
-	 * @param      array  $array Data to populate select with
-	 * @param      mixed  $value Value to select
-	 * @param      string $class Class to add
-	 * @return     string HTML
+	 * @param   string  $name   Field name
+	 * @param   array   $array  Data to populate select with
+	 * @param   mixed   $value  Value to select
+	 * @param   string  $class  Class to add
+	 * @return  string  HTML
 	 */
 	public static function formSelect($name, $array, $value, $class='')
 	{
