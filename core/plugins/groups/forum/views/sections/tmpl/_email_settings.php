8006ee1216be5246333133c468d9e15b6a85a8a6
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-10-13 17:44:16 +0000

    [CATALYZECARE][#606][PLG_GROUPS_FORUM] Move email settings elements to partials

---
+++ core/plugins/groups/forum/views/sections/tmpl/_email_settings.php
@@ -0,0 +1,24 @@
+<?php
+
+$base = $this->base;
+
+if (Component::params('com_groups')->get('email_comment_processing') && $this->config->get('access-view-section')) : ?>
+	<div class="container relative-container">
+		<h3><?php echo Lang::txt('PLG_GROUPS_FORUM_EMAIL_SETTINGS'); ?></h3>
+		<?php
+		if (Component::params('com_groups')->get('enable_forum_email_digest', 0))
+		{
+			$emailSettingsMenu = '_email_settings_digest';
+		}
+		else
+		{
+			$emailSettingsMenu = '_email_settings_simple';
+		}
+			$this->view($emailSettingsMenu)
+				->set('base', $base)
+				->set('recvEmailOptionID', $this->recvEmailOptionID)
+				->set('recvEmailOptionValue', $this->recvEmailOptionValue)
+				->display();
+		?>
+	</div>
+<?php endif; ?>
