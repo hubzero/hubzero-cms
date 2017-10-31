5604b08df0b241e3e8ac0d113d64b330bbc033a3
Shawn Rice <zooley@purdue.edu>
2017-10-31 13:47:35 +0000

    [CORE] Adds autoloading of unprefixed components for both PSR4 and lowercase path PSR4

--- core/bootstrap/autoload.php
+++ core/bootstrap/autoload.php
@@ -31,6 +31,25 @@ if (file_exists(PATH_APP . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR 
 
 /*
 |--------------------------------------------------------------------------
+| Register The Extension Auto Loader
+|--------------------------------------------------------------------------
+|
+| We register an auto-loader "behind" the Composer loader that can load
+| classes on the fly. We'll add it to the stack here. It supports PSR-4 and
+| lowercase PSR-4 paths. Ideally, everything would be PSR-4 and the
+| Composer autoloader may be able to handle everything. But, for now, we
+| need this to support the lowercase paths currently in use.
+|
+*/
+
+Hubzero\Base\ClassLoader::addDirectories(array(
+	PATH_APP,
+	PATH_CORE
+));
+Hubzero\Base\ClassLoader::register();
+
+/*
+|--------------------------------------------------------------------------
 | Include Helper Functions
 |--------------------------------------------------------------------------
 |
