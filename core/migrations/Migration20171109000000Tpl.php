3ad0ddc05ed3f6cd5e2610de5779cb7d5bc8c832
Shawn Rice <zooley@purdue.edu>
2017-11-09 13:34:07 +0000

    [TPL_*] Removing old templates no longer supported

---
+++ core/migrations/Migration20171109000000Tpl.php
@@ -0,0 +1,33 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for removing tmeplates no longer supported
+ **/
+class Migration20171109000000Tpl extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->deleteTemplateEntry('hubbasic2012', 0);
+		$this->deleteTemplateEntry('hubbasic2013', 0);
+		$this->deleteTemplateEntry('baselayer', 0);
+		
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->addTemplateEntry('hubbasic2012', 'hubbasic2012', 0, 0, 0, null, 1);
+		$this->addTemplateEntry('hubbasic2013', 'hubbasic2013', 0, 0, 0, null, 1);
+		$this->addTemplateEntry('baselayer', 'baselayer', 0, 0, 0, null, 1);
+	}
+}
