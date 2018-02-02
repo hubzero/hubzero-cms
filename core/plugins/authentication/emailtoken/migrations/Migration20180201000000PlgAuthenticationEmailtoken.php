c843eebd1414c6b9b4a46affb430c4f4d972671f
Zach Weidner <zach.weidner@gmail.com>
2018-02-01 22:55:43 -0500

    [fix] Adding migration for authentication plugin
    Migration to add/delete the email token authentication plugin

---
+++ core/plugins/authentication/emailtoken/migrations/Migration20180201000000PlgAuthenticationEmailtoken.php
@@ -0,0 +1,28 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding Authentication - Email Token plugin
+ **/
+class Migration20180201000000PlgAuthenticationEmailtoken extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->addPluginEntry('authentication', 'emailtoken', 0);
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->deletePluginEntry('authentication', 'emailtoken');
+	}
+}
