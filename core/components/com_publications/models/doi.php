f922164a9aaba682db72b4f4cdb7fb4eb012ca32
root <root@example.com>
2018-02-08 11:06:19 -0500

    [PURR][#1619]Support parsing the reponse to the DataCite DOI minting request

--- core/components/com_publications/models/doi.php
+++ core/components/com_publications/models/doi.php
@@ -732,8 +732,6 @@ class Doi extends Obj
 		curl_setopt_array($ch, $options);
 
 		$response = curl_exec($ch);
-		var_dump($response);
-		echo "\n";
 		$success = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
 
@@ -743,6 +741,7 @@ class Doi extends Obj
 			$doiStr = reset($resArray);
 			$out = explode('/', $doiStr);
 			$handle = trim(end($out));
+			
 			if ($handle)
 			{
 				// Return DOI
