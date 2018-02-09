0c8c8541133ce73d481f9dad86fc118aca0fde8e
Shawn Rice <zooley@purdue.edu>
2018-02-09 15:06:39 +0000

    [style] Formatting fix

--- core/components/com_cron/helpers/Cron/CronExpression.php
+++ core/components/com_cron/helpers/Cron/CronExpression.php
@@ -65,14 +65,14 @@ class CronExpression
 	 * Factory method to create a new CronExpression.
 	 *
 	 * @param string $expression The CRON expression to create.  There are
-	 *	  several special predefined values which can be used to substitute the
-	 *	  CRON expression:
+	 *        several special predefined values which can be used to substitute the
+	 *        CRON expression:
 	 *
-	 *	  @yearly, @annually) - Run once a year, midnight, Jan. 1 - 0 0 1 1 *
-	 *	  @monthly - Run once a month, midnight, first of month - 0 0 1 * *
-	 *	  @weekly - Run once a week, midnight on Sun - 0 0 * * 0
-	 *	  @daily - Run once a day, midnight - 0 0 * * *
-	 *	  @hourly - Run once an hour, first minute - 0 * * * *
+	 * @yearly, @annually - Run once a year, midnight, Jan. 1 - 0 0 1 1 *
+	 * @monthly - Run once a month, midnight, first of month - 0 0 1 * *
+	 * @weekly - Run once a week, midnight on Sun - 0 0 * * 0
+	 * @daily - Run once a day, midnight - 0 0 * * *
+	 * @hourly - Run once an hour, first minute - 0 * * * *
 	 * @param FieldFactory $fieldFactory (optional) Field factory to use
 	 *
 	 * @return CronExpression
