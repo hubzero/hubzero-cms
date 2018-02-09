c9ac9ff135b4550d265885b59d10d1f11db9b190
Zach Weidner <zach.weidner@gmail.com>
2018-02-09 11:38:20 -0500

    [fix] Fixing langauge string
    [COM_INSTALLER]

--- core/components/com_installer/admin/views/packages/tmpl/add.php
+++ core/components/com_installer/admin/views/packages/tmpl/add.php
@@ -32,7 +32,7 @@
 // No direct access
 defined('_HZEXEC_') or die();
 
-Toolbar::title(Lang::txt('COM_INSTALLER_PACKAGES_PACKAGE') . ': ' . 'TEST', 'packages');
+Toolbar::title(Lang::txt('COM_INSTALLER_PACKAGES_PACKAGE') . ': ' . 'ADD NEW PACKAGE', 'packages');
 
 Toolbar::cancel();
 
