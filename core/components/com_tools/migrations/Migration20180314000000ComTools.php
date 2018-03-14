b730525c9a073b50681049f4b1e4b8f34fe53bb5
Shawn Rice <zooley@purdue.edu>
2018-03-14 19:45:05 +0000

    [refactor] Adding primary key to table Removing methods from ORM model to handle a table with no primary key as they are no longer needed.

---
+++ core/components/com_tools/migrations/Migration20180314000000ComTools.php
@@ -0,0 +1,58 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding a primary key to the DOI table
+ **/
+class Migration20180314000000ComTools extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__doi_mapping'))
+		{
+			if (!$this->db->tableHasField('#__doi_mapping', 'id'))
+			{
+				$query = "ALTER TABLE `#__doi_mapping` ADD `id` INT(11) UNSIGNED  NOT NULL  AUTO_INCREMENT  PRIMARY KEY;";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if (!$this->db->tableHasKey('#__doi_mapping', 'idx_rid'))
+			{
+				$query = "ALTER TABLE `#__doi_mapping` ADD INDEX `idx_rid` (`rid`)";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if ($this->db->tableExists('#__doi_mapping'))
+		{
+			if ($this->db->tableHasField('#__doi_mapping', 'id'))
+			{
+				$query = "ALTER TABLE `#__doi_mapping` DROP `id`;";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			if ($this->db->tableHasKey('#__doi_mapping', 'idx_rid'))
+			{
+				$query = "ALTER TABLE `#__doi_mapping` DROP KEY `idx_rid`";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+}
