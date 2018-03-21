3d5c824f6422827e45d2cbb2180b463d3702d585
drewthoennes <dthoenne@purdue.edu>
2018-03-21 17:36:31 +0000

    Cleans up excess code on admin side

--- core/components/com_media/admin/controllers/medialist.php
+++ core/components/com_media/admin/controllers/medialist.php
@@ -1,11 +1,44 @@
 <?php
+/**
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 HUBzero Foundation, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @author    Drew Thoennes <dthoenne@purdue.edu>
+ * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
 
 namespace Components\Media\Admin\Controllers;
 
 use Hubzero\Component\AdminController;
 use Components\Media\Models\Files;
+use Components\Media\Admin\Helpers\MediaHelper;
+use App;
+use Request;
 use User;
-use Html;
+use Filesystem;
 
 class Medialist extends AdminController
 {
@@ -18,8 +51,8 @@ class Medialist extends AdminController
 	{
 		$filters = array();
 
-		$tmpl = Request::getCmd('tmpl');
-		$folder = Request::getVar('folder', '', '', 'path');
+		$tmpl = \Request::getCmd('tmpl');
+		$folder = \Request::getVar('folder', '', '', 'path');
 
 		$redirect = 'index.php?option=com_media&folder=' . $folder;
 		if ($tmpl == 'component')
@@ -30,14 +63,13 @@ class Medialist extends AdminController
 
                 $session = \App::get('session');
                 $state = \User::getState('folder');
-                $directory = '/var/www/hub/app/site/media/';
-                $folders = Filesystem::directoryTree($directory);
-		$folderTree = $this->_buildFolderTree($folders);
+                $folders = \Filesystem::directoryTree(COM_MEDIA_BASE);
+		$folderTree = MediaHelper::_buildFolderTree($folders);
 
-		$children = $this->getChildren($directory, $folder);
-		$parent = $this->getParent($folder);
+		$children = MediaHelper::getChildren(COM_MEDIA_BASE, $folder);
+		$parent = MediaHelper::getParent($folder);
 
-		$style = Request::getState('media.list.layout', 'layout', 'thumbs', 'word');
+		$style = \Request::getState('media.list.layout', 'layout', 'thumbs', 'word');
 		\Hubzero\Document\Assets::addComponentStylesheet('com_media', 'medialist-'.$style.'.css');
 \Hubzero\Document\Assets::addComponentStylesheet('com_media', 'mediamanager.css');
 		$this->setView('medialist', 'thumbs');
@@ -48,49 +80,5 @@ class Medialist extends AdminController
 			->set('children', $children)
 			->set('parent', $parent)
                         ->display();
-	}
-
-	public function deleteTask()
-	{
-		ddie("Deleting?");
-	}
-
-	private function getParent($folder)
-	{
-		$parent = substr($folder, 0, strrpos($folder, '/'));
-		return $parent;
-	}
-
-	private function getChildren($directory, $folder)
-	{
-		$children = \Filesystem::listContents($directory . $folder);
-		foreach ($children as &$child)
-		{
-			$child['name'] = str_replace('/', '', substr($child['path'], 0, strlen($child['path'])));
-			$child['path'] = $folder . $child['path'];
-			if (preg_match("/\.(bmp|gif|jpg|jpe|jpeg|png)$/i", $child['name']))
-			{
-				$child['type'] = 'img';
-			}
-		}
-		return $children;;	
-	}
-
-	private function _buildFolderTree($folders, $parent_id = 0)
-	{
-		$branch = array();
-		foreach ($folders as $folder)
-		{
-			if ($folder['parent'] == $parent_id)
-			{
-				$children = $this->_buildFolderTree($folders, $folder['id']);
-				if ($children)
-				{
-					$folder['children'] = $children;
-				}
-				$branch[] = $folder;
-			}
-		}
-		return $branch;
 	}
 }
--- core/components/com_media/admin/views/media/tmpl/default.php
+++ core/components/com_media/admin/views/media/tmpl/default.php
@@ -1,9 +1,32 @@
 <?php
 /**
- * @package		Joomla.Administrator
- * @subpackage	com_media
- * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 HUBzero Foundation, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ : @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
  */
 
 // No direct access.
@@ -62,34 +85,6 @@ Toolbar::help('media');
 					<iframe src="<?php echo Route::url('index.php?option=com_media&controller=medialist&view=medialist&tmpl=component&folder=' . $this->folder); ?>" id="folderframe" name="folderframe" width="100%" marginwidth="0" marginheight="0" scrolling="auto"></iframe>
 				</div>
 			</fieldset>
-<!--
-<form action="<?php echo Route::url('index.php?option=com_media&controller=media&task=new');?>" name="folderForm" id="folderForm" method="post">
-        <legend><?php echo Lang::txt('COM_MEDIA_CREATE_FOLDER'); ?></legend>
-        <div class="path">
-                <?php if (User::authorise('core.create', 'com_media')): ?>
-                <input class="inputbox" type="text" id="folderpath" readonly="readonly" placeholder="<?php //echo Lang::txt('COM_MEDIA_FOLDER_PATH'); ?>" />
-                <input class="inputbox" type="text" id="foldername" name="foldername" placeholder="<?php echo Lang::txt('COM_MEDIA_FOLDER_NAME'); ?>" />
-                <input class="update-folder" type="hidden" name="parent" id="parent" value="<?php echo $this->parent; ?>" />
-                <button type="submit"><?php echo Lang::txt('COM_MEDIA_CREATE'); ?></button>
-                <?php endif; ?>
-        </div>
-        <?php echo Html::input('token'); ?>
-</form>
-<form action="<?php echo Route::url('index.php?option=com_media&controller=media&task=upload&' . Session::getFormToken() . '=1', true, true); ?>" id="uploadForm" name="uploadForm" method="post" enctype="multipart/form-data">
-        <legend><?php //echo $this->config->get('upload_maxsize')=='0' ? Lang::txt('COM_MEDIA_UPLOAD_FILES_NOLIMIT') : Lang::txt('COM_MEDIA_UPLOAD_FILES', $this->config->get('upload_maxsize')); ?></legend>
-        <fieldset id="uploadform">
-                <fieldset id="upload-noflash" class="actions">
-                        <label for="upload-file" class="hidelabeltxt"><?php echo Lang::txt('COM_MEDIA_UPLOAD_FILE'); ?></label>
-                        <input type="file" id="upload-file" name="Filedata[]" multiple="multiple" />
-                        <label for="upload-submit" class="hidelabeltxt"><?php echo Lang::txt('COM_MEDIA_START_UPLOAD'); ?></label>
-                        <input type="submit" id="upload-submit" value="<?php echo Lang::txt('COM_MEDIA_START_UPLOAD'); ?>"/>
-                </fieldset>
-                <input class="hidden" type="hidden" name="folder" id="folder" value="<?php echo $this->folder; ?>" />
-
-                <input type="hidden" name="return-url" value="<?php echo base64_encode('index.php?option=com_media'); ?>" />
-        </fieldset>
-</form>
--->
 		</td>
 	</tr>
 	<?php echo Html::input('token'); ?>
