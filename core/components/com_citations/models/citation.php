9d1c9c7620535d9d12d0d88e65945c26dfd11e89
Shawn Rice <zooley@purdue.edu>
2018-03-23 19:48:11 +0000

    [fix] Fixing incorrectly named var
    Fixes: https://qubeshub.org/support/ticket/1091

--- core/components/com_citations/models/citation.php
+++ core/components/com_citations/models/citation.php
@@ -1163,7 +1163,7 @@ class Citation extends Relational implements \Hubzero\Search\Searchable
 		if ($this->eprint)
 		{
 			$details .= '<span>|</span>';
-			$details .= '<a href="' . Str::ampReplace($citation->eprint) . '">' . \Lang::txt('Electronic Paper') . '</a>';
+			$details .= '<a href="' . Str::ampReplace($this->eprint) . '">' . \Lang::txt('Electronic Paper') . '</a>';
 		}
 
 		return $details;
