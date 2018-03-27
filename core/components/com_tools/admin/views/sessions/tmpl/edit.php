519dac6fd7c595f9b5d38b986536788e349a626e
Ilya Shunko <ishunko@purdue.edu>
2018-03-27 23:13:36 +0000

    [fix] Fix the path to the HTML helper

--- core/components/com_tools/admin/views/sessions/tmpl/edit.php
+++ core/components/com_tools/admin/views/sessions/tmpl/edit.php
@@ -79,7 +79,7 @@ function submitbutton(pressbutton)
 				<p><?php echo Lang::txt('COM_TOOLS_SESSION_CLASS_USERGROUPS_DESC'); ?></p>
 				<?php
 				// Include the component HTML helpers.
-				Html::addIncludePath(Component::path('com_users') '/admin/helpers/html');
+				Html::addIncludePath(Component::path('com_users') . '/admin/helpers/html');
 				?>
 				<div class="input-wrap">
 					<?php echo Html::access('usergroups', 'fields[groups]', $this->row->getGroupIds(), true); ?>
