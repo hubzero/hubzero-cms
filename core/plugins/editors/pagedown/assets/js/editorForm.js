b6eb3cec5eab7fe1493c2f5092542f2c2cb9f9d0
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2018-03-29 13:32:59 +0000

    PageDown: Implement HTML to Markdown conversion
    HTML will be converted to Markdown for the user to edit

--- core/plugins/editors/pagedown/assets/js/editorForm.js
+++ core/plugins/editors/pagedown/assets/js/editorForm.js
@@ -1,10 +1,17 @@
 
 const INPUT_ID = 'wmd-input'
+const htmlConverter = new HUB.PageDown.HtmlConverter()
 
 $(document).ready(() => {
 	const $editorTextarea = $(`#${INPUT_ID}`)
 	const $inputParentForm = $editorTextarea.closest('form')
 
+	// Convert HTML to Markdown for editing
+	const html = $editorTextarea.val()
+	const markdown = htmlConverter.toMarkdown(html)
+	$editorTextarea.val(markdown)
+
+	// Convert Markdown to HTML before form submission
 	$inputParentForm.on('submit', (e) => {
 		e.preventDefault()
 
