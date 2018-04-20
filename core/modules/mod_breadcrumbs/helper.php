321018f3238b1a2378d0ef4856f5ea09df0c14e0
Shawn Rice <zooley@purdue.edu>
2018-04-20 14:59:43 +0000

    [refactor] Removing references to Joomla code

--- core/modules/mod_breadcrumbs/helper.php
+++ core/modules/mod_breadcrumbs/helper.php
@@ -76,7 +76,7 @@ class Helper extends Module
 
 		$count = count($items);
 
-		// Don't use $items here as it references JPathway properties directly
+		// Don't use $items here as it references Pathway properties directly
 		$crumbs = array();
 		for ($i = 0; $i < $count; $i ++)
 		{
