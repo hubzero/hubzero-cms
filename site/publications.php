bcfa6d9e15c900b3865e8f3a134a7b594c44cdf4
Shawn Rice <zooley@purdue.edu>
2018-04-26 15:49:09 +0000

    [fix] Handle routing potentially bad menu items
    Fixes: https://qubeshub.org/support/ticket/1123

--- site/publications.php
+++ site/publications.php
@@ -32,20 +32,18 @@
 
 namespace Components\Publications\Site;
 
-use Request;
-
 // Include publication model
 require_once dirname(__DIR__) . DS . 'models' . DS . 'publication.php';
 require_once dirname(__DIR__) . DS . 'tables' . DS . 'logs.php';
 require_once dirname(__DIR__) . DS . 'helpers' . DS . 'usage.php';
 require_once dirname(__DIR__) . DS . 'helpers' . DS . 'resourceMapGenerator.php';
 
-$view = Request::getCmd('view', 'publications');
-$controllerName = Request::getCmd('controller', $view);
+$view = \Request::getCmd('view', 'publications');
+$controllerName = \Request::getCmd('controller', $view);
 if (!file_exists(__DIR__ . DS . 'controllers' . DS . $controllerName . '.php'))
 {
 	$controllerName = 'publications';
-	Request::setVar('task', Request::getCmd('task', $view));
+	\Request::setVar('task', \Request::getCmd('task', $view));
 }
 require_once __DIR__ . DS . 'controllers' . DS . $controllerName . '.php';
 $controllerName = __NAMESPACE__ . '\\Controllers\\' . ucfirst(strtolower($controllerName));
