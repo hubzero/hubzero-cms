1f9c779993bdaf3cf4dff2ed00e594ef20642203
Shawn Rice <zooley@purdue.edu>
2018-05-23 19:39:17 +0000

    [fix] Try to handle cases of bad JSON

--- core/components/com_publications/site/controllers/publications.php
+++ core/components/com_publications/site/controllers/publications.php
@@ -1695,6 +1695,10 @@ class Publications extends SiteController
 		// Get manifest from either version record (published) or master type
 		$manifest = $version->get('curation', $publication->type->get('curation'));
 		$curation = json_decode($manifest, true);
+		if (!$curation || json_last_error() !== JSON_ERROR_NONE)
+		{
+			$curation = json_decode($publication->type->get('curation'), true);
+		}
 		$fileParams = array(
 			'directory'    => '',
 			'dirHierarchy' => 1
@@ -1782,7 +1786,7 @@ class Publications extends SiteController
 
 			// For publications forked into an existing project, we will store all files
 			// in a sub directory to avoid any conflicts.
-			$sub = ($pid ? 'publication_' . $vid . '_' . $version->get('id') : '');
+			$sub = 'publication_' . $vid . '_' . $version->get('id');
 
 			if ($attachment->get('type') == 'file')
 			{
@@ -1908,7 +1912,7 @@ class Publications extends SiteController
 							App::abort(500, Lang::txt('Failed to copy file "' . $from . $filename . '" to "' . $toProj . $orig . '"'));
 						}
 
-						if ($pid)
+						if ($pid && $project->params->get('versionTracking'))
 						{
 							// Commit to GIT
 							$fileObj = new \Components\Projects\Models\File(
