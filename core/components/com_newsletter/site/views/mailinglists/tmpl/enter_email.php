489a98a143900b5ebd0120bed825677b947ebc37
Shawn Rice <zooley@purdue.edu>
2018-06-07 12:27:53 +0000

    [fix] Extra security and validation checks

--- core/components/com_newsletter/site/views/mailinglists/tmpl/enter_email.php
+++ core/components/com_newsletter/site/views/mailinglists/tmpl/enter_email.php
@@ -61,18 +61,20 @@ $this->css()
 		}
 	?>
 	<div class="subscribe">
-		<form action="subscribe" method="post" id="hubForm">
+		<form action="<?php echo Route::url('index.php?option=' . $this->option . '&task=subscribe'); ?>" method="post" id="hubForm">
 			<fieldset>
 				<legend><?php echo Lang::txt('COM_NEWSLETTER_MAILINGLISTS_GUEST_PROMPT'); ?></legend>
+
 				<label for="email"><?php echo Lang::txt('COM_NEWSLETTER_MAILINGLISTS_EMAIL'); ?></label>
-				<input type="email" name="e" id="email" />
+				<input type="email" name="e" id="email" placeholder="you@example.com" />
 			</fieldset>
 			<p class="submit">
 				<input type="submit" class="btn btn-success" value="<?php echo Lang::txt('COM_NEWSLETTER_MAILINGLISTS_CONTINUE_GUEST'); ?>">
-				<a class="btn" href="<?php echo $this->redirect; ?>">Login</a>
+				<a class="btn" href="<?php echo $this->redirect; ?>"><?php echo Lang::txt('COM_NEWSLETTER_LOGIN'); ?></a>
 			</p>
 			<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
 			<input type="hidden" name="controller" value="<?php echo $this->controller; ?>" />
+			<input type="hidden" name="task" value="subscribe" />
 		</form>
 	</div>
 </section>
