0eb6b7b73228d30fb4894f3b6314745e3773801d
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2018-07-02 15:42:50 +0000

    Remove submit status end point

---
+++ core/components/com_tools/migrations/Migration20180702134743ComToolsRemoveSubmitSessions.php
@@ -0,0 +1,50 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// no direct access
+defined('_HZEXEC_') or die();
+
+/*
+ * Migration for creating the _tool_submit_sessions table
+ */
+class Migration20180702134743ComToolsRemoveSubmitSessions extends Base
+{
+
+	static $submitTable = '#__tool_submit_sessions';
+	static $migrationsTable = '#__migrations';
+	static $previousMigrationFile = 'Migration20180618142613ComToolsSubmitSessions.php';
+
+	public function up()
+	{
+		// drop submit sessions table
+		$submitTable = self::$submitTable;
+
+		$dropTable = "DROP TABLE $submitTable;";
+
+		if ($this->db->tableExists($submitTable))
+		{
+			$this->db->setQuery($dropTable);
+			$this->db->query();
+		}
+
+		// remove record of previous migrations
+		$migrationsTable = self::$migrationsTable;
+		$previousMigrationFile = self::$previousMigrationFile;
+
+		$deleteMigrationRecords = "DELETE from $migrationsTable"
+			. " where file = '$previousMigrationFile';";
+
+		if ($this->db->tableExists($migrationsTable))
+		{
+			$this->db->setQuery($deleteMigrationRecords);
+			$this->db->query();
+		}
+	}
+
+	public function down()
+	{
+		// stub
+	}
+
+}
