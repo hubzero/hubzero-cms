b10b955d64123a2495826c9766d0574f426f84a8
Shawn Rice <zooley@purdue.edu>
2018-07-05 10:24:32 -0400

    [fix] Handle some lesser known Google mime types
    Refs: https://developers.google.com/drive/api/v3/mime-types
    Fixes: https://qubeshub.org/support/ticket/1202

--- core/plugins/projects/files/views/connected/tmpl/_item_link_file.php
+++ core/plugins/projects/files/views/connected/tmpl/_item_link_file.php
@@ -15,31 +15,57 @@ $itemDropdownClass = ($handlers) ? ' hasMultiple' : '';
 $itemFileNameShort = Html::shortenFileName($this->itemFileName, 60);
 $itemMimeType = $this->itemMimeType;
 
-if ($itemMimeType && strpos($itemMimeType, "application/vnd.google") === 0)
-{
-	$formatMappings = [
-		'document' => 'document',
-		'presentation' => 'presentation',
-		'spreadsheet' => 'spreadsheets'
-	];
-	$match = [];
-	preg_match('/\.(\w+\z)/', $itemMimeType, $match);
-	$itemFormat = $formatMappings[$match[1]];
-	$linkUrl = "https://docs.google.com/$itemFormat/d/$urlEncodedItemName/edit";
-	$linkTarget = 'target="blank"';
-}
-else
-{
-	$linkUrl = Route::url($this->model->link('files') . "&action=download&connection=$this->connectionId$this->subdirPath&asset=$urlEncodedItemName");
-	$linkTarget = '';
-}
+$linkUrl = Route::url($this->model->link('files') . "&action=download&connection=$this->connectionId$this->subdirPath&asset=$urlEncodedItemName");
+$linkTarget = '';
 
+if ($itemMimeType && strpos($itemMimeType, 'application/vnd.google') === 0)
+{
+	// https://developers.google.com/drive/api/v3/mime-types
+	$formatMappings = [
+		'document'     => 'document',      // Google Docs
+		'presentation' => 'presentation',  // Google Slides
+		'spreadsheet'  => 'spreadsheets',  // Google Sheets
+		//'map'          => 'map',           // Google My Maps
+		//'form'         => 'form',          // Google Forms
+		//'site'         => 'site',          // Google Sites
+		//'script'       => 'script',        // Google Apps Scripts
+		//'fusiontable'  => 'fusiontable',   // Google Fusion Tables
+	];
+	$unlinkable = [
+		'drive-sdk'    => 'drive-sdk'      // 3rd party shortcut
+	];
+
+	$match = [];
+
+	// application/vnd.google-apps.{format}
+	if (preg_match('/^application\/vnd\.google\-apps\.([^.]+)/', $itemMimeType, $match))
+	{
+		if (isset($formatMappings[$match[1]]))
+		{
+			$itemFormat = $formatMappings[$match[1]];
+			$linkUrl = "https://docs.google.com/$itemFormat/d/$urlEncodedItemName/edit";
+			$linkTarget = 'target="blank"';
+		}
+		elseif (isset($unlinkable[$match[1]]))
+		{
+			$itemFormat = '';
+			$linkUrl = '';
+			$linkTarget = '';
+
+			$itemFileNameShort .= ' (unlinkable 3rd party shortcut)';
+		}
+	}
+}
 ?>
 
 <div class="file-action-dropdown<?php echo $itemDropdownClass; ?>">
-	<a href="<?php echo $linkUrl; ?>" class="preview file:<?php echo $urlEncodedItemName; ?>" <?php echo $linkTarget; ?>>
-		<?php echo $itemFileNameShort; ?>
-	</a>
+	<?php if ($linkUrl) : ?>
+		<a href="<?php echo $linkUrl; ?>" class="preview file:<?php echo $urlEncodedItemName; ?>" <?php echo $linkTarget; ?>>
+	<?php endif; ?>
+			<?php echo $itemFileNameShort; ?>
+	<?php if ($linkUrl) : ?>
+		</a>
+	<?php endif; ?>
 	<?php if ($handlers && count($handlers) > 0) : ?>
 		<?php foreach ($handlers as $handler) : ?>
 		<a href="<?php echo Route::url($handler['url']); ?>">
