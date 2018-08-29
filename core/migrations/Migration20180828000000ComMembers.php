9e665f5922e64025f865819cf83ee573441a5698
Shawn Rice <zooley@purdue.edu>
2018-08-29 20:13:27 +0000

    [fix] Migration to remove erroneous duplicate entries

---
+++ core/migrations/Migration20180828000000ComMembers.php
@@ -0,0 +1,73 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for removing duplicate extended profile entries
+ **/
+class Migration20180828000000ComMembers extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__user_profiles'))
+		{
+			$query = "SELECT user_id, profile_key, profile_value, count(*) AS no_of_records, group_concat(id) AS duplicates
+				FROM `#__user_profiles`
+				GROUP BY user_id, profile_key, profile_value
+				HAVING count(*) > 1
+				ORDER BY user_id ASC, profile_key ASC;";
+			$this->db->setQuery($query);
+			$rows = $this->db->loadObjectList();
+
+			$delete = array();
+
+			foreach ($rows as $i => $row)
+			{
+				$dupes = explode(',', $row->duplicates);
+
+				if (empty($dupes) || count($dupes) < 2)
+				{
+					unset($rows[$i]);
+					continue;
+				}
+
+				$dupes = array_map('intval', $dupes);
+
+				// Sort lowest to highest
+				sort($dupes);
+
+				// Discard the first (original/oldest record)
+				$first = array_shift($dupes);
+
+				// Add the other entries to the list ot delete
+				foreach ($dupes as $dupe)
+				{
+					$delete[] = $dupe;
+				}
+
+				unset($rows[$i]);
+			}
+
+			if (!empty($delete))
+			{
+				$query = "DELETE FROM `#__user_profiles` WHERE `id` IN (" . implode(',', $delete) . ");";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		// Nothing here
+	}
+}
