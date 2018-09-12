c8df406ef890a3a2c122028577c8ed3b8e0412ad
Shawn Rice <zooley@purdue.edu>
2018-09-12 11:03:26 -0400

    [feat] Autoapprove users based on email This plugin allows for auto-approving new accounts based on if their email matches a specified regex. For exaple, any email that ends in `.edu` could be considered trusted and autoapproved.

---
+++ core/plugins/user/autoapprove/migrations/Migration20180831000000PlgUserAutoapprove.php
@@ -0,0 +1,28 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding User - Auto-approve plugin
+ **/
+class Migration20180831000000PlgUserAutoapprove extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->addPluginEntry('user', 'autoapprove');
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->deletePluginEntry('user', 'autoapprove');
+	}
+}
