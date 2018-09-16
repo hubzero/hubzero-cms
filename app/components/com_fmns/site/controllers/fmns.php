48ddeb35102040dd3052d55701a535d95935345b
M. Drew LaMar <mdlama@wm.edu>
2018-09-16 14:15:01 -0400

    [feat] Added browse page with Gantt chart

--- app/components/com_fmns/site/controllers/fmns.php
+++ app/components/com_fmns/site/controllers/fmns.php
@@ -85,4 +85,104 @@ class Fmns extends SiteController
 		     ->setLayout($pageName)
 		     ->display();    
   }
+  
+  /**
+   * Browse
+   *
+   * @return	void
+   */
+  public function browseTask()
+  {        
+    $this->view->script = $this->drawGanttChart();
+    
+    $this->view
+         ->setLayout('browse')
+         ->display();    
+  }
+  
+  /**
+	 * Draw a Gantt chart of FMNs
+	 *
+	 * @return  string   $script JavaScript of chart
+	 */
+  private function drawGanttChart()
+  {
+    $fmns = Fmn::whereEquals('state', 1)->
+            order('start_date', 'asc');
+
+    $jsObj = '';
+    $script = '';
+
+    // num published fmns
+    $total = $fmns->total();
+    $count = 0;
+    foreach ($fmns as $k => $v)
+    {
+      if ($v->get('state') == 1) {
+        $count++;
+        $normTime = ceil(100*min(max((time() - strtotime($v->date('start_date', $as='seconds')))/(strtotime($v->date('stop_date', $as='seconds')) - strtotime($v->date('start_date', $as='seconds'))), 0), 1));
+        $jsObj .= "[\"{$v->name()}\", 
+                    \"{$v->name()}\",
+                    \"{$v->get('id')}\",
+                    new Date(\"{$v->date()}\"), 
+                    new Date(\"{$v->date('stop_date')}\"),
+                    null,
+                    {$normTime},
+                    null,
+                    \"{$v->link('group')}\"]";
+        if ($count < $total)
+        {
+          $jsObj .= ", \n \t\t\t\t";
+        }
+      }      
+    }
+    
+    $script = "
+      google.charts.load(\"current\", {
+        packages:[\"gantt\"]
+      });
+
+      google.charts.setOnLoadCallback(draw);
+      
+      function draw(data) {
+        var data = new google.visualization.DataTable();
+        var chart = new google.visualization.Gantt(document.getElementById('browse_gantt_chart'));
+        drawChart(data, chart);
+      }
+
+      function drawChart(data, chart) {
+        data.addColumn('string', 'Task ID');
+        data.addColumn('string', 'Task Name');
+        data.addColumn('string', 'Resource');
+        data.addColumn('date', 'Start Date');
+        data.addColumn('date', 'End Date');
+        data.addColumn('number', 'Duration');
+        data.addColumn('number', 'Percent Complete');
+        data.addColumn('string', 'Dependencies');
+        data.addColumn('string', 'URL');
+        data.addRows([
+          {$jsObj}
+        ]);
+
+        var options = {
+        }
+
+        // The select handler. Call the chart's getSelection() method
+        function selectHandler() {
+          var selectedItem = chart.getSelection()[0];
+          if (selectedItem) {
+            var value = data.getValue(selectedItem.row, 8);
+            window.location.href = value;
+          }
+        }
+
+        // Listen for the 'select' event, and call my function selectHandler() when
+        // the user selects something on the chart.
+        google.visualization.events.addListener(chart, 'select', selectHandler);
+          
+        chart.draw(data, options);
+      }";
+
+    return $script;
+  }
 }
