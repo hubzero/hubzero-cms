58015ab265fc848057513458a887501e868e66d4
Zach Weidner <zach.weidner@gmail.com>
2018-09-24 16:57:31 -0400

    [refactor] Adding primary keys to some tables

---
+++ core/components/com_resources/migrations/Migration20180924164242ComResources.php
@@ -0,0 +1,45 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration to add primary key to jos_resource_stats_tools_topvals table
+ **/
+
+class Migration20180924164242ComResources extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__resource_stats_tools_topvals'))
+		{
+			if ($this->db->getPrimaryKey('#__resource_stats_tools_topvals') != 'id')
+			{
+				$query = "ALTER TABLE `#__resource_stats_tools_topvals` ADD PRIMARY KEY (id)";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if ($this->db->tableExists('#__resource_stats_tools_topvals'))
+		{
+			if ($this->db->getPrimaryKey('#__resource_stats_tools_topvals') == 'id')
+			{
+				$query = "ALTER TABLE `#__resource_stats_tools_topvals` DROP PRIMARY KEY";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+}
