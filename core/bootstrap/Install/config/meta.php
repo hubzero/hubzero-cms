1b28ffc97fa240615e807695b7aee45748965d1a
Shawn Rice <zooley@purdue.edu>
2018-10-17 13:53:46 -0400

    [feat] Adding install wizard when config isn't set

---
+++ core/bootstrap/Install/config/meta.php
@@ -0,0 +1,82 @@
+<?php
+return array(
+	/*
+	|--------------------------------------------------------------------------
+	| Meta Author
+	|--------------------------------------------------------------------------
+	|
+	| Show the author meta tag when viewing articles.
+	|    0 = No
+	|    1 = Yes
+	|
+	*/
+	'MetaAuthor' => '1',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Meta Author
+	|--------------------------------------------------------------------------
+	|
+	| Show the author meta tag when viewing articles
+	|    0 = No
+	|    1 = Yes
+	|
+	*/
+	'MetaTitle' => '0',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Meta Description
+	|--------------------------------------------------------------------------
+	|
+	| Enter a description of the overall Web site that is to be used by
+	| search engines. Generally, a maximum of 20 words is optimal.
+	|
+	*/
+	'MetaDesc' => 'HUBzero - The platform for scientific research and collaboration.',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Meta Keywords
+	|--------------------------------------------------------------------------
+	|
+	| Enter the keywords and phrases that best describe your Web site.
+	| Separate keywords and phrases with a comma.
+	|
+	*/
+	'MetaKeys' => 'hub, hubs, hubzero, research, collaboration',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Meta Version
+	|--------------------------------------------------------------------------
+	|
+	| Show the HUBzero version number in the generator meta tag.
+	|
+	*/
+	'MetaVersion' => '0',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Rights for content
+	|--------------------------------------------------------------------------
+	|
+	| Describe what rights others have to use this content.
+	|
+	*/
+	'MetaRights' => '',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Robots
+	|--------------------------------------------------------------------------
+	|
+	| Robots Instructions
+	|    "" = index, follow
+	|    "noindex, follow"
+	|    "index, nofollow"
+	|    "noindex, nofollow"
+	|
+	*/
+	'robots' => '',
+);
---
+++ core/bootstrap/Install/config/offline.php
@@ -0,0 +1,48 @@
+<?php
+return array(
+	/*
+	|--------------------------------------------------------------------------
+	| Site Offline
+	|--------------------------------------------------------------------------
+	|
+	| Select whether access to the Site frontend is available. If Yes, the
+	| frontend will display or not a message depending on the settings below.
+	|
+	*/
+	'offline' => '0',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Display Offline Message
+	|--------------------------------------------------------------------------
+	|
+	| Display or not a frontend message when the site is offline. The custom
+	| offline message uses the value defined in the 'Custom message' field.
+	| The language offline message uses the value defined in the site
+	| language file.
+	|
+	*/
+	'display_offline_message' => '1',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Offline Message
+	|--------------------------------------------------------------------------
+	|
+	| The custom offline message will be used if the 'Offline Message' field
+	| is set to 'Use custom message'.
+	|
+	*/
+	'offline_message' => 'This site is down for maintenance. Please check back again soon.',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Offline Image
+	|--------------------------------------------------------------------------
+	|
+	| An optional image to be displayed on the default offline page.
+	| Make sure the image is less than 400px wide.
+	|
+	*/
+	'offline_image' => '',
+);
