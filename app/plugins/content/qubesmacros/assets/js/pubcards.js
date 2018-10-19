c46e433e59f3bf747c5adc8705147eb220bfcee4
M. Drew LaMar <mdlama@wm.edu>
2018-10-18 22:02:49 -0400

    [feat] Updated to new pub macro

--- app/plugins/content/qubesmacros/assets/js/pubcards.js
+++ app/plugins/content/qubesmacros/assets/js/pubcards.js
@@ -27,6 +27,34 @@ function getContrastYIQ(hexcolor){
 
 $(document).ready(function(){
 
+  // Open and close card on click
+  $('.card > .btn-action').click(function() {
+    var $card = $(this).parent('.card'),
+        $icon = $(this).children('i');
+
+    $icon.addClass('icon-spinner');
+
+    if($card.hasClass('active')) {
+
+      $card.removeClass('active');
+
+      window.setTimeout(function() {
+
+        $icon.removeClass('arrow-left').removeClass('icon-spinner').addClass('menu');
+      }, 800);
+
+    } else {
+
+      $card.addClass('active');
+
+      window.setTimeout(function() {
+        $icon.removeClass('menu').removeClass('icon-spinner').addClass('arrow-left');
+      }, 800);
+    }
+
+  });
+
+  // Legacy
   $('.demo-two-card').hover(function(){
 
       var description = $(this).find('.description'),
