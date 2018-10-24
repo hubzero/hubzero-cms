0edd4ffd99eb4a8299ed6ae4b705494e293cdd58
Jenny Kwan <jakwan33@gmail.com>
2018-10-24 17:08:40 -0500

    [style] Update styles to match new styles. Reduce css bloat and lint.

---
+++ app/templates/bmc/js/pages/oer.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  // Header animations on load
+  var $introBcg = $('.oer-header'),
+      $introH2 = $('.intro h2'),
+      $introP = $('.intro p'),
+      $introLink = $('.intro a'),
+      $anchor = $('.oer'),
+      $tlLoad = new TimelineMax();
+
+   $(window).on('load', function() {
+
+     // Fade in intro
+     $tlLoad
+       .from($introBcg, .8, {autoAlpha: 0})
+       .from($introH2, 0.8, {y: 10, autoAlpha: 0, ease: Power0.easeNone}, '-=0.5')
+       .from($introP, 0.8, {y: 10, autoAlpha: 0, ease: Power0.easeNone}, '-=0.3')
+       .from($introLink, 0.8, {y:10, autoAlpha: 0, ease: Power0.easeNone}, '-=0.3')
+       .staggerFrom($anchor, .6, {y: 10, autoAlpha: 0, ease: Back.easeInOut.config(1.4)}, 0.2, '-=.5');
+   });
+
+});
