a9b5e60745c86d2272f44b1dfb4726ed5ea2bc5b
root <root@example.com>
2018-11-12 12:42:36 -0500

    [PURR][#1969]Fix the target url and doi name are not registered issue

--- core/components/com_publications/site/controllers/curation.php
+++ core/components/com_publications/site/controllers/curation.php
@@ -565,9 +565,23 @@ class Curation extends SiteController
 
 		// Get DOI service
 		$doiService = new \Components\Publications\Models\Doi($this->_pub);
+		
 		if ($this->_pub->version->doi)
 		{
 			$doiService->update($this->_pub->version->doi, true);
+			
+			if ($doiService->getError())
+			{
+				throw new Exception(Lang::txt('COM_PUBLICATIONS_CURATION_ERROR_UPDATE_METADATA'), 403);
+			}
+			
+			// Register URL and DOI name for DataCite DOI service
+			$doiService->register(false, true, $this->_pub->version->doi);
+			
+			if ($doiService->getError())
+			{
+				throw new Exception(Lang::txt('COM_PUBLICATIONS_CURATION_ERROR_REGISTER_URL'), 403);
+			}
 		}
 
 		// Mark as curated
