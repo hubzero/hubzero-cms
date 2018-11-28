bfcf3141425b1e790d48bbb54825e5a12f4084a4
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2018-11-28 18:45:10 +0000

    [refactor] Conditionally render forum reply form
    Forum reply form will only be rendered for authenticated users
    
    fixes: https://cdmhub.org/support/ticket/1783

---
+++ core/plugins/groups/forum/assets/js/user.js
@@ -0,0 +1,36 @@
+/**
+ * @package     hubzero-cms
+ * @file        plugins/groups/forum/forum.js
+ * @copyright   Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+class User {
+
+	static get BASE_API_URL() {
+		return '/users/currentuser'
+	}
+
+	constructor() {
+		this.api = new Api()
+	}
+
+	isAuthenticated() {
+		const authenticationEndpoint = this._buildApiEndpoint('isAuthenticated')
+		const isAuthenticated = this.api.get(authenticationEndpoint)
+
+		return isAuthenticated
+	}
+
+	_buildApiEndpoint(apiTask) {
+		const baseApiEndpoint = this.constructor.BASE_API_URL
+		const apiEndpoint = `${baseApiEndpoint}/${apiTask}`
+
+		return apiEndpoint
+	}
+
+}
+
+var HUB = HUB || {}
+
+HUB.User = User || {}
