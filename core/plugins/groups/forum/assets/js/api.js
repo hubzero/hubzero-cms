813170be9e231538971b69447ba3bd53cf4201f2
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2018-11-28 18:56:22 +0000

    Transpile ES6 to ES5

--- core/plugins/groups/forum/assets/js/api.js
+++ core/plugins/groups/forum/assets/js/api.js
@@ -1,44 +1,49 @@
-/**
- * @package     hubzero-cms
- * @file        plugins/groups/forum/forum.js
- * @copyright   Copyright 2005-2015 HUBzero Foundation, LLC.
- * @license     http://opensource.org/licenses/MIT MIT
- */
+'use strict';
 
-class Api {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	get(url, data = {}) {
-		const promise = this._makeApiRequest(url, data, 'GET')
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-		return promise
-	}
+var Api = function () {
+  function Api() {
+    _classCallCheck(this, Api);
+  }
 
-	post(url, data) {
-		const promise = this._makeApiRequest(url, data, 'POST')
+  _createClass(Api, [{
+    key: 'get',
+    value: function get(url) {
+      var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-		return promise
-	}
+      var promise = this._makeApiRequest(url, data, 'GET');
+      return promise;
+    }
+  }, {
+    key: 'post',
+    value: function post(url, data) {
+      var promise = this._makeApiRequest(url, data, 'POST');
+      return promise;
+    }
+  }, {
+    key: 'delete',
+    value: function _delete(url, data) {
+      var promise = this._makeApiRequest(url, data, 'DELETE');
+      return promise;
+    }
+  }, {
+    key: '_makeApiRequest',
+    value: function _makeApiRequest(url, data, method) {
+      var baseApiUrl = '/api';
+      var promise = $.ajax({
+        url: '' + baseApiUrl + url,
+        data: data,
+        method: method
+      });
+      return promise;
+    }
+  }]);
 
-	delete(url, data) {
-		const promise = this._makeApiRequest(url, data, 'DELETE')
+  return Api;
+}();
 
-		return promise
-	}
-
-	_makeApiRequest(url, data, method) {
-		const baseApiUrl = '/api'
-
-		const promise = $.ajax({
-			url: `${baseApiUrl}${url}`,
-			data,
-			method
-		})
-
-		return promise
-	}
-
-}
-
-var HUB = HUB || {}
-
-HUB.Api = Api
+var HUB = HUB || {};
+HUB.Api = Api;
