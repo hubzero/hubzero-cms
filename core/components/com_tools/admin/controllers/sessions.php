442fda9b8e28f8557abb300a62d5129e9759d8b8
Zach Weidner <zach.weidner@gmail.com>
2018-12-11 14:53:47 -0500

    [feat] Tell maxwell what hub is running

--- core/components/com_tools/admin/controllers/sessions.php
+++ core/components/com_tools/admin/controllers/sessions.php
@@ -213,7 +213,7 @@ class Sessions extends AdminController
 	}
 
 	/**
-	 * Invoke the Python script to do real work.
+	 * Invoke the Python script to do work elsewhere.
 	 *
 	 * @param   string   $comm
 	 * @param   array    &$output
@@ -223,7 +223,8 @@ class Sessions extends AdminController
 	{
 		$retval = true; // Assume success.
 		$output = new \stdClass();
-		$cmd = "/bin/sh " . dirname(dirname(__DIR__)) . "/scripts/mw $comm 2>&1 </dev/null";
+		$hubname = \App::get('config')->get('database.db');
+		$cmd = "/bin/sh " . dirname(dirname(__DIR__)) . "/scripts/mw $comm dbname=$hubname 2>&1 </dev/null";
 
 		exec($cmd, $results, $status);
 
