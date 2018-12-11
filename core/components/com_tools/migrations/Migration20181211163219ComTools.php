bc027e42a18f4d4144e5532a9d6702d7867b14f7
Shawn Rice <zooley@purdue.edu>
2018-12-11 13:39:34 -0500

    [fix] Migration to fix tool publishType param for older entries

---
+++ core/components/com_tools/migrations/Migration20181211163219ComTools.php
@@ -0,0 +1,49 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration to fix old weber publish settings
+ **/
+
+class Migration20181211163219ComTools extends Base
+{
+	/**
+	 * Up;
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__tool_version'))
+		{
+			$query = "SELECT `id`, `params` FROM `#__tool_version` WHERE `params` LIKE '%weber=true%'";
+
+			$this->db->setQuery($query);
+			$rows = $this->db->loadObjectList();
+
+			foreach ($rows as $row)
+			{
+				if (!trim($row->params))
+				{
+					continue;
+				}
+
+				$params = '{"github":"","publishType":"weber="}';
+
+				$query = "UPDATE `#__tool_version` SET `params`=" . $this->db->quote($params) . " WHERE `id`=" . $this->db->quote($row->id);
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		// None
+	}
+}
