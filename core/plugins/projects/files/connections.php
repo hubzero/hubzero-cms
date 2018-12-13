42d028597a73298569895bf8bf88c053ee2efba9
Zach Weidner <zweidner@users.noreply.github.com>
2018-12-13 09:59:12 -0500

    Update connections.php

--- core/plugins/projects/files/connections.php
+++ core/plugins/projects/files/connections.php
@@ -278,7 +278,7 @@ class connections
 		}
 		// Instantiate the connection to trigger any oauth flow required
 		Request::setVar('connection', $connection->get('id'));
-		$connection = Connection::One($connection->get('id'));
+		$connection = Connection::one($connection->get('id'));
 		$connection->adapter();
 
 		// Redirect
