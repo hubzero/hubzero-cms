4f9710b1c56ebad094f8aef83fad98bdd65131a8
M. Drew LaMar <mdlama@wm.edu>
2019-01-06 13:53:56 -0500

    [PLG_PROJECTS_PUBLICATIONS] Squashed commit of the following...
    commit f43d03680d6d85c4013d678d0390a45c3f2e0208
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Sun Jan 6 13:47:51 2019 -0500
    
        [PURR][#1981]Set the DOI state when a publication is reverted from published to draft and from draft to published.
    
        (cherry picked from commit e6567416bf13ec8035ebac0945812463008d0f0b)
    
    commit f3997006622daf82be3b36e970e3ae5cfde8ee12
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Sun Jan 6 13:46:59 2019 -0500
    
        [fix] Get the doi before checking it.
    
        (cherry picked from commit 617dda7a7b18487228de6020cd713f5a358aa62a)
    
    ...

--- app/plugins/projects/publications/publications.php
+++ app/plugins/projects/publications/publications.php
@@ -62,6 +62,14 @@ class plgProjectsPublications extends \Hubzero\Plugin\Plugin
 	protected $_msg = null;
 	
 	/**
+	 * Publication state transition
+	 *
+	 * @const
+	 */
+	const STATE_FROM_PUBLISHED_TO_DRAFTREADY = 0;
+	const STATE_FROM_DRAFTREADY_TO_PUBLISHED = 1;
+
+	/**
 	 * Event call to determine if this plugin should return data
 	 *
 	 * @param   string  $alias
@@ -2239,14 +2247,47 @@ class plgProjectsPublications extends \Hubzero\Plugin\Plugin
 		}
 
 		// When dataset is automatically approved.
-		if (!$review && ($autoApprove || $this->_pubconfig->get('autoapprove') == 1) && $doi)
+		if (!$review && ($autoApprove || $this->_pubconfig->get('autoapprove') == 1) && $pub->version->get('doi'))
 		{
 			// Update DOI metadata
-			$doiService->update($doi, true);
+			$doiService->update($pub->version->get('doi'), true);
 
 			if ($doiService->getError())
 			{
 				$this->setError(Lang::txt('PLG_PROJECTS_PUBLICATIONS_ERROR_UPDATE_DOI') . ' ' . $doiService->getError());
+				$doiErr = true;
+			}
+			else
+			{
+				// Register DOI name and target URL for DataCite DOI
+				$doiService->register(false, true, $pub->version->get('doi'));
+
+				if ($doiService->getError())
+				{
+					$this->setError(Lang::txt('PLG_PROJECTS_PUBLICATIONS_ERROR_REGISTER_NAME_URL') . ' ' . $doiService->getError());
+					$doiErr = true;
+				}
+			}
+		}
+
+		if ($this->_task == 'revert' && $pub->version->doi && $originalStatus == 1)
+		{
+			$doiService->revert($pub->version->doi, self::STATE_FROM_PUBLISHED_TO_DRAFTREADY);
+
+			if ($doiService->getError())
+			{
+				$this->setError(Lang::txt('PLG_PROJECTS_PUBLICATIONS_ERROR_UNREGISTER_DOI') . ' ' . $doiService->getError());
+				$doiErr = true;
+			}
+		}
+
+		if ($this->_task == 'publish' && $pub->version->doi && $originalStatus == 4)
+		{
+			$doiService->revert($pub->version->doi, self::STATE_FROM_DRAFTREADY_TO_PUBLISHED);
+
+			if ($doiService->getError())
+			{
+				$this->setError(Lang::txt('PLG_PROJECTS_PUBLICATIONS_ERROR_UNREGISTER_DOI') . ' ' . $doiService->getError());
 				$doiErr = true;
 			}
 			else
