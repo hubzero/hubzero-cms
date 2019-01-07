27b9714e67c1a751e6250c5cf84da457a84c1f8f
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2019-01-07 14:59:08 +0000

    [refactor] Courses missing columns migration
    Migrations checks for the existence of columns before adding or dropping

--- core/components/com_courses/migrations/Migration20190102152739ComCoursesMissingColumns.php
+++ core/components/com_courses/migrations/Migration20190102152739ComCoursesMissingColumns.php
@@ -5,6 +5,14 @@ use Hubzero\Content\Migration\Base;
 // no direct access
 defined('_HZEXEC_') or die();
 
+$componentPath = Component::path('com_courses');
+
+require_once "$componentPath/helpers/queryAddColumnStatement.php";
+require_once "$componentPath/helpers/queryDropColumnStatement.php";
+
+use Components\Courses\Helpers\QueryAddColumnStatement;
+use Components\Courses\Helpers\QueryDropColumnStatement;
+
 /**
  * Migration to add missing columns
  */
@@ -12,49 +20,109 @@ class Migration20190102152739ComCoursesMissingColumns extends Base
 {
 
 	static $assetsTable = '#__courses_assets';
+	static $assetsColumns = [
+		 ['name' => 'grade_weight', 'type' => 'varchar(255)'],
+		 ['name' => 'graded', 'type' => 'tinyint(2)'],
+		 ['name' => 'subtype', 'type' => 'varchar(255)', 'restriction' => 'NOT NULL ', 'default' => "'file'"],
+	];
 	static $offeringSectionsTable = '#__courses_offering_sections';
+	static $offeringSectionsColumns = [
+		 ['name' => 'enrollment', 'type' => 'tinyint(2)'],
+		 ['name' => 'is_default', 'type' => 'tinyint(2)', 'restriction' => 'NOT NULL ', 'default' => 0],
+	];
 	static $pagesTable = '#__courses_pages';
+	static $pagesColumns = [
+		 ['name' => 'section_id', 'type' => 'int(11)'],
+	];
 
 	public function up()
 	{
 		$assetsTable = self::$assetsTable;
-		$assetsTableQuery = "ALTER TABLE $assetsTable
-			ADD COLUMN grade_weight varchar(255),
-			ADD COLUMN graded tinyint(2),
-			ADD subtype varchar(255) NOT NULL DEFAULT 'file';";
+		$assetsTableQuery = $this->_generateSafeAddColumns($assetsTable, self::$assetsColumns);
 		$this->_queryIfTableExists($assetsTable, $assetsTableQuery);
 
 		$offeringSectionsTable = self::$offeringSectionsTable;
-		$offeringSectionsTableQuery = "ALTER TABLE $offeringSectionsTable
-			ADD COLUMN enrollment tinyint(2),
-			ADD is_default tinyint(2) NOT NULL DEFAULT 0;";
+		$offeringSectionsTableQuery = $this->_generateSafeAddColumns($offeringSectionsTable, self::$offeringSectionsColumns);
 		$this->_queryIfTableExists($offeringSectionsTable, $offeringSectionsTableQuery);
 
 		$pagesTable = self::$pagesTable;
-		$pagesTableQuery = "ALTER TABLE $pagesTable
-			ADD COLUMN section_id int(11);";
+		$pagesTableQuery = $this->_generateSafeAddColumns($pagesTable, self::$pagesColumns);
 		$this->_queryIfTableExists($pagesTable, $pagesTableQuery);
 	}
 
 	public function down()
 	{
 		$assetsTable = self::$assetsTable;
-		$assetsTableQuery  = "ALTER TABLE $assetsTable
-			DROP COLUMN grade_weight,
-			DROP COLUMN graded,
-			DROP COLUMN subtype;";
+		$assetsTableQuery = $this->_generateSafeDropColumns($assetsTable, self::$assetsColumns);
 		$this->_queryIfTableExists($assetsTable, $assetsTableQuery);
 
 		$offeringSectionsTable = self::$offeringSectionsTable;
-		$offeringSectionsTableQuery = "ALTER TABLE $offeringSectionsTable
-			DROP COLUMN enrollment,
-			DROP COLUMN is_default;";
+		$offeringSectionsTableQuery = $this->_generateSafeDropColumns($offeringSectionsTable, self::$offeringSectionsColumns);
 		$this->_queryIfTableExists($offeringSectionsTable, $offeringSectionsTableQuery);
 
 		$pagesTable = self::$pagesTable;
-		$pagesTableQuery = "ALTER TABLE $pagesTable
-			DROP COLUMN section_id;";
+		$pagesTableQuery = $this->_generateSafeDropColumns($pagesTable, self::$pagesColumns);
 		$this->_queryIfTableExists($pagesTable, $pagesTableQuery);
+	}
+
+	protected function _generateSafeAddColumns($table, $columns)
+	{
+		$query = $this->_generateSafeAlterTableColumnOperation(
+			$table, $columns, '_safeAddColumn'
+		);
+
+		return $query;
+	}
+
+	protected function _safeAddColumn($table, $columnData)
+	{
+		$columnName = $columnData['name'];
+		$addColumnStatement = '';
+
+		if (!$this->db->tableHasField($table, $columnName))
+		{
+			$addColumnStatement = (new QueryAddColumnStatement($columnData))
+				->toString();
+		}
+
+		return $addColumnStatement;
+	}
+
+	protected function _generateSafeDropColumns($table, $columns)
+	{
+		$query = $this->_generateSafeAlterTableColumnOperation(
+			$table, $columns, '_safeDropColumn'
+		);
+
+		return $query;
+	}
+
+	protected function _safeDropColumn($table, $columnData)
+	{
+		$columnName = $columnData['name'];
+		$dropColumnStatement = '';
+
+		if ($this->db->tableHasField($table, $columnName))
+		{
+			$dropColumnStatement = (new QueryDropColumnStatement($columnData))
+				->toString();
+		}
+
+		return $dropColumnStatement;
+	}
+
+	protected function _generateSafeAlterTableColumnOperation($table, $columns, $functionName)
+	{
+		$query = "ALTER TABLE $table ";
+
+		foreach ($columns as $columnData)
+		{
+			$query .= $this->$functionName($table, $columnData) . ',';
+		}
+
+		$query = rtrim($query, ',') . ';';
+
+		return $query;
 	}
 
 	protected function _queryIfTableExists($tableName, $query)
