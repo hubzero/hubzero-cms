2df962d4b42a8b3c73e686f2542ff42027d7489b
Shawn Rice <zooley@purdue.edu>
2019-01-11 10:34:58 -0500

    [fix] Adding missing migration

---
+++ core/plugins/authentication/google/migrations/Migration20170831000000PlgAuthenticationGoogle.php
@@ -0,0 +1,28 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding Authentication - Google plugin
+ **/
+class Migration20170831000000PlgAuthenticationGoogle extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->addPluginEntry('authentication', 'google', 0);
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->deletePluginEntry('authentication', 'google');
+	}
+}
