3f2dbefec46bb7e390fd3297a6c611f8a04a74f1
Shawn Rice <zooley@purdue.edu>
2019-02-13 13:22:37 -0500

    [fix] Fixing prompt numbers and missing inputs

--- core/plugins/handlers/ipynb/views/ipynb/tmpl/cell.php
+++ core/plugins/handlers/ipynb/views/ipynb/tmpl/cell.php
@@ -17,20 +17,41 @@ elseif (isset($cell->input)):
 	$source = implode('', $cell->input);
 endif;
 ?>
-<div class="cell <?php echo $cell->cell_type; ?> rendered">
-	<div class="input">
-		<?php if ($cell->cell_type == 'markdown'): ?>
+
+<?php if ($cell->cell_type == 'markdown'): ?>
+	<div class="cell <?php echo $cell->cell_type; ?> rendered">
+		<div class="input">
 			<div class="prompt input_prompt"> </div>
 			<div class="inner_cell">
 				<div class="text_cell_render rendered_html"><?php echo ($this->parser) ? $this->parser->parse($source) : $source; ?></div>
 			</div>
-		<?php endif; ?>
+		</div>
+	</div>
+<?php endif; ?>
+<?php
+if ($cell->cell_type == 'code'):
+	if (!empty($cell->input)):
+		?>
+		<div class="cell <?php echo $cell->cell_type; ?> rendered">
+			<div class="input">
+				<div class="prompt input_prompt">In [<?php echo (isset($cell->prompt_number)) ? $cell->prompt_number : ' '; ?>]:</div>
+				<div class="inner_cell">
+					<pre name="code" class="<?php echo (isset($cell->language)) ? $cell->language : 'python'; ?>:nogutter:nocontrols"><?php echo $this->escape($source); ?></pre>
+				</div>
+			</div>
+		</div>
 		<?php
-		if ($cell->cell_type == 'code'):
-			if (!empty($cell->outputs)):
-				?>
-				<div class="prompt output_prompt">Out [ ]:</div>
-				<div class="inner_cell output_png output_execute_result">
+	endif;
+	if (!empty($cell->outputs)):
+		$cls = '';
+		if (isset($output->png)):
+			$cls = 'output_png';
+		endif;
+		?>
+		<div class="cell <?php echo $cell->cell_type; ?> rendered">
+			<div class="input">
+				<div class="prompt output_prompt">Out[<?php echo (isset($cell->prompt_number)) ? $cell->prompt_number : ' '; ?>]:</div>
+				<div class="inner_cell <?php echo $cls; ?> output_execute_result">
 					<?php
 					$out = array();
 					foreach ($cell->outputs as $output):
@@ -39,7 +60,7 @@ endif;
 							<?php if (isset($output->png)): ?>
 								<div class="output_img"><img src="data:image/png;base64,<?php echo trim($output->png); ?>" alt="<?php echo $this->escape(implode('', $output->text)); ?>" /></div>
 							<?php else: ?>
-								<pre><?php echo $this->escape(implode('', $output->text)); ?></pre>
+								<pre class="output"><?php echo $this->escape(implode('', $output->text)); ?></pre>
 							<?php endif; ?>
 							<?php
 						elseif ($output->output_type == 'stream'):
@@ -49,21 +70,13 @@ endif;
 
 					if (!empty($out)):
 						?>
-						<pre><?php echo $this->escape(implode('', $out)); ?></pre>
+						<pre class="output"><?php echo $this->escape(implode('', $out)); ?></pre>
 						<?php
 					endif;
 					?>
 				</div>
-				<?php
-			else:
-				?>
-				<div class="prompt input_prompt">In [ ]:</div>
-				<div class="inner_cell">
-					<pre name="code" class="<?php echo (isset($cell->language)) ? $cell->language : 'python'; ?>:nogutter:nocontrols"><?php echo $this->escape($source); ?></pre>
-				</div>
-				<?php
-			endif;
-		endif;
-		?>
-	</div>
-</div>
+			</div>
+		</div>
+		<?php
+	endif;
+endif;
