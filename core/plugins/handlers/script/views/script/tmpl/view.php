500d0f80c887b3f523399ff783cc29fb132606de
Shawn Rice <zooley@purdue.edu>
2019-02-13 09:04:33 -0500

    [feat] Adding handlers for ipynb, scripts, md These plugins handle preview/rendering of various file types

---
+++ core/plugins/handlers/script/views/script/tmpl/view.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+$this->css('script.css');
+$this->js('highlighter/shCore.js')
+     ->js('highlighter/shBrush' . ucfirst($this->ext) . '.js')
+     ->js('script.js');
+?>
+<div class="file-preview">
+	<div class="file-preview-code script">
+		<pre name="code" class="<?php echo $this->ext; ?>:nocontrols"><?php
+			$contents = trim($this->file->read());
+			$contents = str_replace(array('<', '>'), array('&lt;', '&gt;'), $contents);
+
+			echo $contents;
+			?></pre>
+	</div>
+</div>
