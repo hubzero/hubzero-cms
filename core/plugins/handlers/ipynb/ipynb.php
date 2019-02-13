cb4bf325a24239c1e1064c7e46d968b0d9b130db
Shawn Rice <zooley@purdue.edu>
2019-02-13 09:04:33 -0500

    [feat] Adding handlers for ipynb, scripts, md These plugins handle preview/rendering of various file types

---
+++ core/plugins/handlers/ipynb/ipynb.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+use Hubzero\Plugin\Plugin;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Plugin class for file Jupyter notebooks
+ */
+class plgHandlersIpynb extends Plugin
+{
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var  boolean
+	 */
+	protected $_autoloadLanguage = true;
+
+	/**
+	 * Determines if the given collection can be handled by this plugin
+	 *
+	 * @param   \Hubzero\Filesystem\Collection  $collection  The file collection to assess
+	 * @return  boolean
+	 **/
+	public function canHandle(Hubzero\Filesystem\Collection $collection)
+	{
+		// We can handle 1 file
+		$need = [
+			'ipynb' => 1
+		];
+
+		// Check extension to make sure we can proceed
+		if (!$collection->hasExtensions($need))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Handles view events for files
+	 *
+	 * @param   \Hubzero\Filesystem\Collection  $collection  The file collection to view
+	 * @return  mixed
+	 **/
+	public function onHandleView(Hubzero\Filesystem\Collection $collection)
+	{
+		if (!$this->canHandle($collection))
+		{
+			return false;
+		}
+
+		$file = $collection->findFirstWithExtension('ipynb');
+
+		if (!$file || !($file instanceof Hubzero\Filesystem\File))
+		{
+			return false;
+		}
+
+		// Create view
+		$view = $this->view('view', 'ipynb');
+
+		$view->file = $file;
+
+		return $view;
+	}
+}
---
+++ core/plugins/handlers/ipynb/views/ipynb/tmpl/view.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+$this->css('ipynb.css');
+$this->js('highlighter/shCore.js')
+     ->js('highlighter/shBrushPython.js')
+     ->js('ipynb.js');
+?>
+<div class="file-preview">
+	<div class="file-preview-code ipynb">
+		<div class="cells">
+			<?php
+			$contents = json_decode($this->file->read());
+
+			$parser = null;
+			$mdpath = Plugin::path('handlers', 'markdown');
+
+			if ($mdpath):
+				$md = array(
+					'block/CodeTrait.php',
+					'block/FencedCodeTrait.php',
+					'block/HeadlineTrait.php',
+					'block/HtmlTrait.php',
+					'block/ListTrait.php',
+					'block/QuoteTrait.php',
+					'block/RuleTrait.php',
+					'block/TableTrait.php',
+					'inline/CodeTrait.php',
+					'inline/EmphStrongTrait.php',
+					'inline/LinkTrait.php',
+					'inline/StrikeoutTrait.php',
+					'inline/UrlLinkTrait.php',
+					'Parser.php',
+					'Markdown.php',
+					'MarkdownExtra.php',
+					'GithubMarkdown.php'
+				);
+				foreach ($md as $mdfile):
+					include_once $mdpath . '/markdown/' . $mdfile;
+				endforeach;
+
+				$parser = new cebe\Markdown\GithubMarkdown();
+			endif;
+
+			$output = array();
+
+			foreach ($contents->cells as $cell):
+				$source = implode('', $cell->source);
+
+				if ($cell->cell_type == 'markdown'):
+					$output[] = '<div class="cell ' . $cell->cell_type . ' rendered">';
+						$output[] = '<div class="input">';
+							$output[] = '<div class="prompt input_prompt"> </div>';
+							$output[] = '<div class="inner_cell">';
+								$output[] = '<div class="text_cell_render rendered_html">' . ($parser ? $parser->parse($source) : $source) . '</div>';
+							$output[] = '</div>';
+						$output[] = '</div>';
+					$output[] = '</div>';
+				endif;
+
+				if ($cell->cell_type == 'code'):
+					$output[] = '<div class="cell ' . $cell->cell_type . ' rendered">';
+						$output[] = '<div class="input">';
+							$output[] = '<div class="prompt input_prompt">In [ ]:</div>';
+							$output[] = '<div class="inner_cell">';
+								$output[] = '<pre name="code" class="python:nogutter:nocontrols">' . $source . '</pre>';
+							$output[] = '</div>';
+						$output[] = '</div>';
+					$output[] = '</div>';
+				endif;
+			endforeach;
+
+			echo implode("\n", $output);
+			?>
+		</div>
+	</div>
+</div>
