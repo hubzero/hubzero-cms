a71234b372c88289515fd2ec7db8d45d160f3460
Shawn Rice <zooley@purdue.edu>
2019-02-13 09:55:38 -0500

    [feat] Adding HTML5 video, audio handlers

---
+++ core/plugins/handlers/audio/views/audio/tmpl/view.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+$this->css('audio.css');
+
+$source = with(new \Hubzero\Content\Moderator($this->file->getAbsolutePath()))->getUrl();
+?>
+<div class="file-preview video">
+	<div class="file-preview-code">
+		<audio src="<?php echo $source; ?>" controls>
+			<p><?php echo Lang::txt('Your browser doesn\'t support HTML5 audio. Here is a <a href="%s">link to the audio</a> instead.', $source); ?></p>
+		</audio>
+	</div>
+</div>
---
+++ core/plugins/handlers/video/views/video/tmpl/view.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+$this->css('video.css');
+
+$source = with(new \Hubzero\Content\Moderator($this->file->getAbsolutePath()))->getUrl();
+?>
+<div class="file-preview video">
+	<div class="file-preview-code">
+		<video src="<?php echo $source; ?>" controls>
+			<p><?php echo Lang::txt('Your browser doesn\'t support HTML5 video. Here is a <a href="%s">link to the video</a> instead.', $source); ?></p>
+		</video>
+	</div>
+</div>
