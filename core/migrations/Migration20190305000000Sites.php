140c32df372f6064bc47b773701f9feef94101a0
Shawn Rice <zooley@purdue.edu>
2019-03-06 12:39:42 -0500

    [fix] Removing refs to deprecated tables Also moving some default install content to migrations in related extensions.

---
+++ core/migrations/Migration20190305000000Sites.php
@@ -0,0 +1,54 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for removing unused sites table
+ **/
+class Migration20190305000000Sites extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__sites'))
+		{
+			$query = "DROP TABLE IF EXISTS `#__sites`;";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if (!$this->db->tableExists('#__sites'))
+		{
+			$query = "CREATE TABLE `#__sites` (
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `title` varchar(100) DEFAULT NULL,
+			  `category` varchar(100) DEFAULT NULL,
+			  `url` varchar(255) DEFAULT NULL,
+			  `image` varchar(255) DEFAULT NULL,
+			  `teaser` varchar(255) DEFAULT NULL,
+			  `description` text,
+			  `notes` text,
+			  `checked_out` int(11) NOT NULL DEFAULT '0',
+			  `checked_out_time` DEFAULT NULL,
+			  `published` tinyint(1) NOT NULL DEFAULT '0',
+			  `published_date`DEFAULT NULL,
+			  `state` varchar(30) DEFAULT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+}
