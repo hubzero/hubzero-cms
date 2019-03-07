b441a813c858301a0c759d40404756c85eca5c31
Shawn Rice <zooley@purdue.edu>
2019-03-07 21:19:51 +0000

    [style] Applying style fixes already on 2.2 branch

--- core/plugins/members/profile/views/index/tmpl/default.php
+++ core/plugins/members/profile/views/index/tmpl/default.php
@@ -131,11 +131,11 @@ foreach ($profiles as $profile)
 }
 
 /**
-* Renders if argument is json?
-*
-* @param  string  $v
-* @return string
-*/
+ * Renders if argument is json?
+ *
+ * @param  string  $v
+ * @return string
+ */
 function renderIfJson($v)
 {
 	if (strstr($v, '{'))
@@ -193,7 +193,7 @@ $legacy = array(
 			</ul>
 		</div>
 	<?php elseif (count($update_missing) > 0) : ?>
-		<?php if (count($update_missing) == 1 && in_array("usageAgreement",array_keys($update_missing))) : ?>
+		<?php if (count($update_missing) == 1 && in_array("usageAgreement", array_keys($update_missing))) : ?>
 		<?php else: ?>
 			<div class="error member-update-missing">
 				<strong><?php echo Lang::txt('PLG_MEMBERS_PROFILE_UPDATE_BEFORE_CONTINUING'); ?></strong>
@@ -797,7 +797,7 @@ $legacy = array(
 				<li class="<?php echo implode(' ', $cls); ?> section" id="input-section-<?php echo $this->escape($field->get('name')); ?>">
 					<div class="section-content">
 						<div class="key"><?php echo $field->get('label'); ?></div>
-						<div class="value"><?php echo (!empty($val) ? (is_array($val) ? implode(', ', $val) : $val) : Lang::txt('PLG_MEMBERS_PROFILE_NOT_SET')); ?></div>
+						<div class="value"><?php echo (!empty($val)) ? (is_array($val) ? implode(', ', $val) : $val) : Lang::txt('PLG_MEMBERS_PROFILE_NOT_SET'); ?></div>
 						<br class="clear" />
 						<?php
 						if ($isUser)
@@ -832,7 +832,7 @@ $legacy = array(
 								}
 								else
 								{
-									$access = '<label>' . Lang::txt('PLG_MEMBERS_PROFILE_PRIVACY')  . '</label>' . Components\Members\Helpers\Html::selectAccess('access[' . $field->get('name') . ']', $profile->get('access', $field->get('access')),'input-select');
+									$access = '<label>' . Lang::txt('PLG_MEMBERS_PROFILE_PRIVACY')  . '</label>' . Components\Members\Helpers\Html::selectAccess('access[' . $field->get('name') . ']', $profile->get('access', $field->get('access')), 'input-select');
 								}
 
 								$this->view('default', 'edit')
@@ -882,13 +882,13 @@ $legacy = array(
 				{
 					$cls .= ($isUser) ? " hidden" : " hide";
 				}
-				if (isset($update_missing) && in_array("optin",array_keys($update_missing)))
+				if (isset($update_missing) && in_array("optin", array_keys($update_missing)))
 				{
 					$cls = str_replace(' hide', '', $cls);
 					$cls .= ' missing';
 				}
 				// dont show meant for stats only
-				$cls .= (!$isUser) ? ' hide' : '' ;
+				$cls .= (!$isUser) ? ' hide' : '';
 
 				// get value of mail preference option
 				switch ($this->profile->get('sendEmail'))
