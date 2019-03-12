8cf165e75704990665fb5d9e218d2aa08c1be540
Shawn Rice <zooley@purdue.edu>
2019-03-12 07:37:15 -0400

    [feat] Ability to add course image from front-end

--- core/components/com_courses/site/views/media/tmpl/filebrowser.php
+++ core/components/com_courses/site/views/media/tmpl/filebrowser.php
@@ -33,21 +33,21 @@
 // No direct access
 defined('_HZEXEC_') or die();
 
+Html::behavior('framework', true);
+
 $this->css('media.css')
      ->js('jquery.fileuploader.js', 'system')
-     ->js('courses.fileupload.js');
-
-$base = rtrim(Request::base(true), '/');
+     ->js('media.js');
 ?>
 <div id="file_browser">
 	<form action="<?php echo Route::url('index.php?option=' . $this->option); ?>" id="adminForm" method="post" enctype="multipart/form-data">
 		<fieldset>
 			<div id="themanager" class="manager">
-				<iframe src="<?php echo $base; ?>/index.php?option=<?php echo $this->option; ?>&amp;tmpl=component&amp;task=listfiles&amp;listdir=<?php echo $this->listdir; ?>" name="imgManager" id="imgManager" width="99%" height="180"></iframe>
+				<iframe src="<?php echo Route::url('index.php?option=' . $this->option . '&controller=' . $this->controller . 'tmpl=component&task=listfiles&listdir=' . $this->listdir); ?>" name="imgManager" id="imgManager" width="99%" height="180"></iframe>
 			</div>
 		</fieldset>
 		<fieldset>
-			<div id="ajax-uploader" data-action="<?php echo $base; ?>/index.php?option=com_courses&amp;task=ajaxupload&amp;listdir=<?php echo $this->listdir; ?>&amp;no_html=1">
+			<div id="ajax-uploader" data-action="<?php echo Route::url('index.php?option=' . $this->option . '&controller=' . $this->controller . '&task=ajaxupload&listdir=' . $this->listdir . '&no_html=1'); ?>">
 				<noscript>
 					<p><input type="file" name="upload" id="upload" /></p>
 					<p><input type="submit" value="<?php echo Lang::txt('COM_COURSES_UPLOAD'); ?>" /></p>
@@ -55,9 +55,11 @@ $base = rtrim(Request::base(true), '/');
 			</div>
 
 			<input type="hidden" name="option" value="<?php echo $this->option; ?>" />
+			<input type="hidden" name="controller" value="<?php echo $this->controller; ?>" />
 			<input type="hidden" name="listdir" id="listdir" value="<?php echo $this->listdir; ?>" />
 			<input type="hidden" name="task" value="upload" />
 			<input type="hidden" name="no_html" value="1" />
+			<?php echo Html::input('token'); ?>
 		</fieldset>
 	</form>
 </div>
