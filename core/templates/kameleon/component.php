664f96f0fc8f2c6cc22c5faedce979ebaf089367
Shawn Rice <zooley@purdue.edu>
2019-03-13 13:38:38 -0400

    [refactor] Simplifying themes, toolbar more visible This makes the toolbar buttons a little mroe visible and simplifies the various color themes, reducing the need for several files.

--- core/templates/kameleon/component.php
+++ core/templates/kameleon/component.php
@@ -35,32 +35,31 @@ defined('_HZEXEC_') or die();
 
 // Load base styles
 $this->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/component.css?v=' . filemtime(__DIR__ . '/css/component.css'));
+
 // Load theme
-if ($theme = $this->params->get('theme'))
+$theme = $this->params->get('theme');
+if ($theme == 'custom')
 {
-	if ($theme == 'custom')
-	{
-		$color = $this->params->get('color');
-		$this->addStyleDeclaration(include_once __DIR__ . '/css/themes/custom.php');
-	}
-	else if ($theme != 'gray')
-	{
-		$this->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/themes/' . $theme . '.css?v=' . filemtime(__DIR__ . '/css/themes/' . $theme . '.css'));
-	}
-}
-// Load language direction CSS
-if ($this->direction == 'rtl')
-{
-	$this->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/common/rtl.css?v=' . filemtime(__DIR__ . '/css/common/rtl.css'));
+	$color = $this->params->get('color');
+	$this->addStyleDeclaration(include_once __DIR__ . '/css/themes/custom.php');
 }
 
 $this->addScript($this->baseurl . '/templates/' . $this->template . '/js/component.js?v=' . filemtime(__DIR__ . '/js/component.js'));
 
 $browser = new \Hubzero\Browser\Detector();
+
+$cls = array(
+	'nojs',
+	$this->direction,
+	$theme,
+	$browser->name(),
+	$browser->name() . $browser->major()
+);
 ?>
 <!DOCTYPE html>
-<html dir="<?php echo $this->direction; ?>" lang="<?php echo $this->language; ?>" class="<?php echo $browser->name() . ' ' . $browser->name() . $browser->major(); ?>">
+<html dir="<?php echo $this->direction; ?>" lang="<?php echo $this->language; ?>" class="<?php echo implode(' ', $cls); ?>">
 	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<jdoc:include type="head" />
 	</head>
 	<body id="component-body" class="contentpane">
