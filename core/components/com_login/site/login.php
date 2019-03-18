ff21a2ea397eb207c83555c9e3d56ef0fccb294c
Shawn Rice <zooley@purdue.edu>
2019-03-18 16:30:03 -0400

    [refactor] Initi copy of login code from com_users This still requires all other extensions and menu items point to it.

---
+++ core/components/com_login/site/login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Components\Login\Site;
+
+require_once __DIR__ . '/controllers/auth.php';
+
+$controller = new Controllers\Auth();
+$controller->execute();
