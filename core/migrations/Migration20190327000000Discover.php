3df28292946c5b35d66cf7d956faa41bb2825463
Shawn Rice <zooley@purdue.edu>
2019-03-27 08:28:43 -0400

    [fix] Updating links from /store to /storefront The store component (com_store) was removed from the core a while ago. Links should point to com_storefront instead.
    Fixes: https://help.hubzero.org/support/ticket/11786

---
+++ core/migrations/Migration20190327000000Discover.php
@@ -0,0 +1,78 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for incorrect link in default discover page content
+ **/
+class Migration20190327000000Discover extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__content'))
+		{
+			$query = "SELECT * FROM `#__content` WHERE `title`='Discover' AND `alias`='discover' AND `introtext` LIKE '%href=\"/store\"%'";
+			$this->db->setQuery($query);
+			$pages = $this->db->loadObjectList();
+
+			if ($pages)
+			{
+				foreach ($pages as $page)
+				{
+					$content = $page->introtext;
+					$content = str_replace('href="/store"', 'href="/storefront"', $content);
+
+					$query = "UPDATE `#__content` SET `introtext`=" . $this->db->quote($content) . " WHERE `id`=" . $this->db->quote($page->id);
+					$this->db->setQuery($query);
+					if ($this->db->query())
+					{
+						$this->log('Updated Store link from /store to /storefront in default Discover page');
+					}
+					else
+					{
+						$this->log($query, 'warning');
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if ($this->db->tableExists('#__content'))
+		{
+			$query = "SELECT * FROM `#__content` WHERE `title`='Discover' AND `alias`='discover' AND `introtext` LIKE '%href=\"/storefront\"%'";
+			$this->db->setQuery($query);
+			$pages = $this->db->loadObjectList();
+
+			if ($pages)
+			{
+				foreach ($pages as $page)
+				{
+					$content = $page->introtext;
+					$content = str_replace('href="/storefront"', 'href="/store"', $content);
+
+					$query = "UPDATE `#__content` SET `introtext`=" . $this->db->quote($content) . " WHERE `id`=" . $this->db->quote($page->id);
+					$this->db->setQuery($query);
+					if ($this->db->query())
+					{
+						$this->log('Updated Store link from /storefront to /store in default Discover page');
+					}
+					else
+					{
+						$this->log($query, 'warning');
+					}
+				}
+			}
+		}
+	}
+}
