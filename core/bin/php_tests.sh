d1342968e39933dc748c4d51d7efaec0deb7a9e1
Zach Weidner <zach.weidner@gmail.com>
2019-04-01 14:29:24 -0400

    [feat] Update travis build
    Adds PHP syntax checking to the linter and more robust rejection of
    non-PHP files to the code sniffer

---
+++ core/bin/php_tests.sh
@@ -0,0 +1,15 @@
+update_status () { if [ $1 -ne 0 ]; then status=$1; fi }
+status=0
+php_args=`echo $@ | grep \.php$`
+if [ -z "$php_args" ]; then exit 0; fi
+
+vendor/squizlabs/php_codesniffer/scripts/phpcs -np --standard=~/build/standards/Php/ruleset.xml --ignore=*/tests/*,/core/libraries/* $php_args
+update_status $?
+for file in $php_args; do
+	php -l $file 2>&1 1>temp
+	update_status $?
+done
+sed "/No syntax errors detected/d" -i temp
+cat temp
+rm temp
+exit $status
