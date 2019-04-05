7834f49243227ca0092d19a2ba3d506c0cfca663
M. Drew LaMar <mdlama@wm.edu>
2019-04-05 15:37:59 -0400

    [refactor] Added plg_groups_projects as subtree

---
+++ app/plugins/groups/projects/assets/js/projects.js
@@ -0,0 +1,15 @@
+/**
+ * @package     hubzero-cms
+ * @file        plugins/groups/projects/projects.js
+ * @copyright   Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+if (!jq) {
+	var jq = $;
+}
+
+jQuery(document).ready(function(jq){
+	var $ = jq;
+
+});
