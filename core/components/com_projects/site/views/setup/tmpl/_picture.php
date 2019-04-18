a5caa9a5f387e444973cfc5c45c80436829c5e82
Shawn Rice <zooley@purdue.edu>
2019-04-18 07:17:13 -0400

    [fix] Accessibility, code reduction, bug fixes  * Adding missing `for` attribute on labels  * Javascript fixes for jQuery upgrade (use `prop()` instead of `attr()`    for testing if a checkbox or radio is selected)  * Some reformatting of views to for easier reading  * Reducing duplicated code  * Adding Bootstrap classes to form elements

--- core/components/com_projects/site/views/setup/tmpl/_picture.php
+++ core/components/com_projects/site/views/setup/tmpl/_picture.php
@@ -42,15 +42,17 @@ if (!$this->model->exists())
 			<img id="project-image-content" src="<?php echo Route::url('index.php?option=' . $this->option . '&controller=media&alias=' . $this->model->get('alias') . '&media=master'); ?>" alt="" />
 		</div>
 		<?php if ($this->model->get('picture')) { ?>
-		<p class="actionlink"><a href="<?php echo Route::url('index.php?option=' . $this->option . '&task=deleteimg&alias=' . $this->model->get('alias') ); ?>" id="deleteimg">[ <?php echo Lang::txt('COM_PROJECTS_DELETE'); ?> ]</a></p>
+		<p class="actionlink"><a href="<?php echo Route::url('index.php?option=' . $this->option . '&task=deleteimg&alias=' . $this->model->get('alias') ); ?>" id="deleteimg">[ <?php echo Lang::txt('JACTION_DELETE'); ?> ]</a></p>
 		<?php } ?>
 	</div>
 	<div class="col span9 omega" id="ajax-upload" data-action="<?php echo Route::url('index.php?option=' . $this->option . '&alias=' . $this->model->get('alias') . '&task=doajaxupload&no_html=1'); ?>">
-		<h5><?php echo Lang::txt('COM_PROJECTS_UPLOAD_NEW_IMAGE'); ?> <span class="hint"><?php echo Lang::txt('COM_PROJECTS_WILL_REPLACE_EXISTING_IMAGE'); ?></span></h5>
-		<p id="status-box"></p>
-		<label>
-			<input name="upload" type="file" class="option uploader" id="uploader" />
-		</label>
+		<div class="form-group">
+			<label for="uploader">
+				<?php echo Lang::txt('COM_PROJECTS_UPLOAD_NEW_IMAGE'); ?> <span class="hint"><?php echo Lang::txt('COM_PROJECTS_WILL_REPLACE_EXISTING_IMAGE'); ?></span>
+				<span id="status-box"></span>
+				<input name="upload" type="file" class="option uploader form-control-file" id="uploader" />
+			</label>
+		</div>
 		<input type="button" value="<?php echo Lang::txt('COM_PROJECTS_UPLOAD'); ?>" class="btn" id="upload-file" />
 	</div>
 </div>
