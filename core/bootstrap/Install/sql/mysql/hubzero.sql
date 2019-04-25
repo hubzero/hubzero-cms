f21b63a6f17a26db1c15295fc7824abc935ed4a9
Shawn Rice <zooley@purdue.edu>
2019-04-25 11:24:47 -0400

    [maint] Updating copyright for bootstrap files

--- core/bootstrap/Install/sql/mysql/hubzero.sql
+++ core/bootstrap/Install/sql/mysql/hubzero.sql
@@ -1,26 +1,7 @@
 #
 # @package      hubzero-cms
-# @file         installation/sql/mysql/hubzero.sql
-# @copyright    Copyright (c) 2010-2015 HUBzero Foundation, LLC.
-# @license      http://www.gnu.org/licenses/gpl2.html GPLv2
-#
-# Copyright (c) 2010-2015 HUBzero Foundation, LLC.
-# All rights reserved.
-#
-# This file is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 2 of the License, or (at your
-# option) any later version.
-#
-# This file is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# HUBzero is a registered trademark of Purdue University.
+# @copyright    Copyright 2005-2019 HUBzero Foundation, LLC.
+# @license      http://opensource.org/licenses/MIT MIT
 #
 
 #
