742a3dbfc4ad956f999742768da7f3bc067abef3
Shawn Rice <zooley@purdue.edu>
2019-04-25 10:31:47 -0400

    [maint] Updating copyright in template assets

--- core/templates/kimera/js/hub.js
+++ core/templates/kimera/js/hub.js
@@ -1,8 +1,7 @@
 /**
- * @package     hubzero-cms
- * @file        templates/kimera/js/hub.js
- * @copyright   Copyright 2005-2015 HUBzero Foundation, LLC.
- * @license     http://opensource.org/licenses/MIT MIT
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
  */
 
 if (typeof console === "undefined" || typeof console.log === "undefined") {
