55308897cb848ab3c4df461de4f486b78dd86ca4
Shawn Rice <zooley@purdue.edu>
2019-04-25 11:24:47 -0400

    [maint] Updating copyright for bootstrap files

--- core/bootstrap/Install/sql/mysql/hz_sample_data.sql
+++ core/bootstrap/Install/sql/mysql/hz_sample_data.sql
@@ -1,30 +1,7 @@
 #
 # @package      hubzero-cms
-# @file         installation/sql/mysql/hz_sample_data.sql
-# @author       Nicholas J. Kisseberth <nkissebe@purdue.edu>
-# @copyright    Copyright (c) 2010-2015 HUBzero Foundation, LLC.
+# @copyright    Copyright 2005-2019 HUBzero Foundation, LLC.
 # @license      http://opensource.org/licenses/MIT MIT
-#
-# Copyright (c) 2010-2015 HUBzero Foundation, LLC.
-# All rights reserved.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 #
 # IMPORTANT - THIS FILE MUST BE SAVED WITH UTF-8 ENCODING ONLY. BEWARE IF EDITING!
 #
--- core/bootstrap/start.php
+++ core/bootstrap/start.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * HUBzero CMS
- *
  * @package    hubzero-cms
  * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
  * @license    http://opensource.org/licenses/MIT MIT
