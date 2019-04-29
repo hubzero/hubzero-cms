a89e877932dd375150cfc930d13b94b505cf2b64
Shawn Rice <zooley@purdue.edu>
2019-04-29 10:35:49 -0400

    [maint] Updating copyright

--- core/plugins/members/activity/assets/js/activity.js
+++ core/plugins/members/activity/assets/js/activity.js
@@ -1,8 +1,7 @@
 /**
- * @package     hubzero-cms
- * @file        plugins/members/activity/assets/js/activity.js
- * @copyright   Copyright 2005-2015 HUBzero Foundation, LLC.
- * @license     http://opensource.org/licenses/MIT MIT
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
  */
 
 if (!jq) {
