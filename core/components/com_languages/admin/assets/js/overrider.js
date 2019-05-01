8d9761eaaa78fd949b2fa79fc706d9d5e66b932f
Shawn Rice <zooley@purdue.edu>
2019-05-01 14:26:35 -0400

    [maint] Updating copyright

--- core/components/com_languages/admin/assets/js/overrider.js
+++ core/components/com_languages/admin/assets/js/overrider.js
@@ -1,4 +1,10 @@
 /**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+/**
  * Some state variables for the overrider
  */
 Hubzero.overrider = {
