cef1f47eb2de9615d99a443319e7365a2ce8a280
Shawn Rice <zooley@purdue.edu>
2019-05-01 11:19:26 -0400

    [maint] Updating copyright

--- core/components/com_groups/migrations/Migration20180524092913ComGroups.php
+++ core/components/com_groups/migrations/Migration20180524092913ComGroups.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
 use Hubzero\Content\Migration\Base;
 use Components\Groups\Models\Orm\Field;
 use Components\Groups\Models\Orm\Answer;
