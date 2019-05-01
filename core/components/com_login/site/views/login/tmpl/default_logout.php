5ad32b2cf8898a349339b38371358fb1cac6230c
Shawn Rice <zooley@purdue.edu>
2019-05-01 14:26:35 -0400

    [maint] Updating copyright

--- core/components/com_login/site/views/login/tmpl/default_logout.php
+++ core/components/com_login/site/views/login/tmpl/default_logout.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package   hubzero-cms
- * @copyright Copyright 2005-2019 HUBzero Foundation, LLC.
- * @license   http://opensource.org/licenses/MIT MIT
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
  */
 
 defined('_HZEXEC_') or die();
@@ -22,8 +22,6 @@ if (!User::isGuest())
 	App::redirect(Route::url('index.php?option=com_members&task=myaccount', false));
 	return;
 }
-
-
 ?>
 <div class="logout<?php echo $this->pageclass_sfx?>">
 	<?php if ($this->params->get('show_page_heading')) : ?>
