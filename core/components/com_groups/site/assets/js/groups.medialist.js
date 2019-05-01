49c131980442bc454a88c4a2527210edb7b87544
Shawn Rice <zooley@purdue.edu>
2019-05-01 12:04:14 -0400

    [style] Formatting fixes

--- core/components/com_groups/site/assets/js/groups.medialist.js
+++ core/components/com_groups/site/assets/js/groups.medialist.js
@@ -313,9 +313,9 @@ HUB.GroupsMediaList = {
 	
 	_getUrlParam: function( paramName )
 	{
-		var reParam = new RegExp( '(?:[\?&]|&)' + paramName + '=([^&]+)', 'i' ) ;
-		var match = window.location.search.match(reParam) ;
-		return ( match && match.length > 1 ) ? match[ 1 ] : null ;
+		var reParam = new RegExp( '(?:[\?&]|&)' + paramName + '=([^&]+)', 'i' );
+		var match = window.location.search.match(reParam);
+		return ( match && match.length > 1 ) ? match[ 1 ] : null;
 	},
 };
 
