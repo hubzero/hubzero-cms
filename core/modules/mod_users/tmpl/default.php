c4c6f9d64261dc1be14aa42b366d36ca7205950a
Shawn Rice <zooley@purdue.edu>
2019-05-03 06:53:58 -0400

    [fix] Filter out blocked & unconfirmed This module shows the number of accounts needing approval. No need to add blocked or unconfirmed (email) accounts to the list.

--- core/modules/mod_users/tmpl/default.php
+++ core/modules/mod_users/tmpl/default.php
@@ -13,7 +13,7 @@ $this->css();
 <div class="<?php echo $this->module->module; ?>">
 	<?php if (count($this->unapproved) > 0) : ?>
 		<div class="pending-users">
-			<a href="<?php echo Route::url('index.php?option=com_users&view=users&filter.approved=0'); ?>">
+			<a href="<?php echo Route::url('index.php?option=com_members&approved=0'); ?>">
 				<span class="count"><?php echo count($this->unapproved); ?></span>
 				<?php echo Lang::txts('MOD_USERS_REQUIRE_APPROVAL', count($this->unapproved)); ?>
 			</a>
