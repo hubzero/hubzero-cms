40476b938b3da21e816243d5a5f2e903dd43c88f
BharathR8 <bharathchandra.bcr@gmail.com>
2019-05-09 02:27:45 -0400

    [refactor] Switched positioning to display the messages at the top

--- app/modules/mod_mymessagesmini/tmpl/default.php
+++ app/modules/mod_mymessagesmini/tmpl/default.php
@@ -26,11 +26,6 @@ else
 	?>
 
 <div<?php echo ($this->moduleclass) ? ' class="' . $this->moduleclass . '"' : ''; ?>>
-		<ul class="module-nav">
-			<li><a class="icon-email-alt" href="<?php echo Route::url('index.php?option=com_members&id=' . User::get('id') . '&active=messages'); ?>"><?php echo Lang::txt('MOD_MYMESSAGESMINI_ALL_MESSAGES'); ?></a></li>
-			<li><a class="icon-plus" href="<?php echo Route::url('index.php?option=com_members&id=' . User::get('id') . '&active=messages&task=settings'); ?>"><?php echo Lang::txt('MOD_MYMESSAGESMINI_MESSAGE_SETTINGS'); ?></a></li>
-		</ul>
-
 		<?php if (count($this->rows) <= 0) { ?>
 			<p><em><?php echo Lang::txt('MOD_MYMESSAGESMINI_NO_MESSAGES'); ?></em></p>
 		<?php } else { ?>
@@ -68,6 +63,12 @@ else
 		<?php if ($this->total > $this->limit) { ?>
 			<p class="note"><?php echo Lang::txt('MOD_MYMESSAGESMINI_YOU_HAVE_MORE', $this->limit, $this->total, Route::url('index.php?option=com_members&id=' . User::get('id') . '&active=messages')); ?></p>
 		<?php } ?>
+
+		<ul class="module-nav">
+			<li><a class="icon-email-alt" href="<?php echo Route::url('index.php?option=com_members&id=' . User::get('id') . '&active=messages'); ?>"><?php echo Lang::txt('MOD_MYMESSAGESMINI_ALL_MESSAGES'); ?></a></li>
+			<li><a class="icon-plus" href="<?php echo Route::url('index.php?option=com_members&id=' . User::get('id') . '&active=messages&task=settings'); ?>"><?php echo Lang::txt('MOD_MYMESSAGESMINI_MESSAGE_SETTINGS'); ?></a></li>
+		</ul>
+		
 	</div>
 	<?php
 }?>
