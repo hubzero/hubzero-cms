306c29f88f40809addc777a0a63b985003772e43
M. Drew LaMar <mdlama@wm.edu>
2019-05-09 06:47:40 -0400

    [plg_publications_comments] Squashed commit of the following...
    commit 3f08274c722f7cd18af9c8c5fc3820630c9b9f5d
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Wed May 8 09:42:35 2019 -0400
    
        [fix] Removed and renamed some migration files
    
        (cherry picked from commit 150580ec2c3ab2a137e5e70985d6c4561ede42ec)
    
    commit 80b138e14033ef0d971168aff186054c5fc2d626
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue May 7 17:28:40 2019 -0400
    
        [maint] Removing unused files
        View XML files are used for menu options in components but aren't
        supported in plugins.
    
        (cherry picked from commit 23e9a22966ba15dda0d6404de2c525756dc1dc7e)
    
    commit 65571da80b20dca8775bedb2da002cd47c276511
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue May 7 17:28:00 2019 -0400
    
        [style] Formatting fixes
    
        (cherry picked from commit 99dbffeba1efc862f0aaa27bd7336d3ebc9cb635)
    
    commit 046e009e06218e3be43ef0e6a78ce8983f6d15a9
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue May 7 17:27:13 2019 -0400
    
        [maint] Updating copyright
    
        (cherry picked from commit 48e96249969d12fb7865e6a264de6e87b149c99c)
    
    commit 98f19f2b7255fc9ac5105bcea6413ead68fae815
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue May 7 17:25:32 2019 -0400
    
        [fix] Removing Joomla references
        In some cases, variables were renamed to avoid polluting searches for
        Joomla references.
    
        (cherry picked from commit 9545871035efd11454780575ae278075adfdece6)
    
    commit c4009d73b8ae5090f7a76fa7d7e807b534595a7b
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue May 7 17:24:41 2019 -0400
    
        [maint] Removing redundant language strings
        Also, some minor formatting fixes.
    
        (cherry picked from commit edfcc83522479c198c9904244733978a5f63e685)
    
    commit d44efd57499da043586b3e3d5457b6cb27566003
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue May 7 17:23:52 2019 -0400
    
        [maint] Moving common lang string to core file
        This moves a couple commonly repeated language strings to the main
        language file to reduce duplication.
    
        (cherry picked from commit 810d43a303910da5650001314bb0971ef236505b)

--- app/plugins/publications/comments/views/browse/tmpl/_rateitem.php
+++ app/plugins/publications/comments/views/browse/tmpl/_rateitem.php
@@ -1,31 +1,8 @@
 <?php
 /**
- * @package		HUBzero CMS
- * @author		Shawn Rice <zooley@purdue.edu>
- * @copyright	Copyright 2005-2009 HUBzero Foundation, LLC.
- * @license		http://opensource.org/licenses/MIT MIT
- *
- * Copyright 2005-2009 HUBzero Foundation, LLC.
- * All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
  */
 
 // No direct access
@@ -72,23 +49,23 @@ else
 <?php if (!$this->item->get('vote')) { ?>
 	<?php if (User::isGuest()) { ?>
 		<span class="vote-like<?php echo $lcls; ?>">
-			<span class="vote-button <?php echo ($this->item->get('helpful', 0) > 0) ? 'like' : 'neutral'; echo $cls; ?>" title="<?php echo $like_title; ?>">
+			<span class="vote-button <?php echo ($this->item->get('helpful', 0) > 0 ? 'like' : 'neutral') .  $cls; ?>" title="<?php echo $like_title; ?>">
 				<?php echo $this->item->get('helpful', 0); ?><span> Like</span>
 			</span>
 		</span>
 		<span class="vote-dislike<?php echo $dcls; ?>">
-			<span class="vote-button <?php echo ($this->item->get('nothelpful', 0) > 0) ? 'dislike' : 'neutral'; echo $cls; ?>" title="<?php echo $dislike_title; ?>">
+			<span class="vote-button <?php echo ($this->item->get('nothelpful', 0) > 0 ? 'dislike' : 'neutral') .  $cls; ?>" title="<?php echo $dislike_title; ?>">
 				<?php echo $this->item->get('nothelpful', 0); ?><span> Dislike</span>
 			</span>
 		</span>
 	<?php } else { ?>
 		<span class="vote-like<?php echo $lcls; ?>">
-			<a class="vote-button <?php echo ($this->item->get('helpful', 0) > 0) ? 'like' : 'neutral'; echo $cls; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->item->get('publication_id') . '&active=reviews&action=rateitem&refid=' . $this->item->get('id') . '&vote=yes'); ?>" title="<?php echo $like_title; ?>">
+			<a class="vote-button <?php echo ($this->item->get('helpful', 0) > 0 ? 'like' : 'neutral') . $cls; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->item->get('publication_id') . '&active=reviews&action=rateitem&refid=' . $this->item->get('id') . '&vote=yes'); ?>" title="<?php echo $like_title; ?>">
 				<?php echo $this->item->get('helpful', 0); ?><span> Like</span>
 			</a>
 		</span>
 		<span class="vote-dislike<?php echo $dcls; ?>">
-			<a class="vote-button <?php echo ($this->item->get('nothelpful', 0) > 0) ? 'dislike' : 'neutral'; echo $cls; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->item->get('publication_id') . '&active=reviews&action=rateitem&refid=' . $this->item->get('id') . '&vote=no'); ?>" title="<?php echo $dislike_title; ?>">
+			<a class="vote-button <?php echo ($this->item->get('nothelpful', 0) > 0 ? 'dislike' : 'neutral') . $cls; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->item->get('publication_id') . '&active=reviews&action=rateitem&refid=' . $this->item->get('id') . '&vote=no'); ?>" title="<?php echo $dislike_title; ?>">
 				<?php echo $this->item->get('nothelpful', 0); ?><span> Dislike</span>
 			</a>
 		</span>
@@ -96,25 +73,25 @@ else
 <?php } else { ?>
 	<?php if (trim($lcls) == 'chosen') { ?>
 		<span class="vote-like<?php echo $lcls; ?>">
-			<span class="vote-button <?php echo ($this->item->get('helpful', 0) > 0) ? 'like' : 'neutral'; echo $cls; ?>" title="<?php echo $like_title; ?>">
+			<span class="vote-button <?php echo ($this->item->get('helpful', 0) > 0 ? 'like' : 'neutral') . $cls; ?>" title="<?php echo $like_title; ?>">
 				<?php echo $this->item->get('helpful', 0); ?><span> Like</span>
 			</span>
 		</span>
 		<span class="vote-dislike<?php echo $dcls; ?>">
-			<a class="vote-button <?php echo ($this->item->get('nothelpful', 0) > 0) ? 'dislike' : 'neutral'; echo $cls; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->item->get('publication_id') . '&active=reviews&action=rateitem&refid=' . $this->item->get('id') . '&vote=no'); ?>" title="<?php echo $dislike_title; ?>">
+			<a class="vote-button <?php echo ($this->item->get('nothelpful', 0) > 0 ? 'dislike' : 'neutral') . $cls; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->item->get('publication_id') . '&active=reviews&action=rateitem&refid=' . $this->item->get('id') . '&vote=no'); ?>" title="<?php echo $dislike_title; ?>">
 				<?php echo $this->item->get('nothelpful', 0); ?><span> Dislike</span>
 			</a>
 		</span>
 	<?php } else { ?>
 		<span class="vote-like<?php echo $lcls; ?>">
-			<a class="vote-button <?php echo ($this->item->get('helpful', 0) > 0) ? 'like' : 'neutral'; echo $cls; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->item->get('publication_id') . '&active=reviews&action=rateitem&refid=' . $this->item->get('id') . '&vote=yes'); ?>" title="<?php echo $like_title; ?>">
+			<a class="vote-button <?php echo ($this->item->get('helpful', 0) > 0 ? 'like' : 'neutral') . $cls; ?>" href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->item->get('publication_id') . '&active=reviews&action=rateitem&refid=' . $this->item->get('id') . '&vote=yes'); ?>" title="<?php echo $like_title; ?>">
 				<?php echo $this->item->get('helpful', 0); ?><span> Like</span>
 			</a>
 		</span>
 		<span class="vote-dislike<?php echo $dcls; ?>">
-			<span class="vote-button <?php echo ($this->item->get('nothelpful', 0) > 0) ? 'dislike' : 'neutral'; echo $cls; ?>" title="<?php echo $dislike_title; ?>">
+			<span class="vote-button <?php echo ($this->item->get('nothelpful', 0) > 0 ? 'dislike' : 'neutral') . $cls; ?>" title="<?php echo $dislike_title; ?>">
 				<?php echo $this->item->get('nothelpful', 0); ?><span> Dislike</span>
 			</span>
 		</span>
 	<?php } ?>
-<?php } ?>
+<?php }
