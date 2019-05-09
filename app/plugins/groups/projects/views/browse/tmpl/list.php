ff8824c6291e8639d293f9245e9a2ca46d824cfe
M. Drew LaMar <mdlama@wm.edu>
2019-05-09 06:39:29 -0400

    [plg_groups_projects] Squashed commit of the following...
    commit 48ef57c0c3214919a6d23cd590403d6a3b52a52f
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue May 7 13:39:40 2019 -0400
    
        [style] Formatting fixes
    
        (cherry picked from commit e302dd5117c54f5be3a763998f3eac1814177976)
    
    commit 1cf18072488717dac3e491c62e410742b1a3399b
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue May 7 13:35:42 2019 -0400
    
        [maint] Updating copyright
    
        (cherry picked from commit 6797fd2e65c1271b3b3cd70c143ff4ccedbaf088)
    
    commit b6c95ad9b4d5afa5930d7d254029b2b3a8a32946
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Tue Apr 9 11:11:39 2019 -0400
    
        [maint] Added composer.json for installation

--- app/plugins/groups/projects/views/browse/tmpl/list.php
+++ app/plugins/groups/projects/views/browse/tmpl/list.php
@@ -1,33 +1,8 @@
 <?php
 /**
- * HUBzero CMS
- *
- * Copyright 2005-2015 HUBzero Foundation, LLC.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * HUBzero is a registered trademark of Purdue University.
- *
- * @package   hubzero-cms
- * @author    Alissa Nedossekina <alisa@purdue.edu>
- * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
- * @license   http://opensource.org/licenses/MIT MIT
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
  */
 
 // No direct access
@@ -94,7 +69,7 @@ switch ($this->which)
 						<?php } ?>
 					</td>
 					<td class="th_privacy">
-						<?php if (!$row->isPublic()) { echo '<span class="privacy-icon">&nbsp;</span>' ; } ?>
+						<?php if (!$row->isPublic()) { echo '<span class="privacy-icon">&nbsp;</span>'; } ?>
 					</td>
 					<td class="th_title">
 						<?php if ($row->access('member') || $row->access('readonly')) { ?>
@@ -165,10 +140,10 @@ switch ($this->which)
 									<a href="<?php echo $link; ?>" class="btn btn-success" disabled><?php echo Lang::txt('PLG_GROUPS_PROJECTS_REQUEST_DENIED') ;?></a>
 								</div>
 							<?php else: ?>
-								<?php echo ($row->get('sync_group') ? '<span class="synced">' . Lang::txt('PLG_GROUPS_PROJECTS_GROUP_SYNCED') . '</span>' : '<span class="selected">' . Lang::txt('PLG_GROUPS_PROJECTS_GROUP_SELECTED') . '</span>'); ?>
+								<?php echo ($row->get('sync_group')) ? '<span class="synced">' . Lang::txt('PLG_GROUPS_PROJECTS_GROUP_SYNCED') . '</span>' : '<span class="selected">' . Lang::txt('PLG_GROUPS_PROJECTS_GROUP_SELECTED') . '</span>'; ?>
 							<?php endif; ?>
 						<?php else: ?>
-							<?php echo ($row->get('sync_group') ? '<span class="synced">' . Lang::txt('PLG_GROUPS_PROJECTS_GROUP_SYNCED') . '</span>' : '<span class="selected">' . Lang::txt('PLG_GROUPS_PROJECTS_GROUP_SELECTED') . '</span>'); ?>
+							<?php echo ($row->get('sync_group')) ? '<span class="synced">' . Lang::txt('PLG_GROUPS_PROJECTS_GROUP_SYNCED') . '</span>' : '<span class="selected">' . Lang::txt('PLG_GROUPS_PROJECTS_GROUP_SELECTED') . '</span>'; ?>
 						<?php endif; ?>
 					</td>
 				</tr>
