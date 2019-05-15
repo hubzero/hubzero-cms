3fd9b5d83de40690a14b9a1363a2ff464edbb6cb
Patrick Mulligan <jpmulligan@purdue.edu>
2019-05-15 10:28:34 -0400

    [fix] Ensure link is removed if master type blacklisted.
    The cron job wasn't removing ftp file links if the file
    was of a master type that didn't require a bundle.
    
    fixes: https://purr.purdue.edu/support/ticket/2039

---
+++ core/components/com_publications/migrations/Migration20190514144700ComPublications.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding ftpBlacklist to publication params
+ **/
+class Migration20190514144700ComPublications extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$publicationParams = Component::params('com_publications');
+		$publicationParams->set('sftptypeblacklist', 'series, databases');
+		$table = '#__extensions';
+		$params = $publicationParams->toString();
+		if ($this->db->tableExists($table) && $this->db->tableHasField($table, 'params'))
+		{
+			$query = "UPDATE `$table` SET `params`='$params' WHERE `name`='com_publications'";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$publicationParams = Component::params('com_publications');
+		$publicationParams->set('sftptypeblacklist', null);
+		$table = '#__extensions';
+		$params = $publicationParams->toString();
+		if ($this->db->tableExists($table) && $this->db->tableHasField($table, 'params'))
+		{
+			$query = "UPDATE `$table` SET `params`='$params' WHERE `name`='com_publications'";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+}
