8403cd180ab3ae276dc2703a9414008993481734
Shawn Rice <zooley@purdue.edu>
2019-05-16 08:03:54 -0400

    [fix] Initialize the file space upon CREATE

--- core/components/com_projects/api/controllers/projectsv2_0.php
+++ core/components/com_projects/api/controllers/projectsv2_0.php
@@ -632,6 +632,14 @@ class Projectsv2_0 extends ApiController
 			throw new Exception($row->getError());
 		}
 
+		require_once \Component::path('com_projects') . '/models/repo.php';
+
+		$repo = new Repo($row, 'local');
+		if (!$repo->iniLocal())
+		{
+			throw new Exception($repo->getError());
+		}
+
 		// Set timestamp with timezone
 		$row->set('created', with(new Date($row->get('created')))->format('Y-m-d\TH:i:s\Z'));
 		if ($row->get('modified') && $row->get('modified') != '0000-00-00 00:00:00')
