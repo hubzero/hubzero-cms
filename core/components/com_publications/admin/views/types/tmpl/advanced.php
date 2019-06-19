73070194ae6f307a891e22c8225fb961904ca243
Shawn Rice <zooley@purdue.edu>
2019-06-19 07:34:26 -0400

    [fix] Pretty print the JSON so it's reaable

--- core/components/com_publications/admin/views/types/tmpl/advanced.php
+++ core/components/com_publications/admin/views/types/tmpl/advanced.php
@@ -15,13 +15,12 @@ Toolbar::title(Lang::txt('COM_PUBLICATIONS_PUBLICATION') . ' ' . Lang::txt('COM_
 Toolbar::save('saveadvanced');
 Toolbar::cancel();
 
-$params = new \Hubzero\Config\Registry($this->row->params);
+$params = new Hubzero\Config\Registry($this->row->params);
 $manifest  = $this->curation->_manifest;
 $curParams = $manifest->params;
 $blocks    = $manifest->blocks;
 
 $blockSelection = array('active' => array());
-
 ?>
 
 <form action="<?php echo Route::url('index.php?option=' . $this->option . '&controller=' . $this->controller . '&task=saveadvanced'); ?>" method="post" id="item-form" name="adminForm">
@@ -39,7 +38,7 @@ $blockSelection = array('active' => array());
 		<p class="hint"><?php echo Lang::txt('COM_PUBLICATIONS_MTYPE_ADVANCED_CURATION_EDITING_HINT'); ?></p>
 
 		<div class="input-wrap">
-			<textarea cols="50" rows="10" name="curation"><?php echo json_encode($manifest); ?></textarea>
+			<textarea cols="50" rows="30" name="curation"><?php echo json_encode($manifest, JSON_PRETTY_PRINT); ?></textarea>
 		</div>
 	</fieldset>
 	<?php echo Html::input('token'); ?>
