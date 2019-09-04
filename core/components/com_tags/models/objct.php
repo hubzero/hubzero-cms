9270a164d65ecada8a7b6c4328fbbc43a6d38cd0
Ilya Shunko <ishunko@purdue.edu>
2019-09-04 05:52:29 +0000

    Prevent merging tags code creating multiple associatins between the same object and the same tag

--- core/components/com_tags/models/objct.php
+++ core/components/com_tags/models/objct.php
@@ -1,13 +1,38 @@
 <?php
 /**
- * @package    hubzero-cms
- * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
- * @license    http://opensource.org/licenses/MIT MIT
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 HUBzero Foundation, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
  */
 
 namespace Components\Tags\Models;
 
 use Hubzero\Database\Relational;
+use Recurr\Exception;
 use stdClass;
 use Lang;
 use Date;
@@ -196,10 +221,24 @@ class Objct extends Relational
 
 		foreach ($items as $item)
 		{
-			$item->set('tagid', $newtagid);
-			$item->save();
+			// Find if there is already a database row with the same object and tag. Delete the item then.
+			$duplicates = self::all()
+				->whereEquals('tbl', $item->get('tbl'))
+				->whereEquals('tagid', $newtagid)
+				->whereEquals('objectid', $item->get('objectid'))
+				->rows();
 
-			$entries[] = $item->toArray();
+			if ($duplicates->count())
+			{
+				$item->destroy();
+			}
+			else
+			{
+				$item->set('tagid', $newtagid);
+				$item->save();
+
+				$entries[] = $item->toArray();
+			}
 		}
 
 		$data = new stdClass;
---
+++ core/migrations/Migration20190902000001ComTags.php
@@ -0,0 +1,38 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for dropping unused #__tags_group table
+ **/
+class Migration20190902000001ComTags extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if ($this->db->tableExists('#__tags_object'))
+		{
+			$query = "ALTER IGNORE TABLE `#__tags_object` ADD UNIQUE INDEX unique_tag_per_obj (objectid, tagid, tbl)";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+
+	/**
+	 * Up
+	 **/
+	public function down()
+	{
+		if ($this->db->tableExists('#__tags_object'))
+		{
+			$query = "ALTER TABLE `#__tags_object` DROP INDEX unique_tag_per_obj";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+}
