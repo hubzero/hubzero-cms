d5de4160e78e4fcbf78a41c97c0e74171ce0a52e
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2019-09-05 13:41:04 +0000

    [com_search] Implement boost query helper classes

---
+++ core/components/com_search/helpers/solariumBoostQuery.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Components\Search\Helpers;
+
+class SolariumBoostQuery
+{
+	protected $boost,
+		$query;
+
+	public function __construct($args = [])
+	{
+		$this->query = null;
+		$this->boost = $args['boost'];
+	}
+
+	public function toArray()
+	{
+		$query = $this->getQuery();
+
+		return ['query' => $query];
+	}
+
+	protected function getQuery()
+	{
+		if (!$this->query)
+		{
+			$this->query = $this->generateQuery();
+		}
+
+		return $this->query;
+	}
+
+	protected function generateQuery()
+	{
+		$field = $this->boost->getField();
+		$fieldValue = $this->boost->getFieldValue();
+		$strength = $this->boost->getStrength();
+
+		return "$field:$fieldValue^$strength";
+	}
+
+	public static function one($args)
+	{
+		return new self($args);
+	}
+
+}
---
+++ core/components/com_search/tests/boostQueryHelperTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Components\Search\Tests;
+
+$componentPath = Component::path('com_search');
+
+require_once "$componentPath/helpers/boostQueryHelper.php";
+require_once "$componentPath/tests/traits/canMock.php";
+
+use Components\Search\Helpers\BoostQueryHelper as Helper;
+use Components\Search\Tests\Traits\canMock;
+use Hubzero\Test\Basic;
+
+class BoostQueryHelperTest extends Basic
+{
+	use canMock;
+
+	public function testGetAllQueriesInvokesOne()
+	{
+		$boosts = [1, 1, 1];
+		$factoryMock = $this->mock([
+			'class' => 'BoostQueries', 'methods' => ['one']
+		]);
+		$ormMock = $this->mock([
+			'class' => 'Boost', 'methods' => ['all' => $boosts]
+		]);
+		$helper = new Helper([
+			'boosts' => $ormMock,
+		 	'queries' => $factoryMock
+		]);
+
+		$factoryMock->expects($this->once())
+			->method('one')
+			->with(['boosts' => $boosts]);
+
+		$boostQueries = $helper->getAllQueries();
+	}
+
+}
