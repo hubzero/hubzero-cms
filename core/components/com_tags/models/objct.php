400ab1d7ee1113b97d60861a26b89a5a515f050d
Ilya Shunko <ishunko@purdue.edu>
2019-09-10 00:22:28 +0000

    Update the header

--- core/components/com_tags/models/objct.php
+++ core/components/com_tags/models/objct.php
@@ -1,38 +1,13 @@
 <?php
 /**
- * HUBzero CMS
- *
- * Copyright 2005-2015 HUBzero Foundation, LLC.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * HUBzero is a registered trademark of Purdue University.
- *
- * @package   hubzero-cms
- * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
- * @license   http://opensource.org/licenses/MIT MIT
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
  */
 
 namespace Components\Tags\Models;
 
 use Hubzero\Database\Relational;
-use Recurr\Exception;
 use stdClass;
 use Lang;
 use Date;
@@ -227,7 +202,6 @@ class Objct extends Relational
 				->whereEquals('tagid', $newtagid)
 				->whereEquals('objectid', $item->get('objectid'))
 				->rows();
-
 			if ($duplicates->count())
 			{
 				$item->destroy();
@@ -236,7 +210,6 @@ class Objct extends Relational
 			{
 				$item->set('tagid', $newtagid);
 				$item->save();
-
 				$entries[] = $item->toArray();
 			}
 		}
