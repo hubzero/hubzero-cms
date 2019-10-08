f95cd15361b08e26d841fb593388c928bbdb5ee5
root <root@example.com>
2019-10-08 15:27:41 -0400

    [PURR][#2100]Display the series that a publication belongs to on the publication page.

--- core/components/com_publications/tables/attachment.php
+++ core/components/com_publications/tables/attachment.php
@@ -875,4 +875,44 @@ class Attachment extends Table
 
 		return true;
 	}
+	
+	/**
+	 * Get array of series that the publication belongs to 
+	 *
+	 * @param   integer  $verID  Pub version ID
+	 * 
+	 * @return  mixed    series  or false
+	 */
+	public function getSeries($verID)
+	{
+		if ($verID === null)
+		{
+			return false;
+		}
+
+		$seriesObjArr = [];
+		
+		// Get publication version id of series
+		$sql = "SELECT publication_version_id FROM `$this->_tbl` WHERE object_id=$verID";
+		$this->_db->setQuery($sql);
+		$pubVerIdObjArr = $this->_db->loadObjectList();
+		
+		// Get title, abstract, pub version id, version number
+		if (!empty($pubVerIdObjArr))
+		{
+			foreach ($pubVerIdObjArr as $pubVerIdObj)
+			{
+				$seriesQuery = "SELECT publication_id, title, abstract, version_number FROM `#__publication_versions` WHERE id = $pubVerIdObj->publication_version_id";
+				$this->_db->setQuery($seriesQuery);
+				$seriesObj = $this->_db->loadObject();
+				$seriesObjArr[] = $seriesObj;
+			}
+			
+			return $seriesObjArr;
+		}
+		else
+		{
+			return false;
+		}	
+	}
 }
