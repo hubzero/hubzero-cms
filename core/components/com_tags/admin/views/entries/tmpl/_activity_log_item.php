e392bad864aae4300cd65b05a5f736cefb265e2e
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2019-12-05 20:43:50 +0000

    [com_tags] Extract tag log presentation logic
    fixes: https://habricentral.org/support/ticket/869

---
+++ core/components/com_tags/admin/views/entries/tmpl/_activity_log_item.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access.
+defined('_HZEXEC_') or die();
+
+use Components\Tags\Helpers\ActivityLogPresenter;
+
+$log = $this->log;
+$id = $log->get('id');
+
+$parser = new ActivityLogPresenter();
+$parsedLog = $parser->parse($log);
+
+if ($parsedLog->activityDescription): ?>
+	<li class="<?php echo $parsedLog->class; ?>"
+		data-id="<?php echo $id; ?>">
+		<span class="entry-log-data">
+			<?php echo $parsedLog->activityDescription; ?>
+		</span>
+	</li>
+<?php endif; ?>
