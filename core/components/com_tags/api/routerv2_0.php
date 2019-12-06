7c3df4ebf05861ab766bcad770526295fb437316
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2019-12-06 18:41:59 +0000

    [com_tags] Implement tag log API endpoint

---
+++ core/components/com_tags/api/routerv2_0.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Components\Tags\Api;
+
+use Hubzero\Component\Router\Base;
+
+class Router extends Base
+{
+
+	public function build(&$query)
+	{
+		$segments = [];
+
+		if (!empty($query['controller']))
+		{
+			$segments[] = $query['controller'];
+			unset($query['controller']);
+		}
+
+		if (!empty($query['task']))
+		{
+			$segments[] = $query['task'];
+			unset($query['task']);
+		}
+
+		return $segments;
+	}
+
+	public function parse(&$segments)
+	{
+		$vars = [];
+
+		if (isset($segments[0]))
+		{
+			$vars['controller'] = $segments[0];
+		}
+
+		if (isset($segments[1]))
+		{
+			if (is_numeric($segments[1]))
+			{
+				$vars['id'] = $segments[1];
+			}
+			else
+			{
+				$vars['task'] = $segments[1];
+			}
+		}
+
+		return $vars;
+	}
+}
