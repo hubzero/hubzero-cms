017b1d4a70ae304f119d49fee7c88df6aa9d2f72
Jenny Kwan <jakwan33@gmail.com>
2020-02-17 12:01:48 -0700

    [style] Update OER page with new text

--- app/templates/bmc/js/pages/oer.js
+++ app/templates/bmc/js/pages/oer.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   // Header animations on load
   var $introBcg = $('.oer-header'),
       $introH2 = $('.intro h2'),
+      $introH3 = $('.intro h3'),
       $introP = $('.intro p'),
       $introLink = $('.intro a'),
       $anchor = $('.oer'),
@@ -14,6 +15,7 @@ $(document).ready(function() {
      $tlLoad
        .from($introBcg, .8, {autoAlpha: 0})
        .from($introH2, 0.8, {y: 10, autoAlpha: 0, ease: Power0.easeNone}, '-=0.5')
+       .from($introH3, 0.8, {y: 10, autoAlpha: 0, ease: Power0.easeNone}, '-=0.5')
        .from($introP, 0.8, {y: 10, autoAlpha: 0, ease: Power0.easeNone}, '-=0.3')
        .from($introLink, 0.8, {y:10, autoAlpha: 0, ease: Power0.easeNone}, '-=0.3')
        .staggerFrom($anchor, .6, {y: 10, autoAlpha: 0, ease: Back.easeInOut.config(1.4)}, 0.2, '-=.5');
