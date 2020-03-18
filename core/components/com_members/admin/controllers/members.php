e4a52afb2107e685d48c48b0120065f1486e8ee2
Erich Huebner <ehuebner@ucsd.edu>
2020-03-18 11:07:06 -0700

    Travis fixes

--- core/components/com_members/admin/controllers/members.php
+++ core/components/com_members/admin/controllers/members.php
@@ -995,8 +995,8 @@ class Members extends AdminController
 
 			// Load user password object
 			$passinfo = \Hubzero\User\Password::getInstance($user->get('id'));
-	
 			// If blocking
+
 			if ($state == 1)
 			{
 				// Set user Shadow Expiration to a past date.  This disabled the account in LDAP.  "10957" is the number of days to January 1, 2000 from epoch start
