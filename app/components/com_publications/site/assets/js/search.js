a072552cb597afd037f5a374113b5a199de28a18
Jenny Kwan <jakwan33@gmail.com>
2020-04-26 12:47:10 -0600

    [fix] Pagination loaded by ajax

--- app/components/com_publications/site/assets/js/search.js
+++ app/components/com_publications/site/assets/js/search.js
@@ -3,9 +3,6 @@ $(document).ready(function() {
   	return this + (this.indexOf('?') == -1 ? '?' : '&') + 'no_html=1';
   };
 
-  // Remove onchange function for pagination
-  $('#limit').removeAttr('onchange');
-
   var url = '/qubesresources/publications/browse';
   var limit = $('#limit').val();
   var queryParams = '?search=';
@@ -14,7 +11,12 @@ $(document).ready(function() {
   var container = $('#results-container');
   var urlToFetch = null;
 
+  // Remove onchange function for pagination
+  const removeOnchange = () => {
+    $('#limit').removeAttr('onchange');
+  }
 
+  removeOnchange();
 
   $('#resourcesform').on('submit', function(e) {
     e.preventDefault();
@@ -23,15 +25,8 @@ $(document).ready(function() {
     var encodedTerms = encodeURIComponent(inputTerms).replace(/%20/g, '+');
     urlToFetch = `${url}${queryParams}${encodedTerms}${sortbyParams}${limitParams}`;
 
-    console.log(inputTerms);
-    console.log(limit);
-    console.log(encodedTerms);
-    console.log(urlToFetch);
-
     if (container.length && inputTerms.length) {
-      container.load(urlToFetch + ' #results-container', function() {
-        container.find('#limit').removeAttr('onchange');
-      });
+      container.load(urlToFetch + ' #results-container', removeOnchange);
       return urlToFetch;
     }
   });
@@ -40,22 +35,21 @@ $(document).ready(function() {
     console.log('limit change detected');
     var newLimit = $('#limit').val();
     limitParams = `&limit=${newLimit}&limitstart=0`;
-    console.log(newLimit);
-    console.log(urlToFetch);
 
     if (urlToFetch === null) {
       urlToFetch = `${url}${queryParams}${sortbyParams}${limitParams}`;
-      console.log(urlToFetch);
-      container.load(urlToFetch + ' #results-container', function() {
-        container.find('#limit').removeAttr('onchange');
-      });
+      container.load(urlToFetch + ' #results-container', removeOnchange);
     } else {
       urlToFetch = urlToFetch.replace(/limit=.+&limitstart=0/, 'limit=' + newLimit + '&limitstart=0');
-      console.log(urlToFetch);
-      container.load(urlToFetch + ' #results-container', function() {
-        container.find('#limit').removeAttr('onchange');
-      });
+      container.load(urlToFetch + ' #results-container', removeOnchange);
     }
   });
 
+  $('body').on('click', '.pagination li a', function(e) {
+    e.preventDefault();
+
+    var page = $(this).attr('href');
+
+    container.load(page + ' #results-container', removeOnchange);
+  });
 });
