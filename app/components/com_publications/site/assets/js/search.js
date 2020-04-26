b603481bb40ff7a740620be7a7be303184007404
Jenny Kwan <jakwan33@gmail.com>
2020-04-25 23:46:40 -0600

    [fix] Limit changes loaded by ajax

--- app/components/com_publications/site/assets/js/search.js
+++ app/components/com_publications/site/assets/js/search.js
@@ -3,20 +3,58 @@ $(document).ready(function() {
   	return this + (this.indexOf('?') == -1 ? '?' : '&') + 'no_html=1';
   };
 
+  // Remove onchange function for pagination
+  $('#limit').removeAttr('onchange');
+
+  var url = '/qubesresources/publications/browse';
+  var limit = $('#limit').val();
+  var queryParams = '?search=';
+  var sortbyParams = '&sortby=date';
+  var limitParams = `&limit=${limit}&limitstart=0`;
+  var container = $('#results-container');
+  var urlToFetch = null;
+
+
+
   $('#resourcesform').on('submit', function(e) {
     e.preventDefault();
 
-    var terms = $('#entry-search-field').val();
-    var encoded = encodeURIComponent(terms).replace(/%20/g, '+');
-    var container = $($(this).attr('data-target'));
-    var url = $(this).attr('action') + '?' + 'search=' + encoded;
+    var inputTerms = $('#entry-search-field').val();
+    var encodedTerms = encodeURIComponent(inputTerms).replace(/%20/g, '+');
+    urlToFetch = `${url}${queryParams}${encodedTerms}${sortbyParams}${limitParams}`;
 
-    console.log(terms);
-    console.log(encoded);
-    console.log(url);
+    console.log(inputTerms);
+    console.log(limit);
+    console.log(encodedTerms);
+    console.log(urlToFetch);
 
-    if (container.length && terms.length) {
-      container.load(url + ' #results-container');
+    if (container.length && inputTerms.length) {
+      container.load(urlToFetch + ' #results-container', function() {
+        container.find('#limit').removeAttr('onchange');
+      });
+      return urlToFetch;
+    }
+  });
+
+  $('body').on('change', '#limit', function() {
+    console.log('limit change detected');
+    var newLimit = $('#limit').val();
+    limitParams = `&limit=${newLimit}&limitstart=0`;
+    console.log(newLimit);
+    console.log(urlToFetch);
+
+    if (urlToFetch === null) {
+      urlToFetch = `${url}${queryParams}${sortbyParams}${limitParams}`;
+      console.log(urlToFetch);
+      container.load(urlToFetch + ' #results-container', function() {
+        container.find('#limit').removeAttr('onchange');
+      });
+    } else {
+      urlToFetch = urlToFetch.replace(/limit=.+&limitstart=0/, 'limit=' + newLimit + '&limitstart=0');
+      console.log(urlToFetch);
+      container.load(urlToFetch + ' #results-container', function() {
+        container.find('#limit').removeAttr('onchange');
+      });
     }
   });
 
