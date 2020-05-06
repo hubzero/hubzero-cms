779e2f9f682a6d9f5f90019e9c289497832f3344
M. Drew LaMar <mdlama@wm.edu>
2020-05-06 16:28:25 -0400

    [feat] Initial commit of collect pubs plugin

---
+++ app/plugins/publications/collections/collections.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Resources Plugin class for adding collections 
+ */
+class plgResourcesCollections extends \Hubzero\Plugin\Plugin
+{
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 */
+	protected $_autoloadLanguage = true;
+
+	/**
+	 * Return data on a resource sub view (this will be some form of HTML)
+	 *
+	 * @param   object   $resource  Current resource
+	 * @param   string   $option    Name of the component
+	 * @param   integer  $miniview  View style
+	 * @return  array
+	 */
+	public function onResourcesSub($resource, $option, $miniview=0)
+	{
+		if (!$resource->type->params->get('plg_collections', 0))
+		{
+			return;
+		}
+		$pparams = Plugin::params('resources', 'collections');
+		$collectionType = $pparams->get('collection_alias');
+		$allowPublished = $pparams->get('collection_afterpublished');
+		$typeObj = Components\Resources\Models\Type::oneByAlias($collectionType);
+		if (!($typeObj) || !($typeObj->get('collection')))
+		{
+			return false;
+		}
+		$parentIds = $resource->parents->fieldsByKey('id');
+		$resources = Components\Resources\Models\Entry::all();
+		$resources->whereEquals('standalone', 1);
+		$resources->whereEquals('type', $typeObj->get('id'));
+		if (!$allowPublished)
+		{
+			$resources->whereEquals('published', 2);
+		}
+		if (!empty($parentIds))
+		{
+			$resources->where('id', 'NOT IN', $parentIds);
+		}
+		if (!User::authorize('core.admin', $option))
+		{
+			$resources->whereEquals('created_by', User::get('id'), 1);
+			// Get the groups the user has access to
+			$xgroups = \Hubzero\User\Helper::getGroups(User::get('id'), 'all');
+			$usersgroups = array();
+			if (!empty($xgroups))
+			{
+				foreach ($xgroups as $group)
+				{
+					if ($group->regconfirmed)
+					{
+						$resources->orWhereLike('group_owner', $group->cn, 1);
+					}
+				}
+			}
+		}
+		// Instantiate a view
+		$view = $this->view('default', 'index');
+		$view
+			->set('resource', $resource)
+			->set('resources', $resources)
+			->set('type', $typeObj)
+			->js('collections.js')
+			->css('collections.css');
+		// Return the output
+		$arr['html'] = $view->loadTemplate();
+		// Return the an array of content
+		return $arr;
+	}
+}
---
+++ app/plugins/publications/collections/views/index/tmpl/default.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+?>
+<p><a class="btn primary" href="#collectionForm" id="add-collection"><?php echo Lang::txt('PLG_RESOURCES_COLLECTIONS_ADD', $this->type->type);?></a></p>
+
+<form action="<?php echo Route::url($this->resource->link());?>"  method="post" id="collectionForm" class="full hide">
+	<fieldset>
+		<legend><?php echo Lang::txt('PLG_RESOURCES_COLLECTIONS_ADD', $this->type->type);?></legend>
+		<div class="grid">
+			<?php if ($this->resources->count() > 0): ?>
+				<div class="col span12">
+					<label for="pid">
+						<?php echo Lang::txt('PLG_RESOURCES_COLLECTIONS_SELECT', $this->type->type); ?>
+					</label>
+					<select name="pid" id="pid">
+						<option value="" selected><?php echo Lang::txt('PLG_RESOURCES_COLLECTIONS_SELECT_PLACEHOLDER', $this->type->type);?></option>
+						<?php foreach ($this->resources as $entry): ?>
+							<option value="<?php echo $entry->id;?>"><?php echo $entry->title; ?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+				<div class="col span12">
+					<p class="or"><?php echo Lang::txt('OR'); ?></p>
+				</div>
+			<?php endif; ?>
+			<div class="col span12" id="new-series-add">
+				<label><?php echo Lang::txt('PLG_RESOURCES_COLLECTIONS_ADD_NEW', $this->type->type);?></label>
+				<label for="resource-title">
+					<?php echo Lang::txt('PLG_RESOURCES_COLLECTIONS_TITLE');?>
+				</label>
+				<input type="text" name="resource-title" id="resource-title" value="" />
+			</div>
+		</div>
+	</fieldset>
+	<p class="submit">
+		<input type="hidden" name="childid" value="<?php echo $this->resource->id;?>"/>
+		<input type="hidden" name="controller" value="attachments" />
+		<input type="hidden" name="task" value="create" />
+		<input type="hidden" name="type" value="<?php echo $this->type->id;?>" />
+		<input type="submit" value="<?php echo Lang::txt('Add'); ?>" />
+	</p>
+</form>
