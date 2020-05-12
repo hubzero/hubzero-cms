f2cbc17c495b1b9e8b80da0e3d5c7002abb984ab
Jenny Kwan <jakwan33@gmail.com>
2020-05-11 18:03:04 -0600

    [refactor] Include header on each page

--- app/components/com_publications/site/views/publications/tmpl/oer.php
+++ app/components/com_publications/site/views/publications/tmpl/oer.php
@@ -11,53 +11,57 @@ $this->css()
      ->css('intro');
 ?>
 
-<div class="oer-wrapper">
-  <div class="philosophy-wrapper">
-    <h3>Our OER philosophy</h3>
-    <p>Open Educational Resources (OER) are any type of educational material that are freely available for teachers and students to use, adapt, share, and reuse (<a href="https://pitt.libguides.com/openeducation/intro" target="_blank">Collister, 2019</a>). The QUBES project supports the use of open licensing to make learning resources widely accessible and to provide opportunities for faculty to get professional credit for their teaching scholarship. The use of OER can support Open Educational Practices (<a href="https://library.educause.edu/resources/2018/7/7-things-you-should-know-about-open-education-practices" target="_blank">OEP</a>) more broadly which promote the use of innovative learning environments and pedagogical strategies. There is emerging evidence that the use of open educational resources can have a positive impact on student academic success and creates more inclusive educational settings (<a href="https://rdcu.be/b0fGz" target="_blank">Fischer, et al., 2015</a>).</p>
+<?php include_once Component::path('com_publications') . DS . 'site' . DS . 'views' . DS . 'publications' . DS . 'tmpl' . DS . 'intro.php';  ?>
 
-    <p>QUBES emphasizes the central role of participation in a scholarly community as a mechanism to advance the development and use of OER. We believe that professional collaborations built around a shared interest and desire to improve and share resources benefits learners in diverse educational contexts. Parallel to the situation in open source software communities, where the users are also developers, QUBES users are invited to share their adaptations to existing educational resources through our publishing platform.</p>
-  </div>
+<div id="live-update-content">
+  <div class="oer-wrapper">
+    <div class="philosophy-wrapper">
+      <h3>Our OER philosophy</h3>
+      <p>Open Educational Resources (OER) are any type of educational material that are freely available for teachers and students to use, adapt, share, and reuse (<a href="https://pitt.libguides.com/openeducation/intro" target="_blank">Collister, 2019</a>). The QUBES project supports the use of open licensing to make learning resources widely accessible and to provide opportunities for faculty to get professional credit for their teaching scholarship. The use of OER can support Open Educational Practices (<a href="https://library.educause.edu/resources/2018/7/7-things-you-should-know-about-open-education-practices" target="_blank">OEP</a>) more broadly which promote the use of innovative learning environments and pedagogical strategies. There is emerging evidence that the use of open educational resources can have a positive impact on student academic success and creates more inclusive educational settings (<a href="https://rdcu.be/b0fGz" target="_blank">Fischer, et al., 2015</a>).</p>
 
-  <div class="oer-on-qubes">
-      <h3>OER on QUBES</h3>
-      <div class="img-wrap">
-				<picture>
-					<source type="image/webp" srcset="/app/site/media/images/laptop.webp">
-					<source type="image/jp2" srcset="/app/site/media/images/laptop.jp2">
-					<source type="image/vnd.ms-photo" srcset="/app/site/media/images/laptop.jxr">
-					<img src="/app/site/media/images/laptop.jpg" alt="Image of man working on a laptop">
-				</picture>
-			</div>
-      <ul>
-        <li>With 1000 user submitted educational resources, QUBES shares diverse materials that can be adapted to fit your classroom. </li>
-        <li>New resources are added regularly and cover many topics in biology, mathematics, and data science.</li>
-        <li>We also host <a href="https://qubeshub.org/qubesresources/software">open source software tools</a>.</li>
-        <li><a href="https://qubeshub.org/community">Join a community</a> around the development and use of OER</li>
-      </ul>
+      <p>QUBES emphasizes the central role of participation in a scholarly community as a mechanism to advance the development and use of OER. We believe that professional collaborations built around a shared interest and desire to improve and share resources benefits learners in diverse educational contexts. Parallel to the situation in open source software communities, where the users are also developers, QUBES users are invited to share their adaptations to existing educational resources through our publishing platform.</p>
     </div>
 
-  <div class="share-your-oer">
-    <h3>Benefits of sharing on QUBES</h3>
-    <p>QUBES believes in providing quality resources to improve quantitative biology education with a focus on equity and inclusion in the classroom. We believe in harnessing the power of community to not only create and incubate ideas, but improve or provide feedback which in turn, improves resources for all educators.</p>
-
-    <h5>Get credit for your teaching scholarship</h5>
-    <div class="contribute-descriptor">
-      <img src="app/components/com_publications/site/assets/img/chart.png" />
-      <div>
-        <p>Participating in OER is valuable work that you should receive credit for! QUBES resources provide multiple ways to document your contributions and track your impact:</p>
+    <div class="oer-on-qubes">
+        <h3>OER on QUBES</h3>
+        <div class="img-wrap">
+  				<picture>
+  					<source type="image/webp" srcset="/app/site/media/images/laptop.webp">
+  					<source type="image/jp2" srcset="/app/site/media/images/laptop.jp2">
+  					<source type="image/vnd.ms-photo" srcset="/app/site/media/images/laptop.jxr">
+  					<img src="/app/site/media/images/laptop.jpg" alt="Image of man working on a laptop">
+  				</picture>
+  			</div>
         <ul>
-          <li><strong>Digital Object Identifier (DOI)</strong>: The QUBES Resource System automatically assigns a DOI to all submitted resources. This DOI provides a stable address that can be used to find your resources and track how it is being used.</li>
-          <li><strong>Usage Metrics</strong>: Information about the impact of your resources are automatically tracked through the number of views, downloads, and adaptations over time.</li>
-          <li><strong>Visibility and Engagement</strong>: QUBES resources are indexed by a variety of OER search engines and our platform makes it easy for interested users to comment on or adapt your materials.</li>
+          <li>With 1000 user submitted educational resources, QUBES shares diverse materials that can be adapted to fit your classroom. </li>
+          <li>New resources are added regularly and cover many topics in biology, mathematics, and data science.</li>
+          <li>We also host <a href="https://qubeshub.org/qubesresources/software">open source software tools</a>.</li>
+          <li><a href="https://qubeshub.org/community">Join a community</a> around the development and use of OER</li>
         </ul>
       </div>
-    </div>
 
-    <h5>Share, Improve, Broaden Impact of Visibility and Engagement</h5>
-    <div class="contribute-descriptor">
-      <img src="app/components/com_publications/site/assets/img/orbit2.png" />
-      <p><a href="https://qubeshub.org/community/partners">Many educational projects and professional societies</a> collaborate with QUBES to further broaden the impacts of their work. Our <a href="https://qubeshub.org/community/fmns">Faculty Mentoring Networks</a> (FMNs) provide partners with an opportunity to share and improve their materials with faculty who implement these resources in their classrooms - providing valuable real-time feedback. When you share on QUBES, you are also searchable in the <a href="https://mason.deepwebaccess.com/mason__MasonLibrariesOpenEducationResources_5f4/desktop/en/search.html">Mason Open Metafinder</a> database.</p>
+    <div class="share-your-oer">
+      <h3>Benefits of sharing on QUBES</h3>
+      <p>QUBES believes in providing quality resources to improve quantitative biology education with a focus on equity and inclusion in the classroom. We believe in harnessing the power of community to not only create and incubate ideas, but improve or provide feedback which in turn, improves resources for all educators.</p>
+
+      <h5>Get credit for your teaching scholarship</h5>
+      <div class="contribute-descriptor">
+        <img src="app/components/com_publications/site/assets/img/chart.png" />
+        <div>
+          <p>Participating in OER is valuable work that you should receive credit for! QUBES resources provide multiple ways to document your contributions and track your impact:</p>
+          <ul>
+            <li><strong>Digital Object Identifier (DOI)</strong>: The QUBES Resource System automatically assigns a DOI to all submitted resources. This DOI provides a stable address that can be used to find your resources and track how it is being used.</li>
+            <li><strong>Usage Metrics</strong>: Information about the impact of your resources are automatically tracked through the number of views, downloads, and adaptations over time.</li>
+            <li><strong>Visibility and Engagement</strong>: QUBES resources are indexed by a variety of OER search engines and our platform makes it easy for interested users to comment on or adapt your materials.</li>
+          </ul>
+        </div>
+      </div>
+
+      <h5>Share, Improve, Broaden Impact of Visibility and Engagement</h5>
+      <div class="contribute-descriptor">
+        <img src="app/components/com_publications/site/assets/img/orbit2.png" />
+        <p><a href="https://qubeshub.org/community/partners">Many educational projects and professional societies</a> collaborate with QUBES to further broaden the impacts of their work. Our <a href="https://qubeshub.org/community/fmns">Faculty Mentoring Networks</a> (FMNs) provide partners with an opportunity to share and improve their materials with faculty who implement these resources in their classrooms - providing valuable real-time feedback. When you share on QUBES, you are also searchable in the <a href="https://mason.deepwebaccess.com/mason__MasonLibrariesOpenEducationResources_5f4/desktop/en/search.html">Mason Open Metafinder</a> database.</p>
+      </div>
     </div>
   </div>
 </div>
