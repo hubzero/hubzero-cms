996ed6c6093f34f2e2d52489a3d6dfa60a643d91
Erich Huebner <ehuebner@ucsd.edu>
2020-09-10 11:11:37 -0700

    Updated display text - Can you DUO, etc.

--- core/plugins/authfactors/google/views/challenge/tmpl/enroll.php
+++ core/plugins/authfactors/google/views/challenge/tmpl/enroll.php
@@ -17,7 +17,7 @@ use phpseclib\Crypt\Hash;
 <form class="gauth2fa" action="<?php echo Request::current(); ?>" method="POST">
 	<div class="title">Google Authenticator Setup</div>
 
-	<p class="subtitle"> Please scan with the <a target="_blank" rel="noopener noreferrer" href="https://support.google.com/accounts/topic/2954345?hl=en&ref_topic=7667090"> Google Authenticator App</a></p>
+	<p class="subtitle"> Please scan with the <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=en_US"> Google Authenticator </a> or the <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.duosecurity.duomobile&hl=en_US"> Duo Mobile</a>  App on your device. </p>
 
 	<?php
 	   // Setup Google Authenticator, call the google_authentictor script
@@ -36,6 +36,7 @@ use phpseclib\Crypt\Hash;
 		<input type="hidden" name="action" value="registered" />
 		<input type="hidden" name="factor" value="gauth2fa" />
 		<div class="grouping">
-			<input type="submit" value="Continue" class="btn btn-success" />
+			<input type="submit" value="Complete 2FA Enrollment" class="btn btn-success" />
+			<p class="subtitle">The QR code will no longer be available after Enrollment is completed.</p>
 		</div>
 </form>
