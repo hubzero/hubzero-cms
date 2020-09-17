7ee9f3fccfd005819deaad360bfff238cfb8c7cc
Damion Junk <junkda@purdue.edu>
2020-09-17 12:13:05 -0500

    Fixing linter issues.

--- core/migrations/Migration20200917000000AuthLinkIndex.php
+++ core/migrations/Migration20200917000000AuthLinkIndex.php
@@ -5,34 +5,37 @@ use Hubzero\Content\Migration\Base;
 // no direct access
 defined('_HZEXEC_') or die();
 
+/**
+ * Migration script for adding indexes to the #__auth_link table.
+ **/
 class Migration202000171000000AuthLinkIndex extends Base
 {
-    public function up()
-    {
-        if ($this->db->tableExists('#__auth_link'))
-        {
-            $query = "ALTER TABLE `#__auth_link` ADD INDEX auth_domain_id_idx (auth_domain_id);";
-            $this->db->setQuery($query);
-            $this->db->query();
+	public function up()
+	{
+		if ($this->db->tableExists('#__auth_link'))
+		{
+			$query = "ALTER TABLE `#__auth_link` ADD INDEX auth_domain_id_idx (auth_domain_id);";
+			$this->db->setQuery($query);
+			$this->db->query();
 
-            $query = "ALTER TABLE `#__auth_link` ADD INDEX user_id_idx (user_id);";
-            $this->db->setQuery($query);
-            $this->db->query();
-        }
-    }
+			$query = "ALTER TABLE `#__auth_link` ADD INDEX user_id_idx (user_id);";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
 
-    public function down()
-    {
-        if ($this->db->tableExists('#__auth_link'))
-        {
-            $query = "ALTER TABLE `#__auth_link` DROP INDEX auth_domain_id_idx;";
-            $this->db->setQuery($query);
-            $this->db->query();
+	public function down()
+	{
+		if ($this->db->tableExists('#__auth_link'))
+		{
+			$query = "ALTER TABLE `#__auth_link` DROP INDEX auth_domain_id_idx;";
+			$this->db->setQuery($query);
+			$this->db->query();
 
-            $query = "ALTER TABLE `#__auth_link` DROP INDEX user_id_idx;";
-            $this->db->setQuery($query);
-            $this->db->query();
-        }
-    }
+			$query = "ALTER TABLE `#__auth_link` DROP INDEX user_id_idx;";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
 
 }
--- core/migrations/Migration20200917100000AuthDomainIndex.php
+++ core/migrations/Migration20200917100000AuthDomainIndex.php
@@ -5,26 +5,29 @@ use Hubzero\Content\Migration\Base;
 // no direct access
 defined('_HZEXEC_') or die();
 
+/**
+ * Migration script for adding indexes to the #__auth_domain table.
+ **/
 class Migration202000171000000AuthDomainIndex extends Base
 {
-    public function up()
-    {
-        if ($this->db->tableExists('#__auth_domain'))
-        {
-            $query = "ALTER TABLE `#__auth_domain` ADD INDEX authenticator_idx (authenticator);";
-            $this->db->setQuery($query);
-            $this->db->query();
-        }
-    }
+	public function up()
+	{
+		if ($this->db->tableExists('#__auth_domain'))
+		{
+			$query = "ALTER TABLE `#__auth_domain` ADD INDEX authenticator_idx (authenticator);";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
 
-    public function down()
-    {
-        if ($this->db->tableExists('#__auth_domain'))
-        {
-            $query = "ALTER TABLE `#__auth_domain` INDEX authenticator_idx;";
-            $this->db->setQuery($query);
-            $this->db->query();
-        }
-    }
+	public function down()
+	{
+		if ($this->db->tableExists('#__auth_domain'))
+		{
+			$query = "ALTER TABLE `#__auth_domain` DROP INDEX authenticator_idx;";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
 
 }
