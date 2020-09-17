d8403d3bf39162fa6bfd23ba6352589cb35d3fdb
Damion Junk <junkda@purdue.edu>
2020-09-17 11:24:25 -0500

    Adding white space to make code less aesthetically pleasing

--- core/migrations/Migration20200917000000AuthLinkIndex.php
+++ core/migrations/Migration20200917000000AuthLinkIndex.php
@@ -7,8 +7,10 @@ defined('_HZEXEC_') or die();
 
 class Migration202000171000000AuthLinkIndex extends Base
 {
-    public function up() {
-        if ($this->db->tableExists('#__auth_link')) {
+    public function up()
+    {
+        if ($this->db->tableExists('#__auth_link'))
+        {
             $query = "ALTER TABLE `#__auth_link` ADD INDEX auth_domain_id_idx (auth_domain_id);";
             $this->db->setQuery($query);
             $this->db->query();
@@ -19,8 +21,10 @@ class Migration202000171000000AuthLinkIndex extends Base
         }
     }
 
-    public function down() {
-        if ($this->db->tableExists('#__auth_link')) {
+    public function down()
+    {
+        if ($this->db->tableExists('#__auth_link'))
+        {
             $query = "ALTER TABLE `#__auth_link` DROP INDEX auth_domain_id_idx;";
             $this->db->setQuery($query);
             $this->db->query();
