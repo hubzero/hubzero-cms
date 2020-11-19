3f1b95ebdd2fabdbf67a20fd2b0f6dc67666ad29
lacunapremise <pmeunier100@gmail.com>
2020-11-19 15:55:26 -0500

    fix whitespace

--- core/components/com_resources/site/views/view/tmpl/workshops.php
+++ core/components/com_resources/site/views/view/tmpl/workshops.php
@@ -272,7 +272,7 @@ if ($mode != 'preview')
 			'start'  => Request::getInt('limitstart', 0),
 			'id'     => $this->model->id
 		);
-                // Prevent SQL injection vulnerability by doing input validation on sortby
+		// Prevent SQL injection vulnerability by doing input validation on sortby
 		if (!in_array($filters['sortby'], array('date', 'date_published', 'date_created', 'date_modified', 'title', 'rating', 'ranking', 'random')))
 		{
 			App::abort(403, Lang::txt('Invalid sort value'));
@@ -343,4 +343,4 @@ if ($mode != 'preview')
 			</section>
 		</form>
 	<?php } ?>
-<?php } 
+<?php }
