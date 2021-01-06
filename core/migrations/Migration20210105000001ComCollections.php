f7872163ec08e4dd3940492351ace6c13e3811fc
Ilya Shunko <ishunko@purdue.edu>
2021-01-06 22:07:20 +0000

    Migration to set the default value to true for collections comments

---
+++ core/migrations/Migration20210105000001ComCollections.php
@@ -0,0 +1,50 @@
+<?php
+
+use Hubzero\Content\Migration\Base;
+
+// no direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for updating Collections params.
+ **/
+class Migration20210105000001ComCollections extends Base
+{
+	public function up()
+	{
+		if ($this->db->tableExists('#__extensions'))
+		{
+			// Get the current params
+			$params = Component::params('com_collections');
+			$allow_comments = $params->get('allow_comments');
+
+			// If the comments param is not set, set it to 1-Yes
+			if (!isset($allow_comments))
+			{
+				$params->set('allow_comments', 1);
+				$query = "UPDATE `#__extensions` SET `params`=" . $this->db->quote($params->toString()) . " WHERE `name`= 'com_collections' AND `element` = 'com_collections'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+
+	public function down()
+	{
+		if ($this->db->tableExists('#__extensions'))
+		{
+			// Get the current params
+			$params = Component::params('com_collections');
+			$allow_comments = $params->get('allow_comments');
+
+			// If the comments param is set, remove it
+			if (isset($allow_comments) && method_exists($params, 'remove'))
+			{
+				$params->remove('allow_comments');
+				$query = "UPDATE `#__extensions` SET `params`=" . $this->db->quote($params->toString()) . " WHERE `name`= 'com_collections' AND `element` = 'com_collections'";
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+		}
+	}
+}
