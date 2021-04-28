949d64b6fa3c29b6c56cc60ab6464220bdf0c909
Erich Huebner <ehuebner@ucsd.edu>
2021-04-28 16:33:44 -0700

    Revert "don't copy tags over if item already has a tag"
    This reverts commit d6f5233fa70e243e470f29793b4157991bbb74d0.

--- core/components/com_tags/models/objct.php
+++ core/components/com_tags/models/objct.php
@@ -231,17 +231,6 @@ class Objct extends Relational
 	}
 
 	/**
-	 * Checks if a particular objct has a tag
-	 *
-	 * @param   integer  $tagid  ID of tag to check 
-	 * @return  boolean  True if Objct has tag 
-	 */
-	public static function hasTag($tagid=null)
-	{
-		return (bool)($tagid && self::all()->whereEquals('tagid', $tagid)->rows());
-	}
-
-	/**
 	 * Copy all tags on an object to another object
 	 *
 	 * @param   integer  $oldtagid  ID of tag to be copied
@@ -259,7 +248,7 @@ class Objct extends Relational
 			->whereEquals('tagid', $oldtagid)
 			->rows();
 
-		if ($rows && !self::hasTag($newtagid))
+		if ($rows)
 		{
 			$entries = array();
 
