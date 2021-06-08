21fd7b081409b9978644c1b975f58229eb124979
Nicholas J. Kisseberth <nkisseberth@ucsd.edu>
2021-06-08 14:09:45 -0700

    [domainrestriction] reparent aetextarea to hubzero class structure

--- core/plugins/user/domainrestriction/fields/aetextarea.php
+++ core/plugins/user/domainrestriction/fields/aetextarea.php
@@ -1,41 +1,26 @@
 <?php
 /**
- * @copyright   Copyright (C) 2005 - 2013 MIchael Richey. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE
+ * @package    framework
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
  */
 
-defined('JPATH_PLATFORM') or die;
+namespace Hubzero\Form\Fields;
 
-/**
- * Form Field class for the Joomla Platform.
- * Adjustment to the textarea field which re-implements the translate_default attribute
- */
-class JFormFieldAETextarea extends JFormFieldTextarea
+use Hubzero\Form\Field;
+
+class AETextarea extends Textarea
 {
-	/**
-	 * The form field type.
-	 *
-	 * @var    string
-	 * @since  11.1
-	 */
 	protected $type = 'AETextarea';
 
 	public function setup(SimpleXMLElement $element, $value, $group = null)
 	{
 		$return = parent::setup($element, $value, $group);
+
 		if ($return) {
-			if (version_compare(JVERSION, 3, '>='))
-			{
-				//error_log('j3');
-				$td = ($this->getAttribute('translate_default') == 'true')?true:false;
-				$default = ($this->value == $this->default)?$this->default:false;
-			}
-			else
-			{
-				//error_log('j2');
-				$td = ($element->attributes()->translate_default == 'true')?true:false;
-				$default = ($this->value == $element->attributes()->default)?$element->attributes()->default:false;
-			}
+			$td = ($element->attributes()->translate_default == 'true')?true:false;
+			$default = ($this->value == $element->attributes()->default)?$element->attributes()->default:false;
+
 			if ($td && $default)
 			{
 				$this->value = Lang::txt($default);
