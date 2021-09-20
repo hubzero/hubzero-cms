1d2c9c482b4874914fe38cf4fcd27ca77bc73ac3
Nicholas J. Kisseberth <nkisseberth@ucsd.edu>
2021-09-20 13:15:34 -0700

    fix css file search to stop when file is found look for importfile/exportfile css and image files in toplevel template directory

--- core/components/com_tools/site/controllers/tools.php
+++ core/components/com_tools/site/controllers/tools.php
@@ -161,7 +161,7 @@ class Tools extends SiteController
 		$file = 'forge.png';
 
 		$paths = array(
-			\App::get('template')->path . DS . 'html' . DS . $this->_option . DS . 'images' . DS . $file,
+			\App::get('template')->path . DS . 'images' . DS . $file,
 			dirname(__DIR__) . DS . 'assets' . DS . 'img' . DS . $file,
 			dirname(__DIR__) . DS . 'images' . DS . $file
 		);
@@ -201,7 +201,7 @@ class Tools extends SiteController
 	public function cssTask($css = 'site_css.css')
 	{
 		$paths = array(
-			\App::get('template')->path . DS . 'html' . DS . $this->_option . DS . $css,
+			\App::get('template')->path . DS . 'css' . DS . $css,
 			dirname(__DIR__) . DS . 'assets' . DS . 'css' . DS . $css,
 			dirname(__DIR__) . DS . 'css' . DS . $css
 		);
@@ -213,6 +213,7 @@ class Tools extends SiteController
 			if (file_exists($path))
 			{
 				$file = $path;
+				break;
 			}
 		}
 
