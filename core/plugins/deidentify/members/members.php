2e1962e827f323dde8630e0e69ae0875139ef5fc
Jesse Woo <woojesse@gmail.com>
2023-06-23 15:41:12 -0700

    initial commit of skeleton code

---
+++ core/plugins/deidentify/members/members.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Members Plugin class for Deidentification
+ */
+class plgDeidentifyMembers extends \Hubzero\Plugin\Plugin
+{
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var  boolean
+	 */
+	protected $_autoloadLanguage = true;
+
+	public function onUserDeidentify($user_id) {
+		echo "com_members: deidentify $user_id<br>";
+	}
+}
