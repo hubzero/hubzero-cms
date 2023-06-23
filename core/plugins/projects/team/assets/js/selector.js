8ff286182ef2ce18a43138b8c96ed1c9162347d0
Jesse Woo <woojesse@gmail.com>
2023-06-22 20:52:41 -0700

    update edit and new author org in projects > pub

--- core/plugins/projects/team/assets/js/selector.js
+++ core/plugins/projects/team/assets/js/selector.js
@@ -307,37 +307,37 @@ jQuery(document).ready(function($){
 // Register the event
 jQuery(document).on('ajaxLoad', HUB.ProjectTeamSelect.initialize);
 
+// This retrieval of organization is specifically for the following modal:
+// projects > publications > authors > new author > organization text field
 $(function(){
-	$("#organization").autocomplete({
-		source: function(req, resp){
-			var rorURL = "index.php?option=com_members&controller=profiles&task=getOrganizations&term=";
+	if ($(".rorApiAvailable")[0]){
+		$("#organization").autocomplete({
+			source: function(req, resp){
+				var rorURL = "index.php?option=com_members&controller=profiles&task=getOrganizations&term=";
+				var terms = $("#organization").val();
 
-			var terms = $("#organization").val();
-
-			if (terms.indexOf(" "))
-			{
-				rorURL = rorURL + terms.split(" ").join("+");
-			}
-			else
-			{
-				rorURL = rorURL + terms;
-			}
-
-			$.ajax({
-				url: rorURL,
-				data: null,
-				dataType: "json",
-				success:function(result){
-					resp(result);
-				},
-				error:function(jqXHR, textStatus, errorThrown){
-					console.log(textStatus);
-					console.log(errorThrown);
-					console.log(jqXHR.responseText);
+				if (terms.indexOf(" ")){
+					rorURL = rorURL + terms.split(" ").join("+");
+				} else{
+					rorURL = rorURL + terms;
 				}
-			});
-		},
 
-		appendTo: '#autocomplete-organization',
-	});
+				$.ajax({
+					url: rorURL,
+					data: null,
+					dataType: "json",
+					success:function(result){
+						resp(result);
+					},
+					error:function(jqXHR, textStatus, errorThrown){
+						console.log(textStatus);
+						console.log(errorThrown);
+						console.log(jqXHR.responseText);
+					}
+				});
+			},
+
+			appendTo: '#autocomplete-organization',
+		});
+	}
 });
