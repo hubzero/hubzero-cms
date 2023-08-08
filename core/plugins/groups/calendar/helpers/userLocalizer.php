e62b0ce987b5424513226a8b06f661d89b28c27a
Shawn Rice <rices@purdue.edu>
2023-08-08 14:30:40 -0400

    fix: No dynamic properties

--- core/plugins/groups/calendar/helpers/userLocalizer.php
+++ core/plugins/groups/calendar/helpers/userLocalizer.php
@@ -9,16 +9,42 @@
 defined('_HZEXEC_') or die();
 
 use Hubzero\Utility\Arr;
+use App;
+use Config;
+use User;
 
 class UserLocalizer
 {
+	/**
+	 * Database connection
+	 *
+	 * @var object
+	 */
+	public $db;
 
+	/**
+	 * System timezone
+	 *
+	 * @var string
+	 */
+	public $systemTimezone;
+
+	/**
+	 * Constructor
+	 *
+	 * @return void
+	 */
 	public function __construct()
 	{
 		$this->db = App::get('db');
 		$this->systemTimezone = Config::get('offset');
 	}
 
+	/**
+	 * Get the timezone
+	 *
+	 * @return string
+	 */
 	public function getTimezone()
 	{
 		if (!User::isGuest())
@@ -33,11 +59,15 @@ class UserLocalizer
 		return $timezone;
 	}
 
+	/**
+	 * Get the user's timezone
+	 *
+	 * @return string
+	 */
 	protected function _getUserTimezone()
 	{
-		$userParams = json_decode(User::get('params'), 1);
+		$userParams = json_decode(User::get('params', '[]'), 1);
 
 		return Arr::getValue($userParams, 'timezone', $this->systemTimezone);
 	}
-
 }
