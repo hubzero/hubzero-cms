3b0cfdc751f4c7498c59ead46f3d37bd79b8749a
Jeanette Sperhac <jsperhac@ucsd.edu>
2023-09-20 14:04:55 -0700

    [NCN-437] Migration script creates 'campaign' database table

---
+++ core/components/com_newsletter/migrations/Migration20230920000000ComNewsletter.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for managing campaigns and their secrets via com_newsletter
+ **/
+class Migration20230920000000ComNewsletter extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		if (!$this->db->tableExists('campaign'))
+		{
+			$query = "CREATE TABLE IF NOT EXISTS `campaign` (
+    			id INT(11) NOT NULL AUTO_INCREMENT,
+    			title VARCHAR(50),
+    			description TEXT,
+    			campaign_date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+				secret CHAR(32) UNIQUE NULL,
+				PRIMARY KEY (id)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		if ($this->db->tableExists('campaign'))
+		{
+			$query = "DROP TABLE IF EXISTS `campaign`;";
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+}
