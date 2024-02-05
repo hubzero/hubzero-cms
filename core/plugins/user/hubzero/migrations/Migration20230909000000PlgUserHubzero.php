cacb7fa38edc15914672364222b42d2d1d79d3d7
Jeanette Sperhac <jsperhac@ucsd.edu>
2024-02-05 12:47:24 -0800

    [NCN-633] Check for 'secret' column, but don't create it.

--- core/plugins/user/hubzero/migrations/Migration20230909000000PlgUserHubzero.php
+++ core/plugins/user/hubzero/migrations/Migration20230909000000PlgUserHubzero.php
@@ -13,9 +13,9 @@ defined('_HZEXEC_') or die();
 /**
  * Migration script for adding User Secret to plugin
  *
- * This migration script creates a jos_users column for the plugin to populate
- * with a unique user secret, on user login. On 'down' migration the column
- * is dropped from the table.
+ * This migration script checks for jos_users column for the plugin to populate
+ * with a unique user secret, on user login. Creation of this column is done
+ * on up migration in com_members component.
  *
  **/
 class Migration20230909000000PlgUserHubzero extends Base
@@ -24,16 +24,16 @@ class Migration20230909000000PlgUserHubzero extends Base
 	static $tableName = '#__users';
 
 	// specify column to create
-	static $columnData = 	[ ['name' => 'secret',
-								'type' => 'char(32)',
-								'restriction' => 'UNIQUE',
-								'default' => 'NULL'],
-							];
+	static $columnData = [['name' => 'secret',
+		'type' => 'char(32)',
+		'restriction' => 'UNIQUE',
+		'default' => 'NULL'],
+	];
 
 	/**
 	 * Up
 	 *
-	 * Create 'secret' column in jos_users if needed.
+	 * Check for 'secret' column in jos_users table.
 	 *
 	 * Note that we have logging callbacks, per
 	 * https://help.hubzero.org/documentation/22/webdevs/database/migrations
@@ -44,20 +44,17 @@ class Migration20230909000000PlgUserHubzero extends Base
 		$tableName = self::$tableName;
 		$columnData = self::$columnData;
 
-		// Add column to jos_users table to hold user secret
-		// If the table exists, add column if it is absent:
+		// Check for column in jos_users table to hold user secret
 		if ($this->db->tableExists($tableName))
 		{
-			if (!$this->db->tableHasField($tableName, $columnData['name'])) {
-				$this->log('No column `secret` found in table `jos_users`, creating...', 'info');
+			// If the table exists, and column is absent, err out:
+			if (!$this->db->tableHasField($tableName, $columnData[0]['name'])) {
+				$this->log('No column `secret` found in table `jos_users`, stopping...', 'error');
+				$this->setError('Run migration for com_members component first');
 
-				// generate and run query to create column
-				$query = $this->_generateSafeAddColumns($tableName, $columnData);
-				$this->_queryIfTableExists($tableName, $query);
-
+			} else {
+				$this->log('Found `secret` column in table `jos_users`, success...', 'success');
 			}
-			// Generate and save user secrets
-			$this->generateUserSecrets();
 		}
 	}
 
@@ -69,46 +66,6 @@ class Migration20230909000000PlgUserHubzero extends Base
 	 **/
 	public function down()
 	{
-		$tableName = self::$tableName;
-		$columnData = self::$columnData;
-
-		// generate query to drop column from table:
-		$query = $this->_generateSafeDropColumns($tableName, $columnData);
-
-		// execute the query, if column is found in the specified table:
-		$this->_queryIfTableExists($tableName, $query);
-
-		$this->log('Column `secret` dropped from table `jos_users`', 'info');
-	}
-
-	/**
-	 * generateUserSecrets()
-	 *
-	 * Populate 'secret' column in jos_users table for those users who have
-	 * logged in over the last year, but have no secret set. Each secret is a
-	 * unique, random 32-character string.
-	 *
-	 **/
-	private function generateUserSecrets()
-	{
-		// User secret will have length 32 characters:
-		$secretLength = 32;
-
-		// Criteria: create secrets for users that have logged in during the last year
-		$criteria = 'DATE_SUB(CURDATE(), INTERVAL 1 YEAR';
-
-		// Fetch ids for all such users who have no secret
-		$targetUsers = \Hubzero\User\User::all()
-						->where('lastvisitdate', '>', $criteria)
-						->whereIsNull('secret');
-
-		// Create and save a unique secret for each such user:
-		foreach ($targetUsers as $oneUser)
-		{
-			$newSecret = \Hubzero\User\Password::genRandomPassword($secretLength);
-			$oneUser->set('secret', $newSecret);
-			$oneUser->save();
-		}
-		$this->log('Saved new secrets for target users', 'info');
+		// No-op
 	}
 }
