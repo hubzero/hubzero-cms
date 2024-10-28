3ddd1d55e46f92dca6bb84648df392ffb2bb9592
Archie To <toanhminh0412@gmail.com>
2024-10-28 18:49:36 +0000

    Fix deprecated code

--- core/components/com_events/site/controllers/events.php
+++ core/components/com_events/site/controllers/events.php
@@ -842,7 +842,7 @@ class Events extends SiteController
 			'row' => $row,
 			'authorized' => $authorized,
 			'fields' => $fields,
-			'config' => $config,
+			'config' => $this->config,
 			'categories' => $categories,
 			'offset' => $offset,
 			'tags' => $tags,
@@ -1826,6 +1826,7 @@ class Events extends SiteController
 		}
 		else
 		{
+			$row->id = null;
 			$state = 'add';
 
 			// New - set created info
