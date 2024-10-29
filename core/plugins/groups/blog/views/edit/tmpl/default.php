eb7fe3d5bc77bb709666a2b62b5833501b1a0b7e
Archie To <toanhminh0412@gmail.com>
2024-10-29 21:55:30 +0000

    Fix deprecated code
    Avoid passing null to stripslashes

--- core/plugins/groups/blog/views/edit/tmpl/default.php
+++ core/plugins/groups/blog/views/edit/tmpl/default.php
@@ -40,7 +40,7 @@ $this->css()
 		<div class="form-group">
 			<label<?php if ($this->task == 'save' && !$this->entry->get('title')) { echo ' class="fieldWithErrors"'; } ?>>
 				<?php echo Lang::txt('PLG_GROUPS_BLOG_TITLE'); ?> <span class="required"><?php echo Lang::txt('JREQUIRED'); ?></span>
-				<input type="text" class="form-control" name="entry[title]" size="35" value="<?php echo $this->escape(stripslashes($this->entry->get('title'))); ?>" />
+				<input type="text" class="form-control" name="entry[title]" size="35" value="<?php echo $this->escape(stripslashes($this->entry->get('title', ''))); ?>" />
 			</label>
 			<?php if ($this->task == 'save' && !$this->entry->get('title')) { ?>
 				<p class="error"><?php echo Lang::txt('PLG_GROUPS_BLOG_ERROR_PROVIDE_TITLE'); ?></p>
