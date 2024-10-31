2ee87dae23fc129c0bfd305aecdfc53c7c1617ce
Archie To <toanhminh0412@gmail.com>
2024-10-31 00:43:56 +0000

    Fix deprecated code
    Avoid passing null to strtolower

--- core/components/com_publications/models/attachments/file.php
+++ core/components/com_publications/models/attachments/file.php
@@ -232,7 +232,7 @@ class File extends Base
 				{
 					$where .= DS . $configs->bundleDirectory;
 				}
-				if ($configs->directory && strtolower($configs->bundleDirectory) != strtolower($configs->directory))
+				if ($configs->directory && strtolower($configs->bundleDirectory ? $configs->bundleDirectory : '') != strtolower($configs->directory))
 				{
 					$where .= $configs->directory != $pub->secret ? DS . $configs->directory : '';
 				}
