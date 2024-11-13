b24346e181a9136734da5f0f169f8a1b4684eb9f
Archie To <toanhminh0412@gmail.com>
2024-11-13 18:36:54 +0000

    Put curly brackets on their own line to maintain coding style

--- core/components/com_wiki/models/page.php
+++ core/components/com_wiki/models/page.php
@@ -638,7 +638,9 @@ class Page extends Relational
 			}
 
 			$this->set('path', implode('/', $path));
-		} else {
+		}
+		else
+		{
 			// Must set parent as 'parent' field requires an int in the database
 			$this->set('parent', 0);
                        
