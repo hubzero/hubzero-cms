13dfe5ea21d4e2db8e8e634fa756cc11231acf34
nkissebe <nkissebe@gmail.com>
2025-10-29 15:51:32 -0400

    Update getGroups method to return empty arrays
    Return an empty array instead of false for unauthenticated users and empty results.

--- core/libraries/Hubzero/User/Helper.php
+++ core/libraries/Hubzero/User/Helper.php
@@ -281,10 +281,10 @@ class Helper
 	public static function getGroups($uid, $type='all', $cat = null)
 	{
 		//return empty groups if user is not authenticated
-		if ($uid == 0)
-                {
-                        return false;
-                }
+		if ($uid == 0
+		{
+			return array();
+		}
 		
 		$db = \App::get('db');
 
@@ -329,7 +329,7 @@ class Helper
 
 		if (empty($result))
 		{
-			return false;
+			return array();
 		}
 
 		return $result;
