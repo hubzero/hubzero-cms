0a3162947cbbbddd03dae758ed3cd16c25d62b02
Daniel Mejia <denphi@denphi.com>
2025-11-15 12:00:10 -0500

    Update Processor.php

--- core/libraries/Hubzero/Image/Processor.php
+++ core/libraries/Hubzero/Image/Processor.php
@@ -141,7 +141,7 @@ class Processor extends Obj
 	{
 		try
 		{
-			if (!is_readable($this->source)) {
+			if (!is_file($this->source) || !is_readable($this->source)) {
 				return false;
 			}
 			$image_atts = getimagesize($this->source);
@@ -562,7 +562,7 @@ class Processor extends Obj
 	 */
 	public function display()
 	{
-		if (!is_readable($this->source)) {
+		if (!is_file($this->source) || !is_readable($this->source)) {
 				return;
 		}
 		if (exif_imagetype($this->source) == false)
