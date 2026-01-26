bd24731516a03fdffb885e8efa297bc253f4ae36
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-01-26 14:48:08 -0500

    Fix loadById method call on Relational model in projects notes plugin
    The Page model extends Relational which doesn't have a loadById method.
    Changed to use the proper Relational ORM pattern with Page::one().
    
    (cherry picked from commit e354795145b83dd9ea07bdd4c2ba0799ecedbe70)

--- core/plugins/projects/notes/views/publist/tmpl/default.php
+++ core/plugins/projects/notes/views/publist/tmpl/default.php
@@ -18,11 +18,15 @@ if (count($this->items) > 0) {
 		<?php foreach ($this->items as $item) {
 			$ref = json_decode($item->reference);
 
-			if (isset($ref->pageid) && $this->page->loadById( $ref->pageid ))
+			if (isset($ref->pageid))
 			{
+				$page = \Components\Wiki\Models\Page::one($ref->pageid);
+				if ($page && $page->get('id'))
+				{
 		?>
-		<li class="notes"><a href="<?php echo Route::url($this->model->link('stamp') . '&s=' . $item->stamp); ?>"><?php echo $this->page->title; ?></li>
+		<li class="notes"><a href="<?php echo Route::url($this->model->link('stamp') . '&s=' . $item->stamp); ?>"><?php echo $page->get('title'); ?></li>
 		<?php }
+			}
 		} ?>
 	</ul>
 </div>
