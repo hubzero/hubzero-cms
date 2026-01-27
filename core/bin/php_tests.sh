36a39a7cd5a6a58cca275f4ce14698f777150a67
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-01-27 12:06:18 -0500

    Modernize Travis CI configuration and php_tests.sh
    - Update PHP versions from 7.0 to 8.2 and 8.3
    - Replace deprecated custom HubZero standards with PSR-12
    - Use composer-installed tools from core/vendor/bin instead of
      manually requiring phpcs and cloning external standards repo
    - Replace manual php -l loop with parallel-lint for faster linting
    - Make php_tests.sh portable by detecting vendor path relative to
      script location, allowing it to run from any working directory
    - Add vendor caching for faster CI builds
    
    (cherry picked from commit bf9257a583880776aa9dd6b7dd9ea0099f1c4145)

--- core/bin/php_tests.sh
+++ core/bin/php_tests.sh
@@ -1,15 +1,24 @@
+#!/bin/bash
+# PHP linting and code style checker for CI
+# Usage: php_tests.sh [file1.php file2.php ...]
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CORE_DIR="$(dirname "$SCRIPT_DIR")"
+VENDOR_BIN="$CORE_DIR/vendor/bin"
+
 update_status () { if [ $1 -ne 0 ]; then status=$1; fi }
 status=0
-php_args=`echo $@ | grep \.php$`
+
+# Filter to only .php files
+php_args=$(echo "$@" | tr ' ' '\n' | grep '\.php$' | tr '\n' ' ')
 if [ -z "$php_args" ]; then exit 0; fi
 
-vendor/squizlabs/php_codesniffer/scripts/phpcs -np --standard=~/build/standards/Php/ruleset.xml --ignore=*/tests/*,/core/libraries/*,/core/bin/* $php_args
+# Run code style checks (PSR-12)
+"$VENDOR_BIN/phpcs" -np --standard=PSR12 --ignore=*/tests/*,*/bin/* $php_args
 update_status $?
-for file in $php_args; do
-	php -l $file 2>&1 1>temp
-	update_status $?
-done
-sed "/No syntax errors detected/d" -i temp
-cat temp
-rm temp
+
+# Run PHP syntax linting
+"$VENDOR_BIN/parallel-lint" --no-progress $php_args
+update_status $?
+
 exit $status
