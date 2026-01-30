ec4f6a9c1a1145978f5e0c83d3da81dd9d916720
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-01-30 12:41:01 -0500

    Add vendor check to bootstrap web installer
    If vendor dependencies are missing, redirect to the bootstrap installer
    instead of failing with a fatal error.
    
    (cherry picked from commit ba4b71234b947174e158cfab1bc988e6607ab44d)

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
 define('PATH_ROOT', isset($_SERVER['DOCUMENT_ROOT']) && $_SERVER['DOCUMENT_ROOT'] ? $_SERVER['DOCUMENT_ROOT'] : __DIR__);
 define('PATH_CORE', isset($_ENV['PATH_CORE']) && $_ENV['PATH_CORE'] ? $_ENV['PATH_CORE'] : PATH_ROOT . '/core');
 
+// Check if vendor dependencies are installed
+if (!file_exists(PATH_CORE . '/vendor/autoload.php')) {
+    // Run the bootstrap installer to handle missing vendor
+    require PATH_CORE . '/bootstrap/Install/web/bootstrap.php';
+    exit;
+}
+
 require PATH_CORE . '/vendor/autoload.php';
 
 (new Hubzero\Base\Application())->run();
