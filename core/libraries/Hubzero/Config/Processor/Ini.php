c88ef6d394b1198f09fe1bdf6b1b151b10f7dbc6
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-01-30 18:49:14 -0500

    Check file exists before parsing INI to avoid PHP warning
    Add is_file() check in Ini::parse() before calling parse_ini_file()
    to throw ParseException without triggering a PHP warning when the
    file doesn't exist.
    
    (cherry picked from commit 4ba47f80ca4a0e80962251b99fc9e438ed3f8062)

--- core/libraries/Hubzero/Config/Processor/Ini.php
+++ core/libraries/Hubzero/Config/Processor/Ini.php
@@ -45,6 +45,11 @@ class Ini extends Base
 	 */
 	public function parse($path)
 	{
+		if (!is_file($path))
+		{
+			throw new ParseException(['message' => "File not found: $path"]);
+		}
+
 		$data = parse_ini_file($path, true, INI_SCANNER_RAW);
 
 		if (!$data)
