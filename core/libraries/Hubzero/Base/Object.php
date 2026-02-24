15409513be0de3b48f7d3ec89ff4a579e21ca3de
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-02-24 15:52:17 -0500

    Remove empty and unreferenced class files
    Hubzero\Base\Object held only a namespace, and com_tags' object.php only
    forwarded to objct.php, where the Objct class lives and which every
    caller names directly. Nothing includes either file or references either
    name.
    
    Three Geocode\Result classes, Country and its two result factories, are
    referenced by nothing either.

--- core/libraries/Hubzero/Base/Object.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @package    framework
- * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
- * @license    http://opensource.org/licenses/MIT MIT
- */
-
-namespace Hubzero\Base;
