3fe23dc5cba45443aad388c6e0f97d0abd545c72
Pascal Meunier <pmeunier@ucsd.edu>
2026-04-27 16:44:44 -0400

    created plugin to add users to a location_us group if they login from a US IP address

---
+++ core/plugins/user/us/migrations/Migration20260421000001PlgUserUs.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding User - US plugin
+ **/
+class Migration20260421000001PlgUserUs extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->addPluginEntry('user', 'us');
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->deletePluginEntry('user', 'us');
+	}
+}
