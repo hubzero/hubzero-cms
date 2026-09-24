d5615a78be88314d02fa499bfac8b15903ae0412
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-09-24 10:08:20 -0400

    com_forum: tolerate an absent parent or lft in the post lft automatic

--- core/components/com_forum/models/post.php
+++ core/components/com_forum/models/post.php
@@ -276,7 +276,8 @@ class Post extends Relational
 	 */
 	public function automaticLft($data)
 	{
-		if (!$data['parent'])
+		// save paths bind an allowlist, so parent/lft may be absent
+		if (empty($data['parent']) || !isset($data['lft']))
 		{
 			$data['lft'] = 0;
 		}
