89248d82a5c278c254e28cc02a1da5197a455858
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-09-24 17:33:20 -0400

    com_cron: release a manually run job at shutdown if it dies mid-run

--- core/components/com_cron/admin/controllers/jobs.php
+++ core/components/com_cron/admin/controllers/jobs.php
@@ -282,6 +282,39 @@ class Jobs extends AdminController
 		$output = new stdClass;
 		$output->jobs = array();
 
+		// A fatal inside a job skips the finally below and left the job marked
+		// active until STALE_ACTIVE_SECONDS (6 hours) passed. Release whatever
+		// is in flight at shutdown, as the site tick does.
+		$inFlight = new stdClass;
+		$inFlight->job = null;
+
+		register_shutdown_function(function () use ($inFlight)
+		{
+			if (!$inFlight->job)
+			{
+				return;
+			}
+
+			$next = null;
+			try
+			{
+				$next = $inFlight->job->nextRun();
+			}
+			catch (\Throwable $e)
+			{
+				// leave next_run as-is if the recurrence can't be parsed
+			}
+
+			try
+			{
+				$inFlight->job->release(gmdate('Y-m-d H:i:s'), $next);
+			}
+			catch (\Throwable $e)
+			{
+				// best-effort at shutdown
+			}
+		});
+
 		// Loop through each ID
 		foreach ($ids as $id)
 		{
@@ -301,6 +334,8 @@ class Jobs extends AdminController
 			}
 
 			$job->mark('start_run');
+
+			$inFlight->job = $job;
 
 			try
 			{
@@ -325,6 +360,8 @@ class Jobs extends AdminController
 				}
 
 				$job->release(Date::toSql(), $next);
+
+				$inFlight->job = null;
 			}
 
 			$output->jobs[] = $job->toArray();
