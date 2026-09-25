f4eacebcbcd93c0c775567af4dc5fde92e582ddd
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-09-24 21:23:26 -0400

    com_dataviewer: dv_auth() declares the $dd global it reads

--- core/components/com_dataviewer/site/lib/auth.php
+++ core/components/com_dataviewer/site/lib/auth.php
@@ -10,7 +10,7 @@ defined('_HZEXEC_') or die();
 
 function dv_auth()
 {
-	global $dv_conf;
+	global $dv_conf, $dd;
 
 	if (isset($dd['acl']['allowed_users']) && (is_array($dd['acl']['allowed_users']) || $dd['acl']['allowed_users'] === false || $dd['acl']['allowed_users'] == 'registered')) {
 		$dv_conf['acl']['allowed_users'] = $dd['acl']['allowed_users'];
