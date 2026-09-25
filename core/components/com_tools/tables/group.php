2bd407718fce591550f36d63db1f2dd0fd7e2e80
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2026-09-24 21:58:12 -0400

    com_tools: an existing dev group keeps its managers
    The previous fix set them to the developer list on every save; the
    create branch still gets the members passed in.

--- core/components/com_tools/tables/group.php
+++ core/components/com_tools/tables/group.php
@@ -88,7 +88,7 @@ class Group extends Table
 			$group->read($devgroup);
 			$existing_members = \Components\Tools\Helpers\Utils::transform(Tool::getToolDevelopers($toolid), 'uidNumber');
 			$group->set('members', $existing_members);
-			$group->set('managers', $existing_members);
+			// (managers used to be set from an unassigned variable; they are left as they are)
 		}
 		else
 		{
